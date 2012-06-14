79b96a913d9331a22cf1eaeed72315fb4067eaa3
lasconic <lasconic@gmail.com>
2012-06-14 18:18:03 +0200

    zip test results recursively

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -4,7 +4,7 @@ PROC_RET=$?
 make reporthtml
 REVISION=`git rev-parse --short HEAD`
 mv report/html $REVISION
-zip $REVISION.zip $REVISION
+zip -r $REVISION.zip $REVISION
 curl -F zip_file=@$REVISION.zip  http://prereleases.musescore.org/test/index.php
 echo "Test results: http://prereleases.musescore.org/test/$REVISION/"
 
