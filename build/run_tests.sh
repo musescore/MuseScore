9baceea2807ee19f5fe00f4516a1c7e98cec5836
lasconic <lasconic@gmail.com>
2012-06-14 17:56:33 +0200

    last fixes for travis ci

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -3,9 +3,9 @@ xvfb-run ctest
 PROC_RET=$?
 make reporthtml
 REVISION=`git rev-parse --short HEAD`
-mv reporthtml $REVISION
+mv report/html $REVISION
 zip $REVISION.zip $REVISION
 curl -F zip_file=@$REVISION.zip  http://prereleases.musescore.org/test/index.php
-echo "Test results: http://prereleasese.musescore.org/test/$REVISION/"
+echo "Test results: http://prereleases.musescore.org/test/$REVISION/"
 
 exit $PROC_RET
