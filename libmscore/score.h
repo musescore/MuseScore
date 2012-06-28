e3d76d3aaaa4f41b108fcd13e376961c654effd0
Werner Schweer <ws@wschweer.de>
2012-06-28 09:28:13 +0200

    Ctrl+drop time signature now creates a 'local' time signature

--- libmscore/score.h
+++ libmscore/score.h
@@ -523,7 +523,7 @@ class Score {
       void update();
 
       void cmdRemoveTimeSig(TimeSig*);
-      void cmdAddTimeSig(Measure*, int staffIdx, TimeSig*);
+      void cmdAddTimeSig(Measure*, int staffIdx, TimeSig*, bool local);
 
       void setUpdateAll(bool v = true) { _updateAll = v;   }
       void setLayoutAll(bool val);
