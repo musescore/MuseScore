b5e9044bcbea62ef6ddade3e0c9905f118b264b2
lasconic <lasconic@gmail.com>
2012-07-13 11:22:42 +0200

    fix helpBrowser toolbar layout

--- mscore/helpBrowser.cpp
+++ mscore/helpBrowser.cpp
@@ -21,6 +21,8 @@ HelpBrowser::HelpBrowser(QWidget* parent)
       {
       view    = new QWebView;
       toolbar = new QWidget;
+      toolbar->setSizePolicy(QSizePolicy::Expanding, 
+      QSizePolicy::Fixed);
       QVBoxLayout* l = new QVBoxLayout;
       l->addWidget(toolbar);
       l->addWidget(view);
