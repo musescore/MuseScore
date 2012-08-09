d506c791babe04f1b35cb26b23515a6d7855d568
lasconic <lasconic@gmail.com>
2012-08-09 11:19:21 +0200

    remove warning for quarter note in Rest::getSymbol

--- libmscore/rest.cpp
+++ libmscore/rest.cpp
@@ -279,6 +279,8 @@ int Rest::getSymbol(TDuration::DurationType type, int line, int lines, int* yoff
                   return (line <= -2 || line >= (lines - 1)) ? outsidewholerestSym : wholerestSym;
             case TDuration::V_HALF:
                   return (line <= -3 || line >= (lines - 2)) ? outsidehalfrestSym : halfrestSym;
+            case TDuration::V_QUARTER:
+                  return rest4Sym;
             case TDuration::V_EIGHT:
                   return rest8Sym;
             case TDuration::V_16TH:
