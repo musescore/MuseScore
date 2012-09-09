42278934f59d25fbbaa211481e7404eefc5a222f
lasconic <lasconic@gmail.com>
2012-09-09 22:05:53 +0200

    update Qt to 4.8.1 for windows build

--- build/mingw32.mingw.cmake
+++ build/mingw32.mingw.cmake
@@ -5,7 +5,7 @@
 set (CMAKE_SYSTEM_NAME "Windows")
 
 set (CROSS C:/QtSDK/mingw)
-set (CROSSQT C:/QtSDK/Desktop/Qt/4.8.0/mingw)
+set (CROSSQT C:/QtSDK/Desktop/Qt/4.8.1/mingw)
 
 set (CMAKE_C_COMPILER     ${CROSS}/bin/gcc.exe)
 set (CMAKE_CXX_COMPILER   ${CROSS}/bin/g++.exe)
