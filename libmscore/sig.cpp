a3c13ff01cfbad4d81c9f136d4415a56e2efc931
Werner Schweer <ws@wschweer.de>
2012-09-10 11:54:46 +0200

    fix 1.2 import of parts & grace notes

--- libmscore/sig.cpp
+++ libmscore/sig.cpp
@@ -166,13 +166,13 @@ void TimeSigMap::tickValues(int t, int* bar, int* beat, int* tick) const
 //    This is not reentrant and only for debugging!
 //---------------------------------------------------------
 
-const char* TimeSigMap::pos(int t)
+const char* TimeSigMap::pos(int t) const
       {
       static char* s = 0;
       delete s;
       int bar, beat, tick;
       tickValues(t, &bar, &beat, &tick);
-      QString ss = QString("%1:%2:%3").arg(bar).arg(beat).arg(tick);
+      QString ss = QString("%1:%2:%3").arg(bar+1).arg(beat).arg(tick);
       s = strdup(qPrintable(ss));
       return s;
       }
