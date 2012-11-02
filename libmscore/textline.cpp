d328ca8f90de48eb9d7be56b2bc48427a6832036
Maurizio Gavioli <mmg@vistamaresoft.com>
2012-11-02 19:02:30 +0100

    TextLineSegment does not keep user offset across saving and re-opening
    Fixed by adding adjustRedPos() to layout().

--- libmscore/textline.cpp
+++ libmscore/textline.cpp
@@ -234,6 +234,7 @@ void TextLineSegment::layout()
                   y1 = h;
             }
       setbbox(QRectF(.0, y1, pp2.x(), y2 - y1));
+      adjustReadPos();
       }
 
 //---------------------------------------------------------
