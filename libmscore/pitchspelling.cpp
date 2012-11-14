985ee4cd4e9f90ccd1bfacccfaace53ba40a3d91
Maurizio M. Gavioli <mmg@vistamaresoft.com>
2012-11-14 13:14:56 +0100

    Adds diatonic mode to the Transposition dlg box.
    Has option for keeping altered degrees or not.
    Includes transposition of chord names, probably to be improved.

--- libmscore/pitchspelling.cpp
+++ libmscore/pitchspelling.cpp
@@ -29,7 +29,7 @@
 
 bool tpcIsValid(int val)
       {
-      return val >= -1 && val <= 33;
+      return val >= TPC_MIN && val <= TPC_MAX;
       }
 
 //---------------------------------------------------------
@@ -52,14 +52,41 @@ int step2tpc(int step, AccidentalVal alter)
              5, 12, 19, 26, 33,  // B
              };
 
-      int i = step * 5 + 2 + alter;
+      int i = step*5 + alter+2;
       Q_ASSERT(i >= 0 && (i < int(sizeof(spellings)/sizeof(*spellings))));
       return spellings[i];
-      };
+      }
 
 int step2tpc(int step)
       {
       return step2tpc(step);
+      }
+
+static const int tpcByStepAndKey[NUM_OF_KEYS][STEP_DELTA_OCTAVE] = {
+// step    C        D        E        F        G        A        B        Key
+      { TPC_C_B, TPC_D_B, TPC_E_B, TPC_F_B, TPC_G_B, TPC_A_B, TPC_B_B}, // Cb
+      { TPC_C_B, TPC_D_B, TPC_E_B, TPC_F,   TPC_G_B, TPC_A_B, TPC_B_B}, // Gb
+      { TPC_C,   TPC_D_B, TPC_E_B, TPC_F,   TPC_G_B, TPC_A_B, TPC_B_B}, // Db
+      { TPC_C,   TPC_D_B, TPC_E_B, TPC_F,   TPC_G,   TPC_A_B, TPC_B_B}, // Ab
+      { TPC_C,   TPC_D,   TPC_E_B, TPC_F,   TPC_G,   TPC_A_B, TPC_B_B}, // Eb
+      { TPC_C,   TPC_D,   TPC_E_B, TPC_F,   TPC_G,   TPC_A,   TPC_B_B}, // B
+      { TPC_C,   TPC_D,   TPC_E,   TPC_F,   TPC_G,   TPC_A,   TPC_B_B}, // F
+      { TPC_C,   TPC_D,   TPC_E,   TPC_F,   TPC_G,   TPC_A,   TPC_B},   // C
+      { TPC_C,   TPC_D,   TPC_E,   TPC_F_S, TPC_G,   TPC_A,   TPC_B},   // G
+      { TPC_C_S, TPC_D,   TPC_E,   TPC_F_S, TPC_G,   TPC_A,   TPC_B},   // D
+      { TPC_C_S, TPC_D,   TPC_E,   TPC_F_S, TPC_G_S, TPC_A,   TPC_B},   // A
+      { TPC_C_S, TPC_D_S, TPC_E,   TPC_F_S, TPC_G_S, TPC_A,   TPC_B},   // E
+      { TPC_C_S, TPC_D_S, TPC_E,   TPC_F_S, TPC_G_S, TPC_A_S, TPC_B},   // H
+      { TPC_C_S, TPC_D_S, TPC_E_S, TPC_F_S, TPC_G_S, TPC_A_S, TPC_B},   // F#
+      { TPC_C_S, TPC_D_S, TPC_E_S, TPC_F_S, TPC_G_S, TPC_A_S, TPC_B_S}, // C#
+};
+
+int step2tpcByKey(int step, int key)
+      {
+      while (step < 0)        step+= STEP_DELTA_OCTAVE;
+      while (key < KEY_MIN)   key += KEY_DELTA_ENHARMONIC;
+      while (key > KEY_MAX)   key -= KEY_DELTA_ENHARMONIC;
+      return tpcByStepAndKey[key-KEY_MIN][step % STEP_DELTA_OCTAVE];
       }
 
 //---------------------------------------------------------
@@ -70,10 +97,23 @@ int tpc2step(int tpc)
       {
       // 14 - C
       // 15 % 7 = 1
-      //                            f  c  g  d  a  e  b
-      static const int steps[7] = { 3, 0, 4, 1, 5, 2, 6 };
-      // TODO: optimize +1
-      return steps[(tpc+1) % 7];
+      //                                            f  c  g  d  a  e  b
+      static const int steps[STEP_DELTA_OCTAVE] = { 3, 0, 4, 1, 5, 2, 6 };
+      // TODO: optimize -TCP_MIN
+      return steps[(tpc-TPC_MIN) % STEP_DELTA_OCTAVE];
+// without a table, could also be rendered as:
+//      return ((tpc-TPC_MIN) * STEP_DELTA_TPC) / STEP_DELTA_OCTAVE + TPC_FIRST_STEP;
+      }
+
+//---------------------------------------------------------
+//   tpc2stepByKey
+//---------------------------------------------------------
+
+int tpc2stepByKey(int tpc, int key, int* pAlter)
+      {
+      if(pAlter)
+            *pAlter = tpc2alterByKey(tpc, key);
+      return tpc2step(tpc);
       }
 
 //---------------------------------------------------------
@@ -100,6 +140,41 @@ int step2tpc(const QString& stepName, AccidentalVal alter)
       }
 
 //---------------------------------------------------------
+//   step2deltaPitchByKey
+//
+// returns the delta pitch above natural C for the given step in the given key
+// step: 0 - 6
+// key: -7 - +7
+//---------------------------------------------------------
+
+static const int pitchByStepAndKey[NUM_OF_KEYS][STEP_DELTA_OCTAVE] = {
+// step  C   D   E   F   G   A   B           Key
+      { -1,  1,  3,  4,  6,  8, 10},      // Cb
+      { -1,  1,  3,  5,  6,  8, 10},      // Gb
+      {  0,  1,  3,  5,  6,  8, 10},      // Db
+      {  0,  1,  3,  5,  7,  8, 10},      // Ab
+      {  0,  2,  3,  5,  7,  8, 10},      // Eb
+      {  0,  2,  3,  5,  7,  9, 10},      // B
+      {  0,  2,  4,  5,  7,  9, 10},      // F
+      {  0,  2,  4,  5,  7,  9, 11},      // C
+      {  0,  2,  4,  6,  7,  9, 11},      // G
+      {  1,  2,  4,  6,  7,  9, 11},      // D
+      {  1,  2,  4,  6,  8,  9, 11},      // A
+      {  1,  3,  4,  6,  8,  9, 11},      // E
+      {  1,  3,  4,  6,  8, 10, 11},      // H
+      {  1,  3,  5,  6,  8, 10, 11},      // F#
+      {  1,  3,  5,  6,  8, 10, 12},      // C#
+};
+
+int step2deltaPitchByKey(int step, int key)
+      {
+      while (step < 0)        step+= STEP_DELTA_OCTAVE;
+      while (key < KEY_MIN)   key += KEY_DELTA_ENHARMONIC;
+      while (key > KEY_MAX)   key -= KEY_DELTA_ENHARMONIC;
+      return pitchByStepAndKey[key-KEY_MIN][step % STEP_DELTA_OCTAVE];
+      }
+
+//---------------------------------------------------------
 //   tpc2pitch
 //---------------------------------------------------------
 
@@ -120,6 +195,25 @@ int tpc2pitch(int tpc)
             abort();
             }
       return pitches[tpc];
+      }
+
+//---------------------------------------------------------
+//   tpc2alterByKey
+//
+// returns the alteration (-3 to 3) of a given tpc in the given key
+// to understand the formula:
+//    in the lowest key (CbMaj), the first 7 tpcs (Fbb to Bbb; tpc-TPC_MIN: 0 to 7)
+//          are all 1 semitone below the key degree (alter = -1)
+//    the second 7 tpcs (Fb to Bb; tpc-TPC_MIN: 8 to 13) are all at key degrees (alter = 0) and so on up to 3
+//    thus, for CbMaj, ((tpc-TPC_MIN - 13) / 7) - 1 is the result
+//    for each next key, the result is shifted 1 tpc 'down' in the tpc sequence:
+//          GbMaj: Fbb => -2, Cbb to Fb => -1 and so on
+//          DbMaj: Fbb,Cbb => -2, Gbb to Cb => -1 and so on
+//          and so on
+//---------------------------------------------------------
+
+int tpc2alterByKey(int tpc, int key) {
+      return (tpc-TPC_MIN-((TPC_DELTA_SEMITONE*2)-1) - (key-KEY_MIN)) / TPC_DELTA_SEMITONE;
       }
 
 //---------------------------------------------------------
@@ -475,14 +569,14 @@ int computeWindow(const QList<Note*>& notes, int start, int end)
             int pa    = 0;
             int pb    = 0;
             int l     = pitch[0] * 2 + (i & 1);
-            if (l < 0 || l > int(sizeof(tab1)/sizeof(*tab1)))
+            if (l < 0 || l > (int)(sizeof(tab1)/sizeof(*tab1)))
                   abort();
             int lof1a = tab1[l];
             int lof1b = tab2[l];
 
             for (int k = 1; k < 10; ++k) {
                   int l = pitch[k] * 2 + ((i & (1 << k)) >> k);
-                  if (l < 0 || l > int(sizeof(tab1)/sizeof(*tab1)))
+                  if (l < 0 || l > (int)(sizeof(tab1)/sizeof(*tab1)))
                         abort();
                   int lof2a = tab1[l];
                   int lof2b = tab2[l];
@@ -685,3 +779,45 @@ int pitch2tpc(int pitch, int key)
       return ptab[key+7][step];
       }
 
+//---------------------------------------------------------
+//   pitch2absStepByKey
+//    absolute step (C0 = 0, D0 = 1, ... C1 = 7, D2 = 8, ...) for a pitch/tpc according to key
+//    if pAlter not null, returns in it the alteration with respect to the corresponding key degree (-3 to 3)
+//    (for instance, an F in GMaj yields alteration -1 i.e. 1 semitone below corresp. deg. of GMaj which is F#)
+//    key: between KEY_MIN and KEY_MAX
+//---------------------------------------------------------
+
+int pitch2absStepByKey(int pitch, int tpc, int key, int *pAlter)
+      {
+      // sanitize input data
+      if(pitch < 0)           pitch += PITCH_DELTA_OCTAVE;
+      if(pitch > 127)         pitch -= PITCH_DELTA_OCTAVE;
+      if(tpc < TPC_MIN)       tpc   += TPC_DELTA_ENHARMONIC;
+      if(tpc > TPC_MAX)       tpc   -= TPC_DELTA_ENHARMONIC;
+      if(key < KEY_MIN)       key   += KEY_DELTA_ENHARMONIC;
+      if(key > KEY_MAX)       key   -= KEY_DELTA_ENHARMONIC;
+
+      int octave = pitch / PITCH_DELTA_OCTAVE;
+      int step = tpc2step(tpc);
+      if(pAlter)
+            *pAlter = tpc2alterByKey(tpc, key);
+      return octave * STEP_DELTA_OCTAVE + step;
+      }
+
+//---------------------------------------------------------
+//   absStep2pitchByKey
+//    the default pitch for the given absolute step in the given key
+//---------------------------------------------------------
+
+int absStep2pitchByKey(int step, int key)
+      {
+      // sanitize input data
+      if(step < 0)           step += STEP_DELTA_OCTAVE;
+      if(step > 74)          step -= STEP_DELTA_OCTAVE;
+      if(key < KEY_MIN)      key  += KEY_DELTA_ENHARMONIC;
+      if(key > KEY_MAX)      key  -= KEY_DELTA_ENHARMONIC;
+
+      int octave = step / STEP_DELTA_OCTAVE;
+      int deltaPitch = step2deltaPitchByKey(step % STEP_DELTA_OCTAVE, key);
+      return octave * PITCH_DELTA_OCTAVE + deltaPitch;
+      }
