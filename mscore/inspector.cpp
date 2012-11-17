ab44fc9ab8ae4cdfeb5f0f5ca3cc895b6f3b5ca1
Maurizio Gavioli <mmg@vistamaresoft.com>
2012-11-18 00:28:38 +0100

    Adds drop boxes to show/set type and span for a bar line.

--- mscore/inspector.cpp
+++ mscore/inspector.cpp
@@ -38,6 +38,9 @@
 #include "libmscore/hook.h"
 #include "libmscore/stem.h"
 #include "libmscore/keysig.h"
+#include "libmscore/barline.h"
+#include "libmscore/staff.h"
+#include "libmscore/measure.h"
 
 //---------------------------------------------------------
 //   showInspector
@@ -168,6 +171,9 @@ void Inspector::setElement(Element* e)
                         break;
                   case Element::HAIRPIN_SEGMENT:
                         ie = new InspectorHairpin(this);
+                        break;
+                  case Element::BAR_LINE:
+                        ie = new InspectorBarLine(this);
                         break;
                   default:
                         ie = new InspectorElement(this);
@@ -1631,6 +1637,183 @@ void InspectorChord::apply()
       MScore::Direction d = MScore::Direction(stemDirection->currentIndex());
       if (d != chord->stemDirection())
             score->undoChangeProperty(chord, P_STEM_DIRECTION, d);
+      score->endCmd();
+      mscore->endCmd();
+      }
+
+//---------------------------------------------------------
+//   InspectorBarLine
+//---------------------------------------------------------
+
+#define BARLINE_TYPE_DEFAULT  -1
+
+QString InspectorBarLine::builtinSpanNames[BARLINE_BUILTIN_SPANS] =
+{
+      tr("Staff default"), tr("Tick"), tr("Tick alt."), tr("Short"), tr("Short alt.")
+};
+
+int InspectorBarLine::builtinSpans[BARLINE_BUILTIN_SPANS][3] =
+{//   span From To
+      { 0,  0,  0},           // = staff defalt
+      { 1, -2,  2},           // tick 1
+      { 1, -1,  1},           // tick 2
+      { 1,  2,  0},           // short 1 (To depends on staff num. of lines)
+      { 1,  1,  0}            // short 2 (To depends on staff num. of lines)
+};
+
+InspectorBarLine::InspectorBarLine(QWidget* parent)
+   : InspectorBase(parent)
+      {
+      iElement = new InspectorElementElement(this);
+      layout->addWidget(iElement);
+      layout->addSpacing(20);
+
+      // "Type" Combo box
+      QHBoxLayout* l = new QHBoxLayout;
+      QLabel* label = new QLabel(tr("Type:"));
+      type = new QComboBox;
+      type->addItem(tr("Measure default"), BARLINE_TYPE_DEFAULT);
+      type->addItem(tr("Normal"), NORMAL_BAR);
+      type->addItem(tr("Dashed"), BROKEN_BAR);
+      type->addItem(tr("Dotted"), DOTTED_BAR);
+      type->addItem(tr("Double"), DOUBLE_BAR);
+      type->addItem(tr("End"), END_BAR);
+      connect(type, SIGNAL(currentIndexChanged(int)), SLOT(apply()));
+      l->addWidget(label);
+      l->addWidget(type);
+      layout->addLayout(l);
+
+      // "Span" combo box
+      l = new QHBoxLayout;
+      label = new QLabel(tr("Span:"));
+      span = new QComboBox;
+      for(int i=0; i < BARLINE_BUILTIN_SPANS; i++)
+            span->addItem(builtinSpanNames[i]);
+      span->addItem(tr("Custom"));
+      connect(span, SIGNAL(currentIndexChanged(int)), SLOT(apply()));
+      l->addWidget(label);
+      l->addWidget(span);
+      layout->addLayout(l);
+      }
+
+//---------------------------------------------------------
+//   setElement
+//---------------------------------------------------------
+
+void InspectorBarLine::setElement(Element* e)
+      {
+      BarLine* bl = static_cast<BarLine*>(e);
+      Measure* m = static_cast<Segment*>(bl->parent())->measure();
+      measureBarLineType = m->endBarLineType();
+
+      iElement->setElement(bl);
+      type->blockSignals(true);
+      span->blockSignals(true);
+
+      type->setEnabled(true);
+      // set type: if measure bar line is a repeat, no other type is possible; disable combo
+      if (measureBarLineType == START_REPEAT || measureBarLineType == END_REPEAT || measureBarLineType == END_START_REPEAT) {
+            type->setEnabled(false);
+            type->setCurrentIndex(0);
+            }
+      // if same as parent measure, set combo to Measure default
+      else if (bl->subtype() == measureBarLineType) {
+            type->setCurrentIndex(0);
+            }
+      // if custom type, set combo to item corresponding to bar line type
+      else
+            for (int i = 1; i < type->count(); i++)
+                  if (type->itemData(i) == bl->subtype()) {
+                        type->setCurrentIndex(i);
+                        break;
+                        }
+
+      // set span: fix spanTo values depending from staff number of lines
+      if(bl->staff()) {
+            Staff* st = bl->staff();
+            int maxSpanTo = (st->lines()-1) * 2;
+            builtinSpans[3][2] = maxSpanTo-2;         // short
+            builtinSpans[4][2] = maxSpanTo-1;         // short alt
+      }
+      else {
+            builtinSpans[3][2] = DEFAULT_BARLINE_TO-2;
+            builtinSpans[4][2] = DEFAULT_BARLINE_TO-1;
+            }
+      // if bar line span is same as staff, set to "Staff default"
+      if (!bl->customSpan())
+            span->setCurrentIndex(0);
+      // if custom span, look for corresponding item in combo box
+      else {
+            int i;
+            for(i=1; i < BARLINE_BUILTIN_SPANS; i++)
+                  if(bl->span() == builtinSpans[i][0]
+                              && bl->spanFrom() == builtinSpans[i][1]
+                              && bl->spanTo() == builtinSpans[i][2])
+                        break;
+            // if no match found among combo items, will set to "Custom"
+            span->setCurrentIndex(i);
+            }
+
+      type->blockSignals(false);
+      span->blockSignals(false);
+      }
+
+//---------------------------------------------------------
+//   apply
+//---------------------------------------------------------
+
+void InspectorBarLine::apply()
+      {
+      BarLine*    bl = static_cast<BarLine*>(inspector->element());
+      Score*      score = bl->score();
+
+      score->startCmd();
+
+      // type
+      int currType = type->itemData(type->currentIndex()).toInt();
+      if(currType == BARLINE_TYPE_DEFAULT)
+            currType = measureBarLineType;
+      if (currType != bl->subtype())
+            score->undoChangeProperty(bl, P_SUBTYPE, currType);
+      // if value reverted to measure default, update combo box
+      if(!bl->customSubtype())
+            type->setCurrentIndex(0);
+
+      // span: determine span, spanFrom and spanTo values for current combo box item
+      int currSpan = span->currentIndex();
+      int spanStaves, spanFrom, spanTo;
+      if(currSpan == 0) {                 // staff default selected
+            if(bl->staff()) {                               // if there is a staff
+                  Staff* st = bl->staff();                  // use its span values as selected values
+                  spanStaves  = st->barLineSpan();
+                  spanFrom    = st->barLineFrom();
+                  spanTo      = st->barLineTo();
+                  }
+            else {                                          // if no staff
+                  spanStaves  = 1;                          // use values for a 'standard' staff
+                  spanFrom    = 0;
+                  spanTo      = DEFAULT_BARLINE_TO;
+                  }
+            }
+      else {                              // specific value selected
+            if (currSpan == BARLINE_BUILTIN_SPANS+1) {      // selecting custom has no effect:
+                  spanStaves  = bl->span();                 // use values from bar line itself
+                  spanFrom    = bl->spanFrom();
+                  spanTo      = bl->spanTo();
+                  }
+            else {
+                  spanStaves  = builtinSpans[currSpan][0];  // use values from selected combo item
+                  spanFrom    = builtinSpans[currSpan][1];
+                  spanTo      = builtinSpans[currSpan][2];
+                  }
+            }
+      // if combo values different from bar line's, set them
+      if(bl->span() != spanStaves || bl->spanFrom() != spanFrom || bl->spanTo() != spanTo)
+            score->undoChangeSingleBarLineSpan(bl, spanStaves, spanFrom, spanTo);
+      // if value reverted to staff default, update combo box
+      if(!bl->customSpan())
+            span->setCurrentIndex(0);
+
       score->endCmd();
       mscore->endCmd();
       }
