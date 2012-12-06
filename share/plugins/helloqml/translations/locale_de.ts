ad443b41cfaad040c1944c242787bd16b227eeb4
Joachim Schmitz <jojo@schmitz-digital.de>
2012-12-06 16:46:42 +0100

    make new plugin translation style compatible with old style

--- share/plugins/helloqml/translations/locale_de.ts
+++ share/plugins/helloqml/translations/locale_de.ts
@@ -4,37 +4,37 @@
 <context>
     <name>helloqml</name>
     <message>
-        <location filename="helloqml.qml" line="8"/>
+	<location filename="../helloqml.qml" line="8"/>
         <source>This demo plugin shows some basic tasks.</source>
         <translation>Dieses Demo-Plugin zeigt einige einfache Funktionen.</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="13"/>
+	<location filename="../helloqml.qml" line="13"/>
         <source>hello world</source>
         <translation>Hallo Welt</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="23"/>
+	<location filename="../helloqml.qml" line="23"/>
         <source>measure</source>
         <translation>Takt</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="26"/>
+	<location filename="../helloqml.qml" line="26"/>
         <source>  segment</source>
         <translation>  Segment</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="29"/>
+	<location filename="../helloqml.qml" line="29"/>
         <source> ---hello segment</source>
         <translation> ---hallo Segment</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="36"/>
+	<location filename="../helloqml.qml" line="36"/>
         <source>    element</source>
         <translation>    Element</translation>
     </message>
     <message>
-        <location filename="helloqml.qml" line="52"/>
+	<location filename="../helloqml.qml" line="52"/>
         <source>Hello Qml</source>
         <translation>Hallo Qml</translation>
     </message>
