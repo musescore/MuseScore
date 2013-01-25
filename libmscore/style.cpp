412f0a7d6e149348885701d7cc1feb8f578fa851
Maurizio Gavioli <mmg@vistamaresoft.com>
2013-01-25 09:54:36 +0100

    Align to latest commits

--- libmscore/style.cpp
+++ libmscore/style.cpp
@@ -599,6 +599,7 @@ StyleData::StyleData()
             StyleVal(ST_tremoloBoxHeight, Spatium(0.65)),
             StyleVal(ST_tremoloStrokeWidth, Spatium(0.35)),
             StyleVal(ST_tremoloDistance, Spatium(0.8)),
+
             StyleVal(ST_linearStretch, qreal(1.5)),
             StyleVal(ST_crossMeasureValues, false)
             };
