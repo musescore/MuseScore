12d1a863a2eba7bcee2d468ee132f25e19bd4c48
lasconic <lasconic@gmail.com>
2013-03-27 09:56:10 +0100

    revert some changes

---
+++ awl/ltest.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "awlplugin.h"
+
+//---------------------------------------------------------
+//   main
+//---------------------------------------------------------
+
+int main(int /*argc*/, char* /*argv*/[])
+      {
+      printf("Hello MusE\n");
+      new KnobPlugin;
+      return 0;
+      }
+
