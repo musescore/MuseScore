5f9906ca0a6f6a6d541feea2742e6fa986e9aa1f
ws <ws@wschweer.de>
2013-04-17 10:49:47 +0200

    add midi import division test

--- libmscore/element.h
+++ libmscore/element.h
@@ -314,7 +314,7 @@ class Element : public QObject {
       virtual void setScore(Score* s)         { _score = s;         }
       Element* parent() const                 { return _parent;     }
       void setParent(Element* e)              { _parent = e;        }
-      Element* parentChordRest();
+//      Element* parentChordRest();
       Element* findMeasure();
 
       qreal spatium() const;
