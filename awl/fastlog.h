17c9d9f6c14c7e5f4c8ae79f5621258287a0219b
lasconic <lasconic@gmail.com>
2013-04-26 10:28:30 +0200

    add license header to fastlog.h

--- awl/fastlog.h
+++ awl/fastlog.h
@@ -1,4 +1,6 @@
 /* Copyright unknown. Code by Laurent de Soras <laurent@ohmforce.com>.
+ * This file is licensed under the WTFPL v2 license.
+ * http://www.wtfpl.net/about/
  */
 
 #ifndef __FASTLOG_H__
