5ece2265cef851ef4e9ba313f70c9f0b1f0ea129
lasconic <lasconic@gmail.com>
2013-05-11 15:22:27 +0200

    replace more icons

--- mscore/icons.h
+++ mscore/icons.h
@@ -44,6 +44,7 @@ enum { longaUp_ICON, brevis_ICON, note_ICON, note2_ICON, note4_ICON, note8_ICON,
       formatIndentMore_ICON, formatIndentLess_ICON, community_ICON,
       metronome_ICON,
       vframe_ICON, hframe_ICON, tframe_ICON, fframe_ICON, measure_ICON, checkmark_ICON,
+      helpContents_ICON, goHome_ICON, goPrevious_ICON, goNext_ICON, viewRefresh_ICON,
       voice1_ICON, voice2_ICON, voice3_ICON, voice4_ICON,
       ICONS
       };
