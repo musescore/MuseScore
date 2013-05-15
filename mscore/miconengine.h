346b171b1bed1b21b5356235aeda91219f166e32
ws <ws@wschweer.de>
2013-05-15 10:57:16 +0200

    colorize toggled icons

--- mscore/miconengine.h
+++ mscore/miconengine.h
@@ -1,3 +1,17 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2011 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2
+//  as published by the Free Software Foundation and appearing in
+//  the file LICENSE.GPL
+//=============================================================================
+
+// this is a modified version of qt QSvgIconEngine
+
 #ifndef __MICONENGINE_H__
 #define __MICONENGINE_H__
 
