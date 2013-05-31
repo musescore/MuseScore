ee4736d89a814cfe0fe972077f19125a0405b621
ws <ws@wschweer.de>
2013-05-31 17:48:47 +0200

    remove VoiceSelector

--- mscore/pianoroll.cpp
+++ mscore/pianoroll.cpp
@@ -18,7 +18,6 @@
 #include "libmscore/staff.h"
 #include "libmscore/score.h"
 #include "libmscore/measure.h"
-#include "voiceselector.h"
 #include "libmscore/note.h"
 #include "awl/pitchlabel.h"
 #include "awl/pitchedit.h"
@@ -76,8 +75,14 @@ PianorollEditor::PianorollEditor(QWidget* parent)
 
       //-------------
       tb = addToolBar(tr("toolbar2"));
-      VoiceSelector* vs = new VoiceSelector;
-      tb->addWidget(vs);
+      static const char* sl3[] = { "voice-1", "voice-2", "voice-3", "voice-4" };
+      QActionGroup* voiceGroup = new QActionGroup(this);
+      for (const char* s : sl3) {
+            QAction* a = getAction(s);
+            a->setCheckable(true);
+            voiceGroup->addAction(a);
+            tb->addAction(getAction(s));
+            }
 
       tb->addSeparator();
       tb->addWidget(new QLabel(tr("Cursor:")));
