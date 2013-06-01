c6b0b986b43287eb84ef26799e33205de4cbf02d
Andrey M. Tokarev <andrey.m.tokarev@gmail.com>
2013-06-02 03:06:41 +0400

    Modified chord handling

--- mscore/importmidi_meter.h
+++ mscore/importmidi_meter.h
@@ -22,6 +22,11 @@ bool isDuple(const Fraction &barFraction);
 bool isTriple(const Fraction &barFraction);
 bool isQuadruple(const Fraction &barFraction);
 
+
+// list of levels (0, -1, ...) of all ticks in bar that are multiples of minDuration
+// if minDuration <= 0 then minDuration will be set to min allowed note length (1/128)
+std::vector<int> metricLevelsOfBar(const Fraction &barFraction, int minDuration);
+
 QList<TDuration> toDurationList(int startTickInBar, int endTickInBar,
                                 const Fraction &barFraction, DurationType durationType,
                                 bool useDots);
