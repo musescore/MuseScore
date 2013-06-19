db8f0cc7f2d3ad99589028805b12643a6bb6ec10
lasconic <lasconic@gmail.com>
2013-06-19 09:53:15 +0200

    respect defaultColor for barline and textlines

--- libmscore/textline.cpp
+++ libmscore/textline.cpp
@@ -81,7 +81,7 @@ void TextLineSegment::draw(QPainter* painter) const
             color = textLine()->curColor();
             normalColor = true;
             }
-
+qDebug() << "normal color" << color;
       qreal l = 0.0;
       int sym = spannerSegmentType() == SEGMENT_MIDDLE ? tl->continueSymbol() : tl->beginSymbol();
       if (_text) {
@@ -110,7 +110,7 @@ void TextLineSegment::draw(QPainter* painter) const
       QPen pen(normalColor ? tl->lineColor() : color, textlineLineWidth);
       pen.setStyle(tl->lineStyle());
       painter->setPen(pen);
-
+qDebug() << "pen color" << pen.color();
       if (spannerSegmentType() == SEGMENT_SINGLE || spannerSegmentType() == SEGMENT_END) {
             if (tl->endSymbol() != -1) {
                   int sym = tl->endSymbol();
@@ -320,8 +320,8 @@ TextLine::TextLine(Score* s)
       _lineStyle         = Qt::SolidLine;
       _beginTextPlace    = PLACE_LEFT;
       _continueTextPlace = PLACE_LEFT;
-      _lineColor         = Qt::black;
-
+      _lineColor         = curColor();
+qDebug() << _lineColor;
       _beginSymbol       = noSym;
       _continueSymbol    = noSym;
       _endSymbol         = noSym;
