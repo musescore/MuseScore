9c7c807c0cdcaf9296d690f9d1f8edda0f540594
Marc Sabatella <marc@outsideshore.com>
2013-06-30 15:34:41 -0600

    fix #21704: imported chords do not render
    Recent changes to default style chordlist handling resulted in imported
    scores never loading a chord list at all, causing them to render
    incorrectly (no root or bass).  This is a proposed fix for that specific
    issue that also addresses other potential problems in chordlist handling
    (honoring the distinction between a truly empty chordlist and one that
    merely contains no chord id's but does contain sym & token definitions).

--- mscore/bb.cpp
+++ mscore/bb.cpp
@@ -380,6 +380,7 @@ Score::FileError importBB(Score* score, const QString& name)
             qDebug("cannot open file <%s>\n", qPrintable(name));
             return Score::FILE_OPEN_ERROR;
             }
+      score->style()->set(StyleVal(ST_chordsXmlFile, true));
       score->style()->chordList()->read("chords.xml");
       *(score->sigmap()) = bb.siglist();
 
