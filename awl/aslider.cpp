1bc7047449004754e771122898bd1aed5797ccd9
lasconic <lasconic@gmail.com>
2013-09-13 12:54:09 +0200

    light theme is the default, adjust some theme color, change restart text for language

--- awl/aslider.cpp
+++ awl/aslider.cpp
@@ -28,7 +28,7 @@ namespace Awl {
 //---------------------------------------------------------
 
 AbstractSlider::AbstractSlider(QWidget* parent)
-   : QWidget(parent), _scaleColor(Qt::darkGray), _scaleValueColor(Qt::blue)
+   : QWidget(parent), _scaleColor(Qt::darkGray), _scaleValueColor(QColor("#91336c"))
       {
       _id         = 0;
       _value      = 0.5;
