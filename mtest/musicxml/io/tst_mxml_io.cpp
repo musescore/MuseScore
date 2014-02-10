749f702404d4ba7c3b157bff2a2de16d8f2f3b4e
lasconic <lasconic@gmail.com>
2014-02-10 12:36:17 +0100

    fix #24624: [MusicXML] Parentheses around a note head is not exported

--- mtest/musicxml/io/tst_mxml_io.cpp
+++ mtest/musicxml/io/tst_mxml_io.cpp
@@ -131,6 +131,7 @@ private slots:
 //      void wedge1() { mxmlIoTest("testWedge1"); }
 //      void wedge2() { mxmlIoTestRef("testWedge2"); }
       void words1() { mxmlIoTest("testWords1"); }
+      void noteheadParentheses() { mxmlIoTest("testNoteheadParentheses"); }
       };
 
 //---------------------------------------------------------
