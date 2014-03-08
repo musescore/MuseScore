30a7c561f3fa34ab5cab9485a964345e5a88bf49
Gai-Luron <bobsatfr@yahoo.fr>
2014-03-08 23:31:34 +0100

    Fix slur error on mmRest

--- libmscore/spanner.cpp
+++ libmscore/spanner.cpp
@@ -348,6 +348,9 @@ QVariant Spanner::propertyDefault(P_ID propertyId) const
 ChordRest* Score::findCR(int tick, int track) const
       {
       Measure* m = tick2measure(tick);
+      // attach to first rest all spanner when mmRest
+      if(m->isMMRest())
+            tick = m->tick();
       if (m == 0) {
             qDebug("findCR: no measure for tick %d\n", tick);
             return 0;
