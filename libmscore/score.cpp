dbef289c0778c0c46fc00fac241e04622cd3aabe
Joachim Schmitz <jojo@schmitz-digital.de>
2014-03-14 11:52:03 +0100

    enabling more metaTags

--- libmscore/score.cpp
+++ libmscore/score.cpp
@@ -265,17 +265,11 @@ void Score::init()
             _metaTags.insert("movementTitle", "");
             _metaTags.insert("workNumber", "");
             _metaTags.insert("workTitle", "");
-            /* TODO enable following block of code
-             * This adds arranger, composer, lyricist, poet and translator fields
-             * to the File / Info dialog.
-             * As these fields are saved in every .msc[xz] file, it requires updating
-             * all regression test files in the mtest directory.
             _metaTags.insert("arranger", "");
             _metaTags.insert("composer", "");
             _metaTags.insert("lyricist", "");
             _metaTags.insert("poet", "");
             _metaTags.insert("translator", "");
-             */
             _metaTags.insert("source", "");
             _metaTags.insert("copyright", "");
             _metaTags.insert("creationDate", QDate::currentDate().toString(Qt::ISODate));
