d1d7f4fef50c74fed1122bbd7463d15dbafb1ac9
Andrey M. Tokarev <andrey.m.tokarev@gmail.com>
2014-03-17 22:45:49 +0400

    Print debug message if tuplet permutation limit was exceeded

--- mscore/importmidi_tuplet.cpp
+++ mscore/importmidi_tuplet.cpp
@@ -635,6 +635,9 @@ minimizeQuantError(const std::vector<std::vector<int>> &indexGroups,
             } while (counter < PERMUTATION_LIMIT &&
                      std::next_permutation(iIndexGroups.begin(), iIndexGroups.end()));
 
+      if (counter >= PERMUTATION_LIMIT)
+            qDebug() << "MidiTuplet, minimizeQuantError: permutation limit was exceeded";
+
       Q_ASSERT_X(validateSelectedTuplets(bestIndexes, tuplets),
                  "MIDI tuplets: minimizeQuantError", "Tuplets have common chords but they shouldn't");
 
