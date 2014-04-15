ba90da74ed90e380c8e672cf91833a24e3f18809
ws <ws@wschweer.de>
2014-04-15 16:20:01 +0200

    fix #25410

--- all.h
+++ all.h
@@ -39,7 +39,7 @@
 #include <QAbstractMessageHandler>
 #include <QXmlSchema>
 #include <QXmlSchemaValidator>
-#include <QXmlStreamReader>
+// #include <QXmlStreamReader>
 
 #include <QPointF>
 #include <QVariant>
