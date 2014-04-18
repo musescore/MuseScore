8cfc6a1da1cc81ef2d5b25d26ab25a5ae8f1011f
Marc Sabatella <marc@outsideshore.com>
2014-04-18 14:45:07 -0600

    fix #25454: unisons should share noteheads despite duration difference

--- vtest/gen.bat
+++ vtest/gen.bat
@@ -10,7 +10,7 @@ set SRC=mmrest-1,bravura-mmrest,mmrest-2,mmrest-4,mmrest-5,mmrest-6,mmrest-7,mmr
  emmentaler-6,bravura-6,emmentaler-7,bravura-7, ^
  emmentaler-8,bravura-8,emmentaler-9,bravura-9,emmentaler-10,bravura-10, ^
  chord-layout-1,chord-layout-2,chord-layout-3,chord-layout-4,chord-layout-5, ^
- chord-layout-6,chord-layout-7,chord-layout-8,chord-layout-9, ^
+ chord-layout-6,chord-layout-7,chord-layout-8,chord-layout-9,chord-layout-10, ^
  accidental-1,accidental-2,accidental-3,accidental-4,accidental-5,accidental-6, ^
  tie-1
 
