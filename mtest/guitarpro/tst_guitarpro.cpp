b162033149b13c788bb86ac5816d17b891cdbc4d
John Pirie <jpirie23@gmail.com>
2014-04-25 16:09:48 +0100

    Added tests for importing copyright statements

--- mtest/guitarpro/tst_guitarpro.cpp
+++ mtest/guitarpro/tst_guitarpro.cpp
@@ -45,6 +45,9 @@ private slots:
       void gp5Volta() { gpReadTest("volta", "gp5"); }
       void gp4Volta() { gpReadTest("volta", "gp4"); }
       void gp3Volta() { gpReadTest("volta", "gp3"); }
+      void gp5copyright() { gpReadTest("copyright", "gp5"); }
+      void gp4copyright() { gpReadTest("copyright", "gp4"); }
+      void gp3copyright() { gpReadTest("copyright", "gp3"); }
       };
 
 //---------------------------------------------------------
