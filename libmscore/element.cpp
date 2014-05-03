d4ae271a29de306bc440d98203d24f34efbc8eb6
lasconic <lasconic@gmail.com>
2014-05-03 11:27:19 +0200

    fix #25616: Rehearsal mark from MuseScore 1.3 causes bad layout and crash

--- libmscore/element.cpp
+++ libmscore/element.cpp
@@ -736,7 +736,7 @@ bool Element::readProperties(XmlReader& e)
             }
       else if (tag == "tick") {
             int val = e.readInt();
-            if (val >= 0 && type() != SYMBOL  && (type() != GLISSANDO || score()->mscVersion() > 114))   // hack for 1.2
+            if (val >= 0 && type() != SYMBOL  && ((type() != GLISSANDO && type() != FINGERING) || score()->mscVersion() > 114))   // hack for 1.2
                   e.setTick(score()->fileDivision(val));
             }
       else if (tag == "offset")
