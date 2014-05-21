fb5a7e262b141e57ab02fbdc4fccc144e9bab237
Marc Sabatella <marc@outsideshore.com>
2014-05-20 19:16:16 -0600

    update vtests

--- vtest/gen.bat
+++ vtest/gen.bat
@@ -13,7 +13,7 @@ set SRC=mmrest-1,bravura-mmrest,mmrest-2,mmrest-4,mmrest-5,mmrest-6,mmrest-7,mmr
  chord-layout-6,chord-layout-7,chord-layout-8,chord-layout-9,chord-layout-10, chord-layout-11, ^
  accidental-1,accidental-2,accidental-3,accidental-4, ^
  accidental-5,accidental-6,accidental-7,accidental-8, ^
- tie-1,grace-1,harmony-1,beams-1,beams-2,user-offset-1,user-offset-2
+ tie-1,grace-1,grace-2,harmony-1,beams-1,beams-2,user-offset-1,user-offset-2
 
 set MSCORE=..\win32install\bin\mscore.exe
 set DPI=130
