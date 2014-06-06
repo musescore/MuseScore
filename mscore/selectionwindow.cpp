422cd642ad8c619e4078b920067bfb5ab83f323b
Bartlomiej Lewandowski <Bartlomiejb.Lewandowski@gmail.com>
2014-06-06 17:56:36 +0200

    Moved selection window labels to an array.

--- mscore/selectionwindow.cpp
+++ mscore/selectionwindow.cpp
@@ -8,6 +8,19 @@ namespace Ms {
 static const int DEFAULT_POS_X  = 300;
 static const int DEFAULT_POS_Y  = 100;
 
+static const char* labels[] = {
+      QT_TRANSLATE_NOOP("selectionfilter", "1st Voice"),
+      QT_TRANSLATE_NOOP("selectionfilter", "2nd Voice"),
+      QT_TRANSLATE_NOOP("selectionfilter", "3rd Voice"),
+      QT_TRANSLATE_NOOP("selectionfilter", "4th Voice"),
+      QT_TRANSLATE_NOOP("selectionfilter", "Dynamics"),
+      QT_TRANSLATE_NOOP("selectionfilter", "Fingering"),
+      QT_TRANSLATE_NOOP("selectionfilter", "Lyrics"),
+      QT_TRANSLATE_NOOP("selectionfilter", "Articulations")
+      };
+
+const int numLabels = sizeof(labels)/sizeof(labels[0]);
+
 SelectionWindow::SelectionWindow(QWidget *parent, Score* score) :
       QDockWidget(tr("Selections"),parent)
       {
@@ -17,21 +30,10 @@ SelectionWindow::SelectionWindow(QWidget *parent, Score* score) :
 
       _listWidget = new QListWidget;
       setWidget(_listWidget);
-      QStringList labels;
-      labels << "1st Voice"
-             << "2nd Voice"
-             << "3rd Voice"
-             << "4th Voice"
-             << "Dynamics"
-             << "Fingering"
-             << "Lyrics"
-             << "Articulations";
 
-      QStringListIterator it(labels);
-      int row = 0;
-      while (it.hasNext()) {
-            QListWidgetItem *listItem = new QListWidgetItem(it.next(),_listWidget);
-            listItem->setData(Qt::UserRole, QVariant(1 << row++));
+      for (int row = 0; row < numLabels; row++) {
+            QListWidgetItem *listItem = new QListWidgetItem(labels[row],_listWidget);
+            listItem->setData(Qt::UserRole, QVariant(1 << row));
             listItem->setCheckState(Qt::Unchecked);
             _listWidget->addItem(listItem);
             }
