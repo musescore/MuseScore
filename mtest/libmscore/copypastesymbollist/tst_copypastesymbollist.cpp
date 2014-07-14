656ebd5bf4b1e03af830392a17a7e96199587561
Bartlomiej Lewandowski <Bartlomiejb.Lewandowski@gmail.com>
2014-07-15 00:46:59 +0200

    Changed tst_copypastesymbollist to use selectSimilar in Score.

--- mtest/libmscore/copypastesymbollist/tst_copypastesymbollist.cpp
+++ mtest/libmscore/copypastesymbollist/tst_copypastesymbollist.cpp
@@ -31,15 +31,15 @@ class TestCopyPasteSymbolList : public QObject, public MTest
       {
       Q_OBJECT
 
-      void copypaste(const char*, const char*);
+      void copypaste(const char*, Element::Type);
 
    private slots:
       void initTestCase();
-      void copypasteArticulation()  { copypaste("articulation", "articulation"); }
-      void copypasteChordNames()    { copypaste("chordnames", "chordnames"); }
-      void copypasteChordNames1()    { copypaste("chordnames-01", "chordnames"); }
-      void copypasteFiguredBass()   { copypaste("figuredbass", "figuredbass"); }
-      void copypasteLyrics()        { copypaste("lyrics", "lyrics"); }
+      void copypasteArticulation()  { copypaste("articulation", Element::Type::ARTICULATION); }
+      void copypasteChordNames()    { copypaste("chordnames", Element::Type::HARMONY); }
+      void copypasteChordNames1()    { copypaste("chordnames-01", Element::Type::HARMONY); }
+      void copypasteFiguredBass()   { copypaste("figuredbass", Element::Type::FIGURED_BASS); }
+      void copypasteLyrics()        { copypaste("lyrics", Element::Type::LYRICS); }
 
       };
 
@@ -53,78 +53,18 @@ void TestCopyPasteSymbolList::initTestCase()
       }
 
 //---------------------------------------------------------
-//   collectMatch
-//
-//    (copied from mscore/musescore.cpp)
-//---------------------------------------------------------
-
-static void collectMatch(void* data, Element* e)
-      {
-      ElementPattern* p = static_cast<ElementPattern*>(data);
-/*      if (p->type == e->type() && p->subtype != e->subtype())
-            qDebug("%s subtype %d does not match", e->name(), e->subtype());
-      */
-//TODO      if ((p->type != int(e->type())) || (p->subtypeValid && p->subtype != int(e->subtype())))
-      if (p->type != int(e->type()))
-            return;
-      if ((p->staff != -1) && (p->staff != e->staffIdx()))
-            return;
-      if (e->type() == Element::Type::CHORD || e->type() == Element::Type::REST || e->type() == Element::Type::NOTE || e->type() == Element::Type::LYRICS) {
-            if (p->voice != -1 && p->voice != e->voice())
-                  return;
-            }
-      if (p->system) {
-            Element* ee = e;
-            do {
-                  if (ee->type() == Element::Type::SYSTEM) {
-                        if (p->system != ee)
-                              return;
-                        break;
-                        }
-                  ee = ee->parent();
-                  } while (ee);
-            }
-      p->el.append(e);
-      }
-
-//---------------------------------------------------------
 //   copypaste
 //    copy measure 2, paste into measure 4
 //---------------------------------------------------------
 
-void TestCopyPasteSymbolList::copypaste(const char* name, const char* idx)
+void TestCopyPasteSymbolList::copypaste(const char* name, Element::Type type)
       {
       Score* score = readScore(DIR + QString("copypastesymbollist-%1.mscx").arg(name));
       score->doLayout();
 
-      // select all elements of a certain type (copied and slightly adapted from
-      // MuseScore::selectSimilar(Element* e, bool sameStaff) in file mscore/musescore.cpp
-      ElementPattern pattern;
-      pattern.subtypeValid = true;
-//TODO      if (type == int(Element::Type::VOLTA_SEGMENT)) {
-            // Volta* volta = static_cast<VoltaSegment*>(e)->volta();
-            // type    = volta->type();
-            // subtype = volta->subtype();
-            pattern.subtypeValid = false;
-//            }
-      if (!strcmp(idx, "articulation"))
-            pattern.type = int(Element::Type::ARTICULATION);
-      else if (!strcmp(idx, "chordnames"))
-            pattern.type = int(Element::Type::HARMONY);
-      else if (!strcmp(idx, "figuredbass"))
-            pattern.type = int(Element::Type::FIGURED_BASS);
-      else if (!strcmp(idx, "lyrics"))
-            pattern.type = int(Element::Type::LYRICS);
-      else
-            return;
-      pattern.subtype = 0; // TODO subtype;
-      pattern.staff   = 0;                      // select from staff 0 only
-      pattern.voice   = -1;
-      pattern.system  = nullptr;
-      score->scanElements(&pattern, collectMatch);
-      foreach(Element* e, pattern.el) {
-            score->select(e, SelectType::ADD, 0);
-            }
+      Element* el = Element::create(type,score);
+      score->selectSimilar(el,false);
+      delete el;
 
       // copy selection to clipboard
       QVERIFY(score->selection().canCopy());
