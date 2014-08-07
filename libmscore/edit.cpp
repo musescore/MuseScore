802479e5e53af35ac841a76cb6bfe8d11d8d5264
Bartlomiej Lewandowski <Bartlomiejb.Lewandowski@gmail.com>
2014-08-08 00:39:13 +0200

    Made voice filter to work for delete

--- libmscore/edit.cpp
+++ libmscore/edit.cpp
@@ -1712,6 +1712,7 @@ void Score::cmdDeleteSelection()
                         }
                   }
             for (int track = track1; track < track2; ++track) {
+                  if(!selectionFilter().canSelectVoice(track)) continue;
                   Fraction f;
                   int tick  = -1;
                   Tuplet* tuplet = 0;
