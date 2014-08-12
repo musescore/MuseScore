997364cde66a6f1e484c42d6f33f69d62a658792
Andrei Tuicu <andrei.tuicu@gmail.com>
2014-08-12 11:48:58 +0300

    Fix Codying style 	modified:   mscore/accessibletoolbutton.cpp 	modified:   mscore/accessibletoolbutton.h 	modified:   mscore/inspector/inspector.cpp 	modified:   mscore/shortcut.cpp

--- mscore/accessibletoolbutton.cpp
+++ mscore/accessibletoolbutton.cpp
@@ -5,36 +5,40 @@
 
 namespace Ms {
 
-AccessibleToolButton::AccessibleToolButton(QWidget* parent, QAction* defaultQAction ): QToolButton(parent){
-    this->setDefaultAction(defaultQAction);
-    this->setFocusPolicy(Qt::TabFocus);
+AccessibleToolButton::AccessibleToolButton(QWidget* parent, QAction* defaultQAction ): QToolButton(parent)
+      {
+      this->setDefaultAction(defaultQAction);
+      this->setFocusPolicy(Qt::TabFocus);
 
-    this->setAccessibleName(defaultQAction->text());
-    this->setAccessibleDescription(defaultQAction->toolTip());
-    }
+      this->setAccessibleName(defaultQAction->text());
+      this->setAccessibleDescription(defaultQAction->toolTip());
+      }
 
 
-void AccessibleToolButton::focusInEvent(QFocusEvent* e){
-    //If the button gains focus by tabbing or backtabbing, it will change its color
-    if(e->reason() == Qt::TabFocusReason || e->reason() == Qt::BacktabFocusReason){
-        setGraphicsEffect(new QGraphicsColorizeEffect());
-        }
+void AccessibleToolButton::focusInEvent(QFocusEvent* e)
+      {
+      //If the button gains focus by tabbing or backtabbing, it will change its color
+      if (e->reason() == Qt::TabFocusReason || e->reason() == Qt::BacktabFocusReason) {
+            setGraphicsEffect(new QGraphicsColorizeEffect());
+            }
 
-    QToolButton::focusInEvent(e);
-    }
+      QToolButton::focusInEvent(e);
+      }
 
-void AccessibleToolButton::focusOutEvent(QFocusEvent* e){
-    this->setGraphicsEffect(NULL);
-    QToolButton::focusInEvent(e);
-    }
+void AccessibleToolButton::focusOutEvent(QFocusEvent* e)
+      {
+      this->setGraphicsEffect(NULL);
+      QToolButton::focusInEvent(e);
+      }
 
-void AccessibleToolButton::keyPressEvent(QKeyEvent *e){
-        //Pressing Enter or Return button triggers the default action
-        if(e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return){
+void AccessibleToolButton::keyPressEvent(QKeyEvent *e)
+      {
+      //Pressing Enter or Return button triggers the default action
+      if (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) {
             this->animateClick();
             return;
             }
 
-    QToolButton::keyPressEvent(e);
-    }
+      QToolButton::keyPressEvent(e);
+      }
 }
