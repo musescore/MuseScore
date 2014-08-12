baba0ad0a43f5f940fdb952de5f07f08bd3fc444
Andrei Tuicu <andrei.tuicu@gmail.com>
2014-08-12 14:32:56 +0300

    Added Q_OBJECT macro 	modified:   mscore/accessibletoolbutton.h

--- mscore/accessibletoolbutton.h
+++ mscore/accessibletoolbutton.h
@@ -5,6 +5,7 @@ namespace Ms {
  * This class inherits QToolButton and allows tabbing through the tool bar's buttons
  */
 class AccessibleToolButton : public QToolButton{
+      Q_OBJECT
 public:
       AccessibleToolButton(QWidget* parent, QAction* defaultQAction );
       void focusInEvent(QFocusEvent* e);
