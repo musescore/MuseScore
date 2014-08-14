06144e463135ad4ef9a509449d2cf0c486041dbe
lasconic <lasconic@gmail.com>
2014-08-14 14:16:11 +0200

    remove outdated templates, change default score to an empty treble clef piano one, move tab_sample to data, remove unused files

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -2169,7 +2169,7 @@ static void loadScores(const QStringList& argv)
                               {
                               Score* score = mscore->readScore(preferences.startScore);
                               if (score == 0)
-                                    score = mscore->readScore(":/data/Promenade_Example.mscz");
+                                    score = mscore->readScore(":/data/My_First_Score.mscx");
                               if (score)
                                     currentScoreView = mscore->appendScore(score);
                               }
