3b7dd6420d5d6eb9f1b3a689973d3ecb7719aae8
Andrey M. Tokarev <andrey.m.tokarev@gmail.com>
2014-08-17 16:45:08 +0400

    Simple key signature detection algorithm

---
+++ mscore/importmidi_key.h
@@ -0,0 +1,17 @@
+#ifndef IMPORTMIDI_KEY_H
+#define IMPORTMIDI_KEY_H
+
+
+namespace Ms {
+
+class MTrack;
+
+namespace MidiKey {
+
+void setMainKeySig(QList<MTrack> &tracks);
+
+} // namespace MidiKey
+} // namespace Ms
+
+
+#endif // IMPORTMIDI_KEY_H
