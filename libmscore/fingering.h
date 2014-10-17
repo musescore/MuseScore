4189822f54153277fd90b1971958a39f4dbe339b
Marc Sabatella <marc@outsideshore.com>
2014-10-16 19:43:54 -0600

    add new fingering styles

--- libmscore/fingering.h
+++ libmscore/fingering.h
@@ -36,6 +36,8 @@ class Fingering : public Text {
       virtual void write(Xml&) const override;
       virtual void read(XmlReader&) override;
       virtual void reset() override;
+      virtual int subtype() const         { return (int) textStyleType(); }
+      virtual QString subtypeName() const { return textStyle().name(); }
 
       virtual QString accessibleInfo() override;
       };
