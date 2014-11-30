34497f1daaeb585b9b0f847fe3acab136d5df9e6
Marc Sabatella <marc@outsideshore.com>
2014-11-30 10:13:31 -0700

    add mtest

--- mtest/libmscore/note/tst_note.cpp
+++ mtest/libmscore/note/tst_note.cpp
@@ -17,7 +17,13 @@
 #include "libmscore/score.h"
 #include "libmscore/note.h"
 #include "libmscore/chord.h"
+#include "libmscore/measure.h"
+#include "libmscore/segment.h"
+#include "libmscore/tremolo.h"
+#include "libmscore/articulation.h"
 #include "mtest/testutils.h"
+
+#define DIR QString("libmscore/note/")
 
 using namespace Ms;
 
@@ -32,6 +38,7 @@ class TestNote : public QObject, public MTest
    private slots:
       void initTestCase();
       void note();
+      void grace();
       };
 
 //---------------------------------------------------------
@@ -293,6 +300,64 @@ void TestNote::note()
       n = static_cast<Note*>(writeReadElement(note));
       QCOMPARE(n->veloType(), Note::ValueType::OFFSET_VAL);
       delete n;
+
+      }
+
+//---------------------------------------------------------
+///   grace
+///   read/write test of grace notes
+//---------------------------------------------------------
+
+void TestNote::grace()
+      {
+      Score* score = readScore(DIR + "grace.mscx");
+      score->doLayout();
+      Chord* chord = score->firstMeasure()->findChord(0, 0);
+      Note* note = chord->upNote();
+
+      // create
+      score->setGraceNote(chord, note->pitch(), NoteType::APPOGGIATURA, MScore::division/2);
+      Chord* gc = chord->graceNotes().first();
+      Note* gn = gc->notes().first();
+//      Note* n = static_cast<Note*>(writeReadElement(gn));
+//      QCOMPARE(n->noteType(), NoteType::APPOGGIATURA);
+//      delete n;
+
+      // tie
+      score->startCmd();
+      score->select(gn);
+      score->cmdAddTie();
+      score->endCmd();
+//      n = static_cast<Note*>(writeReadElement(gn));
+//      QVERIFY(n->tieFor() != 0);
+//      delete n;
+
+      // tremolo
+      score->startCmd();
+      Tremolo* tr = new Tremolo(score);
+      tr->setTremoloType(TremoloType::R16);
+      tr->setParent(gc);
+      tr->setTrack(gc->track());
+      score->undoAddElement(tr);
+      score->endCmd();
+//      Chord* c = static_cast<Chord*>(writeReadElement(gc));
+//      QVERIFY(c->tremolo() != 0);
+//      delete c;
+
+      // articulation
+      score->startCmd();
+      Articulation* ar = new Articulation(score);
+      ar->setArticulationType(ArticulationType::Sforzatoaccent);
+      ar->setParent(gc);
+      ar->setTrack(gc->track());
+      score->undoAddElement(ar);
+      score->endCmd();
+//      c = static_cast<Chord*>(writeReadElement(gc));
+//      QVERIFY(c->articulations().size() == 1);
+//      delete c;
+
+      QVERIFY(saveCompareScore(score, "grace-test.mscx", DIR + "grace-ref.mscx"));
+
       }
 
 QTEST_MAIN(TestNote)
