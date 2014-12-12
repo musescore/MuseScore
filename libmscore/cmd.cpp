65da39bb42da9aa5dafe3f4883d865107e951ddf
Marc Sabatella <marc@outsideshore.com>
2014-12-12 14:19:16 -0700

    fix #41326: correct transposition of MIDI input

--- libmscore/cmd.cpp
+++ libmscore/cmd.cpp
@@ -1695,17 +1695,14 @@ bool Score::processMidiInput()
                         }
                   NoteVal nval(ev.pitch);
                   Staff* st = staff(inputState().track() / VOICES);
-                  Key key   = st->key(inputState().tick());
 
-                  if (styleB(StyleIdx::concertPitch)) {
-                        nval.tpc1 = pitch2tpc(nval.pitch, key, Prefer::NEAREST);
-                        nval.tpc2 = nval.tpc1;  // DEBUG
-                        }
-                  else {
+                  // if transposing, interpret MIDI pitch as representing desired written pitch
+                  // set pitch based on corresponding sounding pitch
+                  if (!styleB(StyleIdx::concertPitch))
                         nval.pitch += st->part()->instr(inputState().tick())->transpose().chromatic;
-                        nval.tpc2  = pitch2tpc(nval.pitch, key, Prefer::NEAREST);
-                        nval.tpc1 = nval.tpc2;  // DEBUG
-                        }
+                  // let addPitch calculate tpc values from pitch
+                  //Key key   = st->key(inputState().tick());
+                  //nval.tpc1 = pitch2tpc(nval.pitch, key, Prefer::NEAREST);
 
                   addPitch(nval, ev.chord);
                   }
