fa89e2c38a8f553a5dd20f8883f3a3a64137dac4
ws <ws@wschweer.de>
2015-01-05 13:17:04 +0100

    do not create invisible c-keysig in new scores

--- mscore/file.cpp
+++ mscore/file.cpp
@@ -610,12 +610,15 @@ void MuseScore::newFile()
                                     int diff = -part->instr()->transpose().chromatic;
                                     nKey.setKey(transposeKey(nKey.key(), diff));
                                     }
-                              staff->setKey(0, nKey);
-                              KeySig* keysig = new KeySig(score);
-                              keysig->setTrack(staffIdx * VOICES);
-                              keysig->setKeySigEvent(nKey);
-                              Segment* s = measure->getSegment(keysig, 0);
-                              s->add(keysig);
+                              // do not create empty, invisible keysig
+                              if (nKey.custom() || nKey.key() != Key::C) {
+                                    staff->setKey(0, nKey);
+                                    KeySig* keysig = new KeySig(score);
+                                    keysig->setTrack(staffIdx * VOICES);
+                                    keysig->setKeySigEvent(nKey);
+                                    Segment* s = measure->getSegment(keysig, 0);
+                                    s->add(keysig);
+                                    }
                               }
                         }
                   if (staff->primaryStaff()) {
