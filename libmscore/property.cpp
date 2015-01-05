8ca1cf300e72ffe97b161bd36a78e951a6ea6510
ws <ws@wschweer.de>
2015-01-05 14:19:14 +0100

    fix #43276: dragging notes with mouse in score or linked parts isn't correctly reflected in part or score respectively

--- libmscore/property.cpp
+++ libmscore/property.cpp
@@ -44,8 +44,8 @@ static const PropertyData propertyList[] = {
       { P_ID::SHOW_COURTESY,       false, "showCourtesy",  P_TYPE::INT   },
       { P_ID::LINE_TYPE,           false, "lineType",      P_TYPE::INT   },
       { P_ID::PITCH,               true,  "pitch",         P_TYPE::INT   },
-      { P_ID::TPC1,                false, "tpc",           P_TYPE::INT   },
-      { P_ID::TPC2,                false, "tpc2",          P_TYPE::INT   },
+      { P_ID::TPC1,                true,  "tpc",           P_TYPE::INT   },
+      { P_ID::TPC2,                true,  "tpc2",          P_TYPE::INT   },
 
       { P_ID::LINE,                false, "line",          P_TYPE::INT   },
       { P_ID::FIXED,               false, "fixed",         P_TYPE::BOOL  },
