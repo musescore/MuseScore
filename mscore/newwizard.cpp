bad790305814fa7a8a3d36cc40f6982d47badc0b
Nicolas Carion <carion.nicolas@gmail.com>
2015-01-11 23:48:57 +0100

    Fix #44071

--- mscore/newwizard.cpp
+++ mscore/newwizard.cpp
@@ -265,7 +265,11 @@ NewWizardPage4::NewWizardPage4(QWidget* parent)
       templateFileBrowser = new ScoreBrowser;
       templateFileBrowser->setStripNumbers(true);
       QDir dir(mscoreGlobalShare + "/templates");
-      templateFileBrowser->setScores(dir.entryInfoList(QDir::NoDotAndDotDot | QDir::Readable | QDir::Dirs | QDir::Files, QDir::Name));
+      QFileInfoList fil = dir.entryInfoList(QDir::NoDotAndDotDot | QDir::Readable | QDir::Dirs | QDir::Files, QDir::Name);
+      if(fil.isEmpty()){
+          fil.append(QFileInfo(QFile(":data/Empty_Score.mscz")));
+          }
+      templateFileBrowser->setScores(fil);
       templateFileBrowser->setSizePolicy(QSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored));
 
       QFileInfo myTemplates(preferences.myTemplatesPath);
