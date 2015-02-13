musescore/MuseScore

commit 07f7cc6891b48afa7481a189c4e14457e6d4ffa7

fix #30626: Tablature stems too long

libmscore/beam.cpp
@@ -292,7 +292,7 @@ void Beam::layout1()
                               }
                         }
                   }
-            foreach(ChordRest* cr, _elements)
+            for (ChordRest* cr : _elements)
                   cr->setUp(_up);
             }
       else {
@@ -357,11 +357,11 @@ void Beam::layout1()
                   }
 
 
-            _cross   = minMove < maxMove;
+            _cross = minMove < maxMove;
             // int idx = (_direction == MScore::Direction::AUTO || _direction == MScore::Direction::DOWN) ? 0 : 1;
-            slope   = 0.0;
+            slope = 0.0;
 
-            foreach(ChordRest* cr, _elements)
+            for (ChordRest* cr : _elements)
                   cr->setUp(_up);
 
             }     // end of if/else(tablature)
@@ -1027,14 +1027,15 @@ static int adjust(qreal _spatium4, int slant, const QList<ChordRest*>& cl)
       int n = cl.size();
       const ChordRest* c1 = cl[0];
       const ChordRest* c2 = cl[n-1];
+      if (c1->staff() && c1->staff()->isTabStaff())
+            return 0;
 
       QPointF p1(c1->stemPosBeam());   // canvas coordinates
       qreal slope = (slant * _spatium4) / (c2->stemPosBeam().x() - p1.x());
       int ml = -1000;
       if (c1->up()) {
             for (int i = 1; i < n; ++i) {
-                  const ChordRest* c = cl[i];
-                  QPointF p3(c->stemPosBeam());
+                  QPointF p3(cl[i]->stemPosBeam());
                   qreal yUp   = p1.y() + (p3.x() - p1.x()) * slope;
                   int l       = lrint((yUp - p3.y()) / (_spatium4));
                   ml          = qMax(ml, l);
@@ -1396,7 +1397,6 @@ void Beam::computeStemLen(const QList<ChordRest*>& cl, qreal& py1, int beamLevel
             else
                   bm.l += graceStemLengthCorrection;
             }
-
       if (dx == 0.0)
             slope = 0.0;
       else

libmscore/chord.cpp
@@ -388,9 +388,9 @@ QPointF Chord::stemPos() const
 QPointF Chord::stemPosBeam() const
       {
       qreal _spatium = spatium();
-      if (staff() && staff()->isTabStaff())
-            return staff()->staffType()->chordStemPosBeam(this) * _spatium + pagePos();
       QPointF p(pagePos());
+      if (staff() && staff()->isTabStaff())
+            return staff()->staffType()->chordStemPosBeam(this) * _spatium + p;
       if (_up) {
             qreal nhw = score()->noteHeadWidth();
             if (_noteType != NoteType::NORMAL)

commit a7c9d9b721417d5dbaa6df4d498031337898bf5b

Fix #47161 : Stem layout in TABs by reverting fix to #30626
A previous fix to #30626: https://github.com/musescore/musescore/commit/07f7cc6 broke stem layout in TAB's in an attempt to shorten through-staff TAB stems which were too long. See discussion in issue thread: http://musescore.org/en/node/47161

This fix reverts the code changes of the previous fix (while keeping its cosmetic and code-style changes) and adds a partial, cheap fix to shorten TAB stems by a fixed amount.

Proper adjustment of TAB through-staff stems would require making the many numeric factors in `Beam::computeStemLen()` aware of staff line distance.

libmscore/beam.cpp
@@ -1035,8 +1035,6 @@ static int adjust(qreal _spatium4, int slant, const QList<ChordRest*>& cl)
       int n = cl.size();
       const ChordRest* c1 = cl[0];
       const ChordRest* c2 = cl[n-1];
-      if (c1->staff() && c1->staff()->isTabStaff())
-            return 0;
 
       QPointF p1(c1->stemPosBeam());   // canvas coordinates
       qreal slope = (slant * _spatium4) / (c2->stemPosBeam().x() - p1.x());
@@ -1057,6 +1055,13 @@ static int adjust(qreal _spatium4, int slant, const QList<ChordRest*>& cl)
                   int l       = lrint((p3.y() - yUp) / (_spatium4));
                   ml          = qMax(ml, l);
                   }
+            }
+      // on tab staff, reduce a bit the stems (value 4 is experimental)
+      // TODO : proper fix should adapt all the numeric vaues used in Beam::computeStemLen() below
+      // to variable line distance
+      if (c1->staff() && c1->staff()->isTabStaff()) {
+            ml = (ml != 0) ? ml - 4 : 0;
+            return ml;
             }
       return (ml > 0) ? ml : 0;
       }