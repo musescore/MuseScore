2783649ffcbfca66e115d201026174e41888075b
ws <ws@wschweer.de>
2015-02-27 13:32:56 +0100

    implement 'reset text to style' in inspector for text elements

--- mscore/inspector/inspector.cpp
+++ mscore/inspector/inspector.cpp
@@ -27,6 +27,7 @@
 #include "inspectorNote.h"
 #include "inspectorAmbitus.h"
 #include "inspectorFret.h"
+#include "inspectorText.h"
 #include "musescore.h"
 #include "scoreview.h"
 
@@ -666,47 +667,6 @@ void InspectorClef::valueChanged(int idx)
       if (idx == 6 && otherClef)
             otherClef->setShowCourtesy(c.showCourtesy->isChecked());
       InspectorBase::valueChanged(idx);
-      }
-
-//---------------------------------------------------------
-//   InspectorText
-//---------------------------------------------------------
-
-InspectorText::InspectorText(QWidget* parent)
-   : InspectorBase(parent)
-      {
-      e.setupUi(addWidget());
-      t.setupUi(addWidget());
-
-      iList = {
-            { P_ID::COLOR,              0, 0, e.color,    e.resetColor    },
-            { P_ID::VISIBLE,            0, 0, e.visible,  e.resetVisible  },
-            { P_ID::USER_OFF,           0, 0, e.offsetX,  e.resetX        },
-            { P_ID::USER_OFF,           1, 0, e.offsetY,  e.resetY        },
-            { P_ID::TEXT_STYLE_TYPE,    0, 0, t.style,    t.resetStyle    }
-            };
-      mapSignals();
-      }
-
-//---------------------------------------------------------
-//   setElement
-//---------------------------------------------------------
-
-void InspectorText::setElement()
-      {
-      Element* e = inspector->element();
-      Score* score = e->score();
-
-      t.style->blockSignals(true);
-      t.style->clear();
-      const QList<TextStyle>& ts = score->style()->textStyles();
-      int n = ts.size();
-      for (int i = 0; i < n; ++i) {
-            if (!(ts.at(i).hidden() & TextStyleHidden::IN_LISTS) )
-                  t.style->addItem(qApp->translate("TextStyle",ts.at(i).name().toLatin1().data()), i);
-            }
-      t.style->blockSignals(false);
-      InspectorBase::setElement();
       }
 
 //---------------------------------------------------------
