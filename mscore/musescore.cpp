25447a12c35a824f05610055989df2ec2d1bdcf1
lasconic <lasconic@gmail.com>
2015-03-21 10:43:55 +0100

    prepare 2.0 release

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -4528,11 +4528,11 @@ int main(int argc, char* av[])
       revision = QString(f.readAll()).trimmed();
       f.close();
 
-      MuseScoreApplication* app = new MuseScoreApplication("mscore-dev", argc, av);
+      MuseScoreApplication* app = new MuseScoreApplication("mscore2", argc, av);
 
       QCoreApplication::setOrganizationName("MuseScore");
       QCoreApplication::setOrganizationDomain("musescore.org");
-      QCoreApplication::setApplicationName("MuseScoreDevelopment");
+      QCoreApplication::setApplicationName("MuseScore2");
       QCoreApplication::setApplicationVersion(VERSION);
       QAccessible::installFactory(AccessibleScoreView::ScoreViewFactory);
       QAccessible::installFactory(AccessibleSearchBox::SearchBoxFactory);
