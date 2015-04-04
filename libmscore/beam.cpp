6df3e08950dc6d2af38219167c46abae134720be
Joachim Schmitz <jojo@schmitz-digital.de>
2015-04-04 15:44:18 +0200

    fix compiler warnings
    reg. potentially uninitialized variables, probably present since quite a
    while and some others introduced recently with xml pull parser

--- libmscore/beam.cpp
+++ libmscore/beam.cpp
@@ -1630,7 +1630,7 @@ void Beam::layout2(QList<ChordRest*>crl, SpannerSegmentType, int frag)
                   // loop through chordrests looking for end
                   int c1 = i;
                   ++i;
-                  bool b32, b64;
+                  bool b32 = false, b64 = false;
                   for (; i < n; ++i) {
                         ChordRest* c = crl[i];
                         ChordRest* p = i ? crl[i - 1] : 0;
