5ec17e77859c7e32ac1a95f8e39e7030c6c664e0
jimka2001 <jimka@rdrop.com>
2015-07-11 17:02:40 +0200

    implementation of trill line playback   including some test cases

--- mscore/inspector/inspectorTrill.cpp
+++ mscore/inspector/inspectorTrill.cpp
@@ -39,7 +39,9 @@ InspectorTrill::InspectorTrill(QWidget* parent)
             { P_ID::LINE_COLOR,  0, 0, l.lineColor,  l.resetLineColor  },
             { P_ID::LINE_WIDTH,  0, 0, l.lineWidth,  l.resetLineWidth  },
             { P_ID::LINE_STYLE,  0, 0, l.lineStyle,  l.resetLineStyle  },
-            { P_ID::TRILL_TYPE,  0, 0, t.trillType,  t.resetTrillType  }
+            { P_ID::TRILL_TYPE,  0, 0, t.trillType,  t.resetTrillType  },
+            { P_ID::ORNAMENT_STYLE,      0, 0, t.ornamentStyle, t.resetOrnamentStyle },
+            { P_ID::PLAY,   0, 0, t.playArticulation, t.resetPlayArticulation}
             };
 
       mapSignals();
