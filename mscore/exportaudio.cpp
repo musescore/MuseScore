musescore/MuseScore

commit c9b27cb72ced2e48309ddbcc754efaead0f6ac2d

fix progress bar issues on exporting audio
* add an "Exporting..." to the progress bars of audio export
* add ability to cancel audio export and delete the partially created
files

mscore/exportaudio.cpp
@@ -82,7 +82,9 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
       QProgressDialog progress(this);
       progress.setWindowFlags(Qt::WindowFlags(Qt::Dialog | Qt::FramelessWindowHint | Qt::WindowTitleHint));
       progress.setWindowModality(Qt::ApplicationModal);
-      progress.setCancelButton(0);
+      //progress.setCancelButton(0);
+      progress.setCancelButtonText(tr("Cancel"));
+      progress.setLabelText(tr("Exporting..."));
       if (!MScore::noGui)
             progress.show();
 
@@ -170,6 +172,8 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
                         }
                   playTime = endTime;
                   if (!MScore::noGui) {
+                        if (progress.wasCanceled())
+                              break;
                         progress.setValue((pass * et + playTime) / 2);
                         qApp->processEvents();
                         }
@@ -179,6 +183,8 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
                   if (playTime >= et && max*peak < 0.000001)
                         break;
                   }
+            if (progress.wasCanceled())
+                  break;
             if (pass == 0 && peak == 0.0) {
                   qDebug("song is empty");
                   break;
@@ -194,6 +200,8 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
             qDebug("close soundfile failed");
             return false;
             }
+      if (progress.wasCanceled())
+            QFile::remove(name);
 
       return true;
       }

mscore/exportmp3.cpp
@@ -694,7 +694,9 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
       QProgressDialog progress(this);
       progress.setWindowFlags(Qt::WindowFlags(Qt::Dialog | Qt::FramelessWindowHint | Qt::WindowTitleHint));
       progress.setWindowModality(Qt::ApplicationModal);
-      progress.setCancelButton(0);
+      //progress.setCancelButton(0);
+      progress.setCancelButtonText(tr("Cancel"));
+      progress.setLabelText(tr("Exporting..."));
       if (!MScore::noGui)
             progress.show();
 
@@ -822,6 +824,8 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
                         }
                   playTime = endTime;
                   if (!MScore::noGui) {
+                        if (progress.wasCanceled())
+                              break;
                         progress.setValue((pass * et + playTime) / 2);
                         qApp->processEvents();
                         }
@@ -831,6 +835,8 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
                   if (playTime >= et && max * peak < 0.000001)
                         break;
                   }
+            if (progress.wasCanceled())
+                  break;
             if (pass == 0 && peak == 0.0) {
                   qDebug("song is empty");
                   break;
@@ -846,6 +852,8 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
       delete synti;
       delete[] bufferOut;
       file.close();
+      if (progress.wasCanceled())
+            file.remove();
       MScore::sampleRate = oldSampleRate;
       return true;
       }

commit 5ec736c0e4dec1479dd2ceab18a620574bc2a5fc

delete audio files only on cancel
fixes an issue introduced in c9b27cb

mscore/exportaudio.cpp
@@ -192,6 +192,7 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
             gain = 0.99 / peak;
             }
 
+      bool wasCanceled = progress.wasCanceled();
       progress.close();
 
       MScore::sampleRate = oldSampleRate;
@@ -200,7 +201,7 @@ bool MuseScore::saveAudio(Score* score, const QString& name)
             qDebug("close soundfile failed");
             return false;
             }
-      if (progress.wasCanceled())
+      if (wasCanceled)
             QFile::remove(name);
 
       return true;

mscore/exportmp3.cpp
@@ -848,11 +848,12 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
       if (bytes > 0L)
             file.write((char*)bufferOut, bytes);
 
+      bool wasCanceled = progress.wasCanceled();
       progress.close();
       delete synti;
       delete[] bufferOut;
       file.close();
-      if (progress.wasCanceled())
+      if (wasCanceled)
             file.remove();
       MScore::sampleRate = oldSampleRate;
       return true;