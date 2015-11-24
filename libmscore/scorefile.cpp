musescore/MuseScore

commit b37137d4800bab83388fb9410541686168c5c0c1

when saving scorefile always use tmp file

libmscore/scorefile.cpp
@@ -355,26 +355,6 @@ bool Score::saveFile()
             MScore::lastError = tr("The following file is locked: \n%1 \n\nTry saving to a different location.").arg(info.filePath());
             return false;
             }
-
-      // if file was already saved in this session
-      // save but don't overwrite backup again
-
-      if (saved()) {
-            try {
-                  if (suffix == "mscx")
-                        saveFile(info);
-                  else
-                        saveCompressedFile(info, false);
-                  }
-            catch (QString s) {
-                  MScore::lastError = s;
-                  return false;
-                  }
-            undo()->setClean();
-            info.refresh();
-            update();
-            return true;
-            }
       //
       // step 1
       // save into temporary file to prevent partially overwriting
@@ -404,31 +384,36 @@ bool Score::saveFile()
             }
       temp.close();
 
-      //
-      // step 2
-      // remove old backup file if exists
-      //
-      QDir dir(info.path());
-      QString backupName = QString(".") + info.fileName() + QString(",");
-      if (dir.exists(backupName)) {
-            if (!dir.remove(backupName)) {
-//                  if (!MScore::noGui)
-//                        QMessageBox::critical(0, tr("MuseScore: Save File"),
-//                           tr("Removing old backup file ") + backupName + tr(" failed"));
-                  }
-            }
-
-      //
-      // step 3
-      // rename old file into backup
-      //
       QString name(info.filePath());
-      if (dir.exists(name)) {
-            if (!dir.rename(name, backupName)) {
-//                  if (!MScore::noGui)
-//                        QMessageBox::critical(0, tr("MuseScore: Save File"),
-//                           tr("Renaming old file <")
-//                            + name + tr("> to backup <") + backupName + tr("> failed"));
+      if (!saved()) {
+            // if file was already saved in this session
+            // save but don't overwrite backup again
+
+            //
+            // step 2
+            // remove old backup file if exists
+            //
+            QDir dir(info.path());
+            QString backupName = QString(".") + info.fileName() + QString(",");
+            if (dir.exists(backupName)) {
+                  if (!dir.remove(backupName)) {
+//                      if (!MScore::noGui)
+//                            QMessageBox::critical(0, tr("MuseScore: Save File"),
+//                               tr("Removing old backup file ") + backupName + tr(" failed"));
+                        }
+                  }
+
+            //
+            // step 3
+            // rename old file into backup
+            //
+            if (dir.exists(name)) {
+                  if (!dir.rename(name, backupName)) {
+//                      if (!MScore::noGui)
+//                            QMessageBox::critical(0, tr("MuseScore: Save File"),
+//                               tr("Renaming old file <")
+//                               + name + tr("> to backup <") + backupName + tr("> failed"));
+                        }
                   }
             }
 #ifdef Q_OS_WIN

mscore/musescore.cpp
@@ -538,7 +538,6 @@ MuseScore::MuseScore()
       fileTools->addSeparator();
       mag = new MagBox;
       connect(mag, SIGNAL(magChanged(MagIdx)), SLOT(magChanged(MagIdx)));
-      connect(mag, SIGNAL(magTextChanged()), SLOT(magTextChanged()));
       fileTools->addWidget(mag);
       viewModeCombo = new QComboBox(this);
 #if defined(Q_OS_MAC)

commit 8e587e4a1856e504c7708df4203ee1ccf9e5fe58

fix build failure on Windows introduced by b37137d

libmscore/scorefile.cpp
@@ -415,12 +415,12 @@ bool Score::saveFile()
 //                               + name + tr("> to backup <") + backupName + tr("> failed"));
                         }
                   }
-            }
 #ifdef Q_OS_WIN
-      QFileInfo fileBackup(dir, backupName);
-      QString backupNativePath = QDir::toNativeSeparators(fileBackup.absoluteFilePath());
-      SetFileAttributes((LPCTSTR)backupNativePath.toLocal8Bit(), FILE_ATTRIBUTE_HIDDEN);
+            QFileInfo fileBackup(dir, backupName);
+            QString backupNativePath = QDir::toNativeSeparators(fileBackup.absoluteFilePath());
+            SetFileAttributes((LPCTSTR)backupNativePath.toLocal8Bit(), FILE_ATTRIBUTE_HIDDEN);
 #endif
+            }
       //
       // step 4
       // rename temp name into file name