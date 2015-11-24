musescore/MuseScore

commit b45c2fc89f4e7a67eaf98afc1fc51ed8f9e1b9e5

fix mag combobox & initial scaling

mscore/magbox.h
@@ -53,28 +53,35 @@ class MagValidator : public QValidator {
 //   MagBox
 //---------------------------------------------------------
 
-class MagBox : public QComboBox {
+class MagBox : protected QComboBox {
       Q_OBJECT
 
       double freeMag;
 
    private slots:
       void indexChanged(int);
+      void textChanged();
 
    signals:
-      void magChanged(int idx);
+      void magChanged(MagIdx);
 
    public:
       MagBox(QWidget* parent = 0);
       void setMag(double);
       void setMagIdx(MagIdx);
-      double getMag(ScoreView*);
+      double getMag(ScoreView*) const;
+      double getLMag(ScoreView*) const;
+      void setEnabled(bool val) { QComboBox::setEnabled(val); }
+      QString currentText() const { return QComboBox::currentText(); }
+      int count() const { return QComboBox::count(); }
+      void removeItem(int i) { QComboBox::removeItem(i); }
       };
 
 
-
-
 } // namespace Ms
+
+Q_DECLARE_METATYPE(Ms::MagIdx)
+
 #endif
 
 

mscore/magbox.cpp
@@ -18,6 +18,7 @@
 //  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //=============================================================================
 
+#include "preferences.h"
 #include "magbox.h"
 #include "scoreview.h"
 #include "libmscore/page.h"
@@ -27,19 +28,31 @@
 
 namespace Ms {
 
-static MagIdx startMag = MagIdx::MAG_100;
-
 //---------------------------------------------------------
 //   magTable
 //    list of strings shown in QComboBox "MagBox"
 //---------------------------------------------------------
 
-static const char* magTable[] = {
-     "25%", "50%", "75%", "100%", "150%", "200%", "400%", "800%", "1600%",
-      QT_TRANSLATE_NOOP("magTable","Page Width"),
-      QT_TRANSLATE_NOOP("magTable","Whole Page"),
-      QT_TRANSLATE_NOOP("magTable","Two Pages"),
-      ""
+struct MagEntry {
+      const char* txt;
+      MagIdx idx;
+      };
+
+static MagIdx startMag = MagIdx::MAG_100;
+
+static const MagEntry magTable[] = {
+     {  "25%",   MagIdx::MAG_25 },
+     {  "50%",   MagIdx::MAG_50 },
+     {  "75%",   MagIdx::MAG_75 },
+     {  "100%",  MagIdx::MAG_100 },
+     {  "150%",  MagIdx::MAG_150 },
+     {  "200%",  MagIdx::MAG_200 },
+     {  "400%",  MagIdx::MAG_400 },
+     {  "800%",  MagIdx::MAG_800 },
+     {  "1600%", MagIdx::MAG_1600 },
+     {  QT_TRANSLATE_NOOP("magTable","Page Width"), MagIdx::MAG_PAGE_WIDTH },
+     {  QT_TRANSLATE_NOOP("magTable","Whole Page"), MagIdx::MAG_PAGE },
+     {  QT_TRANSLATE_NOOP("magTable","Two Pages"),  MagIdx::MAG_DBL_PAGE },
      };
 
 //---------------------------------------------------------
@@ -56,15 +69,41 @@ MagBox::MagBox(QWidget* parent)
       setWhatsThis(tr("Zoom"));
       setValidator(new MagValidator(this));
 
-      unsigned int n = sizeof(magTable)/sizeof(*magTable) - 1;
-      for (unsigned int i =  0; i < n; ++i) {
-            QString ts(QCoreApplication::translate("magTable", magTable[i]));
-            addItem(ts, i);
-            if (MagIdx(i) == startMag)
+      int i = 0;
+      for (const MagEntry& e : magTable) {
+            QString ts(QCoreApplication::translate("magTable", e.txt));
+            addItem(ts, QVariant::fromValue(e.idx));
+            if (e.idx == startMag)
                   setCurrentIndex(i);
+            ++i;
             }
+      setMaxCount(i+1);
       addItem(QString("%1%").arg(freeMag * 100), int(MagIdx::MAG_FREE));
+      setFocusPolicy(Qt::StrongFocus);
+      setAccessibleName(tr("Zoom"));
+      setFixedHeight(preferences.iconHeight + 10);  // hack
       connect(this, SIGNAL(currentIndexChanged(int)), SLOT(indexChanged(int)));
+      connect(lineEdit(), SIGNAL(returnPressed()), SLOT(textChanged()));
+      }
+
+//---------------------------------------------------------
+//   textChanged
+//---------------------------------------------------------
+
+void MagBox::textChanged()
+      {
+      if (!mscore->currentScoreView() || currentText().isEmpty())
+            return;
+      QString s = currentText();
+      if (s.right(1) == "%")
+            s = s.left(s.length()-1);
+
+      bool ok;
+      qreal magVal = s.toFloat(&ok);
+      if (ok) {
+            setMag((double)(magVal/100.0));
+            emit magChanged(MagIdx::MAG_FREE);
+            }
       }
 
 //---------------------------------------------------------
@@ -73,61 +112,78 @@ MagBox::MagBox(QWidget* parent)
 
 void MagBox::indexChanged(int idx)
       {
-      emit magChanged(idx);
+      emit magChanged(itemData(idx).value<MagIdx>());
       }
 
 //---------------------------------------------------------
-//   txt2mag
+//   getLMag
+//    get logical scale
 //---------------------------------------------------------
 
-double MagBox::getMag(ScoreView* canvas)
+double MagBox::getLMag(ScoreView* canvas) const
       {
-      switch(MagIdx(currentIndex())) {
-            case MagIdx::MAG_25:      return 0.25;
-            case MagIdx::MAG_50:      return 0.5;
-            case MagIdx::MAG_75:      return 0.75;
-            case MagIdx::MAG_100:     return 1.0;
-            case MagIdx::MAG_150:     return 1.5;
-            case MagIdx::MAG_200:     return 2.0;
-            case MagIdx::MAG_400:     return 4.0;
-            case MagIdx::MAG_800:     return 8.0;
-            case MagIdx::MAG_1600:    return 16.0;
-            default:               break;
-            }
+      return getMag(canvas) / (mscore->physicalDotsPerInch() / DPI);
+      }
 
-      QSizeF s(canvas->fsize());
-      double cw      = s.width();
-      double ch      = s.height();
-      double nmag    = canvas->mag();
+//---------------------------------------------------------
+//   getMag
+//    get physical scale
+//---------------------------------------------------------
+
+double MagBox::getMag(ScoreView* canvas) const
+      {
       Score* score   = canvas->score();
       if (score == 0)
             return 1.0;
+
+      MagIdx idx           = MagIdx(currentIndex());
+      qreal pmag           = mscore->physicalDotsPerInch() / DPI;
+      double cw            = canvas->width();
+      double ch            = canvas->height();
       const PageFormat* pf = score->pageFormat();
-      switch(MagIdx(currentIndex())) {
+      double nmag;
+
+      switch (idx) {
+            case MagIdx::MAG_25:      nmag = 0.25 * pmag; break;
+            case MagIdx::MAG_50:      nmag = 0.5  * pmag; break;
+            case MagIdx::MAG_75:      nmag = 0.75 * pmag; break;
+            case MagIdx::MAG_100:     nmag = 1.0  * pmag; break;
+            case MagIdx::MAG_150:     nmag = 1.5  * pmag; break;
+            case MagIdx::MAG_200:     nmag = 2.0  * pmag; break;
+            case MagIdx::MAG_400:     nmag = 4.0  * pmag; break;
+            case MagIdx::MAG_800:     nmag = 8.0  * pmag; break;
+            case MagIdx::MAG_1600:    nmag = 16.0 * pmag; break;
+
             case MagIdx::MAG_PAGE_WIDTH:      // page width
-                  nmag *= (cw / (pf->width() * DPI)) / (mscore->physicalDotsPerInch() / DPI);
+                  nmag = cw / (pf->width() * DPI);
                   break;
+
             case MagIdx::MAG_PAGE:     // page
                   {
-                  double mag1 = (cw  / (pf->width() * DPI) / (mscore->physicalDotsPerInch() / DPI));
-                  double mag2 = (ch / (pf->height() * DPI) / (mscore->physicalDotsPerInch() / DPI));
-                  nmag  *= (mag1 > mag2) ? mag2 : mag1;
+                  double mag1 = cw / (pf->width() *  DPI);
+                  double mag2 = ch / (pf->height() * DPI);
+                  nmag  = (mag1 > mag2) ? mag2 : mag1;
                   }
                   break;
+
             case MagIdx::MAG_DBL_PAGE:    // double page
                   {
-                  double mag1 = (cw / (pf->width()*2*DPI+50.0)) / (mscore->physicalDotsPerInch() / DPI);
-                  double mag2 = (ch / (pf->height() * DPI)) / (mscore->physicalDotsPerInch() / DPI);
-                  nmag  *= (mag1 > mag2) ? mag2 : mag1;
+                  double mag1 = cw / (pf->width() * 2 * DPI + 50);
+                  double mag2 = ch / (pf->height() * DPI);
+                  nmag  = (mag1 > mag2) ? mag2 : mag1;
                   }
                   break;
+
             case MagIdx::MAG_FREE:
-                  return freeMag;
+                  nmag = freeMag * pmag;
+                  break;
+
             default:
                   break;
             }
       if (nmag < 0.0001)
             nmag = canvas->mag();
+
       return nmag;
       }
 

mscore/musescore.cpp
@@ -154,6 +154,10 @@ const char* voiceActions[] = { "voice-1", "voice-2", "voice-3", "voice-4" };
 extern bool savePositions(Score*, const QString& name, bool segments );
 extern TextPalette* textPalette;
 
+static constexpr double SCALE_MAX  = 16.0;
+static constexpr double SCALE_MIN  = 0.05;
+static constexpr double SCALE_STEP = 1.7;
+
 //---------------------------------------------------------
 // cmdInsertMeasure
 //---------------------------------------------------------
@@ -533,11 +537,8 @@ MuseScore::MuseScore()
 
       fileTools->addSeparator();
       mag = new MagBox;
-      mag->setFocusPolicy(Qt::StrongFocus);
-      mag->setAccessibleName(tr("Zoom"));
-      mag->setFixedHeight(preferences.iconHeight + 10);  // hack
-      connect(mag, SIGNAL(magChanged(int)), SLOT(magChanged(int)));
-      connect(mag->lineEdit(), SIGNAL(editingFinished()), SLOT(magTextChanged()));
+      connect(mag, SIGNAL(magChanged(MagIdx)), SLOT(magChanged(MagIdx)));
+      connect(mag, SIGNAL(magTextChanged()), SLOT(magTextChanged()));
       fileTools->addWidget(mag);
       viewModeCombo = new QComboBox(this);
 #if defined(Q_OS_MAC)
@@ -1332,7 +1333,7 @@ void MuseScore::setCurrentScoreView(ScoreView* view)
                   updateInputState(cv->score());
                   }
             cs = cv->score();
-            view->setFocusRect();
+            cv->setFocusRect();
             }
       else
             cs = 0;
@@ -1367,7 +1368,7 @@ void MuseScore::setCurrentScoreView(ScoreView* view)
       if (!cs) {
             setWindowTitle(MUSESCORE_NAME_VERSION);
             if (_navigator && _navigator->widget()) {
-                  navigator()->setScoreView(view);
+                  navigator()->setScoreView(cv);
                   navigator()->setScore(0);
                   }
             if (_inspector)
@@ -1392,10 +1393,10 @@ void MuseScore::setCurrentScoreView(ScoreView* view)
       selectionChanged(cs->selection().state());
 
       _sstate = STATE_DISABLED; // defeat optimization
-      changeState(view->mscoreState());
+      changeState(cv->mscoreState());
 
-      view->setFocus(Qt::OtherFocusReason);
-      setFocusProxy(view);
+      cv->setFocus(Qt::OtherFocusReason);
+      setFocusProxy(cv);
 
       getAction("file-save")->setEnabled(cs->isSavable());
       getAction("file-part-export")->setEnabled(cs->rootScore()->excerpts().size() > 0);
@@ -1408,13 +1409,12 @@ void MuseScore::setCurrentScoreView(ScoreView* view)
       getAction("split-measure")->setEnabled(cs->rootScore()->excerpts().size() == 0);
       updateUndoRedo();
 
-      if (view->magIdx() == MagIdx::MAG_FREE)
+      MagIdx midx = cv->magIdx();
+      if (midx == MagIdx::MAG_FREE)
             mag->setMag(view->lmag());
       else {
-            mag->blockSignals(true);
-            mag->setCurrentIndex(int(view->magIdx()));
-            mag->blockSignals(false);
-            magChanged(int(view->magIdx()));
+            mag->setMagIdx(midx);
+            magChanged(midx);
             }
 
       if (cs->parentScore())
@@ -2969,35 +2969,10 @@ void MuseScore::dirtyChanged(Score* s)
 //   magChanged
 //---------------------------------------------------------
 
-void MuseScore::magChanged(int idx)
+void MuseScore::magChanged(MagIdx idx)
       {
       if (cv)
-            cv->setMag((MagIdx)idx, mag->getMag(cv));
-      }
-
-//---------------------------------------------------------
-//   magTextChanged
-//---------------------------------------------------------
-
-void MuseScore::magTextChanged()
-      {
-      if (!cv || mag->currentText().isEmpty())
-            return;
-
-      QString s = mag->currentText();
-      if (s.right(1) == "%")
-            s = s.left(s.length()-1);
-
-      bool ok;
-      qreal magVal = s.toFloat(&ok);
-      if (ok) {
-            mag->setMag((double)(magVal/100.0));
-            cv->setMag(MagIdx::MAG_FREE, magVal/100.0);
-            }
-
-      //prevent the list from growing
-      if (mag->count()-1 > (int)MagIdx::MAG_FREE)
-            mag->removeItem(mag->count()-1);
+            cv->setMag(idx, mag->getLMag(cv));
       }
 
 //---------------------------------------------------------
@@ -3007,9 +2982,9 @@ void MuseScore::magTextChanged()
 void MuseScore::incMag()
       {
       if (cv) {
-            qreal _mag = cv->lmag() * 1.7;
-            if (_mag > 16.0)
-                  _mag = 16.0;
+            qreal _mag = cv->lmag() * SCALE_STEP;
+            if (_mag > SCALE_MAX)
+                  _mag = SCALE_MAX;
             cv->setMag(MagIdx::MAG_FREE, _mag);
             setMag(_mag);
             }
@@ -3022,9 +2997,9 @@ void MuseScore::incMag()
 void MuseScore::decMag()
       {
       if (cv) {
-            qreal _mag = cv->lmag() / 1.7;
-            if (_mag < 0.05)
-                  _mag = 0.05;
+            qreal _mag = cv->lmag() / SCALE_STEP;
+            if (_mag < SCALE_MIN)
+                  _mag = SCALE_MIN;
             cv->setMag(MagIdx::MAG_FREE, _mag);
             setMag(_mag);
             }
@@ -3032,6 +3007,7 @@ void MuseScore::decMag()
 
 //---------------------------------------------------------
 //   getMag
+//    return physical scale
 //---------------------------------------------------------
 
 double MuseScore::getMag(ScoreView* canvas) const
@@ -3041,6 +3017,7 @@ double MuseScore::getMag(ScoreView* canvas) const
 
 //---------------------------------------------------------
 //   setMag
+//    set logical scale
 //---------------------------------------------------------
 
 void MuseScore::setMag(double d)
@@ -3405,9 +3382,12 @@ bool MuseScore::restoreSession(bool always)
                                     }
                               }
                         else if (tag == "ScoreView") {
-                              double x = .0, y = .0, vmag = .0;
+                              qreal x       = .0;
+                              qreal y       = .0;
+                              qreal vmag    = 1.0;
                               MagIdx magIdx = MagIdx::MAG_FREE;
-                              int tab = 0, idx = 0;
+                              int tab       = 0;
+                              int idx       = 0;
                               while (e.readNextStartElement()) {
                                     const QStringRef& tag(e.name());
                                     if (tag == "tab")
@@ -3427,8 +3407,6 @@ bool MuseScore::restoreSession(bool always)
                                           return false;
                                           }
                                     }
-                              if (magIdx != MagIdx::MAG_FREE)
-                                    vmag = mag->getMag(cv);
                               (tab == 0 ? tab1 : tab2)->initScoreView(idx, vmag, magIdx, x, y);
                               }
                         else if (tag == "tab")

mscore/musescore.h
@@ -436,8 +436,7 @@ class MuseScore : public QMainWindow, public MuseScoreCore {
       void seqStopped();
       void cmdAppendMeasures();
       void cmdInsertMeasures();
-      void magChanged(int);
-      void magTextChanged();
+      void magChanged(MagIdx);
       void showPageSettings();
       void removeTab(int);
       void removeTab();

mscore/scoretab.cpp
@@ -412,6 +412,7 @@ void ScoreTab::initScoreView(int idx, double mag, MagIdx magIdx, double xoffset,
             vs->addWidget(v);
             stack->addWidget(vs);
             }
+//      v->setMag(magIdx, mag / (mscore->physicalDotsPerInch() / DPI));
       v->setMag(magIdx, mag);
       v->setOffset(xoffset, yoffset);
       }

mscore/scoreview.cpp
@@ -1199,11 +1199,8 @@ void ScoreView::measurePopup(const QPoint& gpos, Measure* obj)
 
 void ScoreView::resizeEvent(QResizeEvent* /*ev*/)
       {
-      if (_magIdx == MagIdx::MAG_PAGE_WIDTH || _magIdx == MagIdx::MAG_PAGE || _magIdx == MagIdx::MAG_DBL_PAGE) {
-            double m = mscore->getMag(this);
-            setMag(m);
-            }
-      update();
+      if (_magIdx != MagIdx::MAG_FREE)
+            setMag(mscore->getMag(this));
       }
 
 //---------------------------------------------------------
@@ -2378,10 +2375,11 @@ void ScoreView::setMag(qreal nmag)
                   grip[i] = r.translated(p);
                   }
             }
+      update();
       }
 
 //---------------------------------------------------------
-//   setMagIdx
+//   setMag
 //    mag - logical scale
 //---------------------------------------------------------
 

commit 8712163dbaf871407a098c9c0aa7b251031c760e

fix build failure introduced by b45c2fc

mscore/magbox.h
@@ -53,7 +53,7 @@ class MagValidator : public QValidator {
 //   MagBox
 //---------------------------------------------------------
 
-class MagBox : protected QComboBox {
+class MagBox : public QComboBox {
       Q_OBJECT
 
       double freeMag;
@@ -80,7 +80,7 @@ class MagBox : protected QComboBox {
 
 } // namespace Ms
 
-Q_DECLARE_METATYPE(Ms::MagIdx)
+Q_DECLARE_METATYPE(Ms::MagIdx);
 
 #endif
 