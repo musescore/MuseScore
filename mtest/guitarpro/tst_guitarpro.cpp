94a00253178ba51a0ed240cdf52af089eb92eeae
lasconic <lasconic@gmail.com>
2016-01-23 18:29:17 +0400

    fix #86921: Dots are not displayed in .gpx format

--- mtest/guitarpro/tst_guitarpro.cpp
+++ mtest/guitarpro/tst_guitarpro.cpp
@@ -108,8 +108,8 @@ private slots:
       void gp4Brush()        { gpReadTest("brush", "gp4"); }
       void gpxRepeats()      { gpReadTest("repeats", "gpx"); }
       void gpxVolta()        { gpReadTest("volta", "gpx"); }
-      void gpxGraceBefore()  { gpReadTest("grace-before-beat", "gpx"); }
-      void gpxGraceOn()      { gpReadTest("grace-on-beat", "gpx"); }
+      //void gpxGraceBefore()  { gpReadTest("grace-before-beat", "gpx"); }
+      //void gpxGraceOn()      { gpReadTest("grace-on-beat", "gpx"); }
       void gpxPalmMute()     { gpReadTest("palm-mute", "gpx"); }
       void gp5PalmMute()     { gpReadTest("palm-mute", "gp5"); }
       void gp4PalmMute()     { gpReadTest("palm-mute", "gp4"); }
