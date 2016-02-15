a509b23cae36f7d239e465bf1b36121f616490f5
lasconic <lasconic@gmail.com>
2016-02-15 18:53:00 +0400

    fix #53696: Export as audio gets stuck at 99% with some file and some soundfont.

--- mscore/exportmp3.cpp
+++ mscore/exportmp3.cpp
@@ -709,6 +709,7 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
       EventMap::const_iterator endPos = events.cend();
       --endPos;
       const int et = (score->utick2utime(endPos->first) + 1) * MScore::sampleRate;
+      const int maxEndTime = (score->utick2utime(endPos->first) + 3) * MScore::sampleRate;
       progress.setRange(0, et);
 
       for (int pass = 0; pass < 2; ++pass) {
@@ -833,6 +834,9 @@ bool MuseScore::saveMp3(Score* score, const QString& name)
                         synti->allNotesOff(-1);
                   // create sound until the sound decays
                   if (playTime >= et && max * peak < 0.000001)
+                        break;
+                  // hard limit
+                  if (playTime > maxEndTime)
                         break;
                   }
             if (progress.wasCanceled())
