102ba03fdf1d00a66099332238a102035a94fe68
Marc Sabatella <marc@outsideshore.com>
2016-03-09 16:02:51 -0700

    fix #101386: crash on undo of add with inspector not shown

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -1195,7 +1195,7 @@ void MuseScore::selectionChanged(SelState selectionState)
             pianorollEditor->changeSelection(selectionState);
       if (drumrollEditor)
             drumrollEditor->changeSelection(selectionState);
-      if (_inspector && _inspector->isVisible())
+      if (_inspector)
             updateInspector();
       }
 
@@ -1207,7 +1207,7 @@ void MuseScore::updateInspector()
       {
       if (!_inspector)
             return;
-      if (cs) {
+      if (_inspector->isVisible() && cs) {
             if (state() == STATE_EDIT)
                   _inspector->setElement(cv->getEditObject());
             else if (state() == STATE_FOTO)
