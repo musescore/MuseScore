d80fd6df09d9aaa708429b1cc1303faf8c63e663
lasconic <lasconic@gmail.com>
2016-03-17 16:42:52 +0400

    fix #99796: Linked staff becomes unlinked (causing corruption) after delete of part

--- mscore/excerptsdialog.cpp
+++ mscore/excerptsdialog.cpp
@@ -425,7 +425,7 @@ void ExcerptsDialog::accept()
                                                 }
                                           }
                                     // unlink the staff
-                                    score->undo(new UnlinkStaff(s, staff));
+                                    score->undo(new UnlinkStaff(staff, s));
                                     }
                               }
                         // remove the excerpt
