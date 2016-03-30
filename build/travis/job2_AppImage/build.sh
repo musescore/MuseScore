fd2c2e40dfb9071b77b59824e8dabc6b29e7a285
Eric Fontaine <eric@ericfontainejazz.com>
2016-03-29 22:14:49 -0400

    cross compile AppImage in debian docker on travis for armhf

--- build/travis/job2_AppImage/build.sh
+++ build/travis/job2_AppImage/build.sh
@@ -5,7 +5,7 @@
 # always be uploaded unless a list of specific branches is passed in. e.g.:
 #    $   build.sh  --upload-branches  master  my-branch-1  my-branch-2
 # Builds will be for the native architecture (64 bit) unless another is
-# specified for cross-compiling. (e.g. build.sh --32bit)
+# specified for cross-compiling. (e.g. build.sh --i686 or build.sh --armhf)
 
 set -e # exit on error
 set -x # echo commands
@@ -37,17 +37,35 @@ else
   makefile_overrides="" # use Makefile defaults
 fi
 
-# Build AppImage. Are we cross-compiling?
+# Build AppImage depending on arch specified in $1 if cross-compiling, else default build x86_64
 case "$1" in
-  --32bit )
+
+  --armhf )
     shift
-    # Build MuseScore AppImage inside 32-bit Docker image
+    # build MuseScore inside debian x86-64 multiarch image containing arm cross toolchain and libraries
+    docker run -i -v "${PWD}:/MuseScore" \
+      ericfont/musescore:jessie-crosscompile-armhf \
+      /bin/bash -c \
+      "/MuseScore/build/Linux+BSD/portable/RecipeDebian --build-only armhf $makefile_overrides"
+    # then run inside fully emulated arm image for AppImage packing step (which has trouble inside multiarch image)
+    docker run -i --privileged multiarch/qemu-user-static:register
+    docker run -i -v "${PWD}:/MuseScore" --privileged \
+      ericfont/musescore:jessie-packaging-armhf \
+      /bin/bash -c \
+      "/MuseScore/build/Linux+BSD/portable/RecipeDebian --package-only armhf"
+    ;;
+
+  --i686 )
+    shift
+    # Build MuseScore AppImage inside 32-bit x86 Docker image
     docker run -i -v "${PWD}:/MuseScore" toopher/centos-i386:centos6 /bin/bash -c \
       "linux32 --32bit i386 /MuseScore/build/Linux+BSD/portable/Recipe $makefile_overrides"
     ;;
+
+
   * )
-    [ "$1" == "--64bit" ] && shift || true
-    # Build MuseScore AppImage inside native (64-bit) Docker image
+    [ "$1" == "--x86_64" ] && shift || true
+    # Build MuseScore AppImage inside native (64-bit x86) Docker image
     docker run -i -v "${PWD}:/MuseScore" library/centos:6 /bin/bash -c \
       "/MuseScore/build/Linux+BSD/portable/Recipe $makefile_overrides"
     ;;
