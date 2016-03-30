b595cf66f6638ae151a62e37929b28a0d6e4a498
lasconic <lasconic@gmail.com>
2016-03-30 15:31:15 +0400

    clean CMake output on Mac and Windows

--- build/FindPulseAudio.cmake
+++ build/FindPulseAudio.cmake
@@ -13,94 +13,95 @@
 #  BSD license.
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
+if (NOT MINGW AND NOT APPLE)
+  if (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
+    # in cache already
+    set(PULSEAUDIO_FOUND TRUE)
+  else (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
+    # use pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    if (DEFINED CMAKE_TOOLCHAIN_FILE)
+      include (UsePkgConfig)
+      pkgconfig (libpulse PULSEAUDIO_INCLUDEDIRS PULSEAUDIO_LIBDIR PULSEAUDIO_LDFLAGS PULSEAUDIO_CFLAGS)
+      set (PULSEAUDIO_LIBRARY "${PULSEAUDIO_LIBDIR}/libpulse.so" )
+      set (PULSEAUDIO_FOUND TRUE)
+    else (DEFINED CMAKE_TOOLCHAIN_FILE)
+      if (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
+        include(UsePkgConfig)
+        pkgconfig(libpulse _PULSEAUDIO_INCLUDEDIR _PULSEAUDIO_LIBDIR _PULSEAUDIO_LDFLAGS _PULSEAUDIO_CFLAGS)
+      else (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
+        find_package(PkgConfig)
+        if (PKG_CONFIG_FOUND)
+          pkg_check_modules(_PULSEAUDIO libpulse)
+        endif (PKG_CONFIG_FOUND)
+      endif (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
+      find_path(PULSEAUDIO_INCLUDE_DIR
+      NAMES
+        pulse/pulseaudio.h
+      PATHS
+        ${_PULSEAUDIO_INCLUDEDIR}
+        /usr/include
+        /usr/local/include
+        /opt/local/include
+        /sw/include
+      )
 
-if (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
-  # in cache already
-  set(PULSEAUDIO_FOUND TRUE)
-else (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
-  # use pkg-config to get the directories and then use these values
-  # in the FIND_PATH() and FIND_LIBRARY() calls
-  if (DEFINED CMAKE_TOOLCHAIN_FILE)
-    include (UsePkgConfig)
-    pkgconfig (libpulse PULSEAUDIO_INCLUDEDIRS PULSEAUDIO_LIBDIR PULSEAUDIO_LDFLAGS PULSEAUDIO_CFLAGS)
-    set (PULSEAUDIO_LIBRARY "${PULSEAUDIO_LIBDIR}/libpulse.so" )
-    set (PULSEAUDIO_FOUND TRUE)
-  else (DEFINED CMAKE_TOOLCHAIN_FILE)
-    if (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
-      include(UsePkgConfig)
-      pkgconfig(libpulse _PULSEAUDIO_INCLUDEDIR _PULSEAUDIO_LIBDIR _PULSEAUDIO_LDFLAGS _PULSEAUDIO_CFLAGS)
-    else (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
-      find_package(PkgConfig)
-      if (PKG_CONFIG_FOUND)
-        pkg_check_modules(_PULSEAUDIO libpulse)
-      endif (PKG_CONFIG_FOUND)
-    endif (${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} EQUAL 4)
-    find_path(PULSEAUDIO_INCLUDE_DIR
-    NAMES
-      pulse/pulseaudio.h
-    PATHS
-      ${_PULSEAUDIO_INCLUDEDIR}
-      /usr/include
-      /usr/local/include
-      /opt/local/include
-      /sw/include
-    )
+      find_library(PULSEAUDIO_LIBRARY
+      NAMES
+        pulse
+      PATHS
+        ${_PULSEAUDIO_LIBDIR}
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        /sw/lib
+      )
 
-    find_library(PULSEAUDIO_LIBRARY
-    NAMES
-      pulse
-    PATHS
-      ${_PULSEAUDIO_LIBDIR}
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-    )
+      find_library(PULSEAUDIO_SIMPLE_LIBRARY
+      NAMES
+        pulse-simple
+      PATHS
+        ${_PULSEAUDIO_LIBDIR}
+        /usr/lib
+        /usr/local/lib
+        /opt/local/lib
+        /sw/lib
+      )
 
-    find_library(PULSEAUDIO_SIMPLE_LIBRARY
-    NAMES
-      pulse-simple
-    PATHS
-      ${_PULSEAUDIO_LIBDIR}
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      /sw/lib
-    )
+      if (PULSEAUDIO_LIBRARY)
+        set(PULSEAUDIO_FOUND TRUE)
+      endif (PULSEAUDIO_LIBRARY)
 
-    if (PULSEAUDIO_LIBRARY)
-      set(PULSEAUDIO_FOUND TRUE)
-    endif (PULSEAUDIO_LIBRARY)
+      set(PULSEAUDIO_INCLUDE_DIRS
+      ${PULSEAUDIO_INCLUDE_DIR}
+      )
+    endif (DEFINED CMAKE_TOOLCHAIN_FILE)
 
-    set(PULSEAUDIO_INCLUDE_DIRS
-    ${PULSEAUDIO_INCLUDE_DIR}
-    )
-  endif (DEFINED CMAKE_TOOLCHAIN_FILE)
+    if (PULSEAUDIO_FOUND)
+      set(PULSEAUDIO_LIBRARIES
+        ${PULSEAUDIO_LIBRARIES}
+        ${PULSEAUDIO_SIMPLE_LIBRARY}
+        ${PULSEAUDIO_LIBRARY}
+      )
+    endif (PULSEAUDIO_FOUND)
 
-  if (PULSEAUDIO_FOUND)
-    set(PULSEAUDIO_LIBRARIES
-      ${PULSEAUDIO_LIBRARIES}
-      ${PULSEAUDIO_SIMPLE_LIBRARY}
-      ${PULSEAUDIO_LIBRARY}
-    )
-  endif (PULSEAUDIO_FOUND)
+    if (PULSEAUDIO_INCLUDE_DIRS AND PULSEAUDIO_LIBRARIES)
+       set(PULSEAUDIO_FOUND TRUE)
+    endif (PULSEAUDIO_INCLUDE_DIRS AND PULSEAUDIO_LIBRARIES)
 
-  if (PULSEAUDIO_INCLUDE_DIRS AND PULSEAUDIO_LIBRARIES)
-     set(PULSEAUDIO_FOUND TRUE)
-  endif (PULSEAUDIO_INCLUDE_DIRS AND PULSEAUDIO_LIBRARIES)
+    if (PULSEAUDIO_FOUND)
+      if (NOT PULSEAUDIO_FIND_QUIETLY)
+        message(STATUS "Found pulseaudio. LIB ${PULSEAUDIO_LIBRARY}, INCLUDE ${PULSEAUDIO_INCLUDEDIRS}, LIBDIR ${PULSEAUDIO_LIBDIR}, LDFLAGS ${PULSEAUDIO_LDFLAGS}, CFLAGS ${PULSEAUDIO_CFLAGS}")
+      endif (NOT PULSEAUDIO_FIND_QUIETLY)
+    else (PULSEAUDIO_FOUND)
+      if (PULSEAUDIO_FIND_REQUIRED)
+        message(FATAL_ERROR "Could not find PulseAudio")
+      endif (PULSEAUDIO_FIND_REQUIRED)
+    endif (PULSEAUDIO_FOUND)
 
-  if (PULSEAUDIO_FOUND)
-    if (NOT PULSEAUDIO_FIND_QUIETLY)
-      message(STATUS "Found pulseaudio. LIB ${PULSEAUDIO_LIBRARY}, INCLUDE ${PULSEAUDIO_INCLUDEDIRS}, LIBDIR ${PULSEAUDIO_LIBDIR}, LDFLAGS ${PULSEAUDIO_LDFLAGS}, CFLAGS ${PULSEAUDIO_CFLAGS}")
-    endif (NOT PULSEAUDIO_FIND_QUIETLY)
-  else (PULSEAUDIO_FOUND)
-    if (PULSEAUDIO_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find PulseAudio")
-    endif (PULSEAUDIO_FIND_REQUIRED)
-  endif (PULSEAUDIO_FOUND)
+    # show the PULSEAUDIO_INCLUDE_DIRS and PULSEAUDIO_LIBRARIES variables only in the advanced view
+    mark_as_advanced(PULSEAUDIO_INCLUDE_DIRS PULSEAUDIO_LIBRARIES)
 
-  # show the PULSEAUDIO_INCLUDE_DIRS and PULSEAUDIO_LIBRARIES variables only in the advanced view
-  mark_as_advanced(PULSEAUDIO_INCLUDE_DIRS PULSEAUDIO_LIBRARIES)
-
-endif (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
+  endif (PULSEAUDIO_LIBRARIES AND PULSEAUDIO_INCLUDE_DIRS)
+endif (NOT MINGW AND NOT APPLE)
 
--- build/UsePkgConfig1.cmake
+++ build/UsePkgConfig1.cmake
@@ -9,37 +9,39 @@
 # if pkg-config was NOT found or the specified software package doesn't exist, the
 # variable will be empty when the function returns, otherwise they will contain the respective information
 #
+IF (NOT MINGW)
+  FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS ${PATH} /usr/bin /usr/local/bin )
 
-FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS ${PATH} /usr/bin /usr/local/bin )
+  MACRO(PKGCONFIG1 _package _minVersion _include_DIR _link_DIR _link_FLAGS _cflags)
+  # reset the variables at the beginning
+    SET(${_include_DIR})
+    SET(${_link_DIR})
+    SET(${_link_FLAGS})
+    SET(${_cflags})
 
-MACRO(PKGCONFIG1 _package _minVersion _include_DIR _link_DIR _link_FLAGS _cflags)
-# reset the variables at the beginning
-  SET(${_include_DIR})
-  SET(${_link_DIR})
-  SET(${_link_FLAGS})
-  SET(${_cflags})
+    # if pkg-config has been found
+    IF(PKGCONFIG_EXECUTABLE)
 
-  # if pkg-config has been found
-  IF(PKGCONFIG_EXECUTABLE)
+      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --atleast-version=${_minVersion} RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+  #    EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
 
-    EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --atleast-version=${_minVersion} RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
-#    EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+      # and if the package of interest also exists for pkg-config, then get the information
+      IF(NOT _return_VALUE)
 
-    # and if the package of interest also exists for pkg-config, then get the information
-    IF(NOT _return_VALUE)
+        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=includedir OUTPUT_VARIABLE ${_include_DIR} )
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=includedir OUTPUT_VARIABLE ${_include_DIR} )
+        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=libdir OUTPUT_VARIABLE ${_link_DIR} )
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable=libdir OUTPUT_VARIABLE ${_link_DIR} )
+        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs OUTPUT_VARIABLE ${_link_FLAGS} )
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --libs OUTPUT_VARIABLE ${_link_FLAGS} )
+        EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --cflags OUTPUT_VARIABLE ${_cflags} )
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --cflags OUTPUT_VARIABLE ${_cflags} )
+      ENDIF(NOT _return_VALUE)
 
-    ENDIF(NOT _return_VALUE)
+    ENDIF(PKGCONFIG_EXECUTABLE)
 
-  ENDIF(PKGCONFIG_EXECUTABLE)
+  ENDMACRO(PKGCONFIG1 _include_DIR _link_DIR _link_FLAGS _cflags)
 
-ENDMACRO(PKGCONFIG1 _include_DIR _link_DIR _link_FLAGS _cflags)
+  MARK_AS_ADVANCED(PKGCONFIG_EXECUTABLE)
+ENDIF (NOT MINGW)
 
-MARK_AS_ADVANCED(PKGCONFIG_EXECUTABLE)
