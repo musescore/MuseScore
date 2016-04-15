a5122ddd5b0c1efa02d19e33fd30c2f0b5cf1d9d
Joachim Schmitz <jojo@schmitz-digital.de>
2016-04-15 14:24:01 +0200

    fix/update language codes of qt_*.ts files
    and some minor corrections to qt_de.ts

--- share/locale/qt_eu.ts
+++ share/locale/qt_eu.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="eu">
 <context>
     <name>AudioOutput</name>
     <message>
--- share/locale/qt_nl.ts
+++ share/locale/qt_nl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="nl_NL">
+<TS version="2.0" language="nl">
 <context>
     <name>MAC_APPLICATION_MENU</name>
     <message>
