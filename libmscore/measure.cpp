7b27c971ca6cb41c8341fb60110149cf062749e7
Joachim Schmitz <jojo@schmitz-digital.de>
2016-04-21 10:14:42 +0200

    fix #106356: Crash when creating full measure rests

--- libmscore/measure.cpp
+++ libmscore/measure.cpp
@@ -513,7 +513,7 @@ ChordRest* Measure::findChordRest(int tick, int track)
                   return 0;
             if (seg.tick() == tick) {
                   Element* el = seg.element(track);
-                  if (el->isChordRest())
+                  if (el && el->isChordRest())
                         return toChordRest(el);
                   }
             }
