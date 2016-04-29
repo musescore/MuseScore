892e624112ce8bae3b416484725c1dcb12fdb048
lasconic <lasconic@gmail.com>
2016-04-29 19:14:14 +0200

    build Mac OSX nightlies on Travis-CI

---
+++ build/travis/job_macos/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# do not build mac for PR
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+  exit 0
+fi
+
+# install dependencies
+wget -c --no-check-certificate -nv -O bottles.zip https://musescore.org/sites/musescore.org/files/bottles-MuseScore-3.0.zip
+unzip bottles.zip
+
+# we don't use freetype
+rm bottles/freetype*
+
+brew update
+brew install bottles/libogg*
+brew install bottles/libvorbis*
+brew install bottles/flac*
+brew install bottles/libsndfile*
+brew install bottles/portaudio*
+
+# additional dependencies
+brew install jack lame
+
+#install Qt
+which -s qmake
+QT_INSTALLED=$?
+QMAKE_VERSION=
+if [[ $QT_INSTALLED == 0 ]]; then
+  QMAKE_VERSION=$(qmake -query QT_VERSION)
+fi
+
+echo "QMAKE_VERSION $QMAKE_VERSION"
+echo "QT_INSTALLED $QT_INSTALLED"
+echo "QT_LONG_VERSION QT_LONG_VERSION"
+
+if [[ "$QMAKE_VERSION" != "${QT_LONG_VERSION}" ]]; then
+  rm -rf $QT_PATH
+  echo "Downloading Qt"
+  wget -c --no-check-certificate -nv https://download.qt.io/archive/qt/${QT_SHORT_VERSION}/${QT_LONG_VERSION}/${QT_INSTALLER_FILENAME}
+  hdiutil mount ${QT_INSTALLER_FILENAME}
+
+  QT_INSTALLER_EXE=/Volumes/${QT_INSTALLER_ROOT}/${QT_INSTALLER_ROOT}.app/Contents/MacOS/${QT_INSTALLER_ROOT}
+
+  echo "Installing Qt"
+  ./build/travis/job_macos/extract-qt-installer $QT_INSTALLER_EXE $QT_PATH
+else
+  echo "Qt ${QT_LONG_VERSION} already installed"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
