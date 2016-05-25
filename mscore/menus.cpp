musescore/MuseScore

commit 492709f2039a8537056fbcfb90eb9fbf8a6310f6

code cleanups

mscore/menus.cpp
@@ -321,11 +321,13 @@ Palette* MuseScore::newBarLinePalette(bool basic)
       sp->setGrid(42, 38);
 
       // bar line styles
-      for (unsigned i = 0; i < BarLine::barLineTableSize(); ++i) {
+      for (unsigned i = 0;; ++i) {
+            const BarLineTableItem* bti = BarLine::barLineTableItem(i);
+            if (!bti)
+                  break;
             BarLine* b  = new BarLine(gscore);
-            BarLineTableItem bti = BarLine::barLineTableItem(i);
-            b->setBarLineType(bti.type);
-            sp->append(b, qApp->translate("Palette", bti.name));
+            b->setBarLineType(bti->type);
+            sp->append(b, qApp->translate("Palette", bti->name));
             }
 
       if (!basic) {

commit d7ed91eedd25ff8f15b9764a827ae3ac4f03ebfc

fix translation for barlines, broken since 492709f
and C++11 style for loop

mscore/menus.cpp
@@ -327,26 +327,26 @@ Palette* MuseScore::newBarLinePalette(bool basic)
                   break;
             BarLine* b = new BarLine(gscore);
             b->setBarLineType(bti->type);
-            sp->append(b, qApp->translate("Palette", bti->name));
+            sp->append(b, BarLine::userTypeName(bti->type));
             }
 
       if (!basic) {
       // bar line spans
             struct {
                   int         from, to;
-                  const char* name;
-                  } span[] = {
+                  const char* userName;
+                  } spans[] = {
                   { BARLINE_SPAN_TICK1_FROM, BARLINE_SPAN_TICK1_TO, QT_TRANSLATE_NOOP("Palette", "Tick 1 span") },
                   { BARLINE_SPAN_TICK2_FROM, BARLINE_SPAN_TICK2_TO, QT_TRANSLATE_NOOP("Palette", "Tick 2 span") },
                   { BARLINE_SPAN_SHORT1_FROM,BARLINE_SPAN_SHORT1_TO,QT_TRANSLATE_NOOP("Palette", "Short 1 span") },
                   { BARLINE_SPAN_SHORT2_FROM,BARLINE_SPAN_SHORT2_TO,QT_TRANSLATE_NOOP("Palette", "Short 2 span") },
                   };
-            for (unsigned i = 0; i < sizeof(span)/sizeof(*span); ++i) {
-                  BarLine* b  = new BarLine(gscore);
+            for (auto span : spans) {
+                  BarLine* b = new BarLine(gscore);
                   b->setBarLineType(BarLineType::NORMAL);
-                  b->setSpanFrom(span[i].from);
-                  b->setSpanTo(span[i].to);
-                  sp->append(b, qApp->translate("Palette", span[i].name));
+                  b->setSpanFrom(span.from);
+                  b->setSpanTo(span.to);
+                  sp->append(b, qApp->translate("Palette", span.userName));
                   }
             }
       return sp;
@@ -397,7 +397,7 @@ Palette* MuseScore::newRepeatsPalette()
 
             BarLine* b = new BarLine(gscore);
             b->setBarLineType(bti->type);
-            PaletteCell* cell= sp->append(b, qApp->translate("Palette", bti->name));
+            PaletteCell* cell= sp->append(b, BarLine::userTypeName(bti->type));
             cell->drawStaff = false;
             }
 