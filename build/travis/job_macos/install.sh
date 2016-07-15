50c75eeb19eb3deca2dfc2c3773de0d538dc0032
lasconic <lasconic@gmail.com>
2016-07-15 22:09:23 +0200

    travis MacOSX: upgrade ruby to work around ruby/xcpretty incompatibility. See https://github.com/travis-ci/travis-ci/issues/5554

--- build/travis/job_macos/install.sh
+++ build/travis/job_macos/install.sh
@@ -5,6 +5,8 @@ if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
   exit 0
 fi
 
+rvm use ruby-2.2.1
+
 # install dependencies
 wget -c --no-check-certificate -nv -O bottles.zip https://musescore.org/sites/musescore.org/files/bottles-MuseScore-3.0.zip
 unzip bottles.zip
