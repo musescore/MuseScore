7a42f74a8b6e86bb4d8cab228240164658a2fe49
Leon Vinken <leon.vinken@gmail.com>
2016-08-21 12:46:17 +0200

    fix #120156: Infinite loop with an XML

--- mscore/importmxmlpass2.cpp
+++ mscore/importmxmlpass2.cpp
@@ -5544,7 +5544,6 @@ void MusicXMLParserPass2::notations(Note* note, ChordRest* cr, const int tick,
                         else if (_e.name() == "tremolo") {
                               tremoloType = _e.attributes().value("type").toString();
                               tremolo = _e.readElementText().toInt();
-                              _e.readNext();
                               }
                         else if (_e.name() == "accidental-mark")
                               skipLogCurrElem();
