13121aaa1546142c9e1319d50fb134432838e291
lasconic <lasconic@gmail.com>
2016-10-04 16:26:58 +0200

    fix #126901: swap directional icons for RTL languages

--- mscore/pianoroll.cpp
+++ mscore/pianoroll.cpp
@@ -49,8 +49,14 @@ PianorollEditor::PianorollEditor(QWidget* parent)
 
       QWidget* mainWidget = new QWidget;
       QToolBar* tb = addToolBar(tr("Toolbar 1"));
-      tb->addAction(getAction("undo"));
-      tb->addAction(getAction("redo"));
+      if (qApp->layoutDirection() == Qt::LayoutDirection::LeftToRight) {
+            tb->addAction(getAction("undo"));
+            tb->addAction(getAction("redo"));
+            }
+      else {
+            tb->addAction(getAction("redo"));
+            tb->addAction(getAction("undo"));
+            }
       tb->addSeparator();
 #ifdef HAS_MIDI
       tb->addAction(getAction("midi-on"));
