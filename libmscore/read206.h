87d241071dc03f4522067d2e99ae9097100eb590
Werner Schweer <ws@wschweer.de>
2016-10-06 12:32:43 +0200

    add missing file

---
+++ libmscore/read206.h
@@ -0,0 +1,24 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2016 Werner Schweer
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2
+//  as published by the Free Software Foundation and appearing in
+//  the file LICENCE.GPL
+//=============================================================================
+
+#ifndef __READ206_H__
+#define __READ206_H__
+
+namespace Ms {
+
+extern void readArticulation(Articulation* a, XmlReader& e);
+
+}
+
+#endif
+
+
