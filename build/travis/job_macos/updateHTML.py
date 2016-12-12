134569aa6ab9f5babd06a72c8f46bae101dda19f
lasconic <lasconic@gmail.com>
2016-12-12 16:14:37 +0100

    fix #150971: Reverse order of new Mac nightly builds page, add index.html and RSS

---
+++ build/travis/job_macos/updateHTML.py
@@ -0,0 +1,87 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+from datetime import datetime
+import time
+from subprocess import check_output
+
+
+def getFileList(sshKey):
+    a = ["ssh", "-i", sshKey, "musescore-nightlies@ftp-osl.osuosl.org", "ls ftp/macosx/"]
+    return check_output(a)
+
+
+def generateHTML(pathname, lines):
+    li = ""
+    for l in lines:
+        if l:
+            li += '<li><a href="http://ftp.osuosl.org/pub/musescore-nightlies/macosx/' + l + '">' + l + "</a></li>"
+
+    htmlTplPath = pathname + "/web/index.html.tpl"
+    htmlPath = pathname + "/web/index.html"
+
+    htmlTplFile = open(htmlTplPath, 'r')
+    htmlTemplate = htmlTplFile.read()
+    htmlTemplate = htmlTemplate.replace("##PLACEHOLDER##", li)
+    htmlTplFile.close()
+    htmlFile = open(htmlPath, "w")
+    htmlFile.write(htmlTemplate)
+    htmlFile.close()
+
+
+def generateRSS(pathname, lines):
+    li = ""
+    for l in lines:
+        if l:
+            info = l.split("-")
+            date = info[1]
+            branch = info[2]
+            commit = info[3]
+            d = datetime.strptime(date, '%Y%m%d%H%M')
+            from email.utils import formatdate
+            li += '''
+<item>
+<title>''' + l + '''</title>
+<link>http://ftp.osuosl.org/pub/musescore-nightlies/macosx/''' + l + '''</link>
+<pubDate>'''+formatdate(time.mktime(d.timetuple()), usegmt=True)+'''</pubDate>
+<description>Nightly Build of MuseScore, Branch: ''' + branch + ''', Revision: '''+commit + '''</description>
+</item>'''
+
+    rssTplPath = pathname + "/web/nightly.xml.tpl"
+    rssPath = pathname + "/web/nightly.xml"
+
+    rssTplFile = open(rssTplPath, 'r')
+    rssTemplate = rssTplFile.read()
+    rssTemplate = rssTemplate.replace("##PLACEHOLDER##", li)
+    rssTplFile.close()
+    rssFile = open(rssPath, "w")
+    rssFile.write(rssTemplate)
+    rssFile.close()
+
+
+def main():
+    try:
+        sshKey = ""
+        # Parse command line variable
+        if len(sys.argv) == 2:
+            sshKey = sys.argv[1].lower()
+        else:
+            print "Need SSH key"
+            exit(0)
+
+        pathname = os.path.abspath(os.path.dirname(sys.argv[0]))
+        print('path =', pathname)
+        fileList = getFileList(sshKey)
+        l = fileList.split("\n")
+        l.reverse()    # most recent first
+        generateHTML(pathname, l)
+        generateRSS(pathname, l)
+
+    except:
+        print "Unexpected error:", sys.exc_info()[0], "\n"
+        print sys.exc_info()[1], "\n"
+        import traceback
+        print traceback.print_tb(sys.exc_info()[2]), "\n"
+
+if __name__ == '__main__':
+    main()
---
+++ build/travis/job_macos/web/index.html.tpl
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Nightly builds for Mac OS | MuseScore</title>
+<link rel="alternate" type="application/rss+xml" title="Nightly build for Mac OS feed" href="nightly.xml" />
+<style type="text/css">
+#nightly-list li:first-child {font-weight:bold}
+</style>
+</head>
+<body>
+
+<h2>Nightly builds for Mac OS</h2>
+
+<p>The nightly builds allow you to test the very latest developments and bug fixes. The goal is to catch new crashes and regressions before they reach the prereleases or stable versions. For a more detailed description see <a href="http://www.musescore.org/en/handbook/comparison-stable-prerelease-and-nightly-builds">Comparison of stable, prerelease, and nightly builds</a>. You can report any regression or bugs </p>
+
+<p>A nightly build can run side-by-side with other versions of MuseScore on the same computer. If you change the preferences in the nightly build it does not affect your preferences in the prerelease or stable versions of MuseScore. The nightly builds use a cymbal logo to visually differentiate them from normal versions of MuseScore. The cymbal is a play on the fact that nightly builds are likely to crash. If this sounds scary to you, use the latest stable release instead. For the latest stable version of MuseScore visit <a href="http://www.musescore.org/">musescore.org</a>.</p>
+
+<h3 id="instructions">Instructions</h3>
+<ol>
+<li>Download one of the "MuseScoreNightly" files from the Download section below. (The first is the most recent)</li>
+<li>Open the "dmg" file, then drag and drop MuseScoreNighly to your <code>Applications</code> folder.</li>
+</ol>
+
+<h3 id="download">Download</h3>
+<ul id="nightly-list">
+##PLACEHOLDER##
+</ul>
+
+
+<p>* <strong>If you experience a crash on startup</strong>, revert to factory settings by running MuseScore with the -F flag.</p>
+
+<p>Follow nightly builds via <a href="nightly.xml">RSS feed</a></p>
+
+</body>
+</html>
