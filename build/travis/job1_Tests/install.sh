b82a90bcdbe28dcab285079921b2bddcca87f3f9
lasconic <lasconic@gmail.com>
2016-12-30 15:35:00 +0100

    Update translation on Transifex from Travis job test

--- build/travis/job1_Tests/install.sh
+++ build/travis/job1_Tests/install.sh
@@ -4,6 +4,24 @@
 set -e # exit on error
 set -x # echo commands
 
+
+# update translation on transifex
+if [ "$TRAVIS" = true ] && [ "$TRAVIS_PULL_REQUEST" == false ] ; then
+pip install transifex-client
+
+cat > ~/.transifexrc <<EOL
+[https://www.transifex.com]
+hostname = https://www.transifex.com
+password = $TRANSIFEX_PASSWORD
+token =
+username = $TRANSIFEX_USER
+EOL
+
+tx push -s
+
+fi
+
+# build MuseScore
 make revision
 make debug CPUS=2 PREFIX="$HOME/software"
 make installdebug CPUS=2 PREFIX="$HOME/software"
