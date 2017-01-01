14309ada50528b02b581334beacec55897943d5f
Fyrult <fyrult@gmail.com>
2016-12-31 23:34:17 -0500

    fix #153501: After enharmonic change, double flats stay

--- libmscore/accidental.h
+++ libmscore/accidental.h
@@ -320,7 +320,7 @@ class Accidental : public Element {
       static const char* subtype2name(AccidentalType);
       static AccidentalType value2subtype(AccidentalVal);
       static AccidentalType name2subtype(const QString&);
-      static bool isMicrotonal(AccidentalType t)  { return t > AccidentalType::SHARP2; }
+      static bool isMicrotonal(AccidentalType t)  { return t > AccidentalType::FLAT2; }
 
       QString accessibleInfo() const override;
       };
