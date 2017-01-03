865dcf645d96ed8fc9e8a62179452d8e257642da
Raphael Cojocaru <raphael.cojocaru@yahoo.com>
2017-01-03 21:59:32 +0200

    fix #105301: Solved the Find function bug by replacing the editingFinished() signal with returnPressed(). Now the search dialog can display the values previously searched.

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -5226,8 +5226,7 @@ void MuseScore::showSearchDialog()
 
             qDebug("Line edit %p", searchCombo->lineEdit());
 
-            // does not work: connect(searchCombo->lineEdit(), SIGNAL(returnPressed()), SLOT(endSearch()));
-            connect(searchCombo->lineEdit(), SIGNAL(editingFinished()), SLOT(endSearch()));
+            connect(searchCombo->lineEdit(), SIGNAL(returnPressed()), SLOT(endSearch()));
             }
 
       searchCombo->clearEditText();
