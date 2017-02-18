041595c6965815839609097e9c83721631583e82
lasconic <lasconic@gmail.com>
2017-02-18 15:56:10 +0100

    AppImages OSUOSL: use expect

--- build/travis/job2_AppImage/osuosl.sh
+++ build/travis/job2_AppImage/osuosl.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Do not upload artefacts generated as part of a pull request
-if [ $(env | grep TRAVIS_PULL_REQUEST) ] ; then
+if [ $(env | grep TRAVIS_PULL_REQUEST) == "TRAVIS_PULL_REQUEST" ] ; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "Not uploading AppImage since this is a pull request."
     exit 0
   fi
 fi
 
-
 FILE="$1"
 [ -f "$FILE" ] || { echo "$0: Please provide a valid path to a file" >&2 ; exit 1 ;}
 
 # instal ssh key
-openssl aes-256-cbc -K $encrypted_99b076488ab1_key -iv $encrypted_99b076488ab1_iv -in build/travis/resources/osuosl_nighlies_rsa.enc -out build/travis/resources/osuosl_nighlies_rsa -d -pass "pass:$OSUOSL_NIGHTLY_PASSPHRASE"
-# Make ssh dir
-mkdir $HOME/.ssh/
+openssl aes-256-cbc -K $encrypted_99b076488ab1_key -iv $encrypted_99b076488ab1_iv -in build/travis/resources/osuosl_nighlies_rsa.enc -out build/travis/resources/osuosl_nighlies_rsa -d
+
 # Copy over private key, and set permissions
 cp build/travis/resources/osuosl_nighlies_rsa $HOME/.ssh/osuosl_nighlies_rsa
 # set permission
@@ -26,7 +24,12 @@ touch $HOME/.ssh/known_hosts
 ssh-keyscan ftp-osl.osuosl.org >> $HOME/.ssh/known_hosts
 
 eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/osuosl_nighlies_rsa
+expect << EOF
+  spawn ssh-add $HOME/.ssh/osuosl_nighlies_rsa
+  expect "Enter passphrase"
+  send "${OSUOSL_NIGHTLY_PASSPHRASE}\r"
+  expect eof
+EOF
 
 SSH_INDENTITY=$HOME/.ssh/osuosl_nighlies_rsa
 
