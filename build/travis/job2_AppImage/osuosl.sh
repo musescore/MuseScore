a7f6f744828ba24a73fe4e21095aa75569bfae44
lasconic <lasconic@gmail.com>
2017-02-18 09:46:45 +0100

    fix upload of AppImages to OSUOSL

--- build/travis/job2_AppImage/osuosl.sh
+++ build/travis/job2_AppImage/osuosl.sh
@@ -13,7 +13,7 @@ FILE="$1"
 [ -f "$FILE" ] || { echo "$0: Please provide a valid path to a file" >&2 ; exit 1 ;}
 
 # instal ssh key
-openssl aes-256-cbc -K $encrypted_99b076488ab1_key -iv $encrypted_99b076488ab1_iv -in build/travis/resources/osuosl_nighlies_rsa.enc -out build/travis/resources/osuosl_nighlies_rsa -d
+openssl aes-256-cbc -K $encrypted_99b076488ab1_key -iv $encrypted_99b076488ab1_iv -in build/travis/resources/osuosl_nighlies_rsa.enc -out build/travis/resources/osuosl_nighlies_rsa -d -pass "pass:$OSUOSL_NIGHTLY_PASSPHRASE"
 # Make ssh dir
 mkdir $HOME/.ssh/
 # Copy over private key, and set permissions
@@ -25,12 +25,7 @@ touch $HOME/.ssh/known_hosts
 # Add osuosl key to known host
 ssh-keyscan ftp-osl.osuosl.org >> $HOME/.ssh/known_hosts
 
-expect << EOF
-  spawn ssh-add $HOME/.ssh/osuosl_nighlies_rsa
-  expect "Enter passphrase"
-  send "${OSUOSL_NIGHTLY_PASSPHRASE}\r"
-  expect eof
-EOF
+ssh-add $HOME/.ssh/osuosl_nighlies_rsa
 
 SSH_INDENTITY=$HOME/.ssh/osuosl_nighlies_rsa
 
