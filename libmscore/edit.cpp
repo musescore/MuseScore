ca06b2b41983a171981c73d7d5dc1db9c5d990c3
lasconic <lasconic@gmail.com>
2017-02-20 08:57:35 +0100

    fix #52841: No sound feedback in repitch mode when using MIDI or Piano Keyboard

--- libmscore/edit.cpp
+++ libmscore/edit.cpp
@@ -1126,6 +1126,7 @@ Note* Score::addPitch(NoteVal& nval, bool addFlag)
                   }
             // add new note to chord
             undoAddElement(note);
+            _playNote = true;
             // recreate tie forward if there is a note to tie to
             // one-sided ties will not be recreated
             if (firstTiedNote) {
