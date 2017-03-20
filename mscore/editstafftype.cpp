70f554a2c2eb93a5e1e367389845984faeccf99c
lasconic <lasconic@gmail.com>
2017-03-20 13:52:47 +0100

    fix #182221: replace disabled QLineEdit by QLabel in Staff Properties

--- mscore/editstafftype.cpp
+++ mscore/editstafftype.cpp
@@ -154,6 +154,7 @@ EditStaffType::EditStaffType(QWidget* parent, Staff* st)
       connect(templateReset,        SIGNAL(clicked()),                  SLOT(resetToTemplateClicked()));
       connect(addToTemplates,       SIGNAL(clicked()),                  SLOT(addToTemplatesClicked()));
 //      connect(groupCombo,           SIGNAL(currentIndexChanged(int)),   SLOT(staffGroupChanged(int)));
+      addToTemplates->setVisible(false);
 
       MuseScore::restoreGeometry(this);
       }
