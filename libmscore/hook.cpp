cf07d199d095a09def6ab646a0c4e9c2370ba97f
crispy7 <christopherboo66@gmail.com>
2017-03-24 10:55:30 +0800

    fix missing hook in 1024th note

--- libmscore/hook.cpp
+++ libmscore/hook.cpp
@@ -45,6 +45,7 @@ void Hook::setHookType(int i)
             case 5:    setSym(SymId::flag128thUp);   break;
             case 6:    setSym(SymId::flag256thUp);   break;
             case 7:    setSym(SymId::flag512thUp);   break;
+            case 8:    setSym(SymId::flag1024thUp);  break;
 
             case -1:   setSym(SymId::flag8thDown);   break;
             case -2:   setSym(SymId::flag16thDown);  break;
@@ -53,6 +54,7 @@ void Hook::setHookType(int i)
             case -5:   setSym(SymId::flag128thDown); break;
             case -6:   setSym(SymId::flag256thDown); break;
             case -7:   setSym(SymId::flag512thDown); break;
+            case -8:   setSym(SymId::flag1024thDown);break;
             default:
                   qDebug("no hook for subtype %d", i);
                   break;
