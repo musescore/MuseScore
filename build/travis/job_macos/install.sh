c2f86132010b5dd3db7735d42f173a1f6d647fa4
lasconic <lasconic@gmail.com>
2017-03-27 16:16:15 +0200

    sign macOS package on Travis-CI

--- build/travis/job_macos/install.sh
+++ build/travis/job_macos/install.sh
@@ -104,7 +104,17 @@ else
   echo "Qt ${QT_LONG_VERSION} already installed"
 fi
 
-
+#install signing certificate
+export CERTIFICATE_P12=Certificate.p12
+echo $CERTIFICATE_OSX_P12 | base64 - -D -o $CERTIFICATE_P12
+export KEYCHAIN=build.keychain
+security create-keychain -p travis $KEYCHAIN
+security default-keychain -s $KEYCHAIN
+security unlock-keychain -p travis $KEYCHAIN
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l $KEYCHAIN
+security import $CERTIFICATE_P12 -k $KEYCHAIN -P "$CERTIFICATE_OSX_PASSWORD" -T /usr/bin/codesign
 
 
 
