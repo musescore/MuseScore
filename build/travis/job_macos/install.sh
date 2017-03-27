3523f8f21dfde698ff401bf8c849ebe66b50bc6a
lasconic <lasconic@gmail.com>
2017-03-27 19:17:38 +0200

    sign macOS package on Travis-CI (second try)

--- build/travis/job_macos/install.sh
+++ build/travis/job_macos/install.sh
@@ -106,7 +106,7 @@ fi
 
 #install signing certificate
 export CERTIFICATE_P12=Certificate.p12
-echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12
+echo $CERTIFICATE_OSX_P12 | base64 - -D -o $CERTIFICATE_P12
 export KEYCHAIN=build.keychain
 security create-keychain -p travis $KEYCHAIN
 security default-keychain -s $KEYCHAIN
@@ -115,7 +115,6 @@ security unlock-keychain -p travis $KEYCHAIN
 # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
 security set-keychain-settings -t 3600 -l $KEYCHAIN
 security import $CERTIFICATE_P12 -k $KEYCHAIN -P "$CERTIFICATE_OSX_PASSWORD" -T /usr/bin/codesign
-
 
 
 
