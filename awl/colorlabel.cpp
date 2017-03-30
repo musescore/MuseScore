8d65d44118c03b0fd0654ee63dbb08ca6dd96352
roxanaluca <roxanaluca10@gmail.com>
2017-03-30 11:13:35 +0100

    fix #182336: Color Picker window gets larger every time it's used

--- awl/colorlabel.cpp
+++ awl/colorlabel.cpp
@@ -94,7 +94,7 @@ void ColorLabel::mousePressEvent(QMouseEvent*)
       {
       if (_pixmap)
             return;
-      QColor c = QColorDialog::getColor(_color, this,
+      QColor c = QColorDialog::getColor(_color, NULL,
          tr("Select Color"),
          QColorDialog::ShowAlphaChannel
          );
