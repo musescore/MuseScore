081a95c8dde4208f2228e82db9160f7ea4b9ecc8
lasconic <lasconic@gmail.com>
2017-05-05 18:16:50 +0200

    fix #196566: 'Save online' doesn't work properly for scores in 'Continuous View'

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -2533,9 +2533,16 @@ static bool doConvert(Score* cs, QString fn, QString plugin = "")
             }
       if (!plugin.isEmpty()) {
             mscore->setCurrentScore(cs);
+            LayoutMode layoutMode = cs->layoutMode();
+            cs->startCmd();
+            cs->setLayoutAll(true);
+            cs->endCmd();
+            cs->switchToPageMode();
             if (mscore->loadPlugin(plugin))
                   mscore->pluginTriggered(0);
             mscore->unloadPlugins();
+            if (layoutMode != cs->layoutMode())
+                  cs->endCmd(true);       // rollback
             }
       if (fn.endsWith(".mscx")) {
             QFileInfo fi(fn);
