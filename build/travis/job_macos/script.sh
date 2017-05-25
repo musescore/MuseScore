260f21a6408a59def24ce71f9c424667f5485552
lasconic <lasconic@gmail.com>
2017-05-25 09:34:26 +0200

    [Travis] fix syntax error in macOS build script

--- build/travis/job_macos/script.sh
+++ build/travis/job_macos/script.sh
@@ -16,6 +16,7 @@ REVISION="$(echo "$TRAVIS_COMMIT" | cut -c 1-7)"
 [ "REVISION" ] || REVISION="$(make -f Makefile.osx revision && cat mscore/revision.h)"
 
 if [ "$(grep '^[[:blank:]]*set( *MSCORE_UNSTABLE \+TRUE *)' CMakeLists.txt)" ]
+then
 cp -f build/travis/resources/splash-nightly.png  mscore/data/splash.png
 cp -f build/travis/resources/mscore-nightly.icns mscore/data/mscore.icns
 else
