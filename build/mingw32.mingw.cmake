f0362c6851ee9cf0a799036998ee85b1649ea07a
Joachim Schmitz <jojo@schmitz-digital.de>
2017-05-31 09:41:44 +0200

    Switch Windows build to Qt-5.9

--- build/mingw32.mingw.cmake
+++ build/mingw32.mingw.cmake
@@ -5,7 +5,7 @@
 set (CMAKE_SYSTEM_NAME "Windows")
 
 set (CROSS C:/Qt/Tools/mingw530_32)
-set (CROSSQT C:/Qt/5.8/mingw53_32)
+set (CROSSQT C:/Qt/5.9/mingw53_32)
 
 set (CMAKE_C_COMPILER     ${CROSS}/bin/gcc.exe)
 set (CMAKE_CXX_COMPILER   ${CROSS}/bin/g++.exe)
