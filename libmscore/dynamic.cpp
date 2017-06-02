c8fb20177138db9c1175f0b8d991921e4551c37d
Werner Schweer <ws@wschweer.de>
2017-06-02 18:25:04 +0200

    speedup writing of large scores

--- libmscore/dynamic.cpp
+++ libmscore/dynamic.cpp
@@ -199,7 +199,7 @@ void Dynamic::doAutoplace()
             return;
 
       qreal minDistance = score()->styleP(StyleIdx::dynamicsMinDistance);
-      Shape s1          = s->measure()->staffShape(staffIdx());
+      const Shape& s1   = s->measure()->staffShape(staffIdx());
       Shape s2          = shape().translated(s->pos() + pos());
 
       if (placeAbove()) {
