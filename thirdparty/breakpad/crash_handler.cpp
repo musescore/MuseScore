408b76e8e2c5f78788cfe555129ffd0bf9b82bb3
gsocnikos <nickhatz@csu.fullerton.edu>
2017-07-11 07:49:42 -0700

    clear breakpad extra files

--- thirdparty/breakpad/crash_handler.cpp
+++ thirdparty/breakpad/crash_handler.cpp
@@ -1,3 +1,24 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2017 Nikolaos Hatzopoulos (nickhatz@csu.fullerton.edu)
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+
+
 #include "crash_handler.h"
 
 
@@ -84,6 +105,14 @@ namespace Breakpad {
         //parameters["version"] = "0.1.0";
         parameters.insert(pair<wstring, wstring>(L"version", L"0.1.0"));
         // parameters["uptime"] uptime_sec;
+
+        // Pass Crash table data
+        QHashIterator<QString, QString> i(crashTable);
+        while (i.hasNext()) {
+            i.next();
+            //qDebug("%s : %s", qUtf8Printable(i.key()),qUtf8Printable(i.value()) );
+            parameters.insert(pair<wstring, wstring>(i.key().toStdWString(),i.value().toStdWString()));
+        }
 
         //files["upload_file_minidump"] = minidump_path.toStdString();
         files.insert(pair<wstring, wstring>(L"upload_file_minidump", minidump_path));
