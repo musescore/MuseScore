d046ea99bd4688a4aea6c0d58812a45889a29035
lasconic <lasconic@gmail.com>
2017-08-18 21:56:26 +0200

    fix #91056: BPM field in Play Panel is not immediately synced

--- mscore/playpanel.cpp
+++ mscore/playpanel.cpp
@@ -71,7 +71,7 @@ PlayPanel::PlayPanel(QWidget* parent)
       connect(tempoSlider,  SIGNAL(valueChanged(double,int)), SLOT(relTempoChanged(double,int)));
       connect(tempoSlider,  SIGNAL(sliderPressed(int)),       SLOT(tempoSliderPressed(int)));
       connect(tempoSlider,  SIGNAL(sliderReleased(int)),      SLOT(tempoSliderReleased(int)));
-      connect(relTempoBox,  SIGNAL(editingFinished()),        SLOT(relTempoChanged()));
+      connect(relTempoBox,  SIGNAL(valueChanged(double)),     SLOT(relTempoChanged()));
       connect(seq,          SIGNAL(heartBeat(int,int,int)),   SLOT(heartBeat(int,int,int)));
       }
 
@@ -109,7 +109,7 @@ void PlayPanel::relTempoChanged()
       {
       double v = relTempoBox->value();
       tempoSlider->setValue(v);
-      emit relTempoChanged(v * .01);
+      relTempoChanged(v, 0);
       }
 
 //---------------------------------------------------------
