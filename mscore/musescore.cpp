e20a3bb019b988a6e48b537eb23dbf7be79145ff
Tommaso Cucinotta <tommaso.cucinotta@gmail.com>
2017-08-29 22:51:33 +0200

    Make Play Panel dockable and floatable.

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -2103,9 +2103,10 @@ void MuseScore::showPlayPanel(bool visible)
             connect(synti,     SIGNAL(gainChanged(float)), playPanel, SLOT(setGain(float)));
             playPanel->setGain(synti->gain());
             playPanel->setScore(cs);
-            mscore->stackUnder(playPanel);
+            addDockWidget(Qt::RightDockWidgetArea, playPanel);
             }
       playPanel->setVisible(visible);
+      playPanel->setFloating(false);
       playId->setChecked(visible);
       }
 
