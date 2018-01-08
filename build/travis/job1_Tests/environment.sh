f4832bdea80f9c38e1c132f6b34edc07b6d9fe06
lasconic <lasconic@gmail.com>
2018-01-08 16:36:15 +0100

    [Travis] Run tests on Ubuntu trusty and with latest Cmake 3.10.1

--- build/travis/job1_Tests/environment.sh
+++ build/travis/job1_Tests/environment.sh
@@ -16,7 +16,7 @@ export CC=/usr/bin/gcc-4.9
 export CXX=/usr/bin/g++-4.9
 
 #install cmake
-CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz"
+CMAKE_URL="https://cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.tar.gz"
 mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 export PATH=${PWD}/cmake/bin:${PATH}
 
