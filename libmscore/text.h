478cee24c15bbb84aed0aab9e5ed902e4a84cc47
ws <ws@wschweer.de>
2018-01-29 10:16:41 +0100

    fix #268694

--- libmscore/text.h
+++ libmscore/text.h
@@ -341,7 +341,9 @@ class TextBase : public Element {
       QList<TextBlock>& textBlockList()          { return _layout; }
       int rows() const                           { return _layout.size(); }
 
-      void setTextInvalid()                      { textInvalid = true;  };
+      void setTextInvalid()                      { textInvalid = true;   };
+      bool isTextInvalid() const                 { return textInvalid;   }
+      bool isLayoutInvalid() const               { return layoutInvalid; }
 
       friend class TextCursor;
       };
