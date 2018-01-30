c2a762d316b39b521109b34f8bc78755a2112a0c
Giuseppe de Beer <vliegnaarbeer@hotmail.com>
2018-01-30 22:06:50 +0100

    Fix #268968: MusicXML import export lyric color

--- mscore/importmxmlpass2.cpp
+++ mscore/importmxmlpass2.cpp
@@ -5487,6 +5487,8 @@ void MusicXMLParserPass2::lyric(QMap<int, Lyrics*>& numbrdLyrics,
 
       bool hasExtend = false;
       QString strLyricNo = _e.attributes().value("number").toString();
+      QColor lyricColor = QColor::Invalid;
+      lyricColor.setNamedColor(_e.attributes().value("color").toString());
       QString strDefaultY = _e.attributes().value("default-y").toString();
       QString extendType;
       QString formattedText;
@@ -5564,6 +5566,8 @@ void MusicXMLParserPass2::lyric(QMap<int, Lyrics*>& numbrdLyrics,
 
       //qDebug("formatted lyric '%s'", qPrintable(formattedText));
       l->setXmlText(formattedText);
+      if (lyricColor != QColor::Invalid)
+            l->setColor(lyricColor);
       }
 
 
--- mtest/musicxml/io/tst_mxml_io.cpp
+++ mtest/musicxml/io/tst_mxml_io.cpp
@@ -105,6 +105,7 @@ private slots:
       void keysig2() { mxmlIoTest("testKeysig2"); }
       void lines1() { mxmlIoTest("testLines1"); }
       void lines2() { mxmlIoTest("testLines2"); }
+      void lyricColor() { mxmlIoTest("testLyricColor"); }
       void lyrics1() { mxmlIoTestRef("testLyrics1"); }
       void lyricsVoice2a() { mxmlIoTest("testLyricsVoice2a"); }
       void lyricsVoice2b() { mxmlIoTestRef("testLyricsVoice2b"); }
