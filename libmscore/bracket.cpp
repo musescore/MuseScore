48fa0ac12ac4e17de864196bbe862872b8a56b0d
anatoly-os <osokingauss@gmail.com>
2018-02-09 14:22:34 +0200

    fix #269271: crash on drag'n'drop brackets to a score
    Added correct check for Bracket properties.
    Previous implementation processed only BracketItem properties, but general Element properties were ignored.

--- libmscore/bracket.cpp
+++ libmscore/bracket.cpp
@@ -397,7 +397,8 @@ bool Bracket::edit(EditData& ed)
 
 QVariant Bracket::getProperty(P_ID id) const
       {
-      return _bi->getProperty(id);
+      auto prop = _bi->getProperty(id);
+      return prop.isValid() ? prop : Element::getProperty(id);
       }
 
 //---------------------------------------------------------
