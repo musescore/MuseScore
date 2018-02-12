9d03d0cbff3f3e03235bf04abbb32784ce914eea
mattmcclinch <35512972+mattmcclinch@users.noreply.github.com>
2018-02-12 05:34:54 -0500

    fix #269212: "Tab bar" in View menu on macOS Sierra (#3457)
    fix #269212: "Tab bar" in View menu on macOS Sierra

---
+++ mscore/macos/cocoabridge.h
@@ -0,0 +1,31 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2008-2018 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#ifndef __COCOABRIDGE_H__
+#define __COCOABRIDGE_H__
+
+class CocoaBridge
+{
+      CocoaBridge() {}
+
+   public:
+      static void setAllowsAutomaticWindowTabbing(bool flag);
+};
+
+#endif
