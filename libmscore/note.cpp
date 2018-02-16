ae2b690986b88674262e1562bd3305074ed16722
lasconic <lasconic@gmail.com>
2018-02-16 13:36:34 +0100

    fix #246176: Hide Fret conflicts in TAB staff when show unprintable is off

--- libmscore/note.cpp
+++ libmscore/note.cpp
@@ -727,7 +727,7 @@ void Note::draw(QPainter* painter) const
                   else
                         painter->fillRect(bb, Qt::white);
 
-                  if (fretConflict() && !score()->printing()) {          //on fret conflict, draw on red background
+                  if (fretConflict() && !score()->printing() && score()->showUnprintable()) {          //on fret conflict, draw on red background
                         painter->save();
                         painter->setPen(Qt::red);
                         painter->setBrush(QBrush(QColor(Qt::red)));
