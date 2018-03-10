04b6acc73baa7f750e0488b7901e0b5a66fee684
Leon Vinken <leon.vinken@gmail.com>
2018-03-10 10:59:34 +0100

    fix #255791 - [MusicXML import] use lyric number as relative instead of as absolute line number

--- mscore/importxmlfirstpass.h
+++ mscore/importxmlfirstpass.h
@@ -35,6 +35,17 @@ public:
       void calcOctaveShiftShifts();
       };
 
+class LyricNumberHandler {
+public:
+      LyricNumberHandler() {}
+      void addNumber(const QString number);
+      QString toString() const;
+      int getLyricNo(const QString& number) const;
+      void determineLyricNos();
+private:
+      std::map<QString, int> _numberToNo;
+      };
+
 class MusicXmlPart {
 public:
       MusicXmlPart(QString id = "", QString name = "");
@@ -56,6 +67,7 @@ public:
       QString getAbbr() const { return abbr; }
       void setPrintAbbr(bool b) { printAbbr = b; }
       bool getPrintAbbr() const { return printAbbr; }
+      LyricNumberHandler& lyricNumberHandler() { return _lyricNumberHandler; }
 private:
       QString id;
       QString name;
@@ -65,6 +77,7 @@ private:
       QStringList measureNumbers;             // MusicXML measure number attribute
       QList<Fraction> measureDurations;       // duration in fraction for every measure
       QVector<MusicXmlOctaveShiftList> octaveShifts; // octave shift list for every staff
+      LyricNumberHandler _lyricNumberHandler;
       };
 
 } // namespace Ms
