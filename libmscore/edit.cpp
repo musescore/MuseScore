b95897e798cfdfdf891abfc3e41bf6ac337b68ec
lasconic <lasconic@gmail.com>
2018-03-17 13:54:04 +0100

    fix #264073: Edition of fret numbers in TAB staff is not played in Note input mode

--- libmscore/edit.cpp
+++ libmscore/edit.cpp
@@ -1349,6 +1349,7 @@ void Score::putNote(const Position& p, bool replace)
                                     int tpc1 = note->tpc1default(nval.pitch);
                                     int tpc2 = note->tpc2default(nval.pitch);
                                     undoChangeFretting(note, nval.pitch, nval.string, nval.fret, tpc1, tpc2);
+                                    _playNote = true;
                                     return;
                                     }
                         }
