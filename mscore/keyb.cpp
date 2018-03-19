293bd72ab94a60d77a87e751d60fd96a4da73e36
Matt McClinch <mattmcclinch@gmail.com>
2018-03-19 10:51:05 -0400

    fix #270458: Re-Pitch a note to a rest.

--- mscore/keyb.cpp
+++ mscore/keyb.cpp
@@ -69,6 +69,8 @@ void MuseScore::updateInputState(Score* score)
       {
       InputState& is = score->inputState();
       if (is.noteEntryMode()) {
+            if (is.usingNoteEntryMethod(NoteEntryMethod::REPITCH))
+                  is.setDuration(is.cr()->durationType());
             Staff* staff = score->staff(is.track() / VOICES);
             switch (staff->staffType(is.tick())->group()) {
                   case StaffGroup::STANDARD:
