97ca6a2de5171850cd08a2cb31a262ebcc618ad7
Peter Jonas <shoogle@users.noreply.github.com>
2018-03-22 05:49:11 +0000

    fix #252201: playback holds during section break

--- mscore/exportmidi.cpp
+++ mscore/exportmidi.cpp
@@ -344,22 +344,27 @@ void ExportMidi::PauseMap::calculate(const Score* s)
             int tickOffset = rs->utick - rs->tick;
 
             auto se = tempomap->lower_bound(startTick);
-            auto ee = tempomap->lower_bound(endTick);
+            auto ee = tempomap->lower_bound(endTick+1); // +1 to include first tick of next RepeatSegment
 
             for (auto it = se; it != ee; ++it) {
                   int tick = it->first;
                   int utick = tick + tickOffset;
 
                   if (it->second.pause == 0.0) {
-                        tempomapWithPauses->insert(std::pair<const int, TEvent> (this->addPauseTicks(utick), it->second));
+                        // We have a regular tempo change. Don't include tempo change from first tick of next RepeatSegment (it will be included later).
+                        if (tick != endTick)
+                              tempomapWithPauses->insert(std::pair<const int, TEvent> (this->addPauseTicks(utick), it->second));
                         }
                   else {
-                        Fraction timeSig(sigmap->timesig(tick).timesig());
-                        qreal quarterNotesPerMeasure = (4.0 * timeSig.numerator()) / timeSig.denominator();
-                        int ticksPerMeasure =  quarterNotesPerMeasure * MScore::division; // store a full measure of ticks to keep barlines in same places
-                        tempomapWithPauses->setTempo(this->addPauseTicks(utick), quarterNotesPerMeasure / it->second.pause); // new tempo for pause
-                        this->insert(std::pair<const int, int> (utick, ticksPerMeasure + this->offsetAtUTick(utick))); // store running total of extra ticks
-                        tempomapWithPauses->setTempo(this->addPauseTicks(utick), it->second.tempo); // restore previous tempo
+                        // We have a pause event. Don't include pauses from first tick of current RepeatSegment (it was included in the previous one).
+                        if (tick != startTick) {
+                              Fraction timeSig(sigmap->timesig(tick).timesig());
+                              qreal quarterNotesPerMeasure = (4.0 * timeSig.numerator()) / timeSig.denominator();
+                              int ticksPerMeasure =  quarterNotesPerMeasure * MScore::division; // store a full measure of ticks to keep barlines in same places
+                              tempomapWithPauses->setTempo(this->addPauseTicks(utick), quarterNotesPerMeasure / it->second.pause); // new tempo for pause
+                              this->insert(std::pair<const int, int> (utick, ticksPerMeasure + this->offsetAtUTick(utick))); // store running total of extra ticks
+                              tempomapWithPauses->setTempo(this->addPauseTicks(utick), it->second.tempo); // restore previous tempo
+                              }
                         }
                   }
             }
