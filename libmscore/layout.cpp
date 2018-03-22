eea7a183d8ed78cceb2d1af712958f7925ae7362
Peter Jonas <shoogle@users.noreply.github.com>
2018-03-22 05:49:11 +0000

    fix #252201: playback holds during section break

--- libmscore/layout.cpp
+++ libmscore/layout.cpp
@@ -2362,7 +2362,7 @@ void Score::getNextMeasure(LayoutContext& lc)
       //  implement section break rest
       //
       if (measure->sectionBreak() && measure->pause() != 0.0)
-            setPause(measure->endTick()-1, measure->pause());
+            setPause(measure->endTick(), measure->pause());
 
       //
       // calculate accidentals and note lines,
