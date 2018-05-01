f3c8931dbbc9c5fc59c54924b51b11965d25c03f
Andres Fernandez de Prado <andres_fprado@hotmail.com>
2018-05-01 12:48:42 -0300

    This commit contains all the changes to the CMake build system required to generate a valid Visual Studio 2017 solution and projects for MuseScore.
    In detail, changes are as follows:
    - Changed .gitignore to ignore VS-specific files and directories.
    - VS uses a global settings file for the CMake build process: CMakeSettings.json. This is a text file, which is conceptually equivalent to the Makefile used to invoke CMake through Make. This file might need to be changed on an individual user basis, if dependencies are not installed in default paths.
    - New cmake macros to copy files in build\CopyFilesMacros.cmake. The code is from https://cmake.org/pipermail/cmake/2009-March/027892.html
    - Pre-compiled headers: Visual Studio requires to create pre-compiled headers per-project (a general per-solution PCH file, although possible, is extremely problematic and not recommended). Therefore, the  new macro vstudio_pch in CreatePrecompiledHeader.cmake was created to add the pre-compiled header creation step to an existing target (similar to what is done for XCode). The existing macro precompiled_header was modified to set the values for a group of variables. As part of this, the empty file all.cpp was added to the root of the source tree, as VS requires a source file to create pre-compiled headers (the header file alone can not be compiled).
    - all.h is not copied to binary dir for MSVC, as I could determine no need to do this. Because of this, and the differences in PCH handling, the pseudo-targets mops1 and mops2 are not created for MSVC.
    - Revised all steps conditional on toolchain, and added MSVC paths as needed. In many instances, the MSVC path is the same as the MINGW path, but not always.
    - The manual (genManual) target used the getopt() functionality defined in POSIX libraries, which is not available on Windows. An LGPL'd port of getopt() for Windows was added in manual\getopt. The original source is in GitHub: nanoporetech/getopt-win32, based on a CodeProject article: https://www.codeproject.com/KB/cpp/getopt4win.aspx?msg=3987371. The corresponding CMakeLists.txt file was modified to include this files when compiling with MSVC.
    - Changes in CMakeLists.txt files to create valid MSVC targets. The changes, always conditional on the MSVC toolchain, consist of:
         x Setting target properties for MSVC
         x Using all.h in source dir
         x Adding pre-compiled headers to target
         x Removing dependency from mops1 and mops2
    
    Notes:
    - The INSTALL target has NOT BEEN UPDATED for MSVC.

---
+++ all.cpp
@@ -0,0 +1,24 @@
+//=============================================================================
+//  MusE
+//  Linux Music Score Editor
+//  $Id: allqt.h,v 1.24 2006/03/02 17:08:30 wschweer Exp $
+//
+//  Copyright (C) 2004-2011 Werner Schweer (ws@seh.de)
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+// MSVC requires a cpp compilable file to create pre-compiled headers.
+
+// No need to include all.h, as it is force-included.
+// #include "all.h"
---
+++ manual/getopt/README.md
@@ -0,0 +1,17 @@
+getopt-win32
+============
+Full getopt Port for Unicode and Multibyte Microsoft Visual C or C++
+
+GitHub: nanoporetech/getopt-win32
+
+This repository's branch "original" is a copy of
+
+<https://www.codeproject.com/KB/cpp/getopt4win/getopt_mb_uni_vc10.zip>
+
+originally released by its author Ludvik Jerabek in 2012 under LGPLv3.
+Its sole purpose is to publish the changes made by Oxford Nanopore
+Technologies available in the current branch. In accordance with the
+original licence, Oxford Nanopore Technologies does not take any liability
+or warranty whatsoever.
+
+Building the Release and Debug DLL is done via the Microsoft Visual Studio IDE.
