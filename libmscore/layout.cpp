5e1b72fc6b151c70c5958d926ff3435abaedfe5b
Matt McClinch <mattmcclinch@gmail.com>
2018-05-08 09:43:07 -0400

    fix #272023: Downstem mirrored notes lose accidental
    manually merged from master to 2.3, see https://github.com/musescore/MuseScore/pull/3660

--- libmscore/layout.cpp
+++ libmscore/layout.cpp
@@ -926,6 +926,11 @@ void Score::layoutChords3(QList<Note*>& notes, Staff* staff, Segment* segment)
                   }
             }
 
+      // if there are no non-mirrored notes in a downstem chord,
+      // then use the stem X position as X origin for accidental layout
+      if (nNotes && leftNotes.size() == nNotes)
+            lx = notes.front()->chord()->stemPosX();
+
       if (segment) {
             // align all dots for segment/staff
             // it would be possible to dots for up & down chords separately
