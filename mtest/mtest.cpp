1717f7637e20a0ec8594262ac54ae32ee1af55d3
Joachim Schmitz <jojo@schmitz-digital.de>
2018-05-17 09:30:22 +0200

    Avoid compiler- and shell warning

--- mtest/mtest.cpp
+++ mtest/mtest.cpp
@@ -120,6 +120,8 @@ static void scanDir(QDir d)
 
 int main(int argc, char* argv[])
       {
+      Q_UNUSED(argc);
+      Q_UNUSED(argv);
 #if 0
       logFile.setFileName("mtest.log");
       if (!logFile.open(QIODevice::WriteOnly)) {
