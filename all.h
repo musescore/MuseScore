d10e70415c8e52e2ba9d45de564467e42f66c102
lasconic <lasconic@gmail.com>
2018-06-05 22:32:39 +0200

    added missing #include <QButtonGroup> required by qt >= 5.11

--- all.h
+++ all.h
@@ -131,6 +131,7 @@
 #include <QProgressBar>
 #include <QProgressDialog>
 #include <QRadioButton>
+#include <QButtonGroup>
 #include <QSplashScreen>
 #include <QFontComboBox>
 #include <QApplication>
