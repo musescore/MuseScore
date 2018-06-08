578e00bf017d97ae3e7e53c0bb5f539085f1220c
ws <ws@wschweer.de>
2018-06-08 18:31:36 +0200

    fix typo in last commit

--- mscore/inspector/inspector.cpp
+++ mscore/inspector/inspector.cpp
@@ -177,6 +177,7 @@ void Inspector::update(Score* s)
                   {
                   sameTypes = false;
                   break;
+                  }
             }
       if (oe != element() || oSameTypes != sameTypes) {
             delete ie;
