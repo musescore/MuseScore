659171fb0c7666d14e381759b13b99c537af80b2
lasconic <lasconic@gmail.com>
2018-06-15 13:48:01 +0200

    fix #273032: Provide an easy way to install soundfont, templates, etc.
    Add soundfonts automatically to default synth settings after
    installation is complete

--- synthesizer/msynthesizer.h
+++ synthesizer/msynthesizer.h
@@ -102,6 +102,8 @@ class MasterSynthesizer : public QObject {
       float gain() const     { return _gain; }
       float boost() const    { return _boost; }
       void setBoost(float v) { _boost = v; }
+
+      bool storeState();
       };
 
 }
