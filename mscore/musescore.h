6f17e43ca61c81d48c7ce1127541a96920ca4e22
anatoly-os <osokingauss@gmail.com>
2018-06-25 20:02:18 +0200

    avoid MuseScore update checking for packagers
    See https://github.com/musescore/MuseScore/pull/3742

--- mscore/musescore.h
+++ mscore/musescore.h
@@ -222,7 +222,7 @@ class MuseScore : public QMainWindow, public MuseScoreCore {
       QSettings settings;
       ScoreView* cv                        { 0 };
       ScoreState _sstate;
-      UpdateChecker* ucheck;
+      UpdateChecker* ucheck = nullptr;
 
       static const std::list<const char*> _allNoteInputMenuEntries;
       static const std::list<const char*> _basicNoteInputMenuEntries;
