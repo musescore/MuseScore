6f2d96b4e98503b8ad66b84555c8e7dc6324c20c
lasconic <lasconic@gmail.com>
2018-07-17 16:08:03 +0200

    fix #267644: Add auto updater to MuseScore development versions

--- build/appveyor/build_script.bat
+++ build/appveyor/build_script.bat
@@ -1,2 +1,2 @@
-mingw32-make -f Makefile.mingw release
-mingw32-make -f Makefile.mingw install
+mingw32-make -f Makefile.mingw release BUILD_NUMBER=%APPVEYOR_BUILD_NUMBER%
+mingw32-make -f Makefile.mingw install BUILD_NUMBER=%APPVEYOR_BUILD_NUMBER%
