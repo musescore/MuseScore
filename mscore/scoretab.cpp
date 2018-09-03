f0af36db0558c87cbe84e51384ce22a3f891ae40
Matt McClinch <mattmcclinch@gmail.com>
2018-09-03 10:09:22 -0400

    fix #275808: Crash in parts after undo-redo

--- mscore/scoretab.cpp
+++ mscore/scoretab.cpp
@@ -289,6 +289,7 @@ void ScoreTab::updateExcerpts()
             }
       else {
             tab2->setVisible(false);
+            setCurrentIndex(0);
             }
       blockSignals(true);
       setExcerpt(0);
