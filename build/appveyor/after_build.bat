871b421d7bc7b5a892e1384308ddce89d2a1d146
anatoly-os <osokingauss@gmail.com>
2018-09-09 23:16:23 +0200

    fix signing 1

--- build/appveyor/after_build.bat
+++ build/appveyor/after_build.bat
@@ -48,8 +48,7 @@ XCOPY C:\MuseScore\build\appveyor\special C:\MuseScore\%PACKAGE_NAME%\special /I
 
 :: sign dlls and exe files
 CD C:\MuseScore
-SET dSource=%PACKAGE_NAME%
-for /f "delims=" %%f in ('dir /a-d /b /s "%dSource%\*.dll" "%dSource%\*.exe"') do (
+for /f "delims=" %%f in ('dir /a-d /b /s "%PACKAGE_NAME%\*.dll" "%PACKAGE_NAME%\*.exe"') do (
     echo "Signing %%f"
     "C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /f "C:\MuseScore\build\appveyor\resources\musescore.p12" /t http://timestamp.verisign.com/scripts/timstamp.dll /p "%CERTIFICATE_PASSWORD%" "%%f"
     )
