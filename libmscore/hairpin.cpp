52b41ec93d116850ca0823db4d0693d1744b3e7d
James Thistlewood <jamesthistlewood@gmail.com>
2018-09-19 18:26:43 +0100

    fix #276385: hairpin edit mode positioning issue

--- libmscore/hairpin.cpp
+++ libmscore/hairpin.cpp
@@ -362,6 +362,7 @@ void HairpinSegment::startEdit(EditData& ed)
 void HairpinSegment::startEditDrag(EditData& ed)
       {
       TextLineBaseSegment::startEditDrag(ed);
+      setAutoplace(false);
       ElementEditData* eed = ed.getData(this);
 
       eed->pushProperty(Pid::HAIRPIN_HEIGHT);
