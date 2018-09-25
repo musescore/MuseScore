822ce09eb7ea61aad05ecc949690c468529f5b1e
lasconic <lasconic@gmail.com>
2018-09-25 11:24:37 +0200

    fix #267644: Add auto updater to MuseScore development versions

--- build/travis/job_macos/before_install.sh
+++ build/travis/job_macos/before_install.sh
@@ -28,4 +28,13 @@ expect << EOF
   send "${OSUOSL_NIGHTLY_PASSPHRASE}\r"
   expect eof
 EOF
+
+#set NIGHTLY_BUILD variable if MSCORE_UNSTABLE is TRUE in CMakeLists.txt
+if [ "$(grep '^[[:blank:]]*set( *MSCORE_UNSTABLE \+TRUE *)' CMakeLists.txt)" ]
+then
+export NIGHTLY_BUILD=TRUE
+fi
+
+export MSCORE_RELEASE_CHANNEL=$(grep '^[[:blank:]]*set *( *MSCORE_RELEASE_CHANNEL' CMakeLists.txt | awk -F \" '{print $2}')
+
 fi
