d331e5235814f0bfad40a941c3c3f736b525d02c
anatoly-os <osokingauss@gmail.com>
2018-10-25 13:21:12 +0200

    fix #109661: pageSettings are not applied to excerpts
    Add Score::clone()
    Add std::uniques_ptr usage to prevent potential memory on the cloned Score*

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -2490,7 +2490,7 @@ void MuseScore::showPageSettings()
       {
       if (pageSettings == 0)
             pageSettings = new PageSettings();
-      pageSettings->setScore(cs->masterScore());
+      pageSettings->setScore(cs);
       pageSettings->show();
       pageSettings->raise();
       }
