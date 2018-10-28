ce128f3ed8a438fb3854b9684a6f4328fc2e8b94
ws <ws@wschweer.de>
2018-10-28 11:39:48 +0100

    fix #277276 Chord symbols don't allocate space horizontally

--- libmscore/chordrest.cpp
+++ libmscore/chordrest.cpp
@@ -1147,21 +1147,32 @@ QString ChordRest::accessibleExtraInfo() const
 
 Shape ChordRest::shape() const
       {
-      if (!_lyrics.empty()) {
-            qreal margin = spatium() * .5;
-            qreal x1 = 1000000.0;
-            qreal x2 = -1000000.0;
-            for (Lyrics* l : _lyrics) {
-                  // for horizontal spacing we only need the lyrics width:
-                  x1 = qMin(x1, l->bbox().x() - margin + l->pos().x());
-                  // x2 = qMax(x2, x1 + l->bbox().width() + margin);
-                  x2 = qMax(x2, l->bbox().x() + l->bbox().width() + margin);
-                  if (l->ticks() == Lyrics::TEMP_MELISMA_TICKS)
-                        x2 += spatium();
-                  }
-            return Shape(QRectF(x1, 0.0, x2-x1, 1.0));
+      Shape shape;
+      qreal x1 = 1000000.0;
+      qreal x2 = -1000000.0;
+      bool adjustWidth = false;
+      for (Lyrics* l : _lyrics) {
+            static const qreal margin = spatium() * .5;
+            // for horizontal spacing we only need the lyrics width:
+            x1 = qMin(x1, l->bbox().x() - margin + l->pos().x());
+            x2 = qMax(x2, l->bbox().x() + l->bbox().width() + margin + l->pos().x());
+            if (l->ticks() == Lyrics::TEMP_MELISMA_TICKS)
+                  x2 += spatium();
+            adjustWidth = true;
             }
-      return Shape();
+
+      for (Element* e : segment()->annotations()) {
+            if (e->isHarmony() && e->staffIdx() == staffIdx()) {
+                  e->layout();
+                  x1 = qMin(x1, e->bbox().x() + e->pos().x());
+                  x2 = qMax(x2, x1 + e->bbox().width());
+                  adjustWidth = true;
+                  }
+            }
+      if (adjustWidth)
+            shape.add(QRectF(x1, 0.0, x2-x1, 1.0));
+
+      return shape;
       }
 
 //---------------------------------------------------------
