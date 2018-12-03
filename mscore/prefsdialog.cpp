7778b153a3682bf60fffab0e707dcd49920f434d
anatoly-os <osokingauss@gmail.com>
2018-12-03 09:48:07 +0200

    add build number to the version sent to MuseScore Connect enable Save Online remove incorrect xml-icon file update .msi and .dmg packages

--- mscore/prefsdialog.cpp
+++ mscore/prefsdialog.cpp
@@ -610,7 +610,6 @@ void PreferenceDialog::updateSCListView()
                             && !s->key().startsWith("debugger")
 #endif
                             && !s->key().startsWith("edit_harmony")
-                            && !(MuseScore::unstable() && s->key().startsWith("file-save-online"))
                             && !s->key().startsWith("insert-fretframe"))) {
                   shortcutList->addTopLevelItem(newItem);
                   }
