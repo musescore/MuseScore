e96bb9119b1b05bc1376d8f623c29410cef9a427
anatoly-os <osokingauss@gmail.com>
2018-12-08 11:37:51 +0200

    prepare MacOSX package for Qt 5.9
    Will be update with separate appcast.xml for now

--- build/travis/job_macos/install.sh
+++ build/travis/job_macos/install.sh
@@ -106,10 +106,10 @@ rvm get head
 #  echo "Qt ${QT_LONG_VERSION} already installed"
 #fi
 
-wget -nv http://utils.musescore.org.s3.amazonaws.com/qt5120_mac.zip
+wget -nv http://utils.musescore.org.s3.amazonaws.com/qt593_mac.zip
 mkdir -p $QT_MACOS
-unzip -qq qt5120_mac.zip -d $QT_MACOS
-rm qt5120_mac.zip
+unzip -qq qt593_mac.zip -d $QT_MACOS
+rm qt593_mac.zip
 
 #install sparkle
 export SPARKLE_VERSION=1.20.0
