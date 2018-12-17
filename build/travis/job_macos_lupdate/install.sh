aaf73be4392040fb50fee1287127bd2fd375c30e
Joachim Schmitz <jojo@schmitz-digital.de>
2018-12-17 12:46:55 +0100

    disable --no-obsolete
    also use same Qt version for lupdate as for Mac build

--- build/travis/job_macos_lupdate/install.sh
+++ build/travis/job_macos_lupdate/install.sh
@@ -5,7 +5,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-wget -nv http://utils.musescore.org.s3.amazonaws.com/qt593_mac.zip
+wget -nv http://utils.musescore.org.s3.amazonaws.com/qt5120_mac.zip
 mkdir -p $QT_MACOS
-unzip -qq qt593_mac.zip -d $QT_MACOS
-rm qt593_mac.zip
+unzip -qq qt5120_mac.zip -d $QT_MACOS
+rm qt5120_mac.zip
