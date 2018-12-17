835c19d7a64abaa5abd24a13eae53398721cc4c9
anatoly-os <osokingauss@gmail.com>
2018-12-17 15:43:48 +0200

    update splash screen for high DPI displays

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -7194,7 +7194,10 @@ int main(int argc, char* av[])
 
       QSplashScreen* sc = 0;
       if (!MScore::noGui && preferences.getBool(PREF_UI_APP_STARTUP_SHOWSPLASHSCREEN)) {
-            QPixmap pm(":/data/splash.png");
+            QString pictureScaling;
+            if (QGuiApplication::primaryScreen()->devicePixelRatio() >= 2)
+                  pictureScaling = "@2x";
+            QPixmap pm(":/data/splash" + pictureScaling + ".png");
             sc = new QSplashScreen(pm);
             sc->setWindowTitle(QString("MuseScore Startup"));
 #ifdef Q_OS_MAC // to have session dialog on top of splashscreen on mac
