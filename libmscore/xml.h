295ffe8ed1db7c2f799bf03e033b6c0d356f73f1
anatoly-os <osokingauss@gmail.com>
2018-12-21 15:56:00 +0200

    fix crash when creating workspace with GUI Preferences
    Make it possible to read/write qlonglong values in XmlWriter/XmlReader

--- libmscore/xml.h
+++ libmscore/xml.h
@@ -125,10 +125,12 @@ class XmlReader : public QXmlStreamReader {
       bool hasAttribute(const char* s) const;
 
       // helper routines based on readElementText():
-      int readInt()         { return readElementText().toInt();      }
-      int readInt(bool* ok) { return readElementText().toInt(ok);    }
-      int readIntHex()      { return readElementText().toInt(0, 16); }
-      double readDouble()   { return readElementText().toDouble();   }
+      int readInt()            { return readElementText().toInt();      }
+      int readInt(bool* ok)    { return readElementText().toInt(ok);    }
+      int readIntHex()         { return readElementText().toInt(0, 16); }
+      double readDouble()      { return readElementText().toDouble();   }
+      qlonglong readLongLong() { return readElementText().toLongLong(); }
+      
       double readDouble(double min, double max);
       bool readBool();
       QPointF readPoint();
