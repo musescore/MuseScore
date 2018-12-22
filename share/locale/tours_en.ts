4033acd9d98d9058de274d09c5cb2b805eb952ba
lasconic <lasconic@gmail.com>
2018-12-22 10:10:21 +0100

    more tours -> more translations

--- share/locale/tours_en.ts
+++ share/locale/tours_en.ts
@@ -36,7 +36,7 @@
     <message>
         <source>The Inspector allows you to control various details of the elements in your score.</source>
         <comment>welcome</comment>
-        <translation>The Inspector allows you to control various details of the elements in your score.</translation>
+        <translation type="vanished">The Inspector allows you to control various details of the elements in your score.</translation>
     </message>
     <message>
         <source>Each palette contains items that can be added to the score.</source>
@@ -182,7 +182,7 @@
     Boxes within the thin blue outline represent measures that are currently in view.
     You can click anywhere to jump to the corresponding location in your score.</source>
         <comment>timeline-tour</comment>
-        <translation>Each box represents a measure of a specific instrument.
+        <translation type="vanished">Each box represents a measure of a specific instrument.
     If it is highlighted, then there are notes in that measure.
     The timeline also shows rehearsal marks, changes of tempo, key, or time signature, and other points of interest.
     Boxes within the thin blue outline represent measures that are currently in view.
@@ -216,17 +216,161 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>That&apos;s the end of this tour!
-    As you use MuseScore, more tours will pop up to further explain functionality.
-    To disable these, deselect &quot;Continue showing tours&quot; before closing this window.
-    You can also enable/disable tours in Help / Tours.</source>
+        <source>MuseScore provides many ways to navigate through your score.
+    Click &quot;Next&quot; to learn more.</source>
+        <comment>navigate-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The quickest way to scroll up and down is to use your mouse wheel or equivalent touch gesture (for example, two-finger swipe).
+    To scroll horizontally, hold Shift while using the mouse wheel or touch gesture.</source>
+        <comment>navigate-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The navigation keys on your keyboard such as PgUp, PgDn, Home, and End function as in other applications
+    to move between pages.
+    Keyboards that lack these keys often provide equivalents such as Fn+Up/Down/Left/Right.</source>
+        <comment>navigate-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You can move the score short distances by clicking an empty area of the page and dragging.
+    By the way, you can also select regions by holding Shift while dragging.</source>
+        <comment>navigate-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Additional navigation tools include the Timeline and Navigator, both found in the View menu.</source>
+        <comment>navigate-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome to the Inspector, where you can change individual properties for selected elements.
+    Click &quot;Next&quot; to learn more.</source>
+        <comment>inspector-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Begin by selecting one or more elements in your score, then use the check boxes, spin boxes, dropdown menus, and other controls to change values for the various properties.
+    The set of properties available will differ according to the type of selected elements or elements.</source>
+        <comment>inspector-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you have selected elements of different types, only a few settings common to all elements will be available.
+    You can click the &quot;Notes&quot; and other buttons at the bottom of the Inspector to limit the selection to just the elements of the specified type.</source>
+        <comment>inspector-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you have modified a value, you can click the &quot;Reset&quot; button to reset it to the default.
+    If the value is controlled by a style setting, you can click the &quot;Set as style&quot; button to set the current value as the new default for the style.
+    You can also set style defaults using Format / Style.</source>
+        <comment>inspector-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The Inspector allows you to control various properties of the elements in your score.</source>
         <comment>welcome</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Welcome to the Timeline, where you can view a reduction of the score for easier navigation.
+        <source>That&apos;s the end of this tour!
+    As you use MuseScore, more tours will pop up to further explain functionality.
+    To disable these, deselect &quot;Continue showing tours&quot; before closing this window.
+    You can also enable/disable and reset tours in Help / Tours.</source>
+        <comment>welcome</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Autoplace is a new feature that automatically detects and avoids many types of collisions between elements.
+    You can also manually adjust the position of autoplaced elements, or disable autoplace for selected elements.
+    Click &quot;Next&quot; to learn more.</source>
+        <comment>autoplace-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Autoplace positions most elements according to standard music engraving practice,
+    such as by moving tempo markings above other text or by shortening hairpins to avoid dynamics.
+    In many cases, this means that no further adjustments will be required at all.</source>
+        <comment>autoplace-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If necessary, you can fine-tune the position of elements by dragging, by using the cursor keys while in Edit mode (double-click),
+    or by using the &quot;Offset&quot; fields in the Inspector.
+    This allows you to make adjustments while leaving autoplace enabled to continue avoiding collisions.</source>
+        <comment>autoplace-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>For larger adjustments, it may be more convenient to disable autoplace for selected elements using the Inspector.
+    This will remove them from collision detection and allow you to place them more freely.</source>
+        <comment>autoplace-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Welcome to the Timeline, where you can view a high-level reduction of the score for easier navigation.
     Click &quot;Next&quot; to learn more.</source>
         <comment>timeline-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The timeline shows rehearsal marks, changes of tempo, key, and time signature, and other points of interest.
+    You can click anywhere to jump to the corresponding location in your score.</source>
+        <comment>timeline-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The section below contains a row for each instrument.
+    Each box within the row represents a measure for that instrument.
+    If the box is highlighted, then there are notes in that measure.
+    Boxes with a thin blue outline represent measures that are currently in view.</source>
+        <comment>timeline-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MuseScore supports different types of selections: single, list, and range.
+    Click &quot;Next&quot; to learn more.</source>
+        <comment>select-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>To make a single selection, click the element you want to select.
+    It becomes highlighted to indicate that it is selected.
+    You can use the left and right cursor keys to move the selection through the notes and rests of your score.
+    Tip: if you press the Alt key (Option on macOS) along with the cursor keys you can move through other elements too,
+    and also move up and down from staff to staff.</source>
+        <comment>select-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A list selection is a combination of single selections.
+    Press Ctrl (macOS: Cmd) while clicking to add or remove elements to the list selection.
+    You can also quickly select similar elements by right-clicking (macOS: Ctrl+clicking) one
+    and then choosing one of the &quot;Select&quot; options from the resulting popup menu.</source>
+        <comment>select-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A range selection includes everything from one point in time to another across one or more staves.
+    It is indicated by a blue rectangle encompassing the entire range.
+    To make a range selection, click the first note, rest, or measure you want to select, then Shift+click the last.
+    You can also extend range selections by holding the Shift key while moving the cursor.</source>
+        <comment>select-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Another way to make selections is to hold Shift while dragging.
+    If your selection includes notes, it will automatically be made as a range selection;
+    otherwise it will be made as a list selection.</source>
+        <comment>select-tour</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Finally, the Selection Filter (found in the View menu) allows you to exclude elements of a given type from a range selection.</source>
+        <comment>select-tour</comment>
         <translation type="unfinished"></translation>
     </message>
 </context>
