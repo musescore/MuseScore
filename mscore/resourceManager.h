17a67addd70325d684ef086141adca34e6b16aad
anatoly-os <osokingauss@gmail.com>
2019-01-10 14:32:30 +0200

    update the url to get updated languages

--- mscore/resourceManager.h
+++ mscore/resourceManager.h
@@ -34,7 +34,7 @@ public:
     void selectLanguagesTab();
     void selectExtensionsTab();
 
-    static inline QString baseAddr() { return "http://extensions.musescore.org/3.0/"; }
+    static inline QString baseAddr() { return "http://extensions.musescore.org/3.0.1/"; }
 
 private:
     QMap <QPushButton *, QString> languageButtonMap; 	// QPushButton -> filename
