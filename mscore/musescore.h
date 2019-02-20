032fd1bba451d520b32a7d18eda25c6cf066fa69
anatoly-os <osokingauss@gmail.com>
2019-02-20 11:02:58 +0200

    fix mxl export for musescore.com services
    Raw .musicxml files with .mxl extension were generated instead of real compressed .mxl ones.

--- mscore/musescore.h
+++ mscore/musescore.h
@@ -882,6 +882,7 @@ extern void loadTranslation(QString fileName, QString localeName);
 extern void setMscoreLocale(QString localeName);
 extern bool saveMxl(Score*, const QString& name);
 extern bool saveMxl(Score*, QIODevice*);
+extern bool saveXml(Score*, QIODevice*);
 extern bool saveXml(Score*, const QString& name);
 
 struct PluginDescription;
