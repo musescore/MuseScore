3aeeee48fa888c0f49a2fd22b002ef7f41f6bcec
anatoly-os <osokingauss@gmail.com>
2019-02-26 13:02:03 +0200

    Update MacOS builds to use Qt 5.12.1

--- build/travis/job_macos_lupdate/install.sh
+++ build/travis/job_macos_lupdate/install.sh
@@ -5,7 +5,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-wget -nv http://utils.musescore.org.s3.amazonaws.com/qt5120_mac.zip
+wget -nv http://utils.musescore.org.s3.amazonaws.com/qt5121_mac.zip
 mkdir -p $QT_MACOS
-unzip -qq qt5120_mac.zip -d $QT_MACOS
-rm qt5120_mac.zip
+unzip -qq qt5121_mac.zip -d $QT_MACOS
+rm qt5121_mac.zip
