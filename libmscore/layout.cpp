2d73b5adb1d9e5cc77ec32bc70128b3c046a168c
anatoly-os <osokingauss@gmail.com>
2019-03-04 09:50:05 +0200

    Merge pull request #4743 from songchaow/fix_cross_measure
    fix #285100: Crash when moving beam if first note of beam is in the first measure and other notes of beam are in the second measure

--- libmscore/layout.cpp
+++ libmscore/layout.cpp
@@ -2241,7 +2241,8 @@ void Score::createBeams(Measure* measure)
                                     if (!beamNoContinue(prevCR->beamMode())
                                         && !pm->lineBreak() && !pm->pageBreak() && !pm->sectionBreak()) {
                                           beam = prevCR->beam();
-                                          a1 = beam ? beam->elements().front() : prevCR;
+                                          //a1 = beam ? beam->elements().front() : prevCR;
+                                          a1 = beam ? nullptr : prevCR; // when beam is found, a1 is no longer required.
                                           }
                                     }
                               }
@@ -2361,8 +2362,17 @@ void Score::createBeams(Measure* measure)
                   }
             if (beam)
                   beam->layout1();
-            else if (a1)
+            else if (a1) {
+                  // if a1 is the last chord/rest in current measure
+                  if (a1->segment()->next(SegmentType::ChordRest) == nullptr) {
+                        const auto b = a1->beam();
+                        // if the second chord/rest in a1's beam (it must be in next measure) has forced MID beam mode
+                        if (b && b->elements().startsWith(a1) && b->elements().size()>=2 && b->elements()[1]->beamMode() == Beam::Mode::MID)
+                              // do not delete the origin beam
+                              continue;
+                        }
                   a1->removeDeleteBeam(false);
+                  }
             }
       }
 
