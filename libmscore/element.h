b5bf744ec0664b7b0d8b4321e3ec1041ca3b2829
anatoly-os <osokingauss@gmail.com>
2019-03-07 13:37:23 +0200

    remove redundant TODOs

--- libmscore/element.h
+++ libmscore/element.h
@@ -205,9 +205,7 @@ class Element : public ScoreElement {
       bool generated() const                  { return flag(ElementFlag::GENERATED);  }
       void setGenerated(bool val)             { setFlag(ElementFlag::GENERATED, val);   }
 
-      //TODO: rename to pos()
       const QPointF& ipos() const             { return _pos;                    }
-      //TODO: rename to posWithUserOffset()
       virtual const QPointF pos() const       { return _pos + _offset;          }
       virtual qreal x() const                 { return _pos.x() + _offset.x(); }
       virtual qreal y() const                 { return _pos.y() + _offset.y(); }
