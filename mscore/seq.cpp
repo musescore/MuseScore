e72fee1c329489537d96afb8be73e1083353941d
anatoly-os <osokingauss@gmail.com>
2019-05-01 13:58:59 +0200

    Revert "Merge pull request #4933 from dmitrio95/play-selected-staves"
    This reverts commit 46e1f6a9b345b62c6ee2fe349e997ba119781cfb.

--- mscore/seq.cpp
+++ mscore/seq.cpp
@@ -518,9 +518,12 @@ void Seq::playEvent(const NPlayEvent& event, unsigned framePos)
             bool mute = false;
 
             const Note* note = event.note();
-            if (note)
-                  mute = note->mutePlayback();
-
+            if (note) {
+                  Staff* staff      = note->staff();
+                  Instrument* instr = staff->part()->instrument(note->chord()->tick());
+                  const Channel* a = instr->playbackChannel(note->subchannel(), cs);
+                  mute = a->mute() || a->soloMute() || !staff->playbackVoice(note->voice());
+                  }
             if (!mute) {
                   if (event.discard()) { // ignore noteoff but restrike noteon
                         if (event.velo() > 0)
