2a1d9e89989dc553a7fc8b36444a8a2d11b31723
Josh Wood <joshwd36@gmail.com>
2019-06-02 16:54:28 +0200

    Fix adding objects to multiple staves through selection

--- mscore/palette.cpp
+++ mscore/palette.cpp
@@ -445,7 +445,7 @@ static void applyDrop(Score* score, ScoreView* viewer, Element* target, Element*
             dropData.dropElement->styleChanged();   // update to local style
 
             Element* el = target->drop(dropData);
-            if (el->isInstrumentChange()) {
+            if (el && el->isInstrumentChange()) {
                   mscore->currentScoreView()->selectInstrument(toInstrumentChange(el));
                   }
             if (el && !viewer->noteEntryMode())
@@ -697,34 +697,24 @@ void Palette::applyPaletteElement(PaletteCell* cell, Qt::KeyboardModifiers modif
                   Segment* startSegment = sel.startSegment();
                   Segment* endSegment = sel.endSegment(); //keep it, it could change during the loop
 
-                  if (element->placeMultiple()) {
-                        for (Segment* s = startSegment; s && s != endSegment; s = s->next1()) {
-                              for (int track = track1; track < track2; ++track) {
-                                    Element* e = s->element(track);
-                                    if (e == 0 || !score->selectionFilter().canSelect(e) || !score->selectionFilter().canSelectVoice(track))
-                                          continue;
-                                    if (e->isChord()) {
-                                          Chord* chord = toChord(e);
-                                          for (Note* n : chord->notes())
-                                                applyDrop(score, viewer, n, element, modifiers);
-                                          }
-                                    else {
-                                          // do not apply articulation to barline in a range selection
-                                          if (!e->isBarLine() || !element->isArticulation())
-                                                applyDrop(score, viewer, e, element, modifiers);
-                                          }
+                  for (Segment* s = startSegment; s && s != endSegment; s = s->next1()) {
+                        for (int track = track1; track < track2; ++track) {
+                              Element* e = s->element(track);
+                              if (e == 0 || !score->selectionFilter().canSelect(e) || !score->selectionFilter().canSelectVoice(track))
+                                    continue;
+                              if (e->isChord()) {
+                                    Chord* chord = toChord(e);
+                                    for (Note* n : chord->notes())
+                                          applyDrop(score, viewer, n, element, modifiers);
+                                    }
+                              else {
+                                    // do not apply articulation to barline in a range selection
+                                    if (!e->isBarLine() || !element->isArticulation())
+                                          applyDrop(score, viewer, e, element, modifiers);
                                     }
                               }
-                        }
-                  else {
-                        Element* e = startSegment->element(track1);
-                        if (e->isChord()) {
-                              Chord* chord = toChord(e);
-                              applyDrop(score, viewer, chord->notes().at(0), element, modifiers);
-                              }
-                        else {
-                              applyDrop(score, viewer, e, element, modifiers);
-                              }
+                        if (!element->placeMultiple())
+                              break;
                         }
                   }
             }
