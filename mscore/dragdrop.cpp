257f4bf224c13654562dea3bb58cf3a6e1cb9670
joshwd36 <joshwd36@gmail.com>
2019-06-16 20:41:54 +0100

    Remove braces in a few sections

--- mscore/dragdrop.cpp
+++ mscore/dragdrop.cpp
@@ -568,9 +568,8 @@ void ScoreView::dropEvent(QDropEvent* event)
                               }
 
                         Element* dropElement = el->drop(editData);
-                        if (dropElement && dropElement->isInstrumentChange()) {
+                        if (dropElement && dropElement->isInstrumentChange())
                               mscore->currentScoreView()->selectInstrument(toInstrumentChange(dropElement));
-                              }
                         _score->addRefresh(el->canvasBoundingRect());
                         if (dropElement) {
                               if (!_score->noteEntryMode())
