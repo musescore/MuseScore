438d9eeda35546a749fb199eb154a0a759d6789a
Peter Jonas <shoogle@users.noreply.github.com>
2019-06-19 23:42:26 +0100

    Update generateTs.py to use Python 3 syntax
    Preserve compatibility with Python 2 (for now).
    
    - Replace Python 2 print statement with Python 3's print() function.
    - Remove UTF-8 encoding statements as everything is UTF-8 by default in Python 3.
    - Add compatibility code to make sure the above changes work in Python 2.

--- share/tours/generateTs.py
+++ share/tours/generateTs.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+# If you get Unicode errors on Windows, try setting the environment variable
+# PYTHONIOENCODING=utf-8. More info at https://stackoverflow.com/a/12834315
+
+# BEGIN compatibility code for Python 2
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+
+if sys.version_info[0] < 3:
+    import io
+    open = io.open # open text files in UTF-8 encoding https://stackoverflow.com/a/35086151
+# END compatibility code for Python 2
+
 import os
 import xml.etree.ElementTree as ET
 
@@ -8,21 +21,21 @@ def addMessage(f, text, comment=''):
     text = text.replace('"', r'\"')
     text = text.replace('\n', r'\n')
     if (comment):
-        f.write('QT_TRANSLATE_NOOP3("TourXML", "' + text.encode('utf8') + '", "' + comment.encode('utf8') + '"),\n')
+        f.write('QT_TRANSLATE_NOOP3("TourXML", "' + text + '", "' + comment + '"),\n')
     else:
-        f.write('QT_TRANSLATE_NOOP("TourXML", "' + text.encode('utf8') + '"),\n')
+        f.write('QT_TRANSLATE_NOOP("TourXML", "' + text + '"),\n')
 
 
 scriptPath = os.path.dirname(os.path.realpath(__file__))
 
 #find all tours
 tours = []
-for file in os.listdir(scriptPath):
+for file in sorted(os.listdir(scriptPath)): # sort to get same ordering on all platforms
     if file.endswith(".tour"):
         tours.append(os.path.join(scriptPath, file))
 
-# create tourxml.h
-f = open(scriptPath + '/' + 'tourxml.h', 'w')
+# create tourxml.h (must specify encoding and line ending on Windows)
+f = open(scriptPath + '/' + 'tourxml.h', 'w', newline='\n', encoding='utf-8')
 
 for tour in tours:
     tree = ET.parse(tour)
