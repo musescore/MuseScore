968ca5e45855aeef55d747887734cfbf444de399
anatoly-os <osokingauss@gmail.com>
2019-06-27 18:20:26 +0200

    Fix crash on switching menus and workspaces with Qt 5.9.*

--- mscore/workspace.cpp
+++ mscore/workspace.cpp
@@ -694,20 +694,13 @@ void Workspace::read(XmlReader& e)
                   saveMenuBar = true;
                   QMenuBar* mb = mscore->menuBar();
                   mb->clear();
+                  menuToStringList.clear();
                   while (e.readNextStartElement()) {
                         if (e.hasAttribute("name")) { // is a menu
                               QString menu_id = e.attribute("name");
-                              QMenu* menu = findMenuFromString(menu_id);
-                              if (menu) {
-                                    menu->clear();
-                                    mb->addMenu(menu);
-                                    readMenu(e, menu);
-                                    }
-                              else {
-                                    menu = new QMenu(menu_id);
-                                    mb->addMenu(menu);
-                                    readMenu(e, menu);
-                                    }
+                              QMenu* menu = mb->addMenu(menu_id);
+                              addMenuAndString(menu, menu_id);
+                              readMenu(e, menu);
                               }
                         else { // is an action
                               QString action_id = e.readXml();
@@ -719,6 +712,7 @@ void Workspace::read(XmlReader& e)
                                     }
                               }
                         }
+                  mscore->updateMenus();
                   }
             else if (tag == "State") {
                   saveComponents = true;
@@ -762,17 +756,9 @@ void Workspace::readMenu(XmlReader& e, QMenu* menu)
       while (e.readNextStartElement()) {
             if (e.hasAttribute("name")) { // is a menu
                   QString menu_id = e.attribute("name");
-                  QMenu* new_menu = findMenuFromString(menu_id);
-                  if (new_menu) {
-                        new_menu->clear();
-                        menu->addMenu(new_menu);
-                        readMenu(e, new_menu);
-                        }
-                  else {
-                        new_menu = new QMenu(menu_id);
-                        menu->addMenu(new_menu);
-                        readMenu(e, new_menu);
-                        }
+                  QMenu* new_menu = menu->addMenu(menu_id);
+                  addMenuAndString(new_menu, menu_id);
+                  readMenu(e, new_menu);
                   }
             else { // is an action
                   QString action_id = e.readXml();
@@ -806,20 +792,13 @@ void Workspace::readGlobalMenuBar()
                         if (e.name() == "MenuBar") {
                               QMenuBar* mb = mscore->menuBar();
                               mb->clear();
+                              menuToStringList.clear();
                               while (e.readNextStartElement()) {
                                     if (e.hasAttribute("name")) { // is a menu
                                           QString menu_id = e.attribute("name");
-                                          QMenu* menu = findMenuFromString(menu_id);
-                                          if (menu) {
-                                                menu->clear();
-                                                mb->addMenu(menu);
-                                                readMenu(e, menu);
-                                                }
-                                          else {
-                                                menu = new QMenu(menu_id);
-                                                mb->addMenu(menu);
-                                                readMenu(e, menu);
-                                                }
+                                          QMenu* menu = mb->addMenu(menu_id);
+                                          addMenuAndString(menu, menu_id);
+                                          readMenu(e, menu);
                                           }
                                     else { // is an action
                                           QString action_id = e.readXml();
@@ -831,6 +810,7 @@ void Workspace::readGlobalMenuBar()
                                                 }
                                           }
                                     }
+                              mscore->updateMenus();
                               }
                         else
                               e.unknown();
