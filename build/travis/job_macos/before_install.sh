02f3a5fff6b3e3b1d4ffa0fe6e40bdfff80cccdb
anatoly-os <osokingauss@gmail.com>
2019-07-04 10:40:24 +0200

    switch macosx build to OSX_TARGET 10.10 and Qt 5.9.8
    There will be one package supporting macOS 10.10+ based on Qt 5.9.8.
    Remove obsolete variables from .sh scripts
    Switch lupdate job to Qt 5.12.4

--- build/travis/job_macos/before_install.sh
+++ build/travis/job_macos/before_install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export QT_SHORT_VERSION=5.12
-export QT_LONG_VERSION=5.12.1
-export QT_INSTALLER_ROOT=qt-opensource-mac-x64-clang-${QT_LONG_VERSION}
-export QT_INSTALLER_FILENAME=${QT_INSTALLER_ROOT}.dmg
-
+export QT_SHORT_VERSION=5.9
 export QT_PATH=$HOME/qt
 export QT_MACOS=$QT_PATH/$QT_SHORT_VERSION/clang_64
 export PATH=$PATH:$QT_MACOS/bin
