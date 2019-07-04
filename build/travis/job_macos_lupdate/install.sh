25ecb9a2108d92727411a8b0a50feffcfd0c8b69
anatoly-os <osokingauss@gmail.com>
2019-07-04 11:09:38 +0200

    fix path to qt5124.zip in the lupdate job

--- build/travis/job_macos_lupdate/install.sh
+++ build/travis/job_macos_lupdate/install.sh
@@ -5,7 +5,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-wget -nv http://utils.musescore.org.s3.amazonaws.com/qt5124_mac.zip
+wget -nv https://s3.amazonaws.com/utils.musescore.org/qt5124_mac.zip
 mkdir -p $QT_MACOS
 unzip -qq qt5124_mac.zip -d $QT_MACOS
 rm qt5124_mac.zip
