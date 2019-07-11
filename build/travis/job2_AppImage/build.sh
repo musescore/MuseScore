42453f801b5601844335d400f4d38d2eb63f8d69
Peter Jonas <shoogle@users.noreply.github.com>
2019-07-11 05:28:20 +0100

    Enable contributors to upload artifacts to personal Bintray

--- build/travis/job2_AppImage/build.sh
+++ build/travis/job2_AppImage/build.sh
@@ -4,11 +4,13 @@
 # Build portable Linux AppImages and upload them to Bintray. AppImages will
 # always be uploaded unless a list of specific branches is passed in. e.g.:
 #    $   build.sh  --upload-branches  master  my-branch-1  my-branch-2
-# Builds will be for the native architecture (64 bit) unless another is
-# specified for cross-compiling. (e.g. build.sh --i686 or build.sh --armhf)
 
 set -e # exit on error
 set -x # echo commands
+
+env_error_msg="Required environment variable is empty or unset."
+
+TARGET_ARCH="${TARGET_ARCH:?${env_error_msg}}" # env
 
 date="$(date -u +%Y%m%d%H%M)"
 
@@ -38,6 +40,9 @@ function rebuild-docker-image() { # $1 is arch (e.g. x86_64)
   set -e # disallow errors
 }
 
+# Set AppImage name and automated update information according to
+# https://github.com/AppImage/AppImageSpec/blob/master/draft.md
+# Also set a label to distinguish when running multiple builds on one machine.
 if [ "$(grep '^[[:blank:]]*set( *MSCORE_UNSTABLE \+TRUE *)' CMakeLists.txt)" ]
 then # Build is marked UNSTABLE inside CMakeLists.txt
   if [ "${BINTRAY_REPO_OWNER}" == "musescore" ]
@@ -47,22 +52,30 @@ then # Build is marked UNSTABLE inside CMakeLists.txt
                         BUILD_NUMBER='${TRAVIS_BUILD_NUMBER}' \
                         LABEL='Portable Nightly Build'"
     cp -f build/travis/resources/splash-nightly.png mscore/data/splash.png
+    update_info="" # automatic updates disabled for nightlies as it requires
+    # changes on the server. See https://github.com/musescore/MuseScore/pull/4757.
   else
     # This is someone developing on their own fork
-    makefile_overrides="PREFIX='MuseScoreDev-$date-$branch-$revision' \
+    export BINTRAY_VERSION="${date}-${branch}-${revision}"
+    makefile_overrides="PREFIX='MuseScoreDev-${BINTRAY_VERSION}' \
                         SUFFIX='-portable-dev' \
                         LABEL='Unofficial Developer Build'"
+    export BINTRAY_USER="${BINTRAY_USER:?${env_error_msg}}" # env, otherwise appimagetool will fail
+    export BINTRAY_REPO="${BINTRAY_REPO:-MuseScore}"
+    export BINTRAY_REPO_OWNER="${BINTRAY_REPO_OWNER:-${BINTRAY_USER}}" # env, or use $BINTRAY_USER
+    export BINTRAY_PACKAGE="${BINTRAY_PACKAGE:-MuseScoreDev-${branch}}"
+    update_info="bintray-zsync|${BINTRAY_REPO_OWNER}|${BINTRAY_REPO}|${BINTRAY_PACKAGE}|MuseScoreDev-_latestVersion-${TARGET_ARCH}.AppImage.zsync"
   fi
 else
   # Build is STABLE so create a stable release!
   makefile_overrides="" # use Makefile defaults
+  update_info="gh-releases-zsync|musescore|MuseScore|latest|MuseScore-*x86_64.AppImage.zsync"
 fi
 
 # Build AppImage depending on arch specified in $1 if cross-compiling, else default build x86_64
-case "$1" in
+case "${TARGET_ARCH}" in
 
-  --armhf )
-    shift
+  armhf )
     # build MuseScore inside debian x86-64 multiarch image containing arm cross toolchain and libraries
     docker run -itv "${PWD}:/MuseScore" \
       ericfont/musescore:jessie-crosscompile-armhf \
@@ -76,21 +89,25 @@ case "$1" in
       "/MuseScore/build/Linux+BSD/portable/RecipeDebian --package-only armhf"
     ;;
 
-  --i686 )
-    shift
+  i686 )
     # Build MuseScore AppImage inside 32-bit x86 Docker image
     (set +x; DOCKER_TRIGGER="$DOCKER_TRIGGER_X86_32" rebuild-docker-image x86_32)
     docker run -itv "${PWD}:/MuseScore" "$DOCKER_USER/musescore-x86_32:$docker_tag" /bin/bash -c \
       "/MuseScore/build/Linux+BSD/portable/Recipe $makefile_overrides"
     ;;
 
-  * )
-    [ "$1" == "--x86_64" ] && shift || true
+  x86_64 )
     # Build MuseScore AppImage inside native (64-bit x86) Docker image
     (set +x; DOCKER_TRIGGER="$DOCKER_TRIGGER_X86_64" rebuild-docker-image x86_64)
     docker run -itv "${PWD}:/MuseScore" "$DOCKER_USER/musescore-x86_64:$docker_tag" /bin/bash -c \
-      "/MuseScore/build/Linux+BSD/portable/Recipe $makefile_overrides"
+      "UPDATE_INFORMATION='${update_info}' /MuseScore/build/Linux+BSD/portable/Recipe $makefile_overrides"
     ;;
+
+  * )
+    echo "$0: Unrecognised TARGET_ARCH '${TARGET_ARCH}'" >&2
+    exit 1
+    ;;
+
 esac
 
 # Should the AppImage be uploaded?
@@ -106,12 +123,21 @@ else
 fi
 
 if [ "${upload}" ]; then
-  # Upload AppImage to Bintray
-  #./build/travis/job2_AppImage/bintray.sh build.release/MuseScore*.AppImage
-  ./build/travis/job2_AppImage/osuosl.sh build.release/MuseScore*.AppImage
-  if [[ -f build.release/MuseScore*.AppImage.zsync ]]; then
-    # upload zsync delta for automatic updates
-    ./build/travis/job2_AppImage/osuosl.sh build.release/MuseScore*.AppImage.zsync
+  if [[ "${TRAVIS_PULL_REQUEST}" ]] && [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    # no env secrets available in pull requests
+    echo "$0: Not uploading since this a pull request."
+  else
+    if [[ "${TRAVIS_REPO_SLUG}" == "musescore/MuseScore" ]]; then
+      # this is an official build (stable or nightly)
+      ./build/travis/job2_AppImage/osuosl.sh build.release/MuseScore*.AppImage
+      if [[ -f build.release/MuseScore*.AppImage.zsync ]]; then
+        # upload zsync delta for automatic updates
+        ./build/travis/job2_AppImage/osuosl.sh build.release/MuseScore*.AppImage.zsync
+      fi
+    else
+      # This is a developer building on their personal fork
+      ./build/travis/job2_AppImage/bintray.sh build.release/MuseScore*.AppImage* # both AppImage and zsync
+    fi
   fi
 else
   echo "On branch '$branch' so AppImage will not be uploaded." >&2
