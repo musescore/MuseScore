8916c7c3323069e6d98b47f369db7ca517a14e18
James Thistlewood <jamesthistlewood@gmail.com>
2019-08-10 19:33:05 +0100

    Refactor SND
    - move most of the logic to velo.cpp and VeloList
    - allow dynamic playback of tremolos
    - get velocity directly in note collection code
    - allow use of poly aftertouch in the future

--- libmscore/velo.h
+++ libmscore/velo.h
@@ -13,6 +13,9 @@
 #ifndef __VELO_H__
 #define __VELO_H__
 
+#include "fraction.h"
+#include "hairpin.h"
+
 /**
  \file
  Definition of classes VeloList.
@@ -26,12 +29,33 @@ namespace Ms {
 //---------------------------------------------------------
 
 enum class VeloType : char { FIX, RAMP };
+enum class VeloDirection : signed char {
+      CRESCENDO,
+      DIMINUENDO,
+      };
 
-struct VeloEvent {
+class VeloEvent {
+      // Despite storing the tick as the key of the VeloEvent in the VeloList, we also store it here.
+      // Since we're not going to be changing it (well, not much, anyway), this is good because it provides
+      // multiple ways to access the tick. Keeping it in sync isn't much trouble.
+      Fraction tick;
+      int value;
       VeloType type;
-      char val;
-      VeloEvent() {}
-      VeloEvent(VeloType t, char v) : type(t), val(v) {}
+      Fraction etick;
+      VeloChangeMethod method;
+      int cachedStart   { -1 };
+      int cachedEnd     { -1 };
+      VeloDirection direction;
+
+   public:
+      VeloEvent(Fraction t, int vel) : tick(t), value(vel), type(VeloType::FIX) {}
+      VeloEvent(Fraction s, Fraction e, int diff, VeloChangeMethod m, VeloDirection d)
+            : tick(s), value(diff), type(VeloType::RAMP), etick(e), method(m), direction(d) {}
+
+      bool operator==(const VeloEvent& event) const;
+      bool operator!=(const VeloEvent& event) const;
+
+      friend class VeloList;
       };
 
 //---------------------------------------------------------
@@ -39,15 +63,23 @@ struct VeloEvent {
 ///  List of note velocity changes
 //---------------------------------------------------------
 
-class VeloList : public QMap<int, VeloEvent> {
+class VeloList : public QMultiMap<Fraction, VeloEvent> {
+      bool cleanedUp    { false };
+      static const int DEFAULT_VELOCITY  { 80 };
+
    public:
       VeloList() {}
-      int velo(int tick) const;
-      int nextVelo(int tick) const;
-      void setVelo(int tick, VeloEvent velo);
-      void setVelo(int tick, int velocity);
-      };
+      int velo(Fraction tick);
+      std::vector<std::pair<Fraction, Fraction>> changesInRange(Fraction stick, Fraction etick);
 
+      void addFixed(Fraction tick, int velocity);
+      void addRamp(Fraction stick, Fraction etick, int velChange, VeloChangeMethod method, VeloDirection direction);
+      void cleanup();
+
+      void dump();
+
+      static int interpolateVelocity(VeloEvent& event, Fraction& tick);
+      };
 
 }     // namespace Ms
 #endif
