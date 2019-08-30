1a3d96f928d6e40ece49a64eaa36baad353f7711
anatoly-os <osokingauss@gmail.com>
2019-08-30 20:42:47 +0200

    prepare 3.3 Beta release

--- build/travis/job_macos_lupdate/script.sh
+++ build/travis/job_macos_lupdate/script.sh
@@ -23,15 +23,15 @@ rm mscore.pro
 rm instruments.pro
 rm tours.pro
 
-sudo pip install transifex-client
+#sudo pip install transifex-client
 
-cat > ~/.transifexrc <<EOL
-[https://www.transifex.com]
-hostname = https://www.transifex.com
-password = $TRANSIFEX_PASSWORD
-token =
-username = $TRANSIFEX_USER
-EOL
+#cat > ~/.transifexrc <<EOL
+#[https://www.transifex.com]
+#hostname = https://www.transifex.com
+#password = $TRANSIFEX_PASSWORD
+#token =
+#username = $TRANSIFEX_USER
+#EOL
 
-cp share/locale/mscore_en_US.ts share/locale/mscore_en.ts
-tx push -s
+#cp share/locale/mscore_en_US.ts share/locale/mscore_en.ts
+#tx push -s
