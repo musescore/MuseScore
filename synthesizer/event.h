48b6aea0f884dc62c78f819b2a6c206e95774f72
Peter Hieu Vu <peter.vu8@gmail.com>
2019-09-02 23:40:41 -0700

    fix synth mute for harmony channels

--- synthesizer/event.h
+++ synthesizer/event.h
@@ -19,6 +19,7 @@ namespace Ms {
 
 class Note;
 class XmlWriter;
+class Score;
 
 enum class BeatType : char;
 
@@ -254,7 +255,7 @@ class NPlayEvent : public PlayEvent {
       void setOriginatingStaff(int i) { _origin = i; }
       void setDiscard(int d) { _discard = d; }
       int discard() const { return _discard; }
-      bool isMuted() const;
+      bool isMuted(Score* score = 0) const;
       };
 
 //---------------------------------------------------------
