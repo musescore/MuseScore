5115b6f27d572d40abd199cdad9265f6e1f78bba
pereverzev_v <v.pereverzev@wsmgroup.ru>
2019-12-06 12:08:41 +0200

    Added a QObject based model for management of user permission choice

---
+++ telemetry/models/telemetrypermissionmodel.cpp
@@ -0,0 +1,43 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2019 MuseScore BVBA and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+#include "telemetrypermissionmodel.h"
+#include "settings/types/preferencekeys.h"
+#include <QDesktopServices>
+#include <QUrl>
+
+TelemetryPermissionModel::TelemetryPermissionModel(QObject *parent) : QObject(parent)
+      {
+      m_settings.setValue(PREF_APP_STARTUP_TELEMETRY_ACCESS_REQUESTED, true);
+      }
+
+void TelemetryPermissionModel::accept()
+      {
+      m_settings.setValue(PREF_APP_TELEMETRY_ALLOWED, true);
+      }
+
+void TelemetryPermissionModel::reject()
+      {
+      m_settings.setValue(PREF_APP_TELEMETRY_ALLOWED, false);
+      }
+
+void TelemetryPermissionModel::openLink(const QString &link)
+      {
+      QDesktopServices::openUrl(QUrl(link));
+      }
