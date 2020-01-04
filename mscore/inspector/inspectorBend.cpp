24e95f83c091b0c81e28710f315c5e9c4412b457
Howard-C <howardc@pku.edu.cn>
2020-01-04 17:14:10 +0800

    use enum class for preset bends/tremolo bars

--- mscore/inspector/inspectorBend.cpp
+++ mscore/inspector/inspectorBend.cpp
@@ -19,18 +19,22 @@
 namespace Ms {
      
 //---------------------------------------------------------
-//   Preset bends
+//   BendType
 //---------------------------------------------------------
 
-static const QList<PitchValue> BEND
+enum class BendType : char {
+      BEND, BEND_RELEASE, BEND_RELEASE_BEND, PREBEND, PREBEND_RELEASE, CUSTOM
+      };
+
+static const QList<PitchValue> bend
    = { PitchValue(0, 0),   PitchValue(15, 100), PitchValue(60, 100) };
-static const QList<PitchValue> BEND_RELEASE
+static const QList<PitchValue> bendRelease
    = { PitchValue(0, 0),   PitchValue(10, 100), PitchValue(20, 100), PitchValue(30, 0), PitchValue(60, 0) };
-static const QList<PitchValue> BEND_RELEASE_BEND
+static const QList<PitchValue> bendReleaseBend
    = { PitchValue(0, 0),   PitchValue(10, 100), PitchValue(20, 100), PitchValue(30, 0), PitchValue(40, 0), PitchValue(50, 100), PitchValue(60, 100) };
-static const QList<PitchValue> PREBEND
+static const QList<PitchValue> prebend
    = { PitchValue(0, 100), PitchValue(60, 100) };
-static const QList<PitchValue> PREBEND_RELEASE
+static const QList<PitchValue> prebendRelease
    = { PitchValue(0, 100), PitchValue(15, 100), PitchValue(30, 0),   PitchValue(60, 0) };
 
 //---------------------------------------------------------
@@ -50,7 +54,16 @@ InspectorBend::InspectorBend(QWidget* parent)
             { Pid::FONT_STYLE,     0, g.fontStyle,   g.resetFontStyle   },
             };
       const std::vector<InspectorPanel> ppList = { {g.title, g.panel} };
+
       mapSignals(iiList, ppList);
+
+      g.bendType->clear();
+      g.bendType->addItem(tr("Bend"),              int(BendType::BEND)              );
+      g.bendType->addItem(tr("Bend/Release"),      int(BendType::BEND_RELEASE)      );
+      g.bendType->addItem(tr("Bend/Release/Bend"), int(BendType::BEND_RELEASE_BEND) );
+      g.bendType->addItem(tr("Prebend"),           int(BendType::PREBEND)           );
+      g.bendType->addItem(tr("Prebend/Release"),   int(BendType::PREBEND_RELEASE)   );
+      g.bendType->addItem(tr("Custom"),            int(BendType::CUSTOM)            );
 
       Bend* b = toBend(inspector->element());
       g.bendCanvas->setPoints(b->points());
@@ -67,19 +80,19 @@ void InspectorBend::setElement()
       InspectorElementBase::setElement();
 
       QList<PitchValue> points = g.bendCanvas->points();
-      if (!(g.bendType->currentIndex() == 5)) {  // custom bend
-            if (points == BEND)
-                  g.bendType->setCurrentIndex(0);
-            else if (points == BEND_RELEASE)
-                  g.bendType->setCurrentIndex(1);
-            else if (points == BEND_RELEASE_BEND)
-                  g.bendType->setCurrentIndex(2);
-            else if (points == PREBEND)
-                  g.bendType->setCurrentIndex(3);
-            else if (points == PREBEND_RELEASE)
-                  g.bendType->setCurrentIndex(4);
+      if (!(g.bendType->currentIndex() == int(BendType::CUSTOM))) {  // custom bend
+            if (points == bend)
+                  g.bendType->setCurrentIndex(int(BendType::BEND));
+            else if (points == bendRelease)
+                  g.bendType->setCurrentIndex(int(BendType::BEND_RELEASE));
+            else if (points == bendReleaseBend)
+                  g.bendType->setCurrentIndex(int(BendType::BEND_RELEASE_BEND));
+            else if (points == prebend)
+                  g.bendType->setCurrentIndex(int(BendType::PREBEND));
+            else if (points == prebendRelease)
+                  g.bendType->setCurrentIndex(int(BendType::PREBEND_RELEASE));
             else
-                  g.bendType->setCurrentIndex(5);
+                  g.bendType->setCurrentIndex(int(BendType::CUSTOM));
             }
       }
 
@@ -91,19 +104,19 @@ void InspectorBend::bendTypeChanged(int n)
       {
       switch (n) {
          case 0:
-            g.bendCanvas->setPoints(BEND);
+            g.bendCanvas->setPoints(bend);
             break;
          case 1:
-            g.bendCanvas->setPoints(BEND_RELEASE);
+            g.bendCanvas->setPoints(bendRelease);
             break;
          case 2:
-            g.bendCanvas->setPoints(BEND_RELEASE_BEND);
+            g.bendCanvas->setPoints(bendReleaseBend);
             break;
          case 3:
-            g.bendCanvas->setPoints(PREBEND);
+            g.bendCanvas->setPoints(prebend);
             break;
          case 4:
-            g.bendCanvas->setPoints(PREBEND_RELEASE);
+            g.bendCanvas->setPoints(prebendRelease);
             break;
          case 5:
          default:
@@ -120,12 +133,12 @@ void InspectorBend::bendTypeChanged(int n)
 
 void InspectorBend::updateBend()
       {
-      Bend* bend = toBend(inspector->element());
-      Score* sc = bend->score();
+      Bend* b = toBend(inspector->element());
+      Score* sc = b->score();
       sc->startCmd();
-      for (ScoreElement* b : bend->linkList()) {
-            sc->undo(new ChangeBend(toBend(b), g.bendCanvas->points()));
-            toBend(b)->triggerLayout();
+      for (ScoreElement* el : b->linkList()) {
+            sc->undo(new ChangeBend(toBend(el), g.bendCanvas->points()));
+            toBend(el)->triggerLayout();
             }
       sc->endCmd();
       }
