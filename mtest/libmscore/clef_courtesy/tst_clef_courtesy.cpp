1e141fdfcc64c97d5c3d8b140d69b7e4b1750ee0
AntonioBL <antonio.lotti@alice.it>
2020-01-15 23:09:14 +0100

    fix #151121 fix #286497 fix #298638 : fix courtesy clef visbility and add tests

--- mtest/libmscore/clef_courtesy/tst_clef_courtesy.cpp
+++ mtest/libmscore/clef_courtesy/tst_clef_courtesy.cpp
@@ -35,6 +35,7 @@ class TestClefCourtesy : public QObject, public MTest
       void clef_courtesy02();
       void clef_courtesy03();
       void clef_courtesy_78196();
+      void clef_courtesy04();
       };
 
 //---------------------------------------------------------
@@ -54,7 +55,7 @@ static Measure* getMeasure(Score* score, int idx)
       return m;
       }
 
-static void dropClef(Measure* m, ClefType t)
+static void dropClef(Element* m, ClefType t)
       {
       Clef* clef = new Clef(m->score()); // create a new element, as Measure::drop() will eventually delete it
       clef->setClefType(t);
@@ -62,7 +63,10 @@ static void dropClef(Measure* m, ClefType t)
       dropData.pos = m->pagePos();
       dropData.dropElement = clef;
       m->score()->startCmd();
-      m->drop(dropData);
+      if (m->isMeasure())
+            toMeasure(m)->drop(dropData);
+      else
+            m->findMeasure()->drop(dropData);
       m->score()->endCmd();
       }
 
@@ -101,9 +105,33 @@ void TestClefCourtesy::clef_courtesy01()
       QVERIFY2(seg, "No SegClef in measure 6.");
       clefCourt = toClef(seg->element(0));
       QVERIFY2(clefCourt, "No courtesy clef element in measure 6.");
-//      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 3 is NOT hidden.");
+      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 6 is NOT hidden.");
 
       QVERIFY(saveCompareScore(score, "clef_courtesy01.mscx", DIR + "clef_courtesy01-ref.mscx"));
+
+      Clef*    clef = nullptr;
+      seg = m1->findSegment(SegmentType::HeaderClef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 4.");
+      clef = static_cast<Clef*>(seg->element(0));
+      score->startCmd();
+      clef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      Clef* otherClef = clef->otherClef();
+      if (otherClef)
+            otherClef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      score->doLayout();
+      score->endCmd();
+      // check the required courtesy clef is there but hidden
+      m = m1->prevMeasure();
+      seg = m->findSegment(SegmentType::Clef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 3.");
+      clefCourt = static_cast<Clef*>(seg->element(0));
+      QVERIFY2(clefCourt, "No courtesy clef element in measure 3.");
+      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 3 is not hidden when showCourtesy is false.");
+
+      QVERIFY2(clef->clefType() == ClefType::G8_VA, "Clef type in measure 4 is wrong");
+      dropClef(clef, ClefType::G15_MA);
+      QVERIFY2(clef->clefType() == ClefType::G15_MA, "Clef type in measure 4 is wrong");
+
       delete score;
       }
 
@@ -148,7 +176,7 @@ void TestClefCourtesy::clef_courtesy02()
       QVERIFY2(seg != nullptr, "No SegClef in measure 3.");
       clefCourt = toClef(seg->element(0));
       QVERIFY2(clefCourt != nullptr, "No courtesy clef element in measure 3.");
-//      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 3 is NOT hidden.");
+      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 3 is NOT hidden.");
 
       clefCourt = nullptr;
       m = m2->prevMeasure();
@@ -156,7 +184,7 @@ void TestClefCourtesy::clef_courtesy02()
       QVERIFY2(seg != nullptr, "No SegClef in measure 6.");
       clefCourt = static_cast<Clef*>(seg->element(0));
       QVERIFY2(clefCourt != nullptr, "No courtesy clef element in measure 6.");
-//      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 3 is NOT hidden.");
+      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 6 is NOT hidden.");
 
       QVERIFY(saveCompareScore(score, "clef_courtesy02.mscx", DIR + "clef_courtesy02-ref.mscx"));
       delete score;
@@ -192,7 +220,7 @@ void TestClefCourtesy::clef_courtesy03()
       QVERIFY2(seg != nullptr, "No SegClef in measure 1.");
       clefCourt = static_cast<Clef*>(seg->element(0));
       QVERIFY2(clefCourt != nullptr, "No courtesy clef element in measure 1.");
-//      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 1 is NOT hidden.");
+      QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef in measure 1 is NOT hidden.");
 
       QVERIFY(saveCompareScore(score, "clef_courtesy03.mscx", DIR + "clef_courtesy03-ref.mscx"));
       delete score;
@@ -240,6 +268,134 @@ void TestClefCourtesy::clef_courtesy_78196()
       clefCourt = static_cast<Clef*>(seg->element(0));
       QVERIFY2(clefCourt != nullptr, "No courtesy clef at end of measure 6.");
       QVERIFY2(clefCourt->bbox().width() == 0, "Courtesy clef at end of measure 6 is NOT hidden.");
+      }
+
+//---------------------------------------------------------
+//   clef_courtesy04
+//    add two clefs mid-score at the beginning of systems for different
+//    staves and look for courtesy clefs in different "show courtesy" configurations
+//    also check the behavior after a horizontal frame
+//---------------------------------------------------------
+
+void TestClefCourtesy::clef_courtesy04()
+      {
+      MasterScore* score = readScore(DIR + "clef_courtesy04.mscx");
+
+      Clef*    clef = nullptr;
+      Segment* seg = nullptr;
+
+      // drop G1 clef to 4th measure, track 0 and track 4
+      Measure* m1 = getMeasure(score, 4);
+      seg = m1->findSegment(SegmentType::HeaderClef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 4.");
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No Clef in measure 4, track 0.");
+      dropClef(clef, ClefType::G8_VA);
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No Clef in measure 4, track 4.");
+      dropClef(clef, ClefType::G8_VA);
+
+      // drop G clef to 7th measure, track 4
+      Measure* m2 = getMeasure(score, 7);
+      seg = m2->findSegment(SegmentType::HeaderClef, m2->tick());
+      QVERIFY2(seg, "No SegClef in measure 7.");
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No Clef in measure 7, track 4.");
+      dropClef(clef, ClefType::G);
+
+      // check the required courtesy clefs are there and they are shown
+      Measure* m = m1->prevMeasure();
+      seg = m->findSegment(SegmentType::Clef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 3.");
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No courtesy clef element in measure 3, track 0.");
+      QVERIFY2(clef->bbox().width() > 0, "Courtesy clef in measure 3 is hidden.");
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No courtesy clef element in measure 3, track 4.");
+      QVERIFY2(clef->bbox().width() > 0, "Courtesy clef in measure 3 is hidden.");
+
+      // change "show courtesy" property for the clef in one of the staves
+      seg = m1->findSegment(SegmentType::HeaderClef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 4.");
+      clef = toClef(seg->element(0));
+      score->startCmd();
+      clef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      Clef* otherClef = clef->otherClef();
+      if (otherClef)
+            otherClef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      score->doLayout();
+      score->endCmd();
+
+      // check the required courtesy clef is there but hidden, and the other one is not hidden
+      seg = m->findSegment(SegmentType::Clef, m1->tick());
+      QVERIFY2(seg, "No SegClef in measure 3.");
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No courtesy clef element in measure 3 for track 0.");
+      QVERIFY2(clef->bbox().width() == 0, "Courtesy clef in measure 3, track 0, is not hidden when showCourtesy is false.");
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No courtesy clef element in measure 3 for track 4.");
+      QVERIFY2(clef->bbox().width() > 0, "Courtesy clef in measure 3, track 4, is hidden when showCourtesy is true.");
+
+      // drop clefs after a horizontal frame
+      Measure* m3 = getMeasure(score, 8);
+      seg = m3->findSegment(SegmentType::HeaderClef, m3->tick());
+      QVERIFY2(seg, "No SegClef in measure 8.");
+      // drop G clef to 8th measure, track 0
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No Clef in measure 8, track 0.");
+      dropClef(clef, ClefType::G);
+      // drop G1 clef to 8th measure, track 4
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No Clef in measure 8, track 4.");
+      dropClef(clef, ClefType::G8_VA);
+      score->startCmd();
+      clef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      otherClef = clef->otherClef();
+      if (otherClef)
+            otherClef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      score->doLayout();
+      score->endCmd();
+
+      // check the required courtesy clef is there but hidden, and the other one is not hidden
+      seg = m2->findSegment(SegmentType::Clef, m3->tick());
+      QVERIFY2(seg, "No SegClef in measure 7.");
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No courtesy clef element in measure 7 for track 4.");
+      QVERIFY2(clef->bbox().width() == 0, "Courtesy clef in measure 7, track 4, is not hidden when showCourtesy is false.");
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No courtesy clef element in measure 7 for track 0.");
+      QVERIFY2(clef->bbox().width() > 0, "Courtesy clef in measure 7, track 0, is hidden when showCourtesy is true.");
+
+      // for the section break case,
+      // check the not required courtesy clef element is there but it is not shown
+      m   = m2->prevMeasure();
+      seg = m->findSegment(SegmentType::Clef, m2->tick());
+      QVERIFY2(seg, "No SegClef in measure 6.");
+      clef = toClef(seg->element(4));
+      QVERIFY2(clef, "No courtesy clef element in measure 6, track 4.");
+      QVERIFY2(clef->bbox().width() == 0, "Courtesy clef in measure 6, track 4, is NOT hidden.");
+
+      Measure* m4 = getMeasure(score, 9);
+      // drop G1 clef to 9th measure, track 0
+      dropClef(m4, ClefType::G8_VA);
+      m = m4->prevMeasure();
+      seg = m->findSegment(SegmentType::Clef, m4->tick());
+      QVERIFY2(seg, "No SegClef in measure 8.");
+      clef = toClef(seg->element(0));
+      QVERIFY2(clef, "No Clef change in measure 8, track 0.");
+      QVERIFY2(clef->bbox().width() > 0, "Clef change in measure 8, track 0, is hidden.");
+      score->startCmd();
+      clef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      otherClef = clef->otherClef();
+      if (otherClef)
+            otherClef->undoChangeProperty(Pid::SHOW_COURTESY, false);
+      score->doLayout();
+      score->endCmd();
+      QVERIFY2(clef->bbox().width() > 0, "Clef change in measure 8, track 0, is hidden when showCourtesy is true.");
+
+      QVERIFY(saveCompareScore(score, "clef_courtesy04.mscx", DIR + "clef_courtesy04-ref.mscx"));
+
+      delete score;
       }
 
 QTEST_MAIN(TestClefCourtesy)
