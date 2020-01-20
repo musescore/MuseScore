f3aa719cce9ab1181a017b7435f053afcf2d7cd7
Dmitri Ovodok <dmitrio95@yandex.ru>
2020-01-20 11:19:44 +0200

    fix #299883: allow using Ctrl+arrow on grips in normal mode

--- mscore/events.cpp
+++ mscore/events.cpp
@@ -75,7 +75,9 @@ bool ScoreView::event(QEvent* event)
                               // KeypadModifier is necessary on MacOS as arrow keys seem to always
                               // trigger that modifier there. However it would probably be appropriate
                               // to allow it on other systems too.
-                              constexpr auto allowedModifiers = Qt::ShiftModifier | Qt::KeypadModifier;
+                              const auto allowedModifiers = (editData.curGrip == Grip::NO_GRIP)
+                                 ? (Qt::KeypadModifier | Qt::ShiftModifier)
+                                 : (Qt::KeypadModifier | Qt::ShiftModifier | Qt::ControlModifier);
                               if ((m & ~allowedModifiers) == 0) {
                                     ke->accept();
                                     return true;
