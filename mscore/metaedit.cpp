3b939aef0657f3cf232101a8cf99492e033b5f13
Marr11317 <remimarche@gmail.com>
2020-01-22 22:42:58 -0500

    Add support for deleting (custom) metatags.

--- mscore/metaedit.cpp
+++ mscore/metaedit.cpp
@@ -21,6 +21,8 @@
 #include "libmscore/score.h"
 #include "libmscore/undo.h"
 #include "musescore.h"
+#include "preferences.h"
+#include "icons.h"
 
 namespace Ms {
 
@@ -28,18 +30,21 @@ namespace Ms {
 //   MetaEditDialog
 //---------------------------------------------------------
 
-MetaEditDialog::MetaEditDialog(Score* s, QWidget* parent)
-   : QDialog(parent)
+MetaEditDialog::MetaEditDialog(Score* score, QWidget* parent)
+   : QDialog(parent),
+     m_score(score),
+     m_dirty(false)
       {
       setObjectName("MetaEditDialog");
-      setupUi(this);
-      setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
-      score = s;
-      dirty = false;
 
-      level->setValue(score->mscVersion());
-      version->setText(score->mscoreVersion());
-      int rev = score->mscoreRevision();
+      setupUi(this);
+      QDialog::setWindowFlag(Qt::WindowContextHelpButtonHint, false);
+      QDialog::setWindowFlag(Qt::WindowMinMaxButtonsHint);
+
+      version->setText(m_score->mscoreVersion());
+      level->setText(QString::number(m_score->mscVersion()));
+
+      int rev = m_score->mscoreRevision();
       if (rev > 99999)  // MuseScore 1.3 is decimal 5702, 2.0 and later uses a 7-digit hex SHA
             revision->setText(QString::number(rev, 16));
       else
@@ -55,78 +60,209 @@ MetaEditDialog::MetaEditDialog(Score* s, QWidget* parent)
                                    tr("initially read from:"), previousFileName));
       filePath->setTextInteractionFlags(Qt::TextSelectableByMouse);
 
-      int idx = 0;
-      QMapIterator<QString, QString> i(s->metaTags());
-      QGridLayout* grid = static_cast<QGridLayout*>(scrollWidget->layout());
-      while (i.hasNext()) {
-            i.next();
-            QLabel* l = new QLabel;
-            l->setText(i.key());
-            QLineEdit* text = new QLineEdit(i.value(), 0);
-            connect(text, SIGNAL(textChanged(const QString&)), SLOT(setDirty()));
-            grid->addWidget(l, idx, 0);
-            grid->addWidget(text, idx, 1);
-            ++idx;
+      QMapIterator<QString, QString> iterator(score->metaTags());
+      while (iterator.hasNext()) {
+            iterator.next();
+            const QString key = iterator.key();
+            addTag(key, iterator.value(), isBuiltinTag(key));
             }
-      connect(newButton, SIGNAL(clicked()), SLOT(newClicked()));
+
+      scrollAreaLayout->setColumnStretch(1, 1); // The 'value' column should be expanding
+
+      connect(newButton,  &QPushButton::clicked, this, [this]() { newClicked(); });
+      connect(saveButton, &QPushButton::clicked, this, &MetaEditDialog::save);
       MuseScore::restoreGeometry(this);
       }
 
 //---------------------------------------------------------
-//   newClicked
+//   addTag
+///   Add a tag to the displayed list
+///   returns a pair of widget corresponding to the key and value:
+///           QPair<QLineEdit* key, QLineEdit* value>
 //---------------------------------------------------------
 
-void MetaEditDialog::newClicked()
-      {
-      QString s = QInputDialog::getText(this,
-         tr("Input Tag Name"),
-         tr("New tag name:")
-         );
-      QGridLayout* grid = static_cast<QGridLayout*>(scrollWidget->layout());
-      if (!s.isEmpty()) {
-            int idx = grid->rowCount();
-            QLabel* l = new QLabel;
-            l->setText(s);
-            QLineEdit* text = new QLineEdit;
-            grid->addWidget(l, idx, 0);
-            grid->addWidget(text, idx, 1);
+QPair<QLineEdit*, QLineEdit*> MetaEditDialog::addTag(const QString& key, const QString& value, const bool builtinTag)
+{
+      QLineEdit* tagWidget = new QLineEdit(key);
+      QLineEdit* valueWidget = new QLineEdit(value);
+
+      connect(valueWidget, &QLineEdit::textChanged, this, [this]() { setDirty(); });
+
+      const int numFlags = scrollAreaLayout->rowCount();
+      if (builtinTag) {
+            tagWidget->setReadOnly(true);
+            // Make it clear that builtin tags are not editable
+            tagWidget->setStyleSheet("QLineEdit { background: transparent; }");
+            tagWidget->setFrame(false);
+            tagWidget->setFocusPolicy(Qt::NoFocus);
+            tagWidget->setToolTip(tr("This is a builtin tag. Its name cannot be modified."));
             }
-      dirty = true;
+      else {
+            tagWidget->setPlaceholderText(tr("Name"));
+            QToolButton* deleteButton = new QToolButton();
+            deleteButton->setIcon(*icons[int (Icons::bin_ICON)]);
+
+            // follow gui scaling. The '+ 2' at the end is the margin. (2 * 1px).for top and bottoms.
+            const double size = preferences.getInt(PREF_UI_THEME_ICONWIDTH) * guiScaling * .5 + 2;
+            deleteButton->setIconSize(QSize(size, size));
+
+            connect(tagWidget, &QLineEdit::textChanged, this, [this]() { setDirty(); });
+            connect(deleteButton, &QToolButton::clicked, this,
+                    [this, tagWidget, valueWidget, deleteButton]() { setDirty();
+                                                         tagWidget->deleteLater();
+                                                         valueWidget->deleteLater();
+                                                         deleteButton->deleteLater(); });
+
+            scrollAreaLayout->addWidget(deleteButton, numFlags, 2);
+            }
+      scrollAreaLayout->addWidget(tagWidget,   numFlags, 0);
+      scrollAreaLayout->addWidget(valueWidget, numFlags, 1);
+
+      return QPair<QLineEdit*, QLineEdit*>(tagWidget, valueWidget);
+      }
+
+//---------------------------------------------------------
+//   newClicked
+///   When the 'New' button is clicked, a new tag is appended,
+///   and focus is set to the QLineEdit corresponding to its name.
+//---------------------------------------------------------
+
+void MetaEditDialog::newClicked() {
+      QPair<QLineEdit*, QLineEdit*> pair = addTag("", "", false);
+
+      pair.first->setFocus();
+      pair.second->setPlaceholderText(tr("Value"));
+      // scroll down to see the newly created tag.
+      // ugly workaround because scrolling to maximum doesn't completely scroll
+      // to the maximum, for some unknow reason.
+      // See https://www.qtcentre.org/threads/32852-How-can-I-always-keep-the-scroll-bar-at-the-bottom-of-a-QScrollArea
+      QScrollBar* scrollBar = scrollArea->verticalScrollBar();
+      scrollBar->setMaximum(scrollBar->maximum() + 1);
+      scrollBar->setValue(scrollBar->maximum());
+
+      setDirty();
+      }
+
+//---------------------------------------------------------
+//   isBuiltinTag
+///   returns true if the tag is one of Musescore's builtin tags
+///   see also MasterScore::MasterScore()
+//---------------------------------------------------------
+
+const bool MetaEditDialog::isBuiltinTag(const QString& tag) const {
+      return (tag ==  "platform"      || tag ==  "movementNumber" || tag ==  "movementTitle"
+              || tag ==  "workNumber" || tag ==  "workTitle"      || tag ==  "arranger"
+              || tag ==  "composer"   || tag ==  "lyricist"       || tag ==  "poet"
+              || tag ==  "translator" || tag ==  "source"         || tag ==  "copyright"
+              || tag ==  "creationDate");
+      }
+
+//---------------------------------------------------------
+//   setDirty
+///    Sets the editor as having unsaved changes
+//---------------------------------------------------------
+
+void MetaEditDialog::setDirty(const bool dirty)
+      {
+      if (dirty == m_dirty)
+            return;
+
+      saveButton->setEnabled(dirty);
+      setWindowTitle(tr("Score properties: ") + m_score->title() + (dirty ? "*" : ""));
+
+      m_dirty = dirty;
+      }
+
+//---------------------------------------------------------
+//   save
+///   Save the currently displayed metatags
+//---------------------------------------------------------
+
+const bool MetaEditDialog::save()
+      {
+      if (m_dirty) {
+            const int idx = scrollAreaLayout->rowCount();
+            QMap<QString, QString> map;
+            for (int i = 0; i < idx; ++i) {
+                  QLayoutItem *tagItem   = scrollAreaLayout->itemAtPosition(i, 0);
+                  QLayoutItem *valueItem = scrollAreaLayout->itemAtPosition(i, 1);
+                  if (tagItem && valueItem) {
+                        QLineEdit *tag   = static_cast<QLineEdit*>(tagItem->widget());
+                        QLineEdit *value = static_cast<QLineEdit*>(valueItem->widget());
+
+                        QString tagText = tag->text();
+                        if (tagText.isEmpty()) {
+                              QMessageBox::warning(this, tr("MuseScore"),
+                                                   tr("Tags can't have empty names."),
+                                                   QMessageBox::Ok, QMessageBox::Ok);
+                              tag->setFocus();
+                              return false;
+                              }
+                        if (map.contains(tagText)) {
+                              if (isBuiltinTag(tagText)) {
+                                    QMessageBox::warning(this, tr("MuseScore"),
+                                                         tagText + tr(" is a reserved builtin tag.\n"
+                                                                      "It can't be used."),
+                                                         QMessageBox::Ok, QMessageBox::Ok);
+                                    tag->setFocus();
+                                    return false;
+                                    }
+                              QMessageBox::warning(this, tr("MuseScore"),
+                                                   tr("You have multiple fields with the same name."),
+                                                   QMessageBox::Ok, QMessageBox::Ok);
+                              tag->setFocus();
+                              return false;
+                              }
+                        map.insert(tagText, value->text());
+                        }
+                  else
+                        qDebug("MetaEditDialog: abnormal configuration: %i", i);
+                  }
+            m_score->undo(new ChangeMetaTags(m_score, map));
+            setDirty(false);
+            }
+      return true;
       }
 
 //---------------------------------------------------------
 //   accept
+///   Reimplemented to save modifications before closing the dialog.
 //---------------------------------------------------------
 
 void MetaEditDialog::accept()
       {
-      if (dirty) {
-            QGridLayout* grid = static_cast<QGridLayout*>(scrollWidget->layout());
-            int idx = grid->rowCount();
-            QMap<QString, QString> m;
-            for (int i = 0; i < idx; ++i) {
-                  QLayoutItem* labelItem = grid->itemAtPosition(i, 0);
-                  QLayoutItem* dataItem  = grid->itemAtPosition(i, 1);
-                  if (labelItem && dataItem) {
-                        QLabel* l = static_cast<QLabel*>(labelItem->widget());
-                        QLineEdit* le = static_cast<QLineEdit*>(dataItem->widget());
-                        m.insert(l->text(), le->text());
-                        }
-                  }
-            score->undo(new ChangeMetaTags(score, m));
-            }
+      if (!save())
+            return;
+
       QDialog::accept();
       }
 
 //---------------------------------------------------------
 //   hideEvent
+///   Reimplemented to notify the user that he/she is quitting without saving
 //---------------------------------------------------------
 
-void MetaEditDialog::hideEvent(QHideEvent* event)
+void MetaEditDialog::closeEvent(QCloseEvent* event)
       {
+      if (m_dirty) {
+            QMessageBox::StandardButton button = QMessageBox::warning(this, tr("MuseScore"),
+                                                                      tr("You have unsaved changes.\nSave?"),
+                                                                      QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel,
+                                                                      QMessageBox::Save);
+            if (button == QMessageBox::Save) {
+                  if (!save()) {
+                        event->ignore();
+                        return;
+                        }
+                  }
+            else if (button == QMessageBox::Cancel) {
+                  event->ignore();
+                  return;
+                  }
+            }
       MuseScore::saveGeometry(this);
-      QWidget::hideEvent(event);
+      event->accept();
       }
 
-}
+} // namespace Ms
 
