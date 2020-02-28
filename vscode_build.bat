c819d36123699957a078674d952e93f99500d31e
Marr11317 <remimarche@gmail.com>
2020-02-28 14:02:14 -0500

    Build and Debug unde VsCode with intellicode

---
+++ vscode_build.bat
@@ -0,0 +1,4 @@
+REM Build MuseScore
+C:/Users/main/Documents/GitHub/MuseScore/msvc_build.bat relwithdebinfo
+REM Install MuseScore
+C:/Users/main/Documents/GitHub/MuseScore/msvc_build.bat installrelwithdebinfo
