6c711ead9f56c300669386956761c6f9fd7cd67d
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-08 21:55:51 +0200

    Implemented the Glissando playback model

---
+++ inspectors/models/general/playback/internal_models/glissandoplaybackmodel.cpp
@@ -0,0 +1,33 @@
+#include "glissandoplaybackmodel.h"
+
+GlissandoPlaybackModel::GlissandoPlaybackModel(QObject* parent, IElementRepositoryService* repository) : AbstractInspectorModel(parent, repository)
+{
+    setTitle(tr("Glissando"));
+
+    createProperties();
+}
+
+void GlissandoPlaybackModel::createProperties()
+{
+    m_styleType = buildPropertyItem(Ms::Pid::GLISSANDO_STYLE);
+}
+
+void GlissandoPlaybackModel::requestElements()
+{
+    m_elementList = m_repository->findElementsByType(Ms::ElementType::GLISSANDO_SEGMENT);
+}
+
+void GlissandoPlaybackModel::loadProperties()
+{
+    loadPropertyItem(m_styleType);
+}
+
+void GlissandoPlaybackModel::resetProperties()
+{
+    m_styleType->resetToDefault();
+}
+
+PropertyItem* GlissandoPlaybackModel::styleType() const
+{
+    return m_styleType;
+}
