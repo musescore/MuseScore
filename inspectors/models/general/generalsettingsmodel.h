7b70e1a183f1c5b526f5e9d10e910d566259ce98
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-08 21:32:00 +0200

    Implemented the GeneralSettingsModel as a handler for general element's properties (like visible, small, etc.)

---
+++ inspectors/models/general/generalsettingsmodel.h
@@ -0,0 +1,55 @@
+#ifndef GENERALSETTINGSMODEL_H
+#define GENERALSETTINGSMODEL_H
+
+#include "models/abstractinspectormodel.h"
+#include "playback/playbackproxymodel.h"
+
+class GeneralSettingsModel : public AbstractInspectorModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(PropertyItem* isVisible READ isVisible CONSTANT)
+    Q_PROPERTY(PropertyItem* isAutoPlaceAllowed READ isAutoPlaceAllowed CONSTANT)
+    Q_PROPERTY(PropertyItem* isPlayable READ isPlayable CONSTANT)
+    Q_PROPERTY(PropertyItem* isPlayModeAvailable READ isPlayModeAvailable CONSTANT)
+    Q_PROPERTY(PropertyItem* isSmall READ isSmall CONSTANT)
+    Q_PROPERTY(PropertyItem* isSmallModeAvailable READ isSmallModeAvailable CONSTANT)
+
+    Q_PROPERTY(QObject* playbackProxyModel READ playbackProxyModel NOTIFY playbackProxyModelChanged)
+
+    public:
+        explicit GeneralSettingsModel(QObject* parent, IElementRepositoryService* repository);
+
+    PropertyItem* isVisible() const;
+    PropertyItem* isAutoPlaceAllowed() const;
+    PropertyItem* isPlayable() const;
+    PropertyItem* isPlayModeAvailable() const;
+    PropertyItem* isSmall() const;
+    PropertyItem* isSmallModeAvailable() const;
+
+    QObject* playbackProxyModel() const;
+
+public slots:
+    void setPlaybackProxyModel(PlaybackProxyModel* playbackProxyModel);
+
+signals:
+    void playbackProxyModelChanged(QObject* playbackProxyModel);
+
+protected:
+    void createProperties() override;
+    void requestElements() override;
+    void loadProperties() override;
+    void resetProperties() override;
+
+private:
+    PropertyItem* m_isVisible = nullptr;
+    PropertyItem* m_isAutoPlaceAllowed = nullptr;
+    PropertyItem* m_isPlayable = nullptr;
+    PropertyItem* m_isPlayModeAvailable = nullptr;
+    PropertyItem* m_isSmall = nullptr;
+    PropertyItem* m_isSmallModeAvailable = nullptr;
+
+    PlaybackProxyModel* m_playbackProxyModel = nullptr;
+};
+
+#endif // GENERALSETTINGSMODEL_H
