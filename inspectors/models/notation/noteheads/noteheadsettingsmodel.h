6f722cc075a479e2159c553b443a1bf8d075bea3
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-08 21:35:02 +0200

    Implemented NoteHeadSettingsModel for the Notehead inspector's section

---
+++ inspectors/models/notation/noteheads/noteheadsettingsmodel.h
@@ -0,0 +1,59 @@
+#ifndef NOTEHEADSETTINGSMODEL_H
+#define NOTEHEADSETTINGSMODEL_H
+
+#include "models/abstractinspectormodel.h"
+#include "noteheadtypesmodel.h"
+
+class NoteheadSettingsModel : public AbstractInspectorModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QObject* noteheadTypesModel READ noteheadTypesModel NOTIFY noteheadTypesModelChanged)
+    Q_PROPERTY(PropertyItem* isHeadHidden READ isHeadHidden CONSTANT)
+    Q_PROPERTY(PropertyItem* headDirection READ headDirection CONSTANT)
+    Q_PROPERTY(PropertyItem* headGroup READ headGroup CONSTANT)
+    Q_PROPERTY(PropertyItem* headType READ headType CONSTANT)
+    Q_PROPERTY(PropertyItem* dotPosition READ dotPosition CONSTANT)
+    Q_PROPERTY(PropertyItem* horizontalOffset READ horizontalOffset CONSTANT)
+    Q_PROPERTY(PropertyItem* verticalOffset READ verticalOffset CONSTANT)
+
+public:
+    explicit NoteheadSettingsModel(QObject* parent, IElementRepositoryService* repository);
+
+public:
+
+    QObject* noteheadTypesModel() const;
+
+    PropertyItem* isHeadHidden() const;
+    PropertyItem* headDirection() const;
+    PropertyItem* headGroup() const;
+    PropertyItem* headType() const;
+    PropertyItem* dotPosition() const;
+    PropertyItem* horizontalOffset() const;
+    PropertyItem* verticalOffset() const;
+
+public slots:
+    void setNoteheadTypesModel(NoteheadTypesModel* noteheadTypesModel);
+
+signals:
+    void noteheadTypesModelChanged(QObject* noteheadTypesModel);
+
+protected:
+    void createProperties() override;
+    void requestElements() override;
+    void loadProperties() override;
+    void resetProperties() override;
+
+private:
+    NoteheadTypesModel* m_noteheadTypesModel = nullptr;
+
+    PropertyItem* m_isHeadHidden = nullptr;
+    PropertyItem* m_headDirection = nullptr;
+    PropertyItem* m_headGroup = nullptr;
+    PropertyItem* m_headType = nullptr;
+    PropertyItem* m_dotPosition = nullptr;
+    PropertyItem* m_horizontalOffset = nullptr;
+    PropertyItem* m_verticalOffset = nullptr;
+};
+
+#endif // NOTEHEADSETTINGSMODEL_H
---
+++ inspectors/models/notation/noteheads/noteheadtypesmodel.cpp
@@ -0,0 +1,80 @@
+#include "noteheadtypesmodel.h"
+
+NoteheadTypesModel::NoteheadTypesModel(QObject* parent) : QAbstractListModel(parent)
+{
+    m_roleNames.insert(HeadGroupRole, "headGroupRole");
+    m_roleNames.insert(HintRole, "hintRole");
+}
+
+void NoteheadTypesModel::load()
+{
+    if (!m_noteheadTypeDataList.isEmpty())
+        return;
+
+    for (int i = 0; i < static_cast<int>(Ms::NoteHead::Group::HEAD_DO_WALKER); ++i) {
+          HeadTypeData headTypeData;
+
+          headTypeData.group = static_cast<Ms::NoteHead::Group>(i);
+          headTypeData.hint = Ms::NoteHead::group2userName(headTypeData.group);
+
+          m_noteheadTypeDataList << headTypeData;
+    }
+}
+
+QHash<int, QByteArray> NoteheadTypesModel::roleNames() const
+{
+    return m_roleNames;
+}
+
+int NoteheadTypesModel::rowCount(const QModelIndex&) const
+{
+    return m_noteheadTypeDataList.count();
+}
+
+QVariant NoteheadTypesModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || index.row() >= rowCount() || m_noteheadTypeDataList.isEmpty())
+          return QVariant();
+
+    HeadTypeData headTypeData = m_noteheadTypeDataList.at(index.row());
+
+    switch (role) {
+    case HeadGroupRole: return static_cast<int>(headTypeData.group);
+    case HintRole: return headTypeData.hint;
+    default: return QVariant();
+    }
+}
+
+void NoteheadTypesModel::init(const Ms::NoteHead::Group noteHeadGroup)
+{
+    m_selectedHeadTypeIndex = indexOfHeadGroup(noteHeadGroup);
+    emit selectedHeadTypeIndexChanged(m_selectedHeadTypeIndex);
+
+    load();
+}
+
+int NoteheadTypesModel::selectedHeadTypeIndex() const
+{
+    return m_selectedHeadTypeIndex;
+}
+
+void NoteheadTypesModel::setSelectedHeadTypeIndex(int selectedHeadTypeIndex)
+{
+    if (m_selectedHeadTypeIndex == selectedHeadTypeIndex)
+        return;
+
+    m_selectedHeadTypeIndex = selectedHeadTypeIndex;
+    emit selectedHeadTypeIndexChanged(m_selectedHeadTypeIndex);
+
+    emit noteHeadGroupSelected(static_cast<int>(m_noteheadTypeDataList.at(selectedHeadTypeIndex).group));
+}
+
+int NoteheadTypesModel::indexOfHeadGroup(const Ms::NoteHead::Group group) const
+{
+    for (int i = 0; i < m_noteheadTypeDataList.count(); ++i) {
+        if (m_noteheadTypeDataList.at(i).group == group)
+            return i;
+    }
+
+    return -1;
+}
