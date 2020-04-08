eec3c714569a849b33a6f925c18752842ea0f47e
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-08 21:53:47 +0200

    Implemented the Breaths playback model

---
+++ inspectors/models/general/playback/internal_models/breathplaybackmodel.h
@@ -0,0 +1,28 @@
+#ifndef BREATHPLAYBACKMODEL_H
+#define BREATHPLAYBACKMODEL_H
+
+#include "models/abstractinspectormodel.h"
+
+class BreathPlaybackModel : public AbstractInspectorModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(PropertyItem* pauseTime READ pauseTime CONSTANT)
+
+public:
+    explicit BreathPlaybackModel(QObject* parent, IElementRepositoryService* repository);
+
+public:
+    PropertyItem* pauseTime() const;
+
+protected:
+    void createProperties() override;
+    void requestElements() override;
+    void loadProperties() override;
+    void resetProperties() override;
+
+private:
+    PropertyItem* m_pauseTime = nullptr;
+};
+
+#endif // BREATHPLAYBACKMODEL_H
