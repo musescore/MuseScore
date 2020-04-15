1d014d558a5e591c4398f78731b37c587b943a0a
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-15 12:17:20 +0200

    Protected from the old crash when updating the grips

--- libmscore/beam.cpp
+++ libmscore/beam.cpp
@@ -2265,13 +2265,18 @@ std::vector<QPointF> Beam::gripsPositions(const EditData& ed) const
     ChordRest* c1 = nullptr;
     ChordRest* c2 = nullptr;
     int n = _elements.size();
+
+    if (n == 0) {
+        return std::vector<QPointF>();
+    }
+
     for (int i = 0; i < n; ++i) {
         if (_elements[i]->isChordRest()) {
             c1 = toChordRest(_elements[i]);
             break;
         }
     }
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = n-1; i >= 0; --i) {
         if (_elements[i]->isChordRest()) {
             c2 = toChordRest(_elements[i]);
             break;
