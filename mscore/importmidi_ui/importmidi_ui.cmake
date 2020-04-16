db8decd0a7a780159beea2855d9a04838021f8b1
Igor Korsukov <igor.korsukov@gmail.com>
2020-04-16 14:27:21 +0200

    made .cmake files for mscore subdirs

---
+++ mscore/importmidi_ui/importmidi_ui.cmake
@@ -0,0 +1,13 @@
+
+set (IMPORTMIDIUI_SRC
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_delegate.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_delegate.h
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_panel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_panel.h
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_view.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_view.h
+    )
+
+set (IMPORTMIDIUI_UI
+    ${CMAKE_CURRENT_LIST_DIR}/importmidi_panel.ui
+    )
