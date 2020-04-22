01d9c1e40c848a5a1ab8b1b51ff27cdef46ffc24
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-04-22 11:49:10 +0200

    Implemented the fermata's inspector model

---
+++ inspectors/models/notation/fermatas/fermatasettingsmodel.h
@@ -0,0 +1,25 @@
+#ifndef FERMATASETTINGSMODEL_H
+#define FERMATASETTINGSMODEL_H
+
+#include "models/abstractinspectormodel.h"
+
+class FermataSettingsModel : public AbstractInspectorModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(PropertyItem* placementType READ placementType CONSTANT)
+public:
+    explicit FermataSettingsModel(QObject* parent, IElementRepositoryService* repository);
+
+    void createProperties() override;
+    void requestElements() override;
+    void loadProperties() override;
+    void resetProperties() override;
+
+    PropertyItem* placementType() const;
+
+private:
+    PropertyItem* m_placementType = nullptr;
+};
+
+#endif // FERMATASETTINGSMODEL_H
