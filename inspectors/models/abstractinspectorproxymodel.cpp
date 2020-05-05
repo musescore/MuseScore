e58e9334a41a3a539029af4b3c84ac24bff9f1b0
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-05-05 21:42:58 +0200

    Implemented new base class for inspector proxy models

---
+++ inspectors/models/abstractinspectorproxymodel.cpp
@@ -0,0 +1,42 @@
+#include "abstractinspectorproxymodel.h"
+
+AbstractInspectorProxyModel::AbstractInspectorProxyModel(QObject* parent) :
+    AbstractInspectorModel(parent)
+{
+}
+
+QObject* AbstractInspectorProxyModel::modelByType(const InspectorModelType type)
+{
+    return m_modelsHash.value(type);
+}
+
+void AbstractInspectorProxyModel::requestResetToDefaults()
+{
+    for (AbstractInspectorModel* model : m_modelsHash.values()) {
+        model->requestResetToDefaults();
+    }
+}
+
+bool AbstractInspectorProxyModel::hasAcceptableElements() const
+{
+    bool result = false;
+
+    for (const AbstractInspectorModel* model : m_modelsHash.values()) {
+        result |= model->hasAcceptableElements();
+    }
+
+    return result;
+}
+
+void AbstractInspectorProxyModel::addModel(AbstractInspectorModel* model)
+{
+    if (!model) {
+        return;
+    }
+
+    connect(model, &AbstractInspectorModel::isEmptyChanged, this, [this] () {
+        setIsEmpty(!hasAcceptableElements());
+    });
+
+    m_modelsHash.insert(model->modelType(), model);
+}
