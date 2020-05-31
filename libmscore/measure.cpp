73b635396281f623f200a5a1a633a508a8cd0dc6
Unknown <isaacaweiss@gmail.com>
2020-05-31 13:41:46 -0400

    Fix #306092: Adding bracket when nothing selected sets its span to 0

--- libmscore/measure.cpp
+++ libmscore/measure.cpp
@@ -1426,7 +1426,10 @@ Element* Measure::drop(EditData& data)
                         firstStaff++;
                         }
                   Selection sel = score()->selection();
-                  score()->undoAddBracket(staff, level, b->bracketType(), sel.staffEnd() - sel.staffStart());
+                  if (sel.isRange())
+                        score()->undoAddBracket(staff, level, b->bracketType(), sel.staffEnd() - sel.staffStart());
+                  else
+                        score()->undoAddBracket(staff, level, b->bracketType(), 1);
                   delete b;
                   }
                   break;
