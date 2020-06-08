f6014edb50542846b160b30da20c0b3b5011b96b
Joachim Schmitz <jojo@schmitz-digital.de>
2020-06-08 14:02:02 +0200

    add Gould arrow down/up accidentals
    see https://musescore.org/en/node/306457 and
    https://musescore.org/en/node/306456

--- libmscore/types.h
+++ libmscore/types.h
@@ -174,6 +174,8 @@ enum class AccidentalType : char {
       SHARP2_ARROW_DOWN,
       FLAT2_ARROW_UP,
       FLAT2_ARROW_DOWN,
+      ARROW_DOWN,
+      ARROW_UP,
 
       // Stein-Zimmermann
       MIRRORED_FLAT,
