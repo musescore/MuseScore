63b54c3324f0fc46e6bdb57a886b6a36e7d1fd48
Igor Korsukov <igor.korsukov@gmail.com>
2020-06-08 15:58:06 +0200

    added async primitives

---
+++ thirdparty/deto_async/async/async.cmake
@@ -0,0 +1,8 @@
+
+set (ASYNC_SRC
+    ${CMAKE_CURRENT_LIST_DIR}/asyncable.h
+    ${CMAKE_CURRENT_LIST_DIR}/channel.h
+    ${CMAKE_CURRENT_LIST_DIR}/notify.h
+    ${CMAKE_CURRENT_LIST_DIR}/internal/abstractinvoker.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/internal/abstractinvoker.h
+)
