7697019de9c2e9922d365cb15754b0c24238ee17
Joachim Schmitz <jojo@schmitz-digital.de>
2020-06-08 12:34:19 +0200

    fix #305613: add heavy, reverse end and heavy double barlines to Inspector
    follow up for PR #6119, to adjust to the new QML based inspector, also:
    
    * add alterative names to inspector barlinetypes.h to match those from
    mscore.h
    * reuse existing translations for barlines in Inspector, same as the
    Palettes use (Barlines, Repeats & Jumps, Symbols)
    * using the same order of barlines in Inspector as in the Palettes

--- inspectors/types/barlinetypes.h
+++ inspectors/types/barlinetypes.h
@@ -10,13 +10,23 @@ class BarlineTypes
 public:
     enum class LineType {
         TYPE_NORMAL = 1,
+        TYPE_SINGLE= LineType::TYPE_NORMAL,
         TYPE_DOUBLE = 2,
         TYPE_START_REPEAT = 4,
+        TYPE_LEFT_REPEAT = LineType::TYPE_START_REPEAT,
         TYPE_END_REPEAT = 8,
+        TYPE_RIGHT_REPEAT = LineType::TYPE_END_REPEAT,
         TYPE_DASHED = 0x10,
+        TYPE_BROKEN = LineType::TYPE_DASHED,
         TYPE_FINAL = 0x20,
+        TYPE_END = LineType::TYPE_FINAL,
         TYPE_END_START_REPEAT = 0x40,
-        TYPE_DOTTED = 0x80
+        TYPE_LEFT_RIGHT_REPEAT = LineType::TYPE_END_START_REPEAT,
+        TYPE_DOTTED = 0x80,
+        TYPE_REVERSE_END = 0x100,
+        TYPE_REVERS_FINAL = LineType::TYPE_REVERSE_END,
+        TYPE_HEAVY = 0x200,
+        TYPE_DOUBLE_HEAVY = 0x400,
     };
 
     enum class SpanPreset {
@@ -24,7 +34,7 @@ public:
         PRESET_TICK_1,
         PRESET_TICK_2,
         PRESET_SHORT_1,
-        PRESET_SHORT_2
+        PRESET_SHORT_2,
     };
 
     Q_ENUM(LineType)
