92b76f6f55c510125498b9590e78c9c8b933314e
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-06-13 15:46:01 +0200

    Extended the property item model to support the styled properties

--- inspectors/models/propertyitem.cpp
+++ inspectors/models/propertyitem.cpp
@@ -1,6 +1,6 @@
 #include "propertyitem.h"
 
-PropertyItem::PropertyItem(const int propertyId, QObject* parent) : QObject(parent)
+PropertyItem::PropertyItem(const int propertyId, const int styleId, QObject* parent) : QObject(parent)
 {
     m_propertyId = propertyId;
 }
@@ -17,12 +17,18 @@ void PropertyItem::updateCurrentValue(const QVariant& currentValue)
     m_currentValue = currentValue;
 
     emit isUndefinedChanged(isUndefined());
+    emit isModifiedChanged(isModified());
     emit valueChanged(m_currentValue);
 }
 
 void PropertyItem::resetToDefault()
 {
     setValue(m_defaultValue);
+}
+
+void PropertyItem::applyToStyle()
+{
+    emit applyToStyleRequested(m_styleId, m_currentValue);
 }
 
 int PropertyItem::propertyId() const
@@ -50,6 +56,16 @@ bool PropertyItem::isEnabled() const
     return m_isEnabled;
 }
 
+bool PropertyItem::isStyled() const
+{
+    return m_isStyled;
+}
+
+bool PropertyItem::isModified() const
+{
+    return m_currentValue != m_defaultValue;
+}
+
 void PropertyItem::setValue(const QVariant& value)
 {
     if (m_currentValue == value)
@@ -73,4 +89,13 @@ void PropertyItem::setIsEnabled(bool isEnabled)
 
     m_isEnabled = isEnabled;
     emit isEnabledChanged(m_isEnabled);
+}
+
+void PropertyItem::setIsStyled(bool isStyled)
+{
+    if (m_isStyled == isStyled)
+        return;
+
+    m_isStyled = isStyled;
+    emit isStyledChanged(m_isStyled);
 }
