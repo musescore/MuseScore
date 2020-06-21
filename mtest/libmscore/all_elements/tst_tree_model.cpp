e5147683fecfe82e7da4c84c6a1bb2cb6815d1d5
Kumar Kartikay <krkartikay@gmail.com>
2020-06-21 18:32:18 +0530

    Add test for tree model consistency
    Renamed the layout_elements folder to all_elements, for tests that
    need to check every element and don't need reference files. Added
    tst_tree_model to all_elements directory.
    The test checks that for every element el, `el->treeChild(i)->parent`
    should be `el`.

---
+++ mtest/libmscore/all_elements/tst_tree_model.cpp
@@ -0,0 +1,111 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2020 MuseScore BVBA and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2
+//  as published by the Free Software Foundation and appearing in
+//  the file LICENCE.GPL
+//=============================================================================
+
+#include <QtTest/QtTest>
+#include "mtest/testutils.h"
+#include "libmscore/score.h"
+
+#define DIR QString("libmscore/all_elements/")
+
+using namespace Ms;
+
+//---------------------------------------------------------
+//   TestTreeModel
+///   Ensures that the tree model is consistent. Starting
+///   from Score each element in the tree should have the
+///   correct parent element, the one whose children list
+///   it appears in.
+//---------------------------------------------------------
+
+class TestTreeModel : public QObject, public MTest
+{
+    Q_OBJECT
+
+    MasterScore* score;
+    void beam(const char* path);
+    void tstTree(QString file);
+    void traverseTree(ScoreElement* element);
+
+    QString elementToText(ScoreElement* element);
+
+private slots:
+    void initTestCase();
+    void tstTreeElements()  { tstTree("layout_elements.mscx");     }
+    void tstTreeTablature() { tstTree("layout_elements_tab.mscx"); }
+    void tstTreeMoonlight() { tstTree("moonlight.mscx");           }
+    void tstTreeGoldberg()  { tstTree("goldberg.mscx");            }
+};
+
+//---------------------------------------------------------
+//   initTestCase
+//---------------------------------------------------------
+
+void TestTreeModel::initTestCase()
+{
+    initMTest();
+}
+
+//---------------------------------------------------------
+//   tstTree
+//---------------------------------------------------------
+
+void TestTreeModel::tstTree(QString file)
+{
+    MasterScore* score = readScore(DIR + file);
+    traverseTree(score);
+}
+
+//---------------------------------------------------------
+//   traverseTree
+///   Checks whether parent element of current element is
+///   correct, then recursively checks all children.
+//---------------------------------------------------------
+
+void TestTreeModel::traverseTree(ScoreElement* element)
+{
+    for (ScoreElement* child : (*element)) {
+        if (!child) {
+            continue;
+        }
+    
+        // if parent is not correct print some logging info and exit
+        if (child->treeParent() != element) {
+            qDebug() << "Element does not have correct parent!";
+            qDebug() << "Element name: " << elementToText(child);
+            qDebug() << "Parent in tree model: " << elementToText(child->treeParent());
+            qDebug() << "Expected parent: " << elementToText(element);
+        }
+        QCOMPARE(child->treeParent(), element);
+
+        // recursively apply to the rest of the tree
+        traverseTree(child);
+    }
+}
+
+//---------------------------------------------------------
+//   elementToText
+///   for printing debug info about any element
+//---------------------------------------------------------
+
+QString TestTreeModel::elementToText(ScoreElement* element)
+{
+    if (element == nullptr) {
+        return "nullptr";
+    }
+    if (element->isElement()) {
+        return toElement(element)->accessibleInfo();
+    }
+    return element->userName();
+}
+
+QTEST_MAIN(TestTreeModel)
+#include "tst_tree_model.moc"
