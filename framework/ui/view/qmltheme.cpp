ad8e060805f05e5e9cd5916445dd7b207d72dd2c
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-06-29 13:23:47 +0200

    Altered color themes accordingly to design

--- framework/ui/view/qmltheme.cpp
+++ framework/ui/view/qmltheme.cpp
@@ -26,33 +26,47 @@
 using namespace mu::framework;
 
 static const QHash<int, QVariant> DARK_THEME {
-    { QmlTheme::BACKGROUND_COLOR, "#2D2D30" },
-    { QmlTheme::POPUP_BACKGROUND_COLOR, "#2D2D30" },
-    { QmlTheme::HIGHLIGHT_COLOR, "0062C2" },
-    { QmlTheme::STROKE_COLOR, "#25000000" },
-    { QmlTheme::BUTTON_COLOR_NORMAL, "#484848" },
-    { QmlTheme::BUTTON_COLOR_HOVER, "#C0C0C0" },
-    { QmlTheme::BUTTON_COLOR_HIT, "#ADADAD" },
-    { QmlTheme::FONT_COLOR, "#EBEBEB" }
+    { QmlTheme::BACKGROUND_COLOR, "#262626" },
+    { QmlTheme::POPUP_BACKGROUND_COLOR, "#2C2C2C" },
+    { QmlTheme::TEXT_FIELD_COLOR, "#1A1A1A" },
+    { QmlTheme::ACCENT_COLOR, "#0062C2" },
+    { QmlTheme::STROKE_COLOR, "#747474" },
+    { QmlTheme::BUTTON_COLOR, "#595959" },
+    { QmlTheme::FONT_COLOR, "#EBEBEB" },
+
+    { QmlTheme::ACCENT_OPACITY_NORMAL, 0.8 },
+    { QmlTheme::ACCENT_OPACITY_HOVER, 1.0 },
+    { QmlTheme::ACCENT_OPACITY_HIT, 0.5 },
+
+    { QmlTheme::BUTTON_OPACITY_NORMAL, 0.8 },
+    { QmlTheme::BUTTON_OPACITY_HOVER, 1.0 },
+    { QmlTheme::BUTTON_OPACITY_HIT, 0.5 },
 };
 
 static const QHash<int, QVariant> LIGHT_THEME {
     { QmlTheme::BACKGROUND_COLOR, "#E3E3E3" },
     { QmlTheme::POPUP_BACKGROUND_COLOR, "#E3E3E3" },
-    { QmlTheme::HIGHLIGHT_COLOR, "0062C2" },
-    { QmlTheme::STROKE_COLOR, "#25000000" },
-    { QmlTheme::BUTTON_COLOR_NORMAL, "#CECECE" },
-    { QmlTheme::BUTTON_COLOR_HOVER, "#C0C0C0" },
-    { QmlTheme::BUTTON_COLOR_HIT, "#ADADAD" },
-    { QmlTheme::FONT_COLOR, "#000000" }
+    { QmlTheme::TEXT_FIELD_COLOR, "#FFFFFF" },
+    { QmlTheme::ACCENT_COLOR, "#0062C2" },
+    { QmlTheme::STROKE_COLOR, "#D1D1D1" },
+    { QmlTheme::BUTTON_COLOR, "#BDBDBD" },
+    { QmlTheme::FONT_COLOR, "#000000" },
+
+    { QmlTheme::ACCENT_OPACITY_NORMAL, 0.3 },
+    { QmlTheme::ACCENT_OPACITY_HOVER, 0.15 },
+    { QmlTheme::ACCENT_OPACITY_HIT, 0.5 },
+
+    { QmlTheme::BUTTON_OPACITY_NORMAL, 0.7 },
+    { QmlTheme::BUTTON_OPACITY_HOVER, 0.5 },
+    { QmlTheme::BUTTON_OPACITY_HIT, 1.0 },
 };
 
 QmlTheme::QmlTheme(QObject* parent) :
     QObject(parent)
 {
     QSettings settings;
-    m_font.setFamily(settings.value(PREF_UI_THEME_FONTFAMILY).toString());
-    m_font.setPointSize(settings.value(PREF_UI_THEME_FONTSIZE).toInt());
+    m_font.setFamily(settings.value(PREF_UI_THEME_FONTFAMILY, "FreeSans").toString());
+    m_font.setPointSize(settings.value(PREF_UI_THEME_FONTSIZE, 12).toInt());
 }
 
 void QmlTheme::update()
@@ -62,46 +76,71 @@ void QmlTheme::update()
 
 QColor QmlTheme::backgroundColor() const
 {
-    return LIGHT_THEME.value(BACKGROUND_COLOR).toString();
+    return DARK_THEME.value(BACKGROUND_COLOR).toString();
 }
 
 QColor QmlTheme::popupBackgroundColor() const
 {
-    return LIGHT_THEME.value(POPUP_BACKGROUND_COLOR).toString();
+    return DARK_THEME.value(POPUP_BACKGROUND_COLOR).toString();
 }
 
-QColor QmlTheme::highlightColor() const
+QColor QmlTheme::textFieldColor() const
 {
-    return LIGHT_THEME.value(HIGHLIGHT_COLOR).toString();
+    return DARK_THEME.value(TEXT_FIELD_COLOR).toString();
+}
+
+QColor QmlTheme::accentColor() const
+{
+    return DARK_THEME.value(ACCENT_COLOR).toString();
 }
 
 QColor QmlTheme::strokeColor() const
 {
-    return LIGHT_THEME.value(STROKE_COLOR).toString();
+    return DARK_THEME.value(STROKE_COLOR).toString();
 }
 
-QColor QmlTheme::buttonColorNormal() const
+QColor QmlTheme::buttonColor() const
 {
-    return LIGHT_THEME.value(BUTTON_COLOR_NORMAL).toString();
-}
-
-QColor QmlTheme::buttonColorHover() const
-{
-    return LIGHT_THEME.value(BUTTON_COLOR_HOVER).toString();
-}
-
-QColor QmlTheme::buttonColorHit() const
-{
-    return LIGHT_THEME.value(BUTTON_COLOR_HIT).toString();
+    return DARK_THEME.value(BUTTON_COLOR).toString();
 }
 
 QColor QmlTheme::fontColor() const
 {
-    return LIGHT_THEME.value(FONT_COLOR).toString();
+    return DARK_THEME.value(FONT_COLOR).toString();
 }
 
 QFont QmlTheme::font() const
 {
     return m_font;
+}
+
+qreal QmlTheme::accentOpacityNormal() const
+{
+    return DARK_THEME.value(ACCENT_OPACITY_NORMAL).toReal();
+}
+
+qreal QmlTheme::accentOpacityHover() const
+{
+    return DARK_THEME.value(ACCENT_OPACITY_HOVER).toReal();
+}
+
+qreal QmlTheme::accentOpacityHit() const
+{
+    return DARK_THEME.value(ACCENT_OPACITY_HIT).toReal();
+}
+
+qreal QmlTheme::buttonOpacityNormal() const
+{
+    return DARK_THEME.value(BUTTON_OPACITY_NORMAL).toReal();
+}
+
+qreal QmlTheme::buttonOpacityHover() const
+{
+    return DARK_THEME.value(BUTTON_OPACITY_HOVER).toReal();
+}
+
+qreal QmlTheme::buttonOpacityHit() const
+{
+    return DARK_THEME.value(BUTTON_OPACITY_HIT).toReal();
 }
 
