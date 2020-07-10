d6d37c3c151742e960598a00630d41f8c378cc9f
anatoly-os <osokingauss@gmail.com>
2020-07-10 12:11:05 +0300

    stop pushing strings to Transifex

--- build/travis/job_macos_lupdate/script.sh
+++ build/travis/job_macos_lupdate/script.sh
@@ -22,17 +22,3 @@ lupdate ${OBSOLETE} tours.pro
 rm mscore.pro
 rm instruments.pro
 rm tours.pro
-
-sudo pip install transifex-client
-
-cat > ~/.transifexrc <<EOL
-[https://www.transifex.com]
-hostname = https://www.transifex.com
-password = $TRANSIFEX_PASSWORD
-token =
-username = $TRANSIFEX_USER
-EOL
-
-cp share/locale/mscore_en_US.ts share/locale/mscore_en.ts
-
-tx push -s
