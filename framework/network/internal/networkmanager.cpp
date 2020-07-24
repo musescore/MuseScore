7a9925ac71c243ac4bce5ab1e7047934a3007e9b
Eism <eismailzada@gmail.com>
2020-07-24 10:05:13 +0200

    Added network module

---
+++ framework/network/internal/networkmanager.cpp
@@ -0,0 +1,6 @@
+#include "networkmanager.h"
+
+#include "networkerrors.h"
+
+using namespace mu;
+using namespace mu::framework;
---
+++ framework/network/networkerrors.h
@@ -0,0 +1,48 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2020 MuseScore BVBA and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+#ifndef MU_FRAMEWORK_NETWORKERRORS_H
+#define MU_FRAMEWORK_NETWORKERRORS_H
+
+#include "ret.h"
+#include "translation.h"
+
+namespace mu {
+namespace framework {
+enum class Err {
+    Undefined       = int(Ret::Code::Undefined),
+    NoError         = int(Ret::Code::Ok),
+    UnknownError    = int(Ret::Code::NetworkFirst),
+};
+
+inline Ret make_ret(Err e)
+{
+    int retCode = static_cast<int>(e);
+
+    switch (e) {
+    case Err::Undefined: return Ret(retCode);
+    case Err::NoError: return Ret(retCode);
+    case Err::UnknownError: return Ret(retCode);
+    }
+
+    return Ret(static_cast<int>(e));
+}
+}
+}
+
+#endif // MU_FRAMEWORK_NETWORKERRORS_H
