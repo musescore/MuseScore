3e501d99cdbffc1c277c3e5d9f9b11d23daf2b90
Roman Pudashkin <r.pudashkin@gmail.com>
2020-07-28 15:12:03 +0200

    implemented --score-parts flag

--- mscore/musescore.h
+++ mscore/musescore.h
@@ -552,6 +552,7 @@ class MuseScore : public QMainWindow, public MuseScoreCore {
 
       virtual QMenu* createPopupMenu() override;
 
+      QByteArray exportMsczAsJSON(Score*);
       QByteArray exportPdfAsJSON(Score*);
 
    public slots:
@@ -748,6 +749,7 @@ class MuseScore : public QMainWindow, public MuseScoreCore {
       bool exportAllMediaFiles(const QString& inFilePath, const QString& outFilePath = "/dev/stdout");
       bool exportScoreMetadata(const QString& inFilePath, const QString& outFilePath = "/dev/stdout");
       bool exportMp3AsJSON(const QString& inFilePath, const QString& outFilePath = "/dev/stdout");
+      bool saveScoreParts(const QString& inFilePath, const QString& outFilePath = "/dev/stdout");
       bool exportPartsPdfsToJSON(const QString& inFilePath, const QString& outFilePath = "/dev/stdout");
       bool exportTransposedScoreToJSON(const QString& inFilePath, const QString& transposeOptions, const QString& outFilePath = "/dev/stdout");
       /////////////////////////////////////////////////
