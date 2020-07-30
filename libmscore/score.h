f07432f8a7595393bee60ca4656696789e8194a4
anatoly-os <osokingauss@gmail.com>
2020-07-30 15:01:35 +0300

    Manually merged #6085 to master

--- libmscore/score.h
+++ libmscore/score.h
@@ -1089,6 +1089,7 @@ public:
     Element* selectMove(const QString& cmd);
     Element* move(const QString& cmd);
     void cmdEnterRest(const TDuration& d);
+    void enterRest(const TDuration& d, InputState* externalInputState = nullptr);
     void cmdAddInterval(int, const std::vector<Note*>&);
     void cmdCreateTuplet(ChordRest*, Tuplet*);
     void removeAudio();
