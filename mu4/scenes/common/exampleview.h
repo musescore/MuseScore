c892edc0999f57edff6155c041836e3e165e544c
Igor Korsukov <igor.korsukov@gmail.com>
2020-07-30 15:44:00 +0200

    redesigned midi data

--- mu4/scenes/common/exampleview.h
+++ mu4/scenes/common/exampleview.h
@@ -66,7 +66,7 @@ public:
     virtual void layoutChanged();
     virtual void dataChanged(const QRectF&);
     virtual void updateAll();
-    virtual void adjustCanvasPosition(const Element* el, bool playBack);
+    virtual void adjustCanvasPosition(const Element* el, bool playBack, int staff = -1);
     virtual void setScore(Score*);
     virtual void removeScore();
 
