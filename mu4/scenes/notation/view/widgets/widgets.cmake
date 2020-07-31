bc8c56bcb9507a97969f4a0d8ae9530a43ccd952
pereverzev_v <v.pereverzev@wsmgroup.ru>
2020-07-31 15:18:03 +0200

    Made a copy of "MeasurePropertiesDialog" for MU4 with its own implementation

---
+++ mu4/scenes/notation/view/widgets/widgets.cmake
@@ -0,0 +1,9 @@
+
+set (WIDGETS_UI
+    ${CMAKE_CURRENT_LIST_DIR}/measureproperties.ui
+    )
+
+set(WIDGETS_SRC
+    ${CMAKE_CURRENT_LIST_DIR}/measureproperties.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/measureproperties.h
+    )
