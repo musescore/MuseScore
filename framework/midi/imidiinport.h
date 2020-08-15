a9f0e9f625e4e98c719ba3868326c5bb6129d863
Igor Korsukov <igor.korsukov@gmail.com>
2020-08-15 15:06:29 +0200

    added alsa midi input port

---
+++ framework/midi/imidiinport.h
@@ -0,0 +1,40 @@
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2020 MuseScore BVBA and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+#ifndef MU_MIDI_IMIDIINPORT_H
+#define MU_MIDI_IMIDIINPORT_H
+
+#include "modularity/ioc.h"
+
+namespace mu {
+namespace midi {
+class IMidiInPort : MODULE_EXPORT_INTERFACE
+{
+    INTERFACE_ID(IMidiInPort)
+public:
+    virtual ~IMidiInPort() = default;
+
+    struct Device {
+        std::string
+    };
+
+};
+}
+}
+
+#endif // MU_MIDI_IMIDIINPORT_H
