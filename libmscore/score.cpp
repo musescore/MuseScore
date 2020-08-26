2ffa5eda30239a0dd3820c20d79e913cee44996c
anatoly-os <osokingauss@gmail.com>
2020-08-27 02:20:40 +0300

    Manually merged #6453 to master

--- libmscore/score.cpp
+++ libmscore/score.cpp
@@ -800,6 +800,7 @@ void MasterScore::setPlaylistDirty()
 {
     _playlistDirty = true;
     _repeatList->setScoreChanged();
+    _repeatList2->setScoreChanged();
 }
 
 //---------------------------------------------------------
@@ -1925,6 +1926,7 @@ void MasterScore::setExpandRepeats(bool expand)
 void MasterScore::updateRepeatListTempo()
 {
     _repeatList->updateTempo();
+    _repeatList2->updateTempo();
 }
 
 //---------------------------------------------------------
@@ -1935,6 +1937,16 @@ const RepeatList& MasterScore::repeatList() const
 {
     _repeatList->update(_expandRepeats);
     return *_repeatList;
+}
+
+//---------------------------------------------------------
+//   repeatList2
+//---------------------------------------------------------
+
+const RepeatList& MasterScore::repeatList2() const
+{
+    _repeatList2->update(false);
+    return *_repeatList2;
 }
 
 //---------------------------------------------------------
@@ -4512,8 +4524,7 @@ int Score::duration()
 
 int Score::durationWithoutRepeats()
 {
-    masterScore()->setExpandRepeats(false);
-    const RepeatList& rl = repeatList();
+    const RepeatList& rl = repeatList2();
     if (rl.empty()) {
         return 0;
     }
@@ -4878,6 +4889,7 @@ MasterScore::MasterScore()
     _tempomap    = new TempoMap;
     _sigmap      = new TimeSigMap();
     _repeatList  = new RepeatList(this);
+    _repeatList2 = new RepeatList(this);
     _revisions   = new Revisions;
     setMasterScore(this);
 
@@ -4920,6 +4932,7 @@ MasterScore::~MasterScore()
 {
     delete _revisions;
     delete _repeatList;
+    delete _repeatList2;
     delete _sigmap;
     delete _tempomap;
     qDeleteAll(_excerpts);
--- libmscore/score.h
+++ libmscore/score.h
@@ -1042,6 +1042,7 @@ public:
     void adjustKeySigs(int sidx, int eidx, KeyList km);
 
     virtual inline const RepeatList& repeatList() const;
+    virtual inline const RepeatList& repeatList2() const;
     qreal utick2utime(int tick) const;
     int utime2utick(qreal utime) const;
 
@@ -1318,6 +1319,7 @@ class MasterScore : public Score
     TimeSigMap * _sigmap;
     TempoMap* _tempomap;
     RepeatList* _repeatList;
+    RepeatList* _repeatList2;
     bool _expandRepeats     { MScore::playRepeats };
     bool _playlistDirty     { true };
     QList<Excerpt*> _excerpts;
@@ -1381,6 +1383,7 @@ public:
     void setExpandRepeats(bool expandRepeats);
     void updateRepeatListTempo();
     virtual const RepeatList& repeatList() const override;
+    virtual const RepeatList& repeatList2() const override;
 
     virtual QList<Excerpt*>& excerpts() override { return _excerpts; }
     virtual const QList<Excerpt*>& excerpts() const override { return _excerpts; }
@@ -1502,6 +1505,7 @@ public:
 
 inline UndoStack* Score::undoStack() const { return _masterScore->undoStack(); }
 inline const RepeatList& Score::repeatList()  const { return _masterScore->repeatList(); }
+inline const RepeatList& Score::repeatList2()  const { return _masterScore->repeatList2(); }
 inline TempoMap* Score::tempomap() const { return _masterScore->tempomap(); }
 inline TimeSigMap* Score::sigmap() const { return _masterScore->sigmap(); }
 inline QList<Excerpt*>& Score::excerpts() { return _masterScore->excerpts(); }
