fac738223061fdb051df73e2e59a808837a7cfbe
Eism <eismailzada@gmail.com>
2020-08-31 12:17:34 +0200

    Implemented staff item

--- mu4/instruments/view/instrumenttreeitem.h
+++ mu4/instruments/view/instrumenttreeitem.h
@@ -1,5 +1,23 @@
-#ifndef INSTRUMENTTREEITEM_H
-#define INSTRUMENTTREEITEM_H
+//=============================================================================
+//  MuseScore
+//  Music Composition & Notation
+//
+//  Copyright (C) 2020 MuseScore BVBA and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+#ifndef MU_INSTRUMENTS_INSTRUMENTTREEITEM_H
+#define MU_INSTRUMENTS_INSTRUMENTTREEITEM_H
 
 #include "abstractinstrumentpaneltreeitem.h"
 
@@ -7,17 +25,17 @@
 
 namespace mu {
 namespace instruments {
-
 class InstrumentTreeItem : public AbstractInstrumentPanelTreeItem
 {
     Q_OBJECT
 public:
     explicit InstrumentTreeItem(notation::INotationParts* notationParts, QObject* parent = nullptr);
 
-    void moveChildren(const int sourceRow, const int count, AbstractInstrumentPanelTreeItem* destinationParent, const int destinationRow) override;
+    void moveChildren(const int sourceRow, const int count, AbstractInstrumentPanelTreeItem* destinationParent,
+                      const int destinationRow) override;
     void removeChildren(const int row, const int count, const bool deleteChild) override;
 };
 }
 }
 
-#endif // INSTRUMENTTREEITEM_H
+#endif // MU_INSTRUMENTS_INSTRUMENTTREEITEM_H
