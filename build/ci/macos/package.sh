5f621612c9b437fac75e55ceb7fa83a6c1235ab7
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-02 11:38:15 +0200

    added sign and notarize for macos build

--- build/ci/macos/package.sh
+++ build/ci/macos/package.sh
@@ -3,6 +3,23 @@
 echo "Package MuseScore"
 
 ARTIFACTS_DIR="build.artifacts"
+SIGN_CERTIFICATE_ENCRYPT_SECRET=""
+SIGN_CERTIFICATE_PASSWORD=""
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --signsecret) SIGN_CERTIFICATE_ENCRYPT_SECRET="$2"; shift ;;
+        --signpass) SIGN_CERTIFICATE_PASSWORD="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ -z "$SIGN_CERTIFICATE_ENCRYPT_SECRET" ]; then echo "warning: not set SIGN_CERTIFICATE_ENCRYPT_SECRET"; fi
+if [ -z "$SIGN_CERTIFICATE_PASSWORD" ]; then echo "warning: not set SIGN_CERTIFICATE_PASSWORD"; fi
+
+echo "SIGN_CERTIFICATE_ENCRYPT_SECRET: $SIGN_CERTIFICATE_ENCRYPT_SECRET"
+echo "SIGN_CERTIFICATE_PASSWORD: $SIGN_CERTIFICATE_PASSWORD"
 
 mkdir -p applebuild/mscore.app/Contents/Resources/Frameworks
 wget -c --no-check-certificate -nv -O musescore_dependencies_macos.zip  http://utils.musescore.org.s3.amazonaws.com/musescore_dependencies_macos.zip
@@ -11,6 +28,24 @@ unzip musescore_dependencies_macos.zip -d applebuild/mscore.app/Contents/Resourc
 # install Sparkle
 mkdir -p applebuild/mscore.app/Contents/Frameworks
 cp -Rf ~/Library/Frameworks/Sparkle.framework applebuild/mscore.app/Contents/Frameworks
+
+# Setup keychain for code sign
+if [ -n "$SIGN_CERTIFICATE_ENCRYPT_SECRET" ]; then 
+
+    7z x -y ./build/ci/macos/resources/mac_musescore.p12.enc -o./build/ci/macos/resources/ -p${SIGN_CERTIFICATE_ENCRYPT_SECRET}
+
+    export CERTIFICATE_P12=./build/ci/macos/resources/mac_musescore.p12
+    export KEYCHAIN=build.keychain
+    security create-keychain -p ci $KEYCHAIN
+    security default-keychain -s $KEYCHAIN
+    security unlock-keychain -p ci $KEYCHAIN
+    # Set keychain timeout to 1 hour for long builds
+    # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+    security set-keychain-settings -t 3600 -l $KEYCHAIN
+    security import $CERTIFICATE_P12 -k $KEYCHAIN -P "$SIGN_CERTIFICATE_PASSWORD" -T /usr/bin/codesign
+
+    security set-key-partition-list -S apple-tool:,apple: -s -k ci $KEYCHAIN
+fi
 
 build/package_mac
 
