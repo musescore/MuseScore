e498f781d1648f2dce4911f03bae9fce0ea647cb
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-09 13:23:46 +0200

    sync CI from 3.x to master

--- build/ci/macos/build.sh
+++ build/ci/macos/build.sh
@@ -18,11 +18,20 @@ done
 if [ -z "$BUILD_NUMBER" ]; then echo "error: not set BUILD_NUMBER"; exit 1; fi
 if [ -z "$TELEMETRY_TRACK_ID" ]; then TELEMETRY_TRACK_ID=""; fi
 
+BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env)
+MUSESCORE_BUILD_CONFIG=dev
+if [ "$BUILD_MODE" == "devel_build" ]; then MUSESCORE_BUILD_CONFIG=dev; fi
+if [ "$BUILD_MODE" == "nightly_build" ]; then MUSESCORE_BUILD_CONFIG=dev; fi
+if [ "$BUILD_MODE" == "testing_build" ]; then MUSESCORE_BUILD_CONFIG=testing; fi
+if [ "$BUILD_MODE" == "stable_build" ]; then MUSESCORE_BUILD_CONFIG=release; fi
+
+echo "MUSESCORE_BUILD_CONFIG: $MUSESCORE_BUILD_CONFIG"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
 echo "TELEMETRY_TRACK_ID: $TELEMETRY_TRACK_ID"
 echo "BUILD_UI_MU4: $BUILD_UI_MU4"
 
-make -f Makefile.osx ci BUILD_NUMBER=$BUILD_NUMBER TELEMETRY_TRACK_ID=$TELEMETRY_TRACK_ID BUILD_UI_MU4=${BUILD_UI_MU4}
+make -f Makefile.osx ci MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG BUILD_NUMBER=$BUILD_NUMBER TELEMETRY_TRACK_ID=$TELEMETRY_TRACK_ID BUILD_UI_MU4=${BUILD_UI_MU4}
+
 
 bash ./build/ci/tools/make_release_channel_env.sh 
 bash ./build/ci/tools/make_version_env.sh $BUILD_NUMBER
