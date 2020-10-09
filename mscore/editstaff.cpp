70f74dd10cc0a6af0b6882d159ac2810586dea9b
Joachim Schmitz <jojo@schmitz-digital.de>
2020-10-09 16:07:05 +0200

    fixing a compiler warning seen on Mac builds

--- mscore/editstaff.cpp
+++ mscore/editstaff.cpp
@@ -387,7 +387,7 @@ void EditStaff::apply()
                         score->undo(new ChangePart(part, new Instrument(*part->instrument()), newPartName));
                   if (instrumentFieldChanged) {
                         Segment* s = score->tick2segment(_tickStart, true, SegmentType::ChordRest);
-                        const std::vector<Element*> elist = s ? s->findAnnotations(ElementType::INSTRUMENT_CHANGE, part->startTrack(), part->endTrack()) : elist;
+                        const std::vector<Element*> elist = s ? s->findAnnotations(ElementType::INSTRUMENT_CHANGE, part->startTrack(), part->endTrack()) : std::vector<Element*>();
                         if (elist.size())
                               for (Element* e : elist) // Change instrument in all Instrument Changes (for linked staves)
                                     score->undo(new ChangeInstrument(toInstrumentChange(e), new Instrument(instrument)));
