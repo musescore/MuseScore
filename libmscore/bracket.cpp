2b3fd83be67f439ace4442a1c87f2c5b09d8bde9
Niek van den Berg <njvdberg@xs4all.nl>
2020-10-12 09:37:36 +0200

    Fix #311084 - Wrong curly bracket scaling when using MuseJazz

--- libmscore/bracket.cpp
+++ libmscore/bracket.cpp
@@ -247,11 +247,11 @@ void Bracket::draw(QPainter* painter) const
             painter->drawPath(path);
         } else {
             qreal h        = 2 * h2;
-            qreal mag      = h / 100;
+            qreal mag      = h / (100 * magS());
             painter->setPen(curColor());
             painter->save();
             painter->scale(_magx, mag);
-            drawSymbol(_braceSymbol, painter, QPointF(0, 100));
+            drawSymbol(_braceSymbol, painter, QPointF(0, 100 * magS()));
             painter->restore();
         }
     }
