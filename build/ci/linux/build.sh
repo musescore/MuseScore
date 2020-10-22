ea2fa7c82a60aac65dde5c61919010cc6221745f
AntonioBL <antonio.lotti@alice.it>
2020-10-22 16:29:39 +0200

    Separate mtests job from build job

--- build/ci/linux/build.sh
+++ build/ci/linux/build.sh
@@ -7,11 +7,18 @@ trap 'echo Build failed; exit 1' ERR
 
 TELEMETRY_TRACK_ID=""
 ARTIFACTS_DIR=build.artifacts
+# portable build is the default build
+BUILDTYPE=portable
+OPTIONS=""
+
+BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env)
+MUSESCORE_BUILD_CONFIG=dev
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -n|--number) BUILD_NUMBER="$2"; shift ;;
         --telemetry) TELEMETRY_TRACK_ID="$2"; shift ;;
+        --build_mode) BUILD_MODE="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -20,16 +27,17 @@ done
 if [ -z "$BUILD_NUMBER" ]; then echo "error: not set BUILD_NUMBER"; exit 1; fi
 if [ -z "$TELEMETRY_TRACK_ID" ]; then TELEMETRY_TRACK_ID=""; fi
 
-BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env)
-MUSESCORE_BUILD_CONFIG=dev
 if [ "$BUILD_MODE" == "devel_build" ]; then MUSESCORE_BUILD_CONFIG=dev; fi
 if [ "$BUILD_MODE" == "nightly_build" ]; then MUSESCORE_BUILD_CONFIG=dev; fi
 if [ "$BUILD_MODE" == "testing_build" ]; then MUSESCORE_BUILD_CONFIG=testing; fi
 if [ "$BUILD_MODE" == "stable_build" ]; then MUSESCORE_BUILD_CONFIG=release; fi
+if [ "$BUILD_MODE" == "mtests" ]; then MUSESCORE_BUILD_CONFIG=dev; BUILDTYPE=installdebug; OPTIONS="USE_SYSTEM_FREETYPE=ON UPDATE_CACHE=FALSE PREFIX=$ARTIFACTS_DIR/software"; fi
 
 echo "MUSESCORE_BUILD_CONFIG: $MUSESCORE_BUILD_CONFIG"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
 echo "TELEMETRY_TRACK_ID: $TELEMETRY_TRACK_ID"
+echo "BUILDTYPE: $BUILDTYPE"
+echo "OPTIONS: $OPTIONS"
 
 echo "=== ENVIRONMENT === "
 
@@ -47,7 +55,7 @@ echo " "
 echo "=== BUILD === "
 
 make revision
-make -j2 MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG BUILD_NUMBER=$BUILD_NUMBER TELEMETRY_TRACK_ID=$TELEMETRY_TRACK_ID portable
+make -j2 $OPTIONS MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG BUILD_NUMBER=$BUILD_NUMBER TELEMETRY_TRACK_ID=$TELEMETRY_TRACK_ID $BUILDTYPE
 
 
 bash ./build/ci/tools/make_release_channel_env.sh -c $MUSESCORE_BUILD_CONFIG
