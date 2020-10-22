6e3d38a17c6c93e63e80820add75432d21bdaef3
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-22 15:27:00 +0200

    added tx2s3 workflow

---
+++ build/ci/tx2s3/tx2s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ENV_FILE=./../musescore_tx2s3_environment.sh
+source $ENV_FILE
+
+command -v lrelease >/dev/null 2>&1 || { echo "error: not found 'lrelease'" >&2; exit 1; }
+command -v tx >/dev/null 2>&1 || { echo "error: not found 'tx'" >&2; exit 1; }
+command -v s3cmd >/dev/null 2>&1 || { echo "error: not found 's3cmd'" >&2; exit 1; }
+
+echo "lrelease: $(lrelease -version)"
+echo "tx: $(tx --version)"
+echo "s3cmd: $(s3cmd --version)"
+
+echo "Updating translation on s3..."
+SCRIPT_PATH=$(dirname $0)
+python3 $SCRIPT_PATH/tx2s3.py
+echo "Translation updated"
