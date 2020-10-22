9062b1a86bd0331a6691486b844f6d830434da49
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-22 10:59:06 +0200

    added tx2s3 workflow

--- build/ci/lupdate/publish_to_tx.sh
+++ build/ci/lupdate/publish_to_tx.sh
@@ -15,6 +15,7 @@ done
 if [ -z "$TRANSIFEX_USER" ]; then echo "error: not set TRANSIFEX_USER"; exit 1; fi
 if [ -z "$TRANSIFEX_PASSWORD" ]; then echo "error: not set TRANSIFEX_PASSWORD"; exit 1; fi
 
+apt install python3-setuptools
 pip3 install transifex-client
 
 cat > ~/.transifexrc <<EOL
@@ -26,4 +27,4 @@ username = $TRANSIFEX_USER
 EOL
 
 echo "tx version: $(tx --version)"
-tx push -s
+#tx push -s
