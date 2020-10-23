0c03fb24c26c2f9cb3694c1c4ce467983addf791
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-23 13:07:06 +0200

    resctructured linux package script for CI

--- build/ci/linux/package.sh
+++ build/ci/linux/package.sh
@@ -3,232 +3,56 @@
 echo "Package MuseScore"
 trap 'echo Package failed; exit 1' ERR
 
-ARTIFACTS_DIR="build.artifacts"
-
-echo "=== ENVIRONMENT === "
-
-ENV_FILE=./../musescore_environment.sh
-cat ${ENV_FILE}
-. ${ENV_FILE}
-
-echo " "
-appimagetool --version
-echo " "
-linuxdeploy --list-plugins
-echo "===================="
-
-
-##########################################################################
-# BUNDLE DEPENDENCIES INTO APPDIR
-##########################################################################
-
-prefix="$(cat build.release/PREFIX.txt)" # MuseScore was installed here
-cd "$(dirname "${prefix}")"
-appdir="$(basename "${prefix}")" # directory that will become the AppImage
-
-# Prevent linuxdeploy setting RUNPATH in binaries that shouldn't have it
-mv "${appdir}/bin/findlib" "${appdir}/../findlib"
-
-# Remove Qt plugins for MySQL and PostgreSQL to prevent
-# linuxdeploy-plugin-qt from failing due to missing dependencies.
-# SQLite plugin alone should be enough for our AppImage.
-# rm -f ${QT_PATH}/plugins/sqldrivers/libqsql{mysql,psql}.so
-qt_sql_drivers_path="${QT_PATH}/plugins/sqldrivers"
-qt_sql_drivers_tmp="/tmp/qtsqldrivers"
-mkdir -p "$qt_sql_drivers_tmp"
-mv "${qt_sql_drivers_path}/libqsqlmysql.so" "${qt_sql_drivers_tmp}/libqsqlmysql.so"
-mv "${qt_sql_drivers_path}/libqsqlpsql.so" "${qt_sql_drivers_tmp}/libqsqlpsql.so"
-
-# Colon-separated list of root directories containing QML files.
-# Needed for linuxdeploy-plugin-qt to scan for QML imports.
-# Qml files can be in different directories, the qmlimportscanner will go through everything recursively.
-export QML_SOURCES_PATHS=./
-
-linuxdeploy --appdir "${appdir}" # adds all shared library dependencies
-linuxdeploy-plugin-qt --appdir "${appdir}" # adds all Qt dependencies
-
-unset QML_SOURCES_PATHS
-
-# In case this container is reused multiple times, return the moved libraries back
-mv "${qt_sql_drivers_tmp}/libqsqlmysql.so" "${qt_sql_drivers_path}/libqsqlmysql.so"
-mv "${qt_sql_drivers_tmp}/libqsqlpsql.so" "${qt_sql_drivers_path}/libqsqlpsql.so"
-
-# Put the non-RUNPATH binaries back
-mv "${appdir}/../findlib" "${appdir}/bin/findlib"
-
-##########################################################################
-# BUNDLE REMAINING DEPENDENCIES MANUALLY
-##########################################################################
-
-function find_library()
-{
-  # Print full path to a library or return exit status 1 if not found
-  "${appdir}/bin/findlib" "$@"
-}
-
-function fallback_library()
-{
-  # Copy a library into a special fallback directory inside the AppDir.
-  # Fallback libraries are not loaded at runtime by default, but they can
-  # be loaded if it is found that the application would crash otherwise.
-  local library="$1"
-  local full_path="$(find_library "$1")"
-  local new_path="${appdir}/fallback/${library}"
-  mkdir -p "${new_path}" # directory has the same name as the library
-  cp -L "${full_path}" "${new_path}/${library}"
-  # Use the AppRun script to check at runtime whether the user has a copy of
-  # this library. If not then add our copy's directory to $LD_LIBRARY_PATH.
-}
-
-# UNWANTED FILES
-# linuxdeploy or linuxdeploy-plugin-qt may have added some files or folders
-# that we don't want. List them here using paths relative to AppDir root.
-# Report new additions at https://github.com/linuxdeploy/linuxdeploy/issues
-# or https://github.com/linuxdeploy/linuxdeploy-plugin-qt/issues for Qt libs.
-unwanted_files=(
-  # none
-)
-
-# ADDITIONAL QT COMPONENTS
-# linuxdeploy-plugin-qt may have missed some Qt files or folders that we need.
-# List them here using paths relative to the Qt root directory. Report new
-# additions at https://github.com/linuxdeploy/linuxdeploy-plugin-qt/issues
-additional_qt_components=(
-  /plugins/printsupport/libcupsprintersupport.so
-)
-
-# ADDITIONAL LIBRARIES
-# linuxdeploy may have missed some libraries that we need
-# Report new additions at https://github.com/linuxdeploy/linuxdeploy/issues
-additional_libraries=(
-  libssl.so.1.0.0    # OpenSSL (for Save Online)
-  libcrypto.so.1.0.0 # OpenSSL (for Save Online)
-)
-
-# FALLBACK LIBRARIES
-# These get bundled in the AppImage, but are only loaded if the user does not
-# already have a version of the library installed on their system. This is
-# helpful in cases where it is necessary to use a system library in order for
-# a particular feature to work properly, but where the program would crash at
-# startup if the library was not found. The fallback library may not provide
-# the full functionality of the system version, but it does avoid the crash.
-# Report new additions at https://github.com/linuxdeploy/linuxdeploy/issues
-fallback_libraries=(
-  libjack.so.0 # https://github.com/LMMS/lmms/pull/3958
-)
-
-for file in "${unwanted_files[@]}"; do
-  rm -rf "${appdir}/${file}"
-done
-
-for file in "${additional_qt_components[@]}"; do
-  mkdir -p "${appdir}/$(dirname "${file}")"
-  cp -L "${QT_PATH}/${file}" "${appdir}/${file}"
-done
-
-for lib in "${additional_libraries[@]}"; do
-  full_path="$(find_library "${lib}")"
-  cp -L "${full_path}" "${appdir}/lib/${lib}"
-done
-
-for fb_lib in "${fallback_libraries[@]}"; do
-  fallback_library "${fb_lib}"
-done
-
-# METHOD OF LAST RESORT
-# Special treatment for some dependencies when all other methods fail
-
-# Bundle libnss3 and friends as fallback libraries. Needed on Chromebook.
-# See discussion at https://github.com/probonopd/linuxdeployqt/issues/35
-libnss3_files=(
-  # https://packages.ubuntu.com/xenial/amd64/libnss3/filelist
-  libnss3.so
-  libnssutil3.so
-  libsmime3.so
-  libssl3.so
-  nss/libfreebl3.chk
-  nss/libfreebl3.so
-  nss/libfreeblpriv3.chk
-  nss/libfreeblpriv3.so
-  nss/libnssckbi.so
-  nss/libnssdbm3.chk
-  nss/libnssdbm3.so
-  nss/libsoftokn3.chk
-  nss/libsoftokn3.so
-)
-
-libnss3_system_path="$(dirname "$(find_library libnss3.so)")"
-libnss3_appdir_path="${appdir}/fallback/libnss3.so" # directory named like library
-
-mkdir -p "${libnss3_appdir_path}/nss"
-
-for file in "${libnss3_files[@]}"; do
-  cp -L "${libnss3_system_path}/${file}" "${libnss3_appdir_path}/${file}"
-  rm -f "${appdir}/lib/$(basename "${file}")" # in case it was already packaged by linuxdeploy
-done
-
-##########################################################################
-# TURN APPDIR INTO AN APPIMAGE
-##########################################################################
-
-appimage="${appdir%.AppDir}.AppImage" # name to use for AppImage file
-
-appimagetool_args=( # array
-  # none
-  )
-
-created_files=(
-  "${appimage}"
-  )
-
-if [[ "${UPDATE_INFORMATION}" ]]; then
-  appimagetool_args+=( # append to array
-    --updateinformation "${UPDATE_INFORMATION}"
-    )
-  created_files+=(
-    "${appimage}.zsync" # this file will contain delta update data
-    )
-else
-  cat >&2 <<EOF
-$0: Automatic updates disabled.
-To enable automatic updates, please set the env. variable UPDATE_INFORMATION
-according to <https://github.com/AppImage/AppImageSpec/blob/master/draft.md>.
-EOF
-fi
-
-# create AppImage
-appimagetool "${appimagetool_args[@]}" "${appdir}" "${appimage}"
-
-# We are running as root in the Docker image so all created files belong to
-# root. Allow non-root users outside the Docker image to access these files.
-chmod a+rwx "${created_files[@]}"
-parent_dir="${PWD}"
-while [[ "$(dirname "${parent_dir}")" != "${parent_dir}" ]]; do
-  [[ "$parent_dir" == "/" ]] && break
-  chmod a+rwx "$parent_dir"
-  parent_dir="$(dirname "$parent_dir")"
-done
-
+source ./../musescore_environment.sh
 
 ARTIFACTS_DIR=build.artifacts
+BUILD_MODE=""
+BUILD_DIR=build.release
+INSTALL_DIR="$(cat $BUILD_DIR/PREFIX.txt)" # MuseScore was installed here
 
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --build_mode) BUILD_MODE="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
-BUILD_MODE=$(cat ../$ARTIFACTS_DIR/env/build_mode.env)
+if [ -z "$BUILD_MODE" ]; then BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env); fi
+if [ -z "$BUILD_MODE" ]; then echo "error: not set BUILD_MODE"; exit 1; fi
+
+PACKTYPE=appimage
+if [ "$BUILD_MODE" == "devel_build" ]; then PACKTYPE=appimage; fi
+if [ "$BUILD_MODE" == "nightly_build" ]; then PACKTYPE=appimage; fi
+if [ "$BUILD_MODE" == "testing_build" ]; then PACKTYPE=appimage; fi
+if [ "$BUILD_MODE" == "stable_build" ]; then PACKTYPE=appimage; fi
+
+echo "BUILD_MODE: $BUILD_MODE"
+echo "PACKTYPE: $PACKTYPE"
+echo "INSTALL_DIR: $INSTALL_DIR"
 
 if [ "$BUILD_MODE" == "nightly_build" ]; then
-  BUILD_DATETIME=$(cat ../$ARTIFACTS_DIR/env/build_datetime.env)
-  BUILD_BRANCH=$(cat ../$ARTIFACTS_DIR/env/build_branch.env)
-  BUILD_REVISION=$(cat ../$ARTIFACTS_DIR/env/build_revision.env)
-  ARTIFACT_NAME=MuseScoreNightly-${BUILD_DATETIME}-${BUILD_BRANCH}-${BUILD_REVISION}-x86_64.AppImage
+  BUILD_DATETIME=$(cat $ARTIFACTS_DIR/env/build_datetime.env)
+  BUILD_BRANCH=$(cat $ARTIFACTS_DIR/env/build_branch.env)
+  BUILD_REVISION=$(cat $ARTIFACTS_DIR/env/build_revision.env)
+  ARTIFACT_NAME=MuseScoreNightly-${BUILD_DATETIME}-${BUILD_BRANCH}-${BUILD_REVISION}-x86_64
 else
-  BUILD_VERSION=$(cat ../$ARTIFACTS_DIR/env/build_version.env)
-  ARTIFACT_NAME=MuseScore-${BUILD_VERSION}-x86_64.AppImage  
+  BUILD_VERSION=$(cat $ARTIFACTS_DIR/env/build_version.env)
+  ARTIFACT_NAME=MuseScore-${BUILD_VERSION}-x86_64  
 fi
 
-mv ${appimage} ../${ARTIFACTS_DIR}/${ARTIFACT_NAME}
+if [ "$PACKTYPE" == "7z" ]; then
+mv $INSTALL_DIR $ARTIFACT_NAME
+7z a $ARTIFACTS_DIR/$ARTIFACT_NAME.7z $ARTIFACT_NAME
+bash ./build/ci/tools/make_artifact_name_env.sh $ARTIFACT_NAME.7z
+chmod a+rw $ARTIFACT_NAME.7z
+fi
 
-cd ..
-
-bash ./build/ci/tools/make_artifact_name_env.sh $ARTIFACT_NAME
+if [ "$PACKTYPE" == "appimage" ]; then
+bash ./build/ci/linux/tools/make_appimage.sh $INSTALL_DIR
+APPIMAGE_FILE="$(ls $BUILD_DIR/*.AppImage)"
+mv $APPIMAGE_FILE $ARTIFACTS_DIR/$ARTIFACT_NAME.AppImage
+bash ./build/ci/tools/make_artifact_name_env.sh $ARTIFACT_NAME.AppImage
+fi
 
 echo "Package has finished!" 
