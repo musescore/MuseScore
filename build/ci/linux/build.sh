fd2cf22b7cf8a13b9caa9a894f70bf3ce3fba419
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-23 14:13:06 +0200

    sync CI scripts 3.x to master

--- build/ci/linux/build.sh
+++ build/ci/linux/build.sh
@@ -55,7 +55,7 @@ make revision
 make -j2 MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG BUILD_NUMBER=$BUILD_NUMBER TELEMETRY_TRACK_ID=$TELEMETRY_TRACK_ID BUILD_UI_MU4=${BUILD_UI_MU4} BUILD_UNIT_TESTS=ON portable
 
 
-bash ./build/ci/tools/make_release_channel_env.sh 
+bash ./build/ci/tools/make_release_channel_env.sh -c $MUSESCORE_BUILD_CONFIG
 bash ./build/ci/tools/make_version_env.sh $BUILD_NUMBER
 bash ./build/ci/tools/make_revision_env.sh
 bash ./build/ci/tools/make_branch_env.sh
