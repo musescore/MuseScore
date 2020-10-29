39db33d885a1269ca53663ac340af86e9c41fd15
Igor Korsukov <igor.korsukov@gmail.com>
2020-10-29 11:58:08 +0200

    removed travis and appveyor scripts

--- build/appveyor/build_script.bat
+++
@@ -1,6 +0,0 @@
-IF "%NIGHTLY_BUILD%" == "" (
-  SET MSCORE_STABLE_BUILD="TRUE"
-)
-
-call C:\MuseScore\msvc_build.bat relwithdebinfo %TARGET_PROCESSOR_BITS% %APPVEYOR_BUILD_NUMBER%
-call C:\MuseScore\msvc_build.bat installrelwithdebinfo %TARGET_PROCESSOR_BITS% %APPVEYOR_BUILD_NUMBER%
--- build/appveyor/support/nightly.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start bin/nightly.exe
-exit
