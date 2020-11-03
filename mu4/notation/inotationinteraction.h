f296a3ec7b212402cdffd28e3d1cb80d5b333469
Roman Pudashkin <r.pudashkin@gmail.com>
2020-11-03 11:22:50 +0200

    fixed dependencies on libmscore and mscore

--- mu4/notation/inotationinteraction.h
+++ mu4/notation/inotationinteraction.h
@@ -53,7 +53,7 @@ public:
     // Select
     virtual Element* hitElement(const QPointF& pos, float width) const = 0;
     virtual int hitStaffIndex(const QPointF& pos) const = 0;
-    virtual void select(Element* e, SelectType type, int staffIdx = 0) = 0;
+    virtual void select(Element* element, SelectType type, int staffIndex = 0) = 0;
     virtual void selectAll() = 0;
     virtual INotationSelectionPtr selection() const = 0;
     virtual void clearSelection() = 0;
@@ -94,6 +94,7 @@ public:
     virtual void deleteSelection() = 0;
 
     virtual void setBreaksSpawnInterval(BreaksSpawnIntervalType intervalType, int interval = 0) = 0;
+    virtual void transpose(const TransposeOptions& options) = 0;
 };
 
 using INotationInteractionPtr = std::shared_ptr<INotationInteraction>;
