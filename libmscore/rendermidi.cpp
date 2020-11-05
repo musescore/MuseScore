23843879adad77a5231a3f7f64fead3df4bbaf8d
Pavel Smokotnin <p.smokotnin@gmail.com>
2020-11-05 08:46:01 +0300

    fix freeze when tie has the same start and end note

--- libmscore/rendermidi.cpp
+++ libmscore/rendermidi.cpp
@@ -1959,7 +1959,7 @@ void renderChordArticulation(Chord* chord, QList<NoteEventList> & ell, int & gat
 
 static bool shouldRenderNote(Note* n)
       {
-      while (n->tieBack()) {
+      while (n->tieBack() && n != n->tieBack()->startNote()) {
             n = n->tieBack()->startNote();
             if (findFirstTrill(n->chord()))
                   // The previous tied note probably has events for this note too.
