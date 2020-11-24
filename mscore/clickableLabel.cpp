746d5f0f549697f111ffe090157522923e2e9d7a
Joachim Schmitz <jojo@schmitz-digital.de>
2020-11-24 19:26:42 +0100

    Fix (MinGW) compiler warnings
    reg. unused variable, and parameters, and about wrong initialization
    order

--- mscore/clickableLabel.cpp
+++ mscore/clickableLabel.cpp
@@ -23,13 +23,14 @@ namespace Ms {
 
 ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f)
    : QLabel(parent)
-      {}
+      { Q_UNUSED(f); }
 
 ClickableLabel::~ClickableLabel()
       {}
 
 void ClickableLabel::mousePressEvent(QMouseEvent* event)
       {
+      Q_UNUSED(event);
       emit clicked();
       }
 }
