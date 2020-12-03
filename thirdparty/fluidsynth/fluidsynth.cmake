9881519549c2351fd4c9428acbdfee0a7967ad2a
Joachim Schmitz <jojo@schmitz-digital.de>
2020-12-03 17:12:41 +0100

    Disable `-Wno-deprecated-copy` for MinGW
    as that doesn know it and warms about that

--- thirdparty/fluidsynth/fluidsynth.cmake
+++ thirdparty/fluidsynth/fluidsynth.cmake
@@ -106,7 +106,9 @@ set (FLUIDSYNTH_SRC
     )
 
 if (NOT MSVC)
-   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-copy")
+   if (NOT MINGW)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-copy")
+   endif (NOT MINGW)
 else (NOT MSVC)
    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4013 /wd4100 /wd4101 /wd4115 /wd4127 /wd4201 /wd4244 /wd4267 /wd4456 /wd4701 /wd4703 /wd4706")
 endif (NOT MSVC)
