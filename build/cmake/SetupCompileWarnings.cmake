e573e2349dd2a03507b7e12dd36aed5b529bc851
Igor Korsukov <igor.korsukov@gmail.com>
2020-12-09 16:11:22 +0300

    disabled warning for thridparty

--- build/cmake/SetupCompileWarnings.cmake
+++ build/cmake/SetupCompileWarnings.cmake
@@ -3,6 +3,7 @@
 if (MSVC)
     add_compile_options(/W4)
     add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+    add_compile_options(/wd4127) # disabled warning: C4127: conditional expression is constant
 else()
     add_compile_options(-Wall -Wextra)
 endif()
@@ -20,21 +21,44 @@ function(target_no_warning TARGET WNAME)
     elseif(WNAME STREQUAL "-Wimplicit-fallthrough=0")
 
     elseif(WNAME STREQUAL "-Wno-unused-parameter")
-
+        set(MSVC_Warning /wd4100)
     elseif(WNAME STREQUAL "-Wno-unused-variable")
-
+        set(MSVC_Warning /wd4101)
     elseif(WNAME STREQUAL "-Wunused-const-variable=0")
 
     elseif(WNAME STREQUAL "-Wno-type-limits")
 
     elseif(WNAME STREQUAL "-Wno-unknown-pragmas")
 
-    elseif(WNAME STREQUAL "-Wfloat-conversion")
+    elseif(WNAME STREQUAL "-Wno-uninitialized")
+        set(MSVC_Warning /wd4701 /wd4703)
+    elseif(WNAME STREQUAL "-Wno-float-conversion")
         set(MSVC_Warning /wd4244)
+    elseif(WNAME STREQUAL "-Wno-deprecated")
+        set(MSVC_Warning /wd4996)
+    elseif(WNAME STREQUAL "-Wno-conversion")
+        set(MSVC_Warning /wd4244 /wd4267 /wd4245)
+    elseif(WNAME STREQUAL "-Wno-cast-function-type")
+
     # Only MSVC warnings
     elseif(WNAME STREQUAL "-WMSVC-no-translation-unit-is-empty")
         unset(GCC_Warning)
         set(MSVC_Warning /wd4206)
+    elseif(WNAME STREQUAL "-WMSVC-no-nonstandard-extension-used")
+        unset(GCC_Warning)
+        set(MSVC_Warning /wd4201)
+    elseif(WNAME STREQUAL "-WMSVC-no-assignment-within-conditional-expression")
+        unset(GCC_Warning)
+        set(MSVC_Warning /wd4706)
+    elseif(WNAME STREQUAL "-WMSVC-no-hides-previous")
+        unset(GCC_Warning)
+        set(MSVC_Warning /wd4456 /wd4456 /wd4457)
+    elseif(WNAME STREQUAL "-WMSVC-no-undefined-assuming-extern") # warning: C4013: 'read' undefined; assuming extern returning int
+        unset(GCC_Warning)
+        set(MSVC_Warning /wd4013)
+    elseif(WNAME STREQUAL "-WMSVC-named-type-definition-in-parentheses")
+        unset(GCC_Warning)
+        set(MSVC_Warning /wd4115)
     else()
         message(WARNING "Unknown warning: ${WNAME}, please add this warning to ${CMAKE_CURRENT_LIST_DIR}/SetupCompileWarnings.cmake")
     endif()
