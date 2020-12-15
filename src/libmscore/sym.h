83b5258fa268909f26ff257dbdc5177bbe46407a
Dmitri Ovodok <dmitrio95@yandex.ru>
2020-12-15 04:57:18 +0300

    Avoid duplicating BEGIN/END_QT_REGISTERED_ENUM macros

--- src/libmscore/sym.h
+++ src/libmscore/sym.h
@@ -17,24 +17,10 @@
 
 #include "config.h"
 #include "style.h"
+#include "qtenum.h"
 
 #include "ft2build.h"
 #include FT_FREETYPE_H
-
-// Needs to be duplicated here and in style.h since moc doesn't handle macros from #include'd files
-#ifdef SCRIPT_INTERFACE
-#define BEGIN_QT_REGISTERED_ENUM(Name) \
-    class MSQE_##Name { \
-        Q_GADGET \
-    public:
-#define END_QT_REGISTERED_ENUM(Name) \
-    Q_ENUM(Name); \
-}; \
-    using Name = MSQE_##Name::Name;
-#else
-#define BEGIN_QT_REGISTERED_ENUM(Name)
-#define END_QT_REGISTERED_ENUM(Name)
-#endif
 
 namespace Ms {
 //---------------------------------------------------------
