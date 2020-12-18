bfd75feec3e4da1b8c01cc0fd0aa81350ed42710
Igor Korsukov <igor.korsukov@gmail.com>
2020-12-18 11:48:32 +0200

    added run utest job on CI

--- build/ci/linux/runutests.sh
+++ build/ci/linux/runutests.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-ENV_FILE=./../musescore_environment.sh
-cat ${ENV_FILE}
-. ${ENV_FILE}
+source ./../musescore_environment.sh
 
-cd build.release
+cd build.debug
+
 export GTEST_OUTPUT=xml:test-results
 export GTEST_COLOR=1
-export QT_QPA_PLATFORM=minimal
+
+# run the tests in "minimal" platform for headless systems
+# enable fonts handling
+export QT_QPA_PLATFORM=minimal:enable_fonts
+
+# if AddressSanitizer was used, disable leak detection
+export ASAN_OPTIONS=detect_leaks=0:new_delete_type_mismatch=0
 
 ctest -V
