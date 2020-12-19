fb14a51cd7c557d4c0f7e97a454cbfd630b5965d
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2020-12-19 16:27:26 +0100

    Fix #314439: Action of Score Improvements Checkbox was reversed
    Plus some (internal) clean-up in the Preferences Dialog

--- mscore/file.cpp
+++ mscore/file.cpp
@@ -371,15 +371,14 @@ Score* MuseScore::openScore(const QString& fn, bool switchTab)
 
             migrator.registerHandler(new StyleDefaultsHandler());
 
-           if (Ms::preferences.getBool(PREF_MIGRATION_DO_NOT_ASK_ME_AGAIN)) {
-
-                  if (Ms::preferences.getBool(PREF_MIGRATION_APPLY_LELAND_STYLE))
+            if (preferences.getBool(PREF_MIGRATION_DO_NOT_ASK_ME_AGAIN)) {
+                  if (preferences.getBool(PREF_MIGRATION_APPLY_LELAND_STYLE))
                         migrator.registerHandler(new LelandStyleHandler());
 
-                  if (Ms::preferences.getBool(PREF_MIGRATION_APPLY_EDWIN_STYLE))
+                  if (preferences.getBool(PREF_MIGRATION_APPLY_EDWIN_STYLE))
                         migrator.registerHandler(new EdwinStyleHandler());
 
-                  if (Ms::preferences.getString(PREF_IMPORT_COMPATIBILITY_RESET_ELEMENT_POSITIONS).contains("Yes"))
+                  if (preferences.getBool(PREF_MIGRATION_RESET_ELEMENT_POSITIONS))
                         migrator.registerHandler(new ResetAllElementsPositionsHandler());
                   }
 
