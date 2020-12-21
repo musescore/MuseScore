e9cc4725e790bff8dc83167f04a0a9e301fa2d52
Joachim Schmitz <jojo@schmitz-digital.de>
2020-12-21 12:09:16 +0100

    Fix #314551: Duplicate check of identical expression
    Apparently a long time bug, introduced 6 years ago in f4067835

--- importexport/midiimport/importmidi_voice.cpp
+++ importexport/midiimport/importmidi_voice.cpp
@@ -61,7 +61,7 @@ bool areNotesSortedByOffTimeInAscOrder(
 bool areNotesSortedByOffTimeInAscOrder(const QList<MidiNote>& notes)
       {
       for (int i = 0; i != (int)notes.size() - 1; ++i) {
-            if (notes[i].offTime > notes[i].offTime)
+            if (notes[i].offTime > notes[i + 1].offTime)
                   return false;
             }
       return true;
