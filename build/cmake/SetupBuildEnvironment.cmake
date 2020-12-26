f5b8884fbf1ebb64079bedbb0151bb8eceb24008
Igor Korsukov <igor.korsukov@gmail.com>
2020-12-26 11:55:33 +0200

    updated used XCode on CI

--- build/cmake/SetupBuildEnvironment.cmake
+++ build/cmake/SetupBuildEnvironment.cmake
@@ -106,45 +106,7 @@ endif()
 # APPLE specific
 if (OS_IS_MAC)
       set(CMAKE_OSX_ARCHITECTURES x86_64)
-      set(MACOSX_DEPLOYMENT_TARGET 10.10)
-      set(CMAKE_OSX_DEPLOYMENT_TARGET 10.10)
+      set(MACOSX_DEPLOYMENT_TARGET 10.14)
+      set(CMAKE_OSX_DEPLOYMENT_TARGET 10.14)
 endif(OS_IS_MAC)
 
-
-#   string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
-#   # Windows: Add -mconsole to LINK_FLAGS to get a console window for debug output
-#   if(CMAKE_BUILD_TYPE MATCHES "DEBUG")
-#     set_target_properties( mscore
-#        PROPERTIES
-#           COMPILE_FLAGS "${PCH_INCLUDE} -g -Wall -Wextra -Winvalid-pch ${QT_DEFINITIONS} -DQT_SVG_LIB -DQT_GUI_LIB -DQT_XML_LIB -DQT_CORE_LIB"
-#           LINK_FLAGS "-mwindows -mconsole -L ${QT_INSTALL_LIBS}"
-#        )
-#   else(CMAKE_BUILD_TYPE MATCHES "DEBUG")
-#     set_target_properties( mscore
-#          PROPERTIES
-#             COMPILE_FLAGS "${PCH_INCLUDE} -Wall -Wextra -Winvalid-pch ${QT_DEFINITIONS} -DQT_SVG_LIB -DQT_GUI_LIB -DQT_XML_LIB -DQT_CORE_LIB"
-#             LINK_FLAGS "-Wl,-S -mwindows -L ${QT_INSTALL_LIBS}"
-#          )
-#   endif(CMAKE_BUILD_TYPE MATCHES "DEBUG")
-
-
-#if (OS_IS_MAC)
-
-#else (OS_IS_MAC)
-#   if (MSVC)
-#   # Set compiler options for VS2017/19 toolchain.
-#   # Note: /D_CRT_SECURE_NO WARNINGS disables warnings when using "non-secure" library functions like sscanf...
-##      set(CMAKE_CXX_FLAGS                "/MP /DWIN32 /D_WINDOWS /GR /EHsc /D_UNICODE /DUNICODE /D_CRT_SECURE_NO_WARNINGS /execution-charset:utf-8 /source-charset:utf-8")
-##      set(CMAKE_C_FLAGS                  "/MP /DWIN32 /D_WINDOWS /D_CRT_SECURE_NO_WARNINGS")
-##      set(CMAKE_CXX_FLAGS_DEBUG          "/MT /permissive- /std:c++17 /W4 /Zi /Ob0 /Od /RTC1")
-##      set(CMAKE_CXX_FLAGS_RELEASE        "/MT /permissive- /std:c++17 /W4 /O2 /Ob2 /DNDEBUG /DQT_NO_DEBUG /DQT_NO_DEBUG_OUTPUT")
-##      set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MT /permissive- /std:c++17 /W4 /Zi /O2 /Ob1 /DNDEBUG /DQT_NO_DEBUG /DQT_NO_DEBUG_OUTPUT")
-##      set(CMAKE_C_FLAGS_DEBUG            "/MT /W4 /Zi /Ob0 /Od /RTC1")
-##      set(CMAKE_C_FLAGS_RELEASE          "/MT /W4 /O2 /Ob2 /DNDEBUG")
-##      set(CMAKE_C_FLAGS_RELWITHDEBINFO   "/MT /W4 /Zi /O2 /Ob1 /DNDEBUG")
-##      set(CMAKE_EXE_LINKER_FLAGS         "/DYNAMICBASE:NO")
-
-#endif(APPLE)
-
-#set(CMAKE_BUILD_WITH_INSTALL_RPATH ON) # Call CMake with option -DCMAKE_SKIP_RPATH to not set RPATH (Debian packaging requirement)
-#set(CMAKE_SKIP_RULE_DEPENDENCY TRUE)
