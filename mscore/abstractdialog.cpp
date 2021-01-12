3d8caaaa9ec313b30ad27570883cae89269d9cd3
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-01-12 21:06:31 +0100

    Retranslate Edit Style dialog on language switch (version 2)

--- mscore/abstractdialog.cpp
+++ mscore/abstractdialog.cpp
@@ -21,21 +21,16 @@
 
 namespace Ms {
 
-AbstractDialog::AbstractDialog(QWidget * parent, Qt::WindowFlags f)
+AbstractDialog::AbstractDialog(QWidget* parent, Qt::WindowFlags f)
     : QDialog(parent, f)
       {
       }
 
-AbstractDialog::~AbstractDialog()
-      {
-
-      }
-
-void AbstractDialog::changeEvent(QEvent *event)
+void AbstractDialog::changeEvent(QEvent* event)
       {
       QDialog::changeEvent(event);
       if (event->type() == QEvent::LanguageChange)
             retranslate();
       }
-}
 
+}
