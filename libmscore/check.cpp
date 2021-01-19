4dd04b78956c02db6ab124e38a406093314ceeb0
Joachim Schmitz <jojo@schmitz-digital.de>
2021-01-19 13:59:25 +0100

    Fix bogus message of the sanity check
    See https://musescore.org/en/node/284796#comment-1053832

--- libmscore/check.cpp
+++ libmscore/check.cpp
@@ -214,7 +214,7 @@ bool Score::sanityCheck(const QString& name)
                         }
                   for (int v = 1; v < VOICES; ++v) {
                         if (voices[v] > mLen) {
-                              QString msg = QObject::tr("Measure %1, staff %2, voice %3 too long. Expected: %4; Found: %5").arg(mNumber).arg(staffIdx + 1, v + 1).arg(mLen.print(), voices[v].print());
+                              QString msg = QObject::tr("Measure %1, staff %2, voice %3 too long. Expected: %4; Found: %5").arg(mNumber).arg(staffIdx + 1).arg(v + 1).arg(mLen.print(), voices[v].print());
                               qDebug() << msg;
                               error += QString("%1\n").arg(msg);
 #ifndef NDEBUG
