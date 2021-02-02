9341f49fb6869b3c783480e9ebe0f3dc2874f2ce
Roman Pudashkin <r.pudashkin@gmail.com>
2021-02-02 11:00:49 +0200

    implemented a view for playback settings

--- src/playback/view/internal/playbacksettingsmodel.h
+++ src/playback/view/internal/playbacksettingsmodel.h
@@ -16,70 +16,45 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //=============================================================================
-#ifndef MU_PLAYBACK_PLAYBACKTOOLBARMODEL_H
-#define MU_PLAYBACK_PLAYBACKTOOLBARMODEL_H
+#ifndef MU_PLAYBACK_PLAYBACKSETTINGSMODEL_H
+#define MU_PLAYBACK_PLAYBACKSETTINGSMODEL_H
 
 #include <QAbstractListModel>
 
 #include "modularity/ioc.h"
-#include "iplaybackcontroller.h"
 #include "actions/iactionsdispatcher.h"
-#include "actions/iactionsregister.h"
-#include "async/asyncable.h"
-#include "actions/actiontypes.h"
 #include "uicomponents/uicomponentstypes.h"
-#include "workspace/iworkspacemanager.h"
-#include "iinteractive.h"
 
 namespace mu::playback {
-class PlaybackToolBarModel : public QAbstractListModel, public async::Asyncable
+class PlaybackSettingsModel : public QAbstractListModel
 {
     Q_OBJECT
 
     INJECT(playback, actions::IActionsDispatcher, dispatcher)
-    INJECT(playback, actions::IActionsRegister, actionsRegister)
-    INJECT(playback, IPlaybackController, playbackController)
-    INJECT(playback, workspace::IWorkspaceManager, workspaceManager)
-    INJECT(playback, framework::IInteractive, interactive)
-
-    Q_PROPERTY(qreal playPosition READ playPosition WRITE setPlayPosition NOTIFY playPositionChanged)
 
 public:
-    explicit PlaybackToolBarModel(QObject* parent = nullptr);
+    explicit PlaybackSettingsModel(QObject* parent = nullptr);
 
     int rowCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index, int role) const override;
     QHash<int, QByteArray> roleNames() const override;
 
-    qreal playPosition() const;
-
     Q_INVOKABLE void load();
     Q_INVOKABLE void handleAction(const QString& action);
-
-public slots:
-    void setPlayPosition(qreal position);
-
-signals:
-    void playPositionChanged(qreal position);
 
 private:
     enum Roles {
         CodeRole = Qt::UserRole + 1,
-        HintRole,
+        DescriptionRole,
         IconRole,
-        EnabledRole,
-        CheckedRole
+        CheckedRole,
+        SectionRole
     };
 
-    void updateState();
-
-    actions::ActionList currentWorkspaceActions() const;
-
-    uicomponents::MenuItem& item(const actions::ActionCode& actionCode);
-    uicomponents::MenuItem settingsItem() const;
+    QString resolveSection(const std::string& actionCode) const;
 
     QList<uicomponents::MenuItem> m_items;
 };
 }
 
-#endif // MU_PLAYBACK_PLAYBACKTOOLBARMODEL_H
+#endif // MU_PLAYBACK_PLAYBACKSETTINGSMODEL_H
