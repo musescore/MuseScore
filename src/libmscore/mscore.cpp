347a1807521290de17e05aeb8f4a8cd4d262383d
Igor Korsukov <igor.korsukov@gmail.com>
2021-02-12 10:27:07 +0200

    ported #7293 : fixes

--- src/libmscore/mscore.cpp
+++ src/libmscore/mscore.cpp
@@ -412,7 +412,7 @@ bool MScore::readDefaultStyle(QString file)
     if (!f.open(QIODevice::ReadOnly)) {
         return false;
     }
-    bool rv = style.load(&f);
+    bool rv = style.load(&f, true);
     if (rv) {
         setDefaultStyle(style);
     }
