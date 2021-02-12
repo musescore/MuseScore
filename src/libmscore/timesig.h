9c658affe1ec7104b58be4391155e726f32bf27d
Joachim Schmitz <jojo@schmitz-digital.de>
2021-02-12 13:07:54 +0100

    Fix #317319: Remove unneeded skyline extension for time- and key signatures

--- src/libmscore/timesig.h
+++ src/libmscore/timesig.h
@@ -79,7 +79,6 @@ public:
     void write(XmlWriter& xml) const override;
     void read(XmlReader&) override;
     void layout() override;
-    Shape shape() const override;
 
     Fraction sig() const { return _sig; }
     void setSig(const Fraction& f, TimeSigType st = TimeSigType::NORMAL);
