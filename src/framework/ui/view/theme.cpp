64797f9cb1c5ff2c450a90442d674828313426f3
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-02-21 12:32:44 +0100

    Set application font for Widgets to theme font

--- src/framework/ui/view/theme.cpp
+++ src/framework/ui/view/theme.cpp
@@ -385,6 +385,7 @@ void Theme::setupWidgetTheme()
     palette.setColor(QPalette::Disabled, QPalette::PlaceholderText, fontPrimaryColorDisabled);
 
     QApplication::setPalette(palette);
+    QApplication::setFont(bodyFont());
 
     platformTheme()->setAppThemeDark(configuration()->actualThemeType() == IUiConfiguration::ThemeType::DARK_THEME);
 }
