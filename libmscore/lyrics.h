705b26432a540d97c696a2b420a2defaf9b98d95
Joachim Schmitz <jojo@schmitz-digital.de>
2021-03-05 11:33:41 +0100

    Fix detection of even/odd lyrics
    Shamelessly stolen from #7627

--- libmscore/lyrics.h
+++ libmscore/lyrics.h
@@ -83,7 +83,7 @@ class Lyrics final : public TextBase {
       QString subtypeName() const override    { return QObject::tr("Verse %1").arg(_no + 1); }
       void setNo(int n)                               { _no = n; }
       int no() const                                  { return _no; }
-      bool isEven() const                             { return _no % 1; }
+      bool isEven() const                             { return _no & 1; }
       void setSyllabic(Syllabic s)                    { _syllabic = s; }
       Syllabic syllabic() const                       { return _syllabic; }
       void add(Element*) override;
