15028b275c4a33e6eebbe2d5ba122e86c31fd1df
Eism <eismailzada@gmail.com>
2021-03-17 16:43:50 +0200

    Implemented preferences menu

--- src/appshell/view/preferencesmodel.h
+++ src/appshell/view/preferencesmodel.h
@@ -19,24 +19,61 @@
 #ifndef MU_APPSHELL_PREFERENCESMODEL_H
 #define MU_APPSHELL_PREFERENCESMODEL_H
 
-#include <QObject>
+#include <QAbstractItemModel>
 
+#include "modularity/ioc.h"
 #include "actions/iactionsdispatcher.h"
 #include "actions/iactionsregister.h"
-#include "modularity/ioc.h"
+#include "ui/view/iconcodes.h"
+
+#include "preferencepageitem.h"
 
 namespace mu::appshell {
-class PreferencesModel : public QObject
+class PreferencesModel : public QAbstractItemModel
 {
+    Q_OBJECT
+
     INJECT(appshell, actions::IActionsDispatcher, dispatcher)
     INJECT(appshell, actions::IActionsRegister, actionsRegister)
 
+    Q_PROPERTY(QString currentMenuId READ currentMenuId WRITE setCurrentMenuId NOTIFY currentMenuIdChanged)
+
 public:
     explicit PreferencesModel(QObject* parent = nullptr);
+    ~PreferencesModel();
+
+    QModelIndex index(int row, int column, const QModelIndex& parent) const override;
+    QModelIndex parent(const QModelIndex& child) const override;
+    int rowCount(const QModelIndex& parent) const override;
+    int columnCount(const QModelIndex& parent) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    QString currentMenuId() const;
 
     Q_INVOKABLE void load();
     Q_INVOKABLE void resetFactorySettings();
     Q_INVOKABLE bool apply();
+
+public slots:
+    void setCurrentMenuId(QString currentMenuId);
+
+signals:
+    void currentMenuIdChanged(QString currentMenuId);
+
+private:
+
+    enum RoleNames {
+        ItemRole = Qt::UserRole + 1
+    };
+
+    PreferencePageItem* makeItem(const QString& id, const QString& title, ui::IconCode::Code icon = mu::ui::IconCode::Code::NONE,
+                                 const QList<PreferencePageItem*>& children = {}) const;
+
+    PreferencePageItem* modelIndexToItem(const QModelIndex& index) const;
+
+    PreferencePageItem* m_rootItem = nullptr;
+    QString m_currentMenuId;
 };
 }
 
