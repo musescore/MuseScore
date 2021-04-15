c8f8676150ff2c8000ead1027f24f159045a1906
vpereverzev <v.pereverzev@wsmgroup.ru>
2021-04-15 15:52:04 +0200

    Updated licensing headers for GPLv3 version

--- src/importexport/imagesexport/internal/svggenerator.h
+++ src/importexport/imagesexport/internal/svggenerator.h
@@ -1,43 +1,24 @@
-/****************************************************************************
-**
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-** This file is part of the QtSvg module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** GNU Lesser General Public License Usage
-** This file may be used under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation and
-** appearing in the file LICENSE.LGPL included in the packaging of this
-** file. Please review the following information to ensure the GNU Lesser
-** General Public License version 2.1 requirements will be met:
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU General
-** Public License version 3.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of this
-** file. Please review the following information to ensure the GNU General
-** Public License version 3.0 requirements will be met:
-** http://www.gnu.org/copyleft/gpl.html.
-**
-** Other Usage
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
-**
-**
-**
-**
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+/**
+ * // SPDX-License-Identifier: GPL-3.0-only
+ * // MuseScore-CLA-applies
+ * //=============================================================================
+ * //  MuseScore
+ * //  Music Composition & Notation
+ * //
+ * //  Copyright (C) 2021 MuseScore BVBA and others
+ * //
+ * //  This program is free software: you can redistribute it and/or modify
+ * //  it under the terms of the GNU General Public License version 3 as
+ * //  published by the Free Software Foundation.
+ * //
+ * //  This program is distributed in the hope that it will be useful,
+ * //  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * //  GNU General Public License for more details.
+ * //
+ * //  You should have received a copy of the GNU General Public License
+ * //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * //============================================================================= **/
 
 #ifndef SVGGENERATOR_H
 #define SVGGENERATOR_H
