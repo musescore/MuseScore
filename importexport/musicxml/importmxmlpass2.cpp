b72715755562e3f37388cdf30aae4b008d3a1d5e
Leon Vinken <leon.vinken@gmail.com>
2021-04-18 21:42:02 +0200

    Fix #270988: [MusicXML] first-fret tag not supported for fretboard diagrams
    Support first-fret tag on both import and export.

--- importexport/musicxml/importmxmlpass2.cpp
+++ importexport/musicxml/importmxmlpass2.cpp
@@ -4888,6 +4888,11 @@ FiguredBass* MusicXMLParserPass2::figuredBass()
 /**
  Parse the /score-partwise/part/measure/harmony/frame node.
  Return the result as a FretDiagram.
+ Notes:
+ - MusicXML's first-fret is a positive integer equivalent to MuseScore's FretDiagram::_fretOffset
+ - it is one-based in MusicXML and zero-based in MuseScore
+ - in MusicXML fret numbers are absolute, in MuseScore they are relative to the fretOffset,
+   which affects both single strings and barres
  */
 
 FretDiagram* MusicXMLParserPass2::frame()
@@ -4901,7 +4906,15 @@ FretDiagram* MusicXMLParserPass2::frame()
       std::map<int, int> bEnds;
 
       while (_e.readNextStartElement()) {
-            if (_e.name() == "frame-frets") {
+            if (_e.name() == "first-fret") {
+                  bool ok {};
+                  int val = _e.readElementText().toInt(&ok);
+                  if (ok && val > 0)
+                        fd->setFretOffset(val - 1);
+                  else
+                        _logger->logError(QString("FretDiagram::readMusicXML: illegal first-fret %1").arg(val), &_e);
+                  }
+            else if (_e.name() == "frame-frets") {
                   int val = _e.readElementText().toInt();
                   if (val > 0)
                         fd->setFrets(val);
@@ -4939,7 +4952,7 @@ FretDiagram* MusicXMLParserPass2::frame()
                         if (fret == 0)
                               fd->setMarker(actualString, FretMarkerType::CIRCLE);
                         else if (fret > 0)
-                              fd->setDot(actualString, fret, true);
+                              fd->setDot(actualString, fret - fd->fretOffset(), true);
                         }
                   else
                         _logger->logError(QString("FretDiagram::readMusicXML: illegal frame-note string %1").arg(string), &_e);
@@ -4970,7 +4983,7 @@ FretDiagram* MusicXMLParserPass2::frame()
                   continue;
 
             int endString = bEnds[fret];
-            fd->setBarre(startString, endString, fret);
+            fd->setBarre(startString, endString, fret - fd->fretOffset());
             }
 
       return fd;
