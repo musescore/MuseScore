f22d53ac14355b2c25a5651f86f7d2c95710b9cd
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-04-19 22:37:11 +0200

    Remove Note Input action from Add menu in NoteInputBar

--- src/notation/view/noteinputbarmodel.cpp
+++ src/notation/view/noteinputbarmodel.cpp
@@ -667,8 +667,6 @@ MenuItemList NoteInputBarModel::addItems() const
 MenuItemList NoteInputBarModel::notesItems() const
 {
     MenuItemList items {
-        makeAction("note-input"),
-        makeSeparator(),
         makeAction("note-c"),
         makeAction("note-d"),
         makeAction("note-e"),
