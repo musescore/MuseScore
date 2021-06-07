5ca35602535a91abf2e73ef276a8a7897ee5008a
Dmitri Ovodok <dmitrio95@yandex.ru>
2021-06-08 01:06:52 +0300

    MusicXML export: add print-object="no" attribute to <notations> tags corresponding to invisible elements

--- src/importexport/musicxml/internal/musicxml/musicxmlsupport.h
+++ src/importexport/musicxml/internal/musicxml/musicxmlsupport.h
@@ -242,6 +242,6 @@ extern AccidentalType mxmlString2accidentalType(const QString mxmlName);
 extern SymId mxmlString2accSymId(const QString mxmlName);
 extern AccidentalType microtonalGuess(double val);
 extern bool isLaissezVibrer(const SymId id);
-extern bool hasLaissezVibrer(const Chord* const chord);
+extern const Articulation* findLaissezVibrer(const Chord* const chord);
 } // namespace Ms
 #endif
