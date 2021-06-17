d8e834a58b3b48cd5ecd25500604f5b755ad7096
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-06-17 17:52:38 +0200

    Changed implementation of MainWindowProvider
    Unified it with macOS's AppWindowStyler.

--- src/appshell/view/dockwindow/mainwindowprovider.h
+++ src/appshell/view/dockwindow/mainwindowprovider.h
@@ -25,10 +25,18 @@
 
 #include "framework/ui/imainwindow.h"
 
+#include <QObject>
+
 namespace mu::dock {
-class MainWindowProvider : public ui::IMainWindow
+class MainWindowProvider : public QObject, public ui::IMainWindow
 {
+    Q_OBJECT
+
+    Q_PROPERTY(QWindow * window READ qWindow WRITE setWindow NOTIFY windowChanged)
+
 public:
+    explicit MainWindowProvider(QObject* parent = nullptr);
+
     QMainWindow* qMainWindow() const override;
     QWindow* qWindow() const override;
 
@@ -51,7 +59,17 @@ public:
     void requestHideAllDockingHolders() override;
     async::Notification hideAllDockingHoldersRequested() const override;
 
+signals:
+    void windowChanged();
+
+protected:
+    virtual void init();
+
+    QWindow* m_window = nullptr;
+
 private:
+    void setWindow(QWindow* window); // Should only be used from QML
+
     async::Channel<QString, framework::Orientation> m_dockOrientationChanged;
     async::Channel<QPoint> m_showToolBarDockingHolderRequested;
     async::Channel<QPoint> m_showPanelDockingHolderRequested;
