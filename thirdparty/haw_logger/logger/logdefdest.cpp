27a2544886d82dc19cc6e126a22883054df31f69
Igor Korsukov <igor.korsukov@gmail.com>
2021-06-29 15:41:34 +0200

    make one instance - one log file

--- thirdparty/haw_logger/logger/logdefdest.cpp
+++ thirdparty/haw_logger/logger/logdefdest.cpp
@@ -42,10 +42,13 @@ std::string MemLogDest::content() const
 }
 
 // FileLogDest
-FileLogDest::FileLogDest(const std::string& path, const std::string& name, const std::string& ext, const LogLayout& l)
-    : LogDest(l), m_path(path), m_name(name), m_ext(ext)
+FileLogDest::FileLogDest(const std::string& filePath, const LogLayout& l)
+    : LogDest(l), m_filePath(filePath)
 {
-    rotate();
+    m_file.open(m_filePath, std::ios_base::out | std::ios_base::app);
+    if (!m_file.is_open()) {
+        std::clog << "failed open log file: " << m_filePath << std::endl;
+    }
 }
 
 FileLogDest::~FileLogDest()
@@ -65,69 +68,8 @@ std::string FileLogDest::filePath() const
     return m_filePath;
 }
 
-void FileLogDest::rotate()
-{
-    if (m_file.is_open()) {
-        m_file.close();
-    }
-
-#ifdef USE_QT_DIR
-    QString path = QString::fromStdString(m_path);
-    if (!QDir(path).exists()) {
-        bool ok = QDir().mkpath(path);
-        assert(ok);
-        if (!ok) {
-            std::clog << "failed create dir: " << path.toStdString() << std::endl;
-            return;
-        }
-    }
-#else
-    fs::path path = m_path;
-    if (!fs::exists(path)) {
-        bool ok = fs::create_directories(path);
-        assert(ok);
-        if (!ok) {
-            std::clog << "failed create dir: " << path << std::endl;
-            return;
-        }
-    }
-#endif
-
-    auto formatDate = [](const Date& d) {
-        std::string str;
-        str.reserve(10);
-
-        str.append(std::to_string(d.year + 1900));
-
-        if (d.mon < 11) {
-            str.push_back('0');
-        }
-        str.append(std::to_string(d.mon + 1));
-
-        if (d.day < 10) {
-            str.push_back('0');
-        }
-        str.append(std::to_string(d.day));
-
-        return str;
-    };
-
-    m_rotateDate = DateTime::now().date;
-    std::string dateStr = formatDate(m_rotateDate);
-    m_filePath = m_path + "/" + m_name + '_' + dateStr + '.' + m_ext;
-
-    m_file.open(m_filePath, std::ios_base::out | std::ios_base::app);
-    if (!m_file.is_open()) {
-        std::clog << "failed open log file: " << m_path << std::endl;
-    }
-}
-
 void FileLogDest::write(const LogMsg& logMsg)
 {
-    if (m_rotateDate != logMsg.datetime.date) {
-        rotate();
-    }
-
     m_file << m_layout.output(logMsg) << std::endl;
     m_file.flush();
 }
