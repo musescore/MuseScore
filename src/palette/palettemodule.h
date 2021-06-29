786496a670c51711839275489d0189cf9e56be70
Igor Korsukov <igor.korsukov@gmail.com>
2021-06-29 11:44:24 +0200

    added onAllInited method to IModuleSetup

--- src/palette/palettemodule.h
+++ src/palette/palettemodule.h
@@ -37,7 +37,7 @@ public:
     void registerUiTypes() override;
 
     void onInit(const framework::IApplication::RunMode& mode) override;
-    void onStartApp() override;
+    void onAllInited() override;
 };
 }
 
