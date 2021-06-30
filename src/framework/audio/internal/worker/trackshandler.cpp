13df727776b8ed11278c5a9f079609190e7d8ace
vpereverzev <v.pereverzev@wsmgroup.ru>
2021-06-30 12:10:36 +0200

    Implemented ITracks facade interface

---
+++ src/framework/audio/internal/worker/trackshandler.cpp
@@ -0,0 +1,160 @@
+#include "trackshandler.h"
+
+#include "log.h"
+#include "async/async.h"
+#include "midi/miditypes.h"
+
+#include "internal/audiothread.h"
+#include "internal/audiosanitizer.h"
+#include "audioerrors.h"
+
+using namespace mu::audio;
+using namespace mu::async;
+
+TracksHandler::TracksHandler(IGetTrackSequence* getSequence)
+    : m_getSequence(getSequence)
+{
+}
+
+TracksHandler::~TracksHandler()
+{
+    m_getSequence = nullptr;
+}
+
+Promise<TrackIdList> TracksHandler::trackIdList(const TrackSequenceId sequenceId) const
+{
+    return Promise<TrackIdList>([this, sequenceId](Promise<TrackIdList>::Resolve resolve, Promise<TrackIdList>::Reject reject) {
+        ONLY_AUDIO_WORKER_THREAD;
+
+        ITrackSequencePtr s = sequence(sequenceId);
+
+        if (s) {
+            resolve(s->trackIdList());
+        } else {
+            reject(static_cast<int>(Err::InvalidSequenceId), "invalid sequence id");
+        }
+    }, AudioThread::ID);
+}
+
+Promise<TrackId> TracksHandler::addTrack(const TrackSequenceId sequenceId, const std::string& trackName,
+                                         midi::MidiData&& inParams, AudioOutputParams&& outParams)
+{
+    return Promise<TrackId>([this, sequenceId, trackName, inParams, outParams](Promise<TrackId>::Resolve resolve,
+                                                                               Promise<TrackId>::Reject reject) {
+        ONLY_AUDIO_WORKER_THREAD;
+
+        ITrackSequencePtr s = sequence(sequenceId);
+
+        if (!s) {
+            reject(static_cast<int>(Err::InvalidSequenceId), "invalid sequence id");
+        }
+
+        RetVal<TrackId> result = s->addTrack(trackName, inParams, outParams);
+
+        if (!result.ret) {
+            reject(result.ret.code(), result.ret.text());
+        }
+
+        resolve(result.val);
+    }, AudioThread::ID);
+}
+
+Promise<TrackId> TracksHandler::addTrack(const TrackSequenceId sequenceId, const std::string& trackName,
+                                         io::path&& inParams, AudioOutputParams&& outParams)
+{
+    return Promise<TrackId>([this, sequenceId, trackName, inParams, outParams](Promise<TrackId>::Resolve resolve,
+                                                                               Promise<TrackId>::Reject reject) {
+        ONLY_AUDIO_WORKER_THREAD;
+
+        ITrackSequencePtr s = sequence(sequenceId);
+
+        if (!s) {
+            reject(static_cast<int>(Err::InvalidSequenceId), "invalid sequence id");
+        }
+
+        RetVal<TrackId> result = s->addTrack(trackName, inParams, outParams);
+
+        if (!result.ret) {
+            reject(result.ret.code(), result.ret.text());
+        }
+
+        resolve(result.val);
+    }, AudioThread::ID);
+}
+
+void TracksHandler::removeTrack(const TrackSequenceId sequenceId, const TrackId trackId)
+{
+    Async::call(this, [this, sequenceId, trackId]() {
+        ONLY_AUDIO_WORKER_THREAD;
+
+        ITrackSequencePtr s = sequence(sequenceId);
+
+        if (!s) {
+            return;
+        }
+
+        s->removeTrack(trackId);
+    }, AudioThread::ID);
+}
+
+void TracksHandler::removeAllTracks(const TrackSequenceId sequenceId)
+{
+    Async::call(this, [this, sequenceId]() {
+        ONLY_AUDIO_WORKER_THREAD;
+
+        ITrackSequencePtr s = sequence(sequenceId);
+
+        if (!s) {
+            return;
+        }
+
+        for (const TrackId& id : s->trackIdList()) {
+            s->removeTrack(id);
+        }
+    }, AudioThread::ID);
+}
+
+Channel<TrackSequenceId, TrackId> TracksHandler::trackAdded() const
+{
+    return m_trackAdded;
+}
+
+Channel<TrackSequenceId, TrackId> TracksHandler::trackRemoved() const
+{
+    return m_trackRemoved;
+}
+
+ITrackSequencePtr TracksHandler::sequence(const TrackSequenceId id) const
+{
+    ONLY_AUDIO_WORKER_THREAD;
+
+    IF_ASSERT_FAILED(m_getSequence) {
+        return nullptr;
+    }
+
+    ITrackSequencePtr s = m_getSequence->sequence(id);
+    ensureSubscriptions(s);
+
+    return s;
+}
+
+void TracksHandler::ensureSubscriptions(const ITrackSequencePtr s) const
+{
+    ONLY_AUDIO_WORKER_THREAD;
+
+    if (!s) {
+        return;
+    }
+
+    if (!s->trackAdded().isConnected()) {
+        s->trackAdded().onReceive(this, [this, s](const TrackId trackId) {
+            m_trackAdded.send(s->id(), trackId);
+        });
+    }
+
+    if (!s->trackRemoved().isConnected()) {
+        s->trackRemoved().onReceive(this, [this, s](const TrackId trackId) {
+            m_trackRemoved.send(s->id(), trackId);
+        });
+    }
+}
