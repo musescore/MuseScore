0e1bce1f1b75efd929beadcf76367b334e697e98
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-06-30 18:31:21 +0200

    Fix build

--- src/stubs/learn/learnmodule.h
+++ src/stubs/learn/learnmodule.h
@@ -25,7 +25,7 @@
 #include "modularity/imodulesetup.h"
 
 namespace mu::learn {
-class LearnModule : public framework::IModuleSetup
+class LearnModule : public modularity::IModuleSetup
 {
 public:
     std::string moduleName() const override;
