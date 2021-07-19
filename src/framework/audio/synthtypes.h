2b67e7e4ee21b2cc225af1e984f4f95713ce552d
v.pereverzev <v.pereverzev@wsmgroup.ru>
2021-07-19 10:42:09 +0200

    Implemented SynthFactory

--- src/framework/audio/synthtypes.h
+++ src/framework/audio/synthtypes.h
@@ -37,10 +37,15 @@ enum class SoundFontFormat {
     Undefined = 0,
     SF2,
     SF3,
-    SFZ,
     Embedded
 };
 using SoundFontFormats = std::set<SoundFontFormat>;
+
+enum class SynthType {
+    Undefined = 0,
+    Fluid,
+    VSTi
+};
 
 struct SynthesizerState {
     enum class ValID {
