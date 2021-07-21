2f80421844363017c02d1d2a66b98baa7e676388
Igor Korsukov <igor.korsukov@gmail.com>
2021-07-21 14:44:25 +0200

    added comments about mcs io file formats

--- src/engraving/io/mscio.h
+++ src/engraving/io/mscio.h
@@ -25,8 +25,21 @@
 #include <string>
 
 namespace mu::engraving {
+//! NOTE The main format is MuseScore, is a zip archive with a specific structure
 static const std::string MSCZ = "mscz";
+
+//! NOTE Before MuseScore 4, MuseScore could save data in one xml file (excluding binary) with `.mscx` extension.
+//! Starting from MuseScore 4, only the score domain model store to the `.mscx` file,
+//! and other data, such as styles, chordlist, synthesizer settings, etc. are stored in separate files.
+//! We can choose to save a `.mscx` file, in this case a folder with the same structure
+//! as a zip container will be created (i.e. an unpacked zip container)
+//! We can select `.mscx` to open, in this case it is now expected that it is in a folder
+//! in which there are also other data, structured as in a zip container.
 static const std::string MSCX = "mscx";
+
+//! NOTE For testing purposes and some workflows of work with score, there is a need to store all data in one text file.
+//! Therefore, a new file type has been introduced for this.
+//! This is a XML file that stores all files from the ZIP container by concatenation (excluding binaries)
 static const std::string MSCS = "mscs";
 
 inline bool isMuseScoreFile(const std::string& suffix)
