9339771a96c4727d05cf9d54fecc78d3183479d0
iveshenry18 <iveshenry18@gmail.com>
2021-07-27 17:10:20 -0600

    ENG-85: Improve tuplet rounding error correction
    MusicXML exporters tend to have a fixed maximum <divisions> value,
    meaning more complex tuplets (5's, 7's) have incorrect duration values.
    These were already handled for tuplets, but sometimes not handled for
    <backup> elements within or after a tuplet.
    
    This commit adds a more thorough system for rounding durations off to
    more sensible values. In doing so, it unifies the conversion process
    from <duration> to Fraction (which previously existed almost identically
    in three separate places) to pass1.calcTicks().

--- importexport/musicxml/importmxmlnoteduration.h
+++ importexport/musicxml/importmxmlnoteduration.h
@@ -15,6 +15,7 @@
 
 #include "libmscore/durationtype.h"
 #include "libmscore/fraction.h"
+#include "importmxmlpass1.h"
 
 namespace Ms {
 
@@ -31,7 +32,8 @@ class MxmlLogger;
 class mxmlNoteDuration
       {
 public:
-      mxmlNoteDuration(int divs, MxmlLogger* logger) : _divs(divs), _logger(logger) { /* nothing so far */ }
+      mxmlNoteDuration(int divs, MxmlLogger* logger, MusicXMLParserPass1* pass1) :
+            _divs(divs), _logger(logger), _pass1(pass1) { /* nothing so far */ }
       QString checkTiming(const QString& type, const bool rest, const bool grace);
       Fraction dura() const { return _dura; }
       int dots() const { return _dots; }
@@ -47,6 +49,7 @@ private:
       Fraction _dura;
       TDuration _normalType;
       Fraction _timeMod { 1, 1 };                     // default to no time modification
+      MusicXMLParserPass1* _pass1;
       MxmlLogger* _logger;                            ///< Error logger
       };
 
