musescore/MuseScore

commit 0c4e33a6e17c27d2521597ab4d413b70b08491ab

[MU3] Fix #2861 Allow all types of Spanner to be copy/pasted

src/engraving/libmscore/rest.cpp
@@ -177,7 +177,6 @@ bool Rest::acceptDrop(EditData& data) const
         || (type == ElementType::STAFF_STATE)
         || (type == ElementType::INSTRUMENT_CHANGE)
         || (type == ElementType::DYNAMIC)
-        || (type == ElementType::HAIRPIN)
         || (type == ElementType::HARMONY)
         || (type == ElementType::TEMPO_TEXT)
         || (type == ElementType::REHEARSAL_MARK)
@@ -189,7 +188,7 @@ bool Rest::acceptDrop(EditData& data) const
         ) {
         return true;
     }
-    return false;
+    return type != ElementType::SLUR && e->isSpanner();
 }
 
 //---------------------------------------------------------

src/engraving/libmscore/chordrest.cpp
@@ -640,17 +640,15 @@ Element* ChordRest::drop(EditData& data)
     }
     break;
 
-    case ElementType::HAIRPIN:
-    {
-        Hairpin* hairpin = toHairpin(e);
-        hairpin->setTick(tick());
-        hairpin->setTrack(track());
-        hairpin->setTrack2(track());
-        score()->undoAddElement(hairpin);
-    }
-        return e;
-
     default:
+        if (e->isSpanner()) {
+            Spanner* spanner = toSpanner(e);
+            spanner->setTick(tick());
+            spanner->setTrack(track());
+            spanner->setTrack2(track());
+            score()->undoAddElement(spanner);
+            return e;
+        }
         qDebug("cannot drop %s", e->name());
         delete e;
         return 0;

src/engraving/libmscore/edit.cpp
@@ -1981,10 +1981,10 @@ void Score::addNoteLine()
     TextLine* line = new TextLine(this);
     line->setParent(firstNote);
     line->setStartElement(firstNote);
-    line->setEndElement(lastNote);
     line->setDiagonal(true);
     line->setAnchor(Spanner::Anchor::NOTE);
     line->setTick(firstNote->chord()->tick());
+    line->setEndElement(lastNote);
 
     undoAddElement(line);
 }

src/engraving/libmscore/note.cpp
@@ -1854,8 +1854,6 @@ bool Note::acceptDrop(EditData& data) const
            || (type == ElementType::KEYSIG)
            || (type == ElementType::TIMESIG)
            || (type == ElementType::BAR_LINE)
-           || (type == ElementType::SLUR)
-           || (type == ElementType::HAIRPIN)
            || (type == ElementType::STAFF_TEXT)
            || (type == ElementType::SYSTEM_TEXT)
            || (type == ElementType::STICKING)
@@ -1864,7 +1862,8 @@ bool Note::acceptDrop(EditData& data) const
            || (type == ElementType::TREMOLOBAR)
            || (type == ElementType::FRET_DIAGRAM)
            || (type == ElementType::FIGURED_BASS)
-           || (type == ElementType::LYRICS);
+           || (type == ElementType::LYRICS)
+           || e->isSpanner();
 }
 
 //---------------------------------------------------------
@@ -1899,15 +1898,6 @@ Element* Note::drop(EditData& data)
             delete e;
         }
         return 0;
-
-    case ElementType::SLUR:
-        data.view()->addSlur(chord(), nullptr, toSlur(e));
-        delete e;
-        return 0;
-
-    case ElementType::HAIRPIN:
-        // forward this event to a chord
-        return chord()->drop(data);
 
     case ElementType::LYRICS:
         e->setParent(ch);
@@ -2089,6 +2079,17 @@ Element* Note::drop(EditData& data)
     break;
 
     default:
+        Spanner* spanner;
+        if (e->isSpanner() && (spanner = toSpanner(e))->anchor() == Spanner::Anchor::NOTE) {
+            spanner->setParent(this);
+            spanner->setStartElement(this);
+            spanner->setTick(tick());
+            spanner->setTrack(track());
+            spanner->setTrack2(track());
+            spanner->computeEndElement();
+            score()->undoAddElement(spanner);
+            return e;
+        }
         return ch->drop(data);
     }
     return 0;

src/engraving/libmscore/slur.cpp
@@ -496,6 +496,12 @@ bool SlurSegment::isEdited() const
     return false;
 }
 
+Slur::Slur(const Slur& s)
+    : SlurTie(s)
+{
+    _sourceStemArrangement = s._sourceStemArrangement;
+}
+
 //---------------------------------------------------------
 //   fixArticulations
 //---------------------------------------------------------
@@ -979,6 +985,16 @@ Slur::Slur(Score* s)
 }
 
 //---------------------------------------------------------
+//   calcStemArrangement
+//---------------------------------------------------------
+
+int calcStemArrangement(Element* start, Element* end)
+{
+    return (start && toChord(start)->stem() && toChord(start)->stem()->up() ? 2 : 0)
+           + (end && end->isChord() && toChord(end)->stem() && toChord(end)->stem()->up() ? 4 : 0);
+}
+
+//---------------------------------------------------------
 //   write
 //---------------------------------------------------------
 
@@ -992,8 +1008,25 @@ void Slur::write(XmlWriter& xml) const
         return;
     }
     xml.stag(this);
+    if (xml.clipboardmode()) {
+        xml.tag("stemArr", calcStemArrangement(startElement(), endElement()));
+    }
     SlurTie::writeProperties(xml);
     xml.etag();
+}
+
+//---------------------------------------------------------
+//   readProperties
+//---------------------------------------------------------
+
+bool Slur::readProperties(XmlReader& e)
+{
+    const QStringRef& tag(e.name());
+    if (tag == "stemArr") {
+        _sourceStemArrangement = e.readInt();
+        return true;
+    }
+    return SlurTie::readProperties(e);
 }
 
 //---------------------------------------------------------
@@ -1090,6 +1123,15 @@ SpannerSegment* Slur::layoutSystem(System* system)
             }
             Chord* c1 = startCR()->isChord() ? toChord(startCR()) : 0;
             Chord* c2 = endCR()->isChord() ? toChord(endCR()) : 0;
+
+            if (_sourceStemArrangement != -1) {
+                if (_sourceStemArrangement != calcStemArrangement(c1, c2)) {
+                    // copy & paste from incompatible stem arrangement, so reset bezier points
+                    for (int g = 0; g < (int)Ms::Grip::GRIPS; ++g) {
+                        slurSegment->ups((Ms::Grip)g) = UP();
+                    }
+                }
+            }
 
             if (c1 && c1->beam() && c1->beam()->cross()) {
                 // TODO: stem direction is not finalized, so we cannot use it here

src/engraving/libmscore/slur.h
@@ -65,14 +65,17 @@ public:
 class Slur final : public SlurTie
 {
     void slurPosChord(SlurPos*);
+    int _sourceStemArrangement = -1;
 
 public:
     Slur(Score* = 0);
+    Slur(const Slur&);
     ~Slur() {}
 
     Slur* clone() const override { return new Slur(*this); }
     ElementType type() const override { return ElementType::SLUR; }
     void write(XmlWriter& xml) const override;
+    bool readProperties(XmlReader&) override;
     void layout() override;
     SpannerSegment* layoutSystem(System*) override;
     void setTrack(int val) override;

src/engraving/libmscore/slurtie.cpp
@@ -383,7 +383,7 @@ void SlurTieSegment::read(XmlReader& e)
             ups(Grip::BEZIER2).off = e.readPoint() * _spatium;
         } else if (tag == "o4") {
             ups(Grip::END).off = e.readPoint() * _spatium;
-        } else if (!Element::readProperties(e)) {
+        } else if (!readProperties(e)) {
             e.unknown();
         }
     }
@@ -460,7 +460,7 @@ SlurTie::~SlurTie()
 
 void SlurTie::writeProperties(XmlWriter& xml) const
 {
-    Element::writeProperties(xml);
+    Spanner::writeProperties(xml);
     int idx = 0;
     for (const SpannerSegment* ss : spannerSegments()) {
         ((SlurTieSegment*)ss)->writeSlur(xml, idx++);
@@ -489,7 +489,7 @@ bool SlurTie::readProperties(XmlReader& e)
         SlurTieSegment* s = newSlurTieSegment();
         s->read(e);
         add(s);
-    } else if (!Element::readProperties(e)) {
+    } else if (!Spanner::readProperties(e)) {
         return false;
     }
     return true;
@@ -501,11 +501,7 @@ bool SlurTie::readProperties(XmlReader& e)
 
 void SlurTie::read(XmlReader& e)
 {
-    while (e.readNextStartElement()) {
-        if (!SlurTie::readProperties(e)) {
-            e.unknown();
-        }
-    }
+    Spanner::read(e);
 }
 
 //---------------------------------------------------------

src/engraving/libmscore/spanner.cpp
@@ -385,6 +385,11 @@ Spanner::Spanner(const Spanner& s)
     _tick         = s._tick;
     _ticks        = s._ticks;
     _track2       = s._track2;
+    if (!s.startElement() && !spannerSegments().size()) {
+        for (auto* segment : s.spannerSegments()) {
+            add(segment->clone());
+        }
+    }
 }
 
 Spanner::~Spanner()
@@ -754,9 +759,14 @@ void Spanner::computeEndElement()
             _endElement = score()->lastMeasure();
         }
         break;
-
-    case Anchor::CHORD:
     case Anchor::NOTE:
+        if (!_endElement) {
+            ChordRest* cr = score()->findCR(tick2(), track2());
+            if (cr && cr->isChord()) {
+                _endElement = toChord(cr)->upNote();
+            }
+        }
+    case Anchor::CHORD:
         break;
     }
 }
@@ -1069,6 +1079,9 @@ void Spanner::setEndElement(Element* e)
     }
 #endif
     _endElement = e;
+    if (e && ticks() == Fraction() && _tick >= Fraction()) {
+        setTicks(e->tick() - _tick);
+    }
 }
 
 //---------------------------------------------------------

commit 11283ce63f9e92c30eb0777cea19d325214add02

Fix crash when pasting tie onto rest
Regression introduced with #8510

src/engraving/libmscore/rest.cpp
@@ -188,7 +188,8 @@ bool Rest::acceptDrop(EditData& data) const
         ) {
         return true;
     }
-    return type != ElementType::SLUR && e->isSpanner();
+    // prevent 'hanging' slurs, avoid crash on tie
+    return type != ElementType::SLUR && type != ElementType::TIE && e->isSpanner();
 }
 
 //---------------------------------------------------------