11cc820047ada45e1211afb65335cc151aded041
Mark McKay <mark@kitfox.com>
2021-08-30 16:48:27 -0400

    Now displaying selection drag rectangle.

--- src/pianoroll/view/pianorollview.cpp
+++ src/pianoroll/view/pianorollview.cpp
@@ -31,6 +31,7 @@
 #include "libmscore/pitchspelling.h"
 
 #include <QPainter>
+#include <QGuiApplication>
 
 using namespace mu::pianoroll;
 
@@ -64,12 +65,14 @@ const BarPattern PianorollView::barPatterns[] = {
       {"",              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}
 };
 
+static const qreal MIN_DRAG_DIST_SQ = 9;
+
 //--------------------
 
 PianorollView::PianorollView(QQuickItem* parent)
     : QQuickPaintedItem(parent)
 {
-//    memset(m_pitchHighlight, 0, 128);
+    setAcceptedMouseButtons(Qt::AllButtons);
 }
 
 void PianorollView::load()
@@ -98,12 +101,13 @@ void PianorollView::onNotationChanged()
         });
     }
 
-    update();
+    buildNoteData();
     updateBoundingSize();
 }
 
 void PianorollView::onCurrentNotationChanged()
 {
+    buildNoteData();
     updateBoundingSize();
     //update();
 
@@ -111,8 +115,71 @@ void PianorollView::onCurrentNotationChanged()
 
 void PianorollView::onSelectionChanged()
 {
+    buildNoteData();
     update();
 }
+
+void PianorollView::buildNoteData()
+{
+    for (auto block : m_noteList)
+        delete block;
+    m_noteList.clear();
+    m_activeStaff = -1;
+    m_selectedStaves.clear();
+
+    notation::INotationPtr notation = globalContext()->currentNotation();
+    if (!notation) {
+        return;
+    }
+
+
+    //Find staff to draw from
+    Ms::Score* score = notation->elements()->msScore();
+    std::vector<Ms::Element*> selectedElements = notation->interaction()->selection()->elements();
+    for (Ms::Element* e: selectedElements)
+    {
+        int idx = e->staffIdx();
+        m_activeStaff = idx;
+        if (std::find(m_selectedStaves.begin(), m_selectedStaves.end(), idx) == m_selectedStaves.end())
+        {
+            m_selectedStaves.push_back(idx);
+        }
+    }
+
+    if (m_activeStaff == -1)
+        m_activeStaff = 0;
+
+    for (int staffIndex : m_selectedStaves)
+    {
+        Ms::Staff* staff = score->staff(staffIndex);
+
+        for (Ms::Segment* s = staff->score()->firstSegment(Ms::SegmentType::ChordRest); s; s = s->next1(Ms::SegmentType::ChordRest))
+        {
+            for (int voice = 0; voice < VOICES; ++voice)
+            {
+                int track = voice + staffIndex * VOICES;
+                Ms::Element* e = s->element(track);
+                if (e && e->isChord())
+                    addChord(toChord(e), voice, staffIndex);
+            }
+        }
+    }
+}
+
+void PianorollView::addChord(Ms::Chord* chrd, int voice, int staffIdx)
+{
+    for (Ms::Chord* c : chrd->graceNotes())
+        addChord(c, voice, staffIdx);
+
+    for (Ms::Note* note : chrd->notes())
+    {
+        if (note->tieBack())
+              continue;
+
+        m_noteList.push_back(new NoteBlock { note, voice, staffIdx });
+    }
+}
+
 
 void PianorollView::setWholeNoteWidth(double value)
 {
@@ -253,31 +320,27 @@ void PianorollView::paint(QPainter* p)
 
     //Find staff to draw from
     Ms::Score* score = notation->elements()->msScore();
-    std::vector<Ms::Element*> selectedElements = notation->interaction()->selection()->elements();
-    std::vector<int> selectedStaves;
-    int activeStaff = -1;
-    for (Ms::Element* e: selectedElements)
-    {
-        int idx = e->staffIdx();
-        qDebug() << "ele idx " << idx;
-        activeStaff = idx;
-        if (std::find(selectedStaves.begin(), selectedStaves.end(), idx) == selectedStaves.end())
-        {
-            selectedStaves.push_back(idx);
-        }
-    }
+    //std::vector<Ms::Element*> selectedElements = notation->interaction()->selection()->elements();
+    //std::vector<int> selectedStaves;
+    //int activeStaff = -1;
+    //for (Ms::Element* e: selectedElements)
+    //{
+    //    int idx = e->staffIdx();
+    //    qDebug() << "ele idx " << idx;
+    //    activeStaff = idx;
+    //    if (std::find(selectedStaves.begin(), selectedStaves.end(), idx) == selectedStaves.end())
+    //    {
+    //        selectedStaves.push_back(idx);
+    //    }
+    //}
 
-    if (activeStaff == -1)
-        activeStaff = 0;
+    //if (activeStaff == -1)
+    //    activeStaff = 0;
 //        return;
-    Ms::Staff* staff = score->staff(activeStaff);
+    Ms::Staff* staff = score->staff(m_activeStaff);
     Ms::Part* part = staff->part();
 
 
-
-    const QColor colSelectionBoxFill = QColor(
-                      m_colorSelectionBox.red(), m_colorSelectionBox.green(), m_colorSelectionBox.blue(),
-                      128);
 
     const QPen penLineMajor = QPen(m_colorGridLine, 2.0, Qt::SolidLine);
     const QPen penLineMinor = QPen(m_colorGridLine, 1.0, Qt::SolidLine);
@@ -394,22 +457,11 @@ void PianorollView::paint(QPainter* p)
 
     //-----------------
     //Notes
-    for (int staffIndex : selectedStaves)
+
+    p->setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform | QPainter::TextAntialiasing);
+    for (NoteBlock* block: m_noteList)
     {
-        Ms::Staff* staff = score->staff(staffIndex);
-//        activeStaff
-
-        for (Ms::Segment* s = staff->score()->firstSegment(Ms::SegmentType::ChordRest); s; s = s->next1(Ms::SegmentType::ChordRest))
-        {
-            for (int voice = 0; voice < VOICES; ++voice)
-            {
-                int track = voice + staffIndex * VOICES;
-                Ms::Element* e = s->element(track);
-                if (e && e->isChord())
-                    drawChord(p, toChord(e), voice, staffIndex == activeStaff);
-            }
-        }
-
+        drawNoteBlock(p, block);
     }
 
 
@@ -424,22 +476,27 @@ void PianorollView::paint(QPainter* p)
 //                }
 //          }
 
-//    //Draw drag selection box
-//    if (_dragStarted && _dragStyle == DragStyle::SELECTION_RECT && _editNoteTool == PianoRollEditTool::SELECT) {
-//          int minX = qMin(_mouseDownPos.x(), _lastMousePos.x());
-//          int minY = qMin(_mouseDownPos.y(), _lastMousePos.y());
-//          int maxX = qMax(_mouseDownPos.x(), _lastMousePos.x());
-//          int maxY = qMax(_mouseDownPos.y(), _lastMousePos.y());
-//          QRectF rect(minX, minY, maxX - minX + 1, maxY - minY + 1);
-
-//          p->setPen(QPen(colSelectionBox, 2));
-//          p->setBrush(QBrush(colSelectionBoxFill, Qt::SolidPattern));
-//          p->drawRect(rect);
-//          }
+    //Draw drag selection box
+    if (m_dragStarted && m_dragStyle == DragStyle::SELECTION_RECT && m_tool == PianorollTool::SELECT)
+    {
+        int minX = qMin(m_mouseDownPos.x(), m_lastMousePos.x());
+        int minY = qMin(m_mouseDownPos.y(), m_lastMousePos.y());
+        int maxX = qMax(m_mouseDownPos.x(), m_lastMousePos.x());
+        int maxY = qMax(m_mouseDownPos.y(), m_lastMousePos.y());
+        QRectF rect(minX, minY, maxX - minX + 1, maxY - minY + 1);
 
 
-    int value = controller()->getNotes();
+        const QColor fillColor = QColor(
+            m_colorSelectionBox.red(), m_colorSelectionBox.green(), m_colorSelectionBox.blue(),
+            128);
+
+        p->setPen(QPen(m_colorSelectionBox, 2));
+        p->setBrush(QBrush(fillColor, Qt::SolidPattern));
+        p->drawRect(rect);
+    }
+
 }
+
 
 QRect PianorollView::boundingRect(Ms::Note* note, Ms::NoteEvent* evt)
 {
@@ -464,6 +521,68 @@ QRect PianorollView::boundingRect(Ms::Note* note, Ms::NoteEvent* evt)
     QRect rect;
     rect.setRect(x0, y0, width, m_noteHeight);
     return rect;
+}
+
+void PianorollView::drawNoteBlock(QPainter *p, NoteBlock *block)
+{
+    Ms::Note* note = block->note;
+    if (note->tieBack())
+        return;
+
+
+    QColor noteColor;
+    switch (block->voice)
+    {
+    case 0:
+        noteColor = m_colorNoteVoice1;
+        break;
+    case 1:
+        noteColor = m_colorNoteVoice2;
+        break;
+    case 2:
+        noteColor = m_colorNoteVoice3;
+        break;
+    case 3:
+        noteColor = m_colorNoteVoice4;
+        break;
+    }
+
+
+    if (note->selected())
+        noteColor = m_colorNoteSel;
+
+    if (block->staffIdx != m_activeStaff)
+        noteColor = noteColor.lighter(150);
+
+    p->setBrush(noteColor);
+    p->setPen(QPen(noteColor.darker(250)));
+
+    for (Ms::NoteEvent& e : note->playEvents())
+    {
+        QRect bounds = boundingRect(note, &e);
+        p->drawRect(bounds);
+
+        //Pitch name
+        if (bounds.width() >= 20 && bounds.height() >= 12)
+        {
+            QRectF textRect(bounds.x() + 2, bounds.y(), bounds.width() - 6, bounds.height() + 1);
+            QRectF textHiliteRect(bounds.x() + 3, bounds.y() + 1, bounds.width() - 6, bounds.height());
+
+            QFont f("FreeSans", 8);
+            p->setFont(f);
+
+            //Note name
+            QString name = note->tpcUserName();
+            p->setPen(QPen(noteColor.lighter(130)));
+            p->drawText(textHiliteRect,
+                Qt::AlignLeft | Qt::AlignTop, name);
+
+            p->setPen(QPen(noteColor.darker(180)));
+            p->drawText(textRect,
+                Qt::AlignLeft | Qt::AlignTop, name);
+        }
+    }
+
 }
 
 void PianorollView::drawChord(QPainter* p, Ms::Chord* chrd, int voice, bool active)
@@ -528,8 +647,326 @@ void PianorollView::drawChord(QPainter* p, Ms::Chord* chrd, int voice, bool acti
                 p->drawText(textRect,
                     Qt::AlignLeft | Qt::AlignTop, name);
             }
-
         }
-
     }
 }
+
+
+void PianorollView::keyReleaseEvent(QKeyEvent* event)
+{
+    if (m_dragStyle == DragStyle::NOTES || m_dragStyle == DragStyle::SELECTION_RECT)
+    {
+        if (event->key() == Qt::Key_Escape)
+        {
+            //Cancel drag
+            m_dragStyle = DragStyle::CANCELLED;
+            m_dragNoteCache = "";
+            m_dragStarted = false;
+//            scene()->update();
+            update();
+        }
+    }
+}
+
+void PianorollView::mousePressEvent(QMouseEvent* event)
+{
+    bool rightBn = event->button() == Qt::RightButton;
+    if (!rightBn)
+    {
+        m_mouseDown = true;
+//        m_mouseDownPos = mapToScene(event->pos());
+        m_mouseDownPos = event->pos();
+        m_lastMousePos = m_mouseDownPos;
+//        scene()->update();
+        update();
+    }
+}
+
+void PianorollView::mouseReleaseEvent(QMouseEvent* event)
+{
+    if (m_dragStyle == DragStyle::CANCELLED) {
+        m_dragStyle = DragStyle::NONE;
+        m_mouseDown = false;
+//        scene()->update();
+        update();
+        return;
+    }
+
+    int modifiers = QGuiApplication::keyboardModifiers();
+    bool bnShift = modifiers & Qt::ShiftModifier;
+    bool bnCtrl = modifiers & Qt::ControlModifier;
+
+    bool rightBn = event->button() == Qt::RightButton;
+
+    if (rightBn)
+        //Right clicks have been handled as popup menu
+        return;
+
+
+    NoteSelectType selType = bnShift ? (bnCtrl ? NoteSelectType::SUBTRACT : NoteSelectType::XOR)
+            : (bnCtrl ? NoteSelectType::ADD : NoteSelectType::REPLACE);
+
+    if (m_dragStarted)
+    {
+        if (m_dragStyle == DragStyle::SELECTION_RECT)
+        {
+            //Update selection
+            qreal minX = qMin(m_mouseDownPos.x(), m_lastMousePos.x());
+            qreal minY = qMin(m_mouseDownPos.y(), m_lastMousePos.y());
+            qreal maxX = qMax(m_mouseDownPos.x(), m_lastMousePos.x());
+            qreal maxY = qMax(m_mouseDownPos.y(), m_lastMousePos.y());
+
+            double startTick = pixelXToWholeNote((int)minX);
+            double endTick = pixelXToWholeNote((int)maxX);
+            int lowPitch = (int)floor(128 - maxY / noteHeight());
+            int highPitch = (int)ceil(128 - minY / noteHeight());
+
+            selectNotes(startTick, endTick, lowPitch, highPitch, selType);
+        }
+        else if (m_dragStyle == DragStyle::NOTES)
+        {
+            if (m_tool == PianorollTool::SELECT)
+            {
+                finishNoteGroupDrag();
+
+                //Keep last note drag event, if any
+                if (m_inProgressUndoEvent)
+                    m_inProgressUndoEvent = false;
+            }
+        }
+
+        m_dragStarted = false;
+    }
+    else {
+          //This was just a click, not a drag
+        switch (m_tool) {
+//          case SELECT:
+//                handleSelectionClick();
+//                break;
+//          case CHANGE_LENGTH:
+//                changeChordLength(_mouseDownPos);
+//                break;
+//          case ERASE:
+//                eraseNote(_mouseDownPos);
+//                break;
+//          case INSERT_NOTE:
+//                insertNote(modifiers);
+//                break;
+//          case APPEND_NOTE:
+//                appendNoteToChord(_mouseDownPos);
+//                break;
+//          case CUT_CHORD:
+//                cutChord(_mouseDownPos);
+//                break;
+//          case TIE:
+//                toggleTie(_mouseDownPos);
+//                break;
+        default:
+            break;
+        }
+    }
+
+
+    m_dragStyle = DragStyle::NONE;
+    m_mouseDown = false;
+    //scene()->update();
+    update();
+}
+
+
+void PianorollView::mouseMoveEvent(QMouseEvent* event)
+{
+    if (m_dragStyle == DragStyle::CANCELLED)
+        return;
+
+//    m_lastMousePos = mapToScene(event->pos());
+    m_lastMousePos = event->pos();
+
+    if (m_mouseDown && !m_dragStarted)
+    {
+        qreal dx = m_lastMousePos.x() - m_mouseDownPos.x();
+        qreal dy = m_lastMousePos.y() - m_mouseDownPos.y();
+
+        if (dx * dx + dy * dy >= MIN_DRAG_DIST_SQ)
+        {
+            //Start dragging
+            m_dragStarted = true;
+
+            //Check for move note
+            double tick = pixelXToWholeNote(m_mouseDownPos.x());
+            int mouseDownPitch = pixelYToPitch(m_mouseDownPos.y());
+
+            NoteBlock* pi = pickNote(m_mouseDownPos.x(), m_mouseDownPos.y());
+            if (pi && m_tool == PianorollTool::SELECT) {
+                  if (!pi->note->selected())
+                  {
+                      selectNotes(tick, tick, mouseDownPitch, mouseDownPitch, NoteSelectType::REPLACE);
+                  }
+
+                  m_dragStyle = DragStyle::NOTES;
+                  m_dragStartPitch = mouseDownPitch;
+                  m_dragNoteCache = serializeSelectedNotes();
+              }
+              else if (!pi && m_tool == PianorollTool::SELECT)
+                  m_dragStyle = DragStyle::SELECTION_RECT;
+              else
+                  m_dragStyle = DragStyle::NONE;
+        }
+    }
+
+    if (m_dragStarted)
+    {
+        switch (m_tool) {
+        case PianorollTool::SELECT:
+        case PianorollTool::EDIT:
+        case PianorollTool::CUT:
+//                scene()->update();
+            update();
+            break;
+        case PianorollTool::ERASE:
+//            eraseNote(m_lastMousePos);
+            break;
+        default:
+            break;
+        }
+    }
+
+
+    //Update mouse tracker
+//    QPointF p(mapToScene(event->pos()));
+//    int pitch = static_cast<int>((m_noteHeight * 128 - p.y()) / m_noteHeight);
+//    emit pitchChanged(pitch);
+
+//    double tick = pixelXToWholeNote(p.x());
+//    if (tick < 0)
+//    {
+//        tick = 0;
+//        trackingPos.setTick(tick);
+//        trackingPos.setInvalid();
+//    }
+//    else
+//        trackingPos.setTick(tick);
+
+//    emit trackingPosChanged(trackingPos);
+}
+
+Ms::Staff* PianorollView::activeStaff()
+{
+    notation::INotationPtr notation = globalContext()->currentNotation();
+    if (!notation) {
+        return nullptr;
+    }
+
+    Ms::Score* score = notation->elements()->msScore();
+
+    Ms::Staff* staff = score->staff(m_activeStaff);
+    return staff;
+}
+
+NoteBlock* PianorollView::pickNote(int pixX, int pixY)
+{
+    for (int i = 0; i < m_noteList.size(); ++i)
+    {
+        NoteBlock* block = m_noteList[i];
+        for (Ms::NoteEvent& e : block->note->playEvents())
+        {
+            QRect bounds = boundingRect(block->note, &e);
+            if (bounds.contains(pixX, pixY))
+            {
+                return block;
+            }
+
+        }
+    }
+
+    return nullptr;
+}
+
+
+void PianorollView::finishNoteGroupDrag()
+{
+
+}
+
+void PianorollView::selectNotes(double startTick, double endTick, int lowPitch, int highPitch, NoteSelectType selType)
+{
+
+}
+
+
+QString PianorollView::serializeSelectedNotes()
+      {
+    return "";
+
+//      Fraction firstTick;
+//      bool init = false;
+//      for (int i = 0; i < _noteList.size(); ++i) {
+//            if (_noteList[i]->note()->selected()) {
+//                  Note* note = _noteList.at(i)->note();
+//                  Fraction startTick = note->chord()->tick();
+
+//                  if (!init || firstTick > startTick) {
+//                        firstTick = startTick;
+//                        init = true;
+//                        }
+//                  }
+//            }
+
+//      //No valid notes
+//      if (!init)
+//            return QByteArray();
+
+//      QString xmlStrn;
+//      QXmlStreamWriter xml(&xmlStrn);
+//      xml.setAutoFormatting(true);
+//      xml.writeStartDocument();
+
+//      xml.writeStartElement("notes");
+//      xml.writeAttribute("firstN", QString::number(firstTick.numerator()));
+//      xml.writeAttribute("firstD", QString::number(firstTick.denominator()));
+
+//      //bundle notes into XML file & send to clipboard.
+//      //This is only affects pianoview and is not part of the regular copy/paste process
+//      for (int i = 0; i < _noteList.size(); ++i) {
+//            if (_noteList[i]->note()->selected()) {
+//                  Note* note = _noteList[i]->note();
+
+//                  Fraction flen = note->playTicksFraction();
+
+//                  Fraction startTick = note->chord()->tick();
+//                  int pitch = note->pitch();
+
+//                  int voice = note->voice();
+
+//                  int veloOff = note->veloOffset();
+//                  Note::ValueType veloType = note->veloType();
+
+//                  xml.writeStartElement("note");
+//                  xml.writeAttribute("startN", QString::number(startTick.numerator()));
+//                  xml.writeAttribute("startD", QString::number(startTick.denominator()));
+//                  xml.writeAttribute("lenN", QString::number(flen.numerator()));
+//                  xml.writeAttribute("lenD", QString::number(flen.denominator()));
+//                  xml.writeAttribute("pitch", QString::number(pitch));
+//                  xml.writeAttribute("voice", QString::number(voice));
+//                  xml.writeAttribute("veloOff", QString::number(veloOff));
+//                  xml.writeAttribute("veloType", veloType == Note::ValueType::OFFSET_VAL ? "o" : "u");
+
+//                  for (NoteEvent& evt : note->playEvents()) {
+//                        int ontime = evt.ontime();
+//                        int len = evt.len();
+
+//                        xml.writeStartElement("evt");
+//                        xml.writeAttribute("ontime", QString::number(ontime));
+//                        xml.writeAttribute("len", QString::number(len));
+//                        xml.writeEndElement();
+//                        }
+
+//                  xml.writeEndElement();
+//                  }
+//            }
+
+//      xml.writeEndElement();
+//      xml.writeEndDocument();
+
+//      return xmlStrn;
+      }
