6395f1b109008f83a31a9163783fd45c0f69682e
Mark McKay <mark@kitfox.com>
2021-08-29 23:58:10 -0400

    Creating keyboard component.

--- src/pianoroll/view/pianorollkeyboard.cpp
+++ src/pianoroll/view/pianorollkeyboard.cpp
@@ -23,42 +23,14 @@
 #include "pianorollkeyboard.h"
 
 #include <QPainter>
-#include <QGuiApplication>
-
-#include "libmscore/drumset.h"
 
 #include "pianorollview.h"
 
 using namespace mu::pianoroll;
 
-struct KeyShape
-{
-    bool white;
-    double marginBottom;
-    double marginTop;
-};
-
-static const KeyShape keyShapes[] {
-    { true, 0, 1.5 },
-    { false, 0, 1 },
-    { true, -.5, 1.5 },
-    { false, 0, 1 },
-    { true, -.5, 1 },
-
-    { true, 0, 1.5 },
-    { false, 0, 1 },
-    { true, -.5, 1.5 },
-    { false, 0, 1 },
-    { true, -.5, 1.5 },
-    { false, 0, 1 },
-    { true, -.5, 1 },
-};
-
-
 PianorollKeyboard::PianorollKeyboard(QQuickItem* parent)
     : QQuickPaintedItem(parent)
 {
-    setAcceptedMouseButtons(Qt::AllButtons);
 }
 
 void PianorollKeyboard::load()
@@ -74,21 +46,12 @@ void PianorollKeyboard::onNotationChanged()
     auto notation = globalContext()->currentNotation();
     if (notation)
     {
-        notation->interaction()->selectionChanged().onNotify(this, [this]() {
-            onSelectionChanged();
-        });
-
         notation->notationChanged().onNotify(this, [this]() {
             onCurrentNotationChanged();
         });
     }
 
     updateBoundingSize();
-}
-
-void PianorollKeyboard::onSelectionChanged()
-{
-    update();
 }
 
 void PianorollKeyboard::onCurrentNotationChanged()
@@ -128,6 +91,13 @@ void PianorollKeyboard::setDisplayObjectHeight(double value)
 
 void PianorollKeyboard::updateBoundingSize()
 {
+//    notation::INotationPtr notation = globalContext()->currentNotation();
+//    if (!notation) {
+//        return;
+//    }
+
+//    Ms::Score* score = notation->elements()->msScore();
+
     setDisplayObjectHeight(m_noteHeight * PianorollView::NUM_PITCHES);
 
     update();
@@ -151,196 +121,5 @@ void PianorollKeyboard::paint(QPainter* p)
     notation::INotationPtr notation = globalContext()->currentNotation();
     if (!notation) {
         return;
-    }
-
-    //Find staff to draw from
-    Ms::Score* score = notation->elements()->msScore();
-    std::vector<Ms::Element*> selectedElements = notation->interaction()->selection()->elements();
-    std::vector<int> selectedStaves;
-    int activeStaff = -1;
-    for (Ms::Element* e: selectedElements)
-    {
-        int idx = e->staffIdx();
-        qDebug() << "ele idx " << idx;
-        activeStaff = idx;
-        if (std::find(selectedStaves.begin(), selectedStaves.end(), idx) == selectedStaves.end())
-        {
-            selectedStaves.push_back(idx);
-        }
-    }
-
-    if (activeStaff == -1)
-        activeStaff = 0;
-//        return;
-    Ms::Staff* staff = score->staff(activeStaff);
-    Ms::Part* part = staff->part();
-
-    //Check for drumset, if any
-    Ms::Drumset* ds = nullptr;
-    Ms::Interval transp;
-    if (staff) {
-        ds = part->instrument()->drumset();
-        transp = part->instrument()->transpose();
-    }
-
-    const int fontSize = 8;
-    QFont font("FreeSans", fontSize);
-    p->setFont(font);
-
-    if (ds)
-    {
-        Ms::Interval transp = part->instrument()->transpose();
-
-        //MIDI notes span [0, 127] and map to pitches with MIDI pitch 0 being the note C-1
-
-        //Colored stripes
-        for (int pitch = 0; pitch < PianorollView::NUM_PITCHES; ++pitch)
-        {
-            double y = pitchToPixelY(pitch + 1);
-
-            int degree = (pitch - transp.chromatic + 60) % 12;
-            const BarPattern& pat = PianorollView::barPatterns[0];
-
-            if (!pat.isWhiteKey[degree])
-            {
-                p->fillRect(0, y, width(), m_noteHeight, pitch == m_curKeyPressed ? m_colorDrumHighlight : m_colorDrumBlack);
-            }
-            else
-            {
-                p->fillRect(0, y, width(), m_noteHeight, pitch == m_curKeyPressed ? m_colorDrumHighlight : m_colorDrumWhite);
-            }
-
-            p->setPen(m_colorText);
-
-            QString noteName = ds->name(pitch).toUtf8().constData();
-
-            QRectF rectText(1, y, width() - 2, m_noteHeight);
-            p->drawText(rectText, Qt::AlignBottom | Qt::AlignLeft, noteName);
-        }
-
-        //Bounding lines
-        p->setPen(m_colorGridLines);
-        for (int pitch = 0; pitch <= PianorollView::NUM_PITCHES; ++pitch)
-        {
-            double y = pitchToPixelY(pitch);
-            p->drawLine(QLineF(0, y, width(), y));
-        }
-
-    }
-    else
-    {
-        p->setPen(m_colorGridLines);
-
-        //White keys
-        for (int pitch = 0; pitch < PianorollView::NUM_PITCHES; ++pitch)
-        {
-            int degree = (pitch - transp.chromatic + 60) % 12;
-            const KeyShape& shape = keyShapes[degree];
-            if (shape.white)
-            {
-                double y0 = pitchToPixelY(pitch + shape.marginTop);
-                double y1 = pitchToPixelY(pitch + shape.marginBottom);
-
-                QRect bounds(0, y0, width(), y1 - y0);
-                p->fillRect(bounds, pitch == m_curKeyPressed ? m_colorKeyHighlight : m_colorKeyWhite);
-//                p->drawLine(0, y0, width(), y0);
-                p->drawRect(bounds);
-
-                if (degree == 0 && font.pointSize() + 2 < bounds.height())
-                {
-                    int octave = (pitch - transp.chromatic) / 12 - 1;
-                    QString text = "C" + QString::number(octave);
-                    p->drawText(bounds, Qt::AlignBottom | Qt::AlignRight, text);
-                }
-            }
-        }
-
-        //Black keys
-        for (int pitch = 0; pitch < PianorollView::NUM_PITCHES; ++pitch)
-        {
-            int degree = (pitch - transp.chromatic + 60) % 12;
-            const KeyShape& shape = keyShapes[degree];
-            if (!shape.white)
-            {
-                double y0 = pitchToPixelY(pitch + shape.marginTop);
-                double y1 = pitchToPixelY(pitch + shape.marginBottom);
-
-                p->fillRect(0, y0, width() * .6, y1 - y0, pitch == m_curKeyPressed ? m_colorKeyHighlight : m_colorKeyBlack);
-                p->drawRect(0, y0, width() * .6, y1 - y0);
-            }
-
-        }
-
-    }
-}
-
-
-void PianorollKeyboard::mousePressEvent(QMouseEvent* event)
-{
-    int pitch = pixelYToPitch(event->pos().y());
-
-    if (pitch < 0 || pitch > 127)
-        pitch = -1;
-
-    m_curKeyPressed = pitch;
-
-    uint modifiers = QGuiApplication::keyboardModifiers();
-    bool bnCtrl = modifiers & Qt::ControlModifier;
-    if (bnCtrl)
-    {
-        //        emit pitchHighlightToggled(pitch);
-        bool highlight = controller()->isPitchHighlight(pitch);
-        controller()->setPitchHighlight(pitch, !highlight);
-    }
-    else
-        emit keyPressed(pitch);
-
-    update();
-}
-
-
-void PianorollKeyboard::mouseReleaseEvent(QMouseEvent*)
-{
-    if (m_curKeyPressed != -1)
-    {
-        emit keyReleased(m_curKeyPressed);
-        m_curKeyPressed = -1;
-        update();
-    }
-}
-
-
-void PianorollKeyboard::mouseMoveEvent(QMouseEvent* event)
-{
-    int pitch = pixelYToPitch(event->pos().y());
-
-    if (pitch < 0 || pitch > 127)
-        pitch = -1;
-
-    if (pitch != m_curPitch) {
-        m_curPitch = pitch;
-
-//        //Set tooltip
-//        int degree = curPitch % 12;
-//        int octave = curPitch / 12;
-//        QString text = qApp->translate("utils", pitchNames[degree]) + QString::number(octave - 1);
-//        Part* part = _staff->part();
-//        Drumset* ds = part->instrument()->drumset();
-//        if (ds)
-//            text += " - " + qApp->translate("drumset", ds->name(curPitch).toUtf8().constData());
-
-//        setToolTip(text);
-
-        //Send event
-//        emit pitchChanged(m_curPitch);
-
-        if ((m_curKeyPressed != -1) && (m_curKeyPressed != pitch))
-        {
-            emit keyReleased(m_curKeyPressed);
-            m_curKeyPressed = pitch;
-            emit keyPressed(m_curKeyPressed);
-        }
-
-        update();
     }
 }
