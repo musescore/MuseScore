bbb1e025bdf462c6f88db55af0733d939603e41e
Mark McKay <mark@kitfox.com>
2021-09-01 12:15:05 -0400

    Creating component for automation data.

---
+++ src/pianoroll/view/pianorollautomationeditor.cpp
@@ -0,0 +1,163 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "pianorollautomationeditor.h"
+
+#include <QPainter>
+
+#include "audio/iplayer.h"
+
+using namespace mu::pianoroll;
+
+
+PianorollAutomationEditor::PianorollAutomationEditor(QQuickItem* parent)
+    : QQuickPaintedItem(parent)
+{
+
+}
+
+void PianorollAutomationEditor::load()
+{
+    onNotationChanged();
+    globalContext()->currentNotationChanged().onNotify(this, [this]() {
+        onNotationChanged();
+    });
+
+    playback()->player()->playbackPositionMsecs().onReceive(this, [this](audio::TrackSequenceId currentTrackSequence, const audio::msecs_t newPosMsecs) {
+        int tick = score()->utime2utick(newPosMsecs / 1000.);
+        setPlaybackPosition(Ms::Fraction::fromTicks(tick));
+    });
+}
+
+void PianorollAutomationEditor::onNotationChanged()
+{
+    auto notation = globalContext()->currentNotation();
+    if (notation)
+    {
+        notation->notationChanged().onNotify(this, [this]() {
+            onCurrentNotationChanged();
+        });
+    }
+
+    updateBoundingSize();
+}
+
+void PianorollAutomationEditor::onCurrentNotationChanged()
+{
+    updateBoundingSize();
+}
+
+Ms::Score* PianorollAutomationEditor::score()
+{
+    notation::INotationPtr notation = globalContext()->currentNotation();
+    if (!notation)
+    {
+        return nullptr;
+    }
+
+    //Find staff to draw from
+    Ms::Score* score = notation->elements()->msScore();
+    return score;
+}
+
+void PianorollAutomationEditor::setPlaybackPosition(Ms::Fraction value)
+{
+    if (value == m_playbackPosition)
+        return;
+    m_playbackPosition = value;
+    update();
+
+    emit playbackPositionChanged();
+}
+
+void PianorollAutomationEditor::setWholeNoteWidth(double value)
+{
+    if (value == m_wholeNoteWidth)
+        return;
+    m_wholeNoteWidth = value;
+    updateBoundingSize();
+
+    emit wholeNoteWidthChanged();
+}
+
+void PianorollAutomationEditor::setCenterX(double value)
+{
+    if (value == m_centerX)
+        return;
+    m_centerX = value;
+    update();
+
+    emit centerXChanged();
+}
+
+void PianorollAutomationEditor::setDisplayObjectWidth(double value)
+{
+    if (value == m_displayObjectWidth)
+        return;
+    m_displayObjectWidth = value;
+    updateBoundingSize();
+
+    emit displayObjectWidthChanged();
+}
+
+void PianorollAutomationEditor::updateBoundingSize()
+{
+    notation::INotationPtr notation = globalContext()->currentNotation();
+    if (!notation) {
+        return;
+    }
+
+    Ms::Score* score = notation->elements()->msScore();
+    Ms::Measure* lm = score->lastMeasure();
+    Ms::Fraction wholeNotesFrac = lm->tick() + lm->ticks();
+    double wholeNotes = wholeNotesFrac.numerator() / (double)wholeNotesFrac.denominator();
+
+    setDisplayObjectWidth(wholeNotes * m_wholeNoteWidth);
+
+    update();
+}
+
+int PianorollAutomationEditor::wholeNoteToPixelX(double tick) const
+{
+    return tick * m_wholeNoteWidth - m_centerX * m_displayObjectWidth + width() / 2;
+}
+
+double PianorollAutomationEditor::pixelXToWholeNote(int pixX) const
+{
+    return (pixX + m_centerX * m_displayObjectWidth - width() / 2) / m_wholeNoteWidth;
+}
+
+
+void PianorollAutomationEditor::paint(QPainter* p)
+{
+    p->fillRect(0, 0, width(), height(), m_colorBackground);
+
+    notation::INotationPtr notation = globalContext()->currentNotation();
+    if (!notation) {
+        return;
+    }
+
+    Ms::Score* score = notation->elements()->msScore();
+    Ms::Staff* staff = score->staff(0);
+}
+
+
