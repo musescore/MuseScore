8a153634c417a91fe7db6490fb98fe52b59c6698
Mark McKay <mark@kitfox.com>
2021-09-03 01:22:39 -0400

    Can now drag values in automation editor.

--- src/pianoroll/view/automation/automationvelocity.cpp
+++ src/pianoroll/view/automation/automationvelocity.cpp
@@ -1,5 +1,7 @@
 #include "automationvelocity.h"
 
+#include "libmscore/undo.h"
+
 using namespace mu::pianoroll;
 
 AutomationVelocity::AutomationVelocity()
@@ -14,15 +16,57 @@ PianorollAutomationEditor::AutomationType AutomationVelocity::type()
 
 double AutomationVelocity::maxValue()
 {
-    return 100;
+    return 200;
 }
 
 double AutomationVelocity::minValue()
 {
-    return 0;
+    return -200;
 }
 
-double AutomationVelocity::value(NoteEventBlock& block)
+double AutomationVelocity::value(Ms::Staff* staff, NoteEventBlock& block)
 {
-    return 50;
+    Ms::Note* note = block.note;
+
+    //Change velocity to equivalent in new metric
+    switch (note->veloType())
+    {
+    case Ms::Note::ValueType::USER_VAL:
+    {
+        int dynamicsVel = staff->velocities().val(note->tick());
+        return static_cast<int>((note->veloOffset() / (qreal)dynamicsVel - 1) * 100);
+    }
+    default:
+    case Ms::Note::ValueType::OFFSET_VAL:
+        return note->veloOffset();
+    }
+}
+
+void AutomationVelocity::setValue(Ms::Staff* staff, NoteEventBlock& block, double value)
+{
+    Ms::Score* score = staff->score();
+    Ms::Note* note = block.note;
+
+    score->startCmd();
+
+    switch (note->veloType())
+    {
+    case Ms::Note::ValueType::USER_VAL:
+    {
+        int dynamicsVel = staff->velocities().val(note->tick());
+        int newVelocity = static_cast<int>(dynamicsVel * (1 + value / 100.0));
+
+        score->undo(new Ms::ChangeVelocity(note, Ms::Note::ValueType::USER_VAL, newVelocity));
+
+        break;
+        }
+    default:
+    case Ms::Note::ValueType::OFFSET_VAL:
+    {
+        score->undo(new Ms::ChangeVelocity(note, Ms::Note::ValueType::OFFSET_VAL, value));
+        break;
+        }
+    }
+
+    score->endCmd();
 }
