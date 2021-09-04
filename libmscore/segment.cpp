1c3e9b997d66681f039e1a2d95f319e7162f8639
Marc Sabatella <marc@outsideshore.com>
2021-09-04 13:22:25 +0200

    improve horizontal spacing & vertical alignment
    Duplicate of #6312, part1. Expected vtest differences.

--- libmscore/segment.cpp
+++ libmscore/segment.cpp
@@ -2231,22 +2231,32 @@ qreal Segment::minHorizontalDistance(Segment* ns, bool systemHeaderGap) const
                   w = ww + score()->styleP(Sid::clefLeftMargin);
                   }
             else {
-                  bool isGap = false;
+                  bool possibleGap = false;
+                  bool notGap = false;
+                  bool isAdjacent = true;
                   for (int i = 0; i < score()->nstaves() * VOICES; i++) {
                         Element* el = element(i);
                         if (!el)
                               continue;
                         if (el->isRest() && toRest(el)->isGap())
-                              isGap = true;
+                              possibleGap = true;
                         else {
-                              isGap = false;
-                              break;
+                              notGap = true;
+                              if (ns && ns->element(i)) {
+                                    isAdjacent = true;
+                                    break;
+                                    }
                               }
                         }
-                  if (isGap)
+                  if (possibleGap && !notGap)
                         return 0.0;
-                  // minimum distance between notes is one note head width
-                  w = qMax(w, score()->noteHeadWidth()) + score()->styleP(Sid::minNoteDistance);
+                  // minimum distance between notes was one note head width
+                  // this should not be necessary given we are calculating actual segment distances
+                  // also, minNoteDistance is only needed in cases where there are adjacent notes
+                  // in the same voice
+                  //w = qMax(w, score()->noteHeadWidth()) + score()->styleP(Sid::minNoteDistance);
+                  if (isAdjacent)
+                        w = w + score()->styleP(Sid::minNoteDistance);
                   }
             }
       else if (nst == SegmentType::ChordRest) {
