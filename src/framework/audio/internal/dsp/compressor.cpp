f8748b32e849a0e2f3b5b3fa30de2bcb260488e0
vpereverzev <v.pereverzev@wsmgroup.ru>
2021-09-13 19:38:11 +0200

    Implemented an internal compressor

--- src/framework/audio/internal/dsp/compressor.cpp
+++ src/framework/audio/internal/dsp/compressor.cpp
@@ -1,43 +1,68 @@
 #include "compressor.h"
 
-#include "internal/audiomathutils.h"
+#include "log.h"
+
+#include "audiomathutils.h"
 
 using namespace mu::audio;
 using namespace mu::audio::dsp;
 
-static constexpr float HALF_KNEE = 3.f;
-static constexpr float RATIO = 1.f / 60.f;
-static constexpr volume_db_t THRESHOLD = -10.f;
-static constexpr float UPPER_KNEE_BOUNDARY = THRESHOLD + HALF_KNEE;
-static constexpr float LOWER_KNEE_BOUNDARY = THRESHOLD - HALF_KNEE;
-static constexpr float QUADRATIC_LOWER_KNEE_BOUNDARY = LOWER_KNEE_BOUNDARY * LOWER_KNEE_BOUNDARY;
-static constexpr float ATTACK_PHASE = 10.f;
-static constexpr float RELEASE_PHASE = 100.f;
-static constexpr float DIRECTION = -1.f;
+static constexpr float RATIO = 4.f;
 
-inline float calculateGainReduction(const volume_db_t& currentAmplitudeDb)
+
+Compressor::Compressor(const unsigned int sampleRate)
+    : m_filterConfig(sampleRate, RATIO),
+      m_feedbackFactor(1.f) // 0 = feed forward, 1 = feed back
 {
-    if (currentAmplitudeDb < UPPER_KNEE_BOUNDARY) {
-        return gainFromDecibels(currentAmplitudeDb + QUADRATIC_LOWER_KNEE_BOUNDARY * (RATIO - 1.f) / (4.f * HALF_KNEE));
-    } else {
-        return gainFromDecibels(THRESHOLD + (currentAmplitudeDb - THRESHOLD) * RATIO);
-    }
 }
 
-void Compressor::process(float* buffer, const samples_t& samplesPerChannel, const float& audioChannelRms,
-                         const audioch_t channelNumber, const audioch_t audioChannelsCount)
+volume_db_t Compressor::gainSmoothing(const float& newGainReduction) const
 {
-    volume_dbfs_t currentAmplitudeDb = dbFullScaleFromSample(audioChannelRms);
+    float coefficient = 0.f;
 
-    if (currentAmplitudeDb < LOWER_KNEE_BOUNDARY) {
-        return;
+    if (newGainReduction <= m_previousGainReduction) {
+        coefficient = m_filterConfig.attackTimeCoefficient();
+    } else {
+        coefficient = m_filterConfig.releaseTimeCoefficient();
     }
 
-    float gainReduction = calculateGainReduction(currentAmplitudeDb);
+    return (coefficient * m_previousGainReduction) + ((1 - coefficient) * newGainReduction);
+}
 
-    for (samples_t i = 0; i < samplesPerChannel; ++i) {
-        int idx = i * audioChannelsCount + channelNumber;
-
-        buffer[idx] = buffer[idx] * gainReduction;
+volume_db_t Compressor::computeGain(const volume_db_t& logarithmSample) const
+{
+    if (logarithmSample < m_softThresholdLower) {
+        return logarithmSample;
     }
+
+    if (logarithmSample >= m_softThresholdLower && logarithmSample <= m_softThresholdUpper) {
+        return logarithmSample
+               + ((((1 / m_filterConfig.ratio()) - 1) * std::pow(logarithmSample - m_softThresholdUpper, 2))
+                  / (2 * m_filterConfig.kneeWidth()));
+    }
+
+    return m_filterConfig.logarithmicThreshold()
+           + ((logarithmSample - m_filterConfig.logarithmicThreshold()) / m_filterConfig.ratio());
+}
+
+void Compressor::process(const float linearRms, float* buffer, const audioch_t& audioChannelsCount, const samples_t samplesPerChannel)
+{
+    float dbGain = dbFromSample(linearRms);
+
+    // trying to predict the next sample by the previous gain reduction
+    dbGain += m_feedbackFactor * m_feedbackGain;
+
+    float dbDiff = computeGain(dbGain) - dbGain;
+
+    m_feedbackGain = dbDiff;
+    float gainFact = linearFromDecibels(dbDiff * (1.f + m_feedbackFactor));
+
+    float currentGainReduction = std::min(gainFact, m_previousGainReduction);
+
+    // apply gain
+    for (audioch_t audioChNum = 0; audioChNum < audioChannelsCount; ++audioChNum) {
+        multiplySamples(buffer, audioChannelsCount, audioChNum, samplesPerChannel, currentGainReduction);
+    }
+
+    m_previousGainReduction = currentGainReduction;
 }
