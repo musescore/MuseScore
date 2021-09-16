d492b70776a254a1ff97b36cb474833c4f8f456a
Roman Pudashkin <r.pudashkin@gmail.com>
2021-09-16 11:03:44 +0200

    implemented the basic model for editing lines

---
+++ src/inspector/models/notation/lines/linesettingsmodel.h
@@ -0,0 +1,119 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_INSPECTOR_LINESETTINGSMODEL_H
+#define MU_INSPECTOR_LINESETTINGSMODEL_H
+
+#include "models/abstractinspectormodel.h"
+
+namespace mu::inspector {
+class LineSettingsModel : public AbstractInspectorModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(PropertyItem * lineStyle READ lineStyle CONSTANT)
+    Q_PROPERTY(PropertyItem * placement READ placement CONSTANT)
+
+    Q_PROPERTY(PropertyItem * thickness READ thickness CONSTANT)
+    Q_PROPERTY(PropertyItem * dashLineLength READ dashLineLength CONSTANT)
+    Q_PROPERTY(PropertyItem * dashGapLength READ dashGapLength CONSTANT)
+
+    Q_PROPERTY(PropertyItem * lineVisible READ lineVisible CONSTANT)
+    Q_PROPERTY(PropertyItem * isDiagonalLocked READ isDiagonalLocked CONSTANT)
+
+    Q_PROPERTY(PropertyItem * beginingText READ beginingText CONSTANT)
+    Q_PROPERTY(PropertyItem * beginingTextHorizontalOffset READ beginingTextHorizontalOffset CONSTANT)
+    Q_PROPERTY(PropertyItem * beginingTextVerticalOffset READ beginingTextVerticalOffset CONSTANT)
+
+    Q_PROPERTY(PropertyItem * continiousText READ continiousText CONSTANT)
+    Q_PROPERTY(PropertyItem * continiousTextHorizontalOffset READ continiousTextHorizontalOffset CONSTANT)
+    Q_PROPERTY(PropertyItem * continiousTextVerticalOffset READ continiousTextVerticalOffset CONSTANT)
+
+    Q_PROPERTY(PropertyItem * endText READ endText CONSTANT)
+    Q_PROPERTY(PropertyItem * endTextHorizontalOffset READ endTextHorizontalOffset CONSTANT)
+    Q_PROPERTY(PropertyItem * endTextVerticalOffset READ endTextVerticalOffset CONSTANT)
+
+public:
+    explicit LineSettingsModel(QObject* parent, IElementRepositoryService* repository);
+
+    PropertyItem* lineStyle() const;
+    PropertyItem* placement() const;
+
+    PropertyItem* thickness() const;
+    PropertyItem* dashLineLength() const;
+    PropertyItem* dashGapLength() const;
+
+    PropertyItem* lineVisible() const;
+    PropertyItem* isDiagonalLocked() const;
+
+    PropertyItem* beginingText() const;
+    PropertyItem* beginingTextHorizontalOffset() const;
+    PropertyItem* beginingTextVerticalOffset() const;
+
+    PropertyItem* continiousText() const;
+    PropertyItem* continiousTextHorizontalOffset() const;
+    PropertyItem* continiousTextVerticalOffset() const;
+
+    PropertyItem* endText() const;
+    PropertyItem* endTextHorizontalOffset() const;
+    PropertyItem* endTextVerticalOffset() const;
+
+protected:
+    enum TextType {
+        BeginingText,
+        ContiniousText,
+        EndText
+    };
+
+    void createProperties() override;
+    void loadProperties() override;
+    void resetProperties() override;
+
+    virtual void onUpdateLinePropertiesAvailability();
+
+    virtual bool isTextVisible(TextType type) const;
+
+private:
+    PropertyItem* m_lineStyle = nullptr;
+    PropertyItem* m_placement = nullptr;
+
+    PropertyItem* m_thickness = nullptr;
+    PropertyItem* m_dashLineLength = nullptr;
+    PropertyItem* m_dashGapLength = nullptr;
+
+    PropertyItem* m_lineVisible = nullptr;
+    PropertyItem* m_isDiagonalLocked = nullptr;
+
+    PropertyItem* m_beginingText = nullptr;
+    PropertyItem* m_beginingTextHorizontalOffset = nullptr;
+    PropertyItem* m_beginingTextVerticalOffset = nullptr;
+
+    PropertyItem* m_continiousText = nullptr;
+    PropertyItem* m_continiousTextHorizontalOffset = nullptr;
+    PropertyItem* m_continiousTextVerticalOffset = nullptr;
+
+    PropertyItem* m_endText = nullptr;
+    PropertyItem* m_endTextHorizontalOffset = nullptr;
+    PropertyItem* m_endTextVerticalOffset = nullptr;
+};
+}
+
+#endif // MU_INSPECTOR_LINESETTINGSMODEL_H
