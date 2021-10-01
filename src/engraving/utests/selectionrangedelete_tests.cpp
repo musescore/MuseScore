30a76730b53e6de64978b456944f6b0e5874e0ad
Igor Korsukov <igor.korsukov@gmail.com>
2021-10-01 11:16:15 +0200

    moved engraving selectionrangedelete tests to gtest

--- src/engraving/utests/selectionrangedelete_tests.cpp
+++ src/engraving/utests/selectionrangedelete_tests.cpp
@@ -20,93 +20,58 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "testing/qtestsuite.h"
-#include "testbase.h"
+#include <gtest/gtest.h>
 
 #include "libmscore/masterscore.h"
 #include "libmscore/measure.h"
 #include "libmscore/undo.h"
 
+#include "utils/scorerw.h"
+#include "utils/scorecomp.h"
+
 static const QString SELRANGEDELETE_DATA_DIR("selectionrangedelete_data/");
 
 using namespace Ms;
+using namespace mu::engraving;
 
-//---------------------------------------------------------
-//   TestSelectionRangeDelete
-//---------------------------------------------------------
-
-class TestSelectionRangeDelete : public QObject, public MTest
+class SelectionRangeDeleteTests : public ::testing::Test
 {
-    Q_OBJECT
+public:
     void verifyDelete(MasterScore* score, size_t spanners);
     void verifyNoDelete(MasterScore* score, size_t spanners);
     void deleteVoice(int voice, QString idx);
-
-private slots:
-    void initTestCase();
-    void deleteSegmentWithSlur();
-    void deleteSegmentWithSpanner();
-    void deleteVoice1() { deleteVoice(0, "03"); }
-    void deleteVoice2() { deleteVoice(1, "04"); }
-    void deleteSkipAnnotations();
 };
 
-//---------------------------------------------------------
-//   initTestCase
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::initTestCase()
-{
-    initMTest();
-}
-
-//---------------------------------------------------------
-//   verifyDelete
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::verifyDelete(MasterScore* score, size_t spanners)
+void SelectionRangeDeleteTests::verifyDelete(MasterScore* score, size_t spanners)
 {
     score->startCmd();
     score->cmdDeleteSelection();
     score->endCmd();
 
-    QVERIFY(score->spanner().size() == spanners - 1);
+    EXPECT_EQ(score->spanner().size(), spanners - 1);
     score->undoRedo(true, 0);
-    QVERIFY(score->spanner().size() == spanners);
+    EXPECT_EQ(score->spanner().size(), spanners);
 }
 
-//---------------------------------------------------------
-//   verifyNoDelete
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::verifyNoDelete(MasterScore* score, size_t spanners)
+void SelectionRangeDeleteTests::verifyNoDelete(MasterScore* score, size_t spanners)
 {
     score->startCmd();
     score->cmdDeleteSelection();
     score->endCmd();
 
-    QVERIFY(score->spanner().size() == spanners);
+    EXPECT_EQ(score->spanner().size(), spanners);
     score->undoRedo(true, 0);
-    QVERIFY(score->spanner().size() == spanners);
+    EXPECT_EQ(score->spanner().size(), spanners);
 }
 
-//---------------------------------------------------------
-//   chordRestAtBeat
-//---------------------------------------------------------
-
-EngravingItem* chordRestAtBeat(Score* score, int beat, int half = 0)
+static EngravingItem* chordRestAtBeat(Score* score, int beat, int half = 0)
 {
-    qDebug("Chordrest at beat %i,%i", beat, half);
     int division = MScore::division;
     int tick = beat * division + half * division / 2;
     return score->tick2segment(Fraction::fromTicks(tick), false, SegmentType::ChordRest, false)->element(0);
 }
 
-//---------------------------------------------------------
-//   deleteSegmentWithSlur
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::deleteSegmentWithSlur()
+TEST_F(SelectionRangeDeleteTests, deleteSegmentWithSlur)
 {
     /*
      *  Score looks like this:
@@ -115,10 +80,10 @@ void TestSelectionRangeDelete::deleteSegmentWithSlur()
      *  ss es ss   es
      *  q  q  q  e e
      */
-    MasterScore* score = readScore(SELRANGEDELETE_DATA_DIR + "selectionrangedelete01.mscx");
+    MasterScore* score = ScoreRW::readScore(SELRANGEDELETE_DATA_DIR + "selectionrangedelete01.mscx");
+    EXPECT_TRUE(score);
 
     score->doLayout();
-    QVERIFY(score);
     size_t spanners = score->spanner().size();
 
     score->select(chordRestAtBeat(score, 0), SelectType::RANGE);
@@ -144,11 +109,7 @@ void TestSelectionRangeDelete::deleteSegmentWithSlur()
     delete score;
 }
 
-//---------------------------------------------------------
-//   deleteSegmentWithSpanner
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::deleteSegmentWithSpanner()
+TEST_F(SelectionRangeDeleteTests, deleteSegmentWithSpanner)
 {
     /*
      *  Score looks like this:
@@ -157,10 +118,11 @@ void TestSelectionRangeDelete::deleteSegmentWithSpanner()
      *  ss    es
      *  q  q  q
      */
-    MasterScore* score = readScore(SELRANGEDELETE_DATA_DIR + "selectionrangedelete02.mscx");
+    MasterScore* score = ScoreRW::readScore(SELRANGEDELETE_DATA_DIR + "selectionrangedelete02.mscx");
+    EXPECT_TRUE(score);
 
     score->doLayout();
-    QVERIFY(score);
+
     size_t spanners = score->spanner().size();
 
     score->select(chordRestAtBeat(score, 0), SelectType::RANGE);
@@ -184,16 +146,13 @@ void TestSelectionRangeDelete::deleteSegmentWithSpanner()
     delete score;
 }
 
-//---------------------------------------------------------
-//   deleteVoice
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::deleteVoice(int voice, QString idx)
+void SelectionRangeDeleteTests::deleteVoice(int voice, QString idx)
 {
-    MasterScore* score = readScore(SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete%1.mscx").arg(idx));
+    MasterScore* score = ScoreRW::readScore(SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete%1.mscx").arg(idx));
+    EXPECT_TRUE(score);
 
     Measure* m1 = score->firstMeasure();
-    QVERIFY(m1);
+    EXPECT_TRUE(m1);
 
     SelectionFilterType voiceFilterType = SelectionFilterType((int)SelectionFilterType::FIRST_VOICE + voice);
     score->selectionFilter().setFiltered(voiceFilterType, false);
@@ -205,21 +164,28 @@ void TestSelectionRangeDelete::deleteVoice(int voice, QString idx)
 
     score->doLayout();
 
-    QVERIFY(saveCompareScore(score, QString("selectionrangedelete%1.mscx").arg(idx),
-                             SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete%1-ref.mscx").arg(idx)));
+    EXPECT_TRUE(ScoreComp::saveCompareScore(score, QString("selectionrangedelete%1.mscx").arg(idx),
+                                            SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete%1-ref.mscx").arg(idx)));
     delete score;
 }
 
-//---------------------------------------------------------
-//   deleteSkipAnnotations
-//---------------------------------------------------------
-
-void TestSelectionRangeDelete::deleteSkipAnnotations()
+TEST_F(SelectionRangeDeleteTests, deleteVoice1)
 {
-    MasterScore* score = readScore(SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete05.mscx"));
+    deleteVoice(0, "03");
+}
+
+TEST_F(SelectionRangeDeleteTests, deleteVoice2)
+{
+    deleteVoice(1, "04");
+}
+
+TEST_F(SelectionRangeDeleteTests, deleteSkipAnnotations)
+{
+    MasterScore* score = ScoreRW::readScore(SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete05.mscx"));
+    EXPECT_TRUE(score);
 
     Measure* m1 = score->firstMeasure();
-    QVERIFY(m1);
+    EXPECT_TRUE(m1);
 
     SelectionFilterType annotationFilterType = SelectionFilterType((int)SelectionFilterType::CHORD_SYMBOL);
     score->selectionFilter().setFiltered(annotationFilterType, false);
@@ -231,11 +197,7 @@ void TestSelectionRangeDelete::deleteSkipAnnotations()
 
     score->doLayout();
 
-    QVERIFY(saveCompareScore(score, QString("selectionrangedelete05.mscx"),
-                             SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete05-ref.mscx")));
+    EXPECT_TRUE(ScoreComp::saveCompareScore(score, QString("selectionrangedelete05.mscx"),
+                                            SELRANGEDELETE_DATA_DIR + QString("selectionrangedelete05-ref.mscx")));
     delete score;
 }
-
-QTEST_MAIN(TestSelectionRangeDelete)
-
-#include "tst_selectionrangedelete.moc"
