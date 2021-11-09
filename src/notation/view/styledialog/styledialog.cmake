ebbd3e10aa24737f499998478ac478223e9888f1
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2021-11-09 21:41:24 +0100

    Implemented AbstractStyleDialogModel and StyleItem classes

---
+++ src/notation/view/styledialog/styledialog.cmake
@@ -0,0 +1,6 @@
+set(STYLEDIALOG_SRC
+    ${CMAKE_CURRENT_LIST_DIR}/abstractstyledialogmodel.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/abstractstyledialogmodel.h
+    ${CMAKE_CURRENT_LIST_DIR}/styleitem.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/styleitem.h
+    )
