58d9183c2bd4a78754d6a31fb9205a365585e65c
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2021-12-14 00:10:49 +0100

    Replace deprecated `QVariant::Type` with `QMetaType::Type`

--- src/framework/actions/api/dispatcherapi.cpp
+++ src/framework/actions/api/dispatcherapi.cpp
@@ -37,23 +37,10 @@ void DispatcherApi::dispatch(const QString& action, const QVariantList& args)
     ActionData data;
     int index = 0;
 
-#ifdef MU_QT5_COMPAT
-    for (const QVariant& arg : args) {
-        switch (arg.type()) {
-        case QVariant::Int: data.setArg<int>(index, arg.value<int>());
-            break;
-        default: {
-            LOGE() << "unknown type: " << arg.typeName();
-            continue;
-        } break;
-        }
-
-        ++index;
-    }
-#else
     for (const QVariant& arg : args) {
         switch (arg.typeId()) {
-        case QMetaType::Int: data.setArg<int>(index, arg.value<int>());
+        case QMetaType::Int:
+            data.setArg<int>(index, arg.value<int>());
             break;
         default: {
             LOGE() << "unknown type: " << arg.typeName();
@@ -63,7 +50,6 @@ void DispatcherApi::dispatch(const QString& action, const QVariantList& args)
 
         ++index;
     }
-#endif
 
     dispatcher()->dispatch(action.toStdString(), data);
 }
