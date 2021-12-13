0b4d5eef285b61c97b9c2bc84f1e5603337980c3
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2021-12-13 02:04:56 +0100

    Remove all traces of Telemetry from build scripts et cetera

--- build/ci/linux/build.sh
+++ build/ci/linux/build.sh
@@ -26,7 +26,6 @@ trap 'echo Build failed; exit 1' ERR
 df -h .
 
 ARTIFACTS_DIR=build.artifacts
-TELEMETRY_TRACK_ID=""
 CRASH_REPORT_URL=""
 BUILD_MODE=""
 SUFFIX="" # appended to `mscore` command name to avoid conflicts (e.g. `mscore-dev`)
@@ -35,7 +34,6 @@ YOUTUBE_API_KEY=""
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -n|--number) BUILD_NUMBER="$2"; shift ;;
-        --telemetry) TELEMETRY_TRACK_ID="$2"; shift ;;
         --crash_log_url) CRASH_REPORT_URL="$2"; shift ;;
         --build_mode) BUILD_MODE="$2"; shift ;;
         --youtube_api_key) YOUTUBE_API_KEY="$2"; shift ;;
@@ -45,7 +43,6 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ -z "$BUILD_NUMBER" ]; then echo "error: not set BUILD_NUMBER"; exit 1; fi
-if [ -z "$TELEMETRY_TRACK_ID" ]; then TELEMETRY_TRACK_ID=""; fi
 if [ -z "$BUILD_MODE" ]; then BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env); fi
 if [ -z "$YOUTUBE_API_KEY" ]; then YOUTUBE_API_KEY=""; fi
 
@@ -61,7 +58,6 @@ esac
 
 echo "MUSESCORE_BUILD_CONFIG: $MUSESCORE_BUILD_CONFIG"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
-echo "TELEMETRY_TRACK_ID: $TELEMETRY_TRACK_ID"
 echo "CRASH_REPORT_URL: $CRASH_REPORT_URL"
 echo "BUILD_MODE: $BUILD_MODE"
 echo "YOUTUBE_API_KEY: $YOUTUBE_API_KEY"
@@ -94,7 +90,6 @@ MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG \
 MUSESCORE_INSTALL_SUFFIX=$SUFFIX \
 MUSESCORE_BUILD_NUMBER=$BUILD_NUMBER \
 MUSESCORE_REVISION=$MUSESCORE_REVISION \
-MUSESCORE_TELEMETRY_ID=$TELEMETRY_TRACK_ID \
 MUSESCORE_CRASHREPORT_URL=$CRASH_REPORT_URL \
 MUSESCORE_BUILD_VST=$BUILD_VST \
 MUSESCORE_VST3_SDK_PATH=$VST3_SDK_PATH \
--- build/ci/macos/build.sh
+++ build/ci/macos/build.sh
@@ -24,14 +24,12 @@ trap 'echo Build failed; exit 1' ERR
 SKIP_ERR=true
 
 ARTIFACTS_DIR=build.artifacts
-TELEMETRY_TRACK_ID=""
 CRASH_REPORT_URL=""
 YOUTUBE_API_KEY=""
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -n|--number) BUILD_NUMBER="$2"; shift ;;
-        --telemetry) TELEMETRY_TRACK_ID="$2"; shift ;;
         --crash_log_url) CRASH_REPORT_URL="$2"; shift ;;
         --youtube_api_key) YOUTUBE_API_KEY="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
@@ -40,7 +38,6 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ -z "$BUILD_NUMBER" ]; then echo "error: not set BUILD_NUMBER"; exit 1; fi
-if [ -z "$TELEMETRY_TRACK_ID" ]; then TELEMETRY_TRACK_ID=""; fi
 if [ -z "$YOUTUBE_API_KEY" ]; then YOUTUBE_API_KEY=""; fi
 
 BUILD_MODE=$(cat $ARTIFACTS_DIR/env/build_mode.env)
@@ -59,7 +56,6 @@ fi
 
 echo "MUSESCORE_BUILD_CONFIG: $MUSESCORE_BUILD_CONFIG"
 echo "BUILD_NUMBER: $BUILD_NUMBER"
-echo "TELEMETRY_TRACK_ID: $TELEMETRY_TRACK_ID"
 echo "CRASH_REPORT_URL: $CRASH_REPORT_URL"
 echo "VST3_SDK_PATH: $VST3_SDK_PATH"
 echo "YOUTUBE_API_KEY: $YOUTUBE_API_KEY"
@@ -70,7 +66,6 @@ MUSESCORE_INSTALL_DIR="../applebuild" \
 MUSESCORE_BUILD_CONFIG=$MUSESCORE_BUILD_CONFIG \
 MUSESCORE_BUILD_NUMBER=$BUILD_NUMBER \
 MUSESCORE_REVISION=$MUSESCORE_REVISION \
-MUSESCORE_TELEMETRY_ID=$TELEMETRY_TRACK_ID \
 MUSESCORE_CRASHREPORT_URL=$CRASH_REPORT_URL \
 MUSESCORE_BUILD_VST=$BUILD_VST \
 MUSESCORE_VST3_SDK_PATH=$VST3_SDK_PATH \
