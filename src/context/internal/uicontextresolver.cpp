e58dc1ddf17fdc664a66bdf4a0e95b733c25807b
Igor Korsukov <igor.korsukov@gmail.com>
2021-12-14 12:03:11 +0200

    fixed notation escape

--- src/context/internal/uicontextresolver.cpp
+++ src/context/internal/uicontextresolver.cpp
@@ -162,10 +162,13 @@ bool UiContextResolver::isShortcutContextAllowed(const std::string& scContext) c
     //!         NotationStaffTab
 
     static const std::string CTX_ANY("any");
+    static const std::string CTX_NOTATION_OPENED("notation-opened");
     static const std::string CTX_NOTATION_STAFF_STANDARD("notation-staff-standard");
     static const std::string CTX_NOTATION_STAFF_TAB("notation-staff-tab");
 
-    if (CTX_NOTATION_STAFF_STANDARD == scContext) {
+    if (CTX_NOTATION_OPENED == scContext) {
+        return matchWithCurrent(context::UiCtxNotationOpened);
+    } else if (CTX_NOTATION_STAFF_STANDARD == scContext) {
         auto notation = globalContext()->currentNotation();
         if (!notation) {
             return false;
