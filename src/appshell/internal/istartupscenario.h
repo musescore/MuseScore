35603ff5136c1ca2a613f51c2e371652c6f3c23b
Roman Pudashkin <r.pudashkin@gmail.com>
2021-12-22 17:08:10 +0200

    fix #9712: fixed opening the score page after opening a score on macOS via double click / context menu

--- src/appshell/internal/istartupscenario.h
+++ src/appshell/internal/istartupscenario.h
@@ -36,7 +36,9 @@ public:
 
     virtual void setSessionType(const QString& sessionType) = 0;
     virtual void setStartupScorePath(const io::path& path) = 0;
+
     virtual void run() = 0;
+    virtual bool startupCompleted() const = 0;
 };
 }
 
