aabf592883e54348cbb9627f1b539c0a9323cc5c
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-01-19 14:05:47 +0200

    Improved dynamic conversions and interpretations

--- src/engraving/playback/utils/dynamicutils.h
+++ src/engraving/playback/utils/dynamicutils.h
@@ -28,45 +28,90 @@
 #include "types/types.h"
 
 namespace mu::engraving {
-inline mpe::DynamicTypeList dynamicType(const DynamicType type)
-{
-    switch (type) {
-    case DynamicType::OTHER: return { mpe::DynamicType::Undefined };
-    case DynamicType::PPPPPP: return { mpe::DynamicType::pppppp };
-    case DynamicType::PPPPP: return { mpe::DynamicType::ppppp };
-    case DynamicType::PPPP: return { mpe::DynamicType::pppp };
-    case DynamicType::PPP: return { mpe::DynamicType::ppp };
-    case DynamicType::PP: return { mpe::DynamicType::pp };
-    case DynamicType::P: return { mpe::DynamicType::p };
-    case DynamicType::MP: return { mpe::DynamicType::mp };
-    case DynamicType::MF: return { mpe::DynamicType::mf };
-    case DynamicType::F: return { mpe::DynamicType::f };
-    case DynamicType::FF: return { mpe::DynamicType::ff };
-    case DynamicType::FFF: return { mpe::DynamicType::fff };
-    case DynamicType::FFFF: return { mpe::DynamicType::ffff };
-    case DynamicType::FFFFF: return { mpe::DynamicType::fffff };
-    case DynamicType::FFFFFF: return { mpe::DynamicType::ffffff };
+struct DynamicTransition {
+    Ms::DynamicType from = Ms::DynamicType::OTHER;
+    Ms::DynamicType to = Ms::DynamicType::OTHER;
 
-    case DynamicType::SFP:
-    case DynamicType::FP:
-        return { mpe::DynamicType::f, mpe::DynamicType::p };
-
-    case DynamicType::SF:
-    case DynamicType::SFZ:
-    case DynamicType::RFZ:
-    case DynamicType::RF:
-    case DynamicType::FZ:
-        return { mpe::DynamicType::f };
-
-    case DynamicType::SFF:
-    case DynamicType::SFFZ:
-        return { mpe::DynamicType::ff };
-
-    case DynamicType::SFPP:
-        return { mpe::DynamicType::f, mpe::DynamicType::pp };
-
-    default: return { mpe::DynamicType::Natural };
+    bool isValid() const
+    {
+        return from != Ms::DynamicType::OTHER && to != Ms::DynamicType::OTHER;
     }
+};
+
+static mpe::dynamic_level_t dynamicLevelFromType(const Ms::DynamicType type)
+{
+    static const std::unordered_map<Ms::DynamicType, mpe::dynamic_level_t> DYNAMIC_LEVELS = {
+        { Ms::DynamicType::PPPPPP, mpe::dynamicLevelFromType(mpe::DynamicType::pppppp) },
+        { Ms::DynamicType::PPPPP, mpe::dynamicLevelFromType(mpe::DynamicType::ppppp) },
+        { Ms::DynamicType::PPPP, mpe::dynamicLevelFromType(mpe::DynamicType::pppp) },
+        { Ms::DynamicType::PPP, mpe::dynamicLevelFromType(mpe::DynamicType::ppp) },
+        { Ms::DynamicType::PP, mpe::dynamicLevelFromType(mpe::DynamicType::pp) },
+        { Ms::DynamicType::P, mpe::dynamicLevelFromType(mpe::DynamicType::p) },
+        { Ms::DynamicType::MP, mpe::dynamicLevelFromType(mpe::DynamicType::mp) },
+        { Ms::DynamicType::MF, mpe::dynamicLevelFromType(mpe::DynamicType::mf) },
+        { Ms::DynamicType::F, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::FF, mpe::dynamicLevelFromType(mpe::DynamicType::ff) },
+        { Ms::DynamicType::FFF, mpe::dynamicLevelFromType(mpe::DynamicType::fff) },
+        { Ms::DynamicType::FFFF, mpe::dynamicLevelFromType(mpe::DynamicType::ffff) },
+        { Ms::DynamicType::FFFFF, mpe::dynamicLevelFromType(mpe::DynamicType::fffff) },
+        { Ms::DynamicType::FFFFFF, mpe::dynamicLevelFromType(mpe::DynamicType::ffffff) },
+        { Ms::DynamicType::SF, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::SFZ, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::SFF, mpe::dynamicLevelFromType(mpe::DynamicType::ff) },
+        { Ms::DynamicType::SFFZ, mpe::dynamicLevelFromType(mpe::DynamicType::ff) },
+        { Ms::DynamicType::RFZ, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::RF, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::FZ, mpe::dynamicLevelFromType(mpe::DynamicType::f) },
+        { Ms::DynamicType::N, mpe::dynamicLevelFromType(mpe::DynamicType::ppppppppp) }
+    };
+
+    auto search = DYNAMIC_LEVELS.find(type);
+
+    if (search != DYNAMIC_LEVELS.cend()) {
+        return search->second;
+    }
+
+    return mpe::dynamicLevelFromType(mpe::DynamicType::Natural);
+}
+
+static bool isOrdinaryDynamicType(const Ms::DynamicType type)
+{
+    static const std::set<Ms::DynamicType> ORDINARY_DYNAMIC_TYPES = {
+        Ms::DynamicType::PPPPPP,
+        Ms::DynamicType::PPPPP,
+        Ms::DynamicType::PPPP,
+        Ms::DynamicType::PPP,
+        Ms::DynamicType::PP,
+        Ms::DynamicType::P,
+        Ms::DynamicType::MP,
+        Ms::DynamicType::MF,
+        Ms::DynamicType::F,
+        Ms::DynamicType::FF,
+        Ms::DynamicType::FFF,
+        Ms::DynamicType::FFFF,
+        Ms::DynamicType::FFFFF,
+        Ms::DynamicType::FFFFFF
+    };
+
+    return ORDINARY_DYNAMIC_TYPES.find(type) != ORDINARY_DYNAMIC_TYPES.cend();
+}
+
+static const DynamicTransition& dynamicTransitionFromType(const Ms::DynamicType type)
+{
+    static const std::unordered_map<Ms::DynamicType, DynamicTransition> DYNAMIC_TRANSITIONS = {
+        { Ms::DynamicType::FP, { Ms::DynamicType::F, Ms::DynamicType::P } },
+        { Ms::DynamicType::PF, { Ms::DynamicType::P, Ms::DynamicType::F } },
+        { Ms::DynamicType::SFP, { Ms::DynamicType::F, Ms::DynamicType::P } },
+        { Ms::DynamicType::SFPP, { Ms::DynamicType::F, Ms::DynamicType::PP } }
+    };
+
+    auto search = DYNAMIC_TRANSITIONS.find(type);
+    if (search != DYNAMIC_TRANSITIONS.cend()) {
+        return search->second;
+    }
+
+    static DynamicTransition empty;
+    return empty;
 }
 }
 
