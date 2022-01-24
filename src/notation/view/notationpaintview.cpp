59055123203627cf08d0f9881aa018376c5e80ee
Roman Pudashkin <r.pudashkin@gmail.com>
2022-01-24 13:42:38 +0200

    optimization: reduced the number of redundant update() calls

--- src/notation/view/notationpaintview.cpp
+++ src/notation/view/notationpaintview.cpp
@@ -350,7 +350,10 @@ void NotationPaintView::onSelectionChanged()
         return;
     }
 
-    adjustCanvasPosition(selectionRect);
+    if (adjustCanvasPosition(selectionRect)) {
+        return;
+    }
+
     update();
 }
 
@@ -646,18 +649,19 @@ qreal NotationPaintView::startVerticalScrollPosition() const
     return (viewport.top() - contentRect.top()) / contentRect.height();
 }
 
-void NotationPaintView::adjustCanvasPosition(const RectF& logicRect)
+bool NotationPaintView::adjustCanvasPosition(const RectF& logicRect)
 {
     TRACEFUNC;
+
     RectF viewRect = viewport();
     RectF showRect = logicRect;
 
     if (viewRect.isEmpty()) {
-        return;
+        return false;
     }
 
     if (viewRect.contains(showRect)) {
-        return;
+        return false;
     }
 
     constexpr int BORDER_SPACING_RATIO = 3;
@@ -691,17 +695,16 @@ void NotationPaintView::adjustCanvasPosition(const RectF& logicRect)
     pos = alignToCurrentPageBorder(showRect, pos);
 
     if (pos == oldPos) {
-        return;
+        return false;
     }
 
-    moveCanvasToPosition(pos);
-
-    update();
+    return moveCanvasToPosition(pos);
 }
 
 void NotationPaintView::ensureViewportInsideScrollableArea()
 {
     TRACEFUNC;
+
     auto [dx, dy] = constraintCanvas(0, 0);
     if (qFuzzyIsNull(dx) && qFuzzyIsNull(dy)) {
         return;
@@ -711,11 +714,12 @@ void NotationPaintView::ensureViewportInsideScrollableArea()
     update();
 }
 
-void NotationPaintView::moveCanvasToPosition(const PointF& logicPos)
+bool NotationPaintView::moveCanvasToPosition(const PointF& logicPos)
 {
     TRACEFUNC;
+
     PointF viewTopLeft = viewportTopLeft();
-    moveCanvas(viewTopLeft.x() - logicPos.x(), viewTopLeft.y() - logicPos.y());
+    return moveCanvas(viewTopLeft.x() - logicPos.x(), viewTopLeft.y() - logicPos.y());
 }
 
 bool NotationPaintView::moveCanvas(qreal dx, qreal dy)
@@ -1032,7 +1036,9 @@ void NotationPaintView::movePlaybackCursor(uint32_t tick)
     }
 
     if (configuration()->isAutomaticallyPanEnabled()) {
-        adjustCanvasPosition(cursorRect);
+        if (adjustCanvasPosition(cursorRect)) {
+            return;
+        }
     }
 
     update(); //! TODO set rect to optimization
