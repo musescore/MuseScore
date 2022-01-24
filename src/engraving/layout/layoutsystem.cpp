735421f9335bf2696c4a4c948d6313857d3f45dd
Michele Spagnolo <spagnolo.michele.1994@gmail.com>
2022-01-24 13:24:50 +0100

    Added some comments

--- src/engraving/layout/layoutsystem.cpp
+++ src/engraving/layout/layoutsystem.cpp
@@ -169,7 +169,10 @@ System* LayoutSystem::collectSystem(const LayoutOptions& options, LayoutContext&
         // check if lc.curMeasure fits, remove if not
         // collect at least one measure and the break
 
-        bool doBreak = (system->measures().size() > 1) && ((curSysWidth + ww) > systemWidth * 1.025);
+        static constexpr acceptanceRange = 1.025; // We allow the initial width of the system to be slightly
+        // larger than the target system width. This avoids having to put a measure in the next system just because
+        // it overshoots the width by a tiny amount.
+        bool doBreak = (system->measures().size() > 1) && ((curSysWidth + ww) > systemWidth * acceptanceRange);
         if (doBreak) {
             breakMeasure = ctx.curMeasure;
             system->removeLastMeasure();
