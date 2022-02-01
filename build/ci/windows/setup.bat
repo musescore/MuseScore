1e58c6a9f6d3971801d530098f813475a1b104fc
Joachim Schmitz <jojo@schmitz-digital.de>
2022-02-01 16:41:35 +0100

    Switch to Qt 5.15.2
    Mainly to fix https://musescore.org/en/node/317323 for macOS. but
    updating the builds for Linux and Windows too.
    Also enables plugins to use ECMA 6 features.

--- build/ci/windows/setup.bat
+++ build/ci/windows/setup.bat
@@ -35,14 +35,14 @@ MKDIR %TEMP_DIR%
 ECHO "=== Install Qt ==="
 
 :: Default for x64
-SET "Qt_ARCHIVE=qt599_msvc2017_64.7z"
+SET "Qt_ARCHIVE=Qt5152_msvc2019_64.7z"
 
 IF %TARGET_PROCESSOR_BITS% == 32 (
-    SET "Qt_ARCHIVE=qt599_msvc2015.7z"
+    SET "Qt_ARCHIVE=Qt5152_msvc2019.7z"
 )
 
 SET "QT_URL=https://s3.amazonaws.com/utils.musescore.org/%Qt_ARCHIVE%"
-SET "QT_DIR=C:\Qt\5.9.9"
+SET "QT_DIR=C:\Qt\5.15.2"
 
 CALL "wget.exe" -q --show-progress --no-check-certificate "%QT_URL%" -O "%TEMP_DIR%\%Qt_ARCHIVE%"
 CALL "7z" x -y "%TEMP_DIR%\%Qt_ARCHIVE%" "-o%QT_DIR%"
