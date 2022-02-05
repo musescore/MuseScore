2c43bd59cd89be68fb0b44284a55751444883a17
Isaac Yong <isaacyong0804@gmail.com>
2022-02-05 16:07:25 +0800

    fix #10385 Play Repeats, Enable Midi, Pan Score Automatically ON by default

--- src/notation/internal/notationconfiguration.cpp
+++ src/notation/internal/notationconfiguration.cpp
@@ -163,9 +163,9 @@ void NotationConfiguration::init()
     fileSystem()->makePath(userStylesPath());
 
     settings()->setDefaultValue(SELECTION_PROXIMITY, Val(2));
-    settings()->setDefaultValue(IS_MIDI_INPUT_ENABLED, Val(false));
+    settings()->setDefaultValue(IS_MIDI_INPUT_ENABLED, Val(true));
     settings()->setDefaultValue(IS_AUTOMATICALLY_PAN_ENABLED, Val(true));
-    settings()->setDefaultValue(IS_PLAY_REPEATS_ENABLED, Val(false));
+    settings()->setDefaultValue(IS_PLAY_REPEATS_ENABLED, Val(true));
     settings()->setDefaultValue(IS_METRONOME_ENABLED, Val(false));
     settings()->setDefaultValue(IS_COUNT_IN_ENABLED, Val(false));
 
