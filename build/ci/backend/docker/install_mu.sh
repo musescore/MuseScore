233cecc43fe0399dd72d87d8711c4527e7a2f7f1
Igor Korsukov <igor.korsukov@gmail.com>
2022-02-17 13:04:06 +0200

    added install mu2 to backend docker

--- build/ci/backend/docker/install_mu.sh
+++ build/ci/backend/docker/install_mu.sh
@@ -5,6 +5,14 @@ MU_DIR=/musescore
 
 mkdir -p $MU_DIR
 
+# MU2
+echo "=== Install MuseScore 2 ==="
+MU2_DISTRO=MuseScore-2.3.2.2022021712
+wget -q --show-progress -O $MU_DIR/$MU2_DISTRO.7z "$S3_URL/$MU2_DISTRO.7z"
+mkdir $MU_DIR/$MU2_DISTRO
+7z x -y $MU_DIR/$MU2_DISTRO.7z -o"$MU_DIR/$MU2_DISTRO/"
+$MU_DIR/$MU2_DISTRO/convertor -v
+
 # MU3
 echo "=== Install MuseScore 3 ==="
 MU3_DISTRO=MuseScore-3.6.2.1853151055
