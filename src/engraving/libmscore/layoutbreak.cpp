53b6ebfc69b04b202632e0083ec5f29ea3bb2e72
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-03-03 19:28:30 +0200

    Request tempo update after changes in LayputBreaks

--- src/engraving/libmscore/layoutbreak.cpp
+++ src/engraving/libmscore/layoutbreak.cpp
@@ -295,6 +295,7 @@ bool LayoutBreak::setProperty(Pid propertyId, const PropertyValue& v)
         break;
     case Pid::PAUSE:
         setPause(v.toDouble());
+        score()->setUpTempoMap();
         break;
     case Pid::START_WITH_LONG_NAMES:
         setStartWithLongNames(v.toBool());
