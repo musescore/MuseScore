abaca44a6f475d4b8c6bac4f7b367a70a7f3cc48
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-03-17 13:42:51 +0200

    Fixed an issue with the playback of arpeggio

--- src/engraving/playback/renderers/arpeggiorenderer.cpp
+++ src/engraving/playback/renderers/arpeggiorenderer.cpp
@@ -50,42 +50,59 @@ void ArpeggioRenderer::doRender(const Ms::EngravingItem* item, const mpe::Articu
     int stepsCount = static_cast<int>(chord->notes().size());
     mpe::percentage_t percentageStep = mpe::HUNDRED_PERCENT / stepsCount;
 
-    auto buildEvent = [&](const int noteIdx) {
-        const Ms::Note* note = chord->notes().at(noteIdx);
-
+    auto buildEvent = [&](const Ms::Note* note, const int stepNumber) {
         if (!isNotePlayable(note)) {
             return;
         }
 
         NominalNoteCtx noteCtx(note, context);
-        noteCtx.chordCtx.commonArticulations.updateOccupiedRange(preferredType, noteIdx * percentageStep, (noteIdx + 1) * percentageStep);
+        noteCtx.chordCtx.commonArticulations.updateOccupiedRange(preferredType, stepNumber * percentageStep,
+                                                                 (stepNumber + 1) * percentageStep);
+        noteCtx.timestamp += timestampOffsetStep(context) * stepNumber;
         result.emplace_back(buildNoteEvent(std::move(noteCtx)));
     };
 
-    if (isDirectionUp(preferredType)) {
-        for (int i = 0; i < stepsCount; ++i) {
-            buildEvent(i);
+    for (int i = 0; i < stepsCount; ++i) {
+        const Ms::Note* note = chord->notes().at(i);
+
+        if (!isDirectionUp(preferredType)) {
+            note = chord->notes().at(stepsCount - 1 - i);
         }
 
-        return;
-    }
-
-    for (int i = stepsCount - 1; i >= 0; --i) {
-        buildEvent(i);
+        buildEvent(note, i);
     }
 }
 
 bool ArpeggioRenderer::isDirectionUp(const mpe::ArticulationType type)
 {
     switch (type) {
-    case mpe::ArticulationType::Arpeggio:
     case mpe::ArticulationType::ArpeggioDown:
     case mpe::ArticulationType::ArpeggioStraightDown:
         return false;
+    case mpe::ArticulationType::Arpeggio:
     case mpe::ArticulationType::ArpeggioUp:
     case mpe::ArticulationType::ArpeggioStraightUp:
         return true;
     default:
         return false;
     }
+}
+
+msecs_t ArpeggioRenderer::timestampOffsetStep(const RenderingContext& ctx)
+{
+    constexpr int MINIMAL_TIMESTAMP_OFFSET_STEP = 60;
+
+    if (RealIsEqualOrMore(ctx.beatsPerSecond.val, PRESTISSIMO_BPS_BOUND)) {
+        return MINIMAL_TIMESTAMP_OFFSET_STEP * 1.5;
+    }
+
+    if (RealIsEqualOrMore(ctx.beatsPerSecond.val, PRESTO_BPS_BOUND)) {
+        return MINIMAL_TIMESTAMP_OFFSET_STEP * 1.25;
+    }
+
+    if (RealIsEqualOrMore(ctx.beatsPerSecond.val, MODERATO_BPS_BOUND)) {
+        return MINIMAL_TIMESTAMP_OFFSET_STEP;
+    }
+
+    return MINIMAL_TIMESTAMP_OFFSET_STEP;
 }
