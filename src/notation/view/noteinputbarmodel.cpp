1ca97a9f17482007efd112dad1aa816c76a947e1
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2022-04-01 17:07:29 +0200

    Implemented updating ToolConfigs stored in workspace
    When new items are added to MuseScore's default items, those items will now also be added to the user's workspace. An attempt is made to insert the new item at a sensible index, even if the user has customized the order of their items.

--- src/notation/view/noteinputbarmodel.cpp
+++ src/notation/view/noteinputbarmodel.cpp
@@ -119,10 +119,7 @@ void NoteInputBarModel::load()
 {
     MenuItemList items;
 
-    ToolConfig noteInputConfig = uiConfiguration()->toolConfig(TOOLBAR_NAME);
-    if (!noteInputConfig.isValid()) {
-        noteInputConfig = NotationUiActions::defaultNoteInputBarConfig();
-    }
+    ToolConfig noteInputConfig = uiConfiguration()->toolConfig(TOOLBAR_NAME, NotationUiActions::defaultNoteInputBarConfig());
 
     int section = 0;
     for (const ToolConfig::Item& citem : noteInputConfig.items) {
