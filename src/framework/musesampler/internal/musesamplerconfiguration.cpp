ae78ec24016b3ac34050a4d7bb61911e1a7aa330
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-04-08 18:21:53 +0200

    Added configuration for MuseSampler module

---
+++ src/framework/musesampler/internal/musesamplerconfiguration.cpp
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2022 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "musesamplerconfiguration.h"
+
+using namespace mu;
+using namespace mu::musesampler;
+
+#ifdef Q_OS_UNIX
+static const io::path DEFAULT_PATH("/libMuseSamplerCoreLib.so");
+#else
+static const io::path DEFAULT_PATH("/libMuseSamplerCoreLib.dll");
+#endif
+
+io::path MuseSamplerConfiguration::libraryPath() const
+{
+    return globalConfig()->appBinPath() + DEFAULT_PATH;
+}
