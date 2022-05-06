5ffcaaf7d241c50c8b38480e9831f5d60b892df4
Josh Soref <jsoref@users.noreply.github.com>
2022-05-06 17:24:23 -0400

    spelling: annotation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/engraving/libmscore/select.cpp
+++ src/engraving/libmscore/select.cpp
@@ -1076,7 +1076,7 @@ QByteArray Selection::symbolListMimeData() const
             bool done = false;
             for (; seg; seg = seg->next1()) {
                 if (seg->isChordRestType()) {
-                    // if no ChordRest in right track, look in anotations
+                    // if no ChordRest in right track, look in annotations
                     if (seg->element(currTrack) == nullptr) {
                         foreach (EngravingItem* el, seg->annotations()) {
                             // do annotations include our element?
