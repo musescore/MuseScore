864d184e2499fc72a198229a91d38af028fa9f7d
Josh Soref <jsoref@users.noreply.github.com>
2022-05-06 17:30:15 -0400

    spelling: incomplete
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/importexport/musicxml/tests/tst_mxml_io.cpp
+++ src/importexport/musicxml/tests/tst_mxml_io.cpp
@@ -153,7 +153,7 @@ private slots:
     void helloReadCompr() { mxmlReadTestCompr("testHello"); }
     void helloReadWriteCompr() { mxmlReadWriteTestCompr("testHello"); }
     void implicitMeasure1() { mxmlIoTest("testImplicitMeasure1"); }
-    // void incompleteTuplet() { mxmlIoTestRef("testIncomplteTuplet"); } fails
+    // void incompleteTuplet() { mxmlIoTestRef("testIncompleteTuplet"); } fails
     // void incorrectMidiProgram() { mxmlIoTestRef("testIncorrectMidiProgram"); } fails
     void incorrectStaffNumber1() { mxmlIoTestRef("testIncorrectStaffNumber1"); }
     void incorrectStaffNumber2() { mxmlIoTestRef("testIncorrectStaffNumber2"); }
