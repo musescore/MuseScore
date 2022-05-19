c267064335968f38c4670adbf41b13f08d9acc30
Joachim Schmitz <jojo@schmitz-digital.de>
2022-05-19 10:26:18 +0200

    Fix MinGW/MSCV compiler warning
    reg. unknown -Wno-sign-compare

--- build/cmake/SetupCompileWarnings.cmake
+++ build/cmake/SetupCompileWarnings.cmake
@@ -48,6 +48,7 @@ function(target_no_warning TARGET WNAME)
     elseif(WNAME STREQUAL "-Wno-attributes")
     elseif(WNAME STREQUAL "-Wno-absolute-value")
     elseif(WNAME STREQUAL "-Wno-tautological-pointer-compare")
+    elseif(WNAME STREQUAL "-Wno-sign-compare")
 
     elseif(WNAME STREQUAL "-w")
         set(MSVC_Warning /w)
