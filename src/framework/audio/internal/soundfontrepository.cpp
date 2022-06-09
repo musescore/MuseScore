8da785107e8b0d43f20b3f44e7e3641d8b22a39c
Roman Pudashkin <r.pudashkin@gmail.com>
2022-06-09 14:04:30 +0200

    fix #11874: changed the dialog content to accommodate the warning about the existence of the file

--- src/framework/audio/internal/soundfontrepository.cpp
+++ src/framework/audio/internal/soundfontrepository.cpp
@@ -86,10 +86,12 @@ mu::Ret SoundFontRepository::addSoundFont(const SoundFontPath& path)
     }
 
     if (fileSystem()->exists(newPath.val)) {
-        title = qtrc("audio", "File \"%1\" already exists.")
-                .arg(newPath.val.toQString()).toStdString();
+        title = trc("audio", "File already exists. Do you want to overwrite it?");
 
-        btn = interactive()->question(title, trc("audio", "Do you want to overwrite it?"), {
+        std::string body = qtrc("audio", "File path: %1")
+                           .arg(newPath.val.toQString()).toStdString();
+
+        btn = interactive()->question(title, body, {
             IInteractive::Button::No,
             IInteractive::Button::Yes
         }, IInteractive::Button::Yes, IInteractive::WithIcon).standardButton();
