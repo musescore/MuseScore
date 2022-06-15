d9bbd8e66a37d61ebe77a6cdc2bcd9d0cafd7876
Joachim Schmitz <jojo@schmitz-digital.de>
2022-06-15 21:53:11 +0200

    Fix MingGW compiler warnings
    reg. comparison of integer expressions of different signedness: 'int' and 'size_t' {aka 'long long unsigned int'} (-Wsign-compare)

--- src/engraving/libmscore/fret.cpp
+++ src/engraving/libmscore/fret.cpp
@@ -131,9 +131,9 @@ EngravingItem* FretDiagram::linkedClone()
 std::shared_ptr<FretDiagram> FretDiagram::createFromString(Score* score, const String& s)
 {
     auto fd = Factory::makeFretDiagram(score->dummy()->segment());
-    size_t strings = s.size();
+    int strings = static_cast<int>(s.size());
 
-    fd->setStrings(static_cast<int>(strings));
+    fd->setStrings(strings);
     fd->setFrets(4);
     fd->setPropertyFlags(Pid::FRET_STRINGS, PropertyFlags::UNSTYLED);
     fd->setPropertyFlags(Pid::FRET_FRETS,   PropertyFlags::UNSTYLED);
