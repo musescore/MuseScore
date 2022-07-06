c5ce4efe8b316b4508b8b3f5378d39d8d188bd6e
Igor Korsukov <igor.korsukov@gmail.com>
2022-07-06 14:20:44 +0200

    updated translation stuff

--- build/ci/lupdate/publish_to_tx.sh
+++ build/ci/lupdate/publish_to_tx.sh
@@ -18,31 +18,30 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-TRANSIFEX_USER=""
-TRANSIFEX_PASSWORD=""
+BUILD_TOOLS=$HOME/build_tools
+mkdir -p $BUILD_TOOLS
+
+TRANSIFEX_API_TOKEN=""
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -u|--user) TRANSIFEX_USER="$2"; shift ;;
-        -p|--password) TRANSIFEX_PASSWORD="$2"; shift ;;
+        -t|--token) TRANSIFEX_API_TOKEN="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-if [ -z "$TRANSIFEX_USER" ]; then echo "error: not set TRANSIFEX_USER"; exit 1; fi
-if [ -z "$TRANSIFEX_PASSWORD" ]; then echo "error: not set TRANSIFEX_PASSWORD"; exit 1; fi
+if [ -z "$TRANSIFEX_API_TOKEN" ]; then echo "error: not set TRANSIFEX_API_TOKEN"; exit 1; fi
 
-apt install python3-setuptools
-pip3 install transifex-client
+# Install tx
+CUR_DIR=$(pwd)
+mkdir -p $BUILD_TOOLS/tx
+cd $BUILD_TOOLS/tx
+curl -o- https://raw.githubusercontent.com/transifex/cli/master/install.sh | bash
+cd $CUR_DIR
+ls $BUILD_TOOLS/tx/
+TX=$BUILD_TOOLS/tx/tx
 
-cat > ~/.transifexrc <<EOL
-[https://www.transifex.com]
-hostname = https://www.transifex.com
-password = $TRANSIFEX_PASSWORD
-token =
-username = $TRANSIFEX_USER
-EOL
-
-echo "tx version: $(tx --version)"
-#tx push -s
+echo "tx version: $($TX --version)"
+echo "tx push:"
+$TX -t $TRANSIFEX_API_TOKEN push -s
