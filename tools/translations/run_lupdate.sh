5dcd43597512543e75010e77b0910279321a12c7
Igor Korsukov <igor.korsukov@gmail.com>
2022-07-08 15:44:32 +0200

    updated tx stuff

--- tools/translations/run_lupdate.sh
+++ tools/translations/run_lupdate.sh
@@ -7,7 +7,7 @@ SRC_DIR=$HERE/../../src
 TS_DIR=$HERE/../../share/locale 
 ARGS="-recursive -tr-function-alias translate+=trc -tr-function-alias translate+=mtrc -tr-function-alias translate+=qtrc -tr-function-alias qsTranslate+=qsTrc -extensions qml,cpp -no-obsolete"
 
-for f in $TS_DIR/mscore_*.ts
+for f in $TS_DIR/musescore_*.ts
 do
   echo "Processing $f ..."
   $LUPDATE $ARGS $SRC_DIR -ts $f
