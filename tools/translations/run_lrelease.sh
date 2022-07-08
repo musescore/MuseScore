c576346f33832caa98bb1ae4dfa51ddfb0d7a644
Igor Korsukov <igor.korsukov@gmail.com>
2022-07-08 16:34:15 +0200

    Updated translation stuff. Step 3

--- tools/translations/run_lrelease.sh
+++ tools/translations/run_lrelease.sh
@@ -21,13 +21,12 @@
 
 HERE="${BASH_SOURCE%/*}" # path to dir that contains this script
 
-LUPDATE=lupdate
-SRC_DIR=$HERE/../../src 
+LRELEASE=lrelease
 TS_DIR=$HERE/../../share/locale 
-ARGS="-recursive -tr-function-alias translate+=trc -tr-function-alias translate+=mtrc -tr-function-alias translate+=qtrc -tr-function-alias qsTranslate+=qsTrc -extensions qml,cpp -no-obsolete"
+QM_DIR=$HERE/../../share/locale 
 
 for f in $TS_DIR/musescore_*.ts
 do
-  echo "Processing $f ..."
-  $LUPDATE $ARGS $SRC_DIR -ts $f
+  FILE_NAME=$(basename $f .ts)
+  $LRELEASE $f -qm $QM_DIR/${FILE_NAME}.qm 
 done
--- tools/translations/tx_pull.sh
+++ tools/translations/tx_pull.sh
@@ -19,15 +19,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-HERE="${BASH_SOURCE%/*}" # path to dir that contains this script
-
-LUPDATE=lupdate
-SRC_DIR=$HERE/../../src 
-TS_DIR=$HERE/../../share/locale 
-ARGS="-recursive -tr-function-alias translate+=trc -tr-function-alias translate+=mtrc -tr-function-alias translate+=qtrc -tr-function-alias qsTranslate+=qsTrc -extensions qml,cpp -no-obsolete"
-
-for f in $TS_DIR/musescore_*.ts
-do
-  echo "Processing $f ..."
-  $LUPDATE $ARGS $SRC_DIR -ts $f
-done
+tx pull -f
