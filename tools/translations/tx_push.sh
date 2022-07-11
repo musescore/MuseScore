ca6dd6f3ceadba5018728df73bd4846d9887d6d3
Igor Korsukov <igor.korsukov@gmail.com>
2022-07-11 13:02:10 +0200

    rewrite CI lupdate action

--- tools/translations/tx_push.sh
+++ tools/translations/tx_push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-only
 # MuseScore-CLA-applies
 #
@@ -18,30 +18,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-BUILD_TOOLS=$HOME/build_tools
-mkdir -p $BUILD_TOOLS
 
-TRANSIFEX_API_TOKEN=""
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -t|--token) TRANSIFEX_API_TOKEN="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
-if [ -z "$TRANSIFEX_API_TOKEN" ]; then echo "error: not set TRANSIFEX_API_TOKEN"; exit 1; fi
-
-# Install tx
-CUR_DIR=$(pwd)
-mkdir -p $BUILD_TOOLS/tx
-cd $BUILD_TOOLS/tx
-curl -o- https://raw.githubusercontent.com/transifex/cli/master/install.sh | bash
-cd $CUR_DIR
-ls $BUILD_TOOLS/tx/
-TX=$BUILD_TOOLS/tx/tx
-
-echo "tx version: $($TX --version)"
-echo "tx push:"
-$TX -t $TRANSIFEX_API_TOKEN push -s
+tx push -s
