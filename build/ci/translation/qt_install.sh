e35efd84bd8b4f626046a8320a39e42e68594e53
Igor Korsukov <igor.korsukov@gmail.com>
2022-07-11 13:02:10 +0200

    rewrite CI lupdate action

---
+++ build/ci/translation/qt_install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+# MuseScore-CLA-applies
+#
+# MuseScore
+# Music Composition & Notation
+#
+# Copyright (C) 2021 MuseScore BVBA and others
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+echo "Setup environment for run lupdate"
+trap 'echo Setup failed; exit 1' ERR
+
+BUILD_TOOLS=$HOME/build_tools
+mkdir -p $BUILD_TOOLS
+
+ENV_FILE=$BUILD_TOOLS/environment.sh
+rm -f $ENV_FILE
+
+echo "echo 'Setup environment for run lupdate'" >> ${ENV_FILE}
+
+##########################################################################
+# GET QT
+##########################################################################
+qt_version="5152"
+qt_dir="$BUILD_TOOLS/Qt/${qt_version}"
+if [[ ! -d "${qt_dir}" ]]; then
+  mkdir -p "${qt_dir}"
+  qt_url="https://s3.amazonaws.com/utils.musescore.org/Qt${qt_version}_gcc64.7z"
+  wget -q --show-progress -O qt5.7z "${qt_url}"
+  7z x -y qt5.7z -o"${qt_dir}"
+fi
+
+echo export PATH="${qt_dir}/bin:\${PATH}" >> ${ENV_FILE}
+
+chmod +x "${ENV_FILE}"
+
+echo "Setup script done"
--- build/ci/translation/run_lupdate.sh
+++ build/ci/translation/run_lupdate.sh
@@ -18,31 +18,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-echo "Setup environment for run lupdate"
-trap 'echo Setup failed; exit 1' ERR
 
 BUILD_TOOLS=$HOME/build_tools
-mkdir -p $BUILD_TOOLS
+ENV_FILE=$BUILD_TOOLS/environment.sh
 
-ENV_FILE=$BUILD_TOOLS/lupdate_environment.sh
-rm -f $ENV_FILE
+source $ENV_FILE
 
-echo "echo 'Setup environment for run lupdate'" >> ${ENV_FILE}
-
-##########################################################################
-# GET QT
-##########################################################################
-qt_version="5152"
-qt_dir="$BUILD_TOOLS/Qt/${qt_version}"
-if [[ ! -d "${qt_dir}" ]]; then
-  mkdir -p "${qt_dir}"
-  qt_url="https://s3.amazonaws.com/utils.musescore.org/Qt${qt_version}_gcc64.7z"
-  wget -q --show-progress -O qt5.7z "${qt_url}"
-  7z x -y qt5.7z -o"${qt_dir}"
-fi
-
-echo export PATH="${qt_dir}/bin:\${PATH}" >> ${ENV_FILE}
-
-chmod +x "${ENV_FILE}"
-
-echo "Setup script done"
+bash ./tools/translations/run_lupdate.sh
