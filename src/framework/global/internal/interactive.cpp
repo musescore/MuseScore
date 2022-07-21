144b4204ad9387428731d5b775908cc3badea963
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2022-07-21 11:43:53 +0200

    Move IInteractive button texts to "global"
    Consistent with other buttons in QML that have been put in "global"

--- src/framework/global/internal/interactive.cpp
+++ src/framework/global/internal/interactive.cpp
@@ -80,27 +80,26 @@ IInteractive::ButtonData Interactive::buttonData(Button b) const
 
     switch (b) {
     case IInteractive::Button::NoButton:    return ButtonData(int(b), "");
-    case IInteractive::Button::Ok:          return ButtonData(int(b), trc("ui", "OK"));
-    case IInteractive::Button::Save:        return ButtonData(int(b), trc("ui", "Save"), accent);
-    case IInteractive::Button::SaveAll:     return ButtonData(int(b), trc("ui", "Save All"));
-    case IInteractive::Button::DontSave:    return ButtonData(int(b), trc("ui", "Don't save"));
-    case IInteractive::Button::Open:        return ButtonData(int(b), trc("ui", "Open"));
-    case IInteractive::Button::Yes:         return ButtonData(int(b), trc("ui", "Yes"), accent);
-    case IInteractive::Button::YesToAll:    return ButtonData(int(b), trc("ui", "Yes to All"), accent);
-    case IInteractive::Button::No:          return ButtonData(int(b), trc("ui", "No"));
-    case IInteractive::Button::NoToAll:     return ButtonData(int(b), trc("ui", "No to All"));
-    case IInteractive::Button::Abort:       return ButtonData(int(b), trc("ui", "Abort"));
-    case IInteractive::Button::Retry:       return ButtonData(int(b), trc("ui", "Retry"));
-    case IInteractive::Button::Ignore:      return ButtonData(int(b), trc("ui", "Ignore"));
-    case IInteractive::Button::Close:       return ButtonData(int(b), trc("ui", "Close"));
-    case IInteractive::Button::Cancel:      return ButtonData(int(b), trc("ui", "Cancel"));
-    case IInteractive::Button::Discard:     return ButtonData(int(b), trc("ui", "Discard"));
-    case IInteractive::Button::Help:        return ButtonData(int(b), trc("ui", "Help"));
-    case IInteractive::Button::Apply:       return ButtonData(int(b), trc("ui", "Apply"));
-    case IInteractive::Button::Reset:       return ButtonData(int(b), trc("ui", "Reset"));
-    case IInteractive::Button::RestoreDefaults: return ButtonData(int(b), trc("ui", "Restore Defaults"));
-    case IInteractive::Button::Continue:    return ButtonData(int(b), trc("ui", "Continue"));
-    case IInteractive::Button::CustomButton: return ButtonData(int(b), "");
+    case IInteractive::Button::Ok:          return ButtonData(int(b), trc("global", "OK"));
+    case IInteractive::Button::Save:        return ButtonData(int(b), trc("global", "Save"), accent);
+    case IInteractive::Button::SaveAll:     return ButtonData(int(b), trc("global", "Save All"));
+    case IInteractive::Button::DontSave:    return ButtonData(int(b), trc("global", "Don't save"));
+    case IInteractive::Button::Open:        return ButtonData(int(b), trc("global", "Open"));
+    case IInteractive::Button::Yes:         return ButtonData(int(b), trc("global", "Yes"), accent);
+    case IInteractive::Button::YesToAll:    return ButtonData(int(b), trc("global", "Yes to All"), accent);
+    case IInteractive::Button::No:          return ButtonData(int(b), trc("global", "No"));
+    case IInteractive::Button::NoToAll:     return ButtonData(int(b), trc("global", "No to All"));
+    case IInteractive::Button::Abort:       return ButtonData(int(b), trc("global", "Abort"));
+    case IInteractive::Button::Retry:       return ButtonData(int(b), trc("global", "Retry"));
+    case IInteractive::Button::Ignore:      return ButtonData(int(b), trc("global", "Ignore"));
+    case IInteractive::Button::Close:       return ButtonData(int(b), trc("global", "Close"));
+    case IInteractive::Button::Cancel:      return ButtonData(int(b), trc("global", "Cancel"));
+    case IInteractive::Button::Discard:     return ButtonData(int(b), trc("global", "Discard"));
+    case IInteractive::Button::Help:        return ButtonData(int(b), trc("global", "Help"));
+    case IInteractive::Button::Apply:       return ButtonData(int(b), trc("global", "Apply"));
+    case IInteractive::Button::Reset:       return ButtonData(int(b), trc("global", "Reset"));
+    case IInteractive::Button::Continue:    return ButtonData(int(b), trc("global", "Continue"));
+    case IInteractive::Button::CustomButton: break;
     }
 
     return ButtonData(int(b), "");
