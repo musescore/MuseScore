12658da3b28fd717784a9cea2b5b293e962a63a0
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2022-08-07 21:12:27 +0200

    Init translation system early
    Before modules that use it during their initialization, for example, before loading instrument names.

--- src/languages/languagesmodule.h
+++ src/languages/languagesmodule.h
@@ -31,7 +31,7 @@ public:
     std::string moduleName() const override;
 
     void registerExports() override;
-    void onInit(const framework::IApplication::RunMode& mode) override;
+    void onPreInit(const framework::IApplication::RunMode& mode) override;
 };
 }
 
