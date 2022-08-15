26a3b701eb0b8577207f3b5d0da60eeb7820d0e6
Joachim Schmitz <jojo@schmitz-digital.de>
2022-08-15 12:24:23 +0200

    Fix GH12921: Fix some menus/shortcut strings with &
    i.e. differentiate them between the menu and the shortcurts. Esp. have the accelerator key only in the menu, not in the shortcut strings and also, for shortcuts have a better description (which in the Menu comes from their context, here: the File menu)

--- src/project/internal/projectuiactions.cpp
+++ src/project/internal/projectuiactions.cpp
@@ -31,14 +31,14 @@ const UiActionList ProjectUiActions::m_actions = {
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "&Open…"),
-             TranslatableString("action", "Open…"),
+             TranslatableString("action", "Open"),
              IconCode::Code::OPEN_FILE
              ),
     UiAction("file-new",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "&New…"),
-             TranslatableString("action", "New…"),
+             TranslatableString("action", "New"),
              IconCode::Code::NEW_FILE
              ),
     UiAction("file-close",
@@ -58,66 +58,66 @@ const UiActionList ProjectUiActions::m_actions = {
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Save &as…"),
-             TranslatableString("action", "Save as…")
+             TranslatableString("action", "Save as")
              ),
     UiAction("file-save-a-copy",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Save a cop&y…"),
-             TranslatableString("action", "Save a copy…")
+             TranslatableString("action", "Save a copy")
              ),
     UiAction("file-save-selection",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Save se&lection…"),
-             TranslatableString("action", "Save selection…")
+             TranslatableString("action", "Save selection")
              ),
     UiAction("file-save-to-cloud",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Save to clo&ud…"),
-             TranslatableString("action", "Save to cloud…"),
+             TranslatableString("action", "Save to cloud"),
              IconCode::Code::CLOUD_FILE
              ),
     UiAction("file-publish",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Pu&blish to MuseScore.com…"),
-             TranslatableString("action", "Publish to MuseScore.com…"),
+             TranslatableString("action", "Publish to MuseScore.com"),
              IconCode::Code::CLOUD_FILE
              ),
     UiAction("file-export",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "&Export…"),
-             TranslatableString("action", "Export…"),
+             TranslatableString("action", "Export"),
              IconCode::Code::SHARE_FILE
              ),
     UiAction("file-import-pdf",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "Import P&DF…"),
-             TranslatableString("action", "Import PDF…"),
+             TranslatableString("action", "Import PDF"),
              IconCode::Code::IMPORT
              ),
     UiAction("project-properties",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
-             TranslatableString("action", "Project propert&ies…"),
-             TranslatableString("action", "Project properties…")
+             TranslatableString("action", "Propert&ies…"),
+             TranslatableString("action", "Properties")
              ),
     UiAction("print",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
              TranslatableString("action", "&Print…"),
-             TranslatableString("action", "Print…"),
+             TranslatableString("action", "Print"),
              IconCode::Code::PRINT
              ),
     UiAction("clear-recent",
              mu::context::UiCtxAny,
              mu::context::CTX_ANY,
-             TranslatableString("action", "&Clear recent files"),
-             TranslatableString("action", "Clear recent files")
+             TranslatableString("action", "&Clear list of recent files"),
+             TranslatableString("action", "Clear list of recent files")
              )
 };
 
