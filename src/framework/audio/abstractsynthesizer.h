e36b071f87a1bc9243f87e80e2803eb9f01a3cd4
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-08-17 13:54:17 +0200

    Removed redundant logic in AbstractSynthesizer

--- src/framework/audio/abstractsynthesizer.h
+++ src/framework/audio/abstractsynthesizer.h
@@ -44,138 +44,30 @@ public:
     const audio::AudioInputParams& params() const;
     async::Channel<audio::AudioInputParams> paramsChanged() const;
 
-    virtual msecs_t playbackPosition() const;
-    virtual void setPlaybackPosition(const msecs_t newPosition);
+    virtual msecs_t playbackPosition() const = 0;
+    virtual void setPlaybackPosition(const msecs_t newPosition) = 0;
 
     void setup(const mpe::PlaybackData& playbackData);
     virtual void flushSound() = 0;
     virtual void revokePlayingNotes();
 
     virtual bool isValid() const = 0;
-    virtual bool isActive() const;
-    virtual void setIsActive(bool arg);
-
-    static constexpr float DAMPER_FACTOR = 5.0;
-    static constexpr msecs_t MIN_NOTE_LENGTH = 25;
+    virtual bool isActive() const = 0;
+    virtual void setIsActive(bool arg) = 0;
 
 protected:
-    using EventsMapIterator = mpe::PlaybackEventsMap::const_iterator;
-    using EventsMapIteratorList = std::vector<EventsMapIterator>;
-
-    struct EventsBuffer {
-        msecs_t from = 0;
-        msecs_t to = 0;
-
-        const mpe::PlaybackEventsMap& events() const
-        {
-            return m_events;
-        }
-
-        void load(const mpe::PlaybackEventsMap& events)
-        {
-            for (const auto& pair : events) {
-                for (const mpe::PlaybackEvent& event : pair.second) {
-                    if (!std::holds_alternative<mpe::NoteEvent>(event)) {
-                        continue;
-                    }
-
-                    mpe::timestamp_t actualTimestamp = std::get<mpe::NoteEvent>(event).arrangementCtx().actualTimestamp;
-
-                    m_events[actualTimestamp].emplace_back(event);
-                }
-            }
-            updateBoundaries();
-        }
-
-        EventsMapIteratorList findEventsRange(const msecs_t rangeFrom, const msecs_t rangeTo) const
-        {
-            if (m_events.empty()) {
-                static EventsMapIteratorList empty;
-                return empty;
-            }
-
-            auto firstNotLess = m_events.lower_bound(rangeFrom);
-
-            if (firstNotLess == m_events.begin()) {
-                return { firstNotLess };
-            }
-
-            auto firstLess = std::prev(firstNotLess);
-
-            if (firstNotLess->first > rangeTo || firstNotLess == m_events.cend()) {
-                return { firstLess };
-            }
-
-            return { firstLess, firstNotLess };
-        }
-
-        void clear()
-        {
-            m_events.clear();
-            updateBoundaries();
-        }
-
-        bool empty() const
-        {
-            return m_events.empty();
-        }
-
-    private:
-
-        void updateBoundaries()
-        {
-            if (empty()) {
-                from = 0;
-                to = 0;
-                return;
-            }
-
-            auto firstEvents = m_events.begin();
-            from = firstEvents->first;
-
-            auto lastEvents = m_events.rbegin();
-            to = lastEvents->first;
-
-            for (const mpe::PlaybackEvent& event : lastEvents->second) {
-                if (!std::holds_alternative<mpe::NoteEvent>(event)) {
-                    continue;
-                }
-
-                const mpe::NoteEvent& noteEvent = std::get<mpe::NoteEvent>(event);
-
-                mpe::duration_t dampedDuration = noteEvent.arrangementCtx().actualDuration * DAMPER_FACTOR;
-                dampedDuration = std::max(dampedDuration, MIN_NOTE_LENGTH);
-                to = std::max(noteEvent.arrangementCtx().actualTimestamp + dampedDuration, to);
-            }
-        }
-
-        mpe::PlaybackEventsMap m_events;
-    };
 
     virtual void setupSound(const mpe::PlaybackSetupData& setupData) = 0;
-    virtual void setupEvents(const mpe::PlaybackData& playbackData);
-    virtual void loadMainStreamEvents(const mpe::PlaybackEventsMap& updatedEvents);
-    virtual void loadOffStreamEvents(const mpe::PlaybackEventsMap& updatedEvents);
-    virtual void loadDynamicLevelChanges(const mpe::DynamicLevelMap& updatedDynamicLevelMap);
+    virtual void setupEvents(const mpe::PlaybackData& playbackData) = 0;
 
     msecs_t samplesToMsecs(const samples_t samplesPerChannel, const samples_t sampleRate) const;
-    msecs_t actualPlaybackPositionStart() const;
-
-    msecs_t m_playbackPosition = 0;
+    samples_t microSecsToSamples(const msecs_t msec, const samples_t sampleRate) const;
 
     mpe::PlaybackSetupData m_setupData;
-    mpe::DynamicLevelMap m_dynamicLevelMap;
-    EventsBuffer m_mainStreamEvents;
-    EventsBuffer m_offStreamEvents;
-
-    mpe::PlaybackEventsChanges m_mainStreamChanges;
-    mpe::PlaybackEventsChanges m_offStreamChanges;
-    async::Channel<mpe::DynamicLevelMap> m_dynamicLevelChanges;
 
     audio::AudioInputParams m_params;
     async::Channel<audio::AudioInputParams> m_paramsChanges;
 
-    bool m_isActive = false;
     samples_t m_sampleRate = 0;
 };
 
