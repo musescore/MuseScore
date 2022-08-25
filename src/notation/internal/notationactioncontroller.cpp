ddd9f4b511af2f1bfdab6a8730476f54181ad6fd
Eism <eismailzada@gmail.com>
2022-08-25 13:44:39 +0200

    Start navigation on score from first element if there is no selection

--- src/notation/internal/notationactioncontroller.cpp
+++ src/notation/internal/notationactioncontroller.cpp
@@ -878,6 +878,8 @@ void NotationActionController::move(MoveDirection direction, bool quickly)
                    && interaction->noteInput()->state().staffGroup == mu::engraving::StaffGroup::TAB) {
             interaction->moveSelection(direction, MoveSelectionType::String);
             return;
+        } else if (!selectedElement) {
+            interaction->selectFirstElement(false);
         } else {
             interaction->movePitch(direction, quickly ? PitchMode::OCTAVE : PitchMode::CHROMATIC);
         }
@@ -891,6 +893,9 @@ void NotationActionController::move(MoveDirection direction, bool quickly)
 
         if (selectedElement && selectedElement->isTextBase()) {
             interaction->nudge(direction, quickly);
+        } else if (!selectedElement) {
+            interaction->selectFirstElement(false);
+            playChord = true;
         } else {
             interaction->moveSelection(direction, quickly ? MoveSelectionType::Measure : MoveSelectionType::Chord);
             playChord = true;
