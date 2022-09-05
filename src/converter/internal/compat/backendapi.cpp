musescore/MuseScore

commit 5d26e7fae00ce4e0ca7bd58d25ffacc7c02d2d9a

Allow *.mxml on read too
writing is allowed already

src/importexport/musicxml/musicxmlmodule.cpp
@@ -65,7 +65,7 @@ void MusicXmlModule::resolveImports()
 
     auto readers = modularity::ioc()->resolve<INotationReadersRegister>(moduleName());
     if (readers) {
-        readers->reg({ "xml", "musicxml", "mxl" }, std::make_shared<MusicXmlReader>());
+        readers->reg({ "xml", "musicxml", "mxl", "mxml" }, std::make_shared<MusicXmlReader>());
     }
 
     auto writers = modularity::ioc()->resolve<INotationWritersRegister>(moduleName());

src/project/view/exportdialogmodel.cpp
@@ -46,6 +46,10 @@ ExportDialogModel::ExportDialogModel(QObject* parent)
                                      qtrc("project/export", "Compressed") + " (*.mxl)",
                                      qtrc("project/export", "Compressed MusicXML files"),
                                      "MusicXmlSettingsPage.qml"),
+        ExportType::makeWithSuffixes({ "mxml" },
+                                     qtrc("project/export", "Compressed") + " (*.mxml)",
+                                     qtrc("project/export", "Compressed MusicXML files"),
+                                     "MusicXmlSettingsPage.qml"),
         ExportType::makeWithSuffixes({ "musicxml" },
                                      qtrc("project/export", "Uncompressed") + " (*.musicxml)",
                                      qtrc("project/export", "Uncompressed MusicXML files"),

build/Linux+BSD/musescore.xml.in
@@ -103,7 +103,8 @@
     <mime-type type="application/vnd.recordare.musicxml">
         <!-- See "Container file" at http://www.musicxml.com/for-developers/musicxml-dtd/ -->
         <comment>MusicXML compressed score</comment>
-        <glob pattern="*.mxl"/>
+        <glob pattern="*.mxl"/><!-- preferred extension must go first -->
+        <glob pattern="*.mxml" weight="40"/><!-- reduced weight (default 50) to avoid matching Macromedia XML files -->
         <sub-class-of type="application/zip"/>
         <icon name="application-vnd.recordare.musicxml@MSCORE_INSTALL_SUFFIX@"/>
     </mime-type>

build/MacOSXBundleInfo.plist.in
@@ -216,25 +216,8 @@
 			<string>Uncompressed MusicXML File</string>
 			<key>CFBundleTypeExtensions</key>
 			<array>
-				<string>xml</string>
-			</array>
-			<key>CFBundleTypeMIMETypes</key>
-			<array>
-				<string>application/vnd.recordare.musicxml+xml</string>
-			</array>
-			<key>CFBundleTypeRole</key>
-			<string>Editor</string>
-			<key>LSTypeIsPackage</key>
-			<false/>
-			<key>NSPersistentStoreTypeKey</key>
-			<string>XML</string>
-		</dict>
-		<dict>
-			<key>CFBundleTypeName</key>
-			<string>Uncompressed MusicXML File</string>
-			<key>CFBundleTypeExtensions</key>
-			<array>
 				<string>musicxml</string>
+				<string>xml</string>
 			</array>
 			<key>CFBundleTypeMIMETypes</key>
 			<array>
@@ -253,6 +236,7 @@
 			<key>CFBundleTypeExtensions</key>
 			<array>
 				<string>mxl</string>
+				<string>mxml</string>
 			</array>
 			<key>CFBundleTypeMIMETypes</key>
 			<array>

build/packaging/WIX.template.in
@@ -100,6 +100,7 @@
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".xml" Value="" Type="string" />
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".musicxml" Value="" Type="string" />
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".mxl" Value="" Type="string" />
+            <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".mxml" Value="" Type="string" />
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".cap" Value="" Type="string" />
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".capx" Value="" Type="string" />
             <RegistryValue Root="$(var.RegistryRoot)" Key="SOFTWARE\Classes\Applications\$(var.ExeKey)\SupportedTypes" Name=".scw" Value="" Type="string" />

src/importexport/musicxml/internal/musicxmlreader.cpp
@@ -38,7 +38,7 @@ mu::Ret MusicXmlReader::read(MasterScore* score, const io::path_t& path, const O
     std::string suffix = mu::io::suffix(path);
     if (suffix == "xml" || suffix == "musicxml") {
         err = importMusicXml(score, path.toQString());
-    } else if (suffix == "mxl") {
+    } else if (suffix == "mxl" || suffix == "mxml") {
         err = importCompressedMusicXml(score, path.toQString());
     }
     return make_ret(err, path);

src/project/internal/projectactionscontroller.cpp
@@ -599,13 +599,13 @@ void ProjectActionsController::printScore()
 
 io::path_t ProjectActionsController::selectScoreOpeningFile()
 {
-    QString allExt = "*.mscz *.mxl *.musicxml *.xml *.mid *.midi *.kar *.md *.mgu *.sgu *.cap *.capx "
+    QString allExt = "*.mscz *.mxl *.mxml *.musicxml *.xml *.mid *.midi *.kar *.md *.mgu *.sgu *.cap *.capx "
                      "*.ove *.scw *.bmw *.bww *.gtp *.gp3 *.gp4 *.gp5 *.gpx *.gp *.ptb *.mscx *.mscs *.mscz~";
 
     QStringList filter;
     filter << qtrc("project", "All supported files") + " (" + allExt + ")"
            << qtrc("project", "MuseScore files") + " (*.mscz)"
-           << qtrc("project", "MusicXML files") + " (*.mxl *.musicxml *.xml)"
+           << qtrc("project", "MusicXML files") + " (*.mxl *.mxml *.musicxml *.xml)"
            << qtrc("project", "MIDI files") + " (*.mid *.midi *.kar)"
            << qtrc("project", "MuseData files") + " (*.md)"
            << qtrc("project", "Capella files") + " (*.cap *.capx)"

src/project/qml/MuseScore/Project/internal/ScoreItem.qml
@@ -262,6 +262,7 @@ FocusScope {
                     case "kar":
                         return "qrc:/resources/Placeholder_MIDI.png"
                     case "mxl":
+                    case "mxml":
                     case "musicxml":
                     case "xml":
                         return "qrc:/resources/Placeholder_MXML.png"

commit a52ae7b7dce1b940056c2cf97ea8c1a661872bba

Prevent creation of .mxml files
The standard file extension for compressed MusicXML is .mxl not .mxml.

Prior to this commit we allowed compressed MusicXML files to be created
on the command line with either of these commands:

    mscore file.mscz -o file.mxl    # should succeed
    mscore file.mscz -o file.mxml   # should fail but was successful

This caused confusion in PR #13122 which saw MXML added as an export
option in the GUI.

Now the GUI option is removed and the second command will correctly
fail with an error message about an unrecognised file extension,
thereby preventing the creation of non-standard files.

The string "mxml" is also used as a key in the JSON output of this
command:

    mscore file.mscz --score-media

This commit avoids changing that behaviour.

src/converter/internal/compat/backendapi.cpp
@@ -54,7 +54,8 @@ static const std::string SEGMENTS_POSITIONS_WRITER_NAME = "sposXML";
 static const std::string MEASURES_POSITIONS_WRITER_NAME = "mposXML";
 static const std::string PDF_WRITER_NAME = "pdf";
 static const std::string MIDI_WRITER_NAME = "midi";
-static const std::string MUSICXML_WRITER_NAME = "mxml";
+static const std::string MUSICXML_WRITER_NAME = "mxl";
+static const std::string MUSICXML_JSON_NAME = "mxml";
 static const std::string META_DATA_NAME = "metadata";
 
 static constexpr bool ADD_SEPARATOR = true;
@@ -421,7 +422,7 @@ Ret BackendApi::exportScoreMusicXML(const INotationPtr notation, BackendJsonWrit
         return writerRetVal.ret;
     }
 
-    jsonWriter.addKey(MUSICXML_WRITER_NAME.c_str());
+    jsonWriter.addKey(MUSICXML_JSON_NAME.c_str());
     jsonWriter.addValue(writerRetVal.val, addSeparator);
 
     return make_ret(Ret::Code::Ok);

src/importexport/musicxml/musicxmlmodule.cpp
@@ -71,6 +71,6 @@ void MusicXmlModule::resolveImports()
     auto writers = modularity::ioc()->resolve<INotationWritersRegister>(moduleName());
     if (writers) {
         writers->reg({ "musicxml", "xml" }, std::make_shared<MusicXmlWriter>());
-        writers->reg({ "mxl", "mxml" }, std::make_shared<MxlWriter>());
+        writers->reg({ "mxl" }, std::make_shared<MxlWriter>());
     }
 }

src/project/view/exportdialogmodel.cpp
@@ -46,10 +46,6 @@ ExportDialogModel::ExportDialogModel(QObject* parent)
                                      qtrc("project/export", "Compressed") + " (*.mxl)",
                                      qtrc("project/export", "Compressed MusicXML files"),
                                      "MusicXmlSettingsPage.qml"),
-        ExportType::makeWithSuffixes({ "mxml" },
-                                     qtrc("project/export", "Compressed") + " (*.mxml)",
-                                     qtrc("project/export", "Compressed MusicXML files"),
-                                     "MusicXmlSettingsPage.qml"),
         ExportType::makeWithSuffixes({ "musicxml" },
                                      qtrc("project/export", "Uncompressed") + " (*.musicxml)",
                                      qtrc("project/export", "Uncompressed MusicXML files"),