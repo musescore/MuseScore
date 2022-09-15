f345b4b8a19ae1f0bd82679f0f512c459198f7c1
vpereverzev <v.pereverzev@wsmgroup.ru>
2022-09-15 17:21:34 +0200

    Absolute MuseSampler's path for mac os

--- src/framework/musesampler/internal/musesamplerconfiguration.cpp
+++ src/framework/musesampler/internal/musesamplerconfiguration.cpp
@@ -30,9 +30,9 @@ using namespace mu::musesampler;
 #if defined(Q_OS_LINUX)
 static const io::path_t DEFAULT_PATH("libMuseSamplerCoreLib.so");
 #elif defined(Q_OS_MAC)
-static const io::path_t DEFAULT_PATH("libMuseSamplerCoreLib.dylib");
+static const io::path_t DEFAULT_PATH("/usr/local/lib/libMuseSamplerCoreLib.dylib");
 #else
-static const io::path_t DEFAULT_PATH("libMuseSamplerCoreLib.dll");
+static const io::path_t DEFAULT_PATH("MuseSamplerCoreLib.dll");
 #endif
 
 io::path_t MuseSamplerConfiguration::libraryPath() const
