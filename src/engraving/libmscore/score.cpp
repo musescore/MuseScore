0bb0a9eeba455c7307cef63308b8c73a9976573c
RomanPudashkin <67867444+RomanPudashkin@users.noreply.github.com>
2022-09-26 17:31:44 +0300

    Revert "[MU4] Fix #12140: Tempo marking ignored when they follow a rit, accel or other gradual tempo change"

--- src/engraving/libmscore/score.cpp
+++ src/engraving/libmscore/score.cpp
@@ -575,7 +575,7 @@ void Score::setUpTempoMap()
         BeatsPerSecond newBps = currentBps * tempoChange->tempoChangeFactor();
 
         std::map<int, double> tempoCurve = TConv::easingValueCurve(tempoChange->ticks().ticks(),
-                                                                   4 /*stepsCount*/,
+                                                                   4,
                                                                    newBps.val - currentBps.val,
                                                                    tempoChange->easingMethod());
 
