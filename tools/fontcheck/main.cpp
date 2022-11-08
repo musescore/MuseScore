61c066ce20fc9f055e6e92cd099f945bf0868913
Igor Korsukov <igor.korsukov@gmail.com>
2022-11-08 10:05:46 +0200

    [fontcheck] added draw all symbols

--- tools/fontcheck/main.cpp
+++ tools/fontcheck/main.cpp
@@ -4,6 +4,11 @@
 #include <QString>
 #include <QFileInfo>
 #include <QApplication>
+#include <QFontDatabase>
+#include <QPainter>
+#include <QImage>
+#include <QPdfWriter>
+#include <QPageLayout>
 
 #include "framework/global/globalmodule.h"
 #include "framework/fonts/fontsmodule.h"
@@ -24,56 +29,21 @@ using namespace mu::engraving;
 #define FORMAT(str, width) QString(str).leftJustified(width, ' ', true).append("  ").toLatin1().constData()
 #define TITLE(str) FORMAT(QString(str), 24)
 
-int main(int argc, char* argv[])
+enum FontCode {
+    Bravura,
+    BravuraText,
+    Leland,
+    LelandText
+};
+
+static std::vector<FontCode> fonts = { Bravura, BravuraText, Leland, LelandText };
+
+void printStat()
 {
-    std::cout << "Hello World!" << std::endl;
-    QApplication a(argc, argv);
-
-    GlobalModule globalModule;
-    std::vector<IModuleSetup*> modules;
-    modules.push_back(new mu::fonts::FontsModule());
-    modules.push_back(new mu::draw::DrawModule());
-
-    // ====================================================
-    // Setup modules: Resources, Exports, Imports, UiTypes
-    // ====================================================
-    globalModule.registerResources();
-    globalModule.registerExports();
-    globalModule.registerUiTypes();
-
-    for (mu::modularity::IModuleSetup* m : modules) {
-        m->registerResources();
-    }
-
-    for (mu::modularity::IModuleSetup* m : modules) {
-        m->registerExports();
-    }
-
-    globalModule.resolveImports();
-    for (mu::modularity::IModuleSetup* m : modules) {
-        m->registerUiTypes();
-        m->resolveImports();
-    }
-
-    // Symbols
-    Smufl::init();
-
-    SymbolFonts::addFont(u"Bravura",    u"Bravura",     ":/fonts/bravura/Bravura.otf");
-    SymbolFonts::addFont(u"Leland",     u"Leland",      ":/fonts/leland/Leland.otf");
-
     SymbolFont* bravura = SymbolFonts::fontByName(u"Bravura");
     SymbolFont* leland = SymbolFonts::fontByName(u"Leland");
 
-    enum FontName {
-        Bravura,
-        BravuraText,
-        Leland,
-        LelandText
-    };
-
-    std::vector<FontName> fonts = { Bravura, BravuraText, Leland, LelandText };
-
-    std::map<FontName, QString> paths;
+    std::map<FontCode, QString> paths;
     paths[Bravura] = ":/fonts/bravura/Bravura.otf";
     paths[BravuraText] = ":/fonts/bravura/BravuraText.otf";
     paths[Leland] = ":/fonts/leland/Leland.otf";
@@ -89,9 +59,9 @@ int main(int argc, char* argv[])
         QString name() const { return QFileInfo(path).baseName(); }
     };
 
-    std::map<FontName, FontStat> stats;
+    std::map<FontCode, FontStat> stats;
 
-    for (FontName f : fonts) {
+    for (FontCode f : fonts) {
         FontStat fs;
         fs.path = paths[f];
         fs.face = new FontFace();
@@ -152,7 +122,7 @@ int main(int argc, char* argv[])
                    << "\n";
         }
 
-        for (FontName f : fonts) {
+        for (FontCode f : fonts) {
             FontStat& fs = stats[f];
             char32_t code = bravuraCode;
             if (f == Leland || f == LelandText) {
@@ -172,7 +142,7 @@ int main(int argc, char* argv[])
     stream << "total syms: " << syms << "\n";
     stream << "\n";
 
-    for (FontName f : fonts) {
+    for (FontCode f : fonts) {
         FontStat& fs = stats[f];
         stream << fs.name() << ": \n";
         stream << "in font:          " << fs.inFont.size() << "\n";
@@ -186,6 +156,166 @@ int main(int argc, char* argv[])
     }
 
     std::cout << str.toStdString();
+}
+
+void drawImage()
+{
+    struct Font {
+        FontCode code;
+        QFont font;
+
+        Font(FontCode c, const QString& f)
+            : code(c), font(f, 20) {}
+    };
+
+    std::vector<Font> fonts;
+    fonts.push_back(Font(Bravura, "Bravura"));
+    fonts.push_back(Font(BravuraText, "BravuraText"));
+    fonts.push_back(Font(Leland, "Leland"));
+    fonts.push_back(Font(LelandText, "LelandText"));
+
+    // QImage img(CONTENT_WIDTH + X_MARGIN * 2, 100 * ROW_HEIGHT, QImage::Format_Grayscale8);
+    QPdfWriter pdf("Symbols.pdf");
+    pdf.setResolution(300);
+    pdf.setTitle("Symbols");
+    QPageSize pageSize(QPageSize::A4);
+    pdf.setPageLayout(QPageLayout(pageSize,
+                                  QPageLayout::Orientation::Portrait,
+                                  QMarginsF()));
+
+    const int PAGE_WIDTH = pageSize.sizePixels(300).width();
+    const int PAGE_HEIGHT = pageSize.sizePixels(300).height();
+    const int MARGIN = 100;
+    const int CONTENT_WIDTH = PAGE_WIDTH - MARGIN * 2;
+    const int COL_WIDTH = CONTENT_WIDTH / 6;
+    const int FIRST_COL_WIDTH = COL_WIDTH * 2;
+    const int ROW_COUNT = 20;
+    const int ROW_HEIGHT = PAGE_HEIGHT / (ROW_COUNT + 1);
+
+    int CUR_X_POS = MARGIN;
+    int CUR_Y_POS = MARGIN;
+
+    auto centerY = [=](int posY) {
+        return posY - (ROW_HEIGHT / 2);
+    };
+
+    QPainter p;
+    p.begin(&pdf);
+
+    QFont headerFont("Ubuntu", 8, 600);
+    QFont symNameFont("Ubuntu", 8, 400);
+
+    for (size_t s = size_t(SymId::noSym); s < size_t(SymId::lastSym); ++s) {
+        if (s % ROW_COUNT == 0) {
+            if (s != 0) {
+                pdf.newPage();
+            }
+            CUR_X_POS = MARGIN;
+            CUR_Y_POS = MARGIN;
+
+            // Draw header
+            p.setPen(QColor("#000000"));
+            p.setFont(headerFont);
+            p.drawText(QPointF(CUR_X_POS, CUR_Y_POS - 10), "Sym");
+            CUR_X_POS += FIRST_COL_WIDTH;
+            for (const Font& f : fonts) {
+                p.drawText(QPointF(CUR_X_POS, CUR_Y_POS - 10), f.font.family());
+                CUR_X_POS += COL_WIDTH;
+            }
+
+            QPen pen(QColor("#000000"));
+            pen.setWidth(2);
+            p.setPen(pen);
+            p.drawLine(MARGIN, CUR_Y_POS, MARGIN + CONTENT_WIDTH, CUR_Y_POS);
+        }
+
+        SymId symId = SymId(s);
+
+        char32_t smuflCode = Smufl::smuflCode(symId);
+
+        QString symStr = QString::fromUcs4(&smuflCode, 1);
+
+        CUR_Y_POS += ROW_HEIGHT;
+        CUR_X_POS = MARGIN;
+
+        // row line
+        {
+            QPen pen(QColor("#000000"));
+            pen.setWidth(2);
+            p.setPen(pen);
+            p.drawLine(CUR_X_POS, CUR_Y_POS, CUR_X_POS + CONTENT_WIDTH, CUR_Y_POS);
+        }
+
+        // base line
+        {
+            p.setPen(QColor("#999999"));
+            p.drawLine(CUR_X_POS, centerY(CUR_Y_POS), CUR_X_POS + CONTENT_WIDTH, centerY(CUR_Y_POS));
+        }
+
+        QPen pen(QColor("#000000"));
+        pen.setWidth(1);
+        p.setPen(pen);
+
+        p.setFont(symNameFont);
+        p.drawText(QPointF(CUR_X_POS, centerY(CUR_Y_POS)), QString::number(s) + ". " + SymNames::nameForSymId(symId).ascii());
+        CUR_X_POS += FIRST_COL_WIDTH;
+
+        for (const Font& f : fonts) {
+            p.setFont(f.font);
+            p.drawText(QPointF(CUR_X_POS, centerY(CUR_Y_POS)), symStr);
+            CUR_X_POS += COL_WIDTH;
+        }
+    }
+
+    p.end();
+
+    //img.save("fonts.png");
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    QApplication a(argc, argv);
+
+    GlobalModule globalModule;
+    std::vector<IModuleSetup*> modules;
+    modules.push_back(new mu::fonts::FontsModule());
+    modules.push_back(new mu::draw::DrawModule());
+
+    // ====================================================
+    // Setup modules: Resources, Exports, Imports, UiTypes
+    // ====================================================
+    globalModule.registerResources();
+    globalModule.registerExports();
+    globalModule.registerUiTypes();
+
+    for (mu::modularity::IModuleSetup* m : modules) {
+        m->registerResources();
+    }
+
+    for (mu::modularity::IModuleSetup* m : modules) {
+        m->registerExports();
+    }
+
+    globalModule.resolveImports();
+    for (mu::modularity::IModuleSetup* m : modules) {
+        m->registerUiTypes();
+        m->resolveImports();
+    }
+
+    // Symbols
+    Smufl::init();
+
+    SymbolFonts::addFont(u"Bravura",    u"Bravura",     ":/fonts/bravura/Bravura.otf");
+    SymbolFonts::addFont(u"Leland",     u"Leland",      ":/fonts/leland/Leland.otf");
+    SymbolFonts::fontByName(u"Bravura"); // load
+    SymbolFonts::fontByName(u"Leland");  // load
+
+    QFontDatabase::addApplicationFont(":/fonts/bravura/BravuraText.otf");
+    QFontDatabase::addApplicationFont(":/fonts/leland/LelandText.otf");
+
+    // printStat();
+    drawImage();
 
     return 0;
 }
