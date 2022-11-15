76a44b7628d0b08234933dfda3d67136aa77bc13
Roman Pudashkin <r.pudashkin@gmail.com>
2022-11-15 12:01:09 +0200

    unlock updates in case of some errors

--- src/project/internal/notationproject.cpp
+++ src/project/internal/notationproject.cpp
@@ -41,6 +41,7 @@
 
 #include "libmscore/undo.h"
 
+#include "defer.h"
 #include "log.h"
 
 using namespace mu;
@@ -194,6 +195,9 @@ mu::Ret NotationProject::doLoad(engraving::MscReader& reader, const io::path_t& 
     }
 
     masterScore->lockUpdates(true);
+    DEFER {
+        masterScore->lockUpdates(false);
+    };
 
     // Setup master score
     err = m_engravingProject->setupMasterScore(forceMode);
