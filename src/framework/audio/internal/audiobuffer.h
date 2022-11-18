04ccf6c77e9398b81d6eca6f80cd70d0284ed894
pereverzev_v <v.pereverzev@wsmgroup.ru>
2022-11-18 22:15:08 +0200

    Implemented lock-free audio-buffer

--- src/framework/audio/internal/audiobuffer.h
+++ src/framework/audio/internal/audiobuffer.h
@@ -25,43 +25,45 @@
 #include <vector>
 #include <memory>
 #include <atomic>
-
-#include "modularity/ioc.h"
+#include <new>
 
 #include "iaudiobuffer.h"
 
 namespace mu::audio {
-class AudioBuffer : public IAudioBuffer
+class AudioBuffer
 {
 public:
     AudioBuffer() = default;
 
     void init(const audioch_t audioChannelsCount, const samples_t renderStep);
 
-    void setSource(std::shared_ptr<IAudioSource> source) override;
-    void forward() override;
+    void setSource(std::shared_ptr<IAudioSource> source);
+    void forward();
 
-    void pop(float* dest, size_t sampleCount) override;
-    void setMinSampleLag(size_t lag) override;
+    void pop(float* dest, size_t sampleCount);
+    void setMinSamplesToReserve(size_t lag);
+
+    void reset();
 
 private:
+    size_t reservedSamples(const size_t writeIdx, const size_t readIdx) const;
+    size_t incrementWriteIndex(const size_t writeIdx, const samples_t samplesPerChannel);
 
-    unsigned int sampleLag() const;
-    void fillup();
-    void updateWriteIndex(const unsigned int samplesPerChannel);
+    size_t m_minSamplesToReserve = 0;
 
-    std::mutex m_mutex;
-    size_t m_minSampleLag = 0;
-    size_t m_writeIndex = 0;
-    size_t m_readIndex = 0;
+    alignas(std::hardware_destructive_interference_size) std::atomic<size_t> m_writeIndex = 0;
+    alignas(std::hardware_destructive_interference_size) std::atomic<size_t> m_readIndex = 0;
+    alignas(std::hardware_destructive_interference_size) std::vector<float> m_data;
+
     samples_t m_samplesPerChannel = 0;
     audioch_t m_audioChannelsCount = 0;
 
     samples_t m_renderStep = 0;
 
-    std::vector<float> m_data = {};
     std::shared_ptr<IAudioSource> m_source = nullptr;
 };
+
+using AudioBufferPtr = std::shared_ptr<AudioBuffer>;
 }
 
 #endif // MU_AUDIO_BUFFER_H
