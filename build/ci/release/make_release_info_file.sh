9394ec196e7937eeb748aea05a634a49e7e94d5b
Eism <eismailzada@gmail.com>
2022-11-18 15:33:31 +0200

    Added additional workflows for testing update

--- build/ci/release/make_release_info_file.sh
+++ build/ci/release/make_release_info_file.sh
@@ -24,19 +24,10 @@ ARTIFACTS_DIR=build.artifacts
 GITHUB_TOKEN=""
 GITHUB_REPOSITORY=""
 
-S3_KEY=""
-S3_SECRET=""
-S3_URL=""
-S3_BUCKET=""
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --token) GITHUB_TOKEN="$2"; shift ;;
         --repo) GITHUB_REPOSITORY="$2"; shift ;;
-        --s3_key) S3_KEY="$2"; shift ;;
-        --s3_secret) S3_SECRET="$2"; shift ;;
-        --s3_url) S3_URL="$2"; shift ;;
-        --s3_bucket) S3_BUCKET="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -52,26 +43,3 @@ RELEASE_INFO=$(curl \
 mkdir -p $ARTIFACTS_DIR
 echo $RELEASE_INFO > $ARTIFACTS_DIR/release_info.json
 cat $ARTIFACTS_DIR/release_info.json
-
-command -v s3cmd >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    echo "=== Install tools ==="
-
-    apt install python3-setuptools
-
-    echo "Install s3cmd"
-    pip3 install s3cmd
-fi
-
-cat >~/.s3cfg <<EOL
-[default]
-access_key = ${S3_KEY}
-secret_key = ${S3_SECRET}
-host_base = ${S3_BUCKET}.com:443
-host_bucket = ${S3_BUCKET}.com:443
-website_endpoint = https://${S3_BUCKET}.com:443
-EOL
-
-echo "=== Publish to S3 ==="
-
-s3cmd put --acl-public --guess-mime-type "$ARTIFACTS_DIR/release_info.json" "$S3_URL"
