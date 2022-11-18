c49ba5e7b7f4a5cc124ee9edbcad0cd3a2e8f182
pereverzev_v <v.pereverzev@wsmgroup.ru>
2022-11-18 22:17:39 +0200

    Decoupled logic from AudioModule::init

--- src/framework/audio/internal/audiothread.cpp
+++ src/framework/audio/internal/audiothread.cpp
@@ -89,7 +89,7 @@ void AudioThread::main()
             m_mainLoopBody();
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     if (m_onFinished) {
