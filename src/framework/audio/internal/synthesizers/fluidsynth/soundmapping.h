01a049348deb74995cd266d819cf1bd434e4e4ba
pereverzev_v <v.pereverzev@wsmgroup.ru>
2022-12-05 19:27:48 +0200

    Properly add default midi_channel for fluid

--- src/framework/audio/internal/synthesizers/fluidsynth/soundmapping.h
+++ src/framework/audio/internal/synthesizers/fluidsynth/soundmapping.h
@@ -1026,14 +1026,14 @@ struct ChannelMap {
 
     midi::channel_t resolveChannelForEvent(const mpe::NoteEvent& event)
     {
+        if (m_standardPrograms.empty()) {
+            return 0;
+        }
+
+        const midi::Program& standardProgram = m_standardPrograms.at(0);
+
         if (event.expressionCtx().articulations.contains(mpe::ArticulationType::Standard)
             || event.expressionCtx().articulations.empty()) {
-            if (m_standardPrograms.empty()) {
-                return 0;
-            }
-
-            const midi::Program& standardProgram = m_standardPrograms.at(0);
-
             return resolveChannel(event.arrangementCtx().voiceLayerIndex, mpe::ArticulationType::Standard, standardProgram);
         }
 
@@ -1050,7 +1050,7 @@ struct ChannelMap {
             return resolveChannel(event.arrangementCtx().voiceLayerIndex, search->first, search->second);
         }
 
-        return 0;
+        return resolveChannel(event.arrangementCtx().voiceLayerIndex, mpe::ArticulationType::Standard, standardProgram);
     }
 
     midi::channel_t lastIndex() const
