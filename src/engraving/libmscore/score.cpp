musescore/MuseScore

commit c0a15a740bfbb40ba50bb35a88192aadf0d3ddda

fix #13366: don't call setUpTempoMap() for excerpts. They're just viewers, they shouldn't have any impact on the playback

src/engraving/libmscore/score.cpp
@@ -524,9 +524,15 @@ void Score::addMeasure(MeasureBase* m, MeasureBase* pos)
 
 void Score::setUpTempoMap()
 {
+    if (!isMaster()) {
+        return;
+    }
+
+    TRACEFUNC;
+
     Fraction tick = Fraction(0, 1);
     Measure* fm = firstMeasure();
-    if (fm == 0) {
+    if (!fm) {
         return;
     }
 
@@ -534,11 +540,9 @@ void Score::setUpTempoMap()
         staff->clearTimeSig();
     }
 
-    if (isMaster()) {
-        tempomap()->clear();
-        sigmap()->clear();
-        sigmap()->add(0, SigEvent(fm->ticks(),  fm->timesig(), 0));
-    }
+    tempomap()->clear();
+    sigmap()->clear();
+    sigmap()->add(0, SigEvent(fm->ticks(),  fm->timesig(), 0));
 
     for (MeasureBase* mb = first(); mb; mb = mb->next()) {
         if (mb->type() != ElementType::MEASURE) {

commit c8c6b59e1cfaec5750a842ddf6c7ff977f2081dd

Fix inserted measures being empty/corrupt in part scores
We _do_ need to run `Score::setUpTempoMap` in excerpt scores, to some extent: the parts about ticks and time signatures on staves must be done. Only sigmap and tempomap stuff belongs exclusively to master scores. (Fix regression caused by c0a15a740bfbb40ba50bb35a88192aadf0d3ddda)

src/engraving/libmscore/score.cpp
@@ -552,10 +552,6 @@ void Score::setUpTempoMapLater()
 
 void Score::setUpTempoMap()
 {
-    if (!isMaster()) {
-        return;
-    }
-
     TRACEFUNC;
 
     Fraction tick = Fraction(0, 1);
@@ -568,9 +564,11 @@ void Score::setUpTempoMap()
         staff->clearTimeSig();
     }
 
-    tempomap()->clear();
-    sigmap()->clear();
-    sigmap()->add(0, SigEvent(fm->ticks(),  fm->timesig(), 0));
+    if (isMaster()) {
+        tempomap()->clear();
+        sigmap()->clear();
+        sigmap()->add(0, SigEvent(fm->ticks(),  fm->timesig(), 0));
+    }
 
     for (MeasureBase* mb = first(); mb; mb = mb->next()) {
         if (mb->type() != ElementType::MEASURE) {
@@ -591,37 +589,39 @@ void Score::setUpTempoMap()
         tick += measureTicks;
     }
 
-    for (const auto& pair : spanner()) {
-        const Spanner* spannerItem = pair.second;
-        if (!spannerItem || !spannerItem->isGradualTempoChange()) {
-            continue;
-        }
+    if (isMaster()) {
+        for (const auto& pair : spanner()) {
+            const Spanner* spannerItem = pair.second;
+            if (!spannerItem || !spannerItem->isGradualTempoChange()) {
+                continue;
+            }
 
-        const GradualTempoChange* tempoChange = toGradualTempoChange(spannerItem);
-        if (!tempoChange) {
-            continue;
-        }
+            const GradualTempoChange* tempoChange = toGradualTempoChange(spannerItem);
+            if (!tempoChange) {
+                continue;
+            }
 
-        int tickPositionFrom = tempoChange->tick().ticks();
-        BeatsPerSecond currentBps = tempomap()->tempo(tickPositionFrom);
-        BeatsPerSecond newBps = currentBps * tempoChange->tempoChangeFactor();
+            int tickPositionFrom = tempoChange->tick().ticks();
+            BeatsPerSecond currentBps = tempomap()->tempo(tickPositionFrom);
+            BeatsPerSecond newBps = currentBps * tempoChange->tempoChangeFactor();
 
-        std::map<int, double> tempoCurve = TConv::easingValueCurve(tempoChange->ticks().ticks(),
-                                                                   4 /*stepsCount*/,
-                                                                   newBps.val - currentBps.val,
-                                                                   tempoChange->easingMethod());
+            std::map<int, double> tempoCurve = TConv::easingValueCurve(tempoChange->ticks().ticks(),
+                                                                       4 /*stepsCount*/,
+                                                                       newBps.val - currentBps.val,
+                                                                       tempoChange->easingMethod());
 
-        for (const auto& pair : tempoCurve) {
-            int tick = tickPositionFrom + pair.first;
+            for (const auto& pair : tempoCurve) {
+                int tick = tickPositionFrom + pair.first;
 
-            if (tempomap()->find(tick) == tempomap()->end()) {
-                tempomap()->setTempo(tick, BeatsPerSecond(currentBps.val + pair.second));
+                if (tempomap()->find(tick) == tempomap()->end()) {
+                    tempomap()->setTempo(tick, BeatsPerSecond(currentBps.val + pair.second));
+                }
             }
         }
-    }
 
-    if (tempomap()->empty()) {
-        tempomap()->setTempo(0, Constants::defaultTempo);
+        if (tempomap()->empty()) {
+            tempomap()->setTempo(0, Constants::defaultTempo);
+        }
     }
 
     masterScore()->updateRepeatListTempo();