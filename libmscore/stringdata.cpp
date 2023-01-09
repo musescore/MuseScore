35c96ad7f0da8a5867aaefa5794363b4163a34a0
Paul Keir <pkeir@outlook.com>
2023-01-09 13:02:21 +0100

    Fix #341410 and GH#15729: off by one error which incorrectly prohibits a tab note moving to a string at the highest fret.
    Backport of #15730

--- libmscore/stringdata.cpp
+++ libmscore/stringdata.cpp
@@ -407,7 +407,7 @@ int StringData::fret(int pitch, int string, int pitchOffset) const
              fret += strg.startFret;  // banjo 5th string adjustment
 
       // fret number is invalid or string cannot be fretted
-      if (fret < 0 || fret >= _frets || (fret > 0 && strg.open))
+      if (fret < 0 || fret > _frets || (fret > 0 && strg.open))
             return INVALID_FRET_INDEX ;
       return fret;
       }
