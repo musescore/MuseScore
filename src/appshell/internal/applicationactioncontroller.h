bee31934052c296920ff60965266fe1338773f8c
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-01-15 19:48:31 +0100

    Fix asking about saving score when receiving Quit event
    For example on macOS when clicking "Quit" in the Dock icon context menu or when shutting down the system

--- src/appshell/internal/applicationactioncontroller.h
+++ src/appshell/internal/applicationactioncontroller.h
@@ -72,7 +72,7 @@ private:
 
     void setupConnections();
 
-    void quit(bool isAllInstances, const io::path_t& installerPath = io::path_t());
+    bool quit(bool isAllInstances, const io::path_t& installerPath = io::path_t());
     void restart();
 
     void toggleFullScreen();
