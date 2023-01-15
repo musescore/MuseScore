fb1cb12ebd2366e79264470c5b20cd9b28c4f08d
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-01-15 23:24:49 +0100

    Fix crash when repeating non-full-measure selection when next measure is measure repeat

--- src/engraving/libmscore/edit.cpp
+++ src/engraving/libmscore/edit.cpp
@@ -400,9 +400,13 @@ Chord* Score::addChord(const Fraction& tick, TDuration d, Chord* oc, bool genTie
 ChordRest* Score::addClone(ChordRest* cr, const Fraction& tick, const TDuration& d)
 {
     ChordRest* newcr;
-    // change a MeasureRepeat() into an Rest()
+    // change a MeasureRepeat into an Rest
+    // To do: *properly* remove the measure repeat, like in Score::deleteItem
+    // https://github.com/musescore/MuseScore/issues/15844
+    // https://github.com/musescore/MuseScore/issues/15874
     if (cr->isMeasureRepeat()) {
         newcr = Factory::copyRest(*toRest(cr));
+        toRest(newcr)->hack_toRestType();
     } else {
         newcr = toChordRest(cr->clone());
     }
