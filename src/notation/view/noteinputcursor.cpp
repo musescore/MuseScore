12d9b815786930e58f6e4f8f5a5c2cbcb3cb12ce
allanohh <allanoberrender@hotmail.com>
2023-01-27 19:16:18 -0600

    made the cursorRect wider, with an easy to change number for quick editing. had to move LeftLineWidth up so I can use it in calculating the new position of the left-justified cursorRect. laid some groundwork for further modifying.

--- src/notation/view/noteinputcursor.cpp
+++ src/notation/view/noteinputcursor.cpp
@@ -24,6 +24,15 @@
 using namespace mu::notation;
 using namespace mu::engraving;
 
+
+
+
+//somethingstarted notes trying to fix #10664  -  dont delete these comments just yet.
+// to-do:     if fret 1-9 one width
+// to-do:       if fret 10+ different width
+//to-do: reference  striungdata.cpp, because eventually i want it to have different widths for 1 or 2 digit frets. 
+
+
 void NoteInputCursor::paint(mu::draw::Painter* painter)
 {
     INotationNoteInputPtr noteInput = currentNoteInput();
@@ -32,14 +41,37 @@ void NoteInputCursor::paint(mu::draw::Painter* painter)
     }
 
     NoteInputState state = noteInput->state();
-    RectF cursorRect = noteInput->cursorRect();
+    RectF cursorRect  = noteInput->cursorRect();
+    
+
+
+            //will need these later. please dont delete. - somethingstarted
+    //double WidthMultiplierBefore = 1.0;  //change size of both (keep at 1 normally)
+    //double OneDigitFretMult = 1.2;  //if it's  0-9,  frets width 
+    //double TwoDigitFretMult = 1.2;  //if it's 10+,  frets width
+
+    double WidthMultiplierFinal = 1.2; 
+    constexpr int leftLineWidth = 3;    
+
+    PointX OldCenter = cursorRect.center(); //store the old center
+    double OldWidth = cursorRect.width();//store old width
+    double NewWidth = cursorRect.width() * WidthMultiplierFinal;      //set new width
+    double PixToSlideLeft = (((OldWidth - NewWidth) / 2) - (leftLineWidth / 2)); //((find new left justification position) - (divide left line by 2 to find a middle ground between 2ch and 1ch frets.))
+
+    cursorRect.setLeft(cursorRect.left() + PixToSlideLeft); 
+    cursorRect.setWidth(NewWidth); 
+    
+    
+    
 
     Color fillColor = configuration()->selectionColor(state.currentVoiceIndex);
     Color cursorRectColor = fillColor;
     cursorRectColor.setAlpha(configuration()->cursorOpacity());
     painter->fillRect(cursorRect, cursorRectColor);
 
-    constexpr int leftLineWidth = 3;
+    
+
+     
     RectF leftLine(cursorRect.topLeft().x(), cursorRect.topLeft().y(), leftLineWidth, cursorRect.height());
     Color lineColor = fillColor;
     painter->fillRect(leftLine, lineColor);
