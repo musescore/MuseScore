5c424f1e8a218188543287adb8d679007fca9455
Igor Korsukov <igor.korsukov@gmail.com>
2023-02-15 16:54:55 +0200

    added the ability to build disabled midi module

---
+++ src/stubs/framework/midi/midistubmodule.cpp
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2023 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "midistubmodule.h"
+
+#include "modularity/ioc.h"
+
+#include "midiconfigurationstub.h"
+
+using namespace mu::midi;
+using namespace mu::modularity;
+
+std::string MidiModule::moduleName() const
+{
+    return "midi_stub";
+}
+
+void MidiModule::registerExports()
+{
+    ioc()->registerExport<IMidiConfiguration>(moduleName(), new MidiConfigurationStub());
+}
