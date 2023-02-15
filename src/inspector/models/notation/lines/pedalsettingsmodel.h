5a0bb7259897cc74659c764ada1109f3f8dfed8f
bakajikara <102702868+bakajikara@users.noreply.github.com>
2023-02-15 20:33:26 +0900

    Added text tab to pedal line properties (#14928)

--- src/inspector/models/notation/lines/pedalsettingsmodel.h
+++ src/inspector/models/notation/lines/pedalsettingsmodel.h
@@ -30,21 +30,15 @@ class PedalSettingsModel : public TextLineSettingsModel
     Q_OBJECT
 
     Q_PROPERTY(PropertyItem * lineType READ lineType CONSTANT)
-    Q_PROPERTY(bool pedalSymbolVisible READ pedalSymbolVisible WRITE setPedalSymbolVisible NOTIFY pedalSymbolVisibleChanged)
     Q_PROPERTY(bool isChangingLineVisibilityAllowed READ isChangingLineVisibilityAllowed NOTIFY isChangingLineVisibilityAllowedChanged)
 
 public:
     explicit PedalSettingsModel(QObject* parent, IElementRepositoryService* repository);
 
     PropertyItem* lineType() const;
-    bool pedalSymbolVisible() const;
     bool isChangingLineVisibilityAllowed() const;
 
-public slots:
-    void setPedalSymbolVisible(bool visible);
-
 signals:
-    void pedalSymbolVisibleChanged();
     void isChangingLineVisibilityAllowedChanged();
 
 private:
