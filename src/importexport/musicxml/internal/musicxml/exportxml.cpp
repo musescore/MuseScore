a5e1fcbb7d5b505ab8f42921bb9bc7efe67db0af
Aaron Sattely <a.sattely@musescore.com>
2023-03-14 18:18:20 -0400

    Add ability to export combined articulations,
    and ensure that correct articulation placement information is exported when changed from default

--- src/importexport/musicxml/internal/musicxml/exportxml.cpp
+++ src/importexport/musicxml/internal/musicxml/exportxml.cpp
@@ -2741,60 +2741,72 @@ static void fermatas(const QVector<EngravingItem*>& cra, XmlWriter& xml, Notatio
 //   symIdToArtic
 //---------------------------------------------------------
 
-static QString symIdToArtic(const SymId sid)
+static std::vector<QString> symIdToArtic(const SymId sid)
 {
     switch (sid) {
     case SymId::articAccentAbove:
     case SymId::articAccentBelow:
-        return "accent";
+        return { "accent" };
         break;
 
     case SymId::articStaccatoAbove:
     case SymId::articStaccatoBelow:
-    case SymId::articAccentStaccatoAbove:
-    case SymId::articAccentStaccatoBelow:
-    case SymId::articMarcatoStaccatoAbove:
-    case SymId::articMarcatoStaccatoBelow:
-        return "staccato";
+        return { "staccato" };
         break;
 
     case SymId::articStaccatissimoAbove:
     case SymId::articStaccatissimoBelow:
-    case SymId::articStaccatissimoStrokeAbove:
-    case SymId::articStaccatissimoStrokeBelow:
-    case SymId::articStaccatissimoWedgeAbove:
-    case SymId::articStaccatissimoWedgeBelow:
-        return "staccatissimo";
+        return { "staccatissimo" };
         break;
 
     case SymId::articTenutoAbove:
     case SymId::articTenutoBelow:
-        return "tenuto";
+        return { "tenuto" };
         break;
 
     case SymId::articMarcatoAbove:
     case SymId::articMarcatoBelow:
-        return "strong-accent";
+        return { "strong-accent" };
         break;
 
     case SymId::articTenutoStaccatoAbove:
     case SymId::articTenutoStaccatoBelow:
-        return "detached-legato";
+        return { "detached-legato" };
         break;
 
     case SymId::articSoftAccentAbove:
     case SymId::articSoftAccentBelow:
-        return "soft-accent";
+        return { "soft-accent" };
         break;
 
     case SymId::articStressAbove:
     case SymId::articStressBelow:
-        return "stress";
+        return { "stress" };
         break;
 
     case SymId::articUnstressAbove:
     case SymId::articUnstressBelow:
-        return "unstress";
+        return { "unstress" };
+        break;
+
+    case SymId::articAccentStaccatoAbove:
+    case SymId::articAccentStaccatoBelow:
+        return { "accent", "staccato" };
+        break;
+
+    case SymId::articMarcatoStaccatoAbove:
+    case SymId::articMarcatoStaccatoBelow:
+        return { "strong-accent", "staccato" };
+        break;
+
+    case SymId::articMarcatoTenutoAbove:
+    case SymId::articMarcatoTenutoBelow:
+        return { "strong-accent", "tenuto" };
+        break;
+
+    case SymId::articTenutoAccentAbove:
+    case SymId::articTenutoAccentBelow:
+        return { "tenuto", "accent" };
         break;
 
     default:
@@ -2802,7 +2814,7 @@ static QString symIdToArtic(const SymId sid)
         break;
     }
 
-    return "";
+    return {};
 }
 
 //---------------------------------------------------------
@@ -2967,14 +2979,21 @@ void ExportMusicXml::chordAttributes(Chord* chord, Notations& notations, Technic
         }
 
         auto sid = a->symId();
-        auto mxmlArtic = symIdToArtic(sid);
+        auto mxmlArtics = symIdToArtic(sid);
 
-        if (mxmlArtic != "") {
-            if (sid == SymId::articMarcatoAbove || sid == SymId::articMarcatoBelow) {
+        for (QString mxmlArtic : mxmlArtics) {
+            if (mxmlArtic == "strong-accent") {
                 if (a->up()) {
                     mxmlArtic += " type=\"up\"";
                 } else {
                     mxmlArtic += " type=\"down\"";
+                }
+            } else if (a->anchor() != ArticulationAnchor::CHORD) {
+                if (a->anchor() == ArticulationAnchor::TOP_CHORD
+                    || a->anchor() == ArticulationAnchor::TOP_STAFF) {
+                    mxmlArtic += " placement=\"above\"";
+                } else {
+                    mxmlArtic += " placement=\"below\"";
                 }
             }
 
@@ -3067,7 +3086,7 @@ void ExportMusicXml::chordAttributes(Chord* chord, Notations& notations, Technic
         }
 
         auto sid = a->symId();
-        if (symIdToArtic(sid) == ""
+        if (symIdToArtic(sid).empty()
             && symIdToOrnam(sid) == ""
             && symIdToTechn(sid) == ""
             && !isLaissezVibrer(sid)) {
