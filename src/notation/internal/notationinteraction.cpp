31da0e3257028ace436fe356b4da23e016b20144
Marc Sabatella <marc@outsideshore.com>
2023-03-22 13:52:39 -0600

    fix rest commands (tab / rhythm)

--- src/notation/internal/notationinteraction.cpp
+++ src/notation/internal/notationinteraction.cpp
@@ -3842,7 +3842,11 @@ void NotationInteraction::putRestToSelection()
     if (!is.duration().isValid() || is.duration().isZero() || is.duration().isMeasure()) {
         is.setDuration(DurationType::V_QUARTER);
     }
-    putRest(is.duration());
+    if (is.usingNoteEntryMethod(NoteEntryMethod::RHYTHM)) {
+        m_noteInput->padNote(Pad::REST);
+    } else {
+        putRest(is.duration());
+    }
 }
 
 void NotationInteraction::putRest(Duration duration)
--- src/notation/internal/notationuiactions.cpp
+++ src/notation/internal/notationuiactions.cpp
@@ -1091,6 +1091,12 @@ const UiActionList NotationUiActions::m_actions = {
              TranslatableString("action", "Eighth rest"),
              TranslatableString("action", "Enter rest: eighth")
              ),
+    UiAction("rest-TAB",
+             mu::context::UiCtxNotationOpened,
+             mu::context::CTX_NOTATION_NOTE_INPUT_STAFF_TAB,
+             X_TAB.arg(TranslatableString("action", "Rest")),
+             X_TAB.arg(TranslatableString("action", "Enter rest"))
+             ),
     UiAction("fret-0",
              mu::context::UiCtxNotationOpened,
              mu::context::CTX_NOTATION_NOTE_INPUT_STAFF_TAB,
@@ -2178,6 +2184,7 @@ const UiActionList NotationUiActions::m_actions = {
              mu::context::UiCtxNotationOpened,
              mu::context::CTX_NOTATION_OPENED,
              TranslatableString("action", "Rest"),
+             TranslatableString("action", "Toggle rest"),
              IconCode::Code::REST
              ),
     UiAction("next-segment-element",
