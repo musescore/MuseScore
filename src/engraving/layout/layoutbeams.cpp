musescore/MuseScore

commit bfb238595bac7c66df0a5362dcad04d014be1a77

prevent addition of unneeded beam fragments in cross-measure situation

src/engraving/layout/layoutbeams.cpp
@@ -332,15 +332,20 @@ void LayoutBeams::createBeams(Score* score, LayoutContext& lc, Measure* measure)
                 BeamMode mode = cr->beamMode();
                 if (mode == BeamMode::MID || mode == BeamMode::END || mode == BeamMode::BEGIN32 || mode == BeamMode::BEGIN64) {
                     ChordRest* prevCR = score->findCR(measure->tick() - Fraction::fromTicks(1), track);
+                    Beam* prevBeam = prevCR->beam();
                     if (prevCR) {
                         const Measure* pm = prevCR->measure();
                         if (!beamNoContinue(prevCR->beamMode())
                             && !pm->lineBreak() && !pm->pageBreak() && !pm->sectionBreak()
                             && lc.prevMeasure
                             && !(prevCR->isChord() && prevCR->durationType().type() <= DurationType::V_QUARTER)) {
-                            beam = prevCR->beam();
+                            beam = prevBeam;
                             //a1 = beam ? beam->elements().front() : prevCR;
                             a1 = beam ? nullptr : prevCR;               // when beam is found, a1 is no longer required.
+                        } else if (prevBeam && prevBeam == cr->beam() && prevBeam->elements().front() == prevCR) {
+                            // remove the beam from the previous chordrest because we do not currently
+                            // support cross-system beams
+                            prevCR->removeDeleteBeam(false);
                         }
                     }
                 }

src/engraving/libmscore/beam.cpp
@@ -459,7 +459,8 @@ void Beam::layout()
 
     size_t n = 0;
     for (ChordRest* cr : _elements) {
-        if (cr->measure()->system() != system) {
+        auto newSystem = cr->measure()->system();
+        if (newSystem && newSystem != system) {
             SpannerSegmentType st;
             if (n == 0) {
                 st = SpannerSegmentType::BEGIN;

commit 6f574deb2617fe506d1be07974694fddf1a8ad7b

Fix #16994: crash regression related to beams
Introduced in bfb238595bac7c66df0a5362dcad04d014be1a77

src/engraving/layout/layoutbeams.cpp
@@ -290,11 +290,11 @@ void LayoutBeams::createBeams(Score* score, LayoutContext& lc, Measure* measure)
             continue;
         }
 
-        ChordRest* a1    = 0;          // start of (potential) beam
+        ChordRest* a1    = nullptr;          // start of (potential) beam
         bool firstCR     = true;
-        Beam* beam       = 0;          // current beam
+        Beam* beam       = nullptr;          // current beam
         BeamMode bm    = BeamMode::AUTO;
-        ChordRest* prev  = 0;
+        ChordRest* prev  = nullptr;
         bool checkBeats  = false;
         Fraction stretch = Fraction(1, 1);
         std::unordered_map<int, TDuration> beatSubdivision;
@@ -326,7 +326,7 @@ void LayoutBeams::createBeams(Score* score, LayoutContext& lc, Measure* measure)
 
         for (Segment* segment = measure->first(st); segment; segment = segment->next(st)) {
             ChordRest* cr = segment->cr(track);
-            if (cr == 0) {
+            if (!cr) {
                 continue;
             }
 
@@ -336,8 +336,8 @@ void LayoutBeams::createBeams(Score* score, LayoutContext& lc, Measure* measure)
                 BeamMode mode = cr->beamMode();
                 if (mode == BeamMode::MID || mode == BeamMode::END || mode == BeamMode::BEGIN32 || mode == BeamMode::BEGIN64) {
                     ChordRest* prevCR = score->findCR(measure->tick() - Fraction::fromTicks(1), track);
-                    Beam* prevBeam = prevCR->beam();
                     if (prevCR) {
+                        Beam* prevBeam = prevCR->beam();
                         const Measure* pm = prevCR->measure();
                         if (!beamNoContinue(prevCR->beamMode())
                             && !pm->lineBreak() && !pm->pageBreak() && !pm->sectionBreak()