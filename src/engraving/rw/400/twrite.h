356970243c35b183d2dd99d7ffc0d46666d95ace
Igor Korsukov <igor.korsukov@gmail.com>
2023-04-12 13:46:29 +0300

    [engraving] added pedal write

--- src/engraving/rw/400/twrite.h
+++ src/engraving/rw/400/twrite.h
@@ -188,6 +188,7 @@ public:
     static void write(const Page* p, XmlWriter& xml, WriteContext& ctx);
     static void write(const PalmMute* p, XmlWriter& xml, WriteContext& ctx);
     static void write(const Part* p, XmlWriter& xml, WriteContext& ctx);
+    static void write(const Pedal* p, XmlWriter& xml, WriteContext& ctx);
 
 private:
     static void writeProperty(const EngravingItem* item, XmlWriter& xml, Pid pid);
