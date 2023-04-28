2b8ba13f14dd52bee86d9cca14dc3d988007c56e
Igor Korsukov <igor.korsukov@gmail.com>
2023-04-28 16:49:40 +0300

    [engraving] moved layout Bracket

--- src/engraving/libmscore/bracket.cpp
+++ src/engraving/libmscore/bracket.cpp
@@ -25,6 +25,7 @@
 #include "draw/types/brush.h"
 
 #include "types/typesconv.h"
+#include "layout/tlayout.h"
 
 #include "bracketItem.h"
 #include "factory.h"
@@ -165,96 +166,8 @@ void Bracket::setStaffSpan(size_t a, size_t b)
 
 void Bracket::layout()
 {
-    path = PainterPath();
-    if (h2 == 0.0) {
-        return;
-    }
-
-    setVisible(_bi->visible());
-    _shape.clear();
-    switch (bracketType()) {
-    case BracketType::BRACE: {
-        if (score()->styleSt(Sid::MusicalSymbolFont) == "Emmentaler" || score()->styleSt(Sid::MusicalSymbolFont) == "Gonville") {
-            _braceSymbol = SymId::noSym;
-            double w = score()->styleMM(Sid::akkoladeWidth);
-
-#define XM(a) (a + 700) * w / 700
-#define YM(a) (a + 7100) * h2 / 7100
-
-            path.moveTo(XM(-8), YM(-2048));
-            path.cubicTo(XM(-8), YM(-3192), XM(-360), YM(-4304), XM(-360), YM(-5400));                 // c 0
-            path.cubicTo(XM(-360), YM(-5952), XM(-264), YM(-6488), XM(32), YM(-6968));                 // c 1
-            path.cubicTo(XM(36), YM(-6974), XM(38), YM(-6984), XM(38), YM(-6990));                     // c 0
-            path.cubicTo(XM(38), YM(-7008), XM(16), YM(-7024), XM(0), YM(-7024));                      // c 0
-            path.cubicTo(XM(-8), YM(-7024), XM(-22), YM(-7022), XM(-32), YM(-7008));                   // c 1
-            path.cubicTo(XM(-416), YM(-6392), XM(-544), YM(-5680), XM(-544), YM(-4960));               // c 0
-            path.cubicTo(XM(-544), YM(-3800), XM(-168), YM(-2680), XM(-168), YM(-1568));               // c 0
-            path.cubicTo(XM(-168), YM(-1016), XM(-264), YM(-496), XM(-560), YM(-16));                  // c 1
-            path.lineTo(XM(-560), YM(0));                    //  l 1
-            path.lineTo(XM(-560), YM(16));                   //  l 1
-            path.cubicTo(XM(-264), YM(496), XM(-168), YM(1016), XM(-168), YM(1568));                   // c 0
-            path.cubicTo(XM(-168), YM(2680), XM(-544), YM(3800), XM(-544), YM(4960));                  // c 0
-            path.cubicTo(XM(-544), YM(5680), XM(-416), YM(6392), XM(-32), YM(7008));                   // c 1
-            path.cubicTo(XM(-22), YM(7022), XM(-8), YM(7024), XM(0), YM(7024));                        // c 0
-            path.cubicTo(XM(16), YM(7024), XM(38), YM(7008), XM(38), YM(6990));                        // c 0
-            path.cubicTo(XM(38), YM(6984), XM(36), YM(6974), XM(32), YM(6968));                        // c 1
-            path.cubicTo(XM(-264), YM(6488), XM(-360), YM(5952), XM(-360), YM(5400));                  // c 0
-            path.cubicTo(XM(-360), YM(4304), XM(-8), YM(3192), XM(-8), YM(2048));                      // c 0
-            path.cubicTo(XM(-8), YM(1320), XM(-136), YM(624), XM(-512), YM(0));                        // c 1
-            path.cubicTo(XM(-136), YM(-624), XM(-8), YM(-1320), XM(-8), YM(-2048));                    // c 0*/
-            setbbox(path.boundingRect());
-            _shape.add(bbox());
-        } else {
-            if (_braceSymbol == SymId::noSym) {
-                _braceSymbol = SymId::brace;
-            }
-            double h = h2 * 2;
-            double w = symWidth(_braceSymbol) * _magx;
-            bbox().setRect(0, 0, w, h);
-            _shape.add(bbox());
-        }
-    }
-    break;
-    case BracketType::NORMAL: {
-        double _spatium = spatium();
-        double w = score()->styleMM(Sid::bracketWidth) * .5;
-        double x = -w;
-
-        double bd   = (score()->styleSt(Sid::MusicalSymbolFont) == "Leland") ? _spatium * .5 : _spatium * .25;
-        _shape.add(RectF(x, -bd, w * 2, 2 * (h2 + bd)));
-        _shape.add(symBbox(SymId::bracketTop).translated(PointF(-w, -bd)));
-        _shape.add(symBbox(SymId::bracketBottom).translated(PointF(-w, bd + 2 * h2)));
-
-        w      += symWidth(SymId::bracketTop);
-        double y = -symHeight(SymId::bracketTop) - bd;
-        double h = (-y + h2) * 2;
-        bbox().setRect(x, y, w, h);
-    }
-    break;
-    case BracketType::SQUARE: {
-        double w = score()->styleMM(Sid::staffLineWidth) * .5;
-        double x = -w;
-        double y = -w;
-        double h = (h2 + w) * 2;
-        w      += (.5 * spatium() + 3 * w);
-        bbox().setRect(x, y, w, h);
-        _shape.add(bbox());
-    }
-    break;
-    case BracketType::LINE: {
-        double _spatium = spatium();
-        double w = 0.67 * score()->styleMM(Sid::bracketWidth) * .5;
-        double x = -w;
-        double bd = _spatium * .25;
-        double y = -bd;
-        double h = (-y + h2) * 2;
-        bbox().setRect(x, y, w, h);
-        _shape.add(bbox());
-    }
-    break;
-    case BracketType::NO_BRACKET:
-        break;
-    }
+    LayoutContext ctx(score());
+    TLayout::layout(this, ctx);
 }
 
 //---------------------------------------------------------
