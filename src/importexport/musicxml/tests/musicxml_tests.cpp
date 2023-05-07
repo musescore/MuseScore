0e5d874125b6da7a622d370c351e6a24b761964f
Hemant Antony <88136054+HemantAntony@users.noreply.github.com>
2023-05-07 21:22:08 +0530

    Fixed MusicXML corruption on tuning to negative octave

--- src/importexport/musicxml/tests/musicxml_tests.cpp
+++ src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -697,6 +697,9 @@ TEST_F(Musicxml_Tests, multiMeasureRest4) {
 TEST_F(Musicxml_Tests, multipleNotations) {
     mxmlIoTestRef("testMultipleNotations");
 }
+TEST_F(Musicxml_Tests, negativeOctave) {
+    mxmlMscxExportTestRef("testNegativeOctave");
+}
 TEST_F(Musicxml_Tests, nonStandardKeySig1) {
     mxmlIoTest("testNonStandardKeySig1");
 }
