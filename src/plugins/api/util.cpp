9ef7ec8e773cdb9948fae78c26322c8fb24d3c85
Peter Jonas <shoogle@users.noreply.github.com>
2023-05-11 00:50:15 +0100

    Fix FileIO::write in Plugin API

--- src/plugins/api/util.cpp
+++ src/plugins/api/util.cpp
@@ -88,7 +88,11 @@ bool FileIO::write(const QString& data)
         return false;
     }
 
-    QFile file(mSource);
+    QUrl url(mSource);
+
+    QString source = (url.isValid() && url.isLocalFile()) ? url.toLocalFile() : mSource;
+
+    QFile file(source);
     if (!file.open(QFile::WriteOnly | QFile::Truncate)) {
         return false;
     }
