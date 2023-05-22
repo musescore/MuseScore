714e4345ac0afca86ca957fcbc2aa4536655479e
sammik <samuelmiklas@gmail.com>
2023-05-22 10:12:02 +0200

    fix #15866 - correct playback of drum palette children notes of Chord needs correct track set

--- src/palette/view/widgets/drumsetpalette.cpp
+++ src/palette/view/widgets/drumsetpalette.cpp
@@ -196,7 +196,7 @@ void DrumsetPalette::previewSound(const Chord* chord, bool newChordSelected, con
 
     Chord* preview = chord->clone();
     preview->setParent(inputState.segment);
-    preview->setStaffIdx(engraving::track2staff(inputState.currentTrack));
+    preview->setTrack(inputState.currentTrack);
 
     playback()->playElements({ preview });
 
