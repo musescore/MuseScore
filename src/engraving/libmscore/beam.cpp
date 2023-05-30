2b48419b9fdb38e4fb3a2094c4e11b1c6f3a7156
asattely <a.sattely@musescore.com>
2023-05-30 13:14:20 -0400

    change beam behavior for rests not at the start or end of a beam
    in the future, when we have things like stemlets or a better way to handle beaming in general, we may want to remove this. but for now this resolves some situations where we can have unwanted beams happening

--- src/engraving/libmscore/beam.cpp
+++ src/engraving/libmscore/beam.cpp
@@ -313,13 +313,13 @@ void Beam::setTremAnchors()
     }
 }
 
-void Beam::calcBeamBreaks(const ChordRest* chord, const ChordRest* prevChord, int level, bool& isBroken32, bool& isBroken64) const
+void Beam::calcBeamBreaks(const ChordRest* cr, const ChordRest* prevCr, int level, bool& isBroken32, bool& isBroken64) const
 {
-    BeamMode beamMode = chord->beamMode();
+    BeamMode beamMode = cr->beamMode();
 
     // get default beam mode -- based on time signature preferences
-    const Groups& group = chord->staff()->group(chord->measure()->tick());
-    BeamMode defaultBeamMode = group.endBeam(chord, prevChord);
+    const Groups& group = cr->staff()->group(cr->measure()->tick());
+    BeamMode defaultBeamMode = group.endBeam(cr, prevCr);
 
     bool isManuallyBroken32 = level >= 1 && beamMode == BeamMode::BEGIN32;
     bool isManuallyBroken64 = level >= 2 && beamMode == BeamMode::BEGIN64;
@@ -332,17 +332,17 @@ void Beam::calcBeamBreaks(const ChordRest* chord, const ChordRest* prevChord, in
     // deal with beam-embedded triplets by breaking beams as if they are their underlying durations
     // note that we use max(hooks, 1) here because otherwise we'd end up breaking the main (level 0) beam for
     // tuplets that take up non-beamed amounts of space (eg. 16th note quintuplets)
-    if (level > 0 && prevChord && chord->beamMode() == BeamMode::AUTO) {
-        if (chord->tuplet() && chord->tuplet() != prevChord->tuplet()) {
+    if (level > 0 && prevCr && cr->beamMode() == BeamMode::AUTO) {
+        if (cr->tuplet() && cr->tuplet() != prevCr->tuplet()) {
             // this cr starts a tuplet
-            int beams = std::max(TDuration(chord->tuplet()->ticks()).hooks(), 1);
+            int beams = std::max(TDuration(cr->tuplet()->ticks()).hooks(), 1);
             if (beams <= level) {
                 isBroken32 = level == 1;
                 isBroken64 = level >= 2;
             }
-        } else if (prevChord->tuplet() && prevChord->tuplet() != chord->tuplet()) {
+        } else if (prevCr->tuplet() && prevCr->tuplet() != cr->tuplet()) {
             // this is a non-tuplet cr that is first after a tuplet
-            int beams = std::max(TDuration(prevChord->tuplet()->ticks()).hooks(), 1);
+            int beams = std::max(TDuration(prevCr->tuplet()->ticks()).hooks(), 1);
             if (beams <= level) {
                 isBroken32 = level == 1;
                 isBroken64 = level >= 2;
