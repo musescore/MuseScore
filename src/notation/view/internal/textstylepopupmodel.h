45067809adb149b746b3aa52fd42d497b5d1ddf5
hirnaymay <sony.vaio.1404@gmail.com>
2023-06-11 03:21:39 +0530

    Add basic TextStylePopup including model
    Update TextStylePopup.qml

---
+++ src/notation/view/internal/textstylepopupmodel.h
@@ -0,0 +1,36 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2023 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MU_NOTATION_TEXTSTYLEPOPUPMODEL_H
+#define MU_NOTATION_TEXTSTYLEPOPUPMODEL_H
+
+#include "abstractelementpopupmodel.h"
+
+namespace mu::notation {
+class TextStylePopupModel : public AbstractElementPopupModel
+{
+public:
+    explicit TextStylePopupModel(QObject* parent = nullptr);
+};
+}
+
+#endif // MU_NOTATION_TEXTSTYLEPOPUPMODEL_H
