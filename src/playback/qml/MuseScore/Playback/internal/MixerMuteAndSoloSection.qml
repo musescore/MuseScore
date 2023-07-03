musescore/MuseScore

commit 649f01534d57145c3677dd7c88c70d0f08af10df

Disable solo when muted

src/playback/qml/MuseScore/Playback/internal/MixerMuteAndSoloSection.qml
@@ -77,6 +77,8 @@ MixerPanelSection {
                 icon: IconCode.SOLO
                 checked: channelItem.solo
 
+                enabled: !channelItem.muted
+
                 navigation.name: "SoloButton"
                 navigation.panel: channelItem.panel
                 navigation.row: root.navigationRowStart + 1

src/playback/view/internal/mixerchannelitem.cpp
@@ -247,6 +247,10 @@ void MixerChannelItem::loadOutputParams(AudioOutputParams&& newParams)
         emit mutedChanged();
     }
 
+    if (newParams.muted) {
+        setSolo(false);
+    }
+
     loadOutputResourceItems(newParams.fxChain);
     loadAuxSendItems(newParams.auxSends);
 }

commit 72f2d3ee2457331a408680b4924710f4c384d618

Fix solo for multiple tracks
Regression introduced in 649f01534d57145c3677dd7c88c70d0f08af10df

src/playback/qml/MuseScore/Playback/internal/MixerMuteAndSoloSection.qml
@@ -78,7 +78,7 @@ MixerPanelSection {
                 icon: IconCode.SOLO
                 checked: channelItem.solo
 
-                enabled: channelItem.type !== MixerChannelItem.Aux && !channelItem.muted
+                enabled: channelItem.type !== MixerChannelItem.Aux && (!channelItem.mutedManually || channelItem.solo)
                 visible: channelItem.type !== MixerChannelItem.Master
 
                 navigation.name: "SoloButton"