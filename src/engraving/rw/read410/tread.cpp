d3be163b8e21fed64d9f8490d008eda41392ffce
Michele Spagnolo <spagnolo.michele.1994@gmail.com>
2023-07-11 10:00:09 +0200

    Correct scaling errors of chordlines

--- src/engraving/rw/read410/tread.cpp
+++ src/engraving/rw/read410/tread.cpp
@@ -2530,6 +2530,9 @@ void TRead::read(ChordLine* l, XmlReader& e, ReadContext& ctx)
                     int type = e.intAttribute("type");
                     double x  = e.doubleAttribute("x");
                     double y  = e.doubleAttribute("y");
+                    double spatium = ctx.spatium();
+                    x *= spatium;
+                    y *= spatium;
                     switch (PainterPath::ElementType(type)) {
                     case PainterPath::ElementType::MoveToElement:
                         path.moveTo(x, y);
