805b819d512690ea3c04aa2808db634b7ecaed79
Laurent Pugin <lxpugin@gmail.com>
2023-07-21 10:49:07 +0200

    Add the libmei.h with defines and namespace level implementations

---
+++ src/importexport/mei/internal/libmei.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        libmei.h
+// Author:      Laurent Pugin
+// Created:     2023
+// Copyright (c) Authors and others. All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __LIBMEI_H__
+#define __LIBMEI_H__
+
+#define DEFINITION_FACTOR 1
+
+namespace libmei {
+
+#define STRING_FORMAT_MAX_LEN 2048
+
+/**
+ * Implement the namespace level libmei function.
+ */
+
+void LogWarning(const char* fmt, ...)
+{
+    std::string str(STRING_FORMAT_MAX_LEN, 0);
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(&str[0], STRING_FORMAT_MAX_LEN, fmt, args);
+    va_end(args);
+    str.resize(strlen(str.data()));
+    // Temporary commented
+    // mu::iex::mei::Convert::logs.push_back(String::fromStdString(str));
+}
+
+/**
+ * Implement the namespace level libmei function.
+ */
+
+std::string StringFormat(const char* fmt, ...)
+{
+    std::string str(STRING_FORMAT_MAX_LEN, 0);
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(&str[0], STRING_FORMAT_MAX_LEN, fmt, args);
+    va_end(args);
+    str.resize(strlen(str.data()));
+    return str;
+}
+} // namespace
+
+#endif
