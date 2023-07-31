fc2be88acfa59f32187835798915b9de2d78c8fb
Antonio Lotti (ABL) <antonio.lotti@alice.it>
2023-07-31 10:27:07 +0200

    Revert the use of static runtime AppImage

--- build/ci/linux/tools/make_appimage.sh
+++ build/ci/linux/tools/make_appimage.sh
@@ -44,14 +44,14 @@ function download_appimage_release()
   local -r github_repo_slug="$1" binary_name="$2" tag="$3"
   local -r appimage="${binary_name}-x86_64.AppImage"
   download_github_release "${github_repo_slug}" "${tag}" "${appimage}"
-  # extract_appimage "${appimage}" "${binary_name}"
-  mv "${appimage}" "${binary_name}"
+  extract_appimage "${appimage}" "${binary_name}"
+  # mv "${appimage}" "${binary_name}" # use this instead of the previous line for the static runtime AppImage
 }
 
 if [[ ! -d $BUILD_TOOLS/appimagetool ]]; then
   mkdir $BUILD_TOOLS/appimagetool
   cd $BUILD_TOOLS/appimagetool
-  download_appimage_release AppImage/appimagetool appimagetool continuous
+  download_appimage_release AppImage/AppImageKit appimagetool continuous # use AppImage/appimagetool for the static runtime AppImage
   cd $ORIGIN_DIR
 fi
 export PATH="$BUILD_TOOLS/appimagetool:$PATH"
