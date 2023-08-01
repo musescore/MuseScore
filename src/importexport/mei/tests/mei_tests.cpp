a6c19390fff2744033db6a05c7279604e6c05c08
Laurent Pugin <lxpugin@gmail.com>
2023-08-01 22:31:50 +0100

    Adjust the MEI test for round-trip testing

--- src/importexport/mei/tests/mei_tests.cpp
+++ src/importexport/mei/tests/mei_tests.cpp
@@ -33,11 +33,13 @@
 #include "modularity/ioc.h"
 #include "importexport/mei/imeiconfiguration.h"
 #include "importexport/mei/internal/meireader.h"
+#include "importexport/mei/internal/meiwriter.h"
 
 using namespace mu;
 using namespace mu::engraving;
 
 static const String MEI_DIR(u"data/");
+static const String TMP_DIR(u"tmp/");
 
 namespace mu::iex::mei {
 class Mei_Tests : public ::testing::Test
@@ -58,6 +60,12 @@ void Mei_Tests::meiReadTest(const char* file, const char* ext)
         return meiReader.import(score, path);
     };
 
+    auto exportFunc = [](Score* score, const io::path_t& path) -> Err {
+        MeiWriter meiWriter;
+        return meiWriter.writeScore(score, path);
+    };
+
+    // Load the .mei file
     MasterScore* score = ScoreRW::readScore(MEI_DIR + fileName + u"." + String::fromUtf8(ext), false, importFunc);
     EXPECT_TRUE(score);
 
@@ -68,8 +76,18 @@ void Mei_Tests::meiReadTest(const char* file, const char* ext)
         return;
     }
 
-    EXPECT_TRUE(ScoreComp::saveCompareScore(score, fileName + u".mscx", MEI_DIR + fileName + u".mscx"));
+    // Save the .mei file to the ./tmp directory for round trip testing
+    bool output = ScoreRW::saveScore(score, ScoreRW::rootPath() + u"/" + TMP_DIR + fileName + u".mei", exportFunc);
+    EXPECT_TRUE(output);
     delete score;
+
+    // Reload the generated .mei file
+    MasterScore* score2 = ScoreRW::readScore(TMP_DIR + fileName + u".mei", false, importFunc);
+    EXPECT_TRUE(score2);
+
+    // Compare with the reference MuseScore file
+    EXPECT_TRUE(ScoreComp::saveCompareScore(score2, fileName + u".mscx", MEI_DIR + fileName + u".mscx"));
+    delete score2;
 }
 
 TEST_F(Mei_Tests, meiAccid01) {
