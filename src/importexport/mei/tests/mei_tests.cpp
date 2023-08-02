837c01d45720c9e32e83c014337ef83e228b2b17
Laurent Pugin <lxpugin@gmail.com>
2023-08-02 11:52:03 +0100

    Change the tests to compare the MEI files instead of reloading them into MuseScore

--- src/importexport/mei/tests/mei_tests.cpp
+++ src/importexport/mei/tests/mei_tests.cpp
@@ -83,11 +83,10 @@ void Mei_Tests::meiReadTest(const char* file, const char* ext)
     EXPECT_TRUE(output);
     delete score;
 
-    // Potentially directly compare the mei files
-    // This currently passes inconsitently because of some order differences in the xml elements
-    // If this can be fixed, we can used that for testing and not re-import the mei file again
-    // EXPECT_TRUE(ScoreComp::compareFiles(fileName + u".test.mei", ScoreRW::rootPath() + u"/" + MEI_DIR + fileName + u".mei"));
+    // Compare the mei files
+    EXPECT_TRUE(ScoreComp::compareFiles(fileName + u".test.mei", ScoreRW::rootPath() + u"/" + MEI_DIR + fileName + u".mei"));
 
+    /*
     // Reload the generated .mei file
     MasterScore* score2 = ScoreRW::readScore(fileName + u".test.mei", true, importFunc);
     EXPECT_TRUE(score2);
@@ -95,12 +94,158 @@ void Mei_Tests::meiReadTest(const char* file, const char* ext)
     // Compare with the reference MuseScore file
     EXPECT_TRUE(ScoreComp::saveCompareScore(score2, fileName + u".mscx", MEI_DIR + fileName + u".mscx"));
     delete score2;
+    */
 }
 
-TEST_F(Mei_Tests, meiAccid01) {
+TEST_F(Mei_Tests, mei_accid_01) {
     meiReadTest("accid-01", "mei");
 }
-TEST_F(Mei_Tests, meiBreath01) {
+
+TEST_F(Mei_Tests, mei_accid_02) {
+    meiReadTest("accid-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_beam_01) {
+    meiReadTest("beam-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_beam_02) {
+    meiReadTest("beam-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_beam_03) {
+    meiReadTest("beam-03", "mei");
+}
+
+TEST_F(Mei_Tests, mei_breaks_01) {
+    meiReadTest("breaks-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_breath_01) {
     meiReadTest("breath-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_chord_label_01) {
+    meiReadTest("chord-label-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_clef_01) {
+    meiReadTest("clef-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_cross_staff_01) {
+    meiReadTest("cross-staff-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_dir_01) {
+    meiReadTest("dir-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_dynamic_01) {
+    meiReadTest("dynamic-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_ending_01) {
+    meiReadTest("ending-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_fermata_01) {
+    meiReadTest("fermata-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_gracenote_01) {
+    meiReadTest("gracenote-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_gracenote_02) {
+    meiReadTest("gracenote-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_jump_01) {
+    meiReadTest("jump-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_jump_02) {
+    meiReadTest("jump-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_key_signature_01) {
+    meiReadTest("key-signature-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_measure_01) {
+    meiReadTest("measure-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_measure_02) {
+    meiReadTest("measure-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_metadata_01) {
+    meiReadTest("metadata-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_page_head_01) {
+    meiReadTest("page-head-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_page_head_02) {
+    meiReadTest("page-head-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_roman_numeral_01) {
+    meiReadTest("roman-numeral-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_score_01) {
+    meiReadTest("score-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_score_02) {
+    meiReadTest("score-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_slur_01) {
+    meiReadTest("slur-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_slur_02) {
+    meiReadTest("slur-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_stem_01) {
+    meiReadTest("stem-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_tempo_01) {
+    meiReadTest("tempo-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_tie_01) {
+    meiReadTest("tie-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_time_signature_01) {
+    meiReadTest("time-signature-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_time_signature_02) {
+    meiReadTest("time-signature-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_transpose_01) {
+    meiReadTest("transpose-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_tuplet_01) {
+    meiReadTest("tuplet-01", "mei");
+}
+
+TEST_F(Mei_Tests, mei_tuplet_02) {
+    meiReadTest("tuplet-02", "mei");
+}
+
+TEST_F(Mei_Tests, mei_tuplet_03) {
+    meiReadTest("tuplet-03", "mei");
 }
 }
