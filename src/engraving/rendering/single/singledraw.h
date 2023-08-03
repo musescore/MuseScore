2d08261feaff281faea934bfc8d3c34022f1960e
Igor Korsukov <igor.korsukov@gmail.com>
2023-08-03 15:13:44 +0300

    [engraving] moved TempoText draw

--- src/engraving/rendering/single/singledraw.h
+++ src/engraving/rendering/single/singledraw.h
@@ -228,6 +228,8 @@ private:
     static void draw(const SystemDivider* item, draw::Painter* painter);
     static void draw(const SystemText* item, draw::Painter* painter);
 
+    static void draw(const TempoText* item, draw::Painter* painter);
+
     static void drawTextBase(const TextBase* item, draw::Painter* painter);
     static void drawTextLineBaseSegment(const TextLineBaseSegment* item, draw::Painter* painter);
 };
