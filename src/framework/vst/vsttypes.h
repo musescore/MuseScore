c4314fb0eb242ab28a171e6ebb8a5286d6f3bcd3
Joachim Schmitz <jojo@schmitz-digital.de>
2023-09-02 22:07:07 +0200

    Revert "Disable `sprintf` deprecation warnings from VST SDK"
    This reverts commit c9303656a3590a2508c3bf04f762f302c7757cf4.

--- src/framework/vst/vsttypes.h
+++ src/framework/vst/vsttypes.h
@@ -26,14 +26,6 @@
 #include <unordered_map>
 #include <vector>
 
-#if (defined (_MSCVER) || defined (_MSC_VER))
-#pragma warning (push)
-#pragma warning (disable: 4996)
-#else
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
 #include "public.sdk/source/vst/hosting/module.h"
 #include "public.sdk/source/vst/hosting/plugprovider.h"
 #include "public.sdk/source/vst/hosting/hostclasses.h"
@@ -47,12 +39,6 @@
 #include "pluginterfaces/vst/ivstaudioprocessor.h"
 #include "pluginterfaces/vst/ivsteditcontroller.h"
 #include "pluginterfaces/vst/ivstmidicontrollers.h"
-
-#if (defined (_MSCVER) || defined (_MSC_VER))
-#pragma warning (pop)
-#else
-#pragma GCC diagnostic pop
-#endif
 
 #include "framework/midi/miditypes.h"
 #include "io/path.h"
