cf17123bea75a33c1940a6f46af7803010c4da22
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-09-03 20:22:13 +0200

    CMake cleanup: clarify names of MUSESCORE_VERSION variables

--- build/ci/tools/make_version_env.sh
+++ build/ci/tools/make_version_env.sh
@@ -26,10 +26,9 @@ ARTIFACTS_DIR="build.artifacts" # default output dir
 
 if [ -z "$2" ]; then OUT_DIR=${ARTIFACTS_DIR}/env; fi
 
-export MUSESCORE_VERSION=$(cmake -P version.cmake | sed -n -e 's/^.*MUSESCORE_VERSION_FULL  *//p')
+export MUSESCORE_VERSION=$(cmake -P version.cmake | sed -n -e 's/^.*MUSESCORE_VERSION  *//p')
 
 MUSESCORE_VERSION_FULL=$MUSESCORE_VERSION.$BUILD_NUMBER
 
 echo ${MUSESCORE_VERSION_FULL} > ${OUT_DIR}/build_version.env
 cat ${OUT_DIR}/build_version.env
-
