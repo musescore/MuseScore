56fa3ccee509e4fdea917f624b2d386e7272f832
Malte Meyn <mail@maltemeyn.de>
2023-09-07 13:32:47 +0200

    Fix #18798: corrects system bracket display in continuous view

--- src/engraving/rendering/dev/scorehorizontalviewlayout.cpp
+++ src/engraving/rendering/dev/scorehorizontalviewlayout.cpp
@@ -343,5 +343,7 @@ void ScoreHorizontalViewLayout::collectLinearSystem(LayoutContext& ctx)
         MeasureLayout::getNextMeasure(ctx);
     }
 
+    SystemLayout::hideEmptyStaves(system, ctx, true);
+
     system->setWidth(pos.x());
 }
