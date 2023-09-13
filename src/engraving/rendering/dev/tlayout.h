ad31285bdf573ff7adf6c3809144d505fa28d884
Igor Korsukov <igor.korsukov@gmail.com>
2023-09-13 17:09:16 +0300

    [engraving] added LD_CONDITIONS to Dynamic layout

--- src/engraving/rendering/dev/tlayout.h
+++ src/engraving/rendering/dev/tlayout.h
@@ -41,6 +41,7 @@
 #include "../../dom/capo.h"
 
 #include "../../dom/deadslapped.h"
+#include "../../dom/dynamic.h"
 
 #include "../../dom/measurebase.h"
 #include "../../dom/measurenumberbase.h"
@@ -56,8 +57,6 @@ class BagpipeEmbellishment;
 class Beam;
 
 class Chord;
-
-class Dynamic;
 
 class Expression;
 
@@ -202,7 +201,7 @@ public:
     static void layout(const Capo* item, Capo::LayoutData* ldata, const LayoutContext& ctx);
 
     static void layout(const DeadSlapped* item, DeadSlapped::LayoutData* ldata);
-    static void layout(Dynamic* item, LayoutContext& ctx);
+    static void layout(const Dynamic* item, Dynamic::LayoutData* ldata, const LayoutConfiguration& conf);
 
     static void layout(Expression* item, LayoutContext& ctx);
 
