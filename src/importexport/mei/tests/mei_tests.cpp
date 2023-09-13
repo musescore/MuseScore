f4b6b1b309013c7eeec5979a5c0e5b51af212a65
Laurent Pugin <lxpugin@gmail.com>
2023-09-13 11:30:57 +0200

    Add color test file

--- src/importexport/mei/tests/mei_tests.cpp
+++ src/importexport/mei/tests/mei_tests.cpp
@@ -40,14 +40,18 @@ using namespace mu::engraving;
 
 static const String MEI_DIR(u"data/");
 
+////////////////////////////////////////////////////////////////
+// Set to true to re-generate the MuseScore reference test files
+#define BUILD_MSCORE_REF_FILE false
+////////////////////////////////////////////////////////////////
+
 namespace mu::iex::mei {
 class Mei_Tests : public ::testing::Test
 {
 public:
     void meiReadTest(const char* file);
 
-    // Change this to true to re-generate reference files
-    inline static bool s_generateReferenceFile = false;
+    inline static bool s_generateReferenceFile = BUILD_MSCORE_REF_FILE;
 };
 
 void Mei_Tests::meiReadTest(const char* file)
@@ -121,6 +125,10 @@ TEST_F(Mei_Tests, mei_chord_label_01) {
 
 TEST_F(Mei_Tests, mei_clef_01) {
     meiReadTest("clef-01");
+}
+
+TEST_F(Mei_Tests, mei_color_01) {
+    meiReadTest("color-01");
 }
 
 TEST_F(Mei_Tests, mei_cross_staff_01) {
