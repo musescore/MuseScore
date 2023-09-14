db03580e701dcae6a3b3eeb8d9d2d7e054e5ac37
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-09-14 23:23:09 +0200

    Final fixes to Windows installer

--- build/SetupWindowsPackaging.cmake
+++ build/SetupWindowsPackaging.cmake
@@ -9,7 +9,7 @@ include(InstallRequiredSystemLibraries)
 set(CPACK_PACKAGE_NAME ${MUSESCORE_NAME})
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "MuseScore is a full featured WYSIWYG score editor")
 set(CPACK_PACKAGE_VENDOR "MuseScore BVBA and Others")
-set(CPACK_PACKAGE_CONTACT "info@musescore.org")
+set(CPACK_PACKAGE_CONTACT "https://musescore.org")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://musescore.org")
 
 set(CPACK_PACKAGE_VERSION_MAJOR "${MUSESCORE_VERSION_MAJOR}")
