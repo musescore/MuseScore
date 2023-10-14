641898e3251f5a742b3ac67fbc416192ca348191
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-10-14 02:09:18 +0200

    macOS Notarization: migrate to `notarytool`
    https://developer.apple.com/documentation/technotes/tn3147-migrating-to-the-latest-notarization-tool?changes=_3_3

--- build/ci/macos/notarize.sh
+++ build/ci/macos/notarize.sh
@@ -43,38 +43,10 @@ echo "APPLE_PASSWORD: $APPLE_PASSWORD"
 ARTIFACT_NAME="$(cat $ARTIFACTS_DIR/env/artifact_name.env)"
 echo "ARTIFACT_NAME: $ARTIFACT_NAME"
 
-echo "Uploading to apple to notarize..."
-RequestUUID=$(xcrun altool --notarize-app --primary-bundle-id "org.musescore.MuseScore" -u $APPLE_USERNAME -p $APPLE_PASSWORD --asc-provider MuseScore --file $ARTIFACTS_DIR/$ARTIFACT_NAME 2>&1 | grep RequestUUID | awk '{print $3'})
-
-if  [ -z "$RequestUUID" ]
-then
-	echo "Notarization failed; running again to get error message"
-	failure=$(xcrun altool --notarize-app --primary-bundle-id "org.musescore.MuseScore" -u $APPLE_USERNAME -p $APPLE_PASSWORD --asc-provider MuseScore --file $ARTIFACTS_DIR/$ARTIFACT_NAME 2>&1)
-	echo "MESSAGE: $failure"
-	exit 1
-else
-	echo "Got notarization request UUID: $RequestUUID"
-fi
+echo "Uploading to Apple to notarize..."
 
 success=0
-sleep 30
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
-	echo "Checking progress..."
-	progress=$(xcrun altool --notarization-info "${RequestUUID}" -u $APPLE_USERNAME -p $APPLE_PASSWORD 2>&1)
-	echo "Progress: ${progress}"
-
-	if [ $? -ne 0 ] || [[ "${progress}" =~ "Invalid" ]]; then
-		echo "Error with notarization. Exiting"
-	fi
-
-	if [[ "${progress}" =~ "success" ]]; then
-		success=1
-		break
-	else
-		echo "Not completed yet. Sleeping for 30 seconds."
-	fi
-	sleep 30
-done
+xcrun notarytool submit --apple-id $APPLE_USERNAME --team-id MuseScore --password $APPLE_PASSWORD --wait $ARTIFACTS_DIR/$ARTIFACT_NAME && success=1
 
 if [ $success -eq 1 ] ; then
 	echo "Stapling and running packaging up"
