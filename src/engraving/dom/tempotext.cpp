7d2cfa33ef6f9f519e6fabdfd8c07daf65add88d
Joachim Schmitz <jojo@schmitz-digital.de>
2023-10-25 19:55:49 +0200

    Ignore "ca." and "approx." in tempo text

--- src/engraving/dom/tempotext.cpp
+++ src/engraving/dom/tempotext.cpp
@@ -259,6 +259,8 @@ void TempoText::updateTempo()
     s.replace(u"≒", u"=");
     s.replace(u"≈", u"=");
     s.replace(u"~", u"=");
+    s.replace(u"ca.", u"");
+    s.replace(u"approx.", u"");
     std::string su8 = s.toStdString();
     for (const TempoPattern& pa : tp) {
         String pattern = String::fromUtf8(pa.pattern);
