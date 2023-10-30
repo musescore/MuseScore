862b1ac35ed708ef15f1c7369c37b50baa951510
Michele Spagnolo <m.spagnolo@mu.se>
2023-10-30 16:59:38 +0100

    Correct GuitarBend::setEndNote()

--- src/engraving/dom/guitarbend.cpp
+++ src/engraving/dom/guitarbend.cpp
@@ -91,7 +91,18 @@ Note* GuitarBend::endNote() const
 void GuitarBend::setEndNotePitch(int pitch)
 {
     Note* note = endNote();
-    score()->undoChangePitch(note, pitch, note->tpc1(), note->tpc2());
+
+    Fraction tick = note->tick();
+    Staff* staff = note->staff();
+
+    Key key = staff->key(tick);
+    Interval interval = staff->transpose(tick);
+    interval.flip();
+
+    int targetTpc1 = pitch2tpc(pitch, key, Prefer::NEAREST);
+    int targetTpc2 = transposeTpc(targetTpc1, interval, true);
+
+    score()->undoChangePitch(note, pitch, targetTpc1, targetTpc2);
 
     computeBendAmount();
 
