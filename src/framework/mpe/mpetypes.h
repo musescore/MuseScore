b50b2e7e7e4d64bc46e6121d0d075adcaf7ca2cb
Roman Pudashkin <r.pudashkin@gmail.com>
2023-12-05 11:04:40 +0200

    Treat Vibrato as a single note articulation
    Thus, it won't cause the last tied note to be played

--- src/framework/mpe/mpetypes.h
+++ src/framework/mpe/mpetypes.h
@@ -352,6 +352,7 @@ inline bool isSingleNoteArticulation(const ArticulationType type)
         ArticulationType::Doit, ArticulationType::Plop, ArticulationType::Scoop,
         ArticulationType::BrassBend, ArticulationType::SlideOutDown, ArticulationType::SlideOutUp,
         ArticulationType::SlideInAbove, ArticulationType::SlideInBelow, ArticulationType::VolumeSwell,
+        ArticulationType::Vibrato,
     };
 
     return SINGLE_NOTE_TYPES.find(type) != SINGLE_NOTE_TYPES.cend();
