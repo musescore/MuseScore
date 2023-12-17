57c4a824b508b1b4be0baddfa66febdf0c7d5fdc
mikekirin <mike.kirin@hotmail.com>
2023-12-17 20:23:22 +0100

    feat: add < operator to ByteArray

--- src/framework/global/types/bytearray.cpp
+++ src/framework/global/types/bytearray.cpp
@@ -128,6 +128,13 @@ bool ByteArray::operator==(const ByteArray& other) const
     return std::memcmp(constData(), other.constData(), size()) == 0;
 }
 
+bool ByteArray::operator<(const ByteArray &other) const {
+    if (size() < other.size()) {
+        return true;
+    }
+    return std::memcmp(constData(), other.constData(), size()) < 0;
+}
+
 bool ByteArray::empty() const
 {
     return size() == 0;
