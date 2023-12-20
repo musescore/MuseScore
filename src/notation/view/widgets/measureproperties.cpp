97bcefebf6fe39287aa8ef138650b7894775a031
Peter Jonas <shoogle@users.noreply.github.com>
2023-12-20 03:46:03 +0000

    Fix crash in Measure & Staff/Part properties dialogs via keyboard
    Fix #20580. Crash occurs when dialogs are opened via the keyboard.

--- src/notation/view/widgets/measureproperties.cpp
+++ src/notation/view/widgets/measureproperties.cpp
@@ -94,6 +94,13 @@ void MeasurePropertiesDialog::initMeasure()
     mu::engraving::Measure* measure = mu::engraving::toMeasure(context.element);
 
     if (!measure) {
+        INotationSelectionPtr selection = m_notation->interaction()->selection();
+        if (selection->isRange()) {
+            measure = selection->range()->measureRange().endMeasure;
+        }
+    }
+
+    IF_ASSERT_FAILED(measure) {
         return;
     }
 
