5c83e938d697cd32635b85241cee42bffe15ec47
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-12-26 03:25:12 +0100

    Fix crash when building with VST3 SDK v3.7.9

--- src/framework/vst/internal/vstplugin.cpp
+++ src/framework/vst/internal/vstplugin.cpp
@@ -119,6 +119,11 @@ void VstPlugin::load()
             return;
         }
 
+        if (!m_pluginProvider->init()) {
+            LOGE() << "Unable to initialize vst plugin provider";
+            return;
+        }
+
         auto controller = m_pluginProvider->controller();
 
         if (!controller) {
