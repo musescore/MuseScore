bbe5b977ddbc9ff13a120f13e430cf26c95adb4e
Eism <eismailzada@gmail.com>
2024-01-03 11:29:24 +0200

    the "Parentheses indicating ties" section should become enabled when Show ties only is selected

--- src/framework/ui/view/uitheme.cpp
+++ src/framework/ui/view/uitheme.cpp
@@ -930,7 +930,9 @@ void UiTheme::drawRadioButtonIndicator(QPainter* painter, const QRect& rect, con
     QColor borderColor = fontPrimaryColor();
     QColor backgroundColor = textFieldColor();
 
-    if (styleState.pressed) {
+    if (!styleState.enabled) {
+        borderColor.setAlphaF(itemOpacityDisabled());
+    } else if (styleState.pressed) {
         borderColor.setAlphaF(buttonOpacityHit());
     } else if (styleState.hovered) {
         borderColor.setAlphaF(buttonOpacityHover());
