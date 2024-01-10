c895c0af4835b732ce52979880235a1b242642f7
Eism <eismailzada@gmail.com>
2024-01-10 17:08:09 +0200

    Added unit tests

---
+++ src/framework/network/tests/mocks/networkmanagercreatormock.h
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2024 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_NETWORK_NETWORKMANAGERCREATORMOCK_H
+#define MU_NETWORK_NETWORKMANAGERCREATORMOCK_H
+
+#include <gmock/gmock.h>
+
+#include "network/inetworkmanagercreator.h"
+
+namespace mu::network {
+class NetworkManagerCreatorMock : public INetworkManagerCreator
+{
+public:
+    MOCK_METHOD(INetworkManagerPtr, makeNetworkManager, (), (const, override));
+};
+}
+
+#endif // MU_NETWORK_NETWORKMANAGERCREATORMOCK_H
