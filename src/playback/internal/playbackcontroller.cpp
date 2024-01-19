fd3d467afadb91bede7b66aff410a76be63af8da
Eism <eismailzada@gmail.com>
2024-01-19 12:29:10 +0200

    Update current tick if we are not playing

--- src/playback/internal/playbackcontroller.cpp
+++ src/playback/internal/playbackcontroller.cpp
@@ -1152,10 +1152,6 @@ void PlaybackController::setupSequencePlayer()
             return;
         }
 
-        if (!isPlaying()) {
-            return;
-        }
-
         setCurrentPlaybackTime(msecs);
         m_tickPlayed.send(m_currentTick);
     });
