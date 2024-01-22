56ca0f585702c12e5454c8d90d88f6d9b9518461
Roman Pudashkin <r.pudashkin@gmail.com>
2024-01-22 15:57:30 +0200

    fix #21099: don't delete auto generated elements immediately
    Before PR #20367, we only deleted them when the user closed the score and the undo stack got cleared
    Now, they will be deleted along with the dummy element when closing the score

--- src/engraving/dom/score.cpp
+++ src/engraving/dom/score.cpp
@@ -1734,7 +1734,8 @@ void Score::doUndoRemoveElement(EngravingItem* element)
 {
     if (element->generated()) {
         removeElement(element);
-        element->deleteLater();
+        //! HACK: don't delete as it may still be used in Inspector
+        // element->deleteLater();
     } else {
         undo(new RemoveElement(element));
     }
