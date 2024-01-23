e413282b01a7c4ff8b5912992640502f949178b9
sammik <samuelmiklas@gmail.com>
2024-01-23 14:20:25 +0100

    Fix GH#14722, part 1: fix Instrument change from palette
    Backport of #14802

--- libmscore/chordrest.cpp
+++ libmscore/chordrest.cpp
@@ -563,10 +563,8 @@ Element* ChordRest::drop(EditData& data)
                         InstrumentChange* ic = toInstrumentChange(e);
                         ic->setParent(segment());
                         ic->setTrack((track() / VOICES) * VOICES);
-                        Instrument* instr = ic->instrument();
-                        Instrument* prevInstr = part()->instrument(tick());
-                        if (instr && instr->isDifferentInstrument(*prevInstr))
-                              ic->setupInstrument(instr);
+                        Instrument* instr = part()->instrument(tick());
+                        ic->setInstrument(instr);
                         score()->undoAddElement(ic);
                         return e;
                         }
