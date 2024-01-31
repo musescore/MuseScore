8ef7bda7cf47723f4a18671ff637c19924d0070a
Igor Korsukov <igor.korsukov@gmail.com>
2024-01-31 10:24:51 +0200

    [musicxml] replace QString to String. step 2

--- src/importexport/musicxml/internal/musicxml/importmxmlnoteduration.cpp
+++ src/importexport/musicxml/internal/musicxml/importmxmlnoteduration.cpp
@@ -39,35 +39,35 @@ namespace mu::engraving {
  Convert MusicXML note type to fraction.
  */
 
-static Fraction noteTypeToFraction(const QString& type)
+static Fraction noteTypeToFraction(const String& type)
 {
-    if (type == "1024th") {
+    if (type == u"1024th") {
         return Fraction(1, 1024);
-    } else if (type == "512th") {
+    } else if (type == u"512th") {
         return Fraction(1, 512);
-    } else if (type == "256th") {
+    } else if (type == u"256th") {
         return Fraction(1, 256);
-    } else if (type == "128th") {
+    } else if (type == u"128th") {
         return Fraction(1, 128);
-    } else if (type == "64th") {
+    } else if (type == u"64th") {
         return Fraction(1, 64);
-    } else if (type == "32nd") {
+    } else if (type == u"32nd") {
         return Fraction(1, 32);
-    } else if (type == "16th") {
+    } else if (type == u"16th") {
         return Fraction(1, 16);
-    } else if (type == "eighth") {
+    } else if (type == u"eighth") {
         return Fraction(1, 8);
-    } else if (type == "quarter") {
+    } else if (type == u"quarter") {
         return Fraction(1, 4);
-    } else if (type == "half") {
+    } else if (type == u"half") {
         return Fraction(1, 2);
-    } else if (type == "whole") {
+    } else if (type == u"whole") {
         return Fraction(1, 1);
-    } else if (type == "breve") {
+    } else if (type == u"breve") {
         return Fraction(2, 1);
-    } else if (type == "long") {
+    } else if (type == u"long") {
         return Fraction(4, 1);
-    } else if (type == "maxima") {
+    } else if (type == u"maxima") {
         return Fraction(8, 1);
     } else {
         return Fraction(0, 0);
@@ -82,7 +82,7 @@ static Fraction noteTypeToFraction(const QString& type)
  Convert note type, number of dots and actual and normal notes into a duration
  */
 
-static Fraction calculateFraction(const QString& type, const int dots, const Fraction timeMod)
+static Fraction calculateFraction(const String& type, const int dots, const Fraction timeMod)
 {
     // type
     Fraction f = noteTypeToFraction(type);
@@ -112,10 +112,10 @@ static Fraction calculateFraction(const QString& type, const int dots, const Fra
  Return empty string if OK, message in case of error.
  */
 
-QString MxmlNoteDuration::checkTiming(const QString& type, const bool rest, const bool grace)
+String MxmlNoteDuration::checkTiming(const String& type, const bool rest, const bool grace)
 {
     //LOGD("type %s rest %d grace %d", qPrintable(type), rest, grace);
-    QString errorStr;
+    String errorStr;
 
     // normalize duration
     if (m_specDura.isValid()) {
@@ -130,7 +130,7 @@ QString MxmlNoteDuration::checkTiming(const QString& type, const bool rest, cons
 
     if (m_specDura.isValid() && m_calcDura.isValid()) {
         if (m_specDura != m_calcDura) {
-            errorStr = QString("calculated duration (%1) not equal to specified duration (%2)")
+            errorStr = String("calculated duration (%1) not equal to specified duration (%2)")
                        .arg(m_calcDura.toString(), m_specDura.toString());
             //LOGD("rest %d type '%s' timemod %s", rest, qPrintable(type), qPrintable(_timeMod.print()));
 
@@ -144,12 +144,12 @@ QString MxmlNoteDuration::checkTiming(const QString& type, const bool rest, cons
                 m_dura = m_specDura;
             } else {
                 if (qAbs(m_calcDura.ticks() - m_specDura.ticks()) <= m_pass1->maxDiff()) {
-                    errorStr += " -> assuming rounding error";
+                    errorStr += u" -> assuming rounding error";
                     m_pass1->insertAdjustedDuration(m_dura, m_calcDura);
                     m_dura = m_calcDura;
                     m_specDura = m_calcDura; // prevent changing off time
                 } else {
-                    errorStr += " -> using calculated duration";
+                    errorStr += u" -> using calculated duration";
                 }
             }
 
@@ -161,8 +161,8 @@ QString MxmlNoteDuration::checkTiming(const QString& type, const bool rest, cons
             if (rest) {
                 if (2 * m_calcDura.ticks() == 3 * m_specDura.ticks()) {
                     m_timeMod = Fraction(2, 3);
-                    errorStr += errorStr.isEmpty() ? " ->" : ",";
-                    errorStr += " assuming triplet";
+                    errorStr += errorStr.isEmpty() ? u" ->" : u",";
+                    errorStr += u" assuming triplet";
                     m_dura = m_specDura;
                 }
             }
@@ -244,8 +244,8 @@ void MxmlNoteDuration::timeModification(QXmlStreamReader& e)
 
     int intActual = 0;
     int intNormal = 0;
-    QString strActual;
-    QString strNormal;
+    String strActual;
+    String strNormal;
 
     while (e.readNextStartElement()) {
         const QStringRef& tag(e.name());
@@ -262,7 +262,7 @@ void MxmlNoteDuration::timeModification(QXmlStreamReader& e)
                 m_normalType.setType(TConv::fromXml(ba.constData(), DurationType::V_INVALID));
             }
         } else {
-            m_logger->logDebugInfo(QString("skipping '%1'").arg(e.name().toString()), &e);
+            m_logger->logDebugInfo(String("skipping '%1'").arg(e.name().toString()), &e);
             e.skipCurrentElement();
         }
     }
@@ -273,7 +273,7 @@ void MxmlNoteDuration::timeModification(QXmlStreamReader& e)
         m_timeMod.set(intNormal, intActual);
     } else {
         m_timeMod.set(1, 1);
-        m_logger->logError(QString("illegal time-modification: actual-notes %1 normal-notes %2")
+        m_logger->logError(String("illegal time-modification: actual-notes %1 normal-notes %2")
                            .arg(strActual, strNormal), &e);
     }
 }
