3a11823fac8f229b63411c4a59b4579b9a6f1450
Roman Pudashkin <r.pudashkin@gmail.com>
2024-01-31 15:31:00 +0200

    simplified ITracks::availableSoundPresets
    so that it does not depend on sequenceId and trackId

--- src/playback/internal/playbackcontroller.cpp
+++ src/playback/internal/playbackcontroller.cpp
@@ -271,7 +271,7 @@ Channel<aux_channel_idx_t, std::string> PlaybackController::auxChannelNameChange
     return m_auxChannelNameChanged;
 }
 
-Promise<SoundPresetList> PlaybackController::availableSoundPresets(InstrumentTrackId instrumentTrackId) const
+Promise<SoundPresetList> PlaybackController::availableSoundPresets(const InstrumentTrackId& instrumentTrackId) const
 {
     auto it = m_instrumentTrackIdMap.find(instrumentTrackId);
     if (it == m_instrumentTrackIdMap.end()) {
@@ -280,7 +280,8 @@ Promise<SoundPresetList> PlaybackController::availableSoundPresets(InstrumentTra
         });
     }
 
-    return playback()->tracks()->availableSoundPresets(m_currentSequenceId, it->second);
+    const AudioInputParams& params = audioSettings()->trackInputParams(instrumentTrackId);
+    return playback()->tracks()->availableSoundPresets(params.resourceMeta);
 }
 
 mu::notation::INotationSoloMuteState::SoloMuteState PlaybackController::trackSoloMuteState(const InstrumentTrackId& trackId) const
