e93a20cc51e65026bf0ef1aeae8bed02e39406a0
James Mizen <jamesmmizen@gmail.com>
2024-02-07 08:44:56 +0000

    Align harmony to the top & centre when attached to fret diagram

--- src/importexport/musicxml/internal/musicxml/importmxmlpass2.cpp
+++ src/importexport/musicxml/internal/musicxml/importmxmlpass2.cpp
@@ -5928,6 +5928,7 @@ void MusicXMLParserPass2::harmony(const String& partId, Measure* measure, const 
     if (fd) {
         fd->setTrack(track);
         Segment* s = measure->getSegment(SegmentType::ChordRest, sTime + offset);
+        ha->setProperty(Pid::ALIGN, Align(AlignH::HCENTER, AlignV::TOP));
         s->add(fd);
     }
 
