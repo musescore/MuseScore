8601c924245ad3d66587d5c69bd05b94adb87b8f
mikekirin <mike.kirin@hotmail.com>
2024-02-22 18:11:54 +0100

    added ability to control font size multiplication by 2 for some cases

--- src/engraving/tests/mocks/engravingconfigurationmock.h
+++ src/engraving/tests/mocks/engravingconfigurationmock.h
@@ -81,6 +81,7 @@ public:
     MOCK_METHOD(bool, guitarProMultivoiceEnabled, (), (const, override));
     MOCK_METHOD(bool, minDistanceForPartialSkylineCalculated, (), (const, override));
     MOCK_METHOD(bool, specificSlursLayoutWorkaround, (), (const, override));
+    MOCK_METHOD(bool, adaptFontSizes, (), (const, override));
 };
 }
 
