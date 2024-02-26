8c028390681fb9f29f1536cbf5cf4c661bf298ee
mikekirin <m.kirin@mu.se>
2024-02-26 13:58:08 +0100

    Fixed float layout mode for one system on page
    Backport of #21689

--- libmscore/layout.cpp
+++ libmscore/layout.cpp
@@ -1984,7 +1984,9 @@ void LayoutContext::layoutPage(Page* page, qreal restHeight, qreal footerPadding
 
       if (sList.empty() || MScore::noVerticalStretch || score->enableVerticalSpread() || score->layoutMode() == LayoutMode::SYSTEM) {
             if (score->layoutMode() == LayoutMode::FLOAT) {
-                  qreal y = restHeight * .5;
+                  qreal y = 0.0;
+                  if (gaps > 0)
+                      y = restHeight * .5;
                   for (System* system : page->systems())
                         system->move(QPointF(0.0, y));
                   }
