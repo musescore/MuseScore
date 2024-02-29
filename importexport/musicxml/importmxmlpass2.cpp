17df7991f6a57c45dd292e0be9817a476cd7e8cd
James Mizen <jamesmmizen@gmail.com>
2024-02-29 18:32:20 +0100

    Set hairpin's default placement to 'Above' on vocal stave
    Backport of #21761

--- importexport/musicxml/importmxmlpass2.cpp
+++ importexport/musicxml/importmxmlpass2.cpp
@@ -3417,17 +3417,22 @@ void MusicXMLParserDirection::direction(const QString& partId,
                   delete desc._sp;
                   }
             else {
+                  QString spannerPlacement = placement();
+                  // Case-based defaults
+                  if (spannerPlacement.isEmpty() && desc._sp->isHairpin()) {
+                      spannerPlacement = isVocalStaff ? "above" : "below";
+                  }
                   if (spdesc._isStopped) {
                         _pass2.addSpanner(desc);
                         // handleSpannerStart and handleSpannerStop must be called in order
                         // due to allocation of elements in the map
-                        handleSpannerStart(desc._sp, track, placement(), tick + _offset, spanners);
+                        handleSpannerStart(desc._sp, track, spannerPlacement, tick + _offset, spanners);
                         handleSpannerStop(spdesc._sp, spdesc._track2, spdesc._tick2, spanners);
                         _pass2.clearSpanner(desc);
                         }
                   else {
                         _pass2.addSpanner(desc);
-                        handleSpannerStart(desc._sp, track, placement(), tick + _offset, spanners);
+                        handleSpannerStart(desc._sp, track, spannerPlacement, tick + _offset, spanners);
                         spdesc._isStarted = true;
                         }
                   }
