db8c4a9995faa116770d573e54f93a52f1e42caa
Igor Korsukov <igor.korsukov@gmail.com>
2024-03-11 13:21:10 +0200

    added extensions module blank

---
+++ src/framework/extensions/extensionsmodule.cpp
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2024 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "extensionsmodule.h"
+
+#include <QtQml>
+
+#include "modularity/ioc.h"
+
+#include "internal/extensionsprovider.h"
+
+#include "log.h"
+
+using namespace mu::extensions;
+using namespace mu::modularity;
+
+std::string ExtensionsModule::moduleName() const
+{
+    return "extensions";
+}
+
+void ExtensionsModule::registerExports()
+{
+    m_extensionsProvider = std::make_shared<ExtensionsProvider>();
+
+    ioc()->registerExport<IExtensionsProvider>(moduleName(), m_extensionsProvider);
+}
+
+void ExtensionsModule::registerUiTypes()
+{
+}
+
+void ExtensionsModule::onInit(const IApplication::RunMode& mode)
+{
+    if (mode != IApplication::RunMode::GuiApp) {
+        return;
+    }
+}
---
+++ src/framework/extensions/extensionsmodule.h
@@ -0,0 +1,47 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2024 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_EXTENSIONS_EXTENSIONSMODULE_H
+#define MU_EXTENSIONS_EXTENSIONSMODULE_H
+
+#include <memory>
+#include <string>
+
+#include "modularity/imodulesetup.h"
+
+namespace mu::extensions {
+class ExtensionsProvider;
+class ExtensionsModule : public modularity::IModuleSetup
+{
+public:
+
+    std::string moduleName() const override;
+    void registerExports() override;
+    void registerUiTypes() override;
+    void onInit(const IApplication::RunMode& mode) override;
+
+private:
+
+    std::shared_ptr<ExtensionsProvider> m_extensionsProvider;
+};
+}
+
+#endif // MU_EXTENSIONS_EXTENSIONSMODULE_H
