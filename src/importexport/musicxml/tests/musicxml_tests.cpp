729fabd2e63a7cb00b3099e95a165f5f72fa79bb
David Lakes <dwlakes@gmail.com>
2024-03-11 20:08:51 -0400

    trying to fix PR issue

--- src/importexport/musicxml/tests/musicxml_tests.cpp
+++ src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -827,7 +827,7 @@ TEST_F(Musicxml_Tests, noteAttributes3) {
     mxmlIoTest("testNoteAttributes3");
 }
 TEST_F(Musicxml_Tests, noteAttributes4) {
-    mxmlIoTestRef("testNoteAttributes4");
+    mxmlIoTest("testNoteAttributes4");
 }
 TEST_F(Musicxml_Tests, noteColor) {
     mxmlIoTest("testNoteColor");
