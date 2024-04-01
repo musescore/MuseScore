ef1bad827a80d6ea837aa78634a6c48977fb002c
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2024-04-01 03:51:06 +0200

    Refactor: eliminate unnecessary signal

--- src/appshell/view/dockwindow/internal/dockbase.cpp
+++ src/appshell/view/dockwindow/internal/dockbase.cpp
@@ -586,10 +586,6 @@ void DockBase::componentComplete()
     connect(this, &DockBase::minimumSizeChanged, this, &DockBase::applySizeConstraints);
     connect(this, &DockBase::maximumSizeChanged, this, &DockBase::applySizeConstraints);
 
-    connect(this, &DockBase::visibleChanged, [this](){
-        emit reorderNavigationRequested();
-    });
-
     m_defaultVisibility = isVisible();
 }
 
