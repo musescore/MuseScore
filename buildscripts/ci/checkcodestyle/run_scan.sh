4affac588883772a53929eb0f9d1790d30468b15
Igor Korsukov <igor.korsukov@gmail.com>
2024-04-12 18:42:06 +0300

    reimplement check codestyle CI scritp

---
+++ buildscripts/ci/checkcodestyle/run_scan.sh
@@ -0,0 +1,14 @@
+SCAN_BIN=$1
+UNCRUSTIFY_BIN=$2
+CONFIG=$3
+UNTIDY_FILE=$4
+DIR=$5
+
+echo "SCAN_BIN: $SCAN_BIN"
+echo "UNCRUSTIFY_BIN: $UNCRUSTIFY_BIN"
+echo "CONFIG: $CONFIG"
+echo "UNTIDY_FILE: $UNTIDY_FILE"
+echo "DIR: $DIR"
+
+$SCAN_BIN -d $DIR -i $UNTIDY_FILE -e cpp,c,cc,hpp,h | xargs -n 1 -P 16 $UNCRUSTIFY_BIN -c $CONFIG --no-backup -l CPP
+$SCAN_BIN -d $DIR -i $UNTIDY_FILE -e mm | xargs -n 1 -P 16 $UNCRUSTIFY_BIN -c $CONFIG --no-backup -l OC+
