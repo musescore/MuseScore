d94524e91e767c9924b0edf2d17e80c126765ea2
Igor Korsukov <igor.korsukov@gmail.com>
2024-04-15 11:43:10 +0300

    fixed build on CI

--- buildscripts/ci/tools/sparkle_appcast_gen.sh
+++ buildscripts/ci/tools/sparkle_appcast_gen.sh
@@ -61,7 +61,7 @@ echo "UPDATE_URL: $UPDATE_URL"
 RSS_DATE="$(LANG=C date +'%a, %d %b %Y %H:%M:%S %z')"
 FILESIZE="$(wc -c ${ARTIFACT_PATH} | awk '{print $1}')"
 APPCAST_URL="https://sparkle.musescore.org/$RELEASE_CHANNEL/$MAJOR_VERSION/$PLATFORM/appcast.xml"
-GIT_LOG=$(build/ci/tools/generateGitLog.sh)
+GIT_LOG=$(./buildscripts/ci/tools/generateGitLog.sh)
 
 #use dummy values for now
 echo "<rss xmlns:sparkle=\"http://www.andymatuschak.org/xml-namespaces/sparkle\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" version=\"2.0\">
