70ebc29618c5a22545ce24ddba875542cf168f41
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-04-23 10:57:06 +0200

    add import/export for percussion clef

--- src/importexport/mei/internal/meiconverter.cpp
+++ src/importexport/mei/internal/meiconverter.cpp
@@ -845,6 +845,8 @@ engraving::ClefType Convert::clefFromMEI(const libmei::Clef& meiClef, bool& warn
         default:
             break;
         }
+    } else if (meiClef.GetShape() == libmei::CLEFSHAPE_perc) {
+        return engraving::ClefType::PERC;
     } else if (meiClef.GetShape() == libmei::CLEFSHAPE_GG && meiClef.GetLine() == 2) {
         return engraving::ClefType::G8_VB_O;
     }
@@ -884,6 +886,10 @@ libmei::Clef Convert::clefToMEI(engraving::ClefType clef)
         break;
     case (engraving::ClefType::G8_VB_O):
         meiClef.SetShape(libmei::CLEFSHAPE_GG);
+        break;
+    case (engraving::ClefType::PERC):
+    case (engraving::ClefType::PERC2):
+        meiClef.SetShape(libmei::CLEFSHAPE_perc);
         break;
     default:
         AsciiStringView glyphName = engraving::SymNames::nameForSymId(engraving::ClefInfo::symId(clef));
