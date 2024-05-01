b78f185e5a1172de60de9406148cdbbbefec93bf
Calum Matheson <mathesoncalum@gmail.com>
2024-05-01 11:54:10 +0100

    Prevent iterator invalidation when deleting chordrests

--- src/engraving/dom/edit.cpp
+++ src/engraving/dom/edit.cpp
@@ -3910,7 +3910,8 @@ void Score::removeChordRest(ChordRest* cr, bool clearSegment)
     std::set<Segment*> segments;
     for (EngravingObject* e : cr->linkList()) {
         if (cr->isChord()) {
-            for (Spanner* spanner : toChord(e)->startingSpanners()) {
+            std::set<Spanner*> startingSpanners = toChord(e)->startingSpanners();
+            for (Spanner* spanner : startingSpanners) {
                 if (spanner->isTrill()) {
                     doUndoRemoveElement(spanner);
                 }
