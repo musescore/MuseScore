c46667893375e7474a32e0709f01ab4cdf0122e2
re1san <kathareaniket@gmail.com>
2024-06-06 17:16:16 +0530

    Add UI of the popup

--- src/notation/view/internal/dynamicpopupmodel.h
+++ src/notation/view/internal/dynamicpopupmodel.h
@@ -1,15 +1,39 @@
 #ifndef MU_NOTATION_DYNAMICPOPUPMODEL_H
 #define MU_NOTATION_DYNAMICPOPUPMODEL_H
 
+#include <engraving/iengravingfontsprovider.h>
+#include <engraving/style/style.h>
 #include <view/abstractelementpopupmodel.h>
+
+#include <QList>
+#include <QObject>
+#include <QVariantList>
+
+using namespace mu::engraving;
 
 namespace mu::notation {
 class DynamicPopupModel : public AbstractElementPopupModel
 {
+    Q_OBJECT
+
+    INJECT(engraving::IEngravingFontsProvider, engravingFonts)
+
+    Q_PROPERTY(QVariantList pages READ pages NOTIFY pagesChanged)
+
 public:
     explicit DynamicPopupModel(QObject *parent = nullptr);
 
     Q_INVOKABLE void init() override;
+
+    QVariantList pages() const;
+
+signals:
+    void pagesChanged();
+
+private:
+    QList<QStringList> m_pages;
+
+    static const QList<QStringList> DYN_NAMES;
 };
 } // namespace mu::notation
 
