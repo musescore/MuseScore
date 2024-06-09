17cb8eae06ea52323bb469046c34bcf28b656abd
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2024-06-09 20:54:15 +0200

    Small cleanup of `MUE_COMPILE_USE_SYSTEM_*` CMake options
    These options were added in https://github.com/musescore/MuseScore/pull/19795 and https://github.com/musescore/MuseScore/pull/22243

--- src/framework/audio/cmake/SetupOpusEnc.cmake
+++ src/framework/audio/cmake/SetupOpusEnc.cmake
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0-only
-# MuseScore-CLA-applies
+# MuseScore-Studio-CLA-applies
 #
-# MuseScore
+# MuseScore Studio
 # Music Composition & Notation
 #
-# Copyright (C) 2023 MuseScore BVBA and others
+# Copyright (C) 2024 MuseScore Limited
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -18,17 +18,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-if (MUE_COMPILE_USE_SYSTEM_FREETYPE)
-    find_package(Freetype)
+if (MUE_COMPILE_USE_SYSTEM_OPUSENC)
+    find_package(PkgConfig REQUIRED)
 
-    if (FREETYPE_FOUND)
-        message(STATUS "Found freetype: ${FREETYPE_VERSION_STRING}")
-    else()
-        message(WARNING "Set MUE_COMPILE_USE_SYSTEM_FREETYPE=ON, but system freetype not found, built-in will be used")
-    endif()
-endif()
+    pkg_check_modules(libopusenc REQUIRED IMPORTED_TARGET libopusenc)
 
-if (NOT FREETYPE_FOUND)
-    # sets FREETYPE_LIBRARIES and FREETYPE_INCLUDE_DIRS
-    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../thirdparty/freetype freetype)
+    # Transitive dependency
+    pkg_check_modules(opus REQUIRED IMPORTED_TARGET opus)
+
+    set(LIBOPUSENC_TARGETS PkgConfig::libopusenc PkgConfig::opus)
+else()
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../thirdparty/opusenc opusenc)
+    set(LIBOPUSENC_TARGETS opusenc)
 endif()
