2790b402b686e1def4744ab194d4a747e902cc71
re1san <kathareaniket@gmail.com>
2024-06-17 16:44:43 +0530

    Add functionality to change dynamic

--- src/notation/view/internal/dynamicpopupmodel.cpp
+++ src/notation/view/internal/dynamicpopupmodel.cpp
@@ -6,46 +6,46 @@
 using namespace mu::notation;
 using namespace mu::engraving;
 
-static const QList<QList<DynamicPopupModel::PageItem>> DYN_POPUP_PAGES = {
+static const QList<QList<DynamicPopupModel::PageItem> > DYN_POPUP_PAGES = {
     {   // Page 1
-        { "dynamicPP",                30, 1.5, DynamicPopupModel::Dynamic },
-        { "dynamicPiano",             21, 1.0, DynamicPopupModel::Dynamic },
-        { "dynamicMP",                29, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicMF",                30, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicForte",             23, 2.5, DynamicPopupModel::Dynamic },
-        { "dynamicFF",                30, 2.5, DynamicPopupModel::Dynamic },
+        { "dynamicPP",                30, 1.5, DynamicPopupModel::Dynamic,     DynamicType::PP },
+        { "dynamicPiano",             21, 1.0, DynamicPopupModel::Dynamic,     DynamicType::P },
+        { "dynamicMP",                29, 0.5, DynamicPopupModel::Dynamic,     DynamicType::MP },
+        { "dynamicMF",                30, 0.5, DynamicPopupModel::Dynamic,     DynamicType::MF },
+        { "dynamicForte",             23, 2.5, DynamicPopupModel::Dynamic,     DynamicType::F },
+        { "dynamicFF",                30, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FF },
     },
     {   // Page 2
-        { "dynamicFFF",               38, 2.5, DynamicPopupModel::Dynamic },
-        { "dynamicFFFF",              45, 2.5, DynamicPopupModel::Dynamic },
-        { "dynamicFFFFF",             53, 2.5, DynamicPopupModel::Dynamic },
+        { "dynamicFFF",               38, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FFF },
+        { "dynamicFFFF",              45, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FFFF },
+        { "dynamicFFFFF",             53, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FFFFF },
     },
     {   // Page 3
-        { "dynamicFortePiano",        30, 2.5, DynamicPopupModel::Dynamic },
-        { "dynamicPF",                32, 2.0, DynamicPopupModel::Dynamic },
-        { "dynamicSforzando1",        25, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicSforzato",          29, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicSforzatoFF",        37, 0.5, DynamicPopupModel::Dynamic },
+        { "dynamicFortePiano",        30, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FP },
+        { "dynamicPF",                32, 2.0, DynamicPopupModel::Dynamic,     DynamicType::PF },
+        { "dynamicSforzando1",        25, 0.5, DynamicPopupModel::Dynamic,     DynamicType::SF },
+        { "dynamicSforzato",          29, 0.5, DynamicPopupModel::Dynamic,     DynamicType::SFZ },
+        { "dynamicSforzatoFF",        37, 0.5, DynamicPopupModel::Dynamic,     DynamicType::SFFZ },
     },
     {   // Page 4
-        { "dynamicSforzatoPiano",     38, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicSforzandoPiano",    33, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicRinforzando2",      30, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicRinforzando1",      26, 0.5, DynamicPopupModel::Dynamic },
-        { "dynamicForzando",          26, 2.5, DynamicPopupModel::Dynamic },
+        { "dynamicSforzatoPiano",     38, 0.5, DynamicPopupModel::Dynamic,     DynamicType::OTHER },
+        { "dynamicSforzandoPiano",    33, 0.5, DynamicPopupModel::Dynamic,     DynamicType::SFP },
+        { "dynamicRinforzando2",      30, 0.5, DynamicPopupModel::Dynamic,     DynamicType::RFZ },
+        { "dynamicRinforzando1",      26, 0.5, DynamicPopupModel::Dynamic,     DynamicType::RF },
+        { "dynamicForzando",          26, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FZ },
     },
     {   // Page 5
-         { "dynamicPPPPPP",           74, 2.0, DynamicPopupModel::Dynamic },
-         { "dynamicFFFFFF",           60, 2.5, DynamicPopupModel::Dynamic },
+        { "dynamicPPPPPP",            74, 2.0, DynamicPopupModel::Dynamic,     DynamicType::PPPPPP },
+        { "dynamicFFFFFF",            60, 2.5, DynamicPopupModel::Dynamic,     DynamicType::FFFFFF },
     },
     {   // Page 6
-        { "dynamicPPPPP",             64, 2.0, DynamicPopupModel::Dynamic },
-        { "dynamicPPPP",              52, 2.0, DynamicPopupModel::Dynamic },
-        { "dynamicPPP",               44, 2.0, DynamicPopupModel::Dynamic },
+        { "dynamicPPPPP",             64, 2.0, DynamicPopupModel::Dynamic,     DynamicType::PPPPP },
+        { "dynamicPPPP",              52, 2.0, DynamicPopupModel::Dynamic,     DynamicType::PPPP },
+        { "dynamicPPP",               44, 2.0, DynamicPopupModel::Dynamic,     DynamicType::PPP },
     },
     {   // Page 7 - Hairpins
-        { "dynamicCrescendoHairpin",  62, 0.0, DynamicPopupModel::Crescendo },
-        { "dynamicDiminuendoHairpin", 62, 0.0, DynamicPopupModel::Decrescendo },
+        { "dynamicCrescendoHairpin",  62, 0.0, DynamicPopupModel::Crescendo,   DynamicType::OTHER },
+        { "dynamicDiminuendoHairpin", 62, 0.0, DynamicPopupModel::Decrescendo, DynamicType::OTHER },
     },
 };
 
@@ -73,13 +73,13 @@ void DynamicPopupModel::init()
 
     for (const QList<DynamicPopupModel::PageItem>& page : DYN_POPUP_PAGES) {
         QVariantList variantPage;
-        for(const DynamicPopupModel::PageItem& item : page) {
+        for (const DynamicPopupModel::PageItem& item : page) {
             SymId id = SymNames::symIdByName(item.name);
             QVariantMap variantMap {
                 { "symbol", engravingFont->toString(id).toQString() },
                 { "width", item.width },
                 { "offset", item.offset },
-                { "type", item.type }
+                { "type", item.itemType },
             };
             variantPage.append(variantMap);
         }
@@ -87,4 +87,21 @@ void DynamicPopupModel::init()
     }
 
     emit pagesChanged();
+}
+
+void DynamicPopupModel::changeDynamic(int page, int index)
+{
+    IF_ASSERT_FAILED(m_item) {
+        return;
+    }
+
+    if (!m_item->isDynamic()) {
+        return;
+    }
+
+    beginCommand();
+    m_item->undoChangeProperty(Pid::TEXT, Dynamic::dynamicText(DYN_POPUP_PAGES[page][index].dynType));
+    m_item->undoChangeProperty(Pid::DYNAMIC_TYPE, DYN_POPUP_PAGES[page][index].dynType);
+    endCommand();
+    updateNotation();
 }
