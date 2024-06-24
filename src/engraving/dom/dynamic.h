524a0c41392759a2d372826c9b4205346b2e597f
Michele Spagnolo <m.spagnolo@mu.se>
2024-06-24 10:49:30 +0200

    Extract useful methods for Dynamic and Hairpin moves

--- src/engraving/dom/dynamic.h
+++ src/engraving/dom/dynamic.h
@@ -114,6 +114,8 @@ public:
     Expression* snappedExpression() const;
     HairpinSegment* findSnapBeforeHairpinAcrossSystemBreak() const;
 
+    void undoMoveSegment(Segment* newSeg, Fraction tickDiff);
+
     bool playDynamic() const { return m_playDynamic; }
     void setPlayDynamic(bool v) { m_playDynamic = v; }
 
