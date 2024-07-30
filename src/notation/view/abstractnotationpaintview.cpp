musescore/MuseScore

commit 8b958961c7977863c345bce7870753937aa0a334

added playback status getter to player

src/notation/view/abstractnotationpaintview.cpp
@@ -1326,10 +1326,9 @@ void AbstractNotationPaintView::onPlayingChanged()
         IF_ASSERT_FAILED(globalContext()->currentPlayer()) {
             return;
         }
-        globalContext()->currentPlayer()->playbackPosition().onResolve(this, [this](audio::secs_t pos) {
-            muse::midi::tick_t tick = notationPlayback()->secToTick(pos);
-            movePlaybackCursor(tick);
-        });
+        audio::secs_t pos = globalContext()->currentPlayer()->playbackPosition();
+        muse::midi::tick_t tick = notationPlayback()->secToTick(pos);
+        movePlaybackCursor(tick);
     } else {
         scheduleRedraw();
     }

src/playback/internal/playbackcontroller.cpp
@@ -161,12 +161,18 @@ Notification PlaybackController::isPlayAllowedChanged() const
 
 bool PlaybackController::isPlaying() const
 {
-    return m_currentPlaybackStatus == PlaybackStatus::Running;
+    if (!currentPlayer()) {
+        return false;
+    }
+    return currentPlayer()->playbackStatus() == PlaybackStatus::Running;
 }
 
 bool PlaybackController::isPaused() const
 {
-    return m_currentPlaybackStatus == PlaybackStatus::Paused;
+    if (!currentPlayer()) {
+        return false;
+    }
+    return currentPlayer()->playbackStatus() == PlaybackStatus::Paused;
 }
 
 bool PlaybackController::isLoaded() const
@@ -543,15 +549,14 @@ void PlaybackController::togglePlay()
         pause();
     } else if (isPaused()) {
         if (currentPlayer()) {
-            currentPlayer()->playbackPosition().onResolve(this, [this](secs_t pos) {
-                secs_t endSecs = milisecsToSecs(playbackEndMsecs());
-                if (pos == endSecs) {
-                    secs_t startSecs = milisecsToSecs(playbackStartMsecs());
-                    seek(startSecs);
-                }
+            secs_t pos = currentPlayer()->playbackPosition();
+            secs_t endSecs = milisecsToSecs(playbackEndMsecs());
+            if (pos == endSecs) {
+                secs_t startSecs = milisecsToSecs(playbackStartMsecs());
+                seek(startSecs);
+            }
 
-                resume();
-            });
+            resume();
         }
     } else {
         play();
@@ -570,7 +575,6 @@ void PlaybackController::play()
     }
 
     currentPlayer()->play();
-    setCurrentPlaybackStatus(PlaybackStatus::Running);
 }
 
 void PlaybackController::rewind(const ActionData& args)
@@ -590,7 +594,6 @@ void PlaybackController::pause()
     }
 
     currentPlayer()->pause();
-    setCurrentPlaybackStatus(PlaybackStatus::Paused);
 }
 
 void PlaybackController::stop()
@@ -600,7 +603,6 @@ void PlaybackController::stop()
     }
 
     currentPlayer()->stop();
-    setCurrentPlaybackStatus(PlaybackStatus::Stopped);
 }
 
 void PlaybackController::resume()
@@ -610,7 +612,6 @@ void PlaybackController::resume()
     }
 
     currentPlayer()->resume();
-    setCurrentPlaybackStatus(PlaybackStatus::Running);
 }
 
 msecs_t PlaybackController::playbackStartMsecs() const
@@ -662,16 +663,6 @@ InstrumentTrackIdSet PlaybackController::instrumentTrackIdSetForRangePlayback() 
     }
 
     return result;
-}
-
-void PlaybackController::setCurrentPlaybackStatus(PlaybackStatus status)
-{
-    if (m_currentPlaybackStatus == status) {
-        return;
-    }
-
-    m_currentPlaybackStatus = status;
-    m_isPlayingChanged.notify();
 }
 
 void PlaybackController::togglePlayRepeats()
@@ -856,7 +847,6 @@ void PlaybackController::resetCurrentSequence()
     playback()->audioOutput()->clearMasterOutputParams();
 
     m_currentTick = 0;
-    setCurrentPlaybackStatus(PlaybackStatus::Stopped);
 
     playback()->removeSequence(m_currentSequenceId);
 
@@ -1273,15 +1263,15 @@ void PlaybackController::setupSequencePlayer()
         }
     });
 
+    currentPlayer()->playbackStatusChanged().onReceive(this, [this](PlaybackStatus) {
+        m_isPlayingChanged.notify();
+    });
+
     currentPlayer()->setDuration(notationPlayback()->totalPlayTime());
 
     notationPlayback()->totalPlayTimeChanged().onReceive(this, [this](const audio::msecs_t totalPlaybackTime) {
         currentPlayer()->setDuration(totalPlaybackTime);
         m_totalPlayTimeChanged.notify();
-    });
-
-    currentPlayer()->playbackStatusChanged().onReceive(this, [this](const PlaybackStatus status) {
-        setCurrentPlaybackStatus(status);
     });
 }
 

src/playback/internal/playbackcontroller.h
@@ -163,8 +163,6 @@ private:
 
     notation::InstrumentTrackIdSet instrumentTrackIdSetForRangePlayback() const;
 
-    void setCurrentPlaybackStatus(muse::audio::PlaybackStatus status);
-
     void togglePlayRepeats();
     void togglePlayChordSymbols();
     void toggleAutomaticallyPan();
@@ -225,7 +223,6 @@ private:
     muse::audio::TrackSequenceId m_currentSequenceId = -1;
 
     muse::async::Notification m_currentSequenceIdChanged;
-    muse::audio::PlaybackStatus m_currentPlaybackStatus = muse::audio::PlaybackStatus::Stopped;
     muse::midi::tick_t m_currentTick = 0;
     notation::Tempo m_currentTempo;
 

src/playback/view/playbacktoolbarmodel.cpp
@@ -86,12 +86,12 @@ void PlaybackToolBarModel::setupConnections()
         updatePlayPosition(secs);
     });
 
-    playbackController()->totalPlayTimeChanged().onNotify(this, [this, player]() {
+    playbackController()->totalPlayTimeChanged().onNotify(this, [this]() {
         emit maxPlayTimeChanged();
-
-        globalContext()->currentPlayer()->playbackPosition().onResolve(this, [this](secs_t pos) {
+        if (globalContext()->currentPlayer()) {
+            secs_t pos = globalContext()->currentPlayer()->playbackPosition();
             updatePlayPosition(pos);
-        });
+        }
     });
 
     playbackController()->currentTempoChanged().onNotify(this, [this]() {

src/framework/audio/internal/worker/clock.cpp
@@ -152,6 +152,11 @@ async::Notification Clock::seekOccurred() const
     return m_seekOccurred;
 }
 
+PlaybackStatus Clock::status() const
+{
+    return m_status.val;
+}
+
 async::Channel<PlaybackStatus> Clock::statusChanged() const
 {
     return m_status.ch;

src/framework/audio/internal/worker/clock.h
@@ -43,6 +43,8 @@ public:
     void seek(const msecs_t msecs) override;
     async::Notification seekOccurred() const override;
     bool isRunning() const override;
+
+    PlaybackStatus status() const override;
     async::Channel<PlaybackStatus> statusChanged() const override;
 
     msecs_t timeDuration() const override;

src/framework/audio/internal/worker/iclock.h
@@ -49,6 +49,8 @@ public:
     virtual void seek(const msecs_t msecs) = 0;
     virtual async::Notification seekOccurred() const = 0;
     virtual bool isRunning() const = 0;
+
+    virtual PlaybackStatus status() const = 0;
     virtual async::Channel<PlaybackStatus> statusChanged() const = 0;
 
     virtual msecs_t timeDuration() const = 0;

src/framework/audio/internal/worker/isequenceplayer.h
@@ -39,6 +39,8 @@ public:
     virtual void stop() = 0;
     virtual void pause() = 0;
     virtual void resume() = 0;
+
+    virtual PlaybackStatus playbackStatus() const = 0;
     virtual async::Channel<PlaybackStatus> playbackStatusChanged() const = 0;
 
     virtual msecs_t duration() const = 0;

commit 2f7f1386831f1ea7733144992105d5bd1a77dd3a

Fix updating the measure/beat field in PlaybackToolBar
PlaybackToolbarModel emitted a "playback position changed" signal in response to notifications on the `playbackState->playbackPositionChanged()` channel. `PlaybackController` also listens to this channel, and uses it to update its `m_currentTick` value. The value is used to determine the current measure/beat position in PlaybackToolbar.

The problem: the order in which channel listeners are invoked is undefined. And in this case we are unlucky: PlaybackToolbarModel's listener is invoked first, so it updates the QML view; and only after that, PlaybackController's listener is invoked, so its `m_currentTick` is updated too late. So, the QML view is updated based on the old `m_currentTick`.

Solution: don't listen to `playbackState->playbackPositionChanged()` directly, but listen to a channel from PlaybackController. This channel sends the notification after updating `m_currentTick`.

This commit also updates the name and signature of this channel, to better reflect its purpose.

This problem was a regression, maybe caused by 8b958961c7977863c345bce7870753937aa0a334, but that is an unverified conjecture.

src/notation/view/abstractnotationpaintview.cpp
@@ -622,7 +622,7 @@ void AbstractNotationPaintView::onNotationSetup()
         onPlayingChanged();
     });
 
-    playbackController()->midiTickPlayed().onReceive(this, [this](uint32_t tick) {
+    playbackController()->currentPlaybackPositionChanged().onReceive(this, [this](audio::secs_t, midi::tick_t tick) {
         movePlaybackCursor(tick);
     });
 

src/playback/internal/playbackcontroller.cpp
@@ -227,9 +227,9 @@ void PlaybackController::seek(const audio::secs_t secs)
     currentPlayer()->seek(secs);
 }
 
-Channel<uint32_t> PlaybackController::midiTickPlayed() const
+muse::async::Channel<secs_t, tick_t> PlaybackController::currentPlaybackPositionChanged() const
 {
-    return m_tickPlayed;
+    return m_currentPlaybackPositionChanged;
 }
 
 TrackSequenceId PlaybackController::currentTrackSequenceId() const
@@ -1264,7 +1264,7 @@ void PlaybackController::setupSequencePlayer()
 {
     currentPlayer()->playbackPositionChanged().onReceive(this, [this](const audio::secs_t pos) {
         m_currentTick = notationPlayback()->secToTick(pos);
-        m_tickPlayed.send(m_currentTick);
+        m_currentPlaybackPositionChanged.send(pos, m_currentTick);
 
         updateCurrentTempo();
 

src/playback/internal/playbackcontroller.h
@@ -63,7 +63,7 @@ public:
 
     void reset() override;
 
-    muse::async::Channel<uint32_t> midiTickPlayed() const override;
+    muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t> currentPlaybackPositionChanged() const override;
 
     muse::audio::TrackSequenceId currentTrackSequenceId() const override;
     muse::async::Notification currentTrackSequenceIdChanged() const override;
@@ -212,7 +212,7 @@ private:
     muse::async::Notification m_isPlayingChanged;
     muse::async::Notification m_totalPlayTimeChanged;
     muse::async::Notification m_currentTempoChanged;
-    muse::async::Channel<uint32_t> m_tickPlayed;
+    muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t> m_currentPlaybackPositionChanged;
     muse::async::Channel<muse::actions::ActionCode> m_actionCheckedChanged;
 
     muse::audio::TrackSequenceId m_currentSequenceId = -1;

src/playback/iplaybackcontroller.h
@@ -50,7 +50,7 @@ public:
 
     virtual void reset() = 0;
 
-    virtual muse::async::Channel<uint32_t> midiTickPlayed() const = 0;
+    virtual muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t> currentPlaybackPositionChanged() const = 0;
 
     virtual muse::audio::TrackSequenceId currentTrackSequenceId() const = 0;
     virtual muse::async::Notification currentTrackSequenceIdChanged() const = 0;

src/playback/view/playbacktoolbarmodel.cpp
@@ -67,8 +67,6 @@ void PlaybackToolBarModel::load()
 
 void PlaybackToolBarModel::setupConnections()
 {
-    context::IPlaybackStatePtr playbackState = globalContext()->playbackState();
-
     playbackController()->isPlayAllowedChanged().onNotify(this, [this]() {
         emit isPlayAllowedChanged();
     });
@@ -77,7 +75,7 @@ void PlaybackToolBarModel::setupConnections()
         onActionsStateChanges({ PLAY_ACTION_CODE });
     });
 
-    playbackState->playbackPositionChanged().onReceive(this, [this](secs_t secs) {
+    playbackController()->currentPlaybackPositionChanged().onReceive(this, [this](secs_t secs, midi::tick_t) {
         updatePlayPosition(secs);
     });
 

src/stubs/playback/playbackcontrollerstub.cpp
@@ -48,9 +48,9 @@ void PlaybackControllerStub::reset()
 {
 }
 
-muse::async::Channel<uint32_t> PlaybackControllerStub::midiTickPlayed() const
+muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t> PlaybackControllerStub::currentPlaybackPositionChanged() const
 {
-    return muse::async::Channel<uint32_t>();
+    return muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t>();
 }
 
 muse::audio::TrackSequenceId PlaybackControllerStub::currentTrackSequenceId() const

src/stubs/playback/playbackcontrollerstub.h
@@ -36,7 +36,7 @@ public:
 
     void reset() override;
 
-    muse::async::Channel<uint32_t> midiTickPlayed() const override;
+    muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t> currentPlaybackPositionChanged() const override;
 
     muse::audio::TrackSequenceId currentTrackSequenceId() const override;
     muse::async::Notification currentTrackSequenceIdChanged() const override;

src/playback/tests/mocks/playbackcontrollermock.h
@@ -38,7 +38,7 @@ public:
 
     MOCK_METHOD(void, reset, (), (override));
 
-    MOCK_METHOD(muse::async::Channel<uint32_t>, midiTickPlayed, (), (const, override));
+    MOCK_METHOD((muse::async::Channel<muse::audio::secs_t, muse::midi::tick_t>), currentPlaybackPositionChanged, (), (const, override));
 
     MOCK_METHOD(muse::audio::TrackSequenceId, currentTrackSequenceId, (), (const, override));
     MOCK_METHOD(muse::async::Notification, currentTrackSequenceIdChanged, (), (const, override));