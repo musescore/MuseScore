742ac19c08d1da675e6117f3000aa7871053ea8f
Michele Spagnolo <m.spagnolo@mu.se>
2024-08-13 09:53:51 +0200

    Look at offset y to decide if item can be centered

--- src/engraving/rendering/dev/systemlayout.cpp
+++ src/engraving/rendering/dev/systemlayout.cpp
@@ -2838,7 +2838,7 @@ void SystemLayout::centerElementsBetweenStaves(const System* system)
 
 bool SystemLayout::elementShouldBeCenteredBetweenStaves(const EngravingItem* item, const System* system)
 {
-    if (item->offset() != item->propertyDefault(Pid::OFFSET).value<PointF>()) {
+    if (item->offset().y() != item->propertyDefault(Pid::OFFSET).value<PointF>().y()) {
         // NOTE: because of current limitations of the offset system, we can't center an element that's been manually moved.
         return false;
     }
