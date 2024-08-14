fda13c5f7532302bc10f8987903379d795d6eb0d
Calum Matheson <mathesoncalum@gmail.com>
2024-08-14 09:11:36 +0100

    Ensure proper conversion of TremoloBar line widths

--- src/engraving/rendering/dev/tdraw.cpp
+++ src/engraving/rendering/dev/tdraw.cpp
@@ -3126,7 +3126,8 @@ void TDraw::draw(const TremoloBar* item, Painter* painter)
 {
     TRACE_DRAW_ITEM;
     const TremoloBar::LayoutData* ldata = item->ldata();
-    Pen pen(item->curColor(), item->lineWidth().val(), PenStyle::SolidLine, PenCapStyle::RoundCap, PenJoinStyle::RoundJoin);
+    const double lw = item->absoluteFromSpatium(item->lineWidth());
+    Pen pen(item->curColor(), lw, PenStyle::SolidLine, PenCapStyle::RoundCap, PenJoinStyle::RoundJoin);
     painter->setPen(pen);
     painter->drawPolyline(ldata->polygon);
 }
