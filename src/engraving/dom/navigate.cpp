82c94849ae8e65ec4e535985426ade7ae8591bd1
Joachim Schmitz <jojo@schmitz-digital.de>
2024-09-03 12:26:28 +0200

    Change `moveAlt()` to take a `MoveDirection`

--- src/engraving/dom/navigate.cpp
+++ src/engraving/dom/navigate.cpp
@@ -35,6 +35,7 @@
 #include "staff.h"
 #include "soundflag.h"
 #include "guitarbend.h"
+#include "notation/notationtypes.h"
 
 using namespace mu;
 
@@ -392,12 +393,12 @@ Note* Score::downAltCtrl(Note* note) const
 //    or top/bottom of next/previous track's chord if at wit's end
 //---------------------------------------------------------
 
-EngravingItem* Score::moveAlt(EngravingItem* element, DirectionV direction)
+EngravingItem* Score::moveAlt(EngravingItem* element, notation::MoveDirection direction)
 {
     EngravingItem* result = nullptr;
     ChordRest* cr = nullptr;
     auto originalTrack = element->track();
-    bool moveUp = (direction == DirectionV::UP);
+    bool moveUp = (direction == notation::MoveDirection::Up);
     bool isNote = element->isNote();
     bool isRest = element->isRest();
 
