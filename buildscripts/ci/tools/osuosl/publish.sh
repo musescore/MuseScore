86342fdef35b445b645a37904a75d85b98e00f14
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2024-09-09 17:30:15 +0200

    Correct number of nightly builds preserved
    and delete some ancient ones from MuseScore 4.0 and 4.2 that use some old naming scheme

--- buildscripts/ci/tools/osuosl/publish.sh
+++ buildscripts/ci/tools/osuosl/publish.sh
@@ -116,13 +116,19 @@ fi
 # Delete old files
 if [ "$BUILD_MODE" == "nightly" ]; then
     echo "Delete old MuseScore-Studio-Nightly files"
-    number_to_keep=42 # includes the one we just uploaded and the symlink to it
-    if [ "$OS" == "linux" ]; then
-        ((++number_to_keep)) # one extra for the zsync file
-    elif [ "$OS" == "windows" ]; then
-        ((number_to_keep *= 2)) # two nightlies each night, namely portable and normal
-    fi
-    ssh -i $SSH_KEY musescore-nightlies@ftp-osl.osuosl.org "cd ~/ftp/$FTP_PATH; ls {MuseScoreNightly,MuseScore-Studio-Nightly}* -t | tail -n +${number_to_keep} | xargs rm -f"
+    num_days=40         # keep old nightlies for this long  
+    num_today=2         # today's build and latest symlink  
+    num_branches=2      # master and release branch  
+    num_variants=1  
+    if [ "$OS" == "windows" ]; then  
+        num_variants=2      # portable and normal  
+    elif [ "$OS" == "linux" ]; then  
+        num_variants=3      # x86_64, aarch64, armv7l  
+        ((num_today += 1))  # zsync file  
+    fi  
+    num_to_keep=$(((num_days + num_today) * num_branches * num_variants))  
+    start_line_num=$((num_to_keep + 1))  
+    ssh -i $SSH_KEY musescore-nightlies@ftp-osl.osuosl.org "cd ~/ftp/$FTP_PATH && ls -t MuseScore{-Studio-,}Nightly-* MuseScore-4.*-x86_64.{7z,paf.exe} | tail -n +${start_line_num} | xargs rm -f"
 fi
 
 # Sending index.html
