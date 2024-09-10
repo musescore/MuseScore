83e1ad2d1d17f0b44d05ab74347cc73f333f3b34
RomanPudashkin <67867444+RomanPudashkin@users.noreply.github.com>
2024-09-10 10:56:41 +0300

    Merge pull request #24590 from RomanPudashkin/midi_export_chord_symbols_fix
    midi_export_chord_symbols_fix

--- src/importexport/midi/internal/midiexport/exportmidi.cpp
+++ src/importexport/midi/internal/midiexport/exportmidi.cpp
@@ -238,7 +238,7 @@ bool ExportMidi::write(QIODevice* device, bool midiExpandRepeats, bool exportRPN
     CompatMidiRendererInternal::Context context;
     context.eachStringHasChannel = false;
     context.instrumentsHaveEffects = false;
-    context.harmonyChannelSetting = CompatMidiRendererInternal::HarmonyChannelSetting::DISABLED;
+    context.harmonyChannelSetting = CompatMidiRendererInternal::HarmonyChannelSetting::DEFAULT;
     context.sndController = CompatMidiRender::getControllerForSnd(m_score, synthState.ccToUse());
     context.useDefaultArticulations = false;
     context.applyCaesuras = true;
