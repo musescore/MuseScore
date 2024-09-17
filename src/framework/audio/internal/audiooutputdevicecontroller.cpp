9c71059f85e08ab3454017317fbb79ac91cdb0f3
Roman Pudashkin <r.pudashkin@gmail.com>
2024-09-17 18:01:18 +0300

    more useful logs

--- src/framework/audio/internal/audiooutputdevicecontroller.cpp
+++ src/framework/audio/internal/audiooutputdevicecontroller.cpp
@@ -34,11 +34,14 @@ void AudioOutputDeviceController::init()
     checkConnection();
 
     audioDriver()->availableOutputDevicesChanged().onNotify(this, [this]() {
+        LOGI() << "Available output devices changed, checking connection...";
         checkConnection();
     });
 
     configuration()->audioOutputDeviceIdChanged().onNotify(this, [this]() {
         AudioDeviceID deviceId = configuration()->audioOutputDeviceId();
+        LOGI() << "Trying to change output device: " << deviceId;
+
         bool ok = audioDriver()->selectOutputDevice(deviceId);
         if (ok) {
             onOutputDeviceChanged();
@@ -47,6 +50,8 @@ void AudioOutputDeviceController::init()
 
     configuration()->driverBufferSizeChanged().onNotify(this, [this]() {
         unsigned int bufferSize = configuration()->driverBufferSize();
+        LOGI() << "Trying to change buffer size: " << bufferSize;
+
         bool ok = audioDriver()->setOutputDeviceBufferSize(bufferSize);
         if (ok) {
             async::Async::call(this, [this, bufferSize](){
@@ -57,6 +62,8 @@ void AudioOutputDeviceController::init()
 
     configuration()->sampleRateChanged().onNotify(this, [this]() {
         unsigned int sampleRate = configuration()->sampleRate();
+        LOGI() << "Trying to change sample rate: " << sampleRate;
+
         bool ok = audioDriver()->setOutputDeviceSampleRate(sampleRate);
         if (ok) {
             async::Async::call(this, [this, sampleRate](){
@@ -85,8 +92,10 @@ void AudioOutputDeviceController::checkConnection()
     bool deviceChanged = false;
 
     if (!preferredDeviceId.empty() && preferredDeviceId != currentDeviceId && containsDevice(devices, preferredDeviceId)) {
+        LOGI() << "Changing output device: " << preferredDeviceId;
         deviceChanged = audioDriver()->selectOutputDevice(preferredDeviceId);
     } else if (!containsDevice(devices, currentDeviceId)) {
+        LOGW() << "Device " << currentDeviceId << " not found, resetting to default";
         deviceChanged = audioDriver()->resetToDefaultOutputDevice();
     }
 
