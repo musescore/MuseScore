bb53cf0bc1a95ccbdb57ad01fe6008540de2a232
Alexander Pavlov <sanch.pavlov@gmail.com>
2024-09-25 14:13:08 +0300

    fix #25010: added midi generation for guitar bends

--- src/engraving/compat/midi/compatmidirender.cpp
+++ src/engraving/compat/midi/compatmidirender.cpp
@@ -7,6 +7,9 @@
 #include "dom/navigate.h"
 
 namespace mu::engraving {
+static int slideTicks(Chord* chord);
+static int graceBendTicks(Chord* chord);
+
 void CompatMidiRender::renderScore(Score* score, EventsHolder& events, const CompatMidiRendererInternal::Context& ctx, bool expandRepeats)
 {
     score->masterScore()->setExpandRepeats(expandRepeats);
@@ -495,7 +498,20 @@ void CompatMidiRender::updateGateTime(const Instrument* instrument, int& gateTim
 
 void CompatMidiRender::createGraceNotesPlayEvents(const Score* score, const Fraction& tick, Chord* chord, int& ontime, int& trailtime)
 {
-    std::vector<Chord*> gnb = chord->graceNotesBefore();
+    auto hasNoPrebendGrace = [] (Chord* chord) -> bool {
+        return std::any_of(chord->notes().begin(), chord->notes().end(), [](Note* n) {
+            return !n->isPreBendStart();
+        });
+    };
+
+    std::vector<Chord*> gnbNotExcludingPrebends = chord->graceNotesBefore();
+    std::vector<Chord*> gnb;
+    for (Chord* ch : gnbNotExcludingPrebends) {
+        if (hasNoPrebendGrace(ch)) {
+            gnb.push_back(ch);
+        }
+    }
+
     std::vector<Chord*> gna = chord->graceNotesAfter();
     int nb = int(gnb.size());
     int na = int(gna.size());
@@ -534,8 +550,12 @@ void CompatMidiRender::createGraceNotesPlayEvents(const Score* score, const Frac
         //  - the grace note duration as notated does not matter
         //
         Chord* graceChord = gnb[0];
+        const auto& graceNotes = graceChord->notes();
+        bool graceBend = std::any_of(graceNotes.begin(), graceNotes.end(), [](Note* note) {
+            return note->isGraceBendStart();
+        });
 
-        if (graceChord->noteType() == NoteType::ACCIACCATURA) {
+        if (graceChord->noteType() == NoteType::ACCIACCATURA || graceBend) {
             ontime = 0;
             graceDuration = 0;
             weighta = 1.0;
@@ -932,7 +952,7 @@ void CompatMidiRender::createSlideOutNotePlayEvents(Note* note, NoteEventList* e
     int slideOn = NoteEvent::NOTE_LENGTH - totalSlideDuration;
     double velocity = !note->ghost() ? NoteEvent::DEFAULT_VELOCITY_MULTIPLIER : NoteEvent::GHOST_VELOCITY_MULTIPLIER;
     if (!hasTremolo) {
-        el->push_back(NoteEvent(0, onTime, slideOn, velocity, !note->tieBack()));
+        el->push_back(NoteEvent(0, onTime, slideOn - onTime, velocity, !note->tieBack()));
     }
 
     int pitch = 0;
@@ -992,9 +1012,18 @@ int CompatMidiRender::adjustTrailtime(int trailtime, Chord* currentChord, Chord*
 
     const std::vector<Chord*>& graceBeforeChords = nextChord->graceNotesBefore();
     std::vector<Chord*> graceNotesBeforeBar;
-    std::copy_if(graceBeforeChords.begin(), graceBeforeChords.end(), std::back_inserter(graceNotesBeforeBar), [](Chord* ch) {
-        return ch->noteType() == NoteType::ACCIACCATURA;
+
+    bool hasGraceBend = std::any_of(graceBeforeChords.begin(), graceBeforeChords.end(), [](Chord* ch) {
+        return std::any_of(ch->notes().begin(), ch->notes().end(), [](Note* n) {
+            return n->isGraceBendStart();
+        });
     });
+
+    if (!hasGraceBend) {
+        std::copy_if(graceBeforeChords.begin(), graceBeforeChords.end(), std::back_inserter(graceNotesBeforeBar), [](Chord* ch) {
+            return ch->noteType() == NoteType::ACCIACCATURA;
+        });
+    }
 
     const int currentTicks = currentChord->ticks().ticks();
     IF_ASSERT_FAILED(currentTicks > 0) {
@@ -1004,14 +1033,19 @@ int CompatMidiRender::adjustTrailtime(int trailtime, Chord* currentChord, Chord*
     int reducedTicks = 0;
 
     const auto& notes = nextChord->notes();
-    bool anySlidesIn = std::any_of(notes.begin(), notes.end(), [](const Note* note) {
-        return note->slideToType() == Note::SlideType::DownToNote || (note->slideToType() == Note::SlideType::UpToNote && note->fret() > 1);
-    });
-
-    if (!graceNotesBeforeBar.empty()) {
+    if (hasGraceBend) {
+        reducedTicks = graceBendTicks(currentChord);
+    } else if (!graceNotesBeforeBar.empty()) {
         reducedTicks = std::min(graceNotesBeforeBar[0]->ticks().ticks(), currentTicks / 2);
-    } else if (anySlidesIn) {
-        reducedTicks = CompatMidiRender::slideTicks(currentChord);
+    } else {
+        bool anySlidesIn = std::any_of(notes.begin(), notes.end(), [](const Note* note) {
+            return note->slideToType() == Note::SlideType::DownToNote
+                   || (note->slideToType() == Note::SlideType::UpToNote && note->fret() > 1);
+        });
+
+        if (anySlidesIn) {
+            reducedTicks = slideTicks(currentChord);
+        }
     }
 
     trailtime += reducedTicks / (double)currentTicks * NoteEvent::NOTE_LENGTH;
@@ -1145,7 +1179,7 @@ int CompatMidiRender::totalTiedNoteTicks(Note* note)
     return total.ticks();
 }
 
-int CompatMidiRender::slideTicks(Chord* chord)
+int slideTicks(Chord* chord)
 {
     const int currentTicks = chord->ticks().ticks();
     if (currentTicks <= SLIDE_DURATION) {
@@ -1153,6 +1187,16 @@ int CompatMidiRender::slideTicks(Chord* chord)
     }
 
     return SLIDE_DURATION;
+}
+
+int graceBendTicks(Chord* chord)
+{
+    const int currentTicks = chord->ticks().ticks();
+    if (currentTicks / 2 <= GRACE_BEND_DURATION) {
+        return currentTicks / 2;
+    }
+
+    return GRACE_BEND_DURATION;
 }
 
 int CompatMidiRender::tick(const CompatMidiRendererInternal::Context& ctx, int tick)
