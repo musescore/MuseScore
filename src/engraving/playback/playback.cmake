97b49d65fae469fc93856bec854b0da869e8b884
Roman Pudashkin <r.pudashkin@gmail.com>
2024-10-03 14:19:06 +0300

    fixed circular dependencies between BendsRenderer and GraceChordsRenderer

--- src/engraving/playback/playback.cmake
+++ src/engraving/playback/playback.cmake
@@ -37,6 +37,8 @@ set(PLAYBACK_SRC
     ${CMAKE_CURRENT_LIST_DIR}/renderers/arpeggiorenderer.h
     ${CMAKE_CURRENT_LIST_DIR}/renderers/gracechordsrenderer.cpp
     ${CMAKE_CURRENT_LIST_DIR}/renderers/gracechordsrenderer.h
+    ${CMAKE_CURRENT_LIST_DIR}/renderers/gracechordcontext.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/renderers/gracechordcontext.h
     ${CMAKE_CURRENT_LIST_DIR}/renderers/tremolorenderer.cpp
     ${CMAKE_CURRENT_LIST_DIR}/renderers/tremolorenderer.h
     ${CMAKE_CURRENT_LIST_DIR}/renderers/chordarticulationsrenderer.cpp
