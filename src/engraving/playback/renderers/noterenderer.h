71f16ec348002b63346a1196b2be43467d224a7a
Roman Pudashkin <r.pudashkin@gmail.com>
2024-10-15 17:56:50 +0300

    improved staccato rendering

--- src/engraving/playback/renderers/noterenderer.h
+++ src/engraving/playback/renderers/noterenderer.h
@@ -33,6 +33,7 @@ public:
 
 private:
     static void renderTiedNotes(const Note* firstNote, NominalNoteCtx& firstNoteCtx);
+    static void addTiedNote(const NominalNoteCtx& tiedNoteCtx, NominalNoteCtx& firstNoteCtx);
     static void updateArticulationBoundaries(const muse::mpe::timestamp_t noteTimestamp, const muse::mpe::duration_t noteDuration,
                                              muse::mpe::ArticulationMap& articulations);
     static void applySwingIfNeed(const Note* note, NominalNoteCtx& noteCtx);
