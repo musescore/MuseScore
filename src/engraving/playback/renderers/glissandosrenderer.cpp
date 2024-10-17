b6491140504500b94f85ee8370d8a4688dbafa70
Roman Pudashkin <r.pudashkin@gmail.com>
2024-10-17 12:02:08 +0300

    don't retrigger the 1st glissando note if it's tied

--- src/engraving/playback/renderers/glissandosrenderer.cpp
+++ src/engraving/playback/renderers/glissandosrenderer.cpp
@@ -31,7 +31,7 @@ using namespace mu::engraving;
 using namespace muse;
 using namespace muse::mpe;
 
-void GlissandosRenderer::renderDiscreteGlissando(const Note* note, const NominalNoteCtx& ctx, mpe::PlaybackEventList& result)
+static std::vector<int> pitchSteps(const Note* note)
 {
     const Glissando* glissando = nullptr;
     for (const Spanner* spanner : note->spannerFor()) {
@@ -42,26 +42,41 @@ void GlissandosRenderer::renderDiscreteGlissando(const Note* note, const Nominal
     }
 
     if (!glissando) {
-        return;
+        return {};
     }
 
     std::vector<int> pitchSteps;
-    if (!Glissando::pitchSteps(glissando, pitchSteps)) {
+    Glissando::pitchSteps(glissando, pitchSteps);
+
+    return pitchSteps;
+}
+
+muse::mpe::duration_t GlissandosRenderer::discreteGlissandoStepDuration(const Note* note, const duration_t noteDuration)
+{
+    const std::vector<int> steps = pitchSteps(note);
+    if (steps.empty()) {
+        return 0;
+    }
+
+    return noteDuration / static_cast<float>(steps.size());
+}
+
+void GlissandosRenderer::renderDiscreteGlissando(const Note* note, const NominalNoteCtx& ctx, mpe::PlaybackEventList& result)
+{
+    const std::vector<int> steps = pitchSteps(note);
+    if (steps.empty()) {
         return;
     }
 
-    if (pitchSteps.empty()) {
-        return;
-    }
-
-    const size_t stepsCount = pitchSteps.size();
+    const size_t stepsCount = steps.size();
     const float durationStep = ctx.duration / static_cast<float>(stepsCount);
+    const size_t firstStep = note->tieBack() ? 1 : 0;
 
-    for (size_t i = 0; i < stepsCount; ++i) {
+    for (size_t i = firstStep; i < stepsCount; ++i) {
         NominalNoteCtx noteCtx(ctx);
         noteCtx.duration = durationStep;
         noteCtx.timestamp += i * durationStep;
-        noteCtx.pitchLevel += pitchSteps.at(i) * mpe::PITCH_LEVEL_STEP;
+        noteCtx.pitchLevel += steps.at(i) * mpe::PITCH_LEVEL_STEP;
 
         int utick = timestampToTick(noteCtx.chordCtx.score, noteCtx.timestamp);
         noteCtx.dynamicLevel = ctx.chordCtx.playbackCtx->appliableDynamicLevel(note->track(), utick);
