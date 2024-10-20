850a705d97380fade35ec4f6d1e4e85082c5606d
Peter Jonas <shoogle@users.noreply.github.com>
2024-10-20 23:21:13 +0100

    Restore previous selection on navigation with empty selection
    Now the viewport won't jump to the beginning or end of the score
    unexpectedly after pressing an arrow key when nothing is selected.

--- src/notation/internal/notationactioncontroller.cpp
+++ src/notation/internal/notationactioncontroller.cpp
@@ -969,6 +969,12 @@ void NotationActionController::move(MoveDirection direction, bool quickly)
         return;
     }
 
+    if (interaction->selection()->isNone()) {
+        interaction->moveSelection(direction, MoveSelectionType::EngravingItem);
+        playSelectedElement(true);
+        return;
+    }
+
     const EngravingItem* selectedElement = interaction->selection()->element();
     bool playChord = false;
 
@@ -986,8 +992,6 @@ void NotationActionController::move(MoveDirection direction, bool quickly)
             }
             interaction->moveSelection(direction, MoveSelectionType::String);
             return;
-        } else if (interaction->selection()->isNone()) {
-            interaction->selectFirstElement(false);
         } else {
             interaction->movePitch(direction, quickly ? PitchMode::OCTAVE : PitchMode::CHROMATIC);
         }
@@ -1035,9 +1039,6 @@ void NotationActionController::move(MoveDirection direction, bool quickly)
         if (selectedElement && selectedElement->isTextBase()) {
             interaction->nudge(direction, quickly);
         } else {
-            if (interaction->selection()->isNone()) {
-                interaction->selectFirstElement(false);
-            }
             interaction->moveSelection(direction, quickly ? MoveSelectionType::Measure : MoveSelectionType::Chord);
             playChord = true;
         }
