2e3bf0f6e02f149d6b73e011f242a90ca61faf13
Robert Patterson <robert@robertgpatterson.com>
2024-10-20 18:05:15 -0500

    fix mis-aligned `styleDef` initialization

--- src/engraving/style/styledef.cpp
+++ src/engraving/style/styledef.cpp
@@ -469,7 +469,7 @@ const std::array<StyleDef::StyleValue, size_t(Sid::STYLES)> StyleDef::styleValue
 
     styleDef(multiVoiceRestTwoSpaceOffset,               false),
     styleDef(mergeMatchingRests,                         false),
-    styleDef(showLedgerLinesOnBreveRests,               false),
+    styleDef(showLedgerLinesOnBreveRests,                false),
     styleDef(createMultiMeasureRests,                    false),
     styleDef(minEmptyMeasures,                           PropertyValue(1)),
     styleDef(singleMeasureMMRestUseNormalRest,           true),
