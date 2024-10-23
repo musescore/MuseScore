dd57bb0ef3d6fb42e3074706fc19b8ed0f8ac81b
Igor <igor.korsukov@gmail.com>
2024-10-23 11:35:06 +0200

    added secs_t type to global

--- src/framework/global/types/secs.h
+++ src/framework/global/types/secs.h
@@ -21,37 +21,8 @@
  */
 #pragma once
 
-#include <cmath>
-
 #include "number.h"
 
 namespace muse {
-inline float db_to_linear(float v)
-{
-    return std::pow(10.0, v / 20.0);
-}
-
-inline float linear_to_db(float v)
-{
-    return 20.0 * std::log10(std::abs(v));
-}
-
-//! NOTE Just linear ratio
-using ratio_t = number_t<float>;
-
-//! NOTE logarithmic ratio (decibel)
-using db_t = number_t<float>;
-
-inline ratio_t db_to_linear(db_t v)
-{
-    return muse::db_to_linear(v.raw());
-}
-
-inline db_t linear_to_db(ratio_t v)
-{
-    return muse::linear_to_db(v.raw());
-}
-
-//! NOTE Percent
-using percent_t = number_t<float>;
+using secs_t = number_t<double>;
 }
