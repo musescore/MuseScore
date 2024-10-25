68058f1c969331f01e964dd1a1ddcb2df9fd45c6
Robert Patterson <robert@robertgpatterson.com>
2024-10-25 14:50:06 -0500

    fix compiler error post-rebase

--- src/notation/view/percussionpanel/percussionpanelmodel.cpp
+++ src/notation/view/percussionpanel/percussionpanelmodel.cpp
@@ -196,7 +196,7 @@ void PercussionPanelModel::writePitch(int pitch)
         undoStack->commitChanges();
     };
 
-    undoStack->prepareChanges();
+    undoStack->prepareChanges(muse::TranslatableString("undoableAction", "Write pitch"));
 
     interaction()->noteInput()->startNoteInput();
 
