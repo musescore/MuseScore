5fc9582fa9e091651dde641d8dafbc415a778db9
Larry Valkama <lz64@proton.me>
2024-10-26 11:46:09 +0200

    fix compiler errors and warnings

--- src/framework/audio/internal/audiomidimanager.cpp
+++ src/framework/audio/internal/audiomidimanager.cpp
@@ -298,7 +298,7 @@ unsigned int AudioMidiManager::outputDeviceSampleRate() const
 bool AudioMidiManager::setOutputDeviceSampleRate(unsigned int sampleRate)
 {
     LOGE("------ setSamplerate: %u", sampleRate);
-    if (m_spec.sampleRate == (int)sampleRate) {
+    if (m_spec.sampleRate == sampleRate) {
         LOGE("------ SAME setSamplerate, doing nothing ------");
         return true;
     }
