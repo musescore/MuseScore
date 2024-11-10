ryannsmarket/pr

update and add test

src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -697,6 +697,9 @@ TEST_F(Musicxml_Tests, harmony7) {
 TEST_F(Musicxml_Tests, harmony8) {
     mxmlIoTest("testHarmony8");
 }
+TEST_F(Musicxml_Tests, harmony9) {
+    mxmlIoTest("testHarmony9");
+}                                                                      // chordnames without chordrest
 TEST_F(Musicxml_Tests, hello) {
     mxmlIoTest("testHello");
 }

musescore/MuseScore

src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -691,6 +691,9 @@ TEST_F(MusicXml_Tests, harmony7) {
 TEST_F(MusicXml_Tests, harmony8) {
     musicXmlIoTest("testHarmony8");
 }
+TEST_F(Musicxml_Tests, harmony9) {
+    musicXmlIoTest("testHarmony9");
+}                                                                      // chordnames without chordrest
 TEST_F(MusicXml_Tests, hello) {
     musicXmlIoTest("testHello");
 }