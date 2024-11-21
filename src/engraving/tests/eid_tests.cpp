de0341a8c76c7a626584d9faa6be89a2c1826e32
Michele Spagnolo <m.spagnolo@mu.se>
2024-11-21 11:34:42 +0100

    New tests

---
+++ src/engraving/tests/eid_tests.cpp
@@ -0,0 +1,64 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include "dom/engravingitem.h"
+
+#include "utils/scorerw.h"
+#include "utils/scorecomp.h"
+#include "utils/testutils.h"
+
+using namespace mu;
+using namespace mu::engraving;
+
+static const String DATA_DIR("all_elements_data/");
+
+class Engraving_EIDTests : public ::testing::Test
+{
+};
+
+static void itemShouldBeRegistered(void*, EngravingItem* item)
+{
+    if (item->registerId()) {
+        EngravingObject* registeredItem = item->masterScore()->eidRegister()->itemFromEID(item->eid());
+        EXPECT_TRUE(registeredItem);
+        EXPECT_EQ(registeredItem, item);
+    }
+}
+
+TEST_F(Engraving_EIDTests, testRegisteredItems) //TODO: verify program change, 72 is wrong surely?
+{
+    bool useRead302 = MScore::useRead302InTestMode;
+    MScore::useRead302InTestMode = false;
+
+    MasterScore* score = ScoreRW::readScore(DATA_DIR + u"random_elements.mscx");
+    EXPECT_TRUE(score);
+
+    score->scanElements(nullptr, itemShouldBeRegistered, true);
+    for (MeasureBase* mb = score->first(); mb; mb = mb->next()) {
+        itemShouldBeRegistered(nullptr, mb);
+    }
+
+    delete score;
+    MScore::useRead302InTestMode = useRead302;
+}
