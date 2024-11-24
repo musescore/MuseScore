e264e129510a216c646d8c4f3c75172ac5b804f8
Leon Vinken <leon.vinken@gmail.com>
2024-11-24 21:14:08 +0100

    Fix GH#25617: Prevent crash importing MusicXML file containing grace rest followed by tuple starting with rest
    Backport of #25629

--- importexport/musicxml/importmxmlpass2.cpp
+++ importexport/musicxml/importmxmlpass2.cpp
@@ -6352,9 +6352,13 @@ Note* MusicXMLParserPass2::note(const QString& partId,
 
       // begin allocation
       if (rest) {
+            if (!grace) {
             const int track = msTrack + msVoice;
             cr = addRest(_score, measure, noteStartTime, track, msMove,
                          duration, dura);
+                  }
+            else
+                  qDebug("ignoring grace rest");
             }
       else {
             if (!grace) {
