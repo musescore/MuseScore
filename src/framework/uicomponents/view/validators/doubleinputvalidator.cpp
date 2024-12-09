2649dc76159ff4a7656be5aa240cc1b358a63206
Eism <eismailzada@gmail.com>
2024-12-09 12:05:26 +0200

    added removal of trailing zeros

--- src/framework/uicomponents/view/validators/doubleinputvalidator.cpp
+++ src/framework/uicomponents/view/validators/doubleinputvalidator.cpp
@@ -31,34 +31,40 @@ DoubleInputValidator::DoubleInputValidator(QObject* parent)
 
 void DoubleInputValidator::fixup(QString& string) const
 {
-    auto zeros = [](int num)->QString {
-        QString s = QString();
-        for (int i = 0; i < num; i++) {
-            s.append("0");
+    auto removeTrailingZeros = [](QString& str) {
+        if (str.isEmpty()) {
+            return;
         }
-        return s;
-    };
 
-    if (!string.contains(".")) {
-        string.append("." + zeros(m_decimal));
-    }
+        if (!str.contains('.')) {
+            return;
+        }
+
+        size_t num = str.size();
+        for (size_t i = num - 1; i > 0; i--) {
+            if (str[i] == '0') {
+                str.remove(i, 1);
+            } else if (str[i] == '.') {
+                str.remove(i, 1);
+                break;
+            } else {
+                break;
+            }
+        }
+    };
 
     if (string.startsWith(".")) {
         string.prepend("0");
     }
 
     if (string.endsWith(".")) {
-        string.append(zeros(m_decimal));
+        string.remove(string.size() - 1, 1);
     }
 
     QStringList strList = string.split(".", Qt::SkipEmptyParts);
 
     QString intPart = strList.at(0);
-    QString floatPart = strList.at(1);
-
-    if (floatPart.length() < m_decimal) {
-        floatPart.append(zeros(m_decimal - floatPart.length()));
-    }
+    QString floatPart = strList.size() > 1 ? strList.at(1) : 0;
 
     if (intPart.contains(QRegularExpression("^0{1,3}$"))) {
         intPart = QString("0");
@@ -66,8 +72,12 @@ void DoubleInputValidator::fixup(QString& string) const
         intPart = QString("-0");
     }
 
-    if (intPart == QString("-0") && floatPart == zeros(m_decimal)) {
+    if (intPart == QString("-0") && floatPart.isEmpty()) {
         intPart = QString("0");
+    }
+
+    if (floatPart.size() > m_decimal) {
+        floatPart = floatPart.remove(m_decimal, floatPart.size() - m_decimal);
     }
 
     string = QString("%1.%2").arg(intPart).arg(floatPart);
@@ -77,21 +87,31 @@ void DoubleInputValidator::fixup(QString& string) const
     } else if (string.toDouble() < m_bottom) {
         string = QString::number(m_bottom, 'f', m_decimal);
     }
+
+    removeTrailingZeros(string);
 }
 
 QValidator::State DoubleInputValidator::validate(QString& inputStr, int& cursorPos) const
 {
     QValidator::State state = Invalid;
 
-    if (inputStr.contains(QRegularExpression(QString("^\\-?\\d{1,3}\\.\\d{%1}$").arg(m_decimal)))) {
-        if (inputStr.contains(QRegularExpression("^\\-?0{2,3}\\."))
-            || (inputStr.startsWith("-") && muse::RealIsNull(inputStr.toDouble()))) {
+    const QRegularExpression validRegex(QString("^\\-?\\d{1,3}(\\.\\d{1,%1})?$").arg(m_decimal));
+
+    if (inputStr.contains(validRegex)) {
+        static const QRegularExpression invalidZeroRegex("^\\-?0{2,3}\\."); // for '-000.'
+        static const QRegularExpression invalidTrailingZeroRegex("^\\-?\\d+\\.0{1,}$"); // for '1.00'
+        static const QRegularExpression invalidTrailingDotRegex("^\\-?\\d+\\.$"); // for '1.'
+
+        if (inputStr.contains(invalidZeroRegex)
+            || (inputStr.startsWith("-") && muse::RealIsNull(inputStr.toDouble())) // for "-000"
+            || inputStr.contains(invalidTrailingZeroRegex)
+            || inputStr.contains(invalidTrailingDotRegex)) {
             state = Intermediate;
         } else {
             state = Acceptable;
         }
     } else if (inputStr.contains(QRegularExpression("^\\-?\\d{0,3}\\.?$"))
-               || inputStr.contains(QRegularExpression(QString("^\\-?\\d{0,3}\\.\\d{0,%1}$").arg(m_decimal)))) {
+               || inputStr.contains(QRegularExpression(QString("^\\-?\\d{0,3}\\.\\d{%1,}$").arg(m_decimal)))) {
         state = Intermediate;
     } else {
         cursorPos = 0;
