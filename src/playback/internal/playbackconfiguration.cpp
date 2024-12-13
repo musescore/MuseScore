e4e6493baed8e9f136129ef95285374d2c333cee
krasko <154632626+krasko78@users.noreply.github.com>
2024-12-13 03:21:19 +0100

    11565: Made the Note Input page update on Reset Preferences

--- src/playback/internal/playbackconfiguration.cpp
+++ src/playback/internal/playbackconfiguration.cpp
@@ -96,7 +96,13 @@ void PlaybackConfiguration::init()
         m_playNotesWhenEditingChanged.notify();
     });
     settings()->setDefaultValue(PLAY_CHORD_WHEN_EDITING, Val(true));
+    settings()->valueChanged(PLAY_CHORD_WHEN_EDITING).onReceive(nullptr, [this](const Val& val) {
+        m_playChordWhenEditingChanged.send(val.toBool());
+    });
     settings()->setDefaultValue(PLAY_HARMONY_WHEN_EDITING, Val(true));
+    settings()->valueChanged(PLAY_HARMONY_WHEN_EDITING).onReceive(nullptr, [this](const Val& val) {
+        m_playHarmonyWhenEditingChanged.send(val.toBool());
+    });
     settings()->setDefaultValue(PLAYBACK_CURSOR_TYPE_KEY, Val(PlaybackCursorType::STEPPED));
     settings()->setDefaultValue(SOUND_PRESETS_MULTI_SELECTION_KEY, Val(false));
     settings()->setDefaultValue(MIXER_RESET_SOUND_FLAGS_WHEN_CHANGE_SOUND_WARNING, Val(true));
@@ -159,6 +165,11 @@ void PlaybackConfiguration::setPlayChordWhenEditing(bool value)
     settings()->setSharedValue(PLAY_CHORD_WHEN_EDITING, Val(value));
 }
 
+muse::async::Channel<bool> PlaybackConfiguration::playChordWhenEditingChanged() const
+{
+    return m_playChordWhenEditingChanged;
+}
+
 bool PlaybackConfiguration::playHarmonyWhenEditing() const
 {
     return settings()->value(PLAY_HARMONY_WHEN_EDITING).toBool();
@@ -167,6 +178,11 @@ bool PlaybackConfiguration::playHarmonyWhenEditing() const
 void PlaybackConfiguration::setPlayHarmonyWhenEditing(bool value)
 {
     settings()->setSharedValue(PLAY_HARMONY_WHEN_EDITING, Val(value));
+}
+
+muse::async::Channel<bool> PlaybackConfiguration::playHarmonyWhenEditingChanged() const
+{
+    return m_playHarmonyWhenEditingChanged;
 }
 
 PlaybackCursorType PlaybackConfiguration::cursorType() const
