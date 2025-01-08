112f211fb23ce40b6b5fd6f6052a1c35d0aa6879
Leon Vinken <leon.vinken@gmail.com>
2025-01-08 07:28:28 +0100

    Initial TablEdit importer

---
+++ src/importexport/tabledit/tableditmodule.h
@@ -0,0 +1,41 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_IMPORTEXPORT_TABLEDITMODULE_H
+#define MU_IMPORTEXPORT_TABLEDITMODULE_H
+
+#include "modularity/imodulesetup.h"
+
+namespace mu::iex::tabledit {
+class TablEditConfiguration;
+class TablEditModule : public muse::modularity::IModuleSetup
+{
+public:
+
+    std::string moduleName() const override;
+    void resolveImports() override;
+
+private:
+    std::shared_ptr<TablEditConfiguration> m_configuration;
+};
+}
+
+#endif // MU_IMPORTEXPORT_TABLEDITMODULE_H
