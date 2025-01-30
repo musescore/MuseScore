5eabbf9667774e9b8c8308b154e3d346c4991226
Igor Korsukov <igor.korsukov@gmail.com>
2025-01-30 08:01:13 +0200

    reimplemented old vst view (qwidget)

--- src/framework/vst/view/vstviewdialog_qwidget.cpp
+++ src/framework/vst/view/vstviewdialog_qwidget.cpp
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "abstractvsteditorview.h"
+#include "vstviewdialog_qwidget.h"
 
 #include <QWindow>
 #include <QTimer>
@@ -35,12 +35,12 @@
 using namespace muse::vst;
 using namespace Steinberg;
 
-::Steinberg::uint32 PLUGIN_API AbstractVstEditorView::addRef()
+::Steinberg::uint32 PLUGIN_API VstViewDialog::addRef()
 {
     return ::Steinberg::FUnknownPrivate::atomicAdd(__funknownRefCount, 1);
 }
 
-::Steinberg::uint32 PLUGIN_API AbstractVstEditorView::release()
+::Steinberg::uint32 PLUGIN_API VstViewDialog::release()
 {
     if (::Steinberg::FUnknownPrivate::atomicAdd(__funknownRefCount, -1) == 0) {
         return 0;
@@ -48,20 +48,20 @@ using namespace Steinberg;
     return __funknownRefCount;
 }
 
-IMPLEMENT_QUERYINTERFACE(AbstractVstEditorView, IPlugFrame, IPlugFrame::iid)
+IMPLEMENT_QUERYINTERFACE(VstViewDialog, IPlugFrame, IPlugFrame::iid)
 
-AbstractVstEditorView::AbstractVstEditorView(QWidget* parent)
+VstViewDialog::VstViewDialog(QWidget* parent)
     : TopLevelDialog(parent)
 {
     setAttribute(Qt::WA_NativeWindow);
 }
 
-AbstractVstEditorView::~AbstractVstEditorView()
+VstViewDialog::~VstViewDialog()
 {
     deinit();
 }
 
-void AbstractVstEditorView::deinit()
+void VstViewDialog::deinit()
 {
     if (m_view) {
         m_view->setFrame(nullptr);
@@ -69,14 +69,14 @@ void AbstractVstEditorView::deinit()
         m_view = nullptr;
     }
 
-    if (m_pluginPtr) {
-        m_pluginPtr->loadingCompleted().resetOnNotify(this);
-        m_pluginPtr->refreshConfig();
-        m_pluginPtr = nullptr;
+    if (m_instance) {
+        m_instance->loadingCompleted().resetOnNotify(this);
+        m_instance->refreshConfig();
+        m_instance = nullptr;
     }
 }
 
-tresult AbstractVstEditorView::resizeView(IPlugView* view, ViewRect* newSize)
+tresult VstViewDialog::resizeView(IPlugView* view, ViewRect* newSize)
 {
     view->checkSizeConstraint(newSize);
 
@@ -106,33 +106,23 @@ tresult AbstractVstEditorView::resizeView(IPlugView* view, ViewRect* newSize)
     return kResultTrue;
 }
 
-IVstPluginInstancePtr AbstractVstEditorView::getInstance() const
+void VstViewDialog::wrapPluginView()
 {
-    if (m_instanceId > 0) {
-        return instancesRegister()->instanceById(m_instanceId);
-    } else {
-        return determineInstance();
-    }
-}
-
-void AbstractVstEditorView::wrapPluginView()
-{
-    m_pluginPtr = getInstance();
-
-    if (!m_pluginPtr) {
+    m_instance = instancesRegister()->instanceById(m_instanceId);
+    if (!m_instance) {
         return;
     }
 
-    if (m_pluginPtr->isLoaded()) {
-        attachView(m_pluginPtr);
+    if (m_instance->isLoaded()) {
+        attachView(m_instance);
     } else {
-        m_pluginPtr->loadingCompleted().onNotify(this, [this]() {
-            attachView(m_pluginPtr);
+        m_instance->loadingCompleted().onNotify(this, [this]() {
+            attachView(m_instance);
         });
     }
 }
 
-void AbstractVstEditorView::attachView(IVstPluginInstancePtr instance)
+void VstViewDialog::attachView(IVstPluginInstancePtr instance)
 {
     if (!instance) {
         return;
@@ -153,7 +143,7 @@ void AbstractVstEditorView::attachView(IVstPluginInstancePtr instance)
     attached = m_view->attached(reinterpret_cast<void*>(windowHandle()->winId()), currentPlatformUiType());
     if (attached != kResultOk) {
         LOGE() << "Unable to attach vst plugin view to window"
-               << ", resourceId: " << m_resourceId;
+               << ", m_instanceId: " << m_instanceId;
         return;
     }
 
@@ -167,7 +157,7 @@ void AbstractVstEditorView::attachView(IVstPluginInstancePtr instance)
     });
 }
 
-void AbstractVstEditorView::updateViewGeometry()
+void VstViewDialog::updateViewGeometry()
 {
     IF_ASSERT_FAILED(m_view) {
         return;
@@ -186,7 +176,7 @@ void AbstractVstEditorView::updateViewGeometry()
     resizeView(m_view, &size);
 }
 
-void AbstractVstEditorView::moveViewToMainWindowCenter()
+void VstViewDialog::moveViewToMainWindowCenter()
 {
     QRectF mainWindowGeo = mainWindow()->qWindow()->geometry();
 
@@ -196,21 +186,21 @@ void AbstractVstEditorView::moveViewToMainWindowCenter()
     move(x, y);
 }
 
-void AbstractVstEditorView::showEvent(QShowEvent* ev)
+void VstViewDialog::showEvent(QShowEvent* ev)
 {
     moveViewToMainWindowCenter();
 
     TopLevelDialog::showEvent(ev);
 }
 
-void AbstractVstEditorView::closeEvent(QCloseEvent* ev)
+void VstViewDialog::closeEvent(QCloseEvent* ev)
 {
     deinit();
 
     TopLevelDialog::closeEvent(ev);
 }
 
-bool AbstractVstEditorView::event(QEvent* ev)
+bool VstViewDialog::event(QEvent* ev)
 {
     if (ev && ev->spontaneous() && ev->type() == QEvent::ShortcutOverride) {
         if (QKeyEvent* keyEvent = dynamic_cast<QKeyEvent*>(ev)) {
@@ -226,7 +216,7 @@ bool AbstractVstEditorView::event(QEvent* ev)
     return TopLevelDialog::event(ev);
 }
 
-FIDString AbstractVstEditorView::currentPlatformUiType() const
+FIDString VstViewDialog::currentPlatformUiType() const
 {
 #ifdef Q_OS_MAC
     return Steinberg::kPlatformTypeNSView;
@@ -239,56 +229,18 @@ FIDString AbstractVstEditorView::currentPlatformUiType() const
 #endif
 }
 
-int AbstractVstEditorView::trackId() const
-{
-    return m_trackId;
-}
-
-void AbstractVstEditorView::setTrackId(int newTrackId)
-{
-    if (m_trackId == newTrackId) {
-        return;
-    }
-
-    m_trackId = newTrackId;
-    emit trackIdChanged();
-
-    if (isAbleToWrapPlugin()) {
-        wrapPluginView();
-    }
-}
-
-const QString& AbstractVstEditorView::resourceId() const
-{
-    return m_resourceId;
-}
-
-void AbstractVstEditorView::setResourceId(const QString& newResourceId)
-{
-    if (m_resourceId == newResourceId) {
-        return;
-    }
-
-    m_resourceId = newResourceId;
-    emit resourceIdChanged();
-
-    setWindowTitle(newResourceId);
-
-    if (isAbleToWrapPlugin()) {
-        wrapPluginView();
-    }
-}
-
-int AbstractVstEditorView::instanceId() const
+int VstViewDialog::instanceId() const
 {
     return m_instanceId;
 }
 
-void AbstractVstEditorView::setInstanceId(int newInstanceId)
+void VstViewDialog::setInstanceId(int newInstanceId)
 {
     if (m_instanceId == newInstanceId) {
         return;
     }
     m_instanceId = newInstanceId;
     emit instanceIdChanged();
+
+    wrapPluginView();
 }
