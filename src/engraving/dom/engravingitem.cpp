988c964a68097ed34ffa441c83a12aa9118abc69
Michele Spagnolo <m.spagnolo@mu.se>
2025-01-30 18:25:38 +0100

    Always force property sync for items within the same score

--- src/engraving/dom/engravingitem.cpp
+++ src/engraving/dom/engravingitem.cpp
@@ -1474,16 +1474,10 @@ PropertyPropagation EngravingItem::propertyPropagation(const EngravingItem* dest
     const Score* sourceScore = score();
     const Score* destinationScore = destinationItem->score();
     const bool isTextProperty = propertyGroup(propertyId) == PropertyGroup::TEXT;
-    const Staff* sourceStaff = staff();
-    const Staff* destinationStaff = destinationItem->staff();
 
     // Properties must be propagated to items cloned for MMRests
-    if (sourceScore == destinationScore && sourceStaff == destinationStaff) {
+    if (sourceScore == destinationScore) {
         return PropertyPropagation::PROPAGATE;
-    }
-
-    if (propertyGroup(propertyId) != PropertyGroup::TEXT && sourceScore == destinationScore) {
-        return PropertyPropagation::NONE;
     }
 
     if (isTextProperty) {
