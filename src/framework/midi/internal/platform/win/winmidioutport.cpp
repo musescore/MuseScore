4587bcb3a0768fb7e879ea57c1d83aadd33edf20
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2025-02-28 00:05:20 +0100

    Clean up MIDI Event class and its usages

--- src/framework/midi/internal/platform/win/winmidioutport.cpp
+++ src/framework/midi/internal/platform/win/winmidioutport.cpp
@@ -192,7 +192,7 @@ Ret WinMidiOutPort::sendEvent(const Event& e)
 
     auto events = e.toMIDI10();
     for (auto& event : events) {
-        uint32_t msg = event.to_MIDI10Package();
+        uint32_t msg = event.toMidi10Package();
         MMRESULT ret = midiOutShortMsg(m_win->midiOut, (DWORD)msg);
         if (ret != MMSYSERR_NOERROR) {
             return make_ret(Err::MidiFailedConnect, "failed send event, error: " + errorString(ret));
