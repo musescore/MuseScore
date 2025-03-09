ryannsmarket/pr

Add adjustedBoundingRect to set a fixed height for bounding rectangle

src/notation/view/abstractelementpopupmodel.cpp
@@ -214,7 +214,17 @@ const mu::engraving::ElementTypeSet& AbstractElementPopupModel::dependentElement
 
 void AbstractElementPopupModel::updateItemRect()
 {
-    QRectF rect = m_item ? fromLogical(m_item->canvasBoundingRect()).toQRectF() : QRectF();
+    QRectF rect;
+
+    if (!m_item) {
+        rect = QRect();
+    } else {
+        if (m_item->isDynamic()) {
+            rect = fromLogical(toDynamic(m_item)->adjustedBoundingRect()).toQRectF();
+        } else {
+            rect = fromLogical(m_item->canvasBoundingRect()).toQRectF();
+        }
+    }
 
     if (m_itemRect != rect) {
         m_itemRect = rect;

musescore/MuseScore

src/notation/view/abstractelementpopupmodel.cpp
@@ -21,8 +21,12 @@
  */
 
 #include "abstractelementpopupmodel.h"
+
 #include "internal/partialtiepopupmodel.h"
 #include "internal/shadownotepopupmodel.h"
+
+#include "engraving/dom/dynamic.h"
+
 #include "log.h"
 
 using namespace mu::notation;
@@ -236,7 +240,16 @@ const mu::engraving::ElementTypeSet& AbstractElementPopupModel::dependentElement
 
 void AbstractElementPopupModel::updateItemRect()
 {
-    const QRectF rect = m_item ? fromLogical(m_item->canvasBoundingRect()).toQRectF() : QRectF();
+    QRectF rect;
+    if (!m_item) {
+        rect = QRectF();
+    } else {
+        if (m_item->isDynamic()) {
+            rect = fromLogical(toDynamic(m_item)->adjustedBoundingRect()).toQRectF();
+        } else {
+            rect = fromLogical(m_item->canvasBoundingRect()).toQRectF();
+        }
+    }
 
     if (m_itemRect != rect) {
         m_itemRect = rect;