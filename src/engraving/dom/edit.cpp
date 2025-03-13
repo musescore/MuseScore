a06e27ee7266349f30c596efb04c49c277e55133
Joachim Schmitz <jojo@schmitz-digital.de>
2025-03-13 09:28:13 +0100

    Fix GH#27069: Crash when deleting multimeasure rest at end of part

--- src/engraving/dom/edit.cpp
+++ src/engraving/dom/edit.cpp
@@ -4596,18 +4596,21 @@ void Score::cmdTimeDelete()
         startSegment = selection().startSegment();
         endSegment   = selection().endSegment();
     }
-
     if (!isMaster() && masterScore()) {
-        Measure* masterStartMeas = masterScore()->tick2measure(startSegment->tick());
-        Measure* masterEndMeas = masterScore()->tick2measure(endSegment->tick());
-        if (endSegment->isEndBarLineType()) {
-            Measure* prevEndMeasure = masterEndMeas->prevMeasure();
-            masterEndMeas = prevEndMeasure ? prevEndMeasure : masterEndMeas;
+        if (!endSegment) {
+            masterScore()->doTimeDelete(startSegment, endSegment);
+        } else {
+            Measure* masterStartMeas = masterScore()->tick2measure(startSegment->tick());
+            Measure* masterEndMeas = masterScore()->tick2measure(endSegment->tick());
+            if (endSegment->isEndBarLineType()) {
+                Measure* prevEndMeasure = masterEndMeas->prevMeasure();
+                masterEndMeas = prevEndMeasure ? prevEndMeasure : masterEndMeas;
+            }
+            Segment* masterStartSeg
+                = masterStartMeas ? masterStartMeas->findSegment(startSegment->segmentType(), startSegment->tick()) : startSegment;
+            Segment* masterEndSeg = masterEndMeas ? masterEndMeas->findSegment(endSegment->segmentType(), endSegment->tick()) : endSegment;
+            masterScore()->doTimeDelete(masterStartSeg, masterEndSeg);
         }
-        Segment* masterStartSeg
-            = masterStartMeas ? masterStartMeas->findSegment(startSegment->segmentType(), startSegment->tick()) : startSegment;
-        Segment* masterEndSeg = masterEndMeas ? masterEndMeas->findSegment(endSegment->segmentType(), endSegment->tick()) : endSegment;
-        masterScore()->doTimeDelete(masterStartSeg, masterEndSeg);
     } else {
         doTimeDelete(startSegment, endSegment);
     }
