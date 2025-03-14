ryannsmarket/pr

Clean up how we link to Qt

buildscripts/cmake/SetupQt6.cmake
@@ -1,14 +1,24 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# MuseScore-Studio-CLA-applies
+#
+# MuseScore Studio
+# Music Composition & Notation
+#
+# Copyright (C) 2024 MuseScore Limited
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-include(GetUtilsFunctions) # library of CMake functions ("fn__" namespace)
-
-# Print Qt version or fail the build if Qt (qmake) is not in PATH.
-fn__require_program(QMAKE Qt --version "https://musescore.org/en/handbook/developers-handbook/compilation" qmake6 qmake)
-
-set(CMAKE_AUTOUIC ON)
-set(CMAKE_AUTOMOC ON)
-set(CMAKE_AUTORCC ON)
-
-set(_components
+set(qt_components
     Core
     Gui
     Widgets
@@ -16,13 +26,10 @@ set(_components
     NetworkAuth
     Qml
     Quick
-    QuickControls2
-    QuickTemplates2
     QuickWidgets
     Xml
     Svg
     PrintSupport
-    OpenGL
     LinguistTools
 
     Core5Compat
@@ -32,64 +39,41 @@ set(_components
     StateMachine
 )
 
-if (NOT OS_IS_WASM)
-    set(_components
-        ${_components}
-        Concurrent
-    )
-endif()
+set(QT_LIBRARIES
+    Qt::Core
+    Qt::Gui
+    Qt::Widgets
+    Qt::Network
+    Qt::NetworkAuth
+    Qt::Qml
+    Qt::Quick
+    Qt::QuickWidgets
+    Qt::Xml
+    Qt::Svg
+    Qt::PrintSupport
 
-if (OS_IS_LIN)
-    set(_components
-        ${_components}
-        DBus
-    )
-endif()
+    Qt::Core5Compat
 
-if (QT_ADD_WEBSOCKET)
-    set(_components
-        ${_components}
-        WebSockets
-    )
-endif()
-
-foreach(_component ${_components})
-    find_package(Qt6${_component} REQUIRED)
-    list(APPEND QT_LIBRARIES ${Qt6${_component}_LIBRARIES})
-    list(APPEND QT_INCLUDES ${Qt6${_component}_INCLUDE_DIRS})
-    add_definitions(${Qt6${_component}_DEFINITIONS})
-endforeach()
-
-include_directories(${QT_INCLUDES})
-
-find_program(QT_QMAKE_EXECUTABLE qmake)
-set(_qmake_vars
-    QT_INSTALL_ARCHDATA
-    QT_INSTALL_BINS
-    QT_INSTALL_CONFIGURATION
-    QT_INSTALL_DATA
-    QT_INSTALL_DOCS
-    QT_INSTALL_EXAMPLES
-    QT_INSTALL_HEADERS
-    QT_INSTALL_IMPORTS
-    QT_INSTALL_LIBEXECS
-    QT_INSTALL_LIBS
-    QT_INSTALL_PLUGINS
-    QT_INSTALL_PREFIX
-    QT_INSTALL_QML
-    QT_INSTALL_TESTS
-    QT_INSTALL_TRANSLATIONS
+    Qt::StateMachine
 )
-foreach(_var ${_qmake_vars})
-    execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} "-query" ${_var}
-        RESULT_VARIABLE _return_val
-        OUTPUT_VARIABLE _out
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
 
-    if(_return_val EQUAL 0)
-        set(${_var} "${_out}")
-    endif(_return_val EQUAL 0)
-endforeach(_var)
+if(NOT OS_IS_WASM)
+    list(APPEND qt_components Concurrent)
+    list(APPEND QT_LIBRARIES Qt::Concurrent)
+endif()
 
-#add_definitions(-DQT_DISABLE_DEPRECATED_BEFORE=0)
+if(OS_IS_LIN)
+    list(APPEND qt_components DBus)
+    list(APPEND QT_LIBRARIES Qt::DBus)
+endif()
+
+if(QT_ADD_WEBSOCKET)
+    list(APPEND qt_components WebSockets)
+    list(APPEND QT_LIBRARIES Qt::WebSockets)
+endif()
+
+find_package(Qt6 6.3 REQUIRED COMPONENTS ${qt_components})
+
+include(QtInstallPaths)
+
+qt_standard_project_setup()

musescore/MuseScore

buildscripts/cmake/SetupQt6.cmake
@@ -1,14 +1,24 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# MuseScore-Studio-CLA-applies
+#
+# MuseScore Studio
+# Music Composition & Notation
+#
+# Copyright (C) 2024 MuseScore Limited
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-include(GetUtilsFunctions) # library of CMake functions ("fn__" namespace)
-
-# Print Qt version or fail the build if Qt (qmake) is not in PATH.
-fn__require_program(QMAKE Qt --version "https://musescore.org/en/handbook/developers-handbook/compilation" qmake6 qmake)
-
-set(CMAKE_AUTOUIC ON)
-set(CMAKE_AUTOMOC ON)
-set(CMAKE_AUTORCC ON)
-
-set(_components
+set(qt_components
     Core
     Gui
     Widgets
@@ -17,82 +27,56 @@ set(_components
     Qml
     Quick
     QuickControls2
-    QuickTemplates2
     QuickWidgets
     Xml
     Svg
     PrintSupport
-    OpenGL
     LinguistTools
 
     Core5Compat
 )
 
-if (NOT OS_IS_WASM)
-    set(_components
-        ${_components}
-        Concurrent
-    )
+set(QT_LIBRARIES
+    Qt::Core
+    Qt::Gui
+    Qt::Widgets
+    Qt::Network
+    Qt::NetworkAuth
+    Qt::Qml
+    Qt::Quick
+    Qt::QuickControls2
+    Qt::QuickWidgets
+    Qt::Xml
+    Qt::Svg
+    Qt::PrintSupport
+
+    Qt::Core5Compat
+)
+
+if(NOT OS_IS_WASM)
+    list(APPEND qt_components Concurrent)
+    list(APPEND QT_LIBRARIES Qt::Concurrent)
 endif()
 
-if (OS_IS_LIN)
-    set(_components
-        ${_components}
-        DBus
-    )
+if(OS_IS_LIN)
+    list(APPEND qt_components DBus)
+    list(APPEND QT_LIBRARIES Qt::DBus)
 endif()
 
 if (QT_ADD_STATEMACHINE)
-    set(_components ${_components}
-        # Note: only used in ExampleView class.
-        # When that class is removed, don't forget to remove this dependency.
-        StateMachine
-    )
+    # Note: only used in ExampleView class.
+    # When that class is removed, don't forget to remove this dependency.
+    list(APPEND qt_components StateMachine)
+    list(APPEND QT_LIBRARIES Qt::StateMachine)
 endif()
 
-if (QT_ADD_WEBSOCKET)
-    set(_components ${_components}
-        WebSockets
-    )
+if(QT_ADD_WEBSOCKET)
+    list(APPEND qt_components WebSockets)
+    list(APPEND QT_LIBRARIES Qt::WebSockets)
 endif()
 
-foreach(_component ${_components})
-    find_package(Qt6${_component} REQUIRED)
-    list(APPEND QT_LIBRARIES ${Qt6${_component}_LIBRARIES})
-    list(APPEND QT_INCLUDES ${Qt6${_component}_INCLUDE_DIRS})
-    add_definitions(${Qt6${_component}_DEFINITIONS})
-endforeach()
+find_package(Qt6 6.3 REQUIRED COMPONENTS ${qt_components})
 
-include_directories(${QT_INCLUDES})
+include(QtInstallPaths)
 
-find_program(QT_QMAKE_EXECUTABLE qmake)
-set(_qmake_vars
-    QT_INSTALL_ARCHDATA
-    QT_INSTALL_BINS
-    QT_INSTALL_CONFIGURATION
-    QT_INSTALL_DATA
-    QT_INSTALL_DOCS
-    QT_INSTALL_EXAMPLES
-    QT_INSTALL_HEADERS
-    QT_INSTALL_IMPORTS
-    QT_INSTALL_LIBEXECS
-    QT_INSTALL_LIBS
-    QT_INSTALL_PLUGINS
-    QT_INSTALL_PREFIX
-    QT_INSTALL_QML
-    QT_INSTALL_TESTS
-    QT_INSTALL_TRANSLATIONS
-)
-foreach(_var ${_qmake_vars})
-    execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} "-query" ${_var}
-        RESULT_VARIABLE _return_val
-        OUTPUT_VARIABLE _out
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-    if(_return_val EQUAL 0)
-        set(${_var} "${_out}")
-    endif(_return_val EQUAL 0)
-endforeach(_var)
-
-#add_definitions(-DQT_DISABLE_DEPRECATED_BEFORE=0)
+qt_standard_project_setup()