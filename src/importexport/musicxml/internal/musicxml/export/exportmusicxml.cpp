eda597de3cec81cc82a331fed094dcf42b92b67e
rettinghaus <klaus.rettinghaus@gmail.com>
2025-03-22 17:19:57 +0100

    prefer switch

--- src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
+++ src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
@@ -742,14 +742,13 @@ static void addColorAttr(const EngravingItem* el, XmlWriter::Attributes& attrs)
 
 static String frame2xml(const TextBase* el)
 {
-    if (el->hasFrame()) {
-        if (el->circle()) {
-            return String(u" enclosure=\"circle\"");
-        } else if (el->square()) {
-            return String(u" enclosure=\"rectangle\"");
-        } else {
-            return String();
-        }
+    switch (el->frameType()) {
+    case FrameType::CIRCLE:
+        return u" enclosure=\"circle\"";
+    case FrameType::SQUARE:
+        return u" enclosure=\"rectangle\"";
+    default:
+        return String();
     }
 }
 
