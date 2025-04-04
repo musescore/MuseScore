d85fa1bebc3032444f96854ec84accbf9e8a4a6f
Roman Pudashkin <r.pudashkin@gmail.com>
2025-04-04 15:07:39 +0300

    render Arpeggio using NoteRenderer

--- src/engraving/playback/renderers/arpeggiorenderer.cpp
+++ src/engraving/playback/renderers/arpeggiorenderer.cpp
@@ -5,7 +5,7 @@
  * MuseScore Studio
  * Music Composition & Notation
  *
- * Copyright (C) 2021 MuseScore Limited
+ * Copyright (C) 2025 MuseScore Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -25,67 +25,13 @@
 #include "dom/chord.h"
 #include "dom/arpeggio.h"
 
-#include "playback/metaparsers/notearticulationsparser.h"
 #include "noterenderer.h"
 
 using namespace mu::engraving;
 using namespace muse;
 using namespace muse::mpe;
 
-const ArticulationTypeSet& ArpeggioRenderer::supportedTypes()
-{
-    static const mpe::ArticulationTypeSet types = {
-        mpe::ArticulationType::Arpeggio, mpe::ArticulationType::ArpeggioDown,
-        mpe::ArticulationType::ArpeggioStraightDown, mpe::ArticulationType::ArpeggioStraightUp,
-        mpe::ArticulationType::ArpeggioUp
-    };
-
-    return types;
-}
-
-void ArpeggioRenderer::doRender(const EngravingItem* item, const mpe::ArticulationType preferredType,
-                                const RenderingContext& context,
-                                mpe::PlaybackEventList& result)
-{
-    const Chord* chord = toChord(item);
-    IF_ASSERT_FAILED(chord) {
-        return;
-    }
-
-    const Arpeggio* arpeggio = chord->arpeggio();
-    IF_ASSERT_FAILED(arpeggio) {
-        return;
-    }
-
-    int stepsCount = static_cast<int>(chord->notes().size());
-    mpe::percentage_t percentageStep = mpe::HUNDRED_PERCENT / stepsCount;
-    usecs_t offsetStep = timestampOffsetStep(context, stepsCount);
-    double stretch = arpeggio->Stretch();
-
-    auto buildEvent = [&](NominalNoteCtx& noteCtx, const int stepNumber) {
-        noteCtx.articulations.updateOccupiedRange(preferredType, stepNumber * percentageStep,
-                                                  (stepNumber + 1) * percentageStep);
-        timestamp_t offset = offsetStep * stepNumber * stretch;
-        noteCtx.timestamp += offset;
-        noteCtx.duration -= offset;
-
-        result.emplace_back(buildNoteEvent(std::move(noteCtx)));
-    };
-
-    std::map<pitch_level_t, NominalNoteCtx> noteCtxMap = arpeggioNotes(chord, context);
-
-    if (isDirectionUp(preferredType)) {
-        for (auto it = noteCtxMap.begin(); it != noteCtxMap.end(); ++it) {
-            buildEvent(it->second, std::distance(noteCtxMap.begin(), it));
-        }
-    } else {
-        for (auto it = noteCtxMap.rbegin(); it != noteCtxMap.rend(); ++it) {
-            buildEvent(it->second, std::distance(noteCtxMap.rbegin(), it));
-        }
-    }
-}
-
-bool ArpeggioRenderer::isDirectionUp(const mpe::ArticulationType type)
+static bool isDirectionUp(const mpe::ArticulationType type)
 {
     switch (type) {
     case mpe::ArticulationType::ArpeggioDown:
@@ -100,32 +46,71 @@ bool ArpeggioRenderer::isDirectionUp(const mpe::ArticulationType type)
     }
 }
 
-usecs_t ArpeggioRenderer::timestampOffsetStep(const RenderingContext& ctx, int stepCount)
+static usecs_t timestampOffsetStep(const RenderingContext& ctx, int stepCount)
 {
     constexpr int MAX_TIMESTAMP_OFFSET_STEP = 60000;
-    int offsetStep = ctx.nominalDuration / stepCount;
+    const int offsetStep = ctx.nominalDuration / stepCount;
 
     return std::min(offsetStep, MAX_TIMESTAMP_OFFSET_STEP);
 }
 
-std::map<pitch_level_t, NominalNoteCtx> ArpeggioRenderer::arpeggioNotes(const Chord* chord, const RenderingContext& ctx)
+static std::map<int /*pitch*/, const Note*> arpeggioNotes(const Chord* chord)
 {
-    std::map<pitch_level_t, NominalNoteCtx> result;
-
+    std::map<int, const Note*> result;
     for (const Note* note : chord->notes()) {
-        if (!NoteRenderer::shouldRender(note, ctx, ctx.commonArticulations)) {
-            continue;
-        }
-
-        NominalNoteCtx noteCtx(note, ctx);
-        if (note->tieForNonPartial()) {
-            noteCtx.duration = tiedNotesTotalDuration(ctx.score, note, noteCtx.duration, ctx.positionTickOffset);
-        }
-
-        NoteArticulationsParser::buildNoteArticulationMap(note, ctx, noteCtx.articulations);
-
-        result.emplace(noteCtx.pitchLevel, std::move(noteCtx));
+        result.emplace(note->pitch(), note);
     }
 
     return result;
+}
+
+const ArticulationTypeSet& ArpeggioRenderer::supportedTypes()
+{
+    static const mpe::ArticulationTypeSet types = {
+        mpe::ArticulationType::Arpeggio, mpe::ArticulationType::ArpeggioDown,
+        mpe::ArticulationType::ArpeggioStraightDown, mpe::ArticulationType::ArpeggioStraightUp,
+        mpe::ArticulationType::ArpeggioUp
+    };
+
+    return types;
+}
+
+void ArpeggioRenderer::doRender(const EngravingItem* item, const mpe::ArticulationType preferredType,
+                                const RenderingContext& ctx, mpe::PlaybackEventList& result)
+{
+    const Chord* chord = toChord(item);
+    IF_ASSERT_FAILED(chord) {
+        return;
+    }
+
+    const Arpeggio* arpeggio = chord->arpeggio();
+    IF_ASSERT_FAILED(arpeggio) {
+        return;
+    }
+
+    const int stepsCount = static_cast<int>(chord->notes().size());
+    const usecs_t offsetStep = timestampOffsetStep(ctx, stepsCount);
+    const double stretch = arpeggio->Stretch();
+
+    auto renderEvent = [&](const Note* note, const int stepNumber) {
+        const timestamp_t offset = offsetStep * stepNumber * stretch;
+
+        RenderingContext noteCtx(ctx);
+        noteCtx.nominalTimestamp += offset;
+        noteCtx.nominalDuration -= offset;
+
+        NoteRenderer::render(note, noteCtx, result);
+    };
+
+    const std::map<int, const Note*> notes = arpeggioNotes(chord);
+
+    if (isDirectionUp(preferredType)) {
+        for (auto it = notes.begin(); it != notes.end(); ++it) {
+            renderEvent(it->second, std::distance(notes.begin(), it));
+        }
+    } else {
+        for (auto it = notes.rbegin(); it != notes.rend(); ++it) {
+            renderEvent(it->second, std::distance(notes.rbegin(), it));
+        }
+    }
 }
