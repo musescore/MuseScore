e5d9398cc70d5e4a844bfbba7468d6b87df00789
James Mizen <j.mizen@mu.se>
2025-04-16 09:44:03 +0100

    Fix tie track2 crash in parts

--- src/engraving/dom/note.cpp
+++ src/engraving/dom/note.cpp
@@ -2377,14 +2377,12 @@ void Note::setTrack(track_idx_t val)
         m_tieFor->setTrack2(val);
         for (SpannerSegment* seg : m_tieFor->spannerSegments()) {
             seg->setTrack(val);
-            seg->setTrack(val);
         }
     }
     if (incomingPartialTie()) {
         m_tieBack->setTrack(val);
         m_tieBack->setTrack2(val);
         for (SpannerSegment* seg : m_tieBack->spannerSegments()) {
-            seg->setTrack(val);
             seg->setTrack(val);
         }
     }
