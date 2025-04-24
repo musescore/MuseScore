613a93b92fcc25c3f4a95209c0eb775fc02c3dd5
rettinghaus <klaus.rettinghaus@gmail.com>
2025-04-24 22:11:21 +0200

    set visibility also on part-name-display

--- src/importexport/musicxml/internal/musicxml/import/importmusicxmlpass1.cpp
+++ src/importexport/musicxml/internal/musicxml/import/importmusicxmlpass1.cpp
@@ -2378,6 +2378,7 @@ void MusicXmlParserPass1::scorePart(const String& curPartGroupName)
             String name = m_e.readText();
             m_parts[id].setName(name);
         } else if (m_e.name() == "part-name-display") {
+            m_parts[id].setPrintName(m_e.asciiAttribute("print-object") != "no");
             String name;
             while (m_e.readNextStartElement()) {
                 if (m_e.name() == "display-text") {
@@ -2400,6 +2401,7 @@ void MusicXmlParserPass1::scorePart(const String& curPartGroupName)
             String name = m_e.readText();
             m_parts[id].setAbbr(name);
         } else if (m_e.name() == "part-abbreviation-display") {
+            m_parts[id].setPrintAbbr(m_e.asciiAttribute("print-object") != "no");
             String name;
             while (m_e.readNextStartElement()) {
                 if (m_e.name() == "display-text") {
