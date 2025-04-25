2ef7a8b98b781a24d967864f31043d2d896e485c
Robert Patterson <robert@robertgpatterson.com>
2025-04-25 18:58:32 -0500

    build out a little further. Now able to read an enigmaxml file and open into the dom.

--- src/importexport/musx/internal/notationmusxreader.h
+++ src/importexport/musx/internal/notationmusxreader.h
@@ -21,15 +21,12 @@
  */
 #pragma once
 
-#include "modularity/imodulesetup.h"
+#include "project/inotationreader.h"
 
 namespace mu::iex::musx {
-class MusxModule : public muse::modularity::IModuleSetup
+class NotationMusxReader : public project::INotationReader
 {
 public:
-
-    std::string moduleName() const override;
-    void resolveImports() override;
+    muse::Ret read(mu::engraving::MasterScore* score, const muse::io::path_t& path, const Options& options = Options()) override;
 };
-
-} // namespace mu::iex::musx
+}
