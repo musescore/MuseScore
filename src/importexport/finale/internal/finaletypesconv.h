5ef7642d61d4827d01c6a887b4ed2fd6e090ef46
XiaoMigros <xiaomigros3@gmail.com>
2025-05-01 09:25:38 +0200

    Move type enum conversion to separate file

---
+++ src/importexport/finale/internal/finaletypesconv.h
@@ -0,0 +1,39 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2025 MuseScore Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "musx/musx.h"
+#include "types/string.h"
+#include "engraving/types/types.h"
+
+namespace mu::iex::finale {
+class FinaleTConv
+{
+public:
+    FinaleTConv() = default;
+
+    static engraving::String instrTemplateIdfromUuid(std::string uuid);
+    static engraving::ClefType toMuseScoreClefType(musx::dom::ClefIndex clef);
+    static engraving::BracketType toMuseScoreBracketType(musx::dom::details::StaffGroup::BracketStyle);    
+};
+
+}
