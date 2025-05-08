75d3c696cf24e9eb4762b9185138e921f00e171d
rettinghaus <klaus.rettinghaus@gmail.com>
2025-05-08 14:27:24 +0200

    extend export of similar symbols

--- src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
+++ src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
@@ -3300,12 +3300,6 @@ static String symIdToTechn(const SymId sid)
         return u"hole";
     case SymId::guitarGolpe:
         return u"golpe";
-    case SymId::guitarClosePedal:
-        return u"stopped smufl=\"guitarClosePedal\"";
-    case SymId::guitarHalfOpenPedal:
-        return u"half-muted smufl=\"guitarHalfOpenPedal\"";
-    case SymId::guitarOpenPedal:
-        return u"open smufl=\"guitarOpenPedal\"";
     case SymId::handbellsBelltree:
         return u"belltree";
     case SymId::handbellsDamp3:
@@ -3330,6 +3324,16 @@ static String symIdToTechn(const SymId sid)
         return u"pluck lift";
     case SymId::handbellsSwing:
         return u"swing";
+    case SymId::guitarClosePedal:
+    case SymId::pictOpenRimShot:
+        return String(u"stopped smufl=\"%1\"").arg(String::fromAscii(SymNames::nameForSymId(sid).ascii()));
+    case SymId::guitarHalfOpenPedal:
+    case SymId::pictHalfOpen1:
+    case SymId::pictHalfOpen2:
+        return String(u"half-muted smufl=\"%1\"").arg(String::fromAscii(SymNames::nameForSymId(sid).ascii()));
+    case SymId::guitarOpenPedal:
+    case SymId::pictOpen:
+        return String(u"open smufl=\"%1\"").arg(String::fromAscii(SymNames::nameForSymId(sid).ascii()));
     default:
         return String(); // nothing
     }
