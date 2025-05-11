645e16429fcf40a7f5796a51c7c5e84a8e22aa2f
bakajikara <102702868+bakajikara@users.noreply.github.com>
2025-05-12 00:35:06 +0900

    Reset properties properly (fixes #15758, #15765, #15771, #22835)

--- src/inspector/models/propertyitem.h
+++ src/inspector/models/propertyitem.h
@@ -77,6 +77,7 @@ signals:
     void isModifiedChanged(bool isModified);
 
     void propertyModified(mu::engraving::Pid propertyId, QVariant newValue);
+    void propertyReset(mu::engraving::Pid propertyId);
     void applyToStyleRequested(mu::engraving::Sid styledId, QVariant newStyleValue);
 
 private:
