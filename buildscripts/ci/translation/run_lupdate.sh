7c2ec72e755b0333db0a9971c88da26b83b09844
Igor <igor.korsukov@gmail.com>
2025-05-14 14:22:34 +0200

    added use_qt69 option to CI

---
+++ buildscripts/ci/translation/run_lupdate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+# MuseScore-Studio-CLA-applies
+#
+# MuseScore Studio
+# Music Composition & Notation
+#
+# Copyright (C) 2021 MuseScore Limited
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+BUILD_TOOLS=$HOME/build_tools
+ENV_FILE=$BUILD_TOOLS/environment.sh
+
+source $ENV_FILE
+
+bash ./tools/translations/run_lupdate.sh $@
