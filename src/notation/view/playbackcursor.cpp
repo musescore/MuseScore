dcc0b67669e6761c67c947b949be574f575325a5
githubwbp1988 <wbp123@yeah.net>
2025-05-17 19:30:38 +0800

    Resolve Issue #14428;  1.When the sheet music is playing, color the currently playing notes; 2.During the sheet music playback phase, the keyboard displays the keys corresponding to the played notes in real time; 3.Distinguish between the keyboard response state during sheet music playback and the keyboard response state during sheet music interaction operations.

--- src/notation/view/playbackcursor.cpp
+++ src/notation/view/playbackcursor.cpp
@@ -1882,7 +1882,6 @@ muse::RectF PlaybackCursor::resolveCursorRectByTick1(muse::midi::tick_t _tick, b
         if (isPlaying) {
             std::vector<EngravingItem*> engravingItemList = s->elist();
             size_t len = engravingItemList.size();
-
             for (size_t i = 0; i < len; i++) {
                 EngravingItem* engravingItem = engravingItemList[i];
                 if (engravingItem == nullptr) {
