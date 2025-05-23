34d6a1f3804166ca7b93d4eef64f5a0c748b0cd9
Michele Spagnolo <m.spagnolo@mu.se>
2025-05-23 17:02:53 +0200

    New lyrics options

--- src/engraving/style/styledef.cpp
+++ src/engraving/style/styledef.cpp
@@ -100,6 +100,8 @@ const std::array<StyleDef::StyleValue, size_t(Sid::STYLES)> StyleDef::styleValue
     styleDef(lyricsMelismaMinLength,                     Spatium(1.0)),
     styleDef(lyricsDashPosAtStartOfSystem,               int(LyricsDashSystemStart::STANDARD)),
     styleDef(lyricsAvoidBarlines,                        true),
+    styleDef(lyricsMaxDashCount,                         0),
+    styleDef(lyricsCenterDashedSyllables,                false),
 
     styleDef(lyricsOddFontFace,                          "Edwin"),
     styleDef(lyricsOddFontSize,                          10.0),
