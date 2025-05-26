fc1ab8287a63883d92fb238a54c44b4fc98dd748
Julian Bühler <juli27@users.noreply.github.com>
2025-05-26 12:41:28 +0200

    Fix properties panel not updating when pasting
    Resolves: #27846
    
    The InspectorListModel listens for selection changes on
    NotationInteraction to update the displayed elements.
    NotationInteraction notifies selection listeners in apply() (indirectly
    via the undo stack). However, since apply() is called before the
    selection is updated in pasteSelection(), no selection update
    notification was sent.
    selectAndStartEditIfNeeded() now notifies listeners, if needed.

--- src/notation/internal/notationinteraction.cpp
+++ src/notation/internal/notationinteraction.cpp
@@ -954,12 +954,12 @@ void NotationInteraction::selectAndStartEditIfNeeded(EngravingItem* element)
 {
     if (element->isSpanner() && !toSpanner(element)->segmentsEmpty()) {
         SpannerSegment* frontSeg = toSpanner(element)->frontSegment();
-        doSelect({ frontSeg }, SelectType::SINGLE);
+        select({ frontSeg }, SelectType::SINGLE);
         if (frontSeg->needStartEditingAfterSelecting()) {
             startEditElement(frontSeg, false);
         }
     } else {
-        doSelect({ element }, SelectType::SINGLE);
+        select({ element }, SelectType::SINGLE);
         if (element->needStartEditingAfterSelecting()) {
             startEditElement(element, false);
         }
