42faf07d9f8b7e7c8944904bd0f82e23fe4c6d52
githubwbp1988 <wbp123@yeah.net>
2025-05-27 19:13:43 +0800

    fix: piano keyboard - handling of tremolo symbols

--- src/notation/internal/notationinteraction.cpp
+++ src/notation/internal/notationinteraction.cpp
@@ -1297,11 +1297,12 @@ muse::async::Notification NotationInteraction::arpeggioTickChanged()
     return m_arpeggioTickChanged;
 }
 
-void NotationInteraction::addTrillNote(mu::engraving::Note* note, int ticks, int duration_ticks) 
+void NotationInteraction::addTrillNote(mu::engraving::Note* note, int ticks, int duration_ticks, int tremolo_type) 
 {
     trill_note = note;
     trill_ticks = ticks;
     trill_duration_ticks = duration_ticks;
+    trill_tremolo_type = tremolo_type;
 }
 int NotationInteraction::trillNoteTicks() const 
 {
@@ -1310,6 +1311,10 @@ int NotationInteraction::trillNoteTicks() const
 int NotationInteraction::trillNoteDurationticks() const 
 {
     return trill_duration_ticks;
+}
+int NotationInteraction::trillNoteTremolotype() const 
+{
+    return trill_tremolo_type;
 }
 int NotationInteraction::trillCurrticks() const 
 {
@@ -1333,6 +1338,7 @@ bool NotationInteraction::trillTick(int ticks)
         trill_curr_ticks = 0;
         trill_ticks = 0;
         trill_duration_ticks = 0;
+        trill_tremolo_type = 0;
         trill_note = nullptr;
         m_trillTickChanged.notify();
         return true;
