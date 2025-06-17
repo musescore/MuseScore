741a4d29e82ce63d3c04fcbe6fe4450268d8c3ac
Igor <igor.korsukov@gmail.com>
2025-06-17 14:48:14 +0200

    added load score file to web api

--- src/webbridge/distr/muapi.js
+++ src/webbridge/distr/muapi.js
@@ -2,17 +2,9 @@ import MuImpl from "./muimpl.js"
 
 const MuApi = {
 
-    onclickTest1: function() {
-        this.Module.ccall('onclickTest1', '', ['number'], [42])
-    },
-
-    load: function(data) {
-        console.log("[js] load len: ", data.length)
-        const ptr = this.Module._malloc(data.length);
-        this.Module.HEAPU8.set(data, ptr);
-        this.Module._load(ptr, data.length);
-        this.Module._free(ptr);
-    },
+    // Load score
+    loadScoreFile: MuImpl.loadScoreFile,
+    loadScoreData: MuImpl.loadScoreData,
 }
 
 async function createMuApi(config) {
