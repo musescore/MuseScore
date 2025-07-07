0513d88788c50ab44f7d8819fac9b6dcf0276d6d
Calum Matheson <mathesoncalum@gmail.com>
2025-07-07 09:39:11 +0100

    Tweaks to update scenarios (onDelayedInit -> checkForUpdate)

--- src/musesounds/musesoundsmodule.cpp
+++ src/musesounds/musesoundsmodule.cpp
@@ -92,5 +92,5 @@ void MuseSoundsModule::onInit(const IApplication::RunMode& mode)
 
 void MuseSoundsModule::onDelayedInit()
 {
-    m_museSoundsCheckUpdateScenario->delayedInit();
+    m_museSoundsCheckUpdateScenario->checkForUpdate(/*manual*/ false);
 }
