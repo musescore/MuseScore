aef0bc5d9d498d4c1ddd19344c0bcf3ee9b0f51e
XiaoMigros <xiaomigros3@gmail.com>
2025-07-24 05:03:44 +0200

    import instruments, brackets and measures

--- src/importexport/finale/internal/importfinaleparser.h
+++ src/importexport/finale/internal/importfinaleparser.h
@@ -42,12 +42,73 @@ class Staff;
 }
 
 namespace mu::iex::finale {
+
+enum class HeaderFooterType {
+    None,
+    FirstPage,
+    SecondPageToEnd
+};
+
+struct FontTracker
+{
+    FontTracker() = default;
+    FontTracker(const engraving::String& name, double size, engraving::FontStyle styles = engraving::FontStyle::Normal, bool spatiumInd = false)
+        : fontName(name), fontSize(size), fontStyle(styles), spatiumIndependent(spatiumInd) {}
+    FontTracker(const std::shared_ptr<const musx::dom::FontInfo>& fontInfo, double additionalSizeScaling = 1.0);
+    FontTracker(const engraving::MStyle& style, const engraving::String& sidNamePrefix);
+
+    engraving::String fontName;
+    double fontSize = 0.0;
+    engraving::FontStyle fontStyle = engraving::FontStyle::Normal;
+    bool spatiumIndependent = false;
+};
+
+struct EnigmaParsingOptions
+{
+    EnigmaParsingOptions() = default;
+    EnigmaParsingOptions(HeaderFooterType hf) : hfType(hf)  {};
+
+    HeaderFooterType hfType = HeaderFooterType::None;
+    double scaleFontSizeBy = 1.0;
+    std::optional<FontTracker> initialFont;
+};
 class FinaleParser : public muse::Injectable
 {
 public:
+    muse::Inject<mu::engraving::IEngravingFontsProvider> engravingFonts = { this };
+
     FinaleParser(engraving::Score* score, const std::shared_ptr<musx::dom::Document>& doc, FinaleLoggerPtr& logger);
 
     void parse();
+
+    // Document
+    const engraving::Score* score() const { return m_score; }
+    std::shared_ptr<musx::dom::Document> musxDocument() const { return m_doc; }
+    const FinaleOptions& musxOptions() const { return m_finaleOptions; }
+    musx::dom::Cmper currentMusxPartId() const { return m_currentMusxPartId; }
+
+    // Text
+    engraving::String stringFromEnigmaText(const musx::util::EnigmaParsingContext& parsingContext, const EnigmaParsingOptions& options = {}, FontTracker* firstFontInfo = nullptr) const;
+    bool fontIsEngravingFont(const std::string& fontName) const;
+    bool fontIsEngravingFont(const std::shared_ptr<const musx::dom::FontInfo>& fontInfo) const { return fontIsEngravingFont(fontInfo->getName()); }
+    bool fontIsEngravingFont(const engraving::String& fontName) const { return fontIsEngravingFont(fontName.toStdString()); }
+
+    FinaleLoggerPtr logger() const { return m_logger; }
+
+private:
+    // scoremap
+    void importParts();
+    void importBrackets();
+    void importMeasures();
+    engraving::Score* m_score;
+    const std::shared_ptr<musx::dom::Document> m_doc;
+    FinaleLoggerPtr m_logger;
+    const musx::dom::Cmper m_currentMusxPartId = musx::dom::SCORE_PARTID; // eventually this may be changed per excerpt/linked part
+
+    std::unordered_map<engraving::staff_idx_t, musx::dom::InstCmper> m_staff2Inst;
+    std::unordered_map<musx::dom::InstCmper, engraving::staff_idx_t> m_inst2Staff;
+    std::unordered_map<musx::dom::MeasCmper, engraving::Fraction> m_meas2Tick;
+    std::map<engraving::Fraction, musx::dom::MeasCmper> m_tick2Meas; // use std::map to avoid need for Fraction hash function
 };
 
 }
