bca7edf371bc4ce151985ef31b14f766ca037f3c
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2025-07-30 02:14:53 +0200

    Fix non-unity build

--- src/importexport/capella/internal/capella.h
+++ src/importexport/capella/internal/capella.h
@@ -586,7 +586,7 @@ public:
     bool tupletStart  = false;       // To correctly read Tuplets with mixed durations
     bool tupletEnd    = false;       // we infer from the Capella file start/stop from the brackets.
     int tupletCount  = 0;            // Real count of the tuplet notes ...
-    Fraction tupletTicks;            // ... and the ticks of the tuplet, used to calculate the end of the tuplet
+    engraving::Fraction tupletTicks; // ... and the ticks of the tuplet, used to calculate the end of the tuplet
     bool tripartite;
     bool isProlonging;
 
