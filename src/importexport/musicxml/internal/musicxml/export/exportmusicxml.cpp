7bfe9581b4376d3da22adb4a4d4ac34de62cc7f4
rettinghaus <klaus.rettinghaus@gmail.com>
2025-07-30 23:27:10 +0200

    fix export of percussion clefs

--- src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
+++ src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
@@ -2602,7 +2602,7 @@ void ExportMusicXml::clef(staff_idx_t staff, const ClefType ct, const String& ex
 
     int line = ClefInfo::line(ct);
     m_xml.tag("sign", info.sign);
-    if (info.sign != "TAB") {
+    if ((std::string_view(info.sign) != "percussion") && (std::string_view(info.sign) != "TAB")) {
         m_xml.tag("line", line);
     }
     if (info.octChng) {
