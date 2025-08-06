d80ae66f652d2cf056613902659179bd4129ee72
Zac Jansheski <69917893+zacjansheski@users.noreply.github.com>
2025-08-06 13:35:27 -0400

    Use GM Metronome Pitches
    Use GM Metronome Pitches for the metronome

--- src/engraving/playback/playbackeventsrenderer.cpp
+++ src/engraving/playback/playbackeventsrenderer.cpp
@@ -73,8 +73,8 @@ static muse::mpe::NoteEvent buildMetronomeEvent(const TimeSigFrac& timeSig, cons
     duration_t duration = durationFromTempoAndTicks(bps, ticksPerBeat);
 
     pitch_level_t eventPitchLevel = beatType == BeatType::DOWNBEAT
-                                    ? pitchLevel(PitchClass::E, 5) // high wood block
-                                    : pitchLevel(PitchClass::F, 5); // low wood block
+                                    ? pitchLevel(PitchClass::A, 1) // high met
+                                    : pitchLevel(PitchClass::G_sharp, 1); // low met
 
     const ArticulationMap articulations = makeStandardArticulationMap(profile, actualTimestamp, duration);
 
