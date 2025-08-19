9634da6b8e71072978d6b042bc000a45e23dd1c1
Michele Spagnolo <m.spagnolo@mu.se>
2025-08-19 13:36:16 +0200

    Fix HOPO should replace slur if entered on slur selected

--- src/notation/internal/notationinteraction.cpp
+++ src/notation/internal/notationinteraction.cpp
@@ -2441,7 +2441,7 @@ void NotationInteraction::applyPaletteElementToList(EngravingItem* element, bool
         return;
     }
 
-    if (element->isSlur() && addSingle) {
+    if (element->isSlur()) {
         doAddSlur(toSlur(element));
         return;
     }
