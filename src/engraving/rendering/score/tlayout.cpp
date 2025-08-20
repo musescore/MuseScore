5b12f6d6e4fc166f32a0f3d9715953ed4298f70c
Michele Spagnolo <m.spagnolo@mu.se>
2025-08-20 18:23:54 +0200

    correction

--- src/engraving/rendering/score/tlayout.cpp
+++ src/engraving/rendering/score/tlayout.cpp
@@ -3313,7 +3313,7 @@ void TLayout::layoutHammerOnPullOffSegment(HammerOnPullOffSegment* item, LayoutC
         bool above = hopoText->placeAbove();
 
         Align align;
-        align.vertical = above ? AlignV::BASELINE : AlignV::TOP;
+        align.vertical = AlignV::BASELINE;
         align.horizontal = AlignH::HCENTER;
         hopoText->setAlign(align);
         layoutItem(hopoText, ctx);
