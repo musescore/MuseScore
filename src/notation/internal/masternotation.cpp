9c4d462f55d59c523ae61f4a927b32f06ad07e2c
Calum Matheson <47119327+mathesoncalum@users.noreply.github.com>
2025-08-21 16:52:13 +0100

    Merge pull request #29237 from RomanPudashkin/restore_solo_mute_state
    Fix #27058: Mute settings are broken in parts whose instruments were deleted and then restored with Undo

--- src/notation/internal/masternotation.cpp
+++ src/notation/internal/masternotation.cpp
@@ -696,6 +696,8 @@ void MasterNotation::updateExcerpts()
             excerptNotation->notation()->elements()->msScore()->doLayout();
         }
 
+        initNotationSoloMuteState(excerptNotation->notation());
+
         updatedExcerpts.push_back(excerptNotation);
     }
 
