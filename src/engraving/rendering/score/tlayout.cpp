ca16d6fa2c2289b1df4eebe2a359a3fcc6e2d4aa
Michele Spagnolo <m.spagnolo@mu.se>
2025-08-22 12:41:38 +0200

    Fix slight bend spacing in TAB

--- src/engraving/rendering/score/tlayout.cpp
+++ src/engraving/rendering/score/tlayout.cpp
@@ -4399,7 +4399,7 @@ void TLayout::fillNoteShape(const Note* item, Note::LayoutData* ldata)
 
     // This method is also called from SingleLayout, where `part` may be nullptr
     Part* part = item->part();
-    if (part && part->instrument()->hasStrings() && !item->staffType()->isTabStaff()) {
+    if (part && part->instrument()->hasStrings()) {
         GuitarBend* bend = item->bendFor();
         if (bend && bend->addToSkyline() && bend->type() == GuitarBendType::SLIGHT_BEND && !bend->segmentsEmpty()) {
             GuitarBendSegment* bendSeg = toGuitarBendSegment(bend->frontSegment());
