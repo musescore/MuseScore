ee9aa2dd94880c9df6c3ee44c2c5f1e48e04b333
Nathan Reed <nathaniel.reed@gmail.com>
2025-08-25 09:17:21 -0700

    Fix #27317: color inversion incorrectly affecting palettes
    - Cherry-pick of #27642 for the 4.6.0 branch

--- src/engraving/dom/engravingitem.cpp
+++ src/engraving/dom/engravingitem.cpp
@@ -2430,7 +2430,7 @@ bool EngravingItem::colorsInversionEnabled() const
     return m_colorsInversionEnabled;
 }
 
-void EngravingItem::setColorsInverionEnabled(bool enabled)
+void EngravingItem::setColorsInversionEnabled(bool enabled)
 {
     m_colorsInversionEnabled = enabled;
 }
