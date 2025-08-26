6b843d9dac52ca286028f04cd89ac17f261b6d7c
Michele Spagnolo <m.spagnolo@mu.se>
2025-08-26 14:41:35 +0200

    Reduce number of calls to Score::rebuildFretBox
    All of these calls will pass by Segment::add and Segment::remove anyway, so we can just call it there.

--- src/engraving/dom/edit.cpp
+++ src/engraving/dom/edit.cpp
@@ -7057,10 +7057,6 @@ void Score::undoAddElement(EngravingItem* element, bool addToLinkedStaves, bool 
                     }
                 }
             }
-
-            if (element->isHarmony() || element->isFretDiagram()) {
-                element->score()->rebuildFretBox();
-            }
         } else if (element->isSlur()
                    || element->isHairpin()
                    || element->isOttava()
@@ -7565,10 +7561,6 @@ void Score::undoRemoveElement(EngravingItem* element, bool removeLinked)
             }
             if (e->explicitParent() && e->explicitParent()->isSystem()) {
                 e->setParent(0);   // systems will be regenerated upon redo, so detach
-            }
-
-            if (e->isHarmony() || e->isFretDiagram()) {
-                e->score()->rebuildFretBox();
             }
         }
     }
