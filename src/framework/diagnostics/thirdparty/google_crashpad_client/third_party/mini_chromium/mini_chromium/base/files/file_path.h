19b2ec42509e862d0b8b70f9ca0f1af4b3d3611f
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2025-09-04 15:40:40 +0200

    Fix compilation of google_crashpad_client with C++20
    See https://chromium.googlesource.com/chromium/mini_chromium/+/707c87bd258dcb8c19be8a69e92efae9fd1b51ad%5E%21/
    
    ('Stolen' from #28014, commit 3)

--- src/framework/diagnostics/thirdparty/google_crashpad_client/third_party/mini_chromium/mini_chromium/base/files/file_path.h
+++ src/framework/diagnostics/thirdparty/google_crashpad_client/third_party/mini_chromium/mini_chromium/base/files/file_path.h
@@ -117,6 +117,7 @@
 #if defined(OS_WIN)
 #define FILE_PATH_USES_DRIVE_LETTERS
 #define FILE_PATH_USES_WIN_SEPARATORS
+#define FILE_PATH_USES_WIDE_CHARACTERS
 #endif  // OS_WIN
 
 namespace base {
@@ -226,8 +227,11 @@ class FilePath {
 
 }  // namespace base
 
-// This is required by googletest to print a readable output on test failures.
-extern void PrintTo(const base::FilePath& path, std::ostream* out);
+// Streams `file_path`'s value to a byte stream, converting from wide
+// characters if called for. (also lets googletest print a readable output on
+// test failures)
+extern std::ostream& operator<<(std::ostream& os,
+                                const base::FilePath& file_path);
 
 // Macros for string literal initialization of FilePath::CharType[], and for
 // using a FilePath::CharType[] in a printf-style format string.
