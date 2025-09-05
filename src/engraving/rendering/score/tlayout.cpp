4af228bce16c7082352ff4634c8beb7d6aed9d0c
Roman Pudashkin <r.pudashkin@gmail.com>
2025-09-05 17:45:34 +0300

    fix #28692: update ticks for GuitarBend, Glissando, NoteLine during layout
    These spanners are attached to notes and are not added to the spanners map
    so their ticks won't be auto updated in Score::undoInsertTime()

--- src/engraving/rendering/score/tlayout.cpp
+++ src/engraving/rendering/score/tlayout.cpp
@@ -2853,6 +2853,15 @@ void TLayout::layoutFretDiagram(const FretDiagram* item, FretDiagram::LayoutData
 void TLayout::layoutGlissando(Glissando* item, LayoutContext& ctx)
 {
     LAYOUT_CALL_ITEM(item);
+
+    if (item->startElement()) {
+        item->setTick(item->startElement()->tick());
+    }
+
+    if (item->endElement()) {
+        item->setTick2(item->endElement()->tick());
+    }
+
     TLayout::layoutLine(const_cast<Glissando*>(item), ctx);
 
     if (item->spannerSegments().empty()) {
@@ -5185,6 +5194,15 @@ void TLayout::layoutNoteAnchoredLine(SLine* item, EngravingItem::LayoutData* lda
 void TLayout::layoutNoteLine(NoteLine* item, LayoutContext& ctx)
 {
     LAYOUT_CALL_ITEM(item);
+
+    if (item->startElement()) {
+        item->setTick(item->startElement()->tick());
+    }
+
+    if (item->endElement()) {
+        item->setTick2(item->endElement()->tick());
+    }
+
     TLayout::layoutLine(item, ctx);
 
     if (item->lineEndPlacement() == NoteLineEndPlacement::OFFSET_ENDS) {
