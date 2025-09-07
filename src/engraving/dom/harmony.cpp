4bcf44aa6c9db1ada4f7c8db2dad6c1e6791df1f
rettinghaus <klaus.rettinghaus@gmail.com>
2025-09-07 12:19:51 +0200

    add fallthrough

--- src/engraving/dom/harmony.cpp
+++ src/engraving/dom/harmony.cpp
@@ -1487,6 +1487,7 @@ bool Harmony::setProperty(Pid pid, const PropertyValue& v)
             }
             score()->rebuildFretBox();
         }
+        [[fallthrough]];
     }
     default:
         return TextBase::setProperty(pid, v);
