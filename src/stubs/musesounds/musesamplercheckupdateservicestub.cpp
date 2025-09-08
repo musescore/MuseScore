a6c2ebe5d7f747137c6b721a4c2b4a532a785d04
Calum Matheson <mathesoncalum@gmail.com>
2025-09-08 18:16:08 +0100

    Add stubs for MuseSampler update service/scenario

---
+++ src/stubs/musesounds/musesamplercheckupdateservicestub.cpp
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2025 MuseScore BVBA and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "musesamplercheckupdateservicestub.h"
+
+using namespace mu::musesounds;
+
+bool MuseSamplerCheckUpdateServiceStub::canCheckForUpdate() const
+{
+    return false;
+}
+
+bool MuseSamplerCheckUpdateServiceStub::incompatibleLocalVersion() const
+{
+    return false;
+}
+
+muse::async::Promise<muse::RetVal<bool> > MuseSamplerCheckUpdateServiceStub::checkForUpdate()
+{
+    return muse::async::Promise<muse::RetVal<bool> >([](auto, auto) {
+        return muse::async::Promise<muse::RetVal<bool> >::dummy_result();
+    });
+}
