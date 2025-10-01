27d8eb9d3698e8ac9eebf2f6544809eff11deb27
Julian Bühler <juli27@users.noreply.github.com>
2025-10-01 23:49:04 +0200

    ci: Use builtin curl instead of wget on Windows

--- buildscripts/ci/windows/setup.bat
+++ buildscripts/ci/windows/setup.bat
@@ -26,9 +26,6 @@ IF %TARGET_PROCESSOR_BITS% == 32 (
 where /q git
 IF ERRORLEVEL 1 ( choco install -y git.install )
 
-where /q wget
-IF ERRORLEVEL 1 ( choco install -y wget )
-
 where /q 7z
 IF ERRORLEVEL 1 ( choco install -y 7zip.install )
 
@@ -38,23 +35,23 @@ MKDIR %TEMP_DIR%
 
 :: Install dependencies
 ECHO "=== Install dependencies ==="
-CALL "wget.exe" -q --show-progress --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/musescore_dependencies_win32.7z" -O %TEMP_DIR%\musescore_dependencies_win32.7z
+CALL "curl.exe" -f -o %TEMP_DIR%\musescore_dependencies_win32.7z "https://s3.amazonaws.com/utils.musescore.org/musescore_dependencies_win32.7z"
 CALL "7z" x -y %TEMP_DIR%\musescore_dependencies_win32.7z "-o%TEMP_DIR%\musescore_dependencies_win32"
 SET JACK_DIR="C:\Program Files (x86)\Jack"
 XCOPY %TEMP_DIR%\musescore_dependencies_win32\dependencies\Jack %JACK_DIR% /E /I /Y
-SET PATH=%JACK_DIR%;%PATH% 
+SET PATH=%JACK_DIR%;%PATH%
 
-CALL "wget.exe" -q --show-progress --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/dependencies.7z" -O  %TEMP_DIR%\dependencies.7z
+CALL "curl.exe" -f -o %TEMP_DIR%\dependencies.7z "https://s3.amazonaws.com/utils.musescore.org/dependencies.7z"
 CALL "7z" x -y %TEMP_DIR%\dependencies.7z "-oC:\musescore_dependencies"
 
 IF %BUILD_WIN_PORTABLE% == ON (
 ECHO "=== Installing PortableApps.com Tools ==="
 :: portableappslauncher is a vanilla installation of PortableApps.com Launcher https://portableapps.com/apps/development/portableapps.com_launcher
-CALL "wget.exe" --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/portableappslauncher.zip" -O %TEMP_DIR%\portableappslauncher.zip
+CALL "curl.exe" -f -o %TEMP_DIR%\portableappslauncher.zip "https://s3.amazonaws.com/utils.musescore.org/portableappslauncher.zip"
 CALL "7z" x -y %TEMP_DIR%\portableappslauncher.zip "-oC:\portableappslauncher"
 
 :: portableappsinstaller is a vanilla installation of PortableApps.com Installer https://portableapps.com/apps/development/portableapps.com_installer
-CALL "wget.exe" --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/portableappsinstaller.zip" -O %TEMP_DIR%\portableappsinstaller.zip
+CALL "curl.exe" -f -o %TEMP_DIR%\portableappsinstaller.zip "https://s3.amazonaws.com/utils.musescore.org/portableappsinstaller.zip"
 CALL "7z" x -y %TEMP_DIR%\portableappsinstaller.zip "-oC:\portableappsinstaller"
 )
 
