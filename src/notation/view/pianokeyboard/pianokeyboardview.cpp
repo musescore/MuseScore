8d000f6d46d15ae81a67b13e041af1d75dcdeedb
SilverGreen93 <mihai.vasiliu.93@gmail.com>
2025-10-08 17:31:22 +0300

    Show localized Piano Keyboard key names
    The C piano keys in the Piano Keyboard panel are always shown in standard pitch notation.
    
    Display the notes with respect to locale by using the strings from `EditPitchBase`.

--- src/notation/view/pianokeyboard/pianokeyboardview.cpp
+++ src/notation/view/pianokeyboard/pianokeyboardview.cpp
@@ -266,7 +266,8 @@ void PianoKeyboardView::paintWhiteKeys(QPainter* painter, const QRectF& viewport
 
             int octaveNumber = (key / 12) - 1;
 
-            QString octaveLabel = "C" + QString::number(octaveNumber);
+            QString octaveLabel = "C " + QString::number(octaveNumber);
+            octaveLabel = muse::qtrc("EditPitchBase", octaveLabel.toStdString().c_str());
             QRect octaveLabelRect(left, top, right - left, bottom - top - bottomOffset);
 
             painter->setPen(backgroundColor);
