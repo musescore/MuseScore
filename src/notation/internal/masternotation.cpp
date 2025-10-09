c16196952745d14be7d87edfdee4714da0359059
Joachim Schmitz <jojo@schmitz-digital.de>
2025-10-09 19:29:01 +0200

    Fix GH#15372: Don't ignore page settings from templates

--- src/notation/internal/masternotation.cpp
+++ src/notation/internal/masternotation.cpp
@@ -25,6 +25,7 @@
 #include <QFileInfo>
 
 #include "log.h"
+#include "realfn.h"
 #include "translation.h"
 
 #include "engraving/style/defaultstyle.h"
@@ -301,10 +302,8 @@ void MasterNotation::applyOptions(mu::engraving::MasterScore* score, const Score
         clearMeasures(score);
 
         // for templates using built-in base page style, set score page style to default (may be user-defined)
-        bool isBaseWidth = (std::abs(score->style().styleD(Sid::pageWidth) - DefaultStyle::baseStyle().styleD(Sid::pageWidth)) < 0.1);
-        bool isBaseHeight = (std::abs(score->style().styleD(Sid::pageHeight) - DefaultStyle::baseStyle().styleD(Sid::pageHeight)) < 0.1);
-        if (isBaseWidth && isBaseHeight) {
-            for (auto st : pageStyles()) {
+        for (auto st : pageStyles()) {
+            if (st != Sid::pageTwosided && RealIsEqual(score->style().styleD(st), DefaultStyle::baseStyle().styleD(st))) {
                 score->style().set(st, DefaultStyle::defaultStyle().value(st));
             }
         }
