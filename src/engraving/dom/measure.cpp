908038ff3c451842d3fbf80250cf71076068b49c
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2025-10-09 23:18:26 +0200

    Don't mask bar lines for inactive measure numbers
    Follow-up to 97bbdd8be9732992937f56753b617ad945663162
    
    Resolves: https://github.com/musescore/MuseScore/issues/30429

--- src/engraving/dom/measure.cpp
+++ src/engraving/dom/measure.cpp
@@ -557,7 +557,7 @@ double Measure::tick2pos(Fraction tck) const
 ///    Whether the measure will show measure number(s) when MeasureNumberMode is set to AUTO
 //---------------------------------------------------------
 
-bool Measure::showMeasureNumberInAutoMode()
+bool Measure::showMeasureNumberInAutoMode() const
 {
     // Check whether any measure number should be shown
     if (!style().styleB(Sid::showMeasureNumber)) {
@@ -598,7 +598,7 @@ bool Measure::showMeasureNumberInAutoMode()
     }
 }
 
-bool Measure::showMeasureNumberOnStaff(staff_idx_t staffIdx)
+bool Measure::showMeasureNumberOnStaff(staff_idx_t staffIdx) const
 {
     IF_ASSERT_FAILED(staffIdx < score()->nstaves()) {
         return false;
@@ -612,7 +612,7 @@ bool Measure::showMeasureNumberOnStaff(staff_idx_t staffIdx)
 ///     Whether the Measure shows a MeasureNumber
 //---------------------------------------------------------
 
-bool Measure::showMeasureNumber()
+bool Measure::showMeasureNumber() const
 {
     switch (m_measureNumberMode) {
     case MeasureNumberMode::AUTO:
