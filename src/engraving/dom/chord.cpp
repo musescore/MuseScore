7a5318125ab87e916181c6ae2b0a0ff9bbcd8433
Michele Spagnolo <m.spagnolo@mu.se>
2025-10-13 14:57:52 +0200

    Different solution

--- src/engraving/dom/chord.cpp
+++ src/engraving/dom/chord.cpp
@@ -841,6 +841,23 @@ bool Chord::allNotesTiedToNext() const
     return true;
 }
 
+bool Chord::allElementsInvisible() const
+{
+    for (EngravingObject* child : scanChildren()) {
+        if (toEngravingItem(child)->visible()) {
+            return false;
+        }
+    }
+
+    for (Chord* grace : m_graceNotes) {
+        if (!grace->allElementsInvisible()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 //---------------------------------------------------------
 //   processSiblings
 //---------------------------------------------------------
