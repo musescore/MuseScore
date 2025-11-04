5a43eab90a24d2505227d8cc7328c4bdb350df62
unknown <r.pudashkin@gmail.com>
2025-11-04 10:04:53 +0200

    use Qt’s built-in transfer timeout API

--- src/framework/network/internal/networkmanager.h
+++ src/framework/network/internal/networkmanager.h
@@ -76,12 +76,10 @@ private:
     bool openDevice(QIODevice* device, QIODevice::OpenModeFlag flags);
     void closeDevice(QIODevice* device);
 
-    bool isAborted() const;
-
     void prepareReplyReceive(QNetworkReply* reply, IncomingDevice* incomingData);
     void prepareReplyTransmit(QNetworkReply* reply);
 
-    Ret waitForReplyFinished(QNetworkReply* reply, int timeoutMs);
+    Ret waitForReplyFinished(QNetworkReply* reply);
     Ret errorFromReply(const QNetworkReply* reply) const;
 
     static String requestTypeToString(RequestType type);
@@ -90,8 +88,6 @@ private:
     IncomingDevice* m_incomingData = nullptr;
     QNetworkReply* m_reply = nullptr;
     Progress m_progress;
-
-    bool m_isAborted = false;
 };
 }
 
