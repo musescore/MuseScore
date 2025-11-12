musescore/MuseScore

commit eecf566ab824809d4969be19ccc2420600b9735c

fix: dead notes for negative frets config (#29020)

src/engraving/rendering/score/tdraw.cpp
@@ -2194,8 +2194,6 @@ void TDraw::draw(const Note* item, Painter* painter, const PaintOptions& opt)
 
     bool negativeFret = item->negativeFretUsed() && item->staff()->isTabStaff(item->tick());
 
-    Color c(negativeFret ? config->criticalColor() : item->curColor(opt));
-    painter->setPen(c);
     bool tablature = item->staff() && item->staff()->isTabStaff(item->chord()->tick());
 
     // tablature
@@ -2217,7 +2215,8 @@ void TDraw::draw(const Note* item, Painter* painter, const PaintOptions& opt)
         Font f(tab->fretFont());
         f.setPointSizeF(f.pointSizeF() * item->magS());
         painter->setFont(f);
-        painter->setPen(c);
+        bool useCritical = negativeFret && !item->deadNote() && !item->score()->printing();
+        painter->setPen(useCritical ? config->criticalColor() : item->curColor(opt));
         double startPosX = ldata->bbox().x();
 
         double yOffset = tab->fretFontYOffset();

src/engraving/rendering/score/tlayout.cpp
@@ -4050,7 +4050,10 @@ void TLayout::layoutNote(const Note* item, Note::LayoutData* ldata)
             const_cast<Note*>(item)->setFretString(tab->fretString(std::fabs(item->fret()), item->string(), item->deadNote()));
 
             if (item->negativeFretUsed()) {
-                const_cast<Note*>(item)->setFretString(u"-" + item->fretString());
+                auto note = toNote(item);
+                if (!note->deadNote()) {
+                    const_cast<Note*>(item)->setFretString(u"-" + item->fretString());
+                }
             }
 
             if (item->displayFret() == Note::DisplayFretOption::ArtificialHarmonic) {

commit 979c4e7337d17f189b6feff60403a1646ec07a89

Fix note select color
Broken by eecf566ab824809d4969be19ccc2420600b9735c; the color would not be set anymore for non-tab staves

Plus a small cleanup in tlayout: `toNote(item)` makes no sense because `item` is already `Note*`

src/engraving/rendering/score/tdraw.cpp
@@ -2190,21 +2190,24 @@ void TDraw::draw(const Note* item, Painter* painter, const PaintOptions& opt)
 
     const Note::LayoutData* ldata = item->ldata();
 
-    auto config = item->configuration();
+    const auto config = item->configuration();
+    const StaffType* staffType = item->staff() ? item->staff()->staffTypeForElement(item) : nullptr;
 
-    bool negativeFret = item->negativeFretUsed() && item->staff()->isTabStaff(item->tick());
+    const bool isTabStaff = staffType && staffType->isTabStaff();
+    const bool negativeFret = isTabStaff && item->negativeFretUsed();
+    const bool useCriticalColor = negativeFret && !item->deadNote() && opt.isPrinting;
 
-    bool tablature = item->staff() && item->staff()->isTabStaff(item->chord()->tick());
+    painter->setPen(useCriticalColor ? config->criticalColor() : item->curColor(opt));
 
     // tablature
-    if (tablature) {
+    if (isTabStaff) {
         if (item->displayFret() == Note::DisplayFretOption::Hide || item->shouldHideFret()) {
             return;
         }
         const Staff* st = item->staff();
         const StaffType* tab = st->staffTypeForElement(item);
 
-        if (negativeFret || (item->fretConflict() && !opt.isPrinting && item->score()->showUnprintable())) {                    // fret conflict
+        if (negativeFret || (item->fretConflict() && !opt.isPrinting && item->score()->showUnprintable())) { // fret conflict
             painter->save();
             painter->setPen(config->criticalColor());
             painter->setBrush(config->criticalBackgroundColor());
@@ -2215,11 +2218,9 @@ void TDraw::draw(const Note* item, Painter* painter, const PaintOptions& opt)
         Font f(tab->fretFont());
         f.setPointSizeF(f.pointSizeF() * item->magS());
         painter->setFont(f);
-        bool useCritical = negativeFret && !item->deadNote() && !item->score()->printing();
-        painter->setPen(useCritical ? config->criticalColor() : item->curColor(opt));
-        double startPosX = ldata->bbox().x();
 
-        double yOffset = tab->fretFontYOffset();
+        const double startPosX = ldata->bbox().x();
+        const double yOffset = tab->fretFontYOffset();
         painter->drawText(PointF(startPosX, yOffset * item->magS()), item->fretString());
     }
     // NOT tablature
@@ -2235,8 +2236,7 @@ void TDraw::draw(const Note* item, Painter* painter, const PaintOptions& opt)
             const Instrument* in = item->part()->instrument(item->chord()->tick());
             int i = item->ppitch();
             if (i < in->minPitchP() || i > in->maxPitchP()) {
-                painter->setPen(
-                    item->selected() ? config->criticalSelectedColor() : config->criticalColor());
+                painter->setPen(item->selected() ? config->criticalSelectedColor() : config->criticalColor());
             } else if (i < in->minPitchA() || i > in->maxPitchA()) {
                 painter->setPen(item->selected() ? config->warningSelectedColor() : config->warningColor());
             }

src/engraving/rendering/score/tlayout.cpp
@@ -4049,11 +4049,8 @@ void TLayout::layoutNote(const Note* item, Note::LayoutData* ldata)
         } else {
             const_cast<Note*>(item)->setFretString(tab->fretString(std::fabs(item->fret()), item->string(), item->deadNote()));
 
-            if (item->negativeFretUsed()) {
-                auto note = toNote(item);
-                if (!note->deadNote()) {
-                    const_cast<Note*>(item)->setFretString(u"-" + item->fretString());
-                }
+            if (item->negativeFretUsed() && !item->deadNote()) {
+                const_cast<Note*>(item)->setFretString(u"-" + item->fretString());
             }
 
             if (item->displayFret() == Note::DisplayFretOption::ArtificialHarmonic) {