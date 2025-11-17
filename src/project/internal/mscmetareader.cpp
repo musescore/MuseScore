musescore/MuseScore

commit 55a5118364f2813e3ff7763dc81bfc9208ba7020

Replace usage of XmlReader & XmlWriter
Use XmlStreamReader or XmlStreamWriter instead

src/project/internal/mscmetareader.cpp
@@ -21,12 +21,9 @@
  */
 #include "mscmetareader.h"
 
-#include <sstream>
+#include "global/io/buffer.h"
+#include "global/serialization/xmlstreamreader.h"
 
-#include "io/buffer.h"
-
-#include "stringutils.h"
-#include "global/deprecated/xmlreader.h"
 #include "engraving/infrastructure/mscreader.h"
 
 #include "log.h"
@@ -45,8 +42,12 @@ RetVal<ProjectMeta> MscMetaReader::readMeta(const muse::io::path_t& filePath) co
     }
 
     // Read score meta
-    ByteArray scoreData = msczReader.readScoreFile();
-    deprecated::XmlReader xmlReader(scoreData.toQByteArray());
+    io::Buffer scoreData(msczReader.readScoreFile());
+    if (!scoreData.open(io::IODevice::ReadOnly)) {
+        return RetVal<ProjectMeta>::make_ret(Ret::Code::InternalError);
+    }
+
+    XmlStreamReader xmlReader(&scoreData);
 
     RetVal<ProjectMeta> meta;
     meta.ret = make_ok();
@@ -76,8 +77,11 @@ muse::RetVal<CloudProjectInfo> MscMetaReader::readCloudProjectInfo(const muse::i
     }
 
     // Read score meta
-    ByteArray scoreData = msczReader.readScoreFile();
-    deprecated::XmlReader xmlReader(scoreData.toQByteArray());
+    io::Buffer scoreData(msczReader.readScoreFile());
+    if (!scoreData.open(io::IODevice::ReadOnly)) {
+        return RetVal<CloudProjectInfo>::make_ret(Ret::Code::InternalError);
+    }
+    XmlStreamReader xmlReader(&scoreData);
 
     ProjectMeta meta;
     doReadMeta(xmlReader, meta);
@@ -113,21 +117,22 @@ Ret MscMetaReader::prepareReader(const muse::io::path_t& filePath, MscReader& re
     return make_ok();
 }
 
-MscMetaReader::RawMeta MscMetaReader::doReadBox(deprecated::XmlReader& xmlReader) const
+MscMetaReader::RawMeta MscMetaReader::doReadBox(XmlStreamReader& xmlReader) const
 {
     RawMeta meta;
 
     while (xmlReader.readNextStartElement()) {
-        if (xmlReader.tagName() == "Text") {
+        if (xmlReader.name() == "Text") {
             bool isTitle = false;
             bool isSubtitle = false;
             bool isComposer = false;
             bool isLyricist = false;
             while (xmlReader.readNextStartElement()) {
-                std::string tag(xmlReader.tagName());
+                const std::string tag(xmlReader.name());
 
                 if (tag == "style") {
-                    std::string val = strings::toLower(xmlReader.readString());
+                    const std::string val = xmlReader.readText()
+                                            .toLower().toStdString();
 
                     if (val == "title" || val == "2") {
                         isTitle = true;
@@ -176,17 +181,17 @@ MscMetaReader::RawMeta MscMetaReader::doReadBox(deprecated::XmlReader& xmlReader
     return meta;
 }
 
-MscMetaReader::RawMeta MscMetaReader::doReadRawMeta(deprecated::XmlReader& xmlReader) const
+MscMetaReader::RawMeta MscMetaReader::doReadRawMeta(XmlStreamReader& xmlReader) const
 {
     RawMeta meta;
 
     while (xmlReader.readNextStartElement()) {
-        std::string tag(xmlReader.tagName());
+        const std::string tag(xmlReader.name());
 
         if (tag == "work-title") {
-            meta.titleTag = QString::fromStdString(xmlReader.readString());
+            meta.titleTag = xmlReader.readText().toQString();
         } else if (tag == "metaTag") {
-            std::string name = xmlReader.attribute("name");
+            const std::string name(xmlReader.asciiAttribute("name"));
 
             if (name == "workTitle") {
                 meta.titleAttribute = readMetaTagText(xmlReader);
@@ -203,12 +208,12 @@ MscMetaReader::RawMeta MscMetaReader::doReadRawMeta(deprecated::XmlReader& xmlRe
             } else if (name == "creationDate") {
                 meta.creationDate = readMetaTagText(xmlReader);
             } else {
-                meta.additionalTags[QString::fromStdString(name)] = readMetaTagText(xmlReader);
+                meta.additionalTags[QString::fromUtf8(name)] = readMetaTagText(xmlReader);
             }
         } else if (tag == "Staff") {
             if (meta.titleStyle.isEmpty()) {
                 while (xmlReader.readNextStartElement()) {
-                    std::string boxTag(xmlReader.tagName());
+                    const std::string boxTag(xmlReader.name());
 
                     if (boxTag == "HBox"
                         || boxTag == "VBox"
@@ -242,20 +247,20 @@ MscMetaReader::RawMeta MscMetaReader::doReadRawMeta(deprecated::XmlReader& xmlRe
     return meta;
 }
 
-void MscMetaReader::doReadMeta(deprecated::XmlReader& xmlReader, ProjectMeta& meta) const
+void MscMetaReader::doReadMeta(XmlStreamReader& xmlReader, ProjectMeta& meta) const
 {
     RawMeta rawMeta;
 
     while (xmlReader.readNextStartElement()) {
-        if (xmlReader.tagName() == "museScore") {
-            std::string version = xmlReader.attribute("version");
+        if (xmlReader.name() == "museScore") {
+            const std::string version(xmlReader.asciiAttribute("version"));
             bool suitedVersion = version.rfind("1", 0) == 0;
 
             if (suitedVersion) {
                 rawMeta = doReadRawMeta(xmlReader);
             } else {
                 while (xmlReader.readNextStartElement()) {
-                    if (xmlReader.tagName() == "Score") {
+                    if (xmlReader.name() == "Score") {
                         rawMeta = doReadRawMeta(xmlReader);
                     } else {
                         xmlReader.skipCurrentElement();
@@ -359,13 +364,15 @@ std::string MscMetaReader::cutXmlTags(const std::string& str) const
     return fin;
 }
 
-QString MscMetaReader::readText(deprecated::XmlReader& xmlReader) const
+QString MscMetaReader::readText(XmlStreamReader& xmlReader) const
 {
-    std::string str = xmlReader.readString(deprecated::XmlReader::IncludeChildElements);
+    const std::string str = xmlReader.readBody().toStdString();
     return formatFromXml(str);
 }
 
-QString MscMetaReader::readMetaTagText(deprecated::XmlReader& xmlReader) const
+QString MscMetaReader::readMetaTagText(XmlStreamReader& xmlReader) const
 {
-    return QString::fromStdString(xmlReader.readString());
+    const std::string_view metaText = xmlReader.readAsciiText();
+
+    return QString::fromUtf8(metaText);
 }

src/framework/global/serialization/xmlstreamwriter.cpp
@@ -118,7 +118,7 @@ std::string XmlStreamWriter::escapeString(const std::string_view str)
 void XmlStreamWriter::writeValue(const Value& v)
 {
     // std::monostate, int, unsigned int, signed long int, unsigned long int, signed long long, unsigned long long,
-    // double, const char*, AsciiStringView, String
+    // double, const char*, std::string_view, String
     switch (v.index()) {
     case 0:
         break;
@@ -138,7 +138,7 @@ void XmlStreamWriter::writeValue(const Value& v)
         break;
     case 8: m_impl->stream << escapeString(AsciiStringView(std::get<const char*>(v)));
         break;
-    case 9: m_impl->stream << escapeString(std::get<AsciiStringView>(v));
+    case 9: m_impl->stream << escapeString(std::get<std::string_view>(v));
         break;
     case 10: m_impl->stream << escapeString(std::get<String>(v).toStdString());
         break;
@@ -149,9 +149,9 @@ void XmlStreamWriter::writeValue(const Value& v)
     }
 }
 
-void XmlStreamWriter::startElement(const AsciiStringView& name, const Attributes& attrs)
+void XmlStreamWriter::startElement(const std::string_view& name, const Attributes& attrs)
 {
-    IF_ASSERT_FAILED(!name.contains(' ')) {
+    IF_ASSERT_FAILED(name.find(' ') == std::string_view::npos) {
     }
 
     m_impl->putLevel();
@@ -162,7 +162,7 @@ void XmlStreamWriter::startElement(const AsciiStringView& name, const Attributes
         m_impl->stream << '\"';
     }
     m_impl->stream << '>' << '\n';
-    m_impl->stack.push_back(name.ascii());
+    m_impl->stack.emplace_back(name);
 }
 
 void XmlStreamWriter::startElement(const String& name, const Attributes& attrs)
@@ -186,9 +186,9 @@ void XmlStreamWriter::endElement()
 }
 
 // <element attr="value" />
-void XmlStreamWriter::element(const AsciiStringView& name, const Attributes& attrs)
+void XmlStreamWriter::element(const std::string_view& name, const Attributes& attrs)
 {
-    IF_ASSERT_FAILED(!name.contains(' ')) {
+    IF_ASSERT_FAILED(name.find(' ') == std::string_view::npos) {
     }
 
     m_impl->putLevel();
@@ -201,9 +201,9 @@ void XmlStreamWriter::element(const AsciiStringView& name, const Attributes& att
     m_impl->stream << "/>\n";
 }
 
-void XmlStreamWriter::element(const AsciiStringView& name, const Value& body)
+void XmlStreamWriter::element(const std::string_view& name, const Value& body)
 {
-    IF_ASSERT_FAILED(!name.contains(' ')) {
+    IF_ASSERT_FAILED(name.find(' ') == std::string_view::npos) {
     }
 
     m_impl->putLevel();
@@ -212,9 +212,9 @@ void XmlStreamWriter::element(const AsciiStringView& name, const Value& body)
     m_impl->stream << "</" << name << '>' << '\n';
 }
 
-void XmlStreamWriter::element(const AsciiStringView& name, const Attributes& attrs, const Value& body)
+void XmlStreamWriter::element(const std::string_view& name, const Attributes& attrs, const Value& body)
 {
-    IF_ASSERT_FAILED(!name.contains(' ')) {
+    IF_ASSERT_FAILED(name.find(' ') == std::string_view::npos) {
     }
 
     m_impl->putLevel();

src/framework/global/serialization/xmlstreamwriter.h
@@ -19,6 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #pragma once
 
 #include <string>
@@ -41,8 +42,8 @@ public:
     XmlStreamWriter& operator=(const XmlStreamWriter&) = delete;
 
     using Value = std::variant<std::monostate, int, unsigned int, signed long int, unsigned long int, signed long long, unsigned long long,
-                               double, const char*, AsciiStringView, String>;
-    using Attribute = std::pair<AsciiStringView, Value>;
+                               double, const char*, std::string_view, String>;
+    using Attribute = std::pair<std::string_view, Value>;
     using Attributes = std::vector<Attribute>;
 
     void setDevice(io::IODevice* dev);
@@ -51,13 +52,13 @@ public:
     void startDocument();
     void writeDoctype(const String& type);
 
-    void startElement(const AsciiStringView& name, const Attributes& attrs = {});
+    void startElement(const std::string_view& name, const Attributes& attrs = {});
     void startElement(const String& name, const Attributes& attrs = {});
     void endElement();
 
-    void element(const AsciiStringView& name, const Attributes& attrs = {});                // <element attr="value" />
-    void element(const AsciiStringView& name, const Value& body);                           // <element>body</element>
-    void element(const AsciiStringView& name, const Attributes& attrs, const Value& body);  // <element attr="value" >body</element>
+    void element(const std::string_view& name, const Attributes& attrs = {});                // <element attr="value" />
+    void element(const std::string_view& name, const Value& body);                           // <element>body</element>
+    void element(const std::string_view& name, const Attributes& attrs, const Value& body);  // <element attr="value" >body</element>
 
     void comment(const String& text);
 

src/framework/shortcuts/internal/midiremote.cpp
@@ -19,10 +19,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #include "midiremote.h"
 
-#include "global/deprecated/xmlreader.h"
-#include "global/deprecated/xmlwriter.h"
+#include "global/io/file.h"
+#include "global/serialization/xmlstreamreader.h"
+#include "global/serialization/xmlstreamwriter.h"
 
 #include "multiinstances/resourcelockguard.h"
 
@@ -32,11 +34,11 @@ using namespace muse;
 using namespace muse::shortcuts;
 using namespace muse::midi;
 
-constexpr std::string_view MIDIMAPPING_TAG("MidiMapping");
-constexpr std::string_view EVENT_TAG("Event");
-constexpr std::string_view MAPPING_ACTION_CODE_TAG("key");
-constexpr std::string_view MAPPING_EVENT_TYPE_TAG("EventType");
-constexpr std::string_view MAPPING_EVENT_VALUE_TAG("EventValue");
+static constexpr std::string_view MIDIMAPPING_TAG("MidiMapping");
+static constexpr std::string_view EVENT_TAG("Event");
+static constexpr std::string_view MAPPING_ACTION_CODE_TAG("key");
+static constexpr std::string_view MAPPING_EVENT_TYPE_TAG("EventType");
+static constexpr std::string_view MAPPING_EVENT_VALUE_TAG("EventValue");
 
 static const std::string REALTIME_ADVANCE_ACTION_NAME("realtime-advance");
 
@@ -126,16 +128,22 @@ void MidiRemote::readMidiMappings()
 {
     muse::mi::ReadResourceLockGuard resource_guard(multiInstancesProvider(), MIDI_MAPPING_RESOURCE_NAME);
 
-    io::path_t midiMappingsPath = configuration()->midiMappingUserAppDataPath();
-    deprecated::XmlReader reader(midiMappingsPath);
+    const io::path_t midiMappingsPath = configuration()->midiMappingUserAppDataPath();
+    io::File mappingsFile(midiMappingsPath);
+    if (!mappingsFile.open(io::IODevice::ReadOnly)) {
+        LOGD() << "failed to open midi mappings file: " << mappingsFile.error();
+        return;
+    }
+
+    XmlStreamReader reader(&mappingsFile);
 
     reader.readNextStartElement();
-    if (reader.tagName() != MIDIMAPPING_TAG) {
+    if (reader.name() != MIDIMAPPING_TAG) {
         return;
     }
 
     while (reader.readNextStartElement()) {
-        if (reader.tagName() != EVENT_TAG) {
+        if (reader.name() != EVENT_TAG) {
             reader.skipCurrentElement();
             continue;
         }
@@ -146,20 +154,20 @@ void MidiRemote::readMidiMappings()
         }
     }
 
-    if (!reader.success()) {
+    if (reader.isError()) {
         LOGE() << "failed parse xml, error: " << reader.error();
     }
 }
 
-MidiControlsMapping MidiRemote::readMidiMapping(deprecated::XmlReader& reader) const
+MidiControlsMapping MidiRemote::readMidiMapping(XmlStreamReader& reader) const
 {
     MidiControlsMapping midiMapping;
 
     while (reader.readNextStartElement()) {
-        std::string tag(reader.tagName());
+        const std::string tag(reader.name());
 
         if (tag == MAPPING_ACTION_CODE_TAG) {
-            midiMapping.action = reader.readString();
+            midiMapping.action = reader.readAsciiText();
         } else if (tag == MAPPING_EVENT_TYPE_TAG) {
             midiMapping.event.type = static_cast<RemoteEventType>(reader.readInt());
         } else if (tag == MAPPING_EVENT_VALUE_TAG) {
@@ -178,29 +186,33 @@ bool MidiRemote::writeMidiMappings(const MidiMappingList& midiMappings) const
 
     muse::mi::WriteResourceLockGuard resource_guard(multiInstancesProvider(), MIDI_MAPPING_RESOURCE_NAME);
 
-    io::path_t midiMappingsPath = configuration()->midiMappingUserAppDataPath();
-    deprecated::XmlWriter writer(midiMappingsPath);
+    const io::path_t midiMappingsPath = configuration()->midiMappingUserAppDataPath();
+    io::File mappingsFile(midiMappingsPath);
+    if (!mappingsFile.open(io::IODevice::WriteOnly)) {
+        return false;
+    }
 
-    writer.writeStartDocument();
-    writer.writeStartElement(MIDIMAPPING_TAG);
+    XmlStreamWriter writer(&mappingsFile);
+
+    writer.startDocument();
+    writer.startElement(MIDIMAPPING_TAG);
 
     for (const MidiControlsMapping& midiMapping : midiMappings) {
         writeMidiMapping(writer, midiMapping);
     }
 
-    writer.writeEndElement();
-    writer.writeEndDocument();
+    writer.endElement();
 
-    return writer.success();
+    return mappingsFile.hasError();
 }
 
-void MidiRemote::writeMidiMapping(deprecated::XmlWriter& writer, const MidiControlsMapping& midiMapping) const
+void MidiRemote::writeMidiMapping(XmlStreamWriter& writer, const MidiControlsMapping& midiMapping) const
 {
-    writer.writeStartElement(EVENT_TAG);
-    writer.writeTextElement(MAPPING_ACTION_CODE_TAG, midiMapping.action);
-    writer.writeTextElement(MAPPING_EVENT_TYPE_TAG, std::to_string(midiMapping.event.type));
-    writer.writeTextElement(MAPPING_EVENT_VALUE_TAG, std::to_string(midiMapping.event.value));
-    writer.writeEndElement();
+    writer.startElement(EVENT_TAG);
+    writer.element(MAPPING_ACTION_CODE_TAG, midiMapping.action);
+    writer.element(MAPPING_EVENT_TYPE_TAG, midiMapping.event.type);
+    writer.element(MAPPING_EVENT_VALUE_TAG, midiMapping.event.value);
+    writer.endElement();
 }
 
 bool MidiRemote::needIgnoreEvent(const Event& event) const

src/framework/shortcuts/internal/midiremote.h
@@ -19,8 +19,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef MUSE_SHORTCUTS_MIDIREMOTE_H
-#define MUSE_SHORTCUTS_MIDIREMOTE_H
+
+#pragma once
 
 #include "async/asyncable.h"
 
@@ -33,9 +33,9 @@
 #include "shortcutstypes.h"
 #include "../imidiremote.h"
 
-namespace muse::deprecated {
-class XmlReader;
-class XmlWriter;
+namespace muse {
+class XmlStreamReader;
+class XmlStreamWriter;
 }
 
 namespace muse::shortcuts {
@@ -68,10 +68,10 @@ public:
 
 private:
     void readMidiMappings();
-    MidiControlsMapping readMidiMapping(deprecated::XmlReader& reader) const;
+    MidiControlsMapping readMidiMapping(XmlStreamReader& reader) const;
 
     bool writeMidiMappings(const MidiMappingList& midiMappings) const;
-    void writeMidiMapping(deprecated::XmlWriter& writer, const MidiControlsMapping& midiMapping) const;
+    void writeMidiMapping(XmlStreamWriter& writer, const MidiControlsMapping& midiMapping) const;
 
     bool needIgnoreEvent(const muse::midi::Event& event) const;
 
@@ -83,5 +83,3 @@ private:
     async::Notification m_midiMappingsChanged;
 };
 }
-
-#endif // MUSE_SHORTCUTS_MIDIREMOTE_H

src/framework/shortcuts/internal/shortcutsregister.cpp
@@ -23,8 +23,10 @@
 
 #include <QKeySequence>
 
-#include "global/deprecated/xmlreader.h"
-#include "global/deprecated/xmlwriter.h"
+#include "global/io/file.h"
+#include "global/serialization/xmlstreamreader.h"
+#include "global/serialization/xmlstreamwriter.h"
+
 #include "multiinstances/resourcelockguard.h"
 
 #include "log.h"
@@ -245,15 +247,21 @@ bool ShortcutsRegister::readFromFile(ShortcutList& shortcuts, const io::path_t& 
 {
     TRACEFUNC;
 
-    deprecated::XmlReader reader(path);
+    io::File file(path);
+    if (!file.open(io::IODevice::ReadOnly)) {
+        LOGD() << "failed to open shortcuts file: " << file.error();
+        return false;
+    }
+
+    XmlStreamReader reader(&file);
 
     reader.readNextStartElement();
-    if (reader.tagName() != SHORTCUTS_TAG) {
+    if (reader.name() != SHORTCUTS_TAG) {
         return false;
     }
 
     while (reader.readNextStartElement()) {
-        if (reader.tagName() != SHORTCUT_TAG) {
+        if (reader.name() != SHORTCUT_TAG) {
             reader.skipCurrentElement();
             continue;
         }
@@ -264,26 +272,27 @@ bool ShortcutsRegister::readFromFile(ShortcutList& shortcuts, const io::path_t& 
         }
     }
 
-    if (!reader.success()) {
+    if (reader.isError()) {
         LOGE() << "failed parse xml, error: " << reader.error() << ", path: " << path;
+        return false;
     }
 
-    return reader.success();
+    return true;
 }
 
-Shortcut ShortcutsRegister::readShortcut(deprecated::XmlReader& reader) const
+Shortcut ShortcutsRegister::readShortcut(XmlStreamReader& reader) const
 {
     Shortcut shortcut;
 
     while (reader.readNextStartElement()) {
-        std::string tag(reader.tagName());
+        const std::string tag(reader.name());
 
         if (tag == ACTION_CODE_TAG) {
-            shortcut.action = reader.readString();
+            shortcut.action = reader.readAsciiText();
         } else if (tag == STANDARD_KEY_TAG) {
             shortcut.standardKey = QKeySequence::StandardKey(reader.readInt());
         } else if (tag == SEQUENCE_TAG) {
-            shortcut.sequences.push_back(reader.readString());
+            shortcut.sequences.emplace_back(reader.readAsciiText());
         } else if (tag == AUTOREPEAT_TAG) {
             shortcut.autoRepeat = reader.readInt();
         } else {
@@ -343,35 +352,40 @@ bool ShortcutsRegister::writeToFile(const ShortcutList& shortcuts, const io::pat
 
     mi::WriteResourceLockGuard guard(multiInstancesProvider(), SHORTCUTS_RESOURCE_NAME);
 
-    deprecated::XmlWriter writer(path);
+    io::File file(path);
+    if (!file.open(io::IODevice::WriteOnly)) {
+        LOGD() << "failed to open shortcuts file: " << file.error();
+        return false;
+    }
 
-    writer.writeStartDocument();
-    writer.writeStartElement(SHORTCUTS_TAG);
+    XmlStreamWriter writer(&file);
+
+    writer.startDocument();
+    writer.startElement(SHORTCUTS_TAG);
 
     for (const Shortcut& shortcut : shortcuts) {
         writeShortcut(writer, shortcut);
     }
 
-    writer.writeEndElement();
-    writer.writeEndDocument();
+    writer.endElement();
 
-    return writer.success();
+    return file.hasError();
 }
 
-void ShortcutsRegister::writeShortcut(deprecated::XmlWriter& writer, const Shortcut& shortcut) const
+void ShortcutsRegister::writeShortcut(XmlStreamWriter& writer, const Shortcut& shortcut) const
 {
-    writer.writeStartElement(SHORTCUT_TAG);
-    writer.writeTextElement(ACTION_CODE_TAG, shortcut.action);
+    writer.startElement(SHORTCUT_TAG);
+    writer.element(ACTION_CODE_TAG, shortcut.action);
 
     if (shortcut.standardKey != QKeySequence::UnknownKey) {
-        writer.writeTextElement(STANDARD_KEY_TAG, QString("%1").arg(shortcut.standardKey).toStdString());
+        writer.element(STANDARD_KEY_TAG, QString("%1").arg(shortcut.standardKey).toStdString());
     }
 
     for (const std::string& seq : shortcut.sequences) {
-        writer.writeTextElement(SEQUENCE_TAG, seq);
+        writer.element(SEQUENCE_TAG, seq);
     }
 
-    writer.writeEndElement();
+    writer.endElement();
 }
 
 Notification ShortcutsRegister::shortcutsChanged() const

src/framework/shortcuts/internal/shortcutsregister.h
@@ -19,8 +19,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef MUSE_SHORTCUTS_SHORTCUTSREGISTER_H
-#define MUSE_SHORTCUTS_SHORTCUTSREGISTER_H
+
+#pragma once
 
 #include <unordered_map>
 
@@ -32,9 +32,9 @@
 #include "io/ifilesystem.h"
 #include "multiinstances/imultiinstancesprovider.h"
 
-namespace muse::deprecated {
-class XmlReader;
-class XmlWriter;
+namespace muse {
+class XmlStreamReader;
+class XmlStreamWriter;
 }
 
 namespace muse::shortcuts {
@@ -76,10 +76,10 @@ public:
 private:
 
     bool readFromFile(ShortcutList& shortcuts, const io::path_t& path) const;
-    Shortcut readShortcut(deprecated::XmlReader& reader) const;
+    Shortcut readShortcut(XmlStreamReader& reader) const;
 
     bool writeToFile(const ShortcutList& shortcuts, const io::path_t& path) const;
-    void writeShortcut(deprecated::XmlWriter& writer, const Shortcut& shortcut) const;
+    void writeShortcut(XmlStreamWriter& writer, const Shortcut& shortcut) const;
 
     void mergeShortcuts(ShortcutList& shortcuts, const ShortcutList& defaultShortcuts) const;
     void mergeAdditionalShortcuts(ShortcutList& shortcuts);
@@ -98,5 +98,3 @@ private:
     async::Notification m_activeChanged;
 };
 }
-
-#endif // MUSE_SHORTCUTS_SHORTCUTSREGISTER_H

commit 5b8703002745b5db38daecfa43a5f7d7090bbac9

project: Fix loading of templates
Resolves: #31076

`MscMetaReader` did not read the title correctly from boxes. Templates
with an empty title are skipped in `TemplatesModel::load`.
Broken in 55a5118. `XmlStreamReader::readBody` does something completely
different from the now removed `XmlReader::readString(...)`.

src/project/internal/mscmetareader.cpp
@@ -33,6 +33,33 @@ using namespace muse::io;
 using namespace mu::project;
 using namespace mu::engraving;
 
+// emulates QXmlStreamReader::readElementText(QXmlStreamReader::IncludeChildElements)
+static std::string readTextAndChildrenText(XmlStreamReader& reader)
+{
+    if (!reader.isStartElement()) {
+        return std::string();
+    }
+
+    std::string text;
+    while (true) {
+        switch (reader.readNext()) {
+        case XmlStreamReader::Comment:
+            break;
+        case XmlStreamReader::Characters:
+            text += reader.asciiText();
+            break;
+        case XmlStreamReader::StartElement:
+            text += readTextAndChildrenText(reader);
+            break;
+        case XmlStreamReader::EndElement:
+            return text;
+        default:
+            LOGE() << "Unexpected token: " << reader.tokenString();
+            return text;
+        }
+    }
+}
+
 RetVal<ProjectMeta> MscMetaReader::readMeta(const muse::io::path_t& filePath) const
 {
     MscReader msczReader;
@@ -366,7 +393,8 @@ std::string MscMetaReader::cutXmlTags(const std::string& str) const
 
 QString MscMetaReader::readText(XmlStreamReader& xmlReader) const
 {
-    const std::string str = xmlReader.readBody().toStdString();
+    const std::string str = readTextAndChildrenText(xmlReader);
+
     return formatFromXml(str);
 }
 