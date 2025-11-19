15bfbdf1ad91f9297a71f5740de5d9c0bb1af8d7
Ash-86 <108089527+Ash-86@users.noreply.github.com>
2025-11-19 16:35:10 -0300

    allow T on list sel

--- src/engraving/editing/edit.cpp
+++ src/engraving/editing/edit.cpp
@@ -2066,7 +2066,7 @@ void Score::cmdAddTie(bool addToChord)
             }
         }
 
-        if (noteEntryMode()) {
+        if (noteEntryMode() || selection().isList()) {
             ChordRest* cr = nullptr;
             Chord* c = note->chord();
             int staffMove = c->staffMove();
@@ -2172,7 +2172,9 @@ Tie* Score::cmdToggleTie()
     bool canAddTies = false;
     const size_t notes = noteList.size();
     std::vector<Note*> tieNoteList(notes);
-    const bool shouldTieListSelection = notes >= 2;
+
+    bool sameChord = std::all_of(noteList.begin(), noteList.end(), [&](const Note* n) { return n->chord() == noteList[0]->chord(); });
+    const bool shouldTieListSelection = !sameChord;
 
     for (size_t i = 0; i < notes; ++i) {
         Note* n = noteList[i];
@@ -2183,6 +2185,16 @@ Tie* Score::cmdToggleTie()
             tieNoteList[i] = tieNote;
             if (tieNote) {
                 canAddTies = true;
+            }
+            if (!tieNote && selection().isList() && sameChord) {
+                cmdAddTie();
+                if (notes >= 2) {
+                    Chord* c = n->chord()->next();
+                    for (Note* cn : c->notes()) {
+                        score()->select(cn, SelectType::ADD);
+                    }
+                }
+                return nullptr;
             }
         }
     }
