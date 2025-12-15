d48e6a37c464448789ddda0f4422f4e4c8245721
Ash-86 <108089527+Ash-86@users.noreply.github.com>
2025-12-15 13:08:11 -0300

    allow ctrl+DoubleClick to add chords to selection

--- src/notationscene/qml/MuseScore/NotationScene/notationviewinputcontroller.cpp
+++ src/notationscene/qml/MuseScore/NotationScene/notationviewinputcontroller.cpp
@@ -1335,8 +1335,9 @@ void NotationViewInputController::mouseDoubleClickEvent(QMouseEvent* event)
         return;
     }
 
-    if (event->modifiers() != Qt::NoModifier) {
-        return;
+    SelectType selectType = SelectType::REPLACE;
+    if (event->modifiers() == Qt::ControlModifier) {
+        selectType = SelectType::ADD;
     }
 
     switch (hitElement->type()) {
@@ -1354,7 +1355,7 @@ void NotationViewInputController::mouseDoubleClickEvent(QMouseEvent* event)
             break;
         }
         const std::vector<Note*> notes = chord->notes();
-        viewInteraction()->select({ notes.begin(), notes.end() });
+        viewInteraction()->select({ notes.begin(), notes.end() }, selectType);
         break;
     }
     case ElementType::FRET_DIAGRAM: {
