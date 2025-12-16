e922b47c25120a3c0f7d007993ebb7b7bb39c5e0
Roman Pudashkin <r.pudashkin@gmail.com>
2025-12-16 15:11:52 +0200

    Fix #31486: seekElement() must be called before playElements()
    Because it cancels the notes that are currently being played

--- src/notation/view/notationviewinputcontroller.h
+++ src/notation/view/notationviewinputcontroller.h
@@ -191,6 +191,7 @@ private:
     bool mousePress_considerGrip(const ClickContext& ctx); // returns true if event is consumed
     bool mousePress_considerDragOutgoingElement(const ClickContext& ctx);
     void mousePress_considerSelect(const ClickContext& ctx);
+    void mousePress_seekSelection(const ClickContext& ctx);
     void cycleOverlappingHitElements(const std::vector<EngravingItem*>& hitElements, staff_idx_t hitStaffIndex);
     bool mousePress_considerDragOutgoingRange(const ClickContext& ctx);
     bool mousePress_considerStartPasteRangeOnRelease(const ClickContext& ctx);
@@ -204,8 +205,6 @@ private:
     bool isAnchorEditingEvent(QKeyEvent* event) const;
 
     bool tryPercussionShortcut(QKeyEvent* event);
-
-    EngravingItem* resolveStartPlayableElement() const;
 
     IControlledView* m_view = nullptr;
 
