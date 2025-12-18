6d9d3ce045d8e3785c9a8c3cf0ea5b1241e3e9b2
Michele Spagnolo <m.spagnolo@mu.se>
2025-12-18 14:35:24 +0100

    On double-click fret diagram, select also harmony

--- src/notation/view/notationviewinputcontroller.cpp
+++ src/notation/view/notationviewinputcontroller.cpp
@@ -34,6 +34,7 @@
 #include "abstractelementpopupmodel.h"
 
 #include "engraving/dom/drumset.h"
+#include "engraving/dom/fret.h"
 #include "engraving/dom/shadownote.h"
 
 #include "defer.h"
@@ -1352,6 +1353,12 @@ void NotationViewInputController::mouseDoubleClickEvent(QMouseEvent* event)
         }
         const std::vector<Note*> notes = chord->notes();
         viewInteraction()->select({ notes.begin(), notes.end() });
+        break;
+    }
+    case ElementType::FRET_DIAGRAM: {
+        if (Harmony* harmony = toFretDiagram(hitElement)->harmony()) {
+            viewInteraction()->select({ hitElement, harmony });
+        }
         break;
     }
     default: break;
