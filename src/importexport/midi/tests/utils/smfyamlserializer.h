eb877dab025132af96a29f8fa2468bf201b8fbb9
Julian Bühler <juli27@users.noreply.github.com>
2025-12-18 22:33:01 +0100

    iex/midi/tests: Re-enable a few basic export tests
    Adds yaml serialization of midi files which is used when the generated
    one differs from the reference file.

---
+++ src/importexport/midi/tests/utils/smfyamlserializer.h
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-CLA-applies
+ *
+ * MuseScore
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2025 MuseScore Limited and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <filesystem>
+
+#include "global/types/ret.h"
+
+namespace muse::io {
+class IODevice;
+}
+
+namespace mu::iex::midi {
+class MidiFile;
+
+// Converts a binary Standard MIDI File to human-readable YAML
+class SmfYamlSerializer
+{
+public:
+    static muse::Ret serialize(const std::filesystem::path& midiPath, muse::io::IODevice* yamlOut);
+    static void serialize(const MidiFile&, muse::io::IODevice* yamlOut);
+};
+}
