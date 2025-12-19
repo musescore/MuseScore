1877b1510b6bf589ef09d033f4d0273864ee65e7
Ash-86 <108089527+Ash-86@users.noreply.github.com>
2025-12-19 01:22:52 -0300

    refine tuplets

--- src/engraving/editing/cmd.cpp
+++ src/engraving/editing/cmd.cpp
@@ -3415,7 +3415,7 @@ void Score::cmdExtendToNextNote()
             if (!ncr->isRest()) {
                 break;
             }
-            if (cr->tuplet() || ncr->tuplet()) {
+            if (cr->tuplet() != ncr->tuplet()) {
                 m_is.setTrack(ncr->track());
                 m_is.setSegment(ncr->segment());
                 m_is.setLastSegment(m_is.segment());
@@ -3440,9 +3440,7 @@ void Score::cmdExtendToNextNote()
             } else {
                 Fraction newDur = cr->ticks() + ncr->ticks();
                 changeCRlen(cr, newDur);
-
-                Fraction tickAtNewDurEnd = cr->tick() + newDur;
-                while (cr->endTick() < tickAtNewDurEnd) { // if resulting cr has ties
+                while (toChord(cr)->notes()[0]->tieFor()) {
                     cr = nextChordRest(cr);
                     toSelect.push_back(cr);
                 }
