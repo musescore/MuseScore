0293787d4b8678678690bc92a3b040780418c1a3
songzhenk <songzhenke@hotmail.com>
2025-12-21 23:42:44 +1100

    Fix duration line when load file by moving duration line to system layout

--- src/engraving/rendering/score/systemlayout.cpp
+++ src/engraving/rendering/score/systemlayout.cpp
@@ -1234,6 +1234,7 @@ void SystemLayout::layoutSystemElements(System* system, LayoutContext& ctx)
         ChordLayout::layoutArticulations(c, ctx);
         ChordLayout::layoutArticulations2(c, ctx);
         ChordLayout::layoutChordBaseFingering(c, system, ctx);
+        ChordLayout::layoutDurationLines(c, ctx);
     }
 
     layoutTuplets(elementsToLayout.chordRests, ctx);
