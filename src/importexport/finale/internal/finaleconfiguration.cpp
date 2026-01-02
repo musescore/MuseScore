0deda39426b4558a02e02e780450db14cb7550e7
XiaoMigros <xiaomigros3@gmail.com>
2026-01-02 21:55:43 +0100

    Symbols conversion preference

--- src/importexport/finale/internal/finaleconfiguration.cpp
+++ src/importexport/finale/internal/finaleconfiguration.cpp
@@ -31,13 +31,19 @@ using namespace mu::iex::finale;
 static const std::string module_name("iex_finale");
 
 static const Settings::Key FINALE_IMPORT_POSITIONS_KEY(module_name, "import/finale/importPositions");
+static const Settings::Key FINALE_CONVERT_TEXT_SYMBOLS_KEY(module_name, "import/finale/convertTextSymbols");
 
 void FinaleConfiguration::init()
 {
     settings()->setDefaultValue(FINALE_IMPORT_POSITIONS_KEY, Val(ImportPositionsType::AdjustmentsOnly));
+    settings()->setDefaultValue(FINALE_CONVERT_TEXT_SYMBOLS_KEY, Val(true));
 
     settings()->valueChanged(FINALE_IMPORT_POSITIONS_KEY).onReceive(this, [this](const Val& val) {
         m_importPositionsTypeChanged.send(val.toEnum<ImportPositionsType>());
+    });
+
+    settings()->valueChanged(FINALE_CONVERT_TEXT_SYMBOLS_KEY).onReceive(this, [this](const Val& val) {
+        m_convertTextSymbolsChanged.send(val.toBool());
     });
 }
 
@@ -46,12 +52,27 @@ IFinaleConfiguration::ImportPositionsType FinaleConfiguration::importPositionsTy
     return settings()->value(FINALE_IMPORT_POSITIONS_KEY).toEnum<IFinaleConfiguration::ImportPositionsType>();
 }
 
+bool FinaleConfiguration::convertTextSymbols() const
+{
+    return settings()->value(FINALE_CONVERT_TEXT_SYMBOLS_KEY).toBool();
+}
+
 void FinaleConfiguration::setImportPositionsType(IFinaleConfiguration::ImportPositionsType importPositionsType)
 {
     settings()->setSharedValue(FINALE_IMPORT_POSITIONS_KEY, Val(importPositionsType));
 }
 
+void FinaleConfiguration::setConvertTextSymbols(bool convert)
+{
+    settings()->setSharedValue(FINALE_CONVERT_TEXT_SYMBOLS_KEY, Val(convert));
+}
+
 async::Channel<IFinaleConfiguration::ImportPositionsType> FinaleConfiguration::importPositionsTypeChanged() const
 {
     return m_importPositionsTypeChanged;
+}
+
+async::Channel<bool> FinaleConfiguration::convertTextSymbolsChanged() const
+{
+    return m_convertTextSymbolsChanged;
 }
