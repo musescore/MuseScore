23315d9949a35f95ecd528930f31648df595cacd
Joachim Schmitz <jojo@schmitz-digital.de>
2026-01-07 17:32:57 +0100

    Fix GH#23898: Make MS recognize the .mscz~ filetype
    build upon #31352

--- src/engraving/infrastructure/mscio.h
+++ src/engraving/infrastructure/mscio.h
@@ -30,6 +30,9 @@ namespace mu::engraving {
 //! NOTE The main format is MuseScore, is a zip archive with a specific structure
 static const std::string MSCZ = "mscz";
 
+//! NOTE This is the automatically generated backup file.
+static const std::string MSCZ_BACKUP = "mscz~";
+
 //! NOTE Before MuseScore 4, MuseScore could save data in one xml file (excluding binary) with `.mscx` extension.
 //! Starting from MuseScore 4, only the score domain model store to the `.mscx` file,
 //! and other data, such as styles, chordlist, synthesizer settings, etc. are stored in separate files.
@@ -46,7 +49,7 @@ static const std::string MSCS = "mscs";
 
 inline bool isMuseScoreFile(const std::string& suffix)
 {
-    return suffix == MSCZ || suffix == MSCX || suffix == MSCS;
+    return suffix == MSCZ || suffix == MSCX || suffix == MSCS || suffix == MSCZ_BACKUP;
 }
 
 enum class MscIoMode : unsigned char {
@@ -58,7 +61,7 @@ enum class MscIoMode : unsigned char {
 
 inline MscIoMode mscIoModeBySuffix(const std::string& suffix)
 {
-    if (suffix == MSCZ) {
+    if (suffix == MSCZ || suffix == MSCZ_BACKUP) {
         return MscIoMode::Zip;
     } else if (suffix == MSCX) {
         return MscIoMode::Dir;
