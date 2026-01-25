e5c89ba7b2ffe2b6ae311c7b4a63425bd03af596
Joachim Schmitz <jojo@schmitz-digital.de>
2026-01-25 18:05:45 +0100

    Fix MSVC compiler warning
    reg.: unreachable code (C4702)

--- src/framework/ui/view/interactiveprovider.cpp
+++ src/framework/ui/view/interactiveprovider.cpp
@@ -499,16 +499,12 @@ QWindow* InteractiveProvider::topWindow() const
     }
 
     // QWidget
-    {
-        QWidget* qwidget = qobject_cast<QWidget*>(last.window);
-        QWindow* qwindow = qwidget->windowHandle();
-        IF_ASSERT_FAILED(qwindow) {
-            return mainWin;
-        }
-        return qwindow;
+    QWidget* qwidget = qobject_cast<QWidget*>(last.window);
+    QWindow* qwindow = qwidget->windowHandle();
+    IF_ASSERT_FAILED(qwindow) {
+        return mainWin;
     }
-
-    return mainWin;
+    return qwindow;
 }
 
 bool InteractiveProvider::topWindowIsWidget() const
