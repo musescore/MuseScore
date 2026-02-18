edbe3f86a024f09da7111e13390fc61855a4736a
Calum Matheson <mathesoncalum@gmail.com>
2026-02-18 17:08:22 +0000

    Fix applying parentheses from palettes

--- src/notation/internal/notationinteraction.cpp
+++ src/notation/internal/notationinteraction.cpp
@@ -2443,9 +2443,20 @@ void NotationInteraction::applyPaletteElementToList(EngravingItem* element, mu::
         return;
     }
 
-    if (element->isActionIcon() && toActionIcon(element)->actionType() == ActionIconType::SYSTEM_LOCK) {
-        EditSystemLocks::applyLockToSelection(score);
-        return;
+    if (element->isActionIcon()) {
+        const ActionIcon* icon = toActionIcon(element);
+        switch (icon->actionType()) {
+        case ActionIconType::SYSTEM_LOCK: {
+            EditSystemLocks::applyLockToSelection(score);
+            return;
+        }
+        case ActionIconType::PARENTHESES: {
+            score->cmdAddParenthesesToNotes();
+            return;
+        }
+        default:
+            break;
+        }
     }
 
     if (element->isSlur()) {
@@ -2682,6 +2693,10 @@ void NotationInteraction::applyPaletteElementToRange(EngravingItem* element, mu:
         switch (actionType) {
         case ActionIconType::SYSTEM_LOCK: {
             EditSystemLocks::applyLockToSelection(score);
+            return;
+        }
+        case ActionIconType::PARENTHESES: {
+            score->cmdAddParenthesesToNotes();
             return;
         }
         case ActionIconType::STANDARD_BEND:
