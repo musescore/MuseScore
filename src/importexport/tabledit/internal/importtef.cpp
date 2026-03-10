5fad612c3ab694c420cc97d69ec53301bccf113e
Leon Vinken <leon.vinken@gmail.com>
2026-03-10 10:34:16 +0100

    fix issue #32775 [TablEdit import] incorrect ties from voice 2 to voice 1

--- src/importexport/tabledit/internal/importtef.cpp
+++ src/importexport/tabledit/internal/importtef.cpp
@@ -253,7 +253,8 @@ static String fingeringTextRH(int rightFinger)
     }
 }
 
-static void addNoteToChord(mu::engraving::Chord* chord, const TefNote* tefNote, int pitch, muse::draw::Color color)
+static void addNoteToChord(mu::engraving::Chord* chord, const TefNote* tefNote, int pitch, muse::draw::Color color,
+                           std::vector<mu::engraving::Note*>& tiedNotes)
 {
     LOGN("pitch %d", pitch);
     mu::engraving::Note* note = Factory::createNote(chord);
@@ -265,7 +266,7 @@ static void addNoteToChord(mu::engraving::Chord* chord, const TefNote* tefNote, 
         note->setString(tefNote->string - 1);
         note->setColor(color);
         if (tefNote->tie) {
-            connectTie(chord, note);
+            tiedNotes.push_back(note);
         }
         if (tefNote->fingeringLH) {
             Fingering* fi = Factory::createFingering(note, TextStyleType::LH_GUITAR_FINGERING);
@@ -356,6 +357,7 @@ void TablEdit::createContents(const MeasureHandler& measureHandler)
 
     for (size_t part = 0; part < tefInstruments.size(); ++part) {
         LOGN("part %zu", part);
+        std::vector<mu::engraving::Note*> tiedNotes;
         for (voice_idx_t voice = 0; voice < mu::engraving::VOICES; ++voice) {
             LOGN("- voice %zu", voice);
             auto& voiceContent { voiceAllocators.at(part).voiceContent(voice) };
@@ -438,7 +440,7 @@ void TablEdit::createContents(const MeasureHandler& measureHandler)
                             int pitch = 96 - instrument.tuning.at(note->string - stringOffset - 1) + note->fret;
                             LOGN("      -> string %d fret %d pitch %d", note->string, note->fret, pitch);
                             // note TableEdit's strings start at 1, MuseScore's at 0
-                            addNoteToChord(chord, note, pitch, toColor(voice));
+                            addNoteToChord(chord, note, pitch, toColor(voice), tiedNotes);
                             if (note->hasGrace) {
                                 // todo fix magical constant 96 and code duplication
                                 int gracePitch = 96 - instrument.tuning.at(/* todo */ note->string - stringOffset - 1) + note->graceFret;
@@ -449,6 +451,9 @@ void TablEdit::createContents(const MeasureHandler& measureHandler)
                     }
                 }
             }
+        }
+        for (const auto note : tiedNotes) {
+            connectTie(note->chord(), note);
         }
     }
 }
