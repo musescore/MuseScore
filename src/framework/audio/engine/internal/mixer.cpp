30a5aa5567743e167d5ed215144d8b655eeaad55
Roman Pudashkin <r.pudashkin@gmail.com>
2026-03-13 14:44:23 +0200

    Set playing state

--- src/framework/audio/engine/internal/mixer.cpp
+++ src/framework/audio/engine/internal/mixer.cpp
@@ -364,6 +364,10 @@ void Mixer::setIsActive(bool arg)
             aux.channel->setIsActive(arg);
         }
     }
+
+    for (IFxProcessorPtr& fx : m_masterFxProcessors) {
+        fx->setPlaying(arg);
+    }
 }
 
 void Mixer::addClock(IClockPtr clock)
