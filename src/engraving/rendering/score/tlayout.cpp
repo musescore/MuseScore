831924790257b2eab04c4b5ec79a3bd63153294f
rettinghaus <klaus.rettinghaus@gmail.com>
2026-03-26 11:29:48 +0100

    add special handling for chant caesura

--- src/engraving/rendering/score/tlayout.cpp
+++ src/engraving/rendering/score/tlayout.cpp
@@ -1483,7 +1483,12 @@ void TLayout::layoutBreath(const Breath* item, Breath::LayoutData* ldata, const 
 
     const double voiceOffset = item->placeBelow() ? item->staff()->staffHeight(item->tick()) : 0.0;
     if (item->isCaesura()) {
-        ldata->setPosY(item->symHeight(item->symId()) / 2 + voiceOffset);
+        if (item->symId() == SymId::chantCaesura) {
+            // special handling for chant caesura, which is higher than the normal one
+            ldata->setPosY(1.5 * item->spatium() + voiceOffset);
+        } else {
+            ldata->setPosY(item->symHeight(item->symId()) / 2 + voiceOffset);
+        }
     } else if ((conf.styleSt(Sid::musicalSymbolFont) == "Emmentaler") && (item->symId() == SymId::breathMarkComma)) {
         const double shift = item->placeBelow() ? -0.5 * item->spatium() + item->symHeight(item->symId()) : 0.5 * item->spatium();
         ldata->setPosY(shift + voiceOffset);
