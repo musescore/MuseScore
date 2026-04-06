b26c2e6be2a0bb0ce125d6f4b91286f612d65bbf
unknown <r.pudashkin@gmail.com>
2026-04-06 12:49:46 +0300

    Fix #32569: use linear interpolation for pitch bend curve

--- src/framework/global/interpolation.h
+++ src/framework/global/interpolation.h
@@ -5,7 +5,7 @@
  * MuseScore
  * Music Composition & Notation
  *
- * Copyright (C) 2023 MuseScore Limited and others
+ * Copyright (C) 2026 MuseScore Limited and others
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -19,22 +19,38 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef MUSE_GLOBAL_INTERPOLATION_H
-#define MUSE_GLOBAL_INTERPOLATION_H
+
+#pragma once
 
 #include <vector>
 
-namespace muse {
-class Interpolation
-{
-public:
-    struct Point {
-        double x = 0.0;
-        double y = 0.0;
-    };
-
-    static std::vector<Point> quadraticBezierCurve(const Point& p0, const Point& p1, const Point& p2, std::size_t N);
+namespace muse::interpolation {
+struct Point {
+    double x = 0.0;
+    double y = 0.0;
 };
-}
 
-#endif // MUSE_GLOBAL_INTERPOLATION_H
+inline std::vector<Point> lerp(const Point& start, const Point& end, std::size_t steps)
+{
+    if (steps == 0) {
+        return {};
+    }
+
+    std::vector<Point> result;
+    result.reserve(steps + 1);
+
+    const double dx = end.x - start.x;
+    const double dy = end.y - start.y;
+
+    for (std::size_t i = 0; i <= steps; ++i) {
+        const double t = static_cast<double>(i) / steps;
+
+        result.emplace_back(Point {
+                start.x + dx * t,
+                start.y + dy * t
+            });
+    }
+
+    return result;
+}
+}
