a3585dfcf8fabb2540f6b610bd71320778a72e8a
Cubiking <yuk.chiu@student.manchester.ac.uk>
2026-04-11 21:56:43 +0100

    VST: call setComponentState() after plugin init

--- src/framework/vst/internal/vstplugininstance.h
+++ src/framework/vst/internal/vstplugininstance.h
@@ -77,6 +77,7 @@ public:
 private:
     void rescanParams();
     void stateBufferFromString(VstMemoryStream& buffer, char* strData, const size_t strSize) const;
+    void syncControllerToComponentState();
 
     VstPluginInstanceId m_id = 0;
     muse::audio::AudioResourceId m_resourceId;
