c14c74b9b782729b63709600bd99e05fb117aa9d
Michele Spagnolo <m.spagnolo@mu.se>
2026-05-19 17:52:13 +0200

    Override Dynamic::sizeIsSpatiumDependent

--- src/engraving/dom/dynamic.h
+++ src/engraving/dom/dynamic.h
@@ -121,6 +121,7 @@ public:
     bool isEditAllowed(EditData&) const override;
 
     bool positionRelativeToNoteheadRest() const override { return true; }
+    bool sizeIsSpatiumDependent() const override { return true; }
 
     Hairpin* leftHairpin() const { return m_leftHairpin; }
     Hairpin* rightHairpin() const { return m_rightHairpin; }
