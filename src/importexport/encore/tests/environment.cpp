901c1507604718eacebf0f826f83c635df89176a
Manolo Carrasco <manolo@apache.org>
2026-05-23 16:00:00 +0200

    enc-test: add the Encore importer test infrastructure

---
+++ src/importexport/encore/tests/environment.cpp
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2026 MuseScore Limited and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Test suite bootstrap: registers engraving modules and loads instrument templates for the Encore importer tests.
+
+#include "testing/environment.h"
+
+#include "draw/drawmodule.h"
+#include "engraving/engravingmodule.h"
+
+#include "engraving/dom/instrtemplate.h"
+#include "engraving/dom/mscore.h"
+
+#include "log.h"
+
+static muse::testing::SuiteEnvironment importexport_se(
+{
+    new muse::draw::DrawModule(),
+    new mu::engraving::EngravingModule()
+},
+    nullptr,
+    []() {
+    LOGI() << "encore tests suite post init";
+
+    mu::engraving::MScore::testMode = true;
+    mu::engraving::MScore::noGui = true;
+
+    mu::engraving::loadInstrumentTemplates(":/engraving/instruments/instruments.xml");
+}
+    );
