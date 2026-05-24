80b9714e8c5169c138e89e3701b574810674164a
Manolo Carrasco <manolo@apache.org>
2026-05-24 13:00:00 +0200

    enc-feat: model Encore score contents

---
+++ src/importexport/encore/internal/parser/elem.h
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2026 MuseScore Limited and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Umbrella header: pulls in every Encore parser element struct in one include.
+
+#ifndef MU_IMPORTEXPORT_ENC_PARSER_ELEMENTS_H
+#define MU_IMPORTEXPORT_ENC_PARSER_ELEMENTS_H
+
+#include "elem-enums.h"
+#include "elem-note.h"
+#include "elem-ornament.h"
+#include "elem-text.h"
+#include "elem-measure.h"
+#include "elem-root.h"
+
+#endif // MU_IMPORTEXPORT_ENC_PARSER_ELEMENTS_H
