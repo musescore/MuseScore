8b0b7f813f6e11934c053fa540481d021a5c6608
Michele Spagnolo <m.spagnolo@mu.se>
2026-05-27 10:03:45 +0200

    Fix #33075 notation overflows vertical video height

--- src/importexport/videoexport/internal/videowriter.cpp
+++ src/importexport/videoexport/internal/videowriter.cpp
@@ -339,9 +339,10 @@ std::optional<VideoWriter::ScoreRestoreData> VideoWriter::prepareScore(INotation
 
     if (score->staves().size() > 3) {
         //! NOTE: Calculate the dpi to display all page elements
-        muse::RectF ttbox = page->tbbox();
+        double originalPageHeight = page->height() - score->style().styleD(engraving::Sid::pageOddTopMargin)
+                                    - score->style().styleD(engraving::Sid::pageOddBottomMargin);
         double margin = 100.0;
-        double ttboxHeight = ttbox.height() + margin * 2;
+        double ttboxHeight = originalPageHeight + margin * 2;
         double scale = config.height / ttboxHeight;
         config.canvasDpi = scale * engraving::DPI;
     }
