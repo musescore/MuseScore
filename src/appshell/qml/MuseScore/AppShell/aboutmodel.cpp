77c0201beb67eafaf175dbda0a36fef379f4db20
Joachim Schmitz <jojo@schmitz-digital.de>
2026-06-06 17:53:05 +0200

    Fixing URL in the About box clipboard
    the old one makes sense on musescore.org, as it automagically turns into a URL there, but it doesn't work on GitHub. This way it works on both.

--- src/appshell/qml/MuseScore/AppShell/aboutmodel.cpp
+++ src/appshell/qml/MuseScore/AppShell/aboutmodel.cpp
@@ -87,15 +87,13 @@ QVariantMap AboutModel::musicXMLLicenseDeedUrl() const
 void AboutModel::copyRevisionToClipboard() const
 {
     QApplication::clipboard()->setText(
-        QString("OS: %1, Arch.: %2, MuseScore Studio version (%3-bit): %4-%5, revision: github-musescore-musescore-%6")
+        QString("OS: %1, Arch.: %2, MuseScore Studio version (%3-bit): %4-%5, revision: "
+                "[%6](https://github.com/musescore/MuseScore/commit/%6)")
         .arg(QSysInfo::prettyProductName()
              + ((QSysInfo::productType() == "windows" && (QSysInfo::productVersion() == "10" || QSysInfo::productVersion() == "11"))
-                ? " or later" : ""))
-        .arg(QSysInfo::currentCpuArchitecture())
+                ? " or later" : ""), QSysInfo::currentCpuArchitecture())
         .arg(QSysInfo::WordSize)
-        .arg(application()->version().toString())
-        .arg(application()->build())
-        .arg(application()->revision()));
+        .arg(application()->version().toString(), application()->build(), application()->revision()));
 }
 
 void AboutModel::toggleDevMode()
