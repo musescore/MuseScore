0f53d56931118ab33ed7147cc392b42965b65b95
Dmitry Makarenko <kryksyh@gmail.com>
2026-06-24 18:09:30 +0300

    Revert MNX changes until it stabilizes
    relevant discussion: https://github.com/musescore/MuseScore/pull/33865#issuecomment-4761791983

--- buildscripts/cmake/DependencyManifest.cmake
+++ buildscripts/cmake/DependencyManifest.cmake
@@ -5,10 +5,3 @@ require_dep(opus)
 require_dep(libsndfile)
 
 require_source_dep(liblouis)
-
-if (MUE_BUILD_IMPEXP_MNX_MODULE)
-    require_dep(nlohmann_json)
-    require_dep(json_schema_validator)
-    require_source_dep(mnx_w3c)
-    require_source_dep(mnxdom)
-endif()
