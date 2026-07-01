e74d4b6ec1f08b0a40f161ba7365e33ecdbea721
pyonpyoco <60975353+pyonpyoco@users.noreply.github.com>
2026-07-02 01:25:26 +0900

    Refine chord bracket shape collection

--- src/engraving/rendering/score/tlayout.cpp
+++ src/engraving/rendering/score/tlayout.cpp
@@ -870,21 +870,24 @@ void TLayout::layoutArpeggio(const Arpeggio* item, Arpeggio::LayoutData* ldata, 
     }
 }
 
-static Shape chordBracketObstacleShape(const ChordBracket* item)
+static Shape getChordsShape(const ChordBracket* item)
 {
     Chord* owner = item->chord();
-    Shape ownerShape = owner->shape();
-    ownerShape.removeTypes({ ElementType::CHORD_BRACKET, ElementType::ARPEGGIO });
+    Shape result = owner->shape();
+    result.removeTypes({ ElementType::CHORD_BRACKET, ElementType::ARPEGGIO });
     if (!owner->segment() || !owner->staff()) {
-        return ownerShape;
+        return result;
     }
 
-    Shape result;
     const track_idx_t startTrack = staff2track(owner->staffIdx());
     const track_idx_t endTrack = startTrack + VOICES;
 
     for (track_idx_t track = startTrack; track < endTrack; ++track) {
         EngravingItem* e = owner->segment()->element(track);
+        if (track == owner->track()) {
+            continue;
+        }
+
         if (!e || !e->isChord()) {
             continue;
         }
@@ -900,7 +903,7 @@ static Shape chordBracketObstacleShape(const ChordBracket* item)
         result.add(chordShape);
     }
 
-    return result.empty() ? ownerShape : result;
+    return result;
 }
 
 void TLayout::layoutChordBracket(const ChordBracket* item, Arpeggio::LayoutData* ldata, const LayoutConfiguration& conf)
@@ -929,7 +932,7 @@ void TLayout::layoutChordBracket(const ChordBracket* item, Arpeggio::LayoutData*
     const Note* upnote = item->chord()->upNote();
     ldata->setPosY(upnote->y() + upnote->ldata()->bbox().top());
 
-    Shape chordShape = chordBracketObstacleShape(item);
+    Shape chordShape = getChordsShape(item);
     Shape itemShape = item->shape().translated(PointF(0.0, item->ldata()->pos().y()));
     if (item->rightSide()) {
         double x = HorizontalSpacing::minHorizontalDistance(chordShape, itemShape, spatium);
