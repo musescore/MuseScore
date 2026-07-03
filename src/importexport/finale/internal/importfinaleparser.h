fe15a049cd90a8fc8a1d564c80927c998bc85df8
XiaoMigros <xiaomigros3@gmail.com>
2026-07-03 11:37:56 +0200

    Fix codestyle

--- src/importexport/finale/internal/importfinaleparser.h
+++ src/importexport/finale/internal/importfinaleparser.h
@@ -428,14 +428,13 @@ private:
     engraving::ChordRest* chordRestFromEntryInfoPtr(const musx::dom::EntryInfoPtr& entryInfoPtr);
     musx::util::ArpeggioSpanOptions arpeggioSpanOptions() const;
     bool createArpeggioForChordRange(const musx::util::ArpeggioSpanCandidate& span,
-                                     const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef,
-                                     bool visible, engraving::Chord* topChord, engraving::Chord* bottomChord);
+                                     const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef, bool visible,
+                                     engraving::Chord* topChord, engraving::Chord* bottomChord);
     bool createPartSplitArpeggios(const musx::util::ArpeggioSpanCandidate& span,
-                                  const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef,
-                                  bool visible, engraving::Chord* topChord, engraving::Chord* bottomChord);
+                                  const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef, bool visible,
+                                  engraving::Chord* topChord, engraving::Chord* bottomChord);
     bool createArpeggioFromSpan(const musx::util::ArpeggioSpanCandidate& span,
-                                const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef = nullptr,
-                                bool visible = true);
+                                const musx::dom::MusxInstance<musx::dom::others::ArticulationDef>& articDef = nullptr, bool visible = true);
 
     // styles
     void importStyles();
