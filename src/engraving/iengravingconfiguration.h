896966243e9c59c7bd3e39fe856ca6fe4c046b54
Alexander Pavlov <sanch.pavlov@gmail.com>
2026-07-08 10:59:47 +0300

    fix: use direct draw without image scaling

--- src/engraving/iengravingconfiguration.h
+++ src/engraving/iengravingconfiguration.h
@@ -144,9 +144,6 @@ public:
     virtual bool specificSlursLayoutWorkaround() const = 0;
     virtual bool preferSameStringForTranspose() const = 0;
     virtual void setPreferSameStringForTranspose(bool preferSameString) = 0;
-
-    virtual int maxScaledImageDim() const = 0;
-    virtual void setMaxScaledImageDim(int maxDim) = 0;
     virtual bool keepDeadNotesUnchangedOnTranspose() const = 0;
 };
 }
