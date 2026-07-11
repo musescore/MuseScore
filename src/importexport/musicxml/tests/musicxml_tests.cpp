8c12cb3d344326ee1cc263151511c7e6f46096ad
rettinghaus <klaus.rettinghaus@gmail.com>
2026-07-11 17:08:09 +0200

    add test

--- src/importexport/musicxml/tests/musicxml_tests.cpp
+++ src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -1053,6 +1053,9 @@ TEST_F(MusicXml_Tests, numberedLyrics) {
 TEST_F(MusicXml_Tests, numerals) {
     musicXmlIoTest("testNumerals");
 }
+TEST_F(MusicXml_Tests, octaveShift) {
+    musicXmlIoTest("testOctaveShift");
+}
 TEST_F(MusicXml_Tests, ornaments) {
     musicXmlIoTest("testOrnaments");
 }
