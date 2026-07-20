748e380717d4eb42042639f649c979490c7ad953
Alexander Pavlov <sanch.pavlov@gmail.com>
2026-07-20 03:55:14 +0300

    gp-import: infrastructure for imrporting simple dive

--- src/importexport/guitarpro/internal/guitarbendimport/divedatacollector.cpp
+++ src/importexport/guitarpro/internal/guitarbendimport/divedatacollector.cpp
@@ -5,7 +5,7 @@
  * MuseScore Studio
  * Music Composition & Notation
  *
- * Copyright (C) 2024 MuseScore Limited and others
+ * Copyright (C) 2026 MuseScore Limited and others
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -19,28 +19,34 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "guitarbendimporter.h"
+#include "divedatacollector.h"
+
+#include <engraving/dom/chord.h>
+#include <engraving/dom/note.h>
+
+#include "diveinfoconverter.h"
 
 using namespace mu::engraving;
 
 namespace mu::iex::guitarpro {
-GuitarBendImporter::GuitarBendImporter(Score* score)
-    : m_bendCollector(score), m_score(score)
+void DiveDataCollector::collectDiveData(const Chord* chord, const PitchValues& pitchValues)
 {
-}
+    if (pitchValues.size() < 2) {
+        return;
+    }
 
-void GuitarBendImporter::collectBend(Note* note, const mu::engraving::PitchValues& pitchValues)
-{
-    m_bendCollector.storeBendData(note, pitchValues);
-}
+    const track_idx_t track = chord->track();
+    const Fraction tick = chord->tick();
 
-void GuitarBendImporter::collectDive(const mu::engraving::Chord* chord, const mu::engraving::PitchValues& pitchValues)
-{
-    m_diveCollector.collectDiveData(chord, pitchValues);
-}
+    for (const Note* note : chord->notes()) {
+        ImportedDiveInfo info = DiveInfoConverter::fillDiveInfo(note, pitchValues);
+        if (info.type != DiveType::DIVE || !info.note) {
+            continue;
+        }
 
-void GuitarBendImporter::addElementsToScore()
-{
-    m_bendBuilder.addElementsToScore(m_score, m_bendCollector.collectBendDataContext(), m_diveCollector.context());
+        const size_t noteIdx = muse::indexOf(chord->notes(), note);
+
+        m_ctx.graceAfterDiveData[track][tick][noteIdx].data = std::move(info.graceDiveSegments);
+    }
 }
 } // namespace mu::iex::guitarpro
