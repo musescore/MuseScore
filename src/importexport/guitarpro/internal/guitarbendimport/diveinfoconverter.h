774064afab5e5dd5a8ca90631ab0c4e3c46d22a7
Alexander Pavlov <sanch.pavlov@gmail.com>
2026-07-20 03:55:14 +0300

    gp-import: infrastructure for imrporting simple dive

--- src/importexport/guitarpro/internal/guitarbendimport/diveinfoconverter.h
+++ src/importexport/guitarpro/internal/guitarbendimport/diveinfoconverter.h
@@ -5,7 +5,7 @@
  * MuseScore Studio
  * Music Composition & Notation
  *
- * Copyright (C) 2024 MuseScore Limited and others
+ * Copyright (C) 2026 MuseScore Limited and others
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -19,28 +19,24 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#include "guitarbendimporter.h"
+#pragma once
 
-using namespace mu::engraving;
+#include <engraving/types/pitchvalue.h>
+
+#include "guitarbendimporttypes.h"
+
+namespace mu::engraving {
+class Note;
+}
 
 namespace mu::iex::guitarpro {
-GuitarBendImporter::GuitarBendImporter(Score* score)
-    : m_bendCollector(score), m_score(score)
-{
-}
+int gpPitchToQuarterTones(int gpPitch);
 
-void GuitarBendImporter::collectBend(Note* note, const mu::engraving::PitchValues& pitchValues)
+class DiveInfoConverter
 {
-    m_bendCollector.storeBendData(note, pitchValues);
-}
+public:
+    static ImportedDiveInfo fillDiveInfo(const mu::engraving::Note* note, const mu::engraving::PitchValues& pitchValues);
 
-void GuitarBendImporter::collectDive(const mu::engraving::Chord* chord, const mu::engraving::PitchValues& pitchValues)
-{
-    m_diveCollector.collectDiveData(chord, pitchValues);
-}
-
-void GuitarBendImporter::addElementsToScore()
-{
-    m_bendBuilder.addElementsToScore(m_score, m_bendCollector.collectBendDataContext(), m_diveCollector.context());
-}
+    static ImportedDiveInfo fromPitchValues(const mu::engraving::PitchValues& pitchValues);
+};
 } // namespace mu::iex::guitarpro
