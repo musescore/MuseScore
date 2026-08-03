910d6e13a87f6e7c63a9ab5ec35f0fc864558533
ajuncosa <ajuncosa@protonmail.com>
2026-08-03 12:54:17 +0200

    Copy master score's metaTags to part score on MscSaver::exportPart

--- src/engraving/rw/mscsaver.cpp
+++ src/engraving/rw/mscsaver.cpp
@@ -183,6 +183,14 @@ bool MscSaver::exportPart(Score* partScore, MscWriter& mscWriter)
         mscWriter.writeStyleFile(excerptStyleData);
     }
 
+    // Add the master score's metaTags to the part
+    const std::map<String, String> partTags = partScore->metaTags();
+    std::map<String, String> effectiveTags = partScore->masterScore()->metaTags();
+    for (const auto& [key, val] : partTags) {
+        effectiveTags.insert_or_assign(key, val);
+    }
+    partScore->setMetaTags(effectiveTags);
+
     // Write excerpt as main score
     {
         ByteArray excerptData;
@@ -193,6 +201,9 @@ bool MscSaver::exportPart(Score* partScore, MscWriter& mscWriter)
 
         mscWriter.writeScoreFile(excerptData);
     }
+
+    // restore in-memory part score's metaTags
+    partScore->setMetaTags(partTags);
 
     // Write thumbnail
     {
