354ab0679dccc8711831cc60cd7e432197f439f5
Calum Matheson <mathesoncalum@gmail.com>
2026-08-04 15:09:10 +0200

    Fix potential nullptr dereferences in importtef.cpp

--- src/importexport/tabledit/internal/importtef.cpp
+++ src/importexport/tabledit/internal/importtef.cpp
@@ -332,6 +332,7 @@ static void addVolta(Score* score, Measure* measure, const Ending& ending)
         Measure* next = endMeasure->nextMeasure();
         if (!next) {
             LOGD() << "Ending at " << "TBD" << " specifies non-existent end measure.";
+            break;
         }
         endMeasure = next;
     }
