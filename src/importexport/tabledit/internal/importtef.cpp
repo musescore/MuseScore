3e51738e3d2e47a516d6d39aefe2cd8aab1ba557
Calum Matheson <mathesoncalum@gmail.com>
2026-08-04 15:16:13 +0200

    Fix potential out-of-bounds access in importtef.cpp

--- src/importexport/tabledit/internal/importtef.cpp
+++ src/importexport/tabledit/internal/importtef.cpp
@@ -944,6 +944,10 @@ void TablEdit::createTexts()
         }
 
         StaffText* staffText = Factory::createStaffText(segment);
+        if (textMarker.index < 0 || textMarker.index >= static_cast<int>(tefTexts.size())) {
+            LOGE() << "text marker index invalid";
+            continue;
+        }
         muse::String text { tefTexts.at(textMarker.index).c_str() };
         staffText->setPlainText(text);
         staffText->setTrack(track);
