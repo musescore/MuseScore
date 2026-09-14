c5c720b66f737f1d7efd675ebef1cda0cb00b7b0
Steve Bennett <me@stevebennett.me>
2026-09-14 14:22:46 +1000

    Support "Add dynamic" when hairpin selected. Fixes #34200.

--- src/notationscene/internal/notationactioncontroller.h
+++ src/notationscene/internal/notationactioncontroller.h
@@ -103,6 +103,7 @@ public:
 
     bool isNoteInputActionAllowed() const override;
     bool isNoteOrRestSelected() const override;
+    bool isHairpinSelected() const override;
     bool isMoveSelectionAvailable(MoveSelectionType type) const override;
 
     bool isToggleLayoutBreakAvailable() const override;
@@ -179,6 +180,7 @@ private:
     void insertClef(mu::engraving::ClefType type);
 
     void addText(TextStyleType type);
+    void addDynamic();
     void addImage();
     void addFiguredBass();
     void addGuitarBend(GuitarBendType bendType);
