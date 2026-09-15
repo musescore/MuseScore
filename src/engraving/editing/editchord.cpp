36b1cbcdd57f54efab092f8e9669e765f5b59b28
Calum Matheson <mathesoncalum@gmail.com>
2026-09-15 11:21:57 +0200

    Avoid potential problems when failing to find linked notes

--- src/engraving/editing/editchord.cpp
+++ src/engraving/editing/editchord.cpp
@@ -179,7 +179,11 @@ void EditChord::undoRemoveParenthesesFromNote(Chord* chord, Note* note, Parenthe
         if (!linkedLeftParen) {
             continue;
         }
+
         Note* linkedNote = toNote(note->findLinkedInStaff(linkedStaff));
+        if (!linkedNote) {
+            continue;
+        }
 
         const NoteParenthesisInfo* noteParenInfo = linkedChord->findNoteParenthesisInfo(linkedNote);
         if (noteParenInfo) {
@@ -214,6 +218,9 @@ void EditChord::undoClearParenthesisGroup(Chord* chord, std::vector<Note*> notes
         std::vector<Note*> linkedNotes;
         for (Note* note : notes) {
             Note* linkedNote = toNote(note->findLinkedInStaff(linkedStaff));
+            if (!linkedNote) {
+                continue;
+            }
             linkedNotes.push_back(linkedNote);
         }
 
